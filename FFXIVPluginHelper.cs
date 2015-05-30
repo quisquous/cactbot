dacb56327458c22cffbe94f9237bacdbae255bd9
Adrienne Walker <enne@quisquo.us>
2015-05-30 12:58:05 -0700

    Local modifications to FFXIVPluginHelper
    Add appropriate license, fix compilation issues.

--- FFXIVPluginHelper.cs
+++ FFXIVPluginHelper.cs
@@ -1,4 +1,26 @@
-﻿using System;
+﻿// From https://github.com/xtuaok/ACT_EnmityPlugin/
+//
+// Copyright (c) 2015 Tomonori Tamagawa
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Reflection;
@@ -7,14 +29,21 @@ using Advanced_Combat_Tracker;
 using System.Runtime.InteropServices;
 
 
-namespace Tamagawa.EnmityPlugin
+namespace ACTBossTime
 {
     public static class FFXIVPluginHelper
     {
         private static Regex regVersion = new Regex(@"FileVersion: (?<version>\d+\.\d+\.\d+\.\d+)\n", RegexOptions.Compiled | RegexOptions.IgnoreCase);
         private static ActPluginData _plugin = null;
 
-        public static object Instance
+        static class NativeMethods
+        {
+            // ReadProcessMemory
+            [DllImport("kernel32.dll")]
+            public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, IntPtr nSize, ref IntPtr lpNumberOfBytesRead);
+        }
+
+        public static ActPluginData Instance
         {
             get
             {
