0fc3b54c3727a85e1abf006148f10b2ee4172242
Timothy Simpson <simothytimpson@gmail.com>
2015-06-25 00:55:07 -0500

    new zones first

--- html/huntlist.js
+++ html/huntlist.js
@@ -7,18 +7,38 @@ var huntList = {
 	{ name: "Kaiser Behemoth", rank: "S" },
     ],
     "Dravanian Forelands": [
-
+	{ name: "Gnath Cometdrone", rank: "B" },
+	{ name: "Thextera", rank: "B" },
+	{ name: "Pylraster", rank: "A" },
+	{ name: "Lord of the Wyverns", rank: "A" },
+	{ name: "Senmurb", rank: "S" },
     ],
     "Dravanian Hinterlands": [
-
+	{ name: "Pterygotus", rank: "B" },
+	{ name: "Gigantopithecus", rank: "B" },
+	{ name: "Slipkinx Steeljoints", rank: "A" },
+	{ name: "Stolas", rank: "A" },
+	{ name: "The Pale Rider", rank: "S" },
     ],
     "The Churning Winds": [
-
+	{ name: "Scitalis", rank: "B" },
+	{ name: "The Scarecrow", rank: "B" },
+	{ name: "Bune", rank: "A" },
+	{ name: "Agathos", rank: "A" },
+	{ name: "Gandarewa", rank: "S" },
     ],
     "The Sea of Clouds": [
-
+	{ name: "Squonk", rank: "B" },
+	{ name: "Sanu Vali of Dancing Wings", rank: "B" },
+	{ name: "Enkelados", rank: "A" },
+	{ name: "Sisiutl", rank: "A" },
+	{ name: "Bird of Paradise", rank: "S" },
     ],
     "Azys Lla": [
-
-    ],
+	{ name: "Lycidas", rank: "B" },
+	{ name: "Omni", rank: "B" },
+	{ name: "Campacti", rank: "A" },
+	{ name: "Stench Blossom", rank: "A" },
+	{ name: "Leucrotta", rank: "S" },
+    ]
 };
