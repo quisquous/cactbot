f2d909a8622bfa013396cec33f38a72e938f001f
Adrienne Walker <enne@quisquo.us>
2015-06-24 19:33:55 -0700

    Add hunt list structure

---
+++ html/huntlist.js
@@ -0,0 +1,24 @@
+var huntList = {
+    "Coerthas Western Highlands": [
+	{ name: "Alteci", rank: "B" },
+	{ name: "Kreutzet", rank: "B" },
+	{ name: "Mirka", rank: "A" },
+	{ name: "Lyuba", rank: "A" },
+	{ name: "Kaiser Behemoth", rank: "S" },
+    ],
+    "Dravanian Forelands": [
+
+    ],
+    "Dravanian Hinterlands": [
+
+    ],
+    "The Churning Winds": [
+
+    ],
+    "The Sea of Clouds": [
+
+    ],
+    "Azys Lla": [
+
+    ],
+};
