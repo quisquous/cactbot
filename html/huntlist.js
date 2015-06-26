c996c700de01dfb6a14c695d39dee2255198ec2b
Timothy Simpson <simothytimpson@gmail.com>
2015-06-25 23:14:29 -0500

    Removing duplicates for aliases, normalizing indents

--- html/huntlist.js
+++ html/huntlist.js
@@ -1,150 +1,133 @@
 var huntList = {
-    "Central Shroud": [
-	{ name: "White Joker", rank: "B" },
-	{ name: "Forneus", rank: "A" },
-	{ name: "Laideronnette", rank: "S" },
-    ],
-    "Central Thanalan": [
-	{ name: "Ovjang", rank: "B" },
-	{ name: "Sabotender Bailarina", rank: "A" },
-	{ name: "Brontes", rank: "S" },
-    ],
-    "Coerthas Central Highlands": [
-	{ name: "Naul", rank: "B" },
-	{ name: "Marraco", rank: "A" },
-	{ name: "Safat", rank: "S" },
-    ],
-    "East Shroud": [
-	{ name: "Stinging Sophie", rank: "B" },
-	{ name: "Melt", rank: "A" },
-	{ name: "Wulgaru", rank: "S" },
-    ],
-    "Eastern La Noscea": [
-	{ name: "Bloody Mary", rank: "B" },
-	{ name: "Hellsclaw", rank: "A" },
-	{ name: "Garlok", rank: "S" },
-    ],
-    "Eastern Thanalan": [
-	{ name: "Gatling", rank: "B" },
-	{ name: "Maahes", rank: "A" },
-	{ name: "Lampalagua", rank: "S" },
-    ],
-    "Lower La Noscea": [
-	{ name: "Barbastelle", rank: "B" },
-	{ name: "Unktehi", rank: "A" },
-	{ name: "Croakadile", rank: "S" },
-    ],
-    "Middle La Noscea": [
-	{ name: "Skogs Fru", rank: "B" },
-	{ name: "Vogaal Ja", rank: "A" },
-	{ name: "Croque-Mitaine", rank: "S" },
-    ],
-    "Mor Dhona": [
-	{ name: "Leech King", rank: "B" },
-	{ name: "Kurrea", rank: "A" },
-	{ name: "Agrippa the Mighty", rank: "S" },
-    ],
-    "North Shroud": [
-	{ name: "Phecda", rank: "B" },
-	{ name: "Girtab", rank: "A" },
-	{ name: "Thousand-cast Theda", rank: "S" },
-    ],
-    "Northern Thanalan": [
-	{ name: "Flame Sergeant Dalvag", rank: "B" },
-	{ name: "Dalvag's Final Flame", rank: "A" },
-	{ name: "Minhocao", rank: "S" },
-    ],
-    "Outer La Noscea": [
-	{ name: "Cornu", rank: "B" },
-	{ name: "Vuokho", rank: "A" },
-	{ name: "Mahisha", rank: "S" },
-    ],
-    "South Shroud": [
-	{ name: "Monarch Ogrefly", rank: "B" },
-	{ name: "Ghede Ti Malice", rank: "A" },
-	{ name: "Mindflayer", rank: "S" },
-    ],
-    "Southern Thanalan": [
-	{ name: "Albin the Ashen", rank: "B" },
-	{ name: "Zanig'oh", rank: "A" },
-	{ name: "Nunyunuwi", rank: "S" },
-    ],
-    "Upper La Noscea": [
-	{ name: "Myradrosh", rank: "B" },
-	{ name: "Marberry", rank: "A" },
-	{ name: "Nandi", rank: "S" },
-    ],
-    "Western La Noscea": [
-	{ name: "Dark Helmet", rank: "B" },
-	{ name: "Nahn", rank: "A" },
-	{ name: "Bonnacon", rank: "S" },
-    ],
-    "Western Thanalan": [
-	{ name: "Sewer Syrup", rank: "B" },
-	{ name: "Alectryon", rank: "A" },
-	{ name: "Zona Seeker", rank: "S" },
-    ],
-    "Coerthas Western Highlands": [
-	{ name: "Alteci", rank: "B" },
-	{ name: "Kreutzet", rank: "B" },
-	{ name: "Mirka", rank: "A" },
-	{ name: "Lyuba", rank: "A" },
-	{ name: "Kaiser Behemoth", rank: "S" },
-    ],
-    "The Dravanian Forelands": [
-        { name: "Gnath Cometdrone", rank: "B" },
-        { name: "Thextera", rank: "B" },
-        { name: "Pylraster", rank: "A" },
-        { name: "Lord of the Wyverns", rank: "A" },
-        { name: "Senmurb", rank: "S" },
-    ],
-    "The Dravinian Forelands": [
-        { name: "Gnath Cometdrone", rank: "B" },
-        { name: "Thextera", rank: "B" },
-        { name: "Pylraster", rank: "A" },
-        { name: "Lord of the Wyverns", rank: "A" },
-        { name: "Senmurb", rank: "S" },
-    ],
-    "The Dravanian Hinterlands": [
-        { name: "Pterygotus", rank: "B" },
-        { name: "Gigantopithecus", rank: "B" },
-        { name: "Slipkinx Steeljoints", rank: "A" },
-        { name: "Stolas", rank: "A" },
-        { name: "The Pale Rider", rank: "S" },
-    ],
-    "The Dravinian Hinterlands": [
-	    { name: "Pterygotus", rank: "B" },
-	    { name: "Gigantopithecus", rank: "B" },
-	    { name: "Slipkinx Steeljoints", rank: "A" },
-	    { name: "Stolas", rank: "A" },
-	    { name: "The Pale Rider", rank: "S" },
-    ],
-    "The Churning Mists": [
-        { name: "Scitalis", rank: "B" },
-        { name: "The Scarecrow", rank: "B" },
-        { name: "Bune", rank: "A" },
-        { name: "Agathos", rank: "A" },
-        { name: "Gandarewa", rank: "S" },
-    ],
-        "The Sea of Clouds": [
-        { name: "Squonk", rank: "B" },
-        { name: "Sanu Vali of Dancing Wings", rank: "B" },
-        { name: "Enkelados", rank: "A" },
-        { name: "Sisiutl", rank: "A" },
-        { name: "Bird of Paradise", rank: "S" },
-    ],
-    "Abalathia's Spine: The Sea Of Clouds": [
-	{ name: "Squonk", rank: "B" },
-	{ name: "Sanu Vali of Dancing Wings", rank: "B" },
-	{ name: "Enkelados", rank: "A" },
-	{ name: "Sisiutl", rank: "A" },
-	{ name: "Bird of Paradise", rank: "S" },
-    ],
-    "Azys Lla": [
-	{ name: "Lycidas", rank: "B" },
-	{ name: "Omni", rank: "B" },
-	{ name: "Campacti", rank: "A" },
-	{ name: "Stench Blossom", rank: "A" },
-	{ name: "Leucrotta", rank: "S" },
-    ]
+	"Central Shroud": [
+		{ name: "White Joker", rank: "B" },
+		{ name: "Forneus", rank: "A" },
+		{ name: "Laideronnette", rank: "S" },
+	],
+	"Central Thanalan": [
+		{ name: "Ovjang", rank: "B" },
+		{ name: "Sabotender Bailarina", rank: "A" },
+		{ name: "Brontes", rank: "S" },
+	],
+	"Coerthas Central Highlands": [
+		{ name: "Naul", rank: "B" },
+		{ name: "Marraco", rank: "A" },
+		{ name: "Safat", rank: "S" },
+	],
+	"East Shroud": [
+		{ name: "Stinging Sophie", rank: "B" },
+		{ name: "Melt", rank: "A" },
+		{ name: "Wulgaru", rank: "S" },
+	],
+	"Eastern La Noscea": [
+		{ name: "Bloody Mary", rank: "B" },
+		{ name: "Hellsclaw", rank: "A" },
+		{ name: "Garlok", rank: "S" },
+	],
+	"Eastern Thanalan": [
+		{ name: "Gatling", rank: "B" },
+		{ name: "Maahes", rank: "A" },
+		{ name: "Lampalagua", rank: "S" },
+	],
+	"Lower La Noscea": [
+		{ name: "Barbastelle", rank: "B" },
+		{ name: "Unktehi", rank: "A" },
+		{ name: "Croakadile", rank: "S" },
+	],
+	"Middle La Noscea": [
+		{ name: "Skogs Fru", rank: "B" },
+		{ name: "Vogaal Ja", rank: "A" },
+		{ name: "Croque-Mitaine", rank: "S" },
+	],
+	"Mor Dhona": [
+		{ name: "Leech King", rank: "B" },
+		{ name: "Kurrea", rank: "A" },
+		{ name: "Agrippa the Mighty", rank: "S" },
+	],
+	"North Shroud": [
+		{ name: "Phecda", rank: "B" },
+		{ name: "Girtab", rank: "A" },
+		{ name: "Thousand-cast Theda", rank: "S" },
+	],
+	"Northern Thanalan": [
+		{ name: "Flame Sergeant Dalvag", rank: "B" },
+		{ name: "Dalvag's Final Flame", rank: "A" },
+		{ name: "Minhocao", rank: "S" },
+	],
+	"Outer La Noscea": [
+		{ name: "Cornu", rank: "B" },
+		{ name: "Vuokho", rank: "A" },
+		{ name: "Mahisha", rank: "S" },
+	],
+	"South Shroud": [
+		{ name: "Monarch Ogrefly", rank: "B" },
+		{ name: "Ghede Ti Malice", rank: "A" },
+		{ name: "Mindflayer", rank: "S" },
+	],
+	"Southern Thanalan": [
+		{ name: "Albin the Ashen", rank: "B" },
+		{ name: "Zanig'oh", rank: "A" },
+		{ name: "Nunyunuwi", rank: "S" },
+	],
+	"Upper La Noscea": [
+		{ name: "Myradrosh", rank: "B" },
+		{ name: "Marberry", rank: "A" },
+		{ name: "Nandi", rank: "S" },
+	],
+	"Western La Noscea": [
+		{ name: "Dark Helmet", rank: "B" },
+		{ name: "Nahn", rank: "A" },
+		{ name: "Bonnacon", rank: "S" },
+	],
+	"Western Thanalan": [
+		{ name: "Sewer Syrup", rank: "B" },
+		{ name: "Alectryon", rank: "A" },
+		{ name: "Zona Seeker", rank: "S" },
+	],
+	"Coerthas Western Highlands": [
+		{ name: "Alteci", rank: "B" },
+		{ name: "Kreutzet", rank: "B" },
+		{ name: "Mirka", rank: "A" },
+		{ name: "Lyuba", rank: "A" },
+		{ name: "Kaiser Behemoth", rank: "S" },
+	],
+	"The Dravanian Forelands": [
+		{ name: "Gnath Cometdrone", rank: "B" },
+		{ name: "Thextera", rank: "B" },
+		{ name: "Pylraster", rank: "A" },
+		{ name: "Lord of the Wyverns", rank: "A" },
+		{ name: "Senmurb", rank: "S" },
+	],
+	"The Dravanian Hinterlands": [
+		{ name: "Pterygotus", rank: "B" },
+		{ name: "Gigantopithecus", rank: "B" },
+		{ name: "Slipkinx Steeljoints", rank: "A" },
+		{ name: "Stolas", rank: "A" },
+		{ name: "The Pale Rider", rank: "S" },
+	],
+	"The Churning Mists": [
+		{ name: "Scitalis", rank: "B" },
+		{ name: "The Scarecrow", rank: "B" },
+		{ name: "Bune", rank: "A" },
+		{ name: "Agathos", rank: "A" },
+		{ name: "Gandarewa", rank: "S" },
+	],
+	"The Sea of Clouds": [
+		{ name: "Squonk", rank: "B" },
+		{ name: "Sanu Vali of Dancing Wings", rank: "B" },
+		{ name: "Enkelados", rank: "A" },
+		{ name: "Sisiutl", rank: "A" },
+		{ name: "Bird of Paradise", rank: "S" },
+	],
+	"Azys Lla": [
+		{ name: "Lycidas", rank: "B" },
+		{ name: "Omni", rank: "B" },
+		{ name: "Campacti", rank: "A" },
+		{ name: "Stench Blossom", rank: "A" },
+		{ name: "Leucrotta", rank: "S" },
+	]
 };
+huntList["The Dravinian Forelands"] = huntList["The Dravanian Forelands"];
+huntList["The Dravinian Hinterlands"] = huntList["The Dravanian Hinterlands"];
+huntList["Abalathia's Spine: The Sea Of Clouds"] = huntList["The Sea of Clouds"];
+
