f4a0635b89d8453b3de4d93f18f170d4c28fd03f
Adrienne Walker <enne@quisquo.us>
2016-05-31 18:07:48 -0700

    Add countdown files

---
+++ html/countdown/countdown.js
@@ -0,0 +1,96 @@
+cb.countdown = {
+    endTime: null,
+    lastCount: null,
+
+    throttleTickMs: 100,
+
+    initialize: function(element) {
+        this.topElement = element;
+    },
+    leaveZone: function() {
+        this.reset();
+    },
+    enterZone: function() {
+        this.reset();
+    },
+    filtersZone: function(zone) {
+        return true;
+    },
+    processLogs: function(logs) {
+        if (act.inCombat()) {
+            return;
+        }
+        var search = 'Pulling in ';
+        var found = null;
+        for (var i = 0; i < logs.length; ++i) {
+            var idx = logs[i].indexOf(search);
+            if (idx == -1) {
+                continue;
+            }
+            found = logs[i].substr(idx + search.length);
+            break;
+        }
+        if (!found) {
+            return;
+        }
+
+        var total = 0;
+        for (var i = 0; i < found.length; ++i) {
+        }
+        total = 20; // OOPS
+
+        // FIXME: handle updates?
+
+        this.endTime= new Date();
+        this.endTime.setSeconds(this.endTime.getSeconds() + total);
+        this.lastCount = total;
+        this.setCount(total);
+    },
+    tick: function(currentTime) {
+        if (!this.endTime) {
+            return;
+        }
+        var timeDiffInSeconds = (this.endTime - currentTime) / 1000;
+        if (timeDiffInSeconds < -1) {
+            this.reset();
+            return;
+        }
+        var newValue = Math.ceil(timeDiffInSeconds);
+        if (newValue == this.lastCount) {
+            return;
+        }
+        this.lastCount = newValue;
+        this.setCount(newValue);
+    },
+    reset: function() {
+        this.topElement.innerText = '';
+        this.endTime = null;
+        this.lastCount = null;
+    },
+    setCount: function(val) {
+        if (val <= 0) {
+            val = "GO!";
+        }
+        this.topElement.innerText = val;
+    },
+};
+
+window.addEventListener('load', function () {
+    cb.util.loadCSS('countdown/countdown.css');
+
+    var element = document.createElement('div');
+    var nodeContainer = document.createElement('div');
+    nodeContainer.classList.add('pullcountdown');
+    element.appendChild(nodeContainer);
+
+    var body = document.getElementsByTagName('body')[0];
+    body.appendChild(element);
+
+    var defaultGeometry = {
+        width: '200px',
+        height: '200px',
+    };
+    cb.windowManager.add('countdown', element, 'countdown', defaultGeometry);
+    cb.countdown.initialize(nodeContainer);
+    cb.updateRegistrar.register(cb.countdown);
+});
