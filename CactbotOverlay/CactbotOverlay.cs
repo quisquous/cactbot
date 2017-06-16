16bfdbc188e499768b93c0143e5d1995efef466b
Adrienne Walker <enne@quisquo.us>
2017-06-16 00:20:03 -0700

    Initial data push

--- CactbotOverlay/CactbotOverlay.cs
+++ CactbotOverlay/CactbotOverlay.cs
@@ -1,22 +1,60 @@
-﻿using RainbowMage.OverlayPlugin;
+﻿using Advanced_Combat_Tracker;
+using FFXIV_ACT_Plugin;
+using RainbowMage.OverlayPlugin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 
 namespace Cactbot
 {
     public class CactbotOverlay : OverlayBase<CactbotOverlayConfig>
     {
+        // Not thread-safe, as OnLogLineRead may happen at any time.
+        List<string> logLines = new List<string>();
+
         public CactbotOverlay(CactbotOverlayConfig config)
             : base(config, config.Name)
         {
+            Advanced_Combat_Tracker.ActGlobals.oFormActMain.OnLogLineRead += OnLogLineRead;
         }
+
+        public override void Dispose()
+        {
+            Advanced_Combat_Tracker.ActGlobals.oFormActMain.OnLogLineRead -= OnLogLineRead;
+            base.Dispose();
+        }
+
+        private void OnLogLineRead(bool isImport, LogLineEventArgs args)
+        {
+            // isImport happens when somebody is importing old encounters and all the log lines are processed.
+            // Don't need to send all of these to the overlay.
+            if (isImport)
+                return;
+            logLines.Add(args.logLine);
+        }
+
+        private class JsonData
+        {
+            public List<string> logs { get; set; }
+            public bool inCombat;
+            public string currentZone;
+        };
 
         public string CreateJson()
         {
-            return "{}";
+            JsonData data = new JsonData()
+            {
+                logs = Interlocked.Exchange(ref logLines, new List<string>()),
+                inCombat = FFXIV_ACT_Plugin.ACTWrapper.InCombat,
+                currentZone = FFXIV_ACT_Plugin.ACTWrapper.CurrentZone,
+            };
+
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            return serializer.Serialize(data);
         }
 
         protected override void Update()
