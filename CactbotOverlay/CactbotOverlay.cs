36440d62ec859ecfa666398bec35bea7e96b7c08
Dana Jansens <danakj@orodu.net>
2017-06-17 23:28:06 -0400

    Format all the csharp
    Settings used:
    <UserSettings><ApplicationIdentity version="15.0"/><ToolsOptions><ToolsOptionsCategory name="TextEditor" RegisteredName="TextEditor"><ToolsOptionsSubCategory name="CSharp" RegisteredName="CSharp" PackageName="Text Management Package"><PropertyValue name="TabSize">2</PropertyValue><PropertyValue name="ShowChanges">true</PropertyValue><PropertyValue name="AutoListMembers">true</PropertyValue><PropertyValue name="ShowPreview">true</PropertyValue><PropertyValue name="ShowMarks">true</PropertyValue><PropertyValue name="IndentStyle">2</PropertyValue><PropertyValue name="ShowCaretPosition">true</PropertyValue><PropertyValue name="HideAdvancedMembers">true</PropertyValue><PropertyValue name="ShowNavigationBar">true</PropertyValue><PropertyValue name="UseMapMode">false</PropertyValue><PropertyValue name="VirtualSpace">false</PropertyValue><PropertyValue name="ShowAnnotations">true</PropertyValue><PropertyValue name="ShowVerticalScrollBar">true</PropertyValue><PropertyValue name="InsertTabs">false</PropertyValue><PropertyValue name="WordWrapGlyphs">true</PropertyValue><PropertyValue name="EnableLeftClickForURLs">true</PropertyValue><PropertyValue name="ShowErrors">true</PropertyValue><PropertyValue name="OverviewWidth">58</PropertyValue><PropertyValue name="ShowLineNumbers">true</PropertyValue><PropertyValue name="WordWrap">false</PropertyValue><PropertyValue name="IndentSize">2</PropertyValue><PropertyValue name="BraceCompletion">true</PropertyValue><PropertyValue name="ShowHorizontalScrollBar">true</PropertyValue><PropertyValue name="CutCopyBlankLines">true</PropertyValue><PropertyValue name="AutoListParams">true</PropertyValue></ToolsOptionsSubCategory></ToolsOptionsCategory></ToolsOptions></UserSettings>

--- CactbotOverlay/CactbotOverlay.cs
+++ CactbotOverlay/CactbotOverlay.cs
@@ -9,92 +9,82 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Script.Serialization;
 
-namespace Cactbot
-{
-    public class CactbotOverlay : OverlayBase<CactbotOverlayConfig>
-    {
-        // Not thread-safe, as OnLogLineRead may happen at any time.
-        List<string> logLines = new List<string>();
+namespace Cactbot {
 
-        public CactbotOverlay(CactbotOverlayConfig config)
-            : base(config, config.Name)
-        {
-            Advanced_Combat_Tracker.ActGlobals.oFormActMain.OnLogLineRead += OnLogLineRead;
-        }
+  public class CactbotOverlay : OverlayBase<CactbotOverlayConfig> {
+    // Not thread-safe, as OnLogLineRead may happen at any time.
+    List<string> logLines = new List<string>();
 
-        public override void Dispose()
-        {
-            Advanced_Combat_Tracker.ActGlobals.oFormActMain.OnLogLineRead -= OnLogLineRead;
-            base.Dispose();
-        }
-
-        private void OnLogLineRead(bool isImport, LogLineEventArgs args)
-        {
-            // isImport happens when somebody is importing old encounters and all the log lines are processed.
-            // Don't need to send all of these to the overlay.
-            if (isImport)
-                return;
-            logLines.Add(args.logLine);
-        }
-
-        private class JsonData
-        {
-            public List<string> logs { get; set; }
-            public bool inCombat;
-            public string currentZone;
-        };
-
-        public string CreateJson()
-        {
-            JsonData data = new JsonData()
-            {
-                logs = Interlocked.Exchange(ref logLines, new List<string>()),
-                inCombat = FFXIV_ACT_Plugin.ACTWrapper.InCombat,
-                currentZone = FFXIV_ACT_Plugin.ACTWrapper.CurrentZone,
-            };
-
-            JavaScriptSerializer serializer = new JavaScriptSerializer();
-            return serializer.Serialize(data);
-        }
-
-        private string CreateEventDispatcherScript()
-        {
-            return "var ActXiv = " + CreateJson() + ";\n" +
-               "document.dispatchEvent(new CustomEvent('onOverlayDataUpdate', { detail: ActXiv }));";
-        }
-
-        protected override void Update()
-        {
-            // MESSAGES
-            // browser -> overlay
-            // cactbot.enterZone(str)
-            // cactbot.leaveZone(str)
-            // cactbot.wipe
-            // cactbot.tick(dataJsonMess)
-
-            // overlay -> broadcast (uhh could just do this in c#, but loading json?)
-            // cactbot.rotation.startfight
-            // cactbot.rotation.endfight
-            // cactbot.rotation.startphase
-            // cactbot.rotation.endPhase
-
-            // DATA JSON MESS
-            // getPlayer -> combatant (posX, posY, posZ, currentHP)
-            // getMobByName -> combatant
-            // inCombat (is this necessary??)
-            // getLogLines
-            // getCurrentPartyList
-            // encounterDPSInfo
-            // combatantDPSInfo
-
-            if (this.Overlay == null)
-                return;
-            if (this.Overlay.Renderer == null)
-                return;
-            if (this.Overlay.Renderer.Browser == null)
-                return;
-
-            this.Overlay.Renderer.Browser.GetMainFrame().ExecuteJavaScript(CreateEventDispatcherScript(), null, 0);
-        }
+    public CactbotOverlay(CactbotOverlayConfig config)
+        : base(config, config.Name) {
+      Advanced_Combat_Tracker.ActGlobals.oFormActMain.OnLogLineRead += OnLogLineRead;
     }
+
+    public override void Dispose() {
+      Advanced_Combat_Tracker.ActGlobals.oFormActMain.OnLogLineRead -= OnLogLineRead;
+      base.Dispose();
+    }
+
+    private void OnLogLineRead(bool isImport, LogLineEventArgs args) {
+      // isImport happens when somebody is importing old encounters and all the log lines are processed.
+      // Don't need to send all of these to the overlay.
+      if (isImport)
+        return;
+      logLines.Add(args.logLine);
+    }
+
+    private class JsonData {
+      public List<string> logs { get; set; }
+      public bool inCombat;
+      public string currentZone;
+    }
+
+    public string CreateJson() {
+      JsonData data = new JsonData() {
+        logs = Interlocked.Exchange(ref logLines, new List<string>()),
+        inCombat = FFXIV_ACT_Plugin.ACTWrapper.InCombat,
+        currentZone = FFXIV_ACT_Plugin.ACTWrapper.CurrentZone,
+      };
+
+      JavaScriptSerializer serializer = new JavaScriptSerializer();
+      return serializer.Serialize(data);
+    }
+
+    protected override void Update() {
+      // MESSAGES
+      // browser -> overlay
+      // cactbot.enterZone(str)
+      // cactbot.leaveZone(str)
+      // cactbot.wipe
+      // cactbot.tick(dataJsonMess)
+
+      // overlay -> broadcast (uhh could just do this in c#, but loading json?)
+      // cactbot.rotation.startfight
+      // cactbot.rotation.endfight
+      // cactbot.rotation.startphase
+      // cactbot.rotation.endPhase
+
+      // DATA JSON MESS
+      // getPlayer -> combatant (posX, posY, posZ, currentHP)
+      // getMobByName -> combatant
+      // inCombat (is this necessary??)
+      // getLogLines
+      // getCurrentPartyList
+      // encounterDPSInfo
+      // combatantDPSInfo
+
+      if (this.Overlay == null)
+        return;
+      if (this.Overlay.Renderer == null)
+        return;
+      if (this.Overlay.Renderer.Browser == null)
+        return;
+
+      string jsdispatch = "var ActXiv = " + CreateJson() + ";\n" +
+        "document.dispatchEvent(new CustomEvent('onOverlayDataUpdate', { detail: ActXiv }));";
+
+      this.Overlay.Renderer.Browser.GetMainFrame().ExecuteJavaScript(jsdispatch, null, 0);
+    }
+  }
 }
+
