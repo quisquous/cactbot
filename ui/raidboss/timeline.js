9a8aa851908f62902d9bd9a6898431f2068e38ad
Dana Jansens <danakj@orodu.net>
2017-08-29 13:21:27 -0400

    timeline: Allow negative 'before' to show a message after an event has happened

--- ui/raidboss/timeline.js
+++ ui/raidboss/timeline.js
@@ -29,7 +29,8 @@ function computeBackgroundColorFrom(element, classList) {
 // # occurance of events with that name. By default the name of the event will be shown,
 // # but you may specify the text to be shown at the end of the line if it should be
 // # different. The |before| parameter must be present, but can be 0 if the text should
-// # be shown at the same time the event happens.
+// # be shown at the same time the event happens. Negative values can be used to show
+// # the text after the event.
 // #
 // # Example which shows the event name 1s before the event happens.
 // infotext "event name" before 1
@@ -87,7 +88,7 @@ class Timeline {
         continue;
       }
 
-      match = line.match(/^(info|alert|alarm)text \"([^"]+)\" +before +([0-9]+(?:\.[0-9]+)?)(?: +\"([^"]+)\")?$/)
+      match = line.match(/^(info|alert|alarm)text \"([^"]+)\" +before +(-?[0-9]+(?:\.[0-9]+)?)(?: +\"([^"]+)\")?$/)
       if (match != null) {
         texts[match[2]] = texts[match[2]] || [];
         texts[match[2]].push({
