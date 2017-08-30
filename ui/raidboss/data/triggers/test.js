d0456ddd2c5deebd1401b8d07f48522a7b5a81da
Dana Jansens <danakj@orodu.net>
2017-08-30 12:00:55 -0400

    Show different text for each call to :test:trigger:

--- ui/raidboss/data/triggers/test.js
+++ ui/raidboss/data/triggers/test.js
@@ -3,11 +3,11 @@
   triggers: [
     {
       regex: /:test:trigger:/,
-      infoText: 'This in info',
-      alertText: 'Alert is like this',
-      alarmText: 'Alarm is here',
-      sound: 'Long',
-      run: function(data) { console.log('me: ' + data.me + ' / job: ' + data.job + ' / role :' + data.role); }
+      infoText: function(data) { data.i = (data.i || 0) + 1; return 'This is an info ' + data.i },
+      alertText: function(data) { data.t = (data.t || 0) + 1; return 'This is an alert ' + data.t },
+      alarmText: function(data) { data.m = (data.m || 0) + 1; return 'This is an alarm ' + data.m },
+      //sound: 'Long',
+      //run: function(data) { console.log('me: ' + data.me + ' / job: ' + data.job + ' / role :' + data.role); }
     },
   ]
 }]
