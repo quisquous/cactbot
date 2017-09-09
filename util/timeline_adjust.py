70e55da36982217e5bcd983128a984be1aae84ab
Adrienne Walker <enne@quisquo.us>
2017-09-09 11:56:32 -0700

    Add python utility to adjust timelines
    It's kind of a pain to adjust all the times in a file or a section by a
    particular amount, so this python file will do it for you.

---
+++ util/timeline_adjust.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+from __future__ import print_function
+import argparse
+import re
+
+time_re = re.compile(r"^\s*#?\s*([0-9]+(?:\.[0-9]+)?)\s+\"")
+first_num_re = re.compile(r"([0-9]+(?:\.[0-9]+)?)")
+
+
+def adjust_lines(lines, adjust):
+  for line in lines:
+    match = re.match(time_re, line)
+    if match:
+      time = float(match.group(1)) + adjust
+      print(re.sub(first_num_re, str(time), line, 1), end='')
+    else:
+      print(line, end='')
+
+def main():
+  parser = argparse.ArgumentParser(
+      description="A utility to uniformly adjust times in an act timeline file")
+  parser.add_argument('--file', required=True, type=argparse.FileType('r', 0),
+      help="The timeline file to adjust times in")
+  parser.add_argument('--adjust', required=True, type=float,
+      help="The amount of time to adjust each entry by")
+
+  args = parser.parse_args()
+  adjust_lines(args.file, args.adjust)
+
+
+if __name__ == "__main__":
+  main()
