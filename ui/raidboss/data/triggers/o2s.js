86b1d349edbd0f1ded330a7d805d8ff95ce0d238
Dana Jansens <danakj@orodu.net>
2017-09-17 10:43:55 -0400

    Trigger 6 Fulms under only once
    The debuff gets applied in the logs repeatedly, so only catch the first time
    and prevent re-trigger until the debuff falls off.

--- ui/raidboss/data/triggers/o2s.js
+++ ui/raidboss/data/triggers/o2s.js
@@ -78,7 +78,14 @@
       delaySeconds: 5,
       infoText: function(data) { if (data.levitating) return '6 Fulms Under'; },
       alertText: function(data) { if (!data.levitating) return '6 Fulms Under: Levitate'; },
+      condition: function(data, matches) { return !data.under && matches[1] == data.me; },
+      run: function(data) { data.under = true; }
+    },
+    {
+      id: 'O2S 6 Fulms Under',
+      regex: /:(\y{Name}) loses the effect of 6 Fulms Under from/,
       condition: function(data, matches) { return matches[1] == data.me; },
+      run: function(data) { data.under = false; }
     },
   ]
 }]
