24953d6e015fe09ed3febc88b8cb654bdfcd85c5
Adrienne Walker <enne@quisquo.us>
2017-09-30 21:51:27 -0700

    Fix o3s library phase triggers
    Library phase stack/prey markers mess up the counting for spellblade holy,
    which was counting sets of 4.  (It would also call out somebody to stack
    on, which was also not super useful.)
    
    Add a phase counter to differentiate which of these spellblade holies
    it is.  Add a separate trigger for library phase markers.

--- ui/raidboss/data/triggers/o3s.js
+++ ui/raidboss/data/triggers/o3s.js
@@ -3,6 +3,25 @@
   zoneRegex: /(Deltascape V3.0 \(Savage\)|Unknown Zone \(2B9\))/,
   triggers: [
     {
+      id: 'O3S Phase Counter',
+      regex: /:Halicarnassus starts Panel Swap/,
+      run: function(data) {
+        data.phase = (data.phase || 0) + 1;
+
+        // In case something went awry, clean up any holy targets
+        // so the next spellblade holy can start afresh.
+        delete data.holyTargets;
+      },
+    },
+    {
+      // Normal spellblade holy with tethers and one stack point.
+      // "64" is a stack marker.  "65" is the prey marker.
+      // The debuff order in the logs is:
+      //   (1) stack marker (tethered to #2)
+      //   (2) prey marker (tethered to #1)
+      //   (3) prey marker (tethered to #4)
+      //   (4) prey marker (tethered to #3)
+      // So, #2 is the person everybody should stack on.
       id: 'O3S Spellblade Holy',
       regex: /1B:........:(\y{Name}):....:....:006[45]:0000:0000:0000:/,
       alarmText: function(data) {
@@ -20,6 +39,9 @@
         return "Stack on " + data.holyTargets[1];
       },
       condition: function(data, matches) {
+        // Library phase stack markers behave differently.
+        if (data.phase == 3)
+          return;
         data.holyTargets = data.holyTargets || [];
         data.holyTargets.push(matches[1]);
         return data.holyTargets.length == 4;
@@ -27,6 +49,36 @@
       run: function(data) {
         delete data.holyTargets;
       }
+    },
+    {
+      // Library phase spellblade holy with 2 stacks / 4 preys / 2 unmarked.
+      id: 'O3S Library Spellblade',
+      regex: /1B:........:(\y{Name}):....:....:(006[45]):0000:0000:0000:/,
+      alertText: function(data) {
+        if (data.librarySpellbladePrinted)
+          return;
+        data.librarySpellbladePrinted = true;
+        if (data.librarySpellbladeMe == "0064")
+          return "outside south (stack)";
+        if (data.librarySpellbladeMe == "0065")
+          return "go north";
+        return "inside south (unmarked stack)";
+      },
+      // Because people can be dead and there are eight marks, delay to
+      // accumulate logs instead of counting marks.  Instantly print if
+      // anything is on you.  The 6 triggers will all have condition=true
+      // and run, but only the first one will print.
+      delaySeconds: function(data, matches) {
+        return matches[1] == data.me ? 0 : 0.5;
+      },
+      condition: function(data, matches) {
+        // This is only for library phase.
+        if (data.phase != 3)
+          return;
+        if (matches[1] == data.me)
+          data.librarySpellbladeMe = matches[2];
+        return true;
+      },
     },
     {
       id: 'O3S Right Face',
