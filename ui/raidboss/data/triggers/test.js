9f639e4b1089bce4af8c9cf173731d29e09bb032
Adrienne Walker <enne@quisquo.us>
2017-10-03 23:20:23 -0700

    Add test timeline in Summerford Farms

---
+++ ui/raidboss/data/triggers/test.js
@@ -0,0 +1,24 @@
+[{
+  zoneRegex: /^Middle La Noscea$/,
+  triggers: [
+    {
+      regex: /:You poke the striking dummy/,
+      infoText: function(data) {
+        data.pokes = (data.pokes || 0) + 1;
+        return "poke #" + data.pokes;
+      },
+    },
+    {
+      regex: /:You psych yourself up alongside the striking dummy/,
+      alertText: function(data) {
+        return "PSYCH!!!";
+      },
+    },
+    {
+      regex: /:You burst out laughing at the striking dummy/,
+      alarmText: function(data) {
+        return "hahahahaha";
+      },
+    },
+  ],
+}]
