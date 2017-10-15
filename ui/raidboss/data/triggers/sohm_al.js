bec860e4262b3e9df8a0b232c2e71a94e03c08c2
Adrienne Walker <enne@quisquo.us>
2017-10-15 13:26:31 -0700

    Add Sohm Al dungeon triggers

---
+++ ui/raidboss/data/triggers/sohm_al.js
@@ -0,0 +1,38 @@
+// Sohm Al (normal)
+// Nobody remembers what to do here, so here's triggers.
+[{
+  zoneRegex: /^Sohm Al$/
+  triggers: [
+    {
+      id: 'Sohm Al Myath Stack',
+      regex: /1B:........:(\y{Name}):....:....:0017:0000:0000:0000:/,
+      alertText: function(data) {
+        if (data.matches[1] == data.me)
+          return 'Stack on YOU';
+        return 'Stack on ' + matches[1];
+      },
+    },
+    {
+      id: 'Sohm Al Myath Spread',
+      regex: /1B:........:(\y{Name}):....:....:00AE:0000:0000:0000:/,
+      alertText: function(data) {
+        if (data.matches[1] == data.me)
+          return 'Move away from others';
+        return 'Move away from ' + matches[1];
+      },
+    },
+    {
+      id: 'Sohm Al Myath Chyme',
+      regex: /:Added new combatant Chyme Of The Mountain/
+      alertText: function(data) { return 'Kill Chyme Add'; },
+    },
+    {
+      id: 'Sohm Al Tioman Meteor',
+      regex: /1B:........:(\y{Name}):....:....:0007:0000:0000:0000:/,
+      alertText: function(data) {
+        if (data.matches[1] == data.me)
+          return 'place meteor on edge';
+      },
+    },
+  ]
+}]
