b185a2178c6b24cab85b4fca943a31efd0cb3cc4
Adrienne Walker <enne@quisquo.us>
2017-10-24 22:38:38 -0700

    ucu: Better Twintania triggers

--- ui/raidboss/data/triggers/unending_coil_ultimate.js
+++ ui/raidboss/data/triggers/unending_coil_ultimate.js
@@ -1,7 +1,7 @@
 // UCU - The Unending Coil Of Bahamut (Ultimate)
 [{
   // TODO: Zone name is empty string for now lol?
-  zoneRegex: /(The Unending Coil Of Bahamut \(Ultimate\)|Unknown Zone \(.*?\))|^$/,
+  zoneRegex: /(The Unending Coil Of Bahamut \(Ultimate\)|^$/,
   triggers: [
     { id: 'UCU Twisters',
       regex: /:26AA:Twintania starts using/,
@@ -9,38 +9,45 @@
         return 'Twisters';
       },
     },
-    /*
-    { id: 'UCU Generate',
-      regex: /:26AE:Twintania starts using/,
-      infoText: function(data) {
-        return 'Generate';
-      },
-    },
-    */
-    
-    { id: 'UCU Generate',
-      regex: /1B:........:(\y{Name}):....:....:0076:0000:0000:0000:/,
-      alertText: function(data, matches) {
-        if (matches[1] == data.me)
-          return 'Generate on YOU';
-      },
-      infoText: function(data, matches) {
-        if (matches[1] != data.me)
-          return 'Generate on ' + matches[1];
-      },
-    },
-
     { id: 'UCU Death Sentence',
-      regex: /:Twintania starts using Death Sentence on (\y{Name})/,
+      regex: /:Twintania readies Death Sentence/,
       alertText: function(data, matches) {
-        if (matches[1] == data.me)
-          return 'Death Sentence on YOU';
-        if (data.role == 'healer')
-          return 'Death Sentence on ' + matches[1];
+        if (data.role == 'tank' || data.role == 'healer')
+          return 'Death Sentence';
       },
+    },
+    { id: 'UCU Fireball Marker',
+      regex: /1B:........:(\y{Name}):....:....:0075:0000:0000:0000:/,
       infoText: function(data, matches) {
-        if (matches[1] != data.me && data.role == 'tank')
-          return 'Death Sentence on ' + matches[1];
+        if (data.me != matches[1])
+          return 'fireball on ' + matches[1];
+      },
+      alertText: function(data, matches) {
+        if (data.me == matches[1])
+          return 'fireball on YOU';
+      },
+    },
+    { id: 'UCU Hatch Marker',
+      regex: /1B:........:(\y{Name}):....:....:0076:0000:0000:0000:/,
+      infoText: function(data, matches) {
+        if (data.me != matches[1])
+          return 'hatch on ' + matches[1];
+      },
+      alarmText: function(data, matches) {
+        if (data.me == matches[1])
+          return 'hatch on YOU';
+      },
+    },
+    { id: 'UCU Twintania P2',
+      regex: /:Twintania HP at 74%/,
+      infoText: function(data, matches) {
+        return "Phase 2 Push";
+      },
+    },
+    { id: 'UCU Twintania P3',
+      regex: /:Twintania HP at 43%/,
+      infoText: function(data, matches) {
+        return "Phase 3 Push";
       },
     },
   ]
