82c534abc315344f0cbbb8cb5b77a2202d90e4e3
Dana Jansens <danakj@orodu.net>
2017-10-25 01:37:07 -0400

    Generate per person trigger and fix zone name for UCU to empty string

--- ui/raidboss/data/triggers/unending_coil_ultimate.js
+++ ui/raidboss/data/triggers/unending_coil_ultimate.js
@@ -1,14 +1,35 @@
 // UCU - The Unending Coil Of Bahamut (Ultimate)
 [{
-  zoneRegex: /(The Unending Coil Of Bahamut \(Ultimate\)|Unknown Zone \(.*?\))|/,
+  // TODO: Zone name is empty string for now lol?
+  zoneRegex: /(The Unending Coil Of Bahamut \(Ultimate\)|Unknown Zone \(.*?\))|^$/,
   triggers: [
     { id: 'UCU Twisters',
       regex: /:26AA:Twintania starts using/,
-      //regex: /:Twintania readies Twister/,
       alertText: function(data) {
         return 'Twisters';
       },
     },
+    /*
+    { id: 'UCU Generate',
+      regex: /:26AE:Twintania starts using/,
+      infoText: function(data) {
+        return 'Generate';
+      },
+    },
+    */
+    
+    { id: 'UCU Generate',
+      regex: /1B:........:(\y{Name}):....:....:0076:0000:0000:0000:/,
+      alertText: function(data, matches) {
+        if (matches[1] == data.me)
+          return 'Generate on YOU';
+      },
+      infoText: function(data, matches) {
+        if (matches[1] != data.me)
+          return 'Generate on ' + matches[1];
+      },
+    },
+
     { id: 'UCU Death Sentence',
       regex: /:Twintania starts using Death Sentence on (\y{Name})/,
       alertText: function(data, matches) {
