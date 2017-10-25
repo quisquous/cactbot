6e425d9879f396f94403dc1278176169caf4c71f
Adrienne Walker <enne@quisquo.us>
2017-10-24 18:56:50 -0700

    Add UCU death sentence

--- ui/raidboss/data/triggers/unending_coil_ultimate.js
+++ ui/raidboss/data/triggers/unending_coil_ultimate.js
@@ -8,5 +8,18 @@
         return 'Twisters';
       },
     },
+    { id: 'UCU Death Sentence',
+      regex: /:Twintania starts using Death Sentence on (\y{Name})/,
+      alertText: function(data, matches) {
+        if (matches[1] == data.me)
+          return 'Death Sentence on YOU';
+        if (data.role == 'healer')
+          return 'Death Sentence on ' + matches[1];
+      },
+      infoText: function(data, matches) {
+        if (matches[1] != data.me && data.role == 'tank')
+          return 'Death Sentence on ' + matches[1];
+      },
+    },
   ]
 }]
