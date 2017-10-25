2ed11fae38388264d9e4ae920559fa8e2b8ca305
Dana Jansens <danakj@orodu.net>
2017-10-24 22:07:32 -0400

    The UCU zone is an empty string/never set.

--- ui/raidboss/data/triggers/unending_coil_ultimate.js
+++ ui/raidboss/data/triggers/unending_coil_ultimate.js
@@ -1,6 +1,6 @@
 // UCU - The Unending Coil Of Bahamut (Ultimate)
 [{
-  zoneRegex: /(The Unending Coil Of Bahamut \(Ultimate\)|Unknown Zone \(.*?\))/,
+  zoneRegex: /(The Unending Coil Of Bahamut \(Ultimate\)|Unknown Zone \(.*?\))|/,
   triggers: [
     { id: 'UCU Twisters',
       regex: /:Twintania starts using Twisters/,
