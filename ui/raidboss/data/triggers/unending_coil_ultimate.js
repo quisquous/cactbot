fdd56fa907e248294038b45263a709cdf05689ec
Dana Jansens <danakj@orodu.net>
2017-10-24 21:34:52 -0400

    Add a triggers file for ultimate coil

---
+++ ui/raidboss/data/triggers/unending_coil_ultimate.js
@@ -0,0 +1,10 @@
+// O3S - Deltascape 3.0 Savage
+[{
+  zoneRegex: /(The Unending Coil Of Bahamut \(Ultimate\)|Unknown Zone \(xyz\))/,
+  triggers: [
+    {
+      id: 'UCU Something',
+      regex: /:Boss starts using Thing/,
+    },
+  ]
+}]
