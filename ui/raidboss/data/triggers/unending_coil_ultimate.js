c2be98a5cac20120fd70f6bf7dfa3b516a86919d
Adrienne Walker <enne@quisquo.us>
2017-10-28 18:13:33 -0700

    ucu: speculative nael timeline and triggers

--- ui/raidboss/data/triggers/unending_coil_ultimate.js
+++ ui/raidboss/data/triggers/unending_coil_ultimate.js
@@ -3,6 +3,29 @@
   // TODO: Zone name is empty string for now lol?
   zoneRegex: /(The Unending Coil Of Bahamut \(Ultimate\)|^$)/,
   triggers: [
+    // --- State ---
+    {
+      regex: /:(y{Name}) gains the effect of Firescorched/,
+      condition: function(data, matches) { return data.me == matches[1]; },
+      run: function(data) { data.fireDebuff = true; },
+    },
+    {
+      regex: /:(y{Name}) loses the effect of Firescorched/,
+      condition: function(data, matches) { return data.me == matches[1]; },
+      run: function(data) { data.fireDebuff = false; },
+    },
+    {
+      regex: /:(y{Name}) gains the effect of Icebitten/,
+      condition: function(data, matches) { return data.me == matches[1]; },
+      run: function(data) { data.iceDebuff = true; },
+    },
+    {
+      regex: /:(y{Name}) loses the effect of Icebitten/,
+      condition: function(data, matches) { return data.me == matches[1]; },
+      run: function(data) { data.iceDebuff = false; },
+    },
+
+    // --- Twintania ---
     { id: 'UCU Twisters',
       regex: /:26AA:Twintania starts using/,
       alertText: function(data) {
@@ -52,6 +75,8 @@
         return "Phase 3 Push";
       },
     },
+
+    // --- Nael ---
     { id: 'UCU Nael Quote 1',
       regex: /From on high I descend, in blessed light to bask/,
       alertText: function(data) { return "spread => in"; },
@@ -84,5 +109,35 @@
       regex: /Fleeting light, outshine the starts for the moon/,
       alertText: function(data) { return "spread => away from MT"; },
     },
+    { id: 'UCU Nael Thunderstruck',
+      // Note: The 0A event happens before "gains the effect" and "starts
+      // casting on" only includes one person.
+      regex: /:Thunderwing:26C7:.*?:........:(\y{Name}):/,
+      condition: function(data, matches) { return data.me == matches[1]; },
+      alarmText: function(data) { return "Thunder on YOU"; },
+    },
+    { id: 'UCU Nael Doom',
+      regex: /:(\y{Name}) gains the effect of Doom from .*? for ([0-9.]+) Seconds/,
+      condition: function(data, matches) { return data.me == matches[1]; },
+      alarmText: function(data) {
+        if (parseFloat(matches[2]) == 6)
+          return "Doom #1 on YOU";
+        if (parseFloat(matches[2]) == 11)
+          return "Doom #2 on YOU";
+        if (parseFloat(matches[2]) == 16)
+          return "Doom #3 on YOU";
+        // TODO: remove this catchall once times are better known.
+        return "Doom: " + parseFloat(matches[2]) + " seconds on you";
+
+        // TODO: call out all doom people
+        // TODO: reminder to clear at the right time
+      },
+    },
+
+    // TODO: fire callouts
+    //   (1) if you have tether (is there a 1B mark??)< whether in or out
+    //   (2) if you have fire (to stay out), if you have ice (to get in)
+    // TODO: cauterize markers
+    // TODO: cauterize placement from dragons
   ]
 }]
