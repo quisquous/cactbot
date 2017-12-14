326076b1ace90da49c241597a55948553c8fa9a9
Adrienne Walker <enne@quisquo.us>
2017-12-14 13:42:51 -0800

    o4s: improved triggers
    * Lasers now tell you which color to go to.
    * Lasers know if you should die on them.
    * Earthshaker warnings.
    * Water stack name callout.
    * Charge callout knows if you have allagan or not.

--- ui/raidboss/data/triggers/o4s.js
+++ ui/raidboss/data/triggers/o4s.js
@@ -87,53 +87,209 @@
     // Part 2
     { // Phase Tracker: Grand Cross Alpha.
       regex: /:242B:Neo Exdeath starts using/,
-      run: function(data) { data.phase = 'alpha'; data.alphaCount = (data.alphaCount || 0) + 1; },
+      run: function(data) {
+        data.phase = 'alpha';
+        data.alphaCount = (data.alphaCount || 0) + 1;
+
+        // Common setup.
+        data.dieDieDieSound = '../../resources/sounds/Overwatch/Reaper_-_Die_die_die.ogg';
+        // TODO: should have options for this.
+        data.dieOnLaser = 1;
+        data.shouldDieOnLaser = function() {
+          if (!this.beyondDeath)
+            return false;
+          // Beyond death doesn't update for laser #2 if you died on
+          // laser #1, so don't tell anybody to die on laser #2.
+          // If you still have beyond death, it'll remind you for #3.
+          if (this.omegaLaserCount == 2 && this.omegaProbablyDiedOnLaser)
+            return false;
+          if (this.phase != 'omega')
+            return true;
+          return this.omegaLaserCount >= this.dieOnLaser;
+        };
+      },
     },
     { // Phase Tracker: Grand Cross Delta.
       regex: /:242C:Neo Exdeath starts using/,
-      run: function(data) { data.phase = 'delta'; },
+      run: function(data) {
+        data.phase = 'delta';
+        data.waterHealer = null;
+      },
     },
     { // Phase Tracker: Grand Cross Omega.
       regex: /:242D:Neo Exdeath starts using/,
-      run: function(data) { data.phase = 'omega'; },
+      run: function(data) {
+        data.phase = 'omega';
+        data.waterHealer = null;
+        data.omegaLaserCount = 1;
+      },
     },
     { // Phase Tracker: Neverwhere.
       regex: /:2426:Neo Exdeath starts using/,
       run: function(data) { data.finalphase = true; },
     },
+    { // Wound tracking
+      regex: /:(\y{Name}) (gains|loses) the effect of White Wound/,
+      condition: function(data, matches) { return matches[1] == data.me; },
+      run: function(data, matches) { data.whiteWound = matches[2] == 'gains'; },
+    },
+    { // Wound tracking
+      regex: /:(\y{Name}) (gains|loses) the effect of Black Wound/,
+      condition: function(data, matches) { return matches[1] == data.me; },
+      run: function(data, matches) { data.blackWound = matches[2] == 'gains'; },
+    },
+    { // Beyond death tracking
+      regex: /:(\y{Name}) (gains|loses) the effect of Beyond Death/,
+      condition: function(data, matches) { return matches[1] == data.me; },
+      run: function(data, matches) { data.beyondDeath = matches[2] == 'gains'; },
+    },
+    { // Allagan field tracking
+      regex: /:(\y{Name}) (gains|loses) the effect of Allagan Field/,
+      condition: function(data, matches) { return matches[1] == data.me; },
+      run: function(data, matches) { data.allaganField = matches[2] == 'gains'; },
+    },
     { // Inner Flood (move out).
       id: 'O4S2 Flood of Naught: Inside',
       regex: /:240E:Neo Exdeath starts using/,
-      alertText: 'Go Outside',
-      tts: 'out out out',
+      durationSeconds: 6,
+      alarmText: function(data) {
+        if (data.shouldDieOnLaser())
+          return 'Die on Inside';
+      },
+      alertText: function(data) {
+        if (!data.shouldDieOnLaser())
+          return 'Go Outside';
+      },
+      sound: function(data) {
+        if (data.shouldDieOnLaser())
+          return data.dieDieDieSound;
+      },
+      tts: function(data) {
+        if (data.shouldDieOnLaser())
+          return 'die in in in';
+        return 'out out out';
+      },
     },
     { // Outer Flood (move in).
       id: 'O4S2 Flood of Naught: Outside',
       regex: /:240F:Neo Exdeath starts using/,
-      alertText: 'Go Inside',
-      tts: 'in in in',
+      durationSeconds: 6,
+      alarmText: function(data) {
+        if (data.shouldDieOnLaser())
+          return 'Die on Outside';
+      },
+      alertText: function(data) {
+        if (!data.shouldDieOnLaser())
+          return 'Go Inside';
+      },
+      sound: function(data) {
+        if (data.shouldDieOnLaser())
+          return data.dieDieDieSound;
+      },
+      tts: function(data) {
+        if (data.shouldDieOnLaser())
+          return 'die out out out';
+        return 'in in in';
+      },
     },
     { // Purple/Blue Flood.
-      id: 'O4S2 Flood of Naught: Colors',
+      id: 'O4S2 Flood of Naught: Colors Purple Blue',
       regex: /:2411:Neo Exdeath starts using/,
-      alertText: 'Color sides',
+      durationSeconds: 6,
+      alarmText: function(data) {
+        if (!data.shouldDieOnLaser())
+          return;
+        if (data.blackWound)
+          return 'Die On Right Blue';
+        else if (data.whiteWound)
+          return 'Die On Left Purple';
+        return 'Die on color sides';
+      },
+      alertText: function(data) {
+        if (data.shouldDieOnLaser())
+          return;
+        if (data.blackWound)
+          return 'Left On Purple';
+        else if (data.whiteWound)
+          return 'Right On Blue';
+        return 'Color sides';
+      },
+      sound: function(data) {
+        if (data.shouldDieOnLaser())
+          return data.dieDieDieSound;
+      },
       tts: 'colors',
     },
     { // Blue/Purple Flood.
-      id: 'O4S2 Flood of Naught: Colors',
+      id: 'O4S2 Flood of Naught: Colors Blue Purple',
       regex: /:2412:Neo Exdeath starts using/,
-      alertText: 'Color sides',
+      durationSeconds: 6,
+      alarmText: function(data) {
+        if (!data.shouldDieOnLaser())
+          return;
+        if (data.blackWound)
+          return 'Die On Left Blue';
+        else if (data.whiteWound)
+          return 'Die On Right Purple';
+        return 'Die on color sides';
+      },
+      alertText: function(data) {
+        if (data.shouldDieOnLaser())
+          return;
+        if (data.blackWound)
+          return 'Be Right On Purple';
+        else if (data.whiteWound)
+          return 'Be Left On Blue';
+        return 'Color sides';
+      },
+      sound: function(data) {
+        if (data.shouldDieOnLaser())
+          return data.dieDieDieSound;
+      },
       tts: 'colors',
+    },
+    { // Laser counter.
+      regex: /:24(OE|0F|11|12):Neo Exdeath starts using/,
+      run: function(data, matches) {
+        if (data.phase != 'omega')
+          return;
+
+        // See comments in shouldDieOnLaser.  Beyond Death
+        // doesn't get removed until after the 2nd laser
+        // appears.  However, colors (THANKFULLY) apply
+        // before the next laser appears.
+        if (data.shouldDieOnLaser())
+          data.omegaProbablyDiedOnLaser = true;
+
+        data.omegaLaserCount++;
+      },
     },
     { // Charge Flood.
       id: 'O4S2 Flood of Naught: Charge',
       regex: /:2416:Neo Exdeath starts using/,
-      infoText: 'Flood of Naught: Charge',
+      infoText: function(data) {
+        if (data.allaganField) {
+          if (data.role == 'tank')
+            return 'Charge: be behind other tank';
+          else
+            return 'Charge: be in the very back';
+        } else {
+          if (data.role == 'tank')
+            return 'Charge: be in front!';
+          else
+            return 'Charge: be behind tanks';
+        }
+      },
+      tts: 'charge',
     },
     {  // Double attack.
       id: 'O4S2 Double Attack',
       regex: /:241C:Neo Exdeath starts using/,
-      alertText: 'Double Attack: Get out',
+      alertText: function(data) {
+        if (data.role == 'tank')
+          return 'Double Attack';
+        return 'Double Attack: Get out';
+      },
       tts: 'double attack',
     },
     { // Grand Cross Alpha.
@@ -180,39 +336,117 @@
       alertText: 'Apocatastasis on healer',
       condition: function(data, matches) { return data.role == 'dps-caster'; },
     },
-    { // Forked Lightning - Get out.
+    { // Forked Lightning - Don't Stack.
       id: 'O4S2 Forked Lightning',
       regex: /:(\y{Name}) gains the effect of Forked Lightning from/,
       delaySeconds: 1,
-      alertText: 'Forked Lightning: Get out',
+      alertText: "Forked Lightning: Don't Stack",
       condition: function(data, matches) { return matches[1] == data.me; },
       tts: 'lightning get out',
     },
     { // Acceleration Bomb
       id: 'O4S2 Acceleration Bomb',
-      regex: /:(\y{Name}) gains the effect of (?:Unknown_568|Acceleration Bomb) from .*? for (\y{Float}) Seconds/,
-      alarmText: 'Stop',
-      delaySeconds: function(data, matches) { return data.ParseLocaleFloat(matches[2]) - 4; },  // 4 second warning.
+      regex: /:(\y{Name}) gains the effect of Acceleration Bomb from .*? for (\y{Float}) Seconds/,
       condition: function(data, matches) { return matches[1] == data.me; },
+      delaySeconds: function(data, matches) { return data.ParseLocaleFloat(matches[2]) - 4; },  // 4 second warning.
+      alarmText: function(data) {
+        if (data.phase == 'omega')
+          return 'look away and stop';
+        return 'stop';
+      },
       tts: 'stop',
     },
-    { // Beyond Death (Delta)
-      id: 'O4S2 Beyond Death',
-      regex: /:(\y{Name}) gains the effect of (?:Unknown_566|Beyond Death) from .*? for (\y{Float}) Seconds/,
-      delaySeconds: 7,
-      alarmText: 'Beyond Death: Die',
-      sound: '../../resources/sounds/Overwatch/Reaper_-_Die_die_die.ogg',
-      condition: function(data, matches) { return data.phase == 'delta' && matches[1] == data.me; },
-      tts: 'die die die',
+    {
+      id: 'O4S2 Acceleration Bomb Delta',
+      regex: /:(\y{Name}) gains the effect of Acceleration Bomb from .*? for (\y{Float}) Seconds/,
+      condition: function(data, matches) { return matches[1] == data.me && data.phase == 'delta'; },
+      infoText: 'Acceleration Bomb',
+      tts: 'bomb',
     },
-    { // Beyond Death (Omega)
-      id: 'O4S2 Beyond Death',
-      regex: /:(\y{Name}) gains the effect of (?:Unknown_566|Beyond Death) from .*? for (\y{Float}) Seconds/,
-      delaySeconds: 8,  // 20 seconds if you want the third flood cast, 8 for the first.
-      alarmText: 'Beyond Death: Die',
-      sound: '../../resources/sounds/Overwatch/Reaper_-_Die_die_die.ogg',
-      condition: function(data, matches) { return data.phase == 'omega' && matches[1] == data.me; },
-      tts: 'die die die',
+    { // Shriek (Omega)
+      id: 'O4S2 Omega Shriek',
+      regex: /:(\y{Name}) gains the effect of Cursed Shriek from .*? for (\y{Float}) Seconds/,
+      condition: function(data, matches) { return matches[1] == data.me && data.phase == 'omega'; },
+      delaySeconds: function(data, matches) { return data.ParseLocaleFloat(matches[2]) - 5; },
+      alertText: 'shriek: get mid, look away',
+      tts: 'shriek',
+    },
+    { // Water Tracking (Delta/Omega)
+      id: 'O4S2 Water',
+      regex: /:(\y{Name}) gains the effect of Compressed Water/,
+      run: function(data, matches) {
+        data.waterHealer = matches[1];
+      },
+    },
+    { // Water Me (Delta/Omega)
+      id: 'O4S2 Water Me',
+      regex: /:(\y{Name}) gains the effect of Compressed Water/,
+      condition: function(data, matches) { return matches[1] == data.me; },
+      alarmText: function(data) {
+        // Not clear where to tell the healer where to go on delta
+        // due to picking a side for uptime strat, or other strats.
+        if (data.phase == 'delta')
+          return 'water on you';
+        else if (data.phase == 'omega')
+          return 'water: stack under neo';
+      },
+      tts: 'water stack',
+    },
+    { // Beyond Death Tank (Delta)
+      id: 'O4S2 Beyond Death Delta Tank',
+      regex: /:(\y{Name}) gains the effect of Beyond Death from .*? for (\y{Float}) Seconds/,
+      condition: function(data, matches) {
+        return data.phase == 'delta' && matches[1] == data.me && data.role == 'tank';
+      },
+      delaySeconds: 0.5,
+      infoText: function(data) {
+        // Something went awry, or maybe healers dead.  Just say stack on water anyway,
+        // instead of trying to be smart when the healers die.
+        if (data.waterHealer)
+          return 'Stack on ' + data.waterHealer;
+        else
+          return 'Stack on water';
+      },
+      tts: 'water stack',
+    },
+    { // Beyond Death (Delta)
+      id: 'O4S2 Beyond Death Delta Initial',
+      regex: /:(\y{Name}) gains the effect of Beyond Death from .*? for (\y{Float}) Seconds/,
+      condition: function(data, matches) {
+        return data.phase == 'delta' && matches[1] == data.me && data.role != 'tank';
+      },
+      infoText: 'Beyond Death',
+      tts: 'death',
+    },
+    { // Off Balance (Omega)
+      id: 'O4S2 Off Balance Omega',
+      regex: /:(\y{Name}) gains the effect of Off-Balance/,
+      condition: function(data, matches) {
+        return data.phase == 'omega' && matches[1] == data.me;
+      },
+      delaySeconds: 0.5,
+      infoText: function(data) {
+        // Good for both dps and tanks.
+        if (data.waterHealer)
+          return 'Stack under boss on ' + data.waterHealer;
+        else
+          return 'Stack on water';
+      },
+      tts: 'water stack',
+    },
+    { // Earthshaker
+      id: 'O4S2 Earthshaker',
+      regex: /1B:........:(\y{Name}):....:....:0028:0000:0000:0000:/,
+      condition: function(data, matches) { return matches[1] == data.me; },
+      alarmText: function(data) {
+        if (data.role != 'tank')
+          return 'Earthshaker on YOU';
+      },
+      infoText: function(data) {
+        if (data.role == 'tank')
+          return 'Earthshaker on you';
+      },
+      tts: 'shaker',
     },
     { // Delta Attack
       id: 'O4S2 Delta Attack',
