3a939c0c4ba4da0b8d0df5fc829ae1496008fafe
Adrienne Walker <enne@quisquo.us>
2017-12-19 13:42:41 -0800

    oopsy: o4s and ucu improvements

--- ui/oopsyraidsy/data/o4s.js
+++ ui/oopsyraidsy/data/o4s.js
@@ -3,13 +3,51 @@
   zoneRegex: /(Deltascape V4.0 \(Savage\)|Unknown Zone \(2Ba\))/,
   triggers: [
     {
+      regex: /:2408:Exdeath starts using The Decisive Battle/,
+      run: function(e, data) {
+        data.isDecisiveBattleElement = true;
+      },
+    },
+    {
+      regex: /:23FE:Exdeath starts using Vacuum Wave/,
+      run: function(e, data) {
+        data.isDecisiveBattleElement = false;
+      },
+    },
+    {
       id: 'O4S2 Blizzard III',
       damageRegex: 'Blizzard III',
       condition: function(e, data) {
-        return data.IsPlayerId(e.targetId);
+        // Ignore unavoidable raid aoe Blizzard III.
+        return data.IsPlayerId(e.targetId) && !data.isDecisiveBattleElement;
       },
       warnText: function(e) {
-        return e.abilityName + ': ' + e.targetName;
+        return data.shortName(e.targetName) + ': ' + e.abilityName;
+      },
+    },
+    {
+      id: 'O4S2 Thunder III',
+      damageRegex: 'Thunder III',
+      condition: function(e, data) {
+        // Only consider this during random mechanic after decisive battle.
+        return data.IsPlayerId(e.targetId) && data.isDecisiveBattleElement;
+      },
+      warnText: function(e) {
+        return data.shortName(e.targetName) + ': ' + e.abilityName;
+      },
+    },
+    {
+      id: 'O4S2 Acceleration Bomb',
+      damageRegex: 'Death Bomb',
+      warnText: function(e) {
+        return data.ShortName(e.targetName) + ': bomb';
+      },
+    },
+    {
+      id: 'O4S2 Petrified',
+      regex: /:(\y{Name}) gains the effect of Petrification/,
+      noText: function(e, data, matches) {
+        return data.ShortName(matches[1]) + ': looked at shriek';
       },
     },
     {
@@ -18,8 +56,42 @@
       condition: function(e, data) {
         return data.IsPlayerId(e.targetId);
       },
-      noText: function(e) {
+      noText: function(e, data) {
         return 'Lightning: ' + data.ShortName(e.attackerName) + ' => ' + data.ShortName(e.targetName);
+      },
+    },
+    {
+      id: 'O4S2 Double Attack',
+      damageRegex: 'Double Attack',
+      condition: function(e, data) {
+        return data.IsPlayerId(e.targetId);
+      },
+      runOnce: true,
+      delaySeconds: 0.5,
+      noText: function(e) {
+        if (e.length <= 2)
+          return;
+        return 'Double Attack hit ' + e.length;
+      },
+    },
+    {
+      id: 'O4S2 Emptiness',
+      damageRegex: 'Emptiness',
+      condition: function(e, data) {
+        return data.IsPlayerId(e.targetId);
+      },
+      warnText: function(e) {
+        return e.abilityName + ': ' + e.targetName;
+      },
+    },
+    {
+      id: 'O4S2 Double Laser',
+      damageRegex: 'Edge Of Death',
+      condition: function(e, data) {
+        return data.IsPlayerId(e.targetId);
+      },
+      noText: function(events) {
+        return data.ShortName(e.targetName) + ': double laser';
       },
     },
   ],
--- ui/oopsyraidsy/data/unending_coil_ultimate.js
+++ ui/oopsyraidsy/data/unending_coil_ultimate.js
@@ -2,5 +2,72 @@
 [{
   zoneRegex: /The Unending Coil Of Bahamut \(Ultimate\)/,
   triggers: [
+    {
+      id: 'UCU Twister Death',
+      damageRegex: 'Twister',
+      condition: function(e, data) {
+        return data.IsPlayerId(e.targetId) && e.flags == '32';
+      },
+      noText: function(e, data) {
+        return data.ShortName(e.targetName) + ': twister pop';
+      },
+    },
+    {
+      id: 'UCU Dynamo',
+      damageRegex: 'Iron Dynamo',
+      condition: function(e, data) {
+        return data.IsPlayerId(e.targetId);
+      },
+      noText: function(e, data) {
+        return data.ShortName(e.targetName) + ': dynamo';
+      },
+    },
+    {
+      id: 'UCU Chariot',
+      damageRegex: 'Iron Chariot',
+      condition: function(e, data) {
+        return data.IsPlayerId(e.targetId);
+      },
+      noText: function(e, data) {
+        return data.ShortName(e.targetName) + ': chariot';
+      },
+    },
+    {
+      id: 'UCU White Puddle',
+      damageRegex: 'Wings Of Salvation',
+      condition: function(e, data) {
+        return data.IsPlayerId(e.targetId);
+      },
+      noText: function(e, data) {
+        return data.ShortName(e.targetName) + ': white puddle';
+      },
+    },
+    {
+      id: 'UCU Chain Lightning',
+      damageRegex: 'Chain Lightning',
+      condition: function(e, data) {
+        // Chain lightning gets log lines when it goes out
+        // on players, so ignore those log lines.
+        return data.IsPlayerId(e.targetId) && e.abilityId == '26C8';
+      },
+      warnText: function(e, data) {
+        return data.ShortName(e.targetName) + ': hit by lightning';
+      },
+    },
+    {
+      id: 'UCU Burns',
+      // TODO: add a buff regex
+      regex: ':(\y{Name}) gains the effect of Burns',
+      noText: function(e, data, matches) {
+        return data.ShortName(matches[1]) + ': burn dot';
+      },
+    },
+    {
+      id: 'UCU Sludge',
+      regex: ':(\y{Name}) gains the effect of Sludge',
+      noText: function(e, data) {
+        return data.ShortName(matches[1]) + ': sludge dot';
+      },
+    },
   ],
 }]
