89b6b006b61818c6b34f3c758cf1d2056443e51a
Kuriyama Hibiya <kuriyama@hibiya.moe>
2017-12-26 16:04:41 +0900

    add id comments to statuses/actions

--- resources/lang/en.js
+++ resources/lang/en.js
@@ -7,42 +7,42 @@ class CactbotLanguageEn extends CactbotLanguage {
 
   InitStrings(playerName) {
     this.kAbility = Object.freeze({
-      DragonKick: 'Dragon Kick',
-      TwinSnakes: 'Twin Snakes',
-      Demolish: 'Demolish',
-      Verstone: 'Verstone',
-      Verfire: 'Verfire',
-      Veraero: 'Veraero',
-      Verthunder: 'Verthunder',
-      Verholy: 'Verholy',
-      Verflare: 'Verflare',
-      Jolt2: 'Jolt II',
-      Jolt: 'Jolt',
-      Impact: 'Impact',
-      Scatter: 'Scatter',
-      Vercure: 'Vercure',
-      Verraise: 'Verraise',
-      Riposte: 'Riposte',
-      Zwerchhau: 'Zwerchhau',
-      Redoublement: 'Redoublement',
-      Moulinet: 'Moulinet',
-      EnchantedRiposte: 'Enchanted Riposte',
-      EnchantedZwerchhau: 'Enchanted Zwerchhau',
-      EnchantedRedoublement: 'Enchanted Redoublement',
-      EnchantedMoulinet: 'Enchanted Moulinet',
-      Tomahawk: 'Tomahawk',
-      Overpower: 'Overpower',
-      HeavySwing: 'Heavy Swing',
-      SkullSunder: 'Skull Sunder',
-      ButchersBlock: "Butcher's Block",
-      Maim: 'Maim',
-      StormsEye: "Storm's Eye",
-      StormsPath: "Storm's Path",
-      TrickAttack: 'Trick Attack',
-      Embolden: 'Embolden',
-      Aetherflow: 'Aetherflow',
-      ChainStrategem: 'Chain Strategem',
-      Hypercharge: 'Hypercharge',
+      DragonKick: 'Dragon Kick', // 0x4a
+      TwinSnakes: 'Twin Snakes', // 0x65
+      Demolish: 'Demolish', // 0x42
+      Verstone: 'Verstone', // 0x1d57
+      Verfire: 'Verfire', // 0x1d56
+      Veraero: 'Veraero', // 0x1d53
+      Verthunder: 'Verthunder', // 0x1d51
+      Verholy: 'Verholy', // 0x1d66
+      Verflare: 'Verflare', // 0x1d65
+      Jolt2: 'Jolt II', // 0x1d64
+      Jolt: 'Jolt', // 0x1d4f
+      Impact: 'Impact', // 0x1d62
+      Scatter: 'Scatter', // 0x1d55
+      Vercure: 'Vercure', // 0x1d5a
+      Verraise: 'Verraise', // 0x1d63
+      Riposte: 'Riposte', // 0x1d50
+      Zwerchhau: 'Zwerchhau', // 0x1d58
+      Redoublement: 'Redoublement', // 0x1d5c
+      Moulinet: 'Moulinet', // 0x1d59
+      EnchantedRiposte: 'Enchanted Riposte', // 0x1d67
+      EnchantedZwerchhau: 'Enchanted Zwerchhau', // 0x1d68
+      EnchantedRedoublement: 'Enchanted Redoublement', // 0x1d69
+      EnchantedMoulinet: 'Enchanted Moulinet', // 0x1d6a
+      Tomahawk: 'Tomahawk', // 0x2e
+      Overpower: 'Overpower', // 0x29
+      HeavySwing: 'Heavy Swing', // 0x1f
+      SkullSunder: 'Skull Sunder', // 0x23
+      ButchersBlock: "Butcher's Block", // 0x2f
+      Maim: 'Maim', // 0x25
+      StormsEye: "Storm's Eye", // 0x2d
+      StormsPath: "Storm's Path", // 0x2a
+      TrickAttack: 'Trick Attack', // 0x8d2
+      Embolden: 'Embolden', // 0x1d60
+      Aetherflow: 'Aetherflow', // 0xa6
+      ChainStrategem: 'Chain Strategem', // 0x1d0c
+      Hypercharge: 'Hypercharge', // 0xb45
     });
 
     this.kZone = Object.freeze({
@@ -54,27 +54,27 @@ class CactbotLanguageEn extends CactbotLanguage {
     });
 
     this.kEffect = Object.freeze({
-      BluntResistDown: 'Blunt Resistance Down',
-      VerstoneReady: 'Verstone Ready',
-      VerfireReady: 'Verfire Ready',
-      Impactful: 'Impactful',
-      FurtherRuin: 'Further Ruin',
-      Aetherflow: 'Aetherflow',
-      WellFed: 'Well Fed',
-      OpoOpoForm: 'Opo-Opo Form',
-      RaptorForm: 'Raptor Form',
-      CoeurlForm: 'Coeurl Form',
-      PerfectBalance: 'Perfect Balance',
-      Medicated: 'Medicated',
-      BattleLitany: 'Battle Litany',
-      Embolden: 'Embolden',
-      Balance: 'The Balance',
-      Hypercharge: 'Hypercharge',
-      LeftEye: 'Left Eye',
-      RightEye: 'Right Eye',
-      Brotherhood: 'Brotherhood',
-      Devotion: 'Devotion',
-      FoeRequiem: 'Foe Requiem',
+      BluntResistDown: 'Blunt Resistance Down', // 0x23d, 0x335, 0x3a3
+      VerstoneReady: 'Verstone Ready', // 0x4d3
+      VerfireReady: 'Verfire Ready', // 0x4d2
+      Impactful: 'Impactful', // 0x557
+      FurtherRuin: 'Further Ruin', // 0x4bc
+      Aetherflow: 'Aetherflow', // 0x130
+      WellFed: 'Well Fed', // 0x30
+      OpoOpoForm: 'Opo-Opo Form', // 0x6b
+      RaptorForm: 'Raptor Form', // 0x6c
+      CoeurlForm: 'Coeurl Form', // 0x6d
+      PerfectBalance: 'Perfect Balance', // 0x6e
+      Medicated: 'Medicated', // tbc
+      BattleLitany: 'Battle Litany', // 0x312
+      Embolden: 'Embolden', // 0x4d7
+      Balance: 'The Balance', // 0x53a
+      Hypercharge: 'Hypercharge', // 0x2b0
+      LeftEye: 'Left Eye', // 0x4a0
+      RightEye: 'Right Eye', // 0x49f
+      Brotherhood: 'Brotherhood', // 0x49e
+      Devotion: 'Devotion', // 0x4bd
+      FoeRequiem: 'Foe Requiem', // up 0x8b, down 0x8c
     });
 
     this.kUIStrings = Object.freeze({
--- resources/lang/ko.js
+++ resources/lang/ko.js
@@ -7,42 +7,42 @@ class CactbotLanguageKo extends CactbotLanguage {
 
   InitStrings(playerName) {
     this.kAbility = Object.freeze({
-      DragonKick: '쌍룡각',
-      TwinSnakes: '쌍장타',
-      Demolish: '파쇄권',
-      Verstone: '버스톤',
-      Verfire: '버파이어',
-      Veraero: '버에어로',
-      Verthunder: '버선더',
-      Verholy: '버홀리',
-      Verflare: '버플레어',
-      Jolt2: '졸트라',
-      Jolt: '졸트',
-      Impact: '임팩트',
-      Scatter: '흩뿌리기',
-      Vercure: '버케알',
-      Verraise: '버레이즈',
-      Riposte: '리포스트',
-      Zwerchhau: '츠베르크하우',
-      Redoublement: '르두블망',
-      Moulinet: '물리네',
-      EnchantedRiposte: '인리포스트',
-      EnchantedZwerchhau: '인츠베르크하우',
-      EnchantedRedoublement: '인르두블망',
-      EnchantedMoulinet: '인물리네',
-      Tomahawk: '도끼 던지기',
-      Overpower: '압도',
-      HeavySwing: '육중한 일격',
-      SkullSunder: '두개골 절단',
-      ButchersBlock: '휘도는 도끼',
-      Maim: '관절 파괴',
-      StormsEye: '폭풍의 눈',
-      StormsPath: '폭풍 쐐기',
-      TrickAttack: '속임수 공격',
-      Embolden: '성원',
-      Aetherflow: '에테르 순환',
-      ChainStrategem: '연환계',
-      Hypercharge: '과충전',
+      DragonKick: '쌍룡각', // 0x4a
+      TwinSnakes: '쌍장타', // 0x65
+      Demolish: '파쇄권', // 0x42
+      Verstone: '버스톤', // 0x1d57
+      Verfire: '버파이어', // 0x1d56
+      Veraero: '버에어로', // 0x1d53
+      Verthunder: '버선더', // 0x1d51
+      Verholy: '버홀리', // 0x1d66
+      Verflare: '버플레어', // 0x1d65
+      Jolt2: '졸트라', // 0x1d64
+      Jolt: '졸트', // 0x1d4f
+      Impact: '임팩트', // 0x1d62
+      Scatter: '흩뿌리기', // 0x1d55
+      Vercure: '버케알', // 0x1d5a
+      Verraise: '버레이즈', // 0x1d63
+      Riposte: '리포스트', // 0x1d50
+      Zwerchhau: '츠베르크하우', // 0x1d58
+      Redoublement: '르두블망', // 0x1d5c
+      Moulinet: '물리네', // 0x1d59
+      EnchantedRiposte: '인리포스트', // 0x1d67
+      EnchantedZwerchhau: '인츠베르크하우', // 0x1d68
+      EnchantedRedoublement: '인르두블망', // 0x1d69
+      EnchantedMoulinet: '인물리네', // 0x1d6a
+      Tomahawk: '도끼 던지기', // 0x2e
+      Overpower: '압도', // 0x29
+      HeavySwing: '육중한 일격', // 0x1f
+      SkullSunder: '두개골 절단', // 0x23
+      ButchersBlock: '휘도는 도끼', // 0x2f
+      Maim: '관절 파괴', // 0x25
+      StormsEye: '폭풍의 눈', // 0x2d
+      StormsPath: '폭풍 쐐기', // 0x2a
+      TrickAttack: '속임수 공격', // 0x8d2
+      Embolden: '성원', // 0x1d60
+      Aetherflow: '에테르 순환', // 0xa6
+      ChainStrategem: '연환계', // 0x1d0c
+      Hypercharge: '과충전', // 0xb45
     });
 
     this.kZone = Object.freeze({
@@ -54,27 +54,27 @@ class CactbotLanguageKo extends CactbotLanguage {
     });
 
     this.kEffect = Object.freeze({
-      BluntResistDown: '타격 저항 감소',
-      VerstoneReady: '버스톤 시전 가능',
-      VerfireReady: '버파이어 시전 가능',
-      Impactful: '임팩트 시전 가능',
-      FurtherRuin: '루인 강화',
-      Aetherflow: '에테르 순환',
-      WellFed: '식사',
-      OpoOpoForm: '원숭이 품새',
-      RaptorForm: '용 품새',
-      CoeurlForm: '호랑이 품새',
-      PerfectBalance: '진각',
-      Medicated: '강화약',
-      BattleLitany: '전투 기도',
-      Embolden: '성원',
-      Balance: '아제마의 균형',
-      Hypercharge: '과충전',
-      LeftEye: '용의 왼쪽 눈',
-      RightEye: '용의 오른쪽 눈',
-      Brotherhood: '도원결의',
-      Devotion: '에기의 가호',
-      FoeRequiem: '마인의 진혼곡',
+      BluntResistDown: '타격 저항 감소', // 0x23d, 0x335, 0x3a3
+      VerstoneReady: '버스톤 시전 가능', // 0x4d3
+      VerfireReady: '버파이어 시전 가능', // 0x4d2
+      Impactful: '임팩트 시전 가능', // 0x557
+      FurtherRuin: '루인 강화', // 0x4bc
+      Aetherflow: '에테르 순환', // 0x130
+      WellFed: '식사', // 0x30
+      OpoOpoForm: '원숭이 품새', // 0x6b
+      RaptorForm: '용 품새', // 0x6c
+      CoeurlForm: '호랑이 품새', // 0x6d
+      PerfectBalance: '진각', // 0x6e
+      Medicated: '강화약', // tbc
+      BattleLitany: '전투 기도', // 0x312
+      Embolden: '성원', // 0x4d7
+      Balance: '아제마의 균형', // 0x53a
+      Hypercharge: '과충전', // 0x2b0
+      LeftEye: '용의 왼쪽 눈', // 0x4a0
+      RightEye: '용의 오른쪽 눈', // 0x49f
+      Brotherhood: '도원결의', // 0x49e
+      Devotion: '에기의 가호', // 0x4bd
+      FoeRequiem: '마인의 진혼곡', // up 0x8b, down 0x8c
     });
 
     this.kUIStrings = Object.freeze({
