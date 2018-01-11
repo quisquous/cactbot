1bcf3a735c27c37d63a0d9cbfb46e00984811cdd
CptDraco <34902985+CptDraco@users.noreply.github.com>
2018-01-11 14:36:19 -0600

    Update jobs.js
    Change to PLD reverted and correctly applied to DRK.
    Thats what I get for rushing.

--- ui/jobs/jobs.js
+++ ui/jobs/jobs.js
@@ -1134,7 +1134,7 @@ class Bars {
     if (blood < 50) {
       this.o.bloodTextBox.classList.add('low');
       this.o.bloodTextBox.classList.remove('mid');
-    } else if (blood < 100) {
+    } else if (blood < 90) {
       this.o.bloodTextBox.classList.remove('low');
       this.o.bloodTextBox.classList.add('mid');
     } else {
@@ -1152,7 +1152,7 @@ class Bars {
     if (oath < 50) {
       this.o.oathTextBox.classList.add('low');
       this.o.oathTextBox.classList.remove('mid');
-    } else if (oath < 90) {
+    } else if (oath < 100) {
       this.o.oathTextBox.classList.remove('low');
       this.o.oathTextBox.classList.add('mid');
     } else {
