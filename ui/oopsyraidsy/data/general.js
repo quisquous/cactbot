731a2e612e961a38287803a88184a3672ad73ecf
Adrienne Walker <enne@quisquo.us>
2018-02-04 23:32:58 -0800

    Fix Graven Images doing ninja bunny rabbits
    Sorry, Ferro.

--- ui/oopsyraidsy/data/general.js
+++ ui/oopsyraidsy/data/general.js
@@ -34,12 +34,14 @@
     {
       id: 'General Rabbit Medium',
       abilityRegex: gLang.kAbility.RabbitMedium,
+      condition: function(e, data) { return data.IsPlayerId(e.attackerId); },
       mistake: function(e, data) {
         return { type: 'warn', blame: e.attackerName, text: 'bunny' };
       },
     },
     {
       id: 'General Missed Trick',
+      condition: function(e, data) { return data.IsPlayerId(e.attackerId); },
       damageRegex: gLang.kAbility.TrickAttack,
       condition: function(e) {
         // 28710?03 == success
