506009872c61f22fb0ffa0c0e97ff55fa6afe45a
Adrienne Walker <enne@quisquo.us>
2018-02-10 17:26:25 -0800

    Update fractal hard triggers

--- ui/raidboss/data/triggers/fractal_continuum_hard.js
+++ ui/raidboss/data/triggers/fractal_continuum_hard.js
@@ -4,13 +4,13 @@
   triggers: [
     {
       id: 'Fractal Swipe',
-      regex: /14:2AE5:Servomechanical Minotaur starts using 16-Tonze Swipe/,
+      regex: /(?:14:2AE5:Servomechanical Minotaur starts using 16-Tonze Swipe|14:29A2:Biomanufactured Minotaur starts using 11-Tonze Swipe)/,
       infoText: 'swipe',
       tts: 'swipe',
     },
     {
       id: 'Fractal Swing',
-      regex: /14:2AE4:Servomechanical Minotaur starts using 128-Tonze Swing/,
+      regex: /(?:14:2AE4:Servomechanical Minotaur starts using 128-Tonze Swing|14:29A1:Biomanufactured Minotaur starts using 111-Tonze Swing)/,
       alertText: 'Swing',
       tts: 'swing',
     },
@@ -28,13 +28,13 @@
     },
     {
       id: 'Fractal Death Spin',
-      regex: /14:27AD:The Ultima Beast starts using Death Spin/,
-      alertText: 'Get Out',
-      tts: 'out out out',
+      regex: /14:27A[DE]:The Ultima Beast starts using Death Spin/,
+      infoText: 'Knockback',
+      tts: 'knockback',
     },
     {
       id: 'Fractal Aether Bend',
-      regex: /14:27AF:The Ultima Beast starts using Aether Bend/,
+      regex: /14:27A[3F]:The Ultima Beast starts using Aether Bend/,
       alertText: 'Get In',
       tts: 'in in in',
     },
