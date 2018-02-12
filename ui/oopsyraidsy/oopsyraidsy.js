1291dee20860dbbd5526589e2b719f2186103fc0
Adrienne Walker <enne@quisquo.us>
2018-02-11 18:34:52 -0800

    oopsy: Fix damage values for 4.2.
    Closes #38.

--- ui/oopsyraidsy/oopsyraidsy.js
+++ ui/oopsyraidsy/oopsyraidsy.js
@@ -93,15 +93,25 @@ Field 7 Flags:
     * If flags are 3C, shift 9+10 two over to be 7+8.  (why???)
     * Plenary indulgence has flags=113/213/313 for stacks, shift two as well.
 
-Examples:
-(1) 18216 damage from Grand Cross Alpha (basic)
-  16:40001333:Neo Exdeath:242B:Grand Cross Alpha:1048638C:Tater Tot:750003:4728:1C:80242B:0:0:0:0:0:0:0:0:0:0:0:0:36906:41241:5160:5160:880:1000:0.009226365:-7.81128:-1.192093E-07:16043015:17702272:12000:12000:1000:1000:-0.01531982:-19.02808:0:
+  Damage:
+    * Left-extend zeroes to 8 chars, e.g. 2934001 => 02934001, or 1000 => 00001000.
+    * Should be interpreted as 4 bytes (8 chars).
+    * First two bytes are damage.
+    * 00004000 mask implies extra damage (and some weird math):
+      bytes = ABCD, where C = 0x40.
+      total damage = DA(B-D), as three bytes together interpreted as an integer.
+      e.g. 424E400F => 0F 42 (4E - 0F = 3F) => 0F423F => 999999
+    * 00001000 mask implies 0 damage, e.g. hallowed.
 
-(2) 191150 damage from Megaflare (0x400000 flag for x10 damage)
-  16:40009492:Nael deus Darnus:26BA:Megaflare:106C1DBA:Okonomi Yaki:750003:404AAB:1C:8026BA:0:0:0:0:0:0:0:0:0:0:0:0:44495:44495:5880:5880:720:1000:5.599976:3.768982:0:108843:5013955:12000:12000:1000:1000:3.555298:1.754761:0:
+Examples:
+(1) 18216 damage from Grand Cross Alpha (basic damage)
+  16:40001333:Neo Exdeath:242B:Grand Cross Alpha:1048638C:Tater Tot:750003:47280000:1C:80242B:0:0:0:0:0:0:0:0:0:0:0:0:36906:41241:5160:5160:880:1000:0.009226365:-7.81128:-1.192093E-07:16043015:17702272:12000:12000:1000:1000:-0.01531982:-19.02808:0:
+
+(2) 82538 damage from Hyperdrive (0x4000 extra damage mask)
+  15:40024FBA:Kefka:28E8:Hyperdrive:106C1DBA:Okonomi Yaki:750003:426B4001:1C:28E88000:0:0:0:0:0:0:0:0:0:0:0:0:35811:62464:4560:4560:940:1000:-0.1586061:-5.753153:0:30098906:31559062:12000:12000:1000:1000:0.3508911:0.4425049:2.384186E-07:
 
 (3) 22109 damage from Grand Cross Omega (:3C:0: shift, unknown 0x40000 flag)
-  16:40001333:Neo Exdeath:242D:Grand Cross Omega:1048638C:Tater Tot:3C:0:750003:4565D:1C:80242D:0:0:0:0:0:0:0:0:0:0:41241:41241:5160:5160:670:1000:-0.3251641:6.526299:1.192093E-07:7560944:17702272:12000:12000:1000:1000:0:19:2.384186E-07:
+  16:40001333:Neo Exdeath:242D:Grand Cross Omega:1048638C:Tater Tot:3C:0:750003:565D0000:1C:80242D:0:0:0:0:0:0:0:0:0:0:41241:41241:5160:5160:670:1000:-0.3251641:6.526299:1.192093E-07:7560944:17702272:12000:12000:1000:1000:0:19:2.384186E-07:
 
 (4) 15732 crit heal from 3 confession stack Plenary Indulgence (:?13:4C3: shift)
   16:10647D2F:Tako Yaki:1D09:Plenary Indulgence:106DD019:Okonomi Yaki:313:4C3:10004:3D74:0:0:0:0:0:0:0:0:0:0:0:0:7124:40265:14400:9192:1000:1000:-10.78815:11.94781:0:11343:40029:19652:16451:1000:1000:6.336648:7.710004:0:
@@ -129,11 +139,19 @@ function ShortNamify(name) {
 }
 
 function DamageFromFields(fields) {
-  // Damage field's right two bytes are damage, other bytes are flags.
-  // 0x400000 flag = damage value is multiplied by 10 (i.e. big damage ends in zero!)
-  // UNKNOWN: 0x[48]0000 is seen sometimes (see example above even) but unclear why.
-  var damageWithFlags = parseInt(fields[kFieldDamage], 16);
-  return (damageWithFlags & 0xFFFF) * (damageWithFlags & 0x400000 ? 10 : 1);
+  var field = fields[kFieldDamage];
+  var len = field.length;
+  if (len <= 4)
+    return 0;
+  // Get the left two bytes as damage.
+  var damage = parseInt(field.substr(0, len - 4), 16);
+  // Check for third byte == 0x40.
+  if (field[len - 4] == '4') {
+    // Wrap in the 4th byte as extra damage.  See notes above.
+    var rightDamage = parseInt(field.substr(len - 2, 2), 16);
+    damage = damage - rightDamage + (rightDamage << 16);
+  }
+  return damage;
 }
 
 function IsCritDamage(flags) {
