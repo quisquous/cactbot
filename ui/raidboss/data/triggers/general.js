f1a5df8266dc9772dfadb0e91ac41c1bde7d8934
Adrienne Walker <enne@quisquo.us>
2018-02-18 11:36:47 -0800

    Add tts for general provoke/shirk/etc triggers
    Also shorten the names.

--- ui/raidboss/data/triggers/general.js
+++ ui/raidboss/data/triggers/general.js
@@ -7,7 +7,10 @@
       regex: /:(\y{Name}):1D6D:Provoke:/,
       condition: function(data) { return data.role == 'tank' },
       infoText: function(data, matches) {
-        return 'Provoke: ' + matches[1];
+        return 'Provoke: ' + data.ShortName(matches[1]);
+      },
+      tts: function(data, matches) {
+        return 'provoke ' + data.ShortName(matches[1]);
       },
     },
     {
@@ -15,7 +18,10 @@
       regex: /:(\y{Name}):1D73:Ultimatum:/,
       condition: function(data) { return data.role == 'tank' },
       infoText: function(data, matches) {
-        return 'Ultimatum: ' + matches[1];
+        return 'Ultimatum: ' + data.ShortName(matches[1]);
+      },
+      tts: function(data, matches) {
+        return 'ultimatum ' + data.ShortName(matches[1]);
       },
     },
     {
@@ -23,7 +29,10 @@
       regex: /:(\y{Name}):1D71:Shirk:/,
       condition: function(data) { return data.role == 'tank' },
       infoText: function(data, matches) {
-        return 'Shirk: ' + matches[1];
+        return 'Shirk: ' + data.ShortName(matches[1]);
+      },
+      tts: function(data, matches) {
+        return 'shirk ' + data.ShortName(matches[1]);
       },
     },
     {
@@ -31,7 +40,10 @@
       regex: /:(\y{Name}):2B:Holmgang:/,
       condition: function(data) { return data.role == 'tank' },
       infoText: function(data, matches) {
-        return 'Holmgang: ' + matches[1];
+        return 'Holmgang: ' + data.ShortName(matches[1]);
+      },
+      tts: function(data, matches) {
+        return 'holmgang ' + data.ShortName(matches[1]);
       },
     },
     {
@@ -39,7 +51,10 @@
       regex: /:(\y{Name}):1E:Hallowed Ground:/,
       condition: function(data) { return data.role == 'tank' },
       infoText: function(data, matches) {
-        return 'Hallowed: ' + matches[1];
+        return 'Hallowed: ' + data.ShortName(matches[1]);
+      },
+      tts: function(data, matches) {
+        return 'hallowed ' + data.ShortName(matches[1]);
       },
     },
     {
@@ -47,7 +62,10 @@
       regex: /:(\y{Name}):E36:Living Dead:/,
       condition: function(data) { return data.role == 'tank' },
       infoText: function(data, matches) {
-        return 'Living: ' + matches[1];
+        return 'Living: ' + data.ShortName(matches[1]);
+      },
+      tts: function(data, matches) {
+        return 'living ' + data.ShortName(matches[1]);
       },
     },
     {
@@ -55,7 +73,10 @@
       regex: /:(\y{Name}) gains the effect of Walking Dead/,
       condition: function(data) { return data.role == 'tank' },
       infoText: function(data, matches) {
-        return 'Walking: ' + matches[1];
+        return 'Walking: ' + data.ShortName(matches[1]);
+      },
+      tts: function(data, matches) {
+        return 'walking ' + data.ShortName(matches[1]);
       },
     },
     {
