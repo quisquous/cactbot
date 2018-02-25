05aa3c1ed9c73e06f0a10af6b765d37a26e64dde
Adrienne Walker <enne@quisquo.us>
2018-02-25 13:08:35 -0800

    raidboss: add load/run o7s triggers

--- ui/raidboss/data/triggers/o7s.js
+++ ui/raidboss/data/triggers/o7s.js
@@ -19,6 +19,34 @@
       },
     },
     {
+      regex: / 1A:Guardian gains the effect of Dadaluma Simulation/,
+      condition: function(data, matches) { return !data.first || data.seenVirus && !data.second },
+      run: function(data) {
+        if (data.seenVirus)
+          data.second = 'dada';
+        else
+          data.first = 'dada';
+      },
+    },
+    {
+      regex: / 1A:Guardian gains the effect of Bibliotaph Simulation/,
+      condition: function(data, matches) { return !data.first || data.seenVirus && !data.second },
+      run: function(data) {
+        if (data.seenVirus)
+          data.second = 'biblio';
+        else
+          data.first = 'biblio';
+      },
+    },
+    {
+      regex: / 1A:Guardian gains the effect of Virus/,
+      run: function(data) {
+        data.seenVirus = true;
+      },
+    },
+
+
+    {
       id: 'O7S Magitek Ray',
       regex: / 14:2788:Guardian starts using Magitek Ray/,
       alertText: 'Magitek Ray',
@@ -123,6 +151,100 @@
           return 'Silence';
       },
       tts: 'silence',
+    },
+    {
+      id: 'O7S Load',
+      // Load: 275C
+      // Skip: 2773
+      // Retrieve: 2774
+      // Paste: 2776
+      regex: / 14:(?:275C|2773|2774|2776):Guardian starts using/,
+      preRun: function(data) {
+        data.loadCount = ++data.loadCount || 1;
+        data.thisLoad = undefined;
+        data.thisLoadText = undefined;
+        data.thisLoadTTS = undefined;
+
+        if (data.loadCount == 1) {
+          // First load is unknown.
+          data.thisLoad = 'screen';
+        } else if (data.loadCount == 2) {
+          data.thisLoad = data.first == 'biblio' ? 'dada' : 'biblio';
+        } else if (data.loadCount == 3) {
+          data.thisLoad = data.first == 'biblio' ? 'ultros' : 'ships';
+        } else if (data.loadCount == 4) {
+          data.thisLoad = data.first == 'biblio' ? 'ships' : 'ultros';
+        } else if (data.loadCount == 5) {
+          data.thisLoad = 'virus';
+        } else if (data.loadCount == 6) {
+          data.thisLoad = data.first == 'biblio' ? 'ultros' : 'ships';
+        } else if (data.loadCount == 7) {
+          // This is the post-virus Load/Skip divergence.
+          data.thisLoad = 'screen';
+        } else if (data.loadCount == 8) {
+          data.thisLoad = data.second == 'biblio' ? 'dada' : 'biblio';
+        } else if (data.loadCount == 9) {
+          data.thisLoad = data.first == 'biblio' ? 'ships' : 'ultros';
+        }
+
+        if (!data.thisLoad) {
+          console.error('Unknown load: ' + data.loadCount);
+          return;
+        }
+        data.thisLoadText = ({
+          'screen': 'Biblio?/Knockback?',
+          'biblio': 'Biblio: Positions',
+          'dada': 'Dada: Knockback',
+          'ships': 'Ships: Out of Melee',
+          'ultros': 'Ultros: Ink Spread',
+          'virus': 'VIRUS',
+        })[data.thisLoad];
+
+        data.thisLoadTTS = ({
+          'screen': 'screen',
+          'biblio': 'biblio positions',
+          'dada': 'knockback',
+          'ships': 'get out',
+          'ultros': 'ink ink ink',
+          'virus': 'virus',
+        })[data.thisLoad];
+      },
+      alertText: function (data) { return data.thisLoadText; },
+      tts: function(data) { return data.thisLoadTTS; },
+    },
+    {
+      id: 'O7S Run',
+      regex: / 14:276F:Guardian starts using Run Program/,
+      preRun: function(data) {
+        data.runCount = ++data.runCount || 1;
+        data.thisRunText = undefined;
+        data.thisRunTTS = undefined;
+
+        if (data.runCount == 1) {
+          data.thisRun = data.first == 'biblio' ? 'dada' : 'dada';
+        } else if (data.runCount == 2) {
+          data.thisRun = data.first == 'biblio' ? 'ultros' : 'ships';
+        } else if (data.runCount == 3) {
+          data.thisRun = data.first == 'biblio' ? 'ships' : 'ultros';
+        } else if (data.runCount == 4) {
+          data.thisRun = data.first == 'biblio' ? 'ultros' : 'ships';
+        } else if (data.runCount == 5) {
+          data.thisRun = 'biblio';
+        } else if (data.runCount == 6) {
+          data.thisRun = data.first == 'biblio' ? 'ships' : 'ultros';
+        }
+
+        data.thisRunText = ({
+          'biblio': 'Biblio Add',
+          'dada': 'Dada Add',
+          'ships': 'Ship Adds',
+          'ultros': 'Ultros Add',
+        })[data.thisRun];
+
+        data.thisRunTTS = data.thisRunText;
+      },
+      infoText: function(data) { return data.thisRunText; },
+      tts: function(data) { return data.thisRunTTS; },
     },
   ]
 }]
