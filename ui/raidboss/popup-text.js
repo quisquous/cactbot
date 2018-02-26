quisquous/cactbot

commit 6c2c5603ed31f522c7f096ce6087f076b85b564a

raidboss: add support for localized triggers
Triggers can now have localized regexes.  For a given local (e.g. ko)
if there's a regexKo entry on the trigger, that will take precedence
over regex on its own.

Additionally, any trigger field that can be treated as a function
can now also return localized responses as an object, e.g.
instead of:

   return 'Get Out';

...triggers can instead do something like:

   return {en: 'Get Out', fr: 'something french here, etc'};

This change also avoids running Regexes.Parse on every single log line
for every single trigger.  @_@;;

This will help issue #57.

ui/raidboss/popup-text.js
@@ -98,9 +98,23 @@ class PopupText {
       }
     }).bind(this);
 
+    var locale = this.options.Language || 'en';
+    // construct something like regexEn or regexFr.
+    var regexLocale = 'regex' + locale.charAt(0).toUpperCase() + locale.slice(1);
+
     for (var i = 0; i < this.triggerSets.length; ++i) {
       var set = this.triggerSets[i];
       if (this.zoneName.search(set.zoneRegex) >= 0) {
+        // Adjust triggers for the locale.
+        for (var j = 0; j < set.triggers.length; ++j) {
+          // Add an additional resolved regex here to save
+          // time later.  This will clobber each time we
+          // load this, but that's ok.
+          var trigger = set.triggers[j];
+          // Locale-based regex takes precedence.
+          var regex = trigger[regexLocale] ? trigger[regexLocale] : trigger.regex;
+          trigger.localRegex = Regexes.Parse(regex);
+        }
         // Save the triggers from each set that matches.
         Array.prototype.push.apply(this.triggers, set.triggers);
         // And set the timeline files/timelines from each set that matches.
@@ -202,7 +216,7 @@ class PopupText {
 
       for (var j = 0; j < this.triggers.length; ++j) {
         var trigger = this.triggers[j];
-        var r = log.match(Regexes.Parse(trigger.regex));
+        var r = log.match(trigger.localRegex);
         if (r != null)
           this.OnTrigger(trigger, r);
       }
@@ -227,9 +241,23 @@ class PopupText {
       trigger.preRun(this.data, matches);
 
     var that = this;
-    var ValueOrFunction = function(f) {
-      return (typeof(f) == "function") ? f(that.data, matches) : f;
-    }
+
+    var ValueOrFunction = (function(f) {
+      var result = (typeof(f) == "function") ? f(this.data, matches) : f;
+      // All triggers return either a string directly, or an object
+      // whose keys are different locale names.  For simplicity, this is
+      // valid to do for any trigger entry that can handle a function.
+      // In case anybody wants to encapsulate any fancy grammar, the values
+      // in this object can also be functions.
+      if (typeof(result) == "string" || result === undefined)
+        return result;
+      var lang = this.options.Language || 'en';
+      if (result.lang)
+        return ValueOrFunction(result.lang);
+      // For partially localized results where this localization doesn't
+      // exist, prefer English over nothing.
+      return ValueOrFunction(result['en']);
+    }).bind(this);
 
     var showText = this.options.TextAlertsEnabled;
     var playSounds = this.options.SoundAlertsEnabled;

ui/raidboss/data/triggers/test.js
@@ -44,14 +44,21 @@
     {
       id: 'Test Poke',
       regex: /:You poke the striking dummy/,
+      regexFr: /:You poke the striking dummy, in French/,
       preRun: function(data) {
         data.pokes = (data.pokes || 0) + 1;
       },
       infoText: function(data) {
-        return 'poke #' + data.pokes;
+        return {
+          en: 'poke #' + data.pokes,
+          fr: 'french poke #' + data.pokes,
+        };
       },
       tts: function(data) {
-        return 'poke ' + data.pokes;
+        return {
+          en: 'poke ' + data.pokes,
+          fr: 'french poke ' + data.pokes,
+        };
       },
     },
     {
@@ -64,11 +71,18 @@
     },
     {
       id: 'Test Laugh',
-      regex: /:You burst out laughing at the striking dummy/,
+      regexEn: /:You burst out laughing at the striking dummy/,
+      regexFr: /:You burst out laughing, in French/,
       alarmText: function(data) {
-        return 'hahahahaha';
+        return {
+          en: 'hahahahaha',
+          fr: 'hahaha in french',
+        };
       },
-      tts: 'hahahahaha',
+      tts: {
+        en: 'hahahahaha',
+        fr: 'hahaha in french',
+      },
     },
     {
       id: 'Test Clap',

commit bdc103a3c401bdcab08aa93019f467989b26fe32

raidboss: Fix triggers returning integers
This fixes a bug introduced in 6c2c5603 where anything that returned an
integer was incorrectly handled, so all delaySeconds became zero.

One day I will learn enough Javascript not to make silly mistakes.

ui/raidboss/popup-text.js
@@ -249,7 +249,7 @@ class PopupText {
       // valid to do for any trigger entry that can handle a function.
       // In case anybody wants to encapsulate any fancy grammar, the values
       // in this object can also be functions.
-      if (typeof(result) == "string" || result === undefined)
+      if (result !== Object(result))
         return result;
       var lang = this.options.Language || 'en';
       if (result.lang)