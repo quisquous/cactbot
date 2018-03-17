3311822e84a281a953eb56db2b1bc4e254181a85
DrLippe <echterbeck.philipp@gmail.com>
2018-03-17 14:29:32 +0100

    Update o5s.js
    cosmetic chages

--- ui/raidboss/data/triggers/o5s.js
+++ ui/raidboss/data/triggers/o5s.js
@@ -14,46 +14,46 @@
         'All in the Mind': 'Psychokinese',
         'Doom Strike': 'Vernichtungsschlag',
         'Head On': 'Frontalangriff',
-		'Diabolic Wind': 'Diabolischer Wind',
-		'Acid Rain': 'Säureregen',
-		'Crossing Whistle': 'Kreuzend Pfeife',
-		'Diabolic Headlamp': 'Diabolische Leuchte',
-		'Tether Whistle': 'Verfolger Pfeife',
-		'Diabolic Light': 'Diabolisches Licht',
-		') Ghosts': ') Geister',
-		'Ghosts spawn': 'Geister erscheinen',
+        'Diabolic Wind': 'Diabolischer Wind',
+        'Acid Rain': 'Säureregen',
+        'Crossing Whistle': 'Kreuzend Pfeife',
+        'Diabolic Headlamp': 'Diabolische Leuchte',
+        'Tether Whistle': 'Verfolger Pfeife',
+        'Diabolic Light': 'Diabolisches Licht',
+        ') Ghosts': ') Geister',
+        'Ghosts spawn': 'Geister erscheinen',
       },
       replaceSync: {
         'Engage!': 'Start!',
-		'Wroth Ghost': 'Erzürnter Geist',
-		'Phantom Train': 'Phantomzug',
-		'Remorse': 'Melancholischer Geist',
-		'Agony': 'Reuiger Geist',
-		'Malice': 'Bösartiger Geist',
+        'Wroth Ghost': 'Erzürnter Geist',
+        'Phantom Train': 'Phantomzug',
+        'Remorse': 'Melancholischer Geist',
+        'Agony': 'Reuiger Geist',
+        'Malice': 'Bösartiger Geist',
       },
     },
-	{
+    {
       locale: 'fr',
       replaceSync: {
         'Engage!': "À l'attaque !",
-		'Wroth Ghost': 'Fantôme Furieux',
-		'Phantom Train': 'Train Fantôme',
-		'Remorse': 'Fantôme Mélancolique',
-		'Agony': 'Fantôme Souffrant',
-		'Malice': 'Fantôme Rancunier',
+        'Wroth Ghost': 'Fantôme Furieux',
+        'Phantom Train': 'Train Fantôme',
+        'Remorse': 'Fantôme Mélancolique',
+        'Agony': 'Fantôme Souffrant',
+        'Malice': 'Fantôme Rancunier',
       },
-	},
-	{
+    },
+    {
       locale: 'jp',
       replaceSync: {
         'Engage!': '戦闘開始！',
-		'Wroth Ghost': 'ロスゴースト',
-		'Phantom Train': '魔列車',
-		'Remorse': '未練のゴースト',
-		'Agony': '苦悶のゴースト',
-		'Malice': '怨念のゴースト',
+        'Wroth Ghost': 'ロスゴースト',
+        'Phantom Train': '魔列車',
+        'Remorse': '未練のゴースト',
+        'Agony': '苦悶のゴースト',
+        'Malice': '怨念のゴースト',
       },
-	},
+    },
   ],
   triggers: [
     {
@@ -65,108 +65,107 @@
     {
       id: 'O5S Doom Strike',
       regex: /14:28B1:(魔列車|Phantom Train|Phantomzug|Train Fantôme) starts using (魔霊撃|Doom Strike|Vernichtungsschlag|Frappe Létale) on (\y{Name})/,
-	  alertText: function(data, matches) {
+      alertText: function(data, matches) {
         if (matches[1] == data.me)
           return {
-			  en: 'Tank Buster on YOU', 
-			  de: 'Tank Buster auf DIR',
-			  };
+              en: 'Tank Buster on YOU', 
+              de: 'Tank Buster auf DIR',
+              };
         if (data.role == 'healer')
           return {
-			  en: 'Buster on ' + data.ShortName(matches[1]),
-			  de: 'Buster auf ' +data.ShortName(matches[1]),
-			  };
+              en: 'Buster on ' + data.ShortName(matches[1]),
+              de: 'Buster auf ' +data.ShortName(matches[1]),
+              };
       },
       tts: function(data, matches) {
         if (matches[1] == data.me)
           return {
-			  en: 'buster',
-			  de: 'buster',
-			  };
+              en: 'buster',
+              de: 'buster',
+              };
       },
     },
     {
       id: 'O5S Head On',
       regex: /14:28A4:(魔列車|Phantom Train|Phantomzug|Train Fantôme) starts using (追突|Head On|Frontalangriff|Plein Fouet)/,
       alertText: function (data) {
-		  return {
-			  en: 'Go To Back',
-			  de: 'Nach HINTEN!',
-			  };
-	  },
+          return {
+              en: 'Go To Back',
+              de: 'Nach HINTEN!',
+              };
+      },
       tts: function(data) {
-		  return {
-			  en: 'run away',
-			  de: 'renn weg',
-		  };
-	  },
+          return {
+              en: 'run away',
+              de: 'renn weg',
+          };
+      },
     },
     {
       id: 'O5S Diabolic Headlamp',
       regex: /14:28B2:(魔列車|Phantom Train|Phantomzug|Train Fantôme) starts using (魔界の前照灯|Diabolic Headlamp|Diabolische Leuchte|Phare Diabolique)/,
-	  alertText: function (data) {
-		  return {
-			  en: 'Stack Middle',
-			  de: 'Versammeln Mitte',
-			  };
-	  },
+      alertText: function (data) {
+          return {
+              en: 'Stack Middle',
+              de: 'Versammeln Mitte',
+              };
+      },
       tts: function(data) {
-		  return {
-			  en: 'stack middle',
-			  de: 'versammeln mitte',
-		  };
-	  },
-
+          return {
+              en: 'stack middle',
+              de: 'versammeln mitte',
+          };
+      },
     },
     {
       id: 'O5S Diabolic Light',
       regex: /1B:........:(\y{Name}):....:....:0001:0000:0000:0000:/,
-	  condition: function(data, matches) { return matches[1] == data.me; },
+      condition: function(data, matches) { return matches[1] == data.me; },
       infoText: function (data) {
-		  return {
-			  en: 'Light',
-			  de: 'Licht',
-		  };
-	  },
+          return {
+              en: 'Light',
+              de: 'Licht',
+          };
+      },
       tts: function (data) {
-		  return {
-			  en: 'Light',
-			  de: 'Licht',
-		  };
-	  },
+          return {
+              en: 'Light',
+              de: 'Licht',
+          };
+      },
     },
     {
       id: 'O5S Diabolic Wind',
       regex: /1B:........:(\y{Name}):....:....:0046:0000:0000:0000:/,
-	  condition: function(data, matches) { return matches[1] == data.me; },
+      condition: function(data, matches) { return matches[1] == data.me; },
       infoText: function (data) {
-		  return {
-			  en: 'Wind',
-			  de: 'Wind',
-		  };
-	  },
+          return {
+              en: 'Wind',
+              de: 'Wind',
+          };
+      },
       tts: function (data) {
-		  return {
-			  en: 'Wind',
-			  de: 'Wind',
-		  };
-	  },
+          return {
+              en: 'Wind',
+              de: 'Wind',
+          };
+      },
     },
     {
       id: 'O5S Remorse',
       regex: /Added new combatant (未練のゴースト|Remorse|Melancholischer Geist|Fantôme Mélancolique)/,
-	  infoText: function (data) {
-		  return {
-			  en: 'Knockback Ghost',
-			  de: 'Rückstoß Geist',
-		  };
-	  },
+      infoText: function (data) {
+          return {
+              en: 'Knockback Ghost',
+              de: 'Rückstoß Geist',
+          };
+      },
       tts: function (data) {
-		  return {
-			  en: 'Knockback Ghost',
-			  de: 'Rückstoß Geist',
-		  };
-	  },
+          return {
+              en: 'Knockback Ghost',
+              de: 'Rückstoß Geist',
+          };
+      },
     },
   ]
 }]
