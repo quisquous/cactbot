d8ac673ca5c8fee005b7dd9c65d3f150982f695d
Lumas Arkfrost <39298126+ArkfrostLumas@users.noreply.github.com>
2018-05-22 02:37:14 +0200

    German done

--- ui/raidboss/data/triggers/drowned_city_of_skalla.js
+++ ui/raidboss/data/triggers/drowned_city_of_skalla.js
@@ -4,6 +4,7 @@
     {
       id: 'Hrodric Tank',
       regex: /:Hrodric Poisontongue starts using Rusting Claw/,
+      regexDe: /:Hrodric Giftzunge starts using Rostklaue/,
       infoText: function(data) {
         return data.role != 'tank' ? 'tank cleave' : '';
       },
@@ -15,29 +16,47 @@
     {
       id: 'Hrodric Tail',
       regex: /:Hrodric Poisontongue starts using Tail Drive/,
+      regexDe: /:Hrodric Giftzunge starts using Schwanzfetzer/,
       infoText: function(data) {
         return data.role == 'tank' ? "tail cleave" : "";
       },
       alertText: function(data) {
         return data.role != 'tank' ? "tail cleave" : "";
       },
-      tts: 'tail attack',
+      tts: {
+        en: 'tail attack',
+        de: 'schwanzangriff',
+      },
     },
     {
       id: 'Hrodric Eye',
       regex: /:Hrodric Poisontongue starts using Eye Of The Fire/,
+      regexDe: /:Hrodric Giftzunge starts using Feuerauge/,
       alertText: function(data) {
-        return "look away";
+        return {
+          en: "look away",
+          de: "wegschauen",
+        };
       },
-      tts: 'look away',
+      tts: {
+        en: 'look away',
+        de: 'weck schauen',
+      },
     },
     {
       id: 'Hrodric Words',
       regex: /:Hrodric Poisontongue starts using Words Of Woe/,
+      regexDe: /:Hrodric Giftzunge starts using Wehklagende Worte/,
       infoText: function(data) {
-        return "avoid eye lasers";
+        return {
+          en: "avoid eye lasers",
+          de: "augenlaser ausweichen",
+        };
       },
-      tts: 'eye laser',
+      tts: {
+        en: 'eye laser',
+        de: 'augenstrahl',
+      },
     },
   ]
 }]
