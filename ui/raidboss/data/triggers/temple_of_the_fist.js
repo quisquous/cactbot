2f4bba0a5e3f7ab1ede27aa279e89c4b1a7916c0
Lumas Arkfrost <39298126+ArkfrostLumas@users.noreply.github.com>
2018-05-27 21:37:48 +0200

    German Update Regexes
    Need the correctly formatted brackets for en: / de: here since I'm not sure how to make them here and last time I tried there was tons of red highlightes text around

--- ui/raidboss/data/triggers/temple_of_the_fist.js
+++ ui/raidboss/data/triggers/temple_of_the_fist.js
@@ -5,18 +5,21 @@
     {
       id: 'Temple Port And Star',
       regex: /:1FDC:Arbuda starts using Port And Star/,
+      regexDe: /:1FDC:Arbuda starts using Links & Rechts/,
       alertText: 'front/back are safe',
       tts: 'go front or back',
     },
     {
       id: 'Temple Fore And Aft',
       regex: /:1FDB:Arbuda starts using Fore And Aft/,
+      regexDe: /:1FDB:Arbuda starts using Vor & Zurück/,
       alertText: 'sides are safe',
       tts: 'go sides',
     },
     {
       id: 'Temple Killer Instinct',
       regex: /:1FDE:Arbuda starts using Killer Instinct/,
+      regexDe: /:1FDE:Arbuda starts using Vorausahnung/,
       alertText: 'watch for safe',
       tts: 'watch for safe',
     },
