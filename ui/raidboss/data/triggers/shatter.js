301e9a776c31230e37fc9b65cb392d068868765b
Adrienne Walker <enne@quisquo.us>
2018-06-02 12:51:04 -0700

    Translate Shatter into German

--- ui/raidboss/data/triggers/shatter.js
+++ ui/raidboss/data/triggers/shatter.js
@@ -3,41 +3,43 @@
   zoneRegex: /^The Fields Of Glory \(Shatter\)$/,
   triggers: [
     {
-      id: 'Big Ice',
+      id: 'Shatter Big Ice',
       regex: /The icebound tomelith A([1-4]) activates and begins to emit heat/,
       regexDe: /Vereister Echolith A([1-4]) activates and begins to emit heat/,
-      alertText: function(data, matches) {
-        var big_ice_dir = {
-          '1': 'Center',
-          '2': 'North',
-          '3': 'Southeast',
-          '4': 'Southwest',
+      preRun: function(data, matches) {
+        data.iceDir = undefined;
+        var ice_lang = {
+          en: {
+            '1': 'Center',
+            '2': 'North',
+            '3': 'Southeast',
+            '4': 'Southwest',
+          },
+          de: {
+            '1': 'Mitte',
+            '2': 'Norden',
+            '3': 'Süden',
+            '4': 'Südwesten',
+          },
         };
 
-        if (!(matches[1] in big_ice_dir)) {
-          return '';
-        }
-        return {
+        var big_ice_dir = ice_lang['en'];
+        if (data.lang in ice_lang)
+          big_ice_dir = ice_lang[data.lang];
+
+        if (!(matches[1] in big_ice_dir))
+          return;
+
+        data.iceDir = {
           en: 'Big Ice: ' + big_ice_dir[matches[1]],
           de: 'Grosses Eis: ' + big_ice_dir[matches[1]],
         };
       },
+      alertText: function(data) {
+        return data.iceDir;
+      },
       tts: function(data, matches) {
-        // TODO: figure out how to not duplicate this? or store func in data?
-        var big_ice_dir = {
-          '1': 'Center',
-          '2': 'North',
-          '3': 'Southeast',
-          '4': 'Southwest',
-        };
-
-        if (!(matches[1] in big_ice_dir)) {
-          return '';
-        }
-        return {
-          en: big_ice_dir[matches[1]] + ' big ice',
-          de: big_ice_dir[matches[1]] + ' großes Eis',
-        };
+        return data.iceDir;
       },
     },
   ],
