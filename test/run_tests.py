3db8c1c0d9506b9f21493b5275cfbeefb953eeb8
Adrienne Walker <enne@quisquo.us>
2018-06-03 11:11:04 -0700

    Add travis CI smoke test
    `npm test` now loads all the trigger files in node, which will complain
    if there's any syntax errors.  More tests in the future would be good
    too, but this "compilation" smoke test is a good start.

---
+++ test/run_tests.py
@@ -0,0 +1,31 @@
+import os
+import re
+import subprocess
+import sys
+
+def base_directory():
+  return os.path.join(os.path.dirname(__file__), '..');
+
+def triggers_directory():
+  return os.path.join(base_directory(), 'ui/raidboss/data/triggers/');
+
+def compile_test(filename):
+  err = subprocess.call(['node', filename])
+  return err == 0
+
+def run_compilation_tests():
+  success = True
+  dir = triggers_directory()
+  for filename in os.listdir(dir):
+    if filename.endswith('.js'):
+      success &= compile_test(os.path.join(dir, filename))
+  return success
+
+def main():
+  success = run_compilation_tests()
+  if not success:
+    sys.exit(1);
+  sys.exit(0);
+
+if __name__ == "__main__":
+    main()
