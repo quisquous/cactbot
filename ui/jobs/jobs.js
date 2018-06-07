8572ceca45f7046f49dc29adb48e84c17bac8ee7
Adrienne Walker <enne@quisquo.us>
2018-06-06 23:04:01 -0700

    Revert "jobs: fix monk blunt debuff timer"
    This reverts commit e1d8309012d031cfdc1c403e7cbde9a64adbef35.

--- ui/jobs/jobs.js
+++ ui/jobs/jobs.js
@@ -1850,8 +1850,6 @@ class Bars {
         r = log.match(kBluntDebuff);
         if (r != null) {
           var seconds = Regexes.ParseLocaleFloat(r[1]);
-          // https://github.com/ravahn/FFXIV_ACT_Plugin/issues/167
-          seconds = 15;
           this.OnMonkDragonKick(seconds);
           continue;
         }
