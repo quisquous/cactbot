quisquous/cactbot

commit fd3e2e093b391152cbc066f2ee8f97808e2ffe52

lint: resources/

resources/resourcebar.js
@@ -1,46 +1,84 @@
-"use strict";
+'use strict';
 
 class ResourceBar extends HTMLElement {
-  static get observedAttributes() { return [ "value", "maxvalue", "lefttext", "centertext", "righttext", "width", "height", "bg", "fg", "toward" ]; }
+  static get observedAttributes() {
+    return ['value', 'maxvalue', 'lefttext', 'centertext', 'righttext', 'width', 'height', 'bg', 'fg', 'toward'];
+  }
 
   // All visual dimensions are scaled by this.
-  set scale(s) { this.setAttribute("scale", s); }
-  get scale() { return this.getAttribute("scale"); }
+  set scale(s) {
+    this.setAttribute('scale', s);
+  }
+  get scale() {
+    return this.getAttribute('scale');
+  }
 
   // Background color.
-  set bg(c) { this.setAttribute("bg", c); }
-  get bg() { return this.getAttribute("bg"); }
+  set bg(c) {
+    this.setAttribute('bg', c);
+  }
+  get bg() {
+    return this.getAttribute('bg');
+  }
 
   // Foreground color.
-  set fg(c) { this.setAttribute("fg", c); }
-  get fg() { return this.getAttribute("fg"); }
+  set fg(c) {
+    this.setAttribute('fg', c);
+  }
+  get fg() {
+    return this.getAttribute('fg');
+  }
 
   // The width of the bar, in pixels (before |scale|).
-  set width(w) { this.setAttribute("width", w); }
-  get width() { return this.getAttribute("width"); }
+  set width(w) {
+    this.setAttribute('width', w);
+  }
+  get width() {
+    return this.getAttribute('width');
+  }
 
   // The height of the bar, in pixels (before |scale|).
-  set height(w) { this.setAttribute("height", w); }
-  get height() { return this.getAttribute("height"); }
+  set height(w) {
+    this.setAttribute('height', w);
+  }
+  get height() {
+    return this.getAttribute('height');
+  }
 
   // A value between 0 and |maxvalue|, indicating the amount of progress.
-  set value(s) { this.setAttribute("value", s); }
-  get value() { return this.getAttribute("value"); }
+  set value(s) {
+    this.setAttribute('value', s);
+  }
+  get value() {
+    return this.getAttribute('value');
+  }
 
   // The maximum value where when reached the progress bar will show 100%.
-  set maxvalue(s) { this.setAttribute("maxvalue", s); }
-  get maxvalue() { return this.getAttribute("maxvalue"); }
-  
+  set maxvalue(s) {
+    this.setAttribute('maxvalue', s);
+  }
+  get maxvalue() {
+    return this.getAttribute('maxvalue');
+  }
+
   // If "right" then fills left-to-right (the default). If "left" then
   // fills right-to-left.
-  set toward(t) { this.setAttribute("toward", t); }
-  get toward() { return this.getAttribute("toward"); }
+  set toward(t) {
+    this.setAttribute('toward', t);
+  }
+  get toward() {
+    return this.getAttribute('toward');
+  }
 
   // If "fill" then the progress goes empty-to-full, if "empty" then the
   // progress bar starts full and goes to empty.
-  set style(s) { this.setAttribute("style", s); }
-  get style() { return this.getAttribute("style"); }
-  
+  set style(s) {
+    this.setAttribute('style', s);
+  }
+  get style() {
+    return this.getAttribute('style');
+  }
+
   // Chooses what should be shown in the text field in each area of
   // the bar. Can be one of:
   // empty - nothing is shown.
@@ -48,29 +86,45 @@ class ResourceBar extends HTMLElement {
   // "maxvalue" - shows the current and maximum raw values.
   // "percent" - shows the percentage.
   // anything else - the given text is shown literally.
-  set lefttext(p) { this.setAttribute("lefttext", p); }
-  get lefttext() { return this.getAttribute("lefttext"); }
-  set righttext(p) { this.setAttribute("righttext", p); }
-  get righttext() { return this.getAttribute("righttext"); }
-  set centertext(p) { this.setAttribute("centertext", p); }
-  get centertext() { return this.getAttribute("centertext"); }
-  
+  set lefttext(p) {
+    this.setAttribute('lefttext', p);
+  }
+  get lefttext() {
+    return this.getAttribute('lefttext');
+  }
+  set righttext(p) {
+    this.setAttribute('righttext', p);
+  }
+  get righttext() {
+    return this.getAttribute('righttext');
+  }
+  set centertext(p) {
+    this.setAttribute('centertext', p);
+  }
+  get centertext() {
+    return this.getAttribute('centertext');
+  }
+
   // This would be used with window.customElements.
   constructor() {
     super();
-    var root = this.attachShadow({mode: 'open'});
+    let root = this.attachShadow({ mode: 'open' });
     this.init(root);
   }
-  
+
   // These would be used by document.registerElement, which is deprecated but
   // ACT uses an old CEF which has this instead of the newer APIs.
   createdCallback() {
-    var root = this.createShadowRoot();
+    let root = this.createShadowRoot();
     this.init(root);
   }
   // Convert from the deprecated API names to the modern API names.
-  attachedCallback() { this.connectedCallback(); }
-  detachedCallback() { this.disconnectedCallback(); }
+  attachedCallback() {
+    this.connectedCallback();
+  }
+  detachedCallback() {
+    this.disconnectedCallback();
+  }
 
   init(root) {
     root.innerHTML = `
@@ -108,91 +162,91 @@ class ResourceBar extends HTMLElement {
         <div id="centertext" class="text"></div>
         <div id="lefttext" class="text"></div>
       </div>
-    `
+    `;
   }
-  
+
   connectedCallback() {
-    this.foregroundElement = this.shadowRoot.getElementById("fg");
-    this.backgroundElement = this.shadowRoot.getElementById("bg");
-    this.leftTextElement = this.shadowRoot.getElementById("lefttext");
-    this.centerTextElement = this.shadowRoot.getElementById("centertext");
-    this.rightTextElement = this.shadowRoot.getElementById("righttext");
+    this.foregroundElement = this.shadowRoot.getElementById('fg');
+    this.backgroundElement = this.shadowRoot.getElementById('bg');
+    this.leftTextElement = this.shadowRoot.getElementById('lefttext');
+    this.centerTextElement = this.shadowRoot.getElementById('centertext');
+    this.rightTextElement = this.shadowRoot.getElementById('righttext');
 
     // Constants.
     this.kBackgroundOpacity = 0.8;
     this.kBorderSize = 1;
     this.kTextLeftRightEdgePadding = this.kBorderSize * 3;
     this.kTextTopBottomEdgePadding = this.kBorderSize * 2;
-    
+
     // Default values.
     this._value = 0;
     this._maxvalue = 1;
     this._width = 200;
     this._height = 20;
-    this._bg = "rgba(0, 0, 0, 0.7)";
-    this._fg = "green";
+    this._bg = 'rgba(0, 0, 0, 0.7)';
+    this._fg = 'green';
     this._scale = 1;
     this._toward_right = true;
     this._style_fill = true;
-    this._left_text = "";
-    this._center_text = "";
-    this._right_text = "";
+    this._left_text = '';
+    this._center_text = '';
+    this._right_text = '';
 
-    if (this.value != null) { this._value = Math.max(parseFloat(this.value), 0); }
-    if (this.maxvalue != null) { this._maxvalue = Math.max(parseFloat(this.maxvalue), 0); }
-    if (this.width != null) { this._width = Math.max(parseInt(this.width), 1); }
-    if (this.height != null) { this._height = Math.max(parseInt(this.height), 1); }
-    if (this.bg != null) { this._bg = this.bg; }
-    if (this.fg != null) { this._fg = this.fg; }
-    if (this.scale != null) { this._scale = Math.max(parseFloat(this.scale), 0.01); }
-    if (this.toward != null) { this._toward_right = this.toward != "left"; }
-    if (this.style != null) { this._style_fill = this.style != "empty"; }
-    if (this.lefttext != null) { this._left_text = this.lefttext; }
-    if (this.centertext != null) { this._center_text = this.centertext; }
-    if (this.righttext != null) { this._right_text = this.righttext; }
-    
+    if (this.value != null) this._value = Math.max(parseFloat(this.value), 0);
+    if (this.maxvalue != null) this._maxvalue = Math.max(parseFloat(this.maxvalue), 0);
+    if (this.width != null) this._width = Math.max(parseInt(this.width), 1);
+    if (this.height != null) this._height = Math.max(parseInt(this.height), 1);
+    if (this.bg != null) this._bg = this.bg;
+    if (this.fg != null) this._fg = this.fg;
+    if (this.scale != null) this._scale = Math.max(parseFloat(this.scale), 0.01);
+    if (this.toward != null) this._toward_right = this.toward != 'left';
+    if (this.style != null) this._style_fill = this.style != 'empty';
+    if (this.lefttext != null) this._left_text = this.lefttext;
+    if (this.centertext != null) this._center_text = this.centertext;
+    if (this.righttext != null) this._right_text = this.righttext;
+
     this._connected = true;
     this.layout();
     this.updateText();
     this.draw();
   }
-  
+
   disconnectedCallback() {
     this._connected = false;
   }
-  
+
   attributeChangedCallback(name, oldValue, newValue) {
-    if (name == "value")
+    if (name == 'value') {
       this._value = Math.max(parseFloat(newValue), 0);
-    else if (name == "maxvalue")
+    } else if (name == 'maxvalue') {
       this._maxvalue = Math.max(parseFloat(newValue), 0);
-    else if (name == "width") {
+    } else if (name == 'width') {
       this._width = Math.max(parseInt(newValue), 1);
       this.layout();
-    } else if (name == "height") {
+    } else if (name == 'height') {
       this._height = Math.max(parseInt(newValue), 1);
       this.layout();
-    } else if (name == "bg") {
+    } else if (name == 'bg') {
       this._bg = newValue;
       this.layout();
-    } else if (name == "fg") {
+    } else if (name == 'fg') {
       this._fg = newValue;
       this.layout();
-    } else if (name == "toward") {
-      this._toward_right = newValue != "left";
+    } else if (name == 'toward') {
+      this._toward_right = newValue != 'left';
       this.layout();
-    } else if (name == "lefttext") {
-      var update = newValue != this._left_text && this._connected;
+    } else if (name == 'lefttext') {
+      let update = newValue != this._left_text && this._connected;
       this._left_text = newValue;
       if (update)
         this.updateText();
-    } else if (name == "centertext") {
-      var update = newValue != this._center_text && this._connected;
+    } else if (name == 'centertext') {
+      let update = newValue != this._center_text && this._connected;
       this._center_text = newValue;
       if (update)
         this.updateText();
-    } else if (name == "righttext") {
-      var update = newValue != this._right_text && this._connected;
+    } else if (name == 'righttext') {
+      let update = newValue != this._right_text && this._connected;
       this._right_text = newValue;
       if (update)
         this.updateText();
@@ -201,7 +255,7 @@ class ResourceBar extends HTMLElement {
     if (this._connected)
       this.draw();
   }
-  
+
   layout() {
     if (!this._connected)
       return;
@@ -210,91 +264,91 @@ class ResourceBar extends HTMLElement {
     // the direction.
     if (!this._style_fill)
       this._toward_right = !this._toward_right;
-    
-    var backgroundStyle = this.backgroundElement.style;
-    var foregroundStyle = this.foregroundElement.style;
-    var lTextStyle = this.leftTextElement.style;
-    var cTextStyle = this.centerTextElement.style;
-    var rTextStyle = this.rightTextElement.style;
-    
+
+    let backgroundStyle = this.backgroundElement.style;
+    let foregroundStyle = this.foregroundElement.style;
+    let lTextStyle = this.leftTextElement.style;
+    let cTextStyle = this.centerTextElement.style;
+    let rTextStyle = this.rightTextElement.style;
+
     backgroundStyle.backgroundColor = this._bg;
     foregroundStyle.backgroundColor = this._fg;
 
     backgroundStyle.opacity = this.kBackgroundOpacity;
-    
+
     backgroundStyle.width = this._width * this._scale;
     backgroundStyle.height = this._height * this._scale;
-    
+
     foregroundStyle.width = (this._width - this.kBorderSize * 2) * this._scale;
     foregroundStyle.height = (this._height - this.kBorderSize * 2) * this._scale;
     foregroundStyle.left = this.kBorderSize * this._scale;
     foregroundStyle.top = this.kBorderSize * this._scale;
 
-    lTextStyle.width = (this._width - this.kBorderSize * 4 - this.kTextLeftRightEdgePadding * 2) * this._scale;
-    lTextStyle.height = (this._height - this.kBorderSize * 4 - this.kTextTopBottomEdgePadding * 2) * this._scale;
+    let borderPadding = this.kBorderSize * 4 - this.kTextLeftRightEdgePadding * 2;
+    lTextStyle.width = (this._width - borderPadding) * this._scale;
+    lTextStyle.height = (this._height - borderPadding) * this._scale;
     lTextStyle.left = (this.kBorderSize + this.kTextLeftRightEdgePadding) * this._scale;
     lTextStyle.top = (this.kBorderSize + this.kTextTopBottomEdgePadding) * this._scale;
     lTextStyle.fontSize = lTextStyle.height;
-    
+
     cTextStyle.width = rTextStyle.width = lTextStyle.width;
     cTextStyle.height = rTextStyle.height = lTextStyle.height;
     cTextStyle.left = rTextStyle.left = lTextStyle.left;
     cTextStyle.top = rTextStyle.top = lTextStyle.top;
     cTextStyle.fontSize = rTextStyle.fontSize = lTextStyle.fontSize;
-    
+
     if (this._toward_right)
-      foregroundStyle.transformOrigin = "0% 0%";
+      foregroundStyle.transformOrigin = '0% 0%';
     else
-      foregroundStyle.transformOrigin = "100% 0%";
+      foregroundStyle.transformOrigin = '100% 0%';
   }
-  
+
   updateText() {
     // These values are filled in during draw() when the values change.
-    if (this._left_text != "value" && this._left_text != "maxvalue" &&
-        this._left_text != "percent") {
+    if (this._left_text != 'value' && this._left_text != 'maxvalue' &&
+        this._left_text != 'percent') {
       // Otherwise the value is fixed so it can be set here.
       this.leftTextElement.innerHTML = this._left_text;
     }
-    if (this._center_text != "value" && this._center_text != "maxvalue" &&
-        this._center_text != "percent") {
+    if (this._center_text != 'value' && this._center_text != 'maxvalue' &&
+        this._center_text != 'percent')
       this.centerTextElement.innerHTML = this._center_text;
-    }
-    if (this._right_text != "value" && this._right_text != "maxvalue" &&
-        this._right_text != "percent") {
+
+    if (this._right_text != 'value' && this._right_text != 'maxvalue' &&
+        this._right_text != 'percent')
       this.rightTextElement.innerHTML = this._right_text;
-    }
   }
 
   draw() {
-    var percent = this._maxvalue <= 0 ? 1 : this._value / this._maxvalue;
+    let percent = this._maxvalue <= 0 ? 1 : this._value / this._maxvalue;
     // Keep it between 0 and 1.
     percent = Math.min(1, Math.max(0, percent));
     if (!this._style_fill)
       percent = 1.0 - percent;
-    this.foregroundElement.style.transform = "scale(" + percent + ",1)";
-    if (this._left_text != "") {
-      if (this._left_text == "value")
+    this.foregroundElement.style.transform = 'scale(' + percent + ',1)';
+    if (this._left_text != '') {
+      if (this._left_text == 'value')
         this.leftTextElement.innerHTML = this._value;
-      else if (this._left_text == "maxvalue")
-        this.leftTextElement.innerHTML = this._value + " / " + this._maxvalue;
-      else if (this._left_text == "percent")
-        this.leftTextElement.innerHTML = parseInt(percent * 100) + " %";
+      else if (this._left_text == 'maxvalue')
+        this.leftTextElement.innerHTML = this._value + ' / ' + this._maxvalue;
+      else if (this._left_text == 'percent')
+        this.leftTextElement.innerHTML = parseInt(percent * 100) + ' %';
     }
-    if (this._center_text != "") {
-      if (this._center_text == "value")
+    if (this._center_text != '') {
+      if (this._center_text == 'value')
         this.centerTextElement.innerHTML = this._value;
-      else if (this._center_text == "maxvalue")
-        this.centerTextElement.innerHTML = this._value + " / " + this._maxvalue;
-      else if (this._center_text == "percent")
-        this.centerTextElement.innerHTML = parseInt(percent * 100) + " %";
+      else if (this._center_text == 'maxvalue')
+        this.centerTextElement.innerHTML = this._value + ' / ' + this._maxvalue;
+      else if (this._center_text == 'percent')
+        this.centerTextElement.innerHTML = parseInt(percent * 100) + ' %';
     }
-    if (this._right_text != "") {
-      if (this._right_text == "value")
+    if (this._right_text != '') {
+      if (this._right_text == 'value')
         this.rightTextElement.innerHTML = this._value;
-      else if (this._right_text == "maxvalue")
-        this.rightTextElement.innerHTML = this._value + " / " + this._maxvalue;
-      else if (this._right_text == "percent")
-        this.rightTextElement.innerHTML = parseInt(percent * 100) + " %";
+      else if (this._right_text == 'maxvalue')
+        this.rightTextElement.innerHTML = this._value + ' / ' + this._maxvalue;
+      else if (this._right_text == 'percent')
+        this.rightTextElement.innerHTML = parseInt(percent * 100) + ' %';
     }
   }
 }
@@ -304,6 +358,6 @@ if (window.customElements) {
   window.customElements.define('resource-bar', ResourceBar);
 } else {
   document.registerElement('resource-bar', {
-    prototype: Object.create(ResourceBar.prototype)
+    prototype: Object.create(ResourceBar.prototype),
   });
 }

commit 673c8c008e3a67d2ee646779d4666de8750a4d33

Fix bugs introduced by reformatting <_<
This fixes bugs introduced in fd3e2e09, which largely resulted in
comically large hp / mp / cp text in bars.  OOPS.

resources/resourcebar.js
@@ -284,9 +284,10 @@ class ResourceBar extends HTMLElement {
     foregroundStyle.left = this.kBorderSize * this._scale;
     foregroundStyle.top = this.kBorderSize * this._scale;
 
-    let borderPadding = this.kBorderSize * 4 - this.kTextLeftRightEdgePadding * 2;
-    lTextStyle.width = (this._width - borderPadding) * this._scale;
-    lTextStyle.height = (this._height - borderPadding) * this._scale;
+    let widthPadding = this.kBorderSize * 4 + this.kTextLeftRightEdgePadding * 2;
+    lTextStyle.width = (this._width - widthPadding) * this._scale;
+    let heightPadding = this.kBorderSize * 4 + this.kTextTopBottomEdgePadding * 2;
+    lTextStyle.height = (this._height - heightPadding) * this._scale;
     lTextStyle.left = (this.kBorderSize + this.kTextLeftRightEdgePadding) * this._scale;
     lTextStyle.top = (this.kBorderSize + this.kTextTopBottomEdgePadding) * this._scale;
     lTextStyle.fontSize = lTextStyle.height;

resources/timericon.js
@@ -248,7 +248,7 @@ class TimerIcon extends HTMLElement {
     borderBackgroundStyle.width = this._width * this._scale;
     borderBackgroundStyle.height = this._height * this._scale;
 
-    let borderPadding = this.kOuterBorderSize * 2 - this._color_border_size * 2;
+    let borderPadding = this.kOuterBorderSize * 2 + this._color_border_size * 2;
     borderForegroundStyle.width = (this._width - borderPadding) * this._scale;
     borderForegroundStyle.height = (this._height - borderPadding) * this._scale;
     borderForegroundStyle.borderWidth = this._color_border_size * this._scale;
@@ -259,7 +259,7 @@ class TimerIcon extends HTMLElement {
 
     let icon_left = (this.kOuterBorderSize * 2 + this._color_border_size) * this._scale;
     let icon_top = (this.kOuterBorderSize * 2 + this._color_border_size) * this._scale;
-    let iconPadding = this.kOuterBorderSize * 4 - this._color_border_size * 2;
+    let iconPadding = this.kOuterBorderSize * 4 + this._color_border_size * 2;
     let icon_width = (this._width - iconPadding ) * this._scale;
     let icon_height = (this._height - iconPadding) * this._scale;
     let text_height = Math.ceil(Math.min(icon_width, icon_height) / 1.8);