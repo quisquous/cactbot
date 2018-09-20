0df07303f285fd3c31ad5acff8fc1a06597820f9
Adrienne Walker <enne@quisquo.us>
2018-09-19 22:38:40 -0700

    Initial o10s triggers

--- ui/raidboss/data/triggers/o10s.js
+++ ui/raidboss/data/triggers/o10s.js
@@ -5,5 +5,44 @@
   zoneRegex: /^Alphascape V2.0 \(Savage\)$/,
   timelineFile: 'o10s.txt',
   triggers: [
+    {
+      id: 'O10S Tail End',
+      regex: / 14:31AA:Midgardsormr starts using Tail End on (\y{Name})/,
+      alertText: function(data, matches) {
+        if (matches[1] == data.me) {
+          return {
+            en: 'Tank Buster on YOU',
+            de: 'Tenkbuster auf DIR',
+            fr: 'Tankbuster sur VOUS',
+          };
+        }
+        if (data.role == 'healer') {
+          return {
+            en: 'Buster on ' + data.ShortName(matches[1]),
+            de: 'Tenkbuster auf ' + data.ShortName(matches[1]),
+            fr: 'Tankbuster sur ' + data.ShortName(matches[1]),
+          };
+        }
+      },
+      tts: function(data, matches) {
+        if (matches[1] == data.me) {
+          return {
+            en: 'buster',
+            de: 'basta',
+            fr: 'tankbuster',
+          };
+        }
+      },
+    },
+    {
+      id: 'O10S Fire Marker',
+      regex: / 1B:........:(\y{Name}):....:....:0017:0000:0000:0000:/,
+      condition: function(data, matches) {
+        return data.me == matches[1];
+      },
+      alarmText: {
+        en: 'Fire Marker',
+      },
+    },
   ],
 }];
