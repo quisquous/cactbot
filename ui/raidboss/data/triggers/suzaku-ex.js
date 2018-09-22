625cf383702b66d3898f6c1b691742be7b64caa5
Adrienne Walker <enne@quisquo.us>
2018-09-22 16:10:39 -0700

    raidboss: suzaku triggers

--- ui/raidboss/data/triggers/suzaku-ex.js
+++ ui/raidboss/data/triggers/suzaku-ex.js
@@ -4,6 +4,70 @@
 [{
   zoneRegex: /^Hells\' Kier \(Extreme\)$/,
   timelineFile: 'suzaku-ex.txt',
-  timelineTriggers: [
+  triggers: [
+    {
+      id: 'SuzEx Cremate',
+      regex: / 14:32D1:Suzaku starts using Cremate on (\y{Name})/,
+      alertText: function(data, matches) {
+        if (matches[1] == data.me) {
+          return {
+            en: 'Tank Buster on YOU',
+            de: 'Tenkbuster auf DIR',
+            fr: 'Tankbuster sur VOUS',
+          };
+        }
+        if (data.role == 'healer') {
+          return {
+            en: 'Buster on ' + data.ShortName(matches[1]),
+            de: 'Tenkbuster auf ' + data.ShortName(matches[1]),
+            fr: 'Tankbuster sur ' + data.ShortName(matches[1]),
+          };
+        }
+      },
+      tts: function(data, matches) {
+        if (matches[1] == data.me) {
+          return {
+            en: 'buster',
+            de: 'basta',
+            fr: 'tankbuster',
+          };
+        }
+      },
+    },
+    {
+      id: 'SuzEx Phantom Flurry',
+      regex: / 14:32DC:Suzaku starts using/,
+      condition: function(data) {
+        return data.role == 'tank' || data.role == 'healer';
+      },
+      alertText: {
+        en: 'Tank Buster',
+        de: 'Tenkbuster',
+        fr: 'Tankbuster ',
+      },
+      tts: function(data, matches) {
+        if (matches[1] == data.me) {
+          return {
+            en: 'buster',
+            de: 'basta',
+            fr: 'tankbuster',
+          };
+        }
+      },
+    },
+    {
+      id: 'SuzEx Mesmerizing Melody',
+      regex: / 14:32DA:Suzaku starts using Mesmerizing Melody/,
+      alertText: {
+        en: 'Get Out',
+      },
+    },
+    {
+      id: 'SuzEx Ruthless Refrain',
+      regex: / 14:32DB:Suzaku starts using Ruthless Refrain/,
+      alertText: {
+        en: 'Get In',
+      },
+    },
   ],
 }];
