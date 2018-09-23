42b9a7dcd8c5419ec59c0da4eee8b9488a00b003
Adrienne Walker <enne@quisquo.us>
2018-09-22 18:43:51 -0700

    raidboss: more o10s triggers

--- ui/raidboss/data/triggers/o10s.js
+++ ui/raidboss/data/triggers/o10s.js
@@ -1,5 +1,13 @@
 'use strict';
 
+// TODO: cross/in/out callouts
+// TODO: fix tail end (seemed to not work??)
+// TODO: add phase tracking (so death from above/below can tell you to swap or not)
+// TODO: add swap callout after exaflares
+// TODO: debuff tracking for when you lose the barrier to remind you to run?
+// TODO: ice head markers
+// TODO: stack head markers
+
 // O10S - Alphascape 2.0 Savage
 [{
   zoneRegex: /^Alphascape V2.0 \(Savage\)$/,
@@ -41,7 +49,31 @@
         return data.me == matches[1];
       },
       alarmText: {
-        en: 'Fire Marker',
+        en: 'Fire Marker on YOU',
+      },
+      infoText: function(data, matches) {
+        if (data.me != matches[1])
+          return 'Fire on ' + data.ShortName(matches[1]);
+      },
+    },
+    {
+      id: 'O10S Death From Below',
+      regex: / 1B:........:(\y{Name}):....:....:008F:0000:0000:0000:/,
+      condition: function(data, matches) {
+        return data.me == matches[1];
+      },
+      infoText: {
+        en: 'Death From Below',
+      },
+    },
+    {
+      id: 'O10S Death From Above',
+      regex: / 1B:........:(\y{Name}):....:....:008E:0000:0000:0000:/,
+      condition: function(data, matches) {
+        return data.me == matches[1];
+      },
+      infoText: {
+        en: 'Death From Above',
       },
     },
   ],
