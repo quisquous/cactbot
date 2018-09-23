5425c9713b6f69aa0dacafd7e4eb7894e53b4f44
unfaiyted <unfaiyted@gmail.com>
2018-09-22 19:10:06 -0500

    fixing issues with o10s failing linter

--- ui/raidboss/data/triggers/o10s.js
+++ ui/raidboss/data/triggers/o10s.js
@@ -55,45 +55,42 @@
       id: '010S Flip Count',
       regex: /: Flip/,
       infoText: {
-        en: 'Flip'
+        en: 'Flip',
       },
       preRun: function(data) {
         data.firstMove = (data.firstMove || 'flip');
         data.totalCoilFlip = (data.totalCoilFlip || 0) + 1;
-
         // Coil Flip
-        if(data.firstMove === 'coil') {
-          data.coilFlipText = 'get in, donut'
+        if (data.firstMove === 'coil') {
+          data.coilFlipText = 'get in, donut';
         } else { // Flip Flip
-          data.coilFlipText = 'plus sign'
+          data.coilFlipText = 'plus sign';
         }
-
       },
       tts: function(data) {
         return (data.totalCoilFlip === 2) ? data.coilFlipText : null;
-      }
+      },
     },
     {
       id: '010S Coil Count',
       regex: /: Flip/,
       infoText: {
-        en: 'Flip'
+        en: 'Flip',
       },
       preRun: function(data) {
         data.firstMove = (data.firstMove || 'coil');
         data.totalCoilFlip = (data.totalCoilFlip || 0) + 1;
 
         // Coil Coil
-        if(data.firstMove === 'coil') {
-          data.coilFlipText = 'get out, max melee'
+        if (data.firstMove === 'coil') {
+          data.coilFlipText = 'get out, max melee';
         } else { // Flip Coil
-          data.coilFlipText = 'X sign'
+          data.coilFlipText = 'X sign';
         }
-
       },
       tts: function(data) {
         return (data.totalCoilFlip === 2) ? data.coilFlipText : null;
-      }
+      },
     },
     {
       id: 'O10S In/Out',
@@ -102,7 +99,29 @@
         // resets coilFlip
         delete data.totalCoilFlip;
         delete data.firstMove;
-      }
-    }
+      },
+    },
+    {
+      id: 'O10S Death from Above',
+      regex: 'Death from Above',
+      tts: function(data, matches) {
+        if (matches[1] == data.me) {
+          return {
+            en: data.ShortName(matches[1]) + ' get ground units',
+          };
+        }
+      },
+    },
+    {
+      id: 'O10S Death from Below',
+      regex: 'Death from Below',
+      tts: function(data, matches) {
+        if (matches[1] == data.me) {
+          return {
+            en: data.ShortName(matches[1]) + ' tank boss',
+          };
+        }
+      },
+    },
   ],
 }];
