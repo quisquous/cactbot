0742271a68e02e77dc284580e7275dc66ae78cfa
mooondark <40036185+mooondark@users.noreply.github.com>
2018-09-30 01:58:34 +0200

    Starting FR translation for o9s/o10n/suzaku-ex. (#226)

--- ui/raidboss/data/triggers/suzaku-ex.js
+++ ui/raidboss/data/triggers/suzaku-ex.js
@@ -8,6 +8,7 @@
     {
       id: 'SuzEx Cremate',
       regex: / 14:32D1:Suzaku starts using Cremate on (\y{Name})/,
+      regexFr: / 14:32D1:Suzaku starts using Crématorium on (\y{Name})/,
       alertText: function(data, matches) {
         if (matches[1] == data.me) {
           return {
@@ -37,13 +38,14 @@
     {
       id: 'SuzEx Phantom Flurry',
       regex: / 14:32DC:Suzaku starts using/,
+      regexFr: / 14:32DC:Suzaku prépare/,
       condition: function(data) {
         return data.role == 'tank' || data.role == 'healer';
       },
       alertText: {
         en: 'Tank Buster',
         de: 'Tenkbuster',
-        fr: 'Tankbuster ',
+        fr: 'Tankbuster',
       },
       tts: function(data, matches) {
         if (matches[1] == data.me) {
@@ -58,15 +60,19 @@
     {
       id: 'SuzEx Mesmerizing Melody',
       regex: / 14:32DA:Suzaku starts using Mesmerizing Melody/,
+      regexFr: / 14:32DA:Suzaku starts using Mélodie Hypnotique/,
       alertText: {
         en: 'Get Out',
+        fr: 'Allez au bord extérieur',
       },
     },
     {
       id: 'SuzEx Ruthless Refrain',
       regex: / 14:32DB:Suzaku starts using Ruthless Refrain/,
+      regexFr: / 14:32DB:Suzaku starts using Refrain Impitoyable/,
       alertText: {
         en: 'Get In',
+        fr: 'Allez au bord intérieur',
       },
     },
   ],
