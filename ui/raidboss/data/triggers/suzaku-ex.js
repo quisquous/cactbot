078419527b2f457ea26b5166f16528398f957e43
mooondark <40036185+mooondark@users.noreply.github.com>
2018-10-01 13:53:12 +0200

    SuzakuEx FR timeline translation

--- ui/raidboss/data/triggers/suzaku-ex.js
+++ ui/raidboss/data/triggers/suzaku-ex.js
@@ -38,7 +38,6 @@
     {
       id: 'SuzEx Phantom Flurry',
       regex: / 14:32DC:Suzaku starts using/,
-      regexFr: / 14:32DC:Suzaku prépare/,
       condition: function(data) {
         return data.role == 'tank' || data.role == 'healer';
       },
@@ -73,6 +72,45 @@
       alertText: {
         en: 'Get In',
         fr: 'Allez au bord intérieur',
+      },
+    },
+  ],
+  timelineReplace: [
+    {
+      'locale': 'fr',
+      'replaceSync': {
+        'Engage!': 'À l\'attaque',
+      },
+      'replaceText': {
+        '--Reset--': '--Réinitialisation--',
+        '--sync--': '--Synchronisation--',
+        '--targetable--': '--Ciblable--',
+        '--untargetable--': '--Impossible à cibler--',
+        'Enrage': 'Enrage',
+        'Screams Of The Damned': 'Cris Des Damnés',
+        'Rout': 'Irruption',
+        'Rekindle': 'Ravivement',
+        'Fleeting Summer': 'Ailes Vermillon',
+        'Cremate': 'Crématorium',
+        'Phoenix Down': 'Queue De Phénix',
+        'Wing And A Prayer': 'Prière De Pennes',
+        'Eternal Flame': 'Flamme Éternelle',
+        'Scarlet Hymn': 'Hymne Vermillon',
+        'Scarlet Fever': 'Fièvre Écarlate',
+        'Southron Star': 'Étoile Australe',
+        'Mesmerizing Melody': 'Mélodie Hypnotique',
+        'Well Of Flame': 'Puits Ardent',
+        'Scathing Net': 'Étoiles Des Enfers',
+        'Phantom Flurry': 'Frénésie Spectrale',
+        'Phantom Half': 'Frénésie - Moitié de plateau',
+        'Hotspot x8': 'Couleurs x8',
+        'Ruthless/Mesmerizing': 'Refrain/Mélodie',
+        'Close-Quarter Crescendo': 'Mélopée Fantoche',
+        'Pay The Piper': 'Pay The Piper',
+        'Incandescent Interlude': 'Mélopée Incandescente',
+        'Ruthless Refrain': 'Refrain Impitoyable',
+        'Hotspot x4': 'Couleurs x4',
+        'Hotspot Enrage': 'Enrage',
       },
     },
   ],
