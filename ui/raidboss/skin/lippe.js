6f34d70b9463a5d9019a9dc6cb88ca990510cc5b
DrLippe <echterbeck.philipp@gmail.com>
2018-11-10 12:34:01 +0100

    Update lippe.js

--- ui/raidboss/skin/lippe.js
+++ ui/raidboss/skin/lippe.js
@@ -1,113 +1,126 @@
-"use strict";
+'use strict';
 
 class TimerBar extends HTMLElement {
-  static get observedAttributes() { return [ "duration", "value", "elapsed", "hideafter", "lefttext", "centertext", "righttext", "width", "height", "bg", "bgborder", "fg", "style", "toward" ]; }
-
-  // Background color.
-  set bg(c) { this.setAttribute("bg", c); }
-  get bg() { return this.getAttribute("bg"); }
-  
-  // Background Border color.
-  set bgborder(c) { this.setAttribute("bgborder", c); }
-  get bgborder() { return this.getAttribute("bgborder"); }
-
-  // Foreground color.
-  set fg(c) { this.setAttribute("fg", c); }
-  get fg() { return this.getAttribute("fg"); }
-
-  // The width of the bar.
-  set width(w) { this.setAttribute("width", w); }
-  get width() { return this.getAttribute("width"); }
-
-  // The height of the bar.
-  set height(w) { this.setAttribute("height", w); }
-  get height() { return this.getAttribute("height"); }
-
-  // The total length of time to count down.
-  set duration(s) { this.attributeChangedCallback("duration" , this.duration, s); }
-  get duration() { return this._duration.toString(); }
-
-  // The length remaining in the count down.
-  set value(s) { this.attributeChangedCallback("value" , this.value, s); }
+  static get observedAttributes() {
+    return ['duration', 'value', 'elapsed', 'hideafter', 'lefttext', 'centertext', 'righttext', 'width', 'height', 'bg', 'bgborder', 'fg', 'style', 'toward'];
+  }
+  set bg(c) {
+    this.setAttribute('bg', c);
+  }
+  get bg() {
+    return this.getAttribute('bg');
+  }
+  set bgborder(c) {
+    this.setAttribute('bgborder', c);
+  }
+  get bgborder() {
+    return this.getAttribute('bgborder');
+  }
+  set fg(c) {
+    this.setAttribute('fg', c);
+  }
+  get fg() {
+    return this.getAttribute('fg');
+  }
+  set width(w) {
+    this.setAttribute('width', w);
+  }
+  get width() {
+    return this.getAttribute('width');
+  }
+  set height(w) {
+    this.setAttribute('height', w);
+  }
+  get height() {
+    return this.getAttribute('height');
+  }
+  set duration(s) {
+    this.attributeChangedCallback('duration' , this.duration, s);
+  }
+  get duration() {
+    return this._duration.toString();
+  }
+  set value(s) {
+    this.attributeChangedCallback('value' , this.value, s);
+  }
   get value() {
     if (!this._start) return this._duration.toString();
-    var elapsedMs = new Date() - this._start;
+    let elapsedMs = new Date() - this._start;
     return Math.max(0, this._duration - (elapsedMs / 1000)).toString();
   }
-
-  // The elapsed time.
-  set elapsed(s) { this.attributeChangedCallback("elapsed" , this.elapsed, s); }
+  set elapsed(s) {
+    this.attributeChangedCallback('elapsed' , this.elapsed, s);
+  }
   get elapsed() {
     if (!this._start) return '0';
     return ((new Date() - this._start) / 1000).toString();
   }
+  set toward(t) {
+    this.setAttribute('toward', t);
+  }
+  get toward() {
+    return this.getAttribute('toward');
+  }
+  set style(s) {
+    this.setAttribute('style', s);
+  }
+  get style() {
+    return this.getAttribute('style');
+  }
+  set hideafter(h) {
+    this.setAttribute('hideafter', h);
+  }
+  get hideafter() {
+    return this.getAttribute('hideafter');
+  }
+  set lefttext(p) {
+    this.setAttribute('lefttext', p);
+  }
+  get lefttext() {
+    return this.getAttribute('lefttext');
+  }
+  set righttext(p) {
+    this.setAttribute('righttext', p);
+  }
+  get righttext() {
+    return this.getAttribute('righttext');
+  }
+  set centertext(p) {
+    this.setAttribute('centertext', p);
+  }
+  get centertext() {
+    return this.getAttribute('centertext');
+  }
 
-  // If "right" then animates left-to-right (the default). If "left"
-  // then animates right-to-left.
-  set toward(t) { this.setAttribute("toward", t); }
-  get toward() { return this.getAttribute("toward"); }
-
-  // If "fill" then the progress goes empty-to-full, if "empty" then the
-  // progress bar starts full and goes to empty.
-  set style(s) { this.setAttribute("style", s); }
-  get style() { return this.getAttribute("style"); }
-  
-  // When the bar reaches 0, it is hidden after this many seconds. If ""
-  // then it is not hidden.
-  set hideafter(h) { this.setAttribute("hideafter", h); }
-  get hideafter() { return this.getAttribute("hideafter"); }
-
-  // Chooses what should be shown in the text field in each area of
-  // the bar. Can be one of:
-  // empty - nothing is shown.
-  // "remain" - shows the remaining time.
-  // "duration" - shows the remaining and total duration time
-  //              of the bar.
-  // "percent" - shows the percentage of remaining time to
-  //             the duration.
-  // "elapsed"   - shows the elapsed time
-  // anything else - the given text is shown literally.
-  set lefttext(p) { this.setAttribute("lefttext", p); }
-  get lefttext() { return this.getAttribute("lefttext"); }
-  set righttext(p) { this.setAttribute("righttext", p); }
-  get righttext() { return this.getAttribute("righttext"); }
-  set centertext(p) { this.setAttribute("centertext", p); }
-  get centertext() { return this.getAttribute("centertext"); }
-  
-  // This would be used with window.customElements.
   constructor() {
     super();
-    var root = this.attachShadow({mode: 'open'});
+    let root = this.attachShadow({ mode: 'open' });
     this.init(root);
   }
-  
-  // These would be used by document.registerElement, which is deprecated but
-  // ACT uses an old CEF which has this instead of the newer APIs.
+
   createdCallback() {
-    var root = this.createShadowRoot();
+    let root = this.createShadowRoot();
     this.init(root);
   }
-  // Convert from the deprecated API names to the modern API names.
-  attachedCallback() { this.connectedCallback(); }
-  detachedCallback() { this.disconnectedCallback(); }
+  attachedCallback() {
+    this.connectedCallback();
+  }
+  detachedCallback() {
+    this.disconnectedCallback();
+  }
 
   init(root) {
-    
     this._duration = 0;
     this._width = '250px';
     this._height = '5px';
     this._toward_right = true;
     this._style_fill = false;
-    this._left_text = "";
-    this._center_text = "";
-    this._right_text = "";
+    this._left_text = '';
+    this._center_text = '';
+    this._right_text = '';
     this._hideafter = -1;
-    
-    this._bgborder = "#AA6E03";
-    this._bg = "#312008";
- 
-  //  this._bg = 'rgba(49, 32, 8, 1)';
-  //  this._bgborder = 'rgba(170, 110, 3, 1)';
+    this._bgborder = '#AA6E03';
+    this._bg = '#312008';
 
     root.innerHTML = `
       <style>
@@ -174,16 +187,16 @@ class TimerBar extends HTMLElement {
         <div class="text-container"><div id="centertext" class="text"></div></div>
         <div class="text-container"><div id="lefttext" class="text"></div></div>
       </div>
-    `
+    `;
   }
-  
+
   connectedCallback() {
-    this.rootElement = this.shadowRoot.getElementById("root");
-    this.foregroundElement = this.shadowRoot.getElementById("fg");
-    this.backgroundElement = this.shadowRoot.getElementById("bg");
-    this.leftTextElement = this.shadowRoot.getElementById("lefttext");
-    this.centerTextElement = this.shadowRoot.getElementById("centertext");
-    this.rightTextElement = this.shadowRoot.getElementById("righttext");
+    this.rootElement = this.shadowRoot.getElementById('root');
+    this.foregroundElement = this.shadowRoot.getElementById('fg');
+    this.backgroundElement = this.shadowRoot.getElementById('bg');
+    this.leftTextElement = this.shadowRoot.getElementById('lefttext');
+    this.centerTextElement = this.shadowRoot.getElementById('centertext');
+    this.rightTextElement = this.shadowRoot.getElementById('righttext');
 
     this._connected = true;
     this.layout();
@@ -193,56 +206,56 @@ class TimerBar extends HTMLElement {
     else
       this.advance();
   }
-  
+
   disconnectedCallback() {
     this._connected = false;
   }
-  
+
   attributeChangedCallback(name, oldValue, newValue) {
-    if (name == "duration") {
+    if (name == 'duration') {
       this._duration = Math.max(parseFloat(newValue), 0);
       this.setvalue(this._duration);
-    } else if (name == "value") {
+    } else if (name == 'value') {
       this.setvalue(Math.max(parseFloat(newValue), 0));
-    } else if (name == "elapsed") {
+    } else if (name == 'elapsed') {
       this.setvalue(this._duration - Math.max(parseFloat(newValue), 0));
-    } else if (name == "width") {
+    } else if (name == 'width') {
       this._width = newValue;
       this.layout();
-    } else if (name == "height") {
+    } else if (name == 'height') {
       this._height = newValue;
       this.layout();
-    } else if (name == "bg") {
+    } else if (name == 'bg') {
       this._bg = newValue;
       this.layout();
-    } else if (name == "bgborder") {
+    } else if (name == 'bgborder') {
       this._bgborder = newValue;
       this.layout();
-    } else if (name == "fg") {
+    } else if (name == 'fg') {
       this._fg = newValue;
       this.layout();
-    } else if (name == "style") {
-      this._style_fill = newValue == "fill";
+    } else if (name == 'style') {
+      this._style_fill = newValue == 'fill';
       this.layout();
-    } else if (name == "toward") {
-      this._toward_right = newValue == "right";
+    } else if (name == 'toward') {
+      this._toward_right = newValue == 'right';
       this.layout();
-    } else if (name == "lefttext") {
-      var update = newValue != this._left_text && this._connected;
+    } else if (name == 'lefttext') {
+      let update = newValue != this._left_text && this._connected;
       this._left_text = newValue;
       if (update)
         this.updateText();
-    } else if (name == "centertext") {
-      var update = newValue != this._center_text && this._connected;
+    } else if (name == 'centertext') {
+      let update = newValue != this._center_text && this._connected;
       this._center_text = newValue;
       if (update)
         this.updateText();
-    } else if (name == "righttext") {
-      var update = newValue != this._right_text && this._connected;
+    } else if (name == 'righttext') {
+      let update = newValue != this._right_text && this._connected;
       this._right_text = newValue;
       if (update)
         this.updateText();
-    } else if (name == "hideafter") {
+    } else if (name == 'hideafter') {
       this._hideafter = Math.max(parseFloat(this.hideafter), 0);
       if (this.value == '0') {
         if (this._hideafter >= 0)
@@ -255,7 +268,7 @@ class TimerBar extends HTMLElement {
     if (this._connected)
       this.draw();
   }
-  
+
   layout() {
     if (!this._connected)
       return;
@@ -272,71 +285,69 @@ class TimerBar extends HTMLElement {
     // To start full and animate to empty, we animate backwards and flip
     // the direction.
     if (this._toward_right ^ this._style_fill)
-      this.foregroundElement.style.transformOrigin = "100% 0%";
+      this.foregroundElement.style.transformOrigin = '100% 0%';
     else
-      this.foregroundElement.style.transformOrigin = "0% 0%";
+      this.foregroundElement.style.transformOrigin = '0% 0%';
   }
 
   updateText() {
-    var varying_texts = ["elapsed", "duration", "percent", "remain"];
+    let varying_texts = ['elapsed', 'duration', 'percent', 'remain'];
     // These values are filled in during draw() when the values change.
     if (varying_texts.indexOf(this._left_text) == -1) {
       // Otherwise the value is fixed so it can be set here.
       this.leftTextElement.innerHTML = this._left_text;
     }
-    if (varying_texts.indexOf(this._center_text) == -1) {
+    if (varying_texts.indexOf(this._center_text) == -1)
       this.centerTextElement.innerHTML = this._center_text;
-    }
-    if (varying_texts.indexOf(this._right_text) == -1) {
+    if (varying_texts.indexOf(this._right_text) == -1)
       this.rightTextElement.innerHTML = this._right_text;
-    }
   }
 
   draw() {
-    var elapsedSec = (new Date() - this._start) / 1000;
-    var remainSec = Math.max(0, this._duration - elapsedSec);
-    var percent = this._duration <= 0 ? 0 : remainSec / this._duration;
+    let elapsedSec = (new Date() - this._start) / 1000;
+    let remainSec = Math.max(0, this._duration - elapsedSec);
+    let percent = this._duration <= 0 ? 0 : remainSec / this._duration;
     // Keep it between 0 and 1.
     percent = Math.min(1, Math.max(0, percent));
-    var display_remain = remainSec ? remainSec.toFixed(1) : ''; //parseInt(this._value + 0.99999999999);
-    var display_elapsed = elapsedSec.toFixed(1);
+    let display_remain = remainSec ? remainSec.toFixed(1) : '';
+    let display_elapsed = elapsedSec.toFixed(1);
     if (this._style_fill)
       percent = 1.0 - percent;
-    this.foregroundElement.style.transform = "scale(" + percent + ",1)";
-    if (this._left_text != "") {
-      if (this._left_text == "remain")
+    this.foregroundElement.style.transform = 'scale(' + percent + ',1)';
+    if (this._left_text != '') {
+      if (this._left_text == 'remain')
         this.leftTextElement.innerHTML = display_remain;
-      else if (this._left_text == "duration")
-        this.leftTextElement.innerHTML = display_remain + " / " + this._duration;
-      else if (this._left_text == "percent")
-        this.leftTextElement.innerHTML = (percent * 100).toFixed(1) + " %";
-      else if (this._left_text == "elapsed")
+      else if (this._left_text == 'duration')
+        this.leftTextElement.innerHTML = display_remain + ' / ' + this._duration;
+      else if (this._left_text == 'percent')
+        this.leftTextElement.innerHTML = (percent * 100).toFixed(1) + ' %';
+      else if (this._left_text == 'elapsed')
         this.leftTextElement.innerHTML = display_elapsed;
     }
-    if (this._center_text != "") {
-      if (this._center_text == "remain")
+    if (this._center_text != '') {
+      if (this._center_text == 'remain')
         this.centerTextElement.innerHTML = display_remain;
-      else if (this._center_text == "duration")
+      else if (this._center_text == 'duration')
         this.centerTextElement.innerHTML = display_remain + " / " + this._duration;
-      else if (this._center_text == "percent")
-        this.centerTextElement.innerHTML = (percent * 100).toFixed(1) + " %";
-      else if (this._center_text == "elapsed")
+      else if (this._center_text == 'percent')
+        this.centerTextElement.innerHTML = (percent * 100).toFixed(1) + ' %';
+      else if (this._center_text == 'elapsed')
         this.centerTextElement.innerHTML = display_elapsed;
     }
-    if (this._right_text != "") {
-      if (this._right_text == "remain")
+    if (this._right_text != '') {
+      if (this._right_text == 'remain')
         this.rightTextElement.innerHTML = display_remain;
-      else if (this._right_text == "duration")
-        this.rightTextElement.innerHTML = display_remain + " / " + this._duration;
-      else if (this._right_text == "percent")
-        this.rightTextElement.innerHTML = (percent * 100).toFixed(1) + " %";
-      else if (this._right_text == "elapsed")
+      else if (this._right_text == 'duration')
+        this.rightTextElement.innerHTML = display_remain + ' / ' + this._duration;
+      else if (this._right_text == 'percent')
+        this.rightTextElement.innerHTML = (percent * 100).toFixed(1) + ' %';
+      else if (this._right_text == 'elapsed')
         this.rightTextElement.innerHTML = display_elapsed;
     }
   }
 
   setvalue(remainSec) {
-    var elapsedSec = Math.max(0, this._duration - remainSec);
+    let elapsedSec = Math.max(0, this._duration - remainSec);
     this._start = new Date() - (elapsedSec * 1000);
 
     if (!this._connected) return;
@@ -347,7 +358,7 @@ class TimerBar extends HTMLElement {
 
     this.advance();
   }
-  
+
   advance() {
     var elapsedSec = (new Date() - this._start) / 1000;
     if (elapsedSec >= this._duration) {
@@ -369,20 +380,19 @@ class TimerBar extends HTMLElement {
 
   show() {
     if (this._connected)
-      this.rootElement.style.display = "block";
+      this.rootElement.style.display = 'block';
   }
 
   hide() {
     if (this._connected)
-      this.rootElement.style.display = "none";
+      this.rootElement.style.display = 'none';
   }
 }
 
 if (window.customElements) {
-  // Preferred method but old CEF doesn't have this.
   window.customElements.define('timer-bar', TimerBar);
 } else {
   document.registerElement('timer-bar', {
-    prototype: Object.create(TimerBar.prototype)
+    prototype: Object.create(TimerBar.prototype),
   });
 }
