4ca2b80ae7af1180f279c1146f697a0e80e39773
Adrienne Walker <enne@quisquo.us>
2019-03-30 19:40:52 -0700

    raidboss: add yojimbo triggers and timeline

---
+++ ui/raidboss/data/triggers/yojimbo.js
@@ -0,0 +1,50 @@
+'use strict';
+
+[{
+  zoneRegex: /Kugane Ohashi/,
+  timelineFile: 'yojimbo.txt',
+  triggers: [
+    {
+      id: 'Yojimbo Giga Jump',
+      regex: / 1B:\y{ObjectId}:(\y{Name}):....:....:0057:/,
+      condition: function(data, matches) {
+        return matches[1] == data.me;
+      },
+      alertText: {
+        en: 'Run Marker Away',
+      },
+    },
+    {
+      id: 'Yojimbo Dorito',
+      regex: / 1B:\y{ObjectId}:(\y{Name}):....:....:0037:/,
+      condition: function(data, matches) {
+        return matches[1] == data.me;
+      },
+      alertText: {
+        en: 'Dorito Stack',
+        de: 'Stacken',
+        fr: 'Packez-vous',
+      },
+    },
+    {
+      id: 'Yojimbo Gekko',
+      regex: / 1B:\y{ObjectId}:(\y{Name}):....:....:0090:/,
+      condition: function(data, matches) {
+        return matches[1] == data.me;
+      },
+      infoText: {
+        en: 'Gekko Marker',
+      },
+    },
+    {
+      id: 'Yojimbo Enchain',
+      regex: / 1B:\y{ObjectId}:(\y{Name}):....:....:0005:/,
+      condition: function(data, matches) {
+        return matches[1] == data.me;
+      },
+      alarmText: {
+        en: 'GTFO',
+      },
+    },
+  ],
+}];
