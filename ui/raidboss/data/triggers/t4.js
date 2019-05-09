2190b7083672503e1e8cd84133efc8bc85470cc6
Adrienne Walker <enne@quisquo.us>
2019-05-08 23:04:50 -0700

    raidboss: t4 triggers

---
+++ ui/raidboss/data/triggers/t4.js
@@ -0,0 +1,27 @@
+'use strict';
+
+[{
+  zoneRegex: /The Binding Coil Of Bahamut - Turn \(4\)/,
+  triggers: [
+    {
+      id: 'T4 Gravity Thrust',
+      regex: / 14:4D4:Spinner-Rook starts using Gravity Thrust on (\y{Name})\./,
+      condition: function(data, matches) {
+        return data.me == matches[1];
+      },
+      infoText: {
+        en: 'LOS Thrust',
+      },
+    },
+    {
+      id: 'T4 Pox',
+      regex: / 14:4D5:Spinner-Rook starts using Pox on (\y{Name})\./,
+      condition: function(data, matches) {
+        return data.me == matches[1];
+      },
+      alertText: {
+        en: 'LOS Pox',
+      },
+    },
+  ],
+}];
