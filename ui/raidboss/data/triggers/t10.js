0e6e558b53cd0b0154ab999adff870e8df3fe9d6
Adrienne Walker <enne@quisquo.us>
2019-05-11 12:57:45 -0700

    raidboss: t10 triggers

--- ui/raidboss/data/triggers/t10.js
+++ ui/raidboss/data/triggers/t10.js
@@ -3,5 +3,53 @@
 [{
   zoneRegex: /The Final Coil Of Bahamut - Turn \(1\)/,
   triggers: [
+    {
+      id: 'T10 Phase Change',
+      regex: / 14:B5D:Imdugud starts using Electrocharge/,
+      sound: 'Long',
+    },
+    {
+      id: 'T10 Heat Lightning',
+      regex: / 14:B5F:Imdugud starts using Heat Lightning/,
+      alertText: {
+        en: 'Spread',
+      },
+    },
+    {
+      id: 'T10 Wild Charge',
+      regex: / 1B:\y{ObjectId}:(\y{Name}):....:....:001F:/,
+      alarmText: function(data, matches) {
+        if (data.me == matches[1]) {
+          return {
+            en: 'Charge on YOU',
+          };
+        }
+      },
+      infoText: function(data, matches) {
+        if (data.me != matches[1]) {
+          return {
+            en: 'Charge on ' + data.ShortName(matches[1]),
+          };
+        }
+      },
+    },
+    {
+      id: 'T10 Prey',
+      regex: / 1B:\y{ObjectId}:(\y{Name}):....:....:001E:/,
+      alertText: function(data, matches) {
+        if (data.me == matches[1]) {
+          return {
+            en: 'Prey on YOU',
+          };
+        }
+      },
+      infoText: function(data, matches) {
+        if (data.me != matches[1]) {
+          return {
+            en: 'Prey on ' + data.ShortName(matches[1]),
+          };
+        }
+      },
+    },
   ],
 }];
