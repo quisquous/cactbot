ae7b8278c69a9c96a27b2a1e1e0498f8ee91f1ad
Adrienne Walker <enne@quisquo.us>
2019-05-20 23:21:33 -0700

    raidboss: more t5 triggers

--- ui/raidboss/data/triggers/t5.js
+++ ui/raidboss/data/triggers/t5.js
@@ -27,6 +27,18 @@
       },
     },
     {
+      id: 'T5 Death Sentence Warning',
+      regex: / 14:5B2:Twintania starts using Death Sentence/,
+      condition: function(data) {
+        return data.role == 'tank' || data.role == 'healer' || data.job == 'BLU';
+      },
+      delaySeconds: 30,
+      suppressSeconds: 5,
+      infoText: {
+        en: 'Death Sentence Soon',
+      },
+    },
+    {
       id: 'T5 Liquid Hell',
       regex: / 14:4DB:The Scourge Of Meracydia starts using Liquid Hell/,
       infoText: {
@@ -38,8 +50,42 @@
       regex: /:Twintania HP at 85%/,
       sound: 'Long',
     },
-    // TODO: fireball
-    // TODO: conflag
+    {
+      id: 'T5 Fireball',
+      regex: / 15:\y{ObjectId}:Twintania:5AC:Fireball:\y{ObjectId}:(\y{Name}):/,
+      alertText: function(data, matches) {
+        if (data.me == matches[1]) {
+          return {
+            en: 'Fireball on YOU',
+          };
+        }
+      },
+      infoText: function(data, matches) {
+        if (data.me != matches[1]) {
+          return {
+            en: 'Fireball on ' + data.ShortName(matches[1]),
+          };
+        }
+      },
+    },
+    {
+      id: 'T5 Conflag',
+      regex: / 15:\y{ObjectId}:Twintania:5AB:Firestorm:\y{ObjectId}:(\y{Name}):/,
+      alarmText: function(data, matches) {
+        if (data.me == matches[1]) {
+          return {
+            en: 'Conflag on YOU',
+          };
+        }
+      },
+      infoText: function(data, matches) {
+        if (data.me != matches[1]) {
+          return {
+            en: 'Conflag on ' + data.ShortName(matches[1]),
+          };
+        }
+      },
+    },
     {
       id: 'T5 Phase 3',
       regex: /:Twintania HP at 55%/,
@@ -57,7 +103,7 @@
       regex: / 15:\y{ObjectId}:Twintania:5B0:Divebomb:/,
       delaySeconds: 60,
       suppressSeconds: 5000,
-      alertText: {
+      infoText: {
         en: 'Divebombs Soon',
       },
     },
