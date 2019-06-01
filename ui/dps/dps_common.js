0858366b177b60c813df99201483985b2332eab1
Adrienne Walker <enne@quisquo.us>
2019-05-31 21:31:51 -0700

    dps: don't show when not a combat job
    Fixes #409.

--- ui/dps/dps_common.js
+++ ui/dps/dps_common.js
@@ -13,25 +13,45 @@ let Options = {
   ],
 };
 
+let gIgnoreCurrentZone = false;
+let gIgnoreCurrentJob = false;
+let gCurrentJob = null;
 let gCurrentZone = null;
-let gIgnoreDps = false;
 let gIgnoreZones = [];
 
 function InitDpsModule(config, updateFunc, hideFunc) {
   document.addEventListener('onOverlayDataUpdate', function(e) {
     // DPS numbers in large pvp is not useful and hella noisy.
-    if (gIgnoreDps)
+    if (gIgnoreCurrentZone || gIgnoreCurrentJob)
       return;
     updateFunc(e);
   });
 
   document.addEventListener('onZoneChangedEvent', function(e) {
-    gCurrentZone = e.detail.zoneName;
-    gIgnoreDps = false;
+    let newZone = e.detail.zoneName;
+    if (gCurrentZone == newZone)
+      return;
+    gCurrentZone = newZone;
+    gIgnoreCurrentZone = false;
     for (let i = 0; i < gIgnoreZones.length; ++i) {
-      if (gCurrentZone.match(gIgnoreZones[i]))
-        gIgnoreDps = true;
+      if (gCurrentZone.match(gIgnoreZones[i])) {
+        gIgnoreCurrentZone = true;
+        hideFunc();
+        return;
+      }
     }
+  });
+
+  document.addEventListener('onPlayerChangedEvent', function(e) {
+    let job = e.detail.job;
+    if (job == gCurrentJob)
+      return;
+    gCurrentJob = job;
+    if (kCraftingJobs.indexOf(job) < 0 && kGatheringJobs.indexOf(job) < 0) {
+      gIgnoreCurrentJob = false;
+      return;
+    }
+    gIgnoreCurrentJob = true;
     hideFunc();
   });
 
