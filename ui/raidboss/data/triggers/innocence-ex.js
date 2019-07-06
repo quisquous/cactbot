426b9ca2273c899a7ce34bdc7535af59fa505e9e
Adrienne Walker <enne@quisquo.us>
2019-07-06 12:16:20 -0700

    raidboss: innocence extreme triggers

--- ui/raidboss/data/triggers/innocence-ex.js
+++ ui/raidboss/data/triggers/innocence-ex.js
@@ -2,73 +2,190 @@
 
 // Innocence Extreme
 [{
-  zoneRegex: /.*/,
+  zoneRegex: /The Crown Of The Immaculate \(Extreme\)/,
   timelineFile: 'innocence-ex.txt',
   triggers: [
-    /* {
-      id: 'InnoEx 2',
-      regex: /14:40BD:Innocence starts using Winged Reprobation on Innocence/,
-      alertText: 'Winged?',
-    },*/
     {
-      id: 'InnoEx 3',
+      id: 'InnoEx Starbirth Start',
+      regex: /14:3EEF:Innocence starts using Starbirth/,
+      run: function(data) {
+        data.starbirthCount = data.starbirthCount || 0;
+        data.starbirthCount++;
+        data.starbirthActive = true;
+      },
+    },
+    {
+      id: 'InnoEx Righteous Bolt',
       regex: /14:3ECD:Innocence starts using Righteous Bolt on (\y{Name})/,
-      alertText: function(data, matches) {
+      alarmText: function(data, matches) {
+        if (matches[1] == data.me || data.role != 'tank')
+          return;
+
         return {
-          en: 'Buster on ' + data.ShortName(matches[1]),
-          de: 'Tankbuster auf ' + data.ShortName(matches[1]),
-          fr: 'Tankbuster sur ' + data.ShortName(matches[1]),
+          en: 'Tank Swap!',
+          de: 'Tankwechsel!',
+          fr: 'Tank swap !',
         };
       },
-    },
-    {
-      id: 'InnoEx 4',
-      regex: /14:3EEA:Innocence starts using Shadowreaver on Innocence/,
-      alertText: 'AoE',
-    },
-    {
-      id: 'InnoEx 5',
-      regex: /14:3EEE:Innocence starts using Beatific Vision on Innocence/,
-      alertText: 'CHARGE!',
-    },
-    {
-      id: 'InnoEx 6',
-      regex: /14:3EEF:Innocence starts using Starbirth on Innocence/,
-      alertText: 'Star Orbs',
-      run: function(data, matches) {
-        // TODO: Track Star Orbs, is it always 3 -> 2 -> 4?
+      alertText: function(data, matches) {
+        if (matches[1] == data.me) {
+          return {
+            en: 'Tank Buster on YOU',
+            de: 'Tankbuster auf DIR',
+            fr: 'Tankbuster sur VOUS',
+          };
+        }
+        if (data.role == 'healer') {
+          return {
+            en: 'Buster on ' + data.ShortName(matches[1]),
+            de: 'Tankbuster auf ' + data.ShortName(matches[1]),
+            fr: 'Tankbuster sur ' + data.ShortName(matches[1]),
+          };
+        }
       },
     },
     {
-      id: 'InnoEx 7',
-      regex: /14:3F3E:Innocence starts using Light Pillar on Innocence/,
-      alertText: 'Stackmarker',
+      id: 'InnoEx Holy Sword Healer',
+      regex: /14:3EC9:Forgiven Venery starts using Holy Sword/,
+      condition: function(data) {
+        return data.role == 'healer';
+      },
+      suppressSeconds: 5,
+      infoText: {
+        en: 'Tank Busters',
+      },
     },
     {
-      id: 'InnoEx 8',
-      regex: /1B:........:(\y{Name}):....:....:00AC:0000:0000:0000:/,
+      id: 'InnoEx Holy Sword Me',
+      regex: /14:3EC9:Forgiven Venery starts using Holy Sword on (\y{Name})/,
       condition: function(data, matches) {
         return matches[1] == data.me;
       },
-      alertText: 'Spear on YOU',
+      alertText: {
+        en: 'Tank Buster on YOU',
+        de: 'Tankbuster auf DIR',
+        fr: 'Tankbuster sur VOUS',
+      },
     },
     {
-      id: 'InnoEx 9',
-      regex: /14:3EE4:Innocence starts using God Ray on Innocence/,
-      alertText: 'Swords then Nuclear Ground',
-    },
-    /* {
-      id: 'InnoEx 9',
-      regex: /16:(\y{ObjectId}):Innocence:3EDB:Holy Trinity:(\y{ObjectId}):(\y{Name})/,
-      alertText: function(data, matches) {
+      id: 'InnoEx Charge',
+      regex: /14:3EEE:Innocence starts using Beatific Vision/,
+      alertText: function(data) {
+        if (data.starbirthActive) {
+          return {
+            en: 'Avoid Charge and Orbs',
+          };
+        }
         return {
-            en: 'Trinity on ' + data.ShortName(matches[1]),
-            de: 'Tankbuster auf ' + data.ShortName(matches[1]),
-            fr: 'Tankbuster sur ' + data.ShortName(matches[1]),
+          en: 'Avoid Charge',
         };
-      }
-    },*/
-  ],
-  timelineReplace: [
+      },
+    },
+    {
+      id: 'InnoEx Starbirth Avoid',
+      regex: /14:3EEF:Innocence starts using Starbirth/,
+      delaySeconds: 3,
+      condition: function(data) {
+        return data.starbirthCount == 1;
+      },
+      alertText: {
+        en: 'Get to Safe Corner',
+      },
+    },
+    {
+      id: 'InnoEx Starbirth Explode',
+      regex: /14:3F3E:Innocence starts using Light Pillar/,
+      delaySeconds: 6.5,
+      alertText: {
+        en: 'Get to Safe Corner',
+      },
+    },
+    {
+      id: 'InnoEx Adds',
+      regex: /15:\y{ObjectId}:Innocence:42B0:/,
+      condition: function(data) {
+        return data.role == 'tank';
+      },
+      infoText: {
+        en: 'Grab East/West Venery Adds',
+      },
+    },
+    {
+      id: 'InnoEx Light Pillar',
+      regex: /15:\y{ObjectId}:Innocence:38FC:[^:]*:\y{ObjectId}:(\y{Name}):/,
+      preRun: function(data) {
+        data.lightPillar = data.lightPillar || 0;
+        data.lightPillar++;
+      },
+      alarmText: function(data, matches) {
+        if (matches[1] != data.me)
+          return;
+
+        if (data.lightPillar == 3) {
+          return {
+            en: 'Aim Line At Back Orb',
+          };
+        }
+        return {
+          en: 'Avoid Orbs With Line',
+        };
+      },
+      alertText: function(data, matches) {
+        if (matches[1] == data.me)
+          return;
+        return {
+          en: 'Line Stack',
+        };
+      },
+    },
+    {
+      id: 'InnoEx Winged Reprobation Tether',
+      regex: /1B:\y{ObjectId}:(\y{Name}):....:....:00AC:0000:0000:0000:/,
+      condition: function(data, matches) {
+        return matches[1] == data.me;
+      },
+      alertText: {
+        en: 'Tether on YOU',
+      },
+    },
+    {
+      id: 'InnoEx Winged Drop Of Light',
+      regex: /1B:\y{ObjectId}:(\y{Name}):....:....:008A:0000:0000:0000:/,
+      condition: function(data, matches) {
+        return matches[1] == data.me;
+      },
+      alertText: function(data, matches) {
+        if (data.starbirthActive) {
+          return {
+            en: 'Circle, Avoid Orbs',
+          };
+        }
+        return {
+          en: 'Circle on YOU',
+        };
+      },
+    },
+    {
+      id: 'InnoEx God Ray',
+      regex: /14:3EE[456]:Innocence starts using God Ray/,
+      suppressSeconds: 15,
+      infoText: {
+        en: 'Avoid Swords then Ray',
+      },
+    },
+    {
+      id: 'InnoEx Starbirth End 1',
+      regex: /14:3EEA:Innocence starts using Shadowreaver/,
+      run: function(data) {
+        delete data.starbirthActive;
+      },
+    },
+    {
+      id: 'InnoEx Starbirth End 2',
+      regex: /14:3EEE:Innocence starts using Beatific Vision/,
+      run: function(data) {
+        delete data.starbirthActive;
+      },
+    },
   ],
 }];
