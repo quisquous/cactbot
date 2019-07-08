f94c374ff91eadf959fcd49f7c65cb6b52f1db6b
Akurosia <kamothegamer@web.de>
2019-07-08 03:04:40 +0200

    added translations for innocence and titania

--- ui/raidboss/data/triggers/innocence-ex.js
+++ ui/raidboss/data/triggers/innocence-ex.js
@@ -191,4 +191,178 @@
       },
     },
   ],
+  'timelineReplace': [
+    {
+      'locale': 'de',
+      'replaceSync': {
+        'Schwert des Urteils': '',
+        'Innocence': 'Innozenz',
+        'Geläuterte Wollust': '',
+        'Geläuterte Schande': '',
+        'Engage!': 'Start!'
+      },
+      'replaceText': {
+        '断罪': '断罪',
+        'attack': 'Attacke',
+        'Winged Reprobation': 'Schwinge des Urteils',
+        'Unknown Ability': 'Unknown Ability',
+        'Starbirth': 'Sternengeburt',
+        'Soul and Body': 'Seele und Körper',
+        'Shadowreaver': 'Schattenplünderer',
+        'Scold\'s Bridle': 'Schandmal',
+        'Rightful Reprobation': 'Rechtmäßige Verurteilung',
+        'Righteous Bolt': 'Blitz der Gerechtigkeit',
+        'Reprobation': 'Verurteilung',
+        'Light Pillar': 'Lichtsäule',
+        'Holy Trinity': 'Heilige Dreifaltigkeit',
+        'Holy Sword': 'Heiliges Schwert',
+        'Guiding Light': 'Leitendes Licht',
+        'God Ray': 'Göttlicher Strahl',
+        'Flaming Sword': 'Flammenschwert',
+        'Explosion': 'Explosion',
+        'Enrage': 'Finalangriff',
+        'Duel Descent': 'Doppelter Sinkflug',
+        'Drop of Light': 'Lichtabfall',
+        'Dream of the Rood': 'Traum des Kreuzes',
+        'Beatific Vision': 'Seligmachende Schau',
+        '--untargetable--': '--nich anvisierbar--',
+        '--targetable--': '--anvisierbar--',
+        '': ''
+      },
+      '~effectNames': {
+        'Physical Vulnerability Up': 'Erhöhte physische Verwundbarkeit',
+        'Lightning Resistance Down II': 'Blitzresistenz - (stark)',
+        'Embolden': 'Ermutigen',
+        'Damage Down': 'Schaden -'
+      }
+    },
+    {
+      'locale': 'fr',
+      'replaceSync': {
+        'Schwert des Urteils': '',
+        'Innocence': 'Innocence',
+        'Geläuterte Wollust': '',
+        'Geläuterte Schande': '',
+        'Engage!': 'À l\'attaque'
+      },
+      'replaceText': {
+        '断罪': '断罪',
+        'attack': 'Attaque',
+        'Winged Reprobation': 'Réprobation ailée',
+        'Unknown Ability': 'Unknown Ability',
+        'Starbirth': 'Accouchement stellaire',
+        'Soul and Body': 'Âme et corps',
+        'Shadowreaver': 'Pilleur d\'ombre',
+        'Scold\'s Bridle': 'Bride-bavarde',
+        'Rightful Reprobation': 'Réprobation légitime',
+        'Righteous Bolt': 'Éclair vertueux',
+        'Reprobation': 'Réprobation',
+        'Light Pillar': 'Pilier de lumière',
+        'Holy Trinity': 'Sainte Trinité',
+        'Holy Sword': 'Épée sacrée',
+        'Guiding Light': 'Lumière directrice',
+        'God Ray': 'Rayon divin',
+        'Flaming Sword': 'Épée du feu des cieux',
+        'Explosion': 'Explosion',
+        'Enrage': 'Enrage',
+        'Duel Descent': 'Double plongeon',
+        'Drop of Light': 'Goutte de lumière',
+        'Dream of the Rood': 'Le Rêve de la Croix',
+        'Beatific Vision': 'Vision béatifique',
+        '--untargetable--': '--Impossible à cibler--',
+        '--targetable--': '--Ciblable--',
+        '--sync--': '--Synchronisation--',
+        '--Reset--': '--Réinitialisation--',
+        '': ''
+      },
+      '~effectNames': {
+        'Physical Vulnerability Up': 'Vulnérabilité physique augmentée',
+        'Lightning Resistance Down II': 'Résistance à La Foudre Réduite+',
+        'Embolden': 'Enhardissement',
+        'Damage Down': 'Malus de dégâts'
+      }
+    },
+    {
+      'locale': 'ja',
+      'replaceSync': {
+        'Schwert des Urteils': '',
+        'Innocence': 'イノセンス',
+        'Geläuterte Wollust': '',
+        'Geläuterte Schande': '',
+        'Engage!': '戦闘開始！'
+      },
+      'replaceText': {
+        '断罪': '断罪',
+        'attack': '攻撃',
+        'Winged Reprobation': '断罪の飛翔',
+        'Unknown Ability': 'Unknown Ability',
+        'Starbirth': 'スターバース',
+        'Soul and Body': 'ソウル・アンド・ボディー',
+        'Shadowreaver': 'シャドウリーヴァー',
+        'Scold\'s Bridle': 'スコルドブライダル',
+        'Rightful Reprobation': '断罪の旋回',
+        'Righteous Bolt': 'ジャッジボルト',
+        'Reprobation': '断罪',
+        'Light Pillar': 'ライトピラー',
+        'Holy Trinity': 'ホーリートリニティー',
+        'Holy Sword': 'ホーリーソード',
+        'Guiding Light': 'ガイディングライト',
+        'God Ray': 'ゴッドレイ',
+        'Flaming Sword': '回転せし炎の剣',
+        'Explosion': '爆散',
+        'Duel Descent': 'デュアルディセント',
+        'Drop of Light': 'ドロップ・オブ・ライト',
+        'Dream of the Rood': 'ドリーム・オブ・ザ・ルード',
+        'Beatific Vision': 'ビーティフィックビジョン',
+        '': ''
+      },
+      '~effectNames': {
+        'Physical Vulnerability Up': '被物理ダメージ増加',
+        'Lightning Resistance Down II': '雷属性耐性低下［強］',
+        'Embolden': 'エンボルデン',
+        'Damage Down': 'ダメージ低下'
+      }
+    },
+    {
+      'locale': 'cn',
+      'replaceSync': {
+        'Schwert des Urteils': '',
+        'Innocence': '',
+        'Geläuterte Wollust': '',
+        'Geläuterte Schande': '',
+        'Engage!': '战斗开始！'
+      },
+      'replaceText': {
+        '断罪': '',
+        'attack': '',
+        'Winged Reprobation': '',
+        'Unknown Ability': 'Unknown Ability',
+        'Starbirth': '',
+        'Soul and Body': '',
+        'Shadowreaver': '',
+        'Scold\'s Bridle': '',
+        'Rightful Reprobation': '',
+        'Righteous Bolt': '',
+        'Reprobation': '',
+        'Light Pillar': '',
+        'Holy Trinity': '',
+        'Holy Sword': '',
+        'Guiding Light': '',
+        'God Ray': '',
+        'Flaming Sword': '',
+        'Explosion': '',
+        'Duel Descent': '',
+        'Drop of Light': '',
+        'Dream of the Rood': '',
+        'Beatific Vision': '',
+        '': ''
+      },
+      '~effectNames': {
+        'Physical Vulnerability Up': '',
+        'Lightning Resistance Down II': '雷属性耐性大幅降低',
+        'Embolden': '鼓励',
+        'Damage Down': ''
+      }
+    }
+  ],
 }];
