92f5b7406248baafc93adea32da7a7b6553a982b
r4ha <index@r4ha.me>
2019-08-01 03:35:19 +0900

    e3s ja

--- ui/raidboss/data/triggers/e3s.js
+++ ui/raidboss/data/triggers/e3s.js
@@ -8,11 +8,22 @@
       id: 'E3S Tidal Wave Look',
       regex: / 14:3FF1:Leviathan starts using Tidal Wave/,
       regexFr: / 14:3FF1:Léviathan starts using Raz-De-Marée/,
+      regexJa: / 14:3FF1:リヴァイアサン starts using タイダルウェーブ/,
       delaySeconds: 3,
       infoText: {
         en: 'Look for Wave',
         fr: 'Repérez la vague',
+        ja: 'タイダルウェーブ見て',
       },
     },
   ],
+  timelineReplace: [
+    {
+      'locale': 'ja',
+      'replaceSync': {
+        'Engage!': '戦闘開始！',
+        'Leviathan': 'リヴァイアサン',
+      },
+    }
+  ]
 }];
