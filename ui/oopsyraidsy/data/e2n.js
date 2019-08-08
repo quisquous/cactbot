630231751388ba42759d09538605edb965272d59
Adrienne Walker <enne@quisquo.us>
2019-08-07 17:09:00 -0700

    oopsy: add e1234n basic support

---
+++ ui/oopsyraidsy/data/e2n.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// TODO: shadoweye failure (top line fail, bottom line success, effect there too)
+// [16:17:35.966] 16:400110FE:Voidwalker:40B7:Shadoweye:106E29AA:Ayra Sagramore:F:10000:100190F:
+// [16:17:35.966] 16:400110FE:Voidwalker:40B7:Shadoweye:1069A42E:Nerio Albion:1:0:1C:8000:
+// gains the effect of Petrification from Voidwalker for 10.00 Seconds.
+// TODO: puddle failure?
+[{
+  zoneRegex: /^Eden's Gate: Descent$/,
+  damageWarn: {
+    'E2S Doomvoid Slicer': '3E3C',
+    'E3S Doomvoid Guillotine': '3E3B',
+  },
+  triggers: [
+    {
+      id: 'E2S Nyx',
+      damageRegex: '3E3D',
+      mistake: function(e, data) {
+        return {
+          type: 'warn',
+          blame: e.targetName,
+          text: {
+            en: 'Booped',
+            // FIXME: ability name here is "Attack", which isn't great.
+            de: e.abilityName,
+            fr: e.abilityName,
+            cn: e.abilityName,
+            ja: e.abilityName,
+          },
+        };
+      },
+    },
+  ],
+}];
