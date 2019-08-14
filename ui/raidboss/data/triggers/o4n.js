274c257ce8b0ca3b02103272a715849613a7abe2
Jonathan Garber <linkthevaliant@gmail.com>
2019-08-14 01:07:30 -0400

    Corrected instance name styling

--- ui/raidboss/data/triggers/o4n.js
+++ ui/raidboss/data/triggers/o4n.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// O4n - Deltascape 4.0 Normal
+// O4N - Deltascape 4.0 Normal
 
 [{
 
@@ -8,7 +8,7 @@
   timelineFile: 'o4n.txt',
   triggers: [
     {
-      id: 'O4n Doom',
+      id: 'O4N Doom',
       regex: / 14:24B7:Exdeath starts using Doom/,
       condition: function(data) {
         return data.CanCleanse();
@@ -18,7 +18,7 @@
       },
     },
     {
-      id: 'O4n Standard Thunder',
+      id: 'O4N Standard Thunder',
       regex: / 14:24BD:Exdeath starts using Thunder III on (\y{Name})/,
       infoText: function(data, matches) {
         if (matches[1] == data.me) {
@@ -36,7 +36,7 @@
       },
     },
     {
-      id: 'O4n Standard Fire',
+      id: 'O4N Standard Fire',
       regex: /14:24BA:Exdeath starts using Fire III on (\y{Name})/,
       alertText: function(data, matches) {
         if (data.me == matches[1]) {
@@ -47,28 +47,28 @@
       },
     },
     {
-      id: 'O4n Empowered Blizzard',
+      id: 'O4N Empowered Blizzard',
       regex: / 14:24C0:Exdeath starts using Blizzard III/,
       infoText: {
         en: 'Move around',
       },
     },
     {
-      id: 'O4n Empowered Fire',
+      id: 'O4N Empowered Fire',
       regex: / 14:24BF:Exdeath starts using Fire III/,
       infoText: {
         en: 'Stop everything',
       },
     },
     {
-      id: 'O4n Empowered Thunder',
+      id: 'O4N Empowered Thunder',
       regex: / 14:24C1:Exdeath starts using Thunder III/,
       alertText: {
         en: 'Get out',
       },
     },
     {
-      id: 'O4n Decisive Battle ',
+      id: 'O4N Decisive Battle ',
       regex: / 14:2408:Exdeath starts using The Decisive Battle/,
       delaySeconds: 6,
       infoText: {
@@ -76,7 +76,7 @@
       },
     },
     {
-      id: 'O4n Zombie Breath',
+      id: 'O4N Zombie Breath',
       regex: / 14:240A:Exdeath starts using Unknown_240A/,
       delaySeconds: 6,
       infoText: {
@@ -84,21 +84,21 @@
       },
     },
     {
-      id: 'O4n Black Hole',
+      id: 'O4N Black Hole',
       regex: / 14:24C8:Exdeath starts using Black Hole on Exdeath/,
       infoText: {
         en: 'Avoid black holes',
       },
     },
     {
-      id: 'O4n Vacuum Wave',
+      id: 'O4N Vacuum Wave',
       regex: / 14:24B8:Exdeath starts using Vacuum Wave on Exdeath/,
       alertText: {
         en: 'Knockback',
       },
     },
     {
-      id: 'O4n Flare',
+      id: 'O4N Flare',
       regex: / 1B:........:(\y{Name}):....:....:0057/,
       condition: function(data, matches) {
         return data.me == matches[1];
@@ -108,7 +108,7 @@
       },
     },
     {
-      id: 'O4n Holy',
+      id: 'O4N Holy',
       regex: / 1B:........:(\y{Name}):....:....:003E/,
       alertText: function(data, matches) {
         if (matches[1] == data.me) {
@@ -122,7 +122,7 @@
       },
     },
     {
-      id: 'O4n Meteor',
+      id: 'O4N Meteor',
       regex: / 14:24C6:Exdeath starts using Meteor/,
       condition: function(data) {
         return data.role == 'healer';
