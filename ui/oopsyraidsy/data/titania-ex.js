c7642bc8a2b4f863e2ea5a94b93fce07158c95e8
Adrienne Walker <enne@quisquo.us>
2019-08-19 23:48:22 -0700

    oopsy: titania ex

---
+++ ui/oopsyraidsy/data/titania-ex.js
@@ -0,0 +1,43 @@
+'use strict';
+
+[{
+  zoneRegex: /^The Dancing Plague \(Extreme\)$/,
+  damageWarn: {
+    'TitaniaEx Wood\'s Embrace': '3D2F',
+    // 'TitaniaEx Frost Rune': '3D2B',
+    'TitaniaEx Gentle Breeze': '3F82',
+    'TitaniaEx Leafstorm': '3D39',
+    'TitaniaEx Puck\'s Rebuke': '3D43',
+    'TitaniaEx Wallop': '3D3B',
+    'TitaniaEx Leafstorm': '3D49',
+  },
+  damageFail: {
+    'TitaniaEx Phantom Rune 1': '3D4C',
+    'TitaniaEx Phantom Rune 2': '3D4D',
+  },
+  triggers: [
+    {
+      // TODO: This could maybe blame the person with the tether?
+      id: 'TitaniaEx Thunder Rune',
+      damageRegex: '3D29',
+      condition: function(e, data) {
+        // Double taps only.
+        return e.type != '15';
+      },
+      mistake: function(e, data) {
+        return { type: 'fail', blame: e.targetName, text: e.abilityName };
+      },
+    },
+    {
+      id: 'TitaniaEx Divination Rune',
+      damageRegex: '3D4A',
+      condition: function(e, data) {
+        // Double taps only.
+        return e.type != '15';
+      },
+      mistake: function(e, data) {
+        return { type: 'fail', blame: e.targetName, text: e.abilityName };
+      },
+    },
+  ],
+}];
