c99da147fde1ceea0fac03c687319f87ca173bbd
Adrienne Walker <enne@quisquo.us>
2019-08-19 23:53:54 -0700

    oopsy: titania nm

---
+++ ui/oopsyraidsy/data/titania.js
@@ -0,0 +1,30 @@
+'use strict';
+
+[{
+  zoneRegex: /^The Dancing Plague$/,
+  damageWarn: {
+    'Titania Wood\'s Embrace': '3D50',
+    // 'Titania Frost Rune': '3D4E',
+    'Titania Gentle Breeze': '3F83',
+    'Titania Leafstorm': '3D55',
+    'Titania Puck\'s Rebuke': '3D58',
+    'Titania Leafstorm': '3E03',
+  },
+  damageFail: {
+    'Titania Phantom Rune 1': '3D5D',
+    'Titania Phantom Rune 2': '3D5E',
+  },
+  triggers: [
+    {
+      id: 'Titania Divination Rune',
+      damageRegex: '3D5B',
+      condition: function(e, data) {
+        // Double taps only.
+        return e.type != '15';
+      },
+      mistake: function(e, data) {
+        return { type: 'fail', blame: e.targetName, text: e.abilityName };
+      },
+    },
+  ],
+}];
