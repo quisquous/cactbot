d35137293ea0419bd5d06583fb57258eebf45603
Jonathan Garber <linkthevaliant@gmail.com>
2019-08-28 22:06:58 -0400

    oopsy: holminster (#584)

---
+++ ui/oopsyraidsy/data/holminster_switch.js
@@ -0,0 +1,45 @@
+'use strict';
+
+[{
+  zoneRegex: /^Holminster Switch$/,
+  damageWarn: {
+    'Holminster Thumbscrew': '3DC6',
+    'Holminster Wooden horse': '3DC7',
+    'Holminster Light Shot': '3DC8',
+    'Holminster Heretic\'s Fork': '3DCE',
+    'Holminster Holy Water': '3DD4',
+    'Holminster Fierce Beating 1': '3DDD',
+    'Holminster Fierce Beating 2': '3DDE',
+    'Holminster Fierce Beating 3': '3DDF',
+    'Holminster Cat O\' Nine Tails': '3DE1',
+    'Holminster Right Knout': '3DE6',
+    'Holminster Left Knout': '3DE7',
+  },
+  damageFail: {
+    'Holminster Aethersup': '3DE9',
+  },
+  triggers: [
+    {
+      id: 'Holminster Flagellation',
+      damageRegex: '3DD6',
+      condition: function(e, data) {
+        // Double taps only.
+        return e.type != '15';
+      },
+      mistake: function(e, data) {
+        return { type: 'warn', blame: e.targetName, text: e.abilityName };
+      },
+    },
+    {
+      id: 'Holminster Taphephobia',
+      damageRegex: '4181',
+      condition: function(e, data) {
+        // Double taps only.
+        return e.type != '15';
+      },
+      mistake: function(e, data) {
+        return { type: 'fail', blame: e.targetName, text: e.abilityName };
+      },
+    },
+  ],
+}];
