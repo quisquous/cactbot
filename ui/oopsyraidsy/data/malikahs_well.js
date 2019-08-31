1a90c4ba294875629bb8335b097c8225492b1e6d
Jonathan Garber <linkthevaliant@gmail.com>
2019-08-31 11:38:15 -0400

    Oopsyraidsy Malikah (#589)

---
+++ ui/oopsyraidsy/data/malikahs_well.js
@@ -0,0 +1,17 @@
+'use strict';
+
+[{
+  zoneRegex: /^Malikah\'s Well$/,
+  damageWarn: {
+    'Malikah Falling Rock': '3CEA',
+    'Malikah Wellbore': '3CED',
+    'Malikah Geyser Eruption': '3CEE',
+    'Malikah Swift Spill': '3CF0',
+    'Malikah Breaking Wheel 1': '3CF5',
+    'Malikah Crystal Nail': '3CF7',
+    'Malikah Heretic\'s Fork 1': '3CF9',
+    'Malikah Breaking Wheel 2': '3CFA',
+    'Malikah Heretic\'s Fork 2': '3E0E',
+    'Malikah Earthshake': '3E39',
+  },
+}];
