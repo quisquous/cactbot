74c76e18a1026b5a5ea70de2bb8c9e01dba5c57c
Adrienne Walker <enne@quisquo.us>
2019-09-22 13:34:18 -0700

    oopsy: add titan extreme

---
+++ ui/oopsyraidsy/data/02-arr/trial/titan_ex.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// Titan Extreme
+[{
+  zoneRegex: /^The Navel \(Extreme\)$/,
+  damageWarn: {
+    'TitanEx Landslide': '5BB',
+    'TitanEx Weight Of The Land': '5BE',
+    'TitanEx Burst': '5BF',
+    'TitanEx Gaoler Landslide': '5C3',
+  },
+  triggers: [
+    // Things that should only hit one person.
+    {
+      id: 'TitanEx Mountain Buster',
+      damageRegex: '5B8',
+      condition: function(e, data) {
+        return e.type != '15';
+      },
+      mistake: function(e, data) {
+        return { type: 'fail', blame: e.targetName, text: e.abilityName };
+      },
+    },
+    {
+      id: 'TitanEx Rock Buster',
+      damageRegex: '5B7',
+      condition: function(e, data) {
+        return e.type != '15';
+      },
+      mistake: function(e, data) {
+        return { type: 'fail', blame: e.targetName, text: e.abilityName };
+      },
+    },
+  ],
+}];
