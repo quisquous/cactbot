e4b9386c842227905755cc06ba1520c1d9f3c6f9
Akurosia Kamo <kamothegamer@web.de>
2019-10-08 11:22:03 +0200

    updated 04-sb/dungeon

--- ui/raidboss/data/04-sb/dungeon/fractal_continuum_hard.js
+++ ui/raidboss/data/04-sb/dungeon/fractal_continuum_hard.js
@@ -9,6 +9,7 @@
       regex: / 14:2AE5:Servomechanical Minotaur starts using 16-Tonze Swipe/,
       regexDe: / 14:2AE5:Servomechanischer Minotaurus starts using 16-Tonzen-Hieb/,
       regexFr: / 14:2AE5:Minotaure Servomécanique starts using Fauche De 16 Tonz/,
+      regexJa: / 14:2AE5:サーヴォ・ミノタウロス starts using 16トンズ・スワイプ/,
       infoText: {
         en: 'swipe',
         de: 'Hieb',
@@ -20,6 +21,7 @@
       regex: / 14:29A2:Biomanufactured Minotaur starts using 11-Tonze Swipe/,
       regexDe: / 14:29A2:Biotech-Minotaurus starts using 11-Tonzen-Hieb/,
       regexFr: / 14:29A2:Minotaure Biologique starts using Fauche De 11 Tonz/,
+      regexJa: / 14:29A2:バイオ・ミノタウロス starts using 11トンズ・スワイプ/,
       infoText: {
         en: 'swipe',
         de: 'Hieb',
@@ -31,6 +33,7 @@
       regex: / 14:2AE4:Servomechanical Minotaur starts using 128-Tonze Swing/,
       regexDe: / 14:2AE4:Servomechanischer Minotaurus starts using 128-Tonzen-Schwung/,
       regexFr: / 14:2AE4:Minotaure Servomécanique starts using Swing De 128 Tonz/,
+      regexJa: / 14:2AE4:サーヴォ・ミノタウロス starts using 128トンズ・スイング/,
       alertText: {
         en: 'Swing',
         de: 'Schwung',
@@ -42,6 +45,7 @@
       regex: / 14:29A1:Biomanufactured Minotaur starts using 111-Tonze Swing/,
       regexDe: / 14:29A1:Biotech-Minotaurus starts using 111-Tonzen-Schwung/,
       regexFr: / 14:29A1:Minotaure Biologique starts using Swing De 111 Tonz/,
+      regexJa: / 14:29A1:バイオ・ミノタウロス starts using 111トンズ・スイング/,
       alertText: {
         en: 'Swing',
         de: 'Schwung',
@@ -53,6 +57,7 @@
       regex: / 14:861:Servomechanical Chimera starts using The Dragon's Voice/,
       regexDe: / 14:861:Servomechanischer Chimära starts using Stimme Des Drachen/,
       regexFr: / 14:861:Chimère Servomécanique starts using Voix Du Dragon/,
+      regexJa: / 14:861:サーヴォ・キマイラ starts using 雷電の咆哮/,
       alertText: {
         en: 'Dragon\'s Voice',
         de: 'Stimme Des Drachen',
@@ -67,8 +72,9 @@
     {
       id: 'Fractal Ram Voice',
       regex: / 14:860:Servomechanical Chimera starts using The Ram's Voice/,
-      regexDe: / 14:861:Servomechanischer Chimära starts using Stimme Des Widders/,
-      regexFr: / 14:861:Chimère Servomécanique starts using Voix Du Bélier/,
+      regexDe: / 14:860:Servomechanischer Chimära starts using Stimme Des Widders/,
+      regexFr: / 14:860:Chimère Servomécanique starts using Voix Du Bélier/,
+      regexJa: / 14:860:サーヴォ・キマイラ starts using 氷結の咆哮/,
       alertText: {
         en: 'Ram\'s Voice',
         de: 'Stimme Des Widders',
@@ -85,6 +91,7 @@
       regex: /14:27AE:The Ultima Beast starts using Death Spin/,
       regexDe: /14:27AE:Ultimative Bestie starts using Strudel Des Todes/,
       regexFr: /14:27AE:Ultima-monstre starts using Tourbillon Mortel/,
+      regexJa: / 14:27AE:アルテマビースト starts using デススピン/,
       infoText: {
         en: 'Knockback',
         de: 'Rückstoß',
@@ -93,9 +100,10 @@
     },
     {
       id: 'Fractal Aether Bend',
-      regex: /14:27(?:A3|AF|B0):The Ultima Beast starts using Aether Bend/,
-      regexDe: /14:27(?:A3|AF|B0):Ultimative Bestie starts using Ätherbeugung/,
-      regexFr: /14:27(?:A3|AF|B0):Ultima-monstre starts using Diffraction éthérée/,
+      regex: /14:27(?:AF|B0):The Ultima Beast starts using Aether Bend/,
+      regexDe: /14:27(?:AF|B0):Ultimative Bestie starts using Ätherbeugung/,
+      regexFr: /14:27(?:AF|B0):Ultima-monstre starts using Diffraction éthérée/,
+      regexJa: / 14:27(?:AF|B0):アルテマビースト starts using エーテルベント/,
       alertText: {
         en: 'Get In',
         de: 'Reingehen',
