dd17b35eb8bd36c29617d186c1f3d7b0092f4414
Akurosia Kamo <kamothegamer@web.de>
2019-10-10 09:26:40 +0200

    fixed small parts in e1s and e2s
    Please enter the commit message for your changes. Lines starting

--- ui/raidboss/data/05-shb/raid/e1s.js
+++ ui/raidboss/data/05-shb/raid/e1s.js
@@ -82,7 +82,7 @@
       id: 'E1S Fragor Maximus',
       regex: / 14:3D8B:Eden Prime starts using Fragor Maximus/,
       regexDe: / 14:3D8B:Prim-Eden starts using Fragor Maximus/,
-      regexFr: / 14:3D8B::Primo-Éden starts using Fragor Maximus/,
+      regexFr: / 14:3D8B:Primo-Éden starts using Fragor Maximus/,
       regexJa: / 14:3D8B:エデン・プライム starts using フラゴルマクシマス/,
       condition: function(data) {
         return data.role == 'healer';
