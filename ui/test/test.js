bff441ec0d0e5274ae253af90ccc857156548532
Adrienne Walker <enne@quisquo.us>
2019-10-29 09:12:57 -0700

    test: update test html to show bait properly

--- ui/test/test.js
+++ ui/test/test.js
@@ -53,6 +53,7 @@ document.addEventListener('onPlayerChangedEvent', function(e) {
     document.getElementById('jobinfo').innerText = '';
 
   document.getElementById('pos').innerText = e.detail.pos.x + ',' + e.detail.pos.y + ',' + e.detail.pos.z;
+  document.getElementById('bait').innerText = e.detail.bait;
 });
 
 document.addEventListener('onTargetChangedEvent', function(e) {
