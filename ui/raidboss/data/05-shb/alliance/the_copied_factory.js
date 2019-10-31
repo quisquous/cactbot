ea0f4046ee6d682d4c354cbb6ba97dbca77bcd84
Adrienne Walker <enne@quisquo.us>
2019-10-30 23:40:50 -0700

    raidboss: initial copied factory triggers

--- ui/raidboss/data/05-shb/alliance/the_copied_factory.js
+++ ui/raidboss/data/05-shb/alliance/the_copied_factory.js
@@ -8,5 +8,73 @@
   timelineTriggers: [
   ],
   triggers: [
+    {
+      id: 'Copied Serial Forceful Impact',
+      regex: / 14:48CF:Serial-Jointed Command Model starts using Forceful Impact/,
+      condition: function(data) {
+        return data.role == 'tank' || data.role == healer || data.CanAddle();
+      },
+      infoText: {
+        en: 'aoe',
+      },
+    },
+    {
+      id: 'Copied Serial Energy Assault',
+      regex: / 14:48B5:Serial-Jointed Command Model starts using Energy Assault/,
+      alertText: {
+        en: 'Get Behind',
+      },
+    },
+    {
+      id: 'Copied Serial High-Caliber Laser',
+      regex: / 14:48FA:Serial-Jointed Service Model starts using High-Caliber Laser/,
+      infoText: {
+        en: 'Look for Lasers',
+      },
+    },
+    {
+      id: 'Copied Serial Clanging Blow',
+      regex: / 14:48CE:Serial-Jointed Command Model starts using Clanging Blow on (\y{Name})\./,
+      alertText: function(data, matches) {
+        if (matches[1] == data.me) {
+          return {
+            en: 'Tank Buster on YOU',
+            de: 'Tankbuster auf DIR',
+            fr: 'Tankbuster sur VOUS',
+          };
+        }
+      },
+      infoText: function(data, matches) {
+        if (matches[1] == data.me || data.role != 'healer')
+          return;
+
+        return {
+          en: 'Buster on ' + data.ShortName(matches[1]),
+          de: 'Tankbuster auf ' + data.ShortName(matches[1]),
+          fr: 'Tankbuster sur ' + data.ShortName(matches[1]),
+        };
+      },
+    },
+    {
+      id: 'Copied Serial Centrifugal Spin',
+      regex: / 14:48C8:Serial-Jointed Command Model starts using Centrifugal Spin/,
+      alertText: {
+        en: 'Go To Sides',
+      },
+    },
+    {
+      id: 'Copied Serial Centrifugal Spin',
+      regex: / 14:48CA:Serial-Jointed Command Model starts using Sidestriking Spin/,
+      alertText: {
+        en: 'Go Front/Back',
+      },
+    },
+    {
+      id: 'Copied Serial Shockwave',
+      regex: / 14:48C3:Serial-Jointed Command Model starts using Shockwave/,
+      infoText: {
+        en: 'Knockback',
+      },
+    },
   ],
 }];
