01e7af88c9123bae6090ec5a6747a13e99fae4dd
Jonathan Garber <linkthevaliant@gmail.com>
2019-11-04 12:14:01 -0500

    Added in additional ignores per origin

--- util/make_timeline.py
+++ util/make_timeline.py
@@ -275,7 +275,9 @@ def main(args):
         'Minfilia',
         'Thancred',
         'Urianger',
-        ''
+        '',
+        '2P',
+        'Crystal Exarch',
     ]
 
     # Format the phase timings
