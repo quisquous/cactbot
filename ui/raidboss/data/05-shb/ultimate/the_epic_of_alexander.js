56c144a890612b9188231270ac742afbed15e9b5
Adrienne Walker <enne@quisquo.us>
2019-11-13 18:19:58 -0800

    raidboss: improve tea tankbuster callouts

--- ui/raidboss/data/05-shb/ultimate/the_epic_of_alexander.js
+++ ui/raidboss/data/05-shb/ultimate/the_epic_of_alexander.js
@@ -5,29 +5,71 @@
   timelineFile: 'the_epic_of_alexander.txt',
   timelineTriggers: [
     {
-      // TODO: maybe figure out who is tanking which and call out Fluid Strike vs Fluid Swing
       id: 'TEA Fluid Swing',
       regex: /Fluid Swing/,
       beforeSeconds: 5,
+      preRun: function(data) {
+        data.swingCount = (data.swingCount || 0) + 1;
+      },
       alertText: function(data) {
-        if (data.role == 'healer' || data.role == 'tank') {
+        let multipleSwings = data.swingCount == 2 || data.swingCount == 3;
+        if (data.role == 'healer') {
+          if (multipleSwings) {
+            return {
+              en: 'Tank Busters',
+              de: 'Tank buster',
+              fr: 'Tank busters',
+            };
+          }
+          if (data.mainTank) {
+            return {
+              en: 'Tank Buster on' + data.ShortName(data.mainTank),
+              de: 'Tank buster',
+              fr: 'Tank buster',
+            };
+          }
           return {
-            en: 'Tank Busters',
+            en: 'Tank Buster',
             de: 'Tank buster',
-            fr: 'Tank busters',
+            fr: 'Tank buster',
           };
+        }
+
+        if (data.role == 'tank') {
+          if (data.me == data.offTank && multipleSwings || data.me == data.mainTank) {
+            return {
+              en: 'Tank Buster on YOU',
+            };
+          }
         }
       },
       infoText: function(data) {
-        if (data.role != 'healer' && data.role != 'tank') {
-          return {
-            en: 'Tank Cleave',
-          };
-        }
+        let multipleSwings = data.swingCount == 2 || data.swingCount == 3;
+        if (data.role == 'healer')
+          return;
+        if (data.me == data.offTank && multipleSwings || data.me == data.mainTank)
+          return;
+        return {
+          en: 'Tank Cleave',
+        };
       },
     },
   ],
   triggers: [
+    {
+      id: 'TEA Main Tank',
+      regex: Regexes.abilityFull({ source: 'Living Liquid', id: '4978' }),
+      run: function(data, matches) {
+        data.mainTank = matches.target;
+      },
+    },
+    {
+      id: 'TEA Off Tank',
+      regex: Regexes.abilityFull({ source: 'Living Liquid', id: '4979' }),
+      run: function(data, matches) {
+        data.offTank = matches.target;
+      },
+    },
     {
       id: 'TEA Cascade',
       regex: Regexes.startsUsing({ source: 'Living Liquid', id: '4826', capture: false }),
