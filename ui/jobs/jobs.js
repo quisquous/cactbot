quisquous/cactbot

commit 283da4da5b5059fab683731116ba983760719749

resources: remove player name from lang
This switches everything over to use Regexes-based regexes instead of
having multiple ways to generate them.

ui/jobs/jobs.js
@@ -253,15 +253,15 @@ function setupComboTracker(callback) {
   return comboTracker;
 }
 
-function setupRegexes() {
+function setupRegexes(playerName) {
   kWellFedZoneRegex = Regexes.anyOf(Options.WellFedZones.map(function(x) {
     return gLang.kZone[x];
   }));
 
-  kYouGainEffectRegex = gLang.youGainEffectRegex('(.*)');
-  kYouLoseEffectRegex = gLang.youLoseEffectRegex('(.*)');
-  kYouUseAbilityRegex = gLang.youUseAbilityRegex('(\\y{AbilityCode})');
-  kAnybodyAbilityRegex = gLang.abilityRegex('(\\y{AbilityCode})');
+  kYouGainEffectRegex = Regexes.gainsEffect({ target: playerName });
+  kYouLoseEffectRegex = Regexes.losesEffect({ target: playerName });
+  kYouUseAbilityRegex = Regexes.ability({ source: playerName });
+  kAnybodyAbilityRegex = Regexes.ability();
 
   // Full skill names of abilities that break combos.
   // TODO: it's sad to have to duplicate combo abilities here to catch out-of-order usage.
@@ -522,8 +522,9 @@ class Buff {
 }
 
 class BuffTracker {
-  constructor(options, leftBuffDiv, rightBuffDiv) {
+  constructor(options, playerName, leftBuffDiv, rightBuffDiv) {
     this.options = options;
+    this.playerName = playerName;
     this.leftBuffDiv = leftBuffDiv;
     this.rightBuffDiv = rightBuffDiv;
     this.buffs = {};
@@ -548,7 +549,7 @@ class BuffTracker {
       },
       trick: {
         gainAbility: gLang.kAbility.TrickAttack,
-        gainRegex: gLang.abilityRegex(gLang.kAbility.TrickAttack),
+        gainRegex: Regexes.ability({ id: gLang.kAbility.TrickAttack }),
         durationSeconds: 15,
         icon: '../../resources/icon/status/trick-attack.png',
         // Magenta.
@@ -572,7 +573,7 @@ class BuffTracker {
         // Instead, use somebody using the effect on you:
         //   16:106C22EF:Tater Tot:1D60:Embolden:106C22EF:Potato Chippy:500020F:4D7: etc etc
         gainAbility: gLang.kAbility.Embolden,
-        gainRegex: gLang.abilityRegex(gLang.kAbility.Embolden, null, gLang.playerName),
+        gainRegex: Regexes.abilityFull({ id: gLang.kAbility.Embolden, target: this.playerName }),
         loseEffect: gLang.kEffect.Embolden,
         durationSeconds: 20,
         icon: '../../resources/icon/status/embolden.png',
@@ -996,8 +997,6 @@ class Bars {
       this.o.leftBuffsList.toward = 'left down';
       this.o.leftBuffsList.elementwidth = this.options.BigBuffIconWidth + 2;
     }
-
-    this.buffTracker = new BuffTracker(this.options, this.o.leftBuffsList, this.o.rightBuffsList);
 
     if (Util.isCraftingJob(this.job)) {
       this.o.cpContainer = document.createElement('div');
@@ -1997,13 +1996,13 @@ class Bars {
       this.o.manaBar.fg = computeBackgroundColorFrom(this.o.manaBar, 'mp-color');
   }
 
-  UpdateCP() {
+  updateCp() {
     if (!this.o.cpBar) return;
     this.o.cpBar.value = this.cp;
     this.o.cpBar.maxvalue = this.maxCP;
   }
 
-  UpdateGP() {
+  UpdateGp() {
     if (!this.o.gpBar) return;
     this.o.gpBar.value = this.gp;
     this.o.gpBar.maxvalue = this.maxGP;
@@ -2106,19 +2105,21 @@ class Bars {
   }
 
   OnPlayerChanged(e) {
+    let nameChanged = this.me !== e.detail.name;
+
     if (!this.init) {
       this.me = e.detail.name;
-      setupRegexes();
+      setupRegexes(this.me);
       this.combo = setupComboTracker(this.OnComboChange.bind(this));
       this.init = true;
     }
 
-    let update_job = false;
-    let update_hp = false;
-    let update_mp = false;
-    let update_cp = false;
-    let update_gp = false;
-    let update_level = false;
+    let updateJob = false;
+    let updateHp = false;
+    let updateMp = false;
+    let updateCp = false;
+    let updateGp = false;
+    let updateLevel = false;
     if (e.detail.job != this.job) {
       this.job = e.detail.job;
       // Combos are job specific.
@@ -2126,18 +2127,18 @@ class Bars {
       // Update MP ticker as umbral stacks has changed.
       this.umbralStacks = 0;
       this.UpdateMPTicker();
-      update_job = update_hp = update_mp = update_cp = update_gp = true;
+      updateJob = updateHp = updateMp = updateCp = updateGp = true;
     }
     if (e.detail.level != this.level) {
       this.level = e.detail.level;
-      update_level = true;
+      updateLevel = true;
     }
     if (e.detail.currentHP != this.hp || e.detail.maxHP != this.maxHP ||
       e.detail.currentShield != this.currentShield) {
       this.hp = e.detail.currentHP;
       this.maxHP = e.detail.maxHP;
       this.currentShield = e.detail.currentShield;
-      update_hp = true;
+      updateHp = true;
 
       if (this.hp == 0)
         this.combo.AbortCombo(); // Death resets combos.
@@ -2145,32 +2146,38 @@ class Bars {
     if (e.detail.currentMP != this.mp || e.detail.maxMP != this.maxMP) {
       this.mp = e.detail.currentMP;
       this.maxMP = e.detail.maxMP;
-      update_mp = true;
+      updateMp = true;
     }
     if (e.detail.currentCP != this.cp || e.detail.maxCP != this.maxCP) {
       this.cp = e.detail.currentCP;
       this.maxCP = e.detail.maxCP;
-      update_cp = true;
+      updateCp = true;
     }
     if (e.detail.currentGP != this.gp || e.detail.maxGP != this.maxGP) {
       this.gp = e.detail.currentGP;
       this.maxGP = e.detail.maxGP;
-      update_gp = true;
+      updateGp = true;
     }
-    if (update_job) {
+    if (updateJob || nameChanged) {
       this.UpdateJob();
       // On reload, we need to set the opacity after setting up the job bars.
       this.UpdateOpacity();
+      // Set up the buff tracker afterr the job bars are created.
+      if (nameChanged) {
+        setupRegexes(this.me);
+        this.buffTracker = new BuffTracker(
+            this.options, this.me, this.o.leftBuffsList, this.o.rightBuffsList);
+      }
     }
-    if (update_hp)
+    if (updateHp)
       this.UpdateHealth();
-    if (update_mp)
+    if (updateMp)
       this.UpdateMana();
-    if (update_cp)
-      this.UpdateCP();
-    if (update_gp)
-      this.UpdateGP();
-    if (update_level)
+    if (updateCp)
+      this.updateCp();
+    if (updateGp)
+      this.UpdateGp();
+    if (updateLevel)
       this.UpdateFoodBuff();
 
     if (e.detail.jobDetail) {
@@ -2230,7 +2237,7 @@ class Bars {
         if (log[16] == 'A') {
           let m = log.match(kYouGainEffectRegex);
           if (m) {
-            let name = m[1];
+            let name = m.groups.effect;
             let f = this.gainEffectFuncMap[name];
             if (f)
               f(name, log);
@@ -2239,7 +2246,7 @@ class Bars {
         } else if (log[16] == 'E') {
           let m = log.match(kYouLoseEffectRegex);
           if (m) {
-            let name = m[1];
+            let name = m.groups.effect;
             let f = this.loseEffectFuncMap[name];
             if (f)
               f(name, log);
@@ -2251,7 +2258,7 @@ class Bars {
         if (log[16] == '5' || log[16] == '6') {
           let m = log.match(kYouUseAbilityRegex);
           if (m) {
-            let id = m[1];
+            let id = m.groups.id;
             this.combo.HandleAbility(id);
             let f = this.abilityFuncMap[id];
             if (f)
@@ -2260,7 +2267,7 @@ class Bars {
           } else {
             let m = log.match(kAnybodyAbilityRegex);
             if (m)
-              this.buffTracker.onUseAbility(m[1], log);
+              this.buffTracker.onUseAbility(m.groups.id, log);
           }
         }
       }
@@ -2271,7 +2278,7 @@ class Bars {
     let logs = [];
     let t = '[10:10:10.000] ';
     logs.push(t + '1A:10000000:' + this.me + ' gains the effect of Medicated from ' + this.me + ' for 30.2 Seconds.');
-    logs.push(t + '1A:10000000:' + this.me + ' gains the effect of Embolden from  for 20 Seconds. (5)');
+    logs.push(t + '15:10000000:Tako Yaki:1D60:Embolden:10000000:' + this.me + ':500020F:4D70000:0:0:0:0:0:0:0:0:0:0:0:0:0:0:42194:42194:10000:10000:0:1000:-655.3301:-838.5481:29.80905:0.523459:42194:42194:10000:10000:0:1000:-655.3301:-838.5481:29.80905:0.523459:00001DE7');
     logs.push(t + '1A:10000000:' + this.me + ' gains the effect of Battle Litany from  for 25 Seconds.');
     logs.push(t + '1A:10000000:' + this.me + ' gains the effect of The Balance from  for 12 Seconds.');
     logs.push(t + '1A:10000000:Okonomi Yaki gains the effect of Foe Requiem from Okonomi Yaki for 9999.00 Seconds.');

resources/lang.js
@@ -6,10 +6,6 @@ class CactbotLanguage {
   constructor(lang) {
     this.InitStrings();
 
-    window.addOverlayListener('onPlayerChangedEvent', (e) => {
-      gLang.OnPlayerNameChange(e.detail.name);
-    });
-
     this.lang = lang;
     this.playerName = null;
     this.kAbility = Object.freeze({
@@ -186,74 +182,5 @@ class CactbotLanguage {
 
   InitStrings() {
     console.error('Derived language class must implement InitStrings');
-  }
-
-  OnPlayerNameChange(playerName) {
-    this.playerName = playerName;
-  }
-
-  // Due to this bug: https://github.com/ravahn/FFXIV_ACT_Plugin/issues/100
-  // We can not look for log messages from FFXIV "You use X" here. Instead we
-  // look for the actual ability usage provided by the XIV plugin.
-  // Also, the networked parse info is given much quicker than the lines
-  // from the game.
-  youUseAbilityRegex(ids) {
-    return Regexes.parse(' 1[56]:\\y{ObjectId}:' + this.playerName + ':' + Regexes.anyOf(ids) + ':');
-  }
-
-  youStartUsingRegex(ids) {
-    return Regexes.parse(' 14:' + Regexes.anyOf(ids) + ':' + this.playerName + ' starts using ');
-  }
-
-  youGainEffectRegex() {
-    let effects = [];
-    for (let i = 0; i < arguments.length; ++i) {
-      let effect = arguments[i];
-      effects.push(effect);
-    }
-    return Regexes.parse(' 1A:\\y{ObjectId}:' + this.playerName + ' gains the effect of ' + Regexes.anyOf(effects) + ' from .* for (\\y{Float}) Seconds\\.');
-  }
-
-  youLoseEffectRegex() {
-    let effects = [];
-    for (let i = 0; i < arguments.length; ++i) {
-      let effect = arguments[i];
-      effects.push(effect);
-    }
-    return Regexes.parse(' 1E:\\y{ObjectId}:' + this.playerName + ' loses the effect of ' + Regexes.anyOf(effects) + ' from .*\\.');
-  }
-
-  // TODO: change uses of these to new regexes functions
-  abilityRegex(abilityId, attacker, target, flags) {
-    if (!abilityId)
-      abilityId = '[^:]*';
-    if (!attacker)
-      attacker = '[^:]*';
-    // type:attackerId:attackerName:abilId:abilName:targetId:targetName:flags:
-    let r = ' 1[56]:\\y{ObjectId}:' + attacker + ':' + abilityId + ':';
-    if (target || flags) {
-      if (!target)
-        target = '[^:]*';
-      if (!flags)
-        flags = '[^:]*';
-      r += '[^:]*:\\y{ObjectId}:' + target + ':' + flags + ':';
-    }
-    return Regexes.parse(r);
-  }
-
-  gainsEffectRegex(effect, target, attacker) {
-    if (!target)
-      target = '[^:]*';
-    if (!attacker)
-      attacker = '[^:]*';
-    return Regexes.parse(' 1A:\\y{ObjectId}:' + target + ' gains the effect of ' + effect + ' from ' + attacker + ' for (\\y{Float}) Seconds\\.');
-  }
-
-  losesEffectRegex(effect, target, attacker) {
-    if (!target)
-      target = '[^:]*';
-    if (!attacker)
-      attacker = '[^:]*';
-    return Regexes.parse(' 1E:\\y{ObjectId}:' + target + ' loses the effect of ' + effect + ' from ' + attacker + '\\.');
   }
 }

ui/oopsyraidsy/data/00-misc/test.js
@@ -77,41 +77,5 @@
         return { type: 'fail', blame: data.me, text: text };
       },
     },
-    {
-      id: 'Test One Ilm Punch',
-      damageRegex: gLang.kAbility.OneIlmPunch,
-      condition: function(e) {
-        return e.targetName == 'Striking Dummy';
-      },
-      mistake: function(e, data) {
-        // Demonstrate returning multiple mistakes.
-        return [
-          {
-            type: 'warn',
-            blame: data.me,
-            text: {
-              en: 'ONE!',
-              de: 'EIN!',
-            },
-          },
-          {
-            type: 'fail',
-            blame: data.me,
-            text: {
-              en: 'ILM!',
-              de: 'ILM!',
-            },
-          },
-          {
-            type: 'potion',
-            blame: data.me,
-            text: {
-              en: 'PUNCH!',
-              de: 'SCHLAG!',
-            },
-          },
-        ];
-      },
-    },
   ],
 }];

ui/oopsyraidsy/oopsyraidsy.js
@@ -762,19 +762,28 @@ class DamageTracker {
     for (let i = 0; i < this.effectTriggers.length; ++i) {
       let trigger = this.effectTriggers[i];
       let matches;
-      if (trigger.gainRegex)
+      let isGainLine;
+      if (trigger.gainRegex) {
         matches = line.match(trigger.gainRegex);
-      if (!matches && trigger.loseRegex)
+        if (matches)
+          isGainLine = true;
+      }
+      if (!matches && trigger.loseRegex) {
         matches = line.match(trigger.loseRegex);
+        if (matches)
+          isGainLine = false;
+      }
       if (matches == null)
         continue;
+      // TODO: just pass groups on through, and change `attacker` to `source`.
+      let g = matches.groups;
       if (!evt) {
         evt = {
-          targetName: matches[1],
-          effectName: matches[2],
-          attackerName: matches[3],
-          gains: !!matches[4],
-          durationSeconds: matches[4] ? parseFloat(matches[4]) : undefined,
+          targetName: g.target,
+          effectName: g.effect,
+          attackerName: g.source,
+          gains: isGainLine,
+          durationSeconds: g.duration,
         };
       }
       this.OnTrigger(trigger, evt, null);
@@ -970,11 +979,11 @@ class DamageTracker {
           this.abilityTriggers.push(trigger);
         }
         if ('gainsEffectRegex' in trigger) {
-          trigger.gainRegex = gLang.gainsEffectRegex('(' + trigger.gainsEffectRegex + ')', '(\\y{Name})', '(.*?)');
+          trigger.gainRegex = Regexes.gainsEffect({ effect: trigger.gainsEffectRegex });
           this.effectTriggers.push(trigger);
         }
         if ('losesEffectRegex' in trigger) {
-          trigger.loseRegex = gLang.losesEffectRegex('(' + trigger.losesEffectRegex + ')', '(\\y{Name})', '(.*?)');
+          trigger.loseRegex = Regexes.losesEffect({ effect: trigger.losesEffectRegex });
           this.effectTriggers.push(trigger);
         }
         if ('healRegex' in trigger) {

commit 60aab528ad597f12270f59d2df4a8759c8d58228

jobs: fix buffs not appearing after changing jobs
This was broken in 283da4d.

ui/jobs/jobs.js
@@ -2116,13 +2116,14 @@ class Bars {
   }
 
   OnPlayerChanged(e) {
-    let nameChanged = this.me !== e.detail.name;
-
     if (!this.init) {
-      this.me = e.detail.name;
-      setupRegexes(this.me);
       this.combo = setupComboTracker(this.OnComboChange.bind(this));
       this.init = true;
+    }
+
+    if (this.me !== e.detail.name) {
+      this.me = e.detail.name;
+      setupRegexes(this.me);
     }
 
     let updateJob = false;
@@ -2169,16 +2170,13 @@ class Bars {
       this.maxGP = e.detail.maxGP;
       updateGp = true;
     }
-    if (updateJob || nameChanged) {
+    if (updateJob) {
       this.UpdateJob();
       // On reload, we need to set the opacity after setting up the job bars.
       this.UpdateOpacity();
-      // Set up the buff tracker afterr the job bars are created.
-      if (nameChanged) {
-        setupRegexes(this.me);
-        this.buffTracker = new BuffTracker(
-            this.options, this.me, this.o.leftBuffsList, this.o.rightBuffsList);
-      }
+      // Set up the buff tracker after the job bars are created.
+      this.buffTracker = new BuffTracker(
+          this.options, this.me, this.o.leftBuffsList, this.o.rightBuffsList);
     }
     if (updateHp)
       this.UpdateHealth();