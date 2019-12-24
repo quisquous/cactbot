a7470c2aa67c55e039bc5855893d6d9e512301c9
Adrienne Walker <enne@quisquo.us>
2019-12-23 18:55:19 -0800

    raidboss: convert general triggers to use regexes
    Doing this first, because it lets me convert yName to something simpler.

--- ui/raidboss/data/00-misc/general.js
+++ ui/raidboss/data/00-misc/general.js
@@ -6,149 +6,132 @@
   triggers: [
     {
       id: 'General Provoke',
-      regex: /:(\y{Name}):1D6D:Provoke:/,
-      regexDe: /:(\y{Name}):1D6D:Herausforderung:/,
-      regexFr: /:(\y{Name}):1D6D:Provocation:/,
-      regexJa: /:(\y{Name}):1D6D:挑発:/,
-      regexCn: /:(\y{Name}):1D6D:挑衅:/,
+      regex: Regexes.ability({ id: '1D6D' }),
       condition: function(data) {
         return data.role == 'tank';
       },
       infoText: function(data, matches) {
+        let name = data.ShortName(matches.source);
         return {
-          en: 'Provoke: ' + data.ShortName(matches[1]),
-          de: 'Herausforderung: ' + data.ShortName(matches[1]),
-          fr: 'Provocation: ' + data.ShortName(matches[1]),
-          ja: '挑発: ' + data.ShortName(matches[1]),
-          cn: '挑衅: ' + data.ShortName(matches[1]),
+          en: 'Provoke: ' + name,
+          de: 'Herausforderung: ' + name,
+          fr: 'Provocation: ' + name,
+          ja: '挑発: ' + name,
+          cn: '挑衅: ' + name,
         };
       },
     },
     {
       id: 'General Shirk',
-      regex: /:(\y{Name}):1D71:Shirk:/,
-      regexDe: /:(\y{Name}):1D71:Geteiltes Leid:/,
-      regexFr: /:(\y{Name}):1D71:Dérobade:/,
-      regexJa: /:(\y{Name}):1D71:シャーク:/,
-      regexCn: /:(\y{Name}):1D71:退避:/,
+      regex: Regexes.ability({ id: '1D71' }),
       condition: function(data) {
         return data.role == 'tank';
       },
       infoText: function(data, matches) {
+        let name = data.ShortName(matches.source);
         return {
-          en: 'Shirk: ' + data.ShortName(matches[1]),
-          de: 'Geteiltes Leid: ' + data.ShortName(matches[1]),
-          fr: 'Dérobade: ' + data.ShortName(matches[1]),
-          ja: 'シャーク: ' + data.ShortName(matches[1]),
-          cn: '退避: ' + data.ShortName(matches[1]),
+          en: 'Shirk: ' + name,
+          de: 'Geteiltes Leid: ' + name,
+          fr: 'Dérobade: ' + name,
+          ja: 'シャーク: ' + name,
+          cn: '退避: ' + name,
         };
       },
     },
     {
       id: 'General Holmgang',
-      regex: /:(\y{Name}):2B:Holmgang:/,
-      regexDe: /:(\y{Name}):2B:Holmgang:/,
-      regexFr: /:(\y{Name}):2B:Holmgang:/,
-      regexJa: /:(\y{Name}):2B:ホルムギャング:/,
-      regexCn: /:(\y{Name}):2B:死斗:/,
+      regex: Regexes.ability({ id: '2B' }),
       condition: function(data) {
         return data.role == 'tank' || data.role == 'healer';
       },
       infoText: function(data, matches) {
+        let name = data.ShortName(matches.source);
         return {
-          en: 'Holmgang: ' + data.ShortName(matches[1]),
-          ja: 'ホルムギャング: ' + data.ShortName(matches[1]),
-          cn: '死斗: ' + data.ShortName(matches[1]),
+          en: 'Holmgang: ' + name,
+          ja: 'ホルムギャング: ' + name,
+          cn: '死斗: ' + name,
         };
       },
     },
     {
       id: 'General Hallowed',
-      regex: /:(\y{Name}):1E:Hallowed Ground:/,
-      regexDe: /:(\y{Name}):1E:Heiliger Boden:/,
-      regexFr: /:(\y{Name}):1E:Invincible:/,
-      regexJa: /:(\y{Name}):1E:インビンシブル:/,
-      regexCn: /:(\y{Name}):1E:神圣领域:/,
+      regex: Regexes.ability({ id: '1E' }),
       condition: function(data) {
         return data.role == 'tank' || data.role == 'healer';
       },
       infoText: function(data, matches) {
+        let name = data.ShortName(matches.source);
         return {
-          en: 'Hallowed: ' + data.ShortName(matches[1]),
-          de: 'Heiliger Boden: ' + data.ShortName(matches[1]),
-          fr: 'Invincible: ' + data.ShortName(matches[1]),
-          ja: 'インビンシブル: ' + data.ShortName(matches[1]),
-          cn: '神圣领域: ' + data.ShortName(matches[1]),
+          en: 'Hallowed: ' + name,
+          de: 'Heiliger Boden: ' + name,
+          fr: 'Invincible: ' + name,
+          ja: 'インビンシブル: ' + name,
+          cn: '神圣领域: ' + name,
         };
       },
     },
     {
       id: 'General Superbolide',
-      regex: /:(\y{Name}):3F18:Superbolide:/,
-      regexDe: /:(\y{Name}):3F18:Meteoritenfall:/,
-      regexFr: /:(\y{Name}):3F18:Bolide:/,
-      regexJa: /:(\y{Name}):3F18:ボーライド:/,
-      regexCn: /:(\y{Name}):3F18:超火流星:/,
+      regex: Regexes.ability({ id: '3F18' }),
       condition: function(data) {
         return data.role == 'tank' || data.role == 'healer';
       },
       infoText: function(data, matches) {
+        let name = data.ShortName(matches.source);
         return {
-          en: 'Bolide: ' + data.ShortName(matches[1]),
-          de: 'Meteoritenfall: ' + data.ShortName(matches[1]),
-          fr: 'Bolide: ' + data.ShortName(matches[1]),
-          ja: 'ボーライド: ' + data.ShortName(matches[1]),
-          cn: '超火流星: ' + data.ShortName(matches[1]),
+          en: 'Bolide: ' + name,
+          de: 'Meteoritenfall: ' + name,
+          fr: 'Bolide: ' + name,
+          ja: 'ボーライド: ' + name,
+          cn: '超火流星: ' + name,
         };
       },
     },
     {
       id: 'General Living',
-      regex: /:(\y{Name}):E36:Living Dead:/,
-      regexDe: /:(\y{Name}):E36:Totenerweckung:/,
-      regexFr: /:(\y{Name}):E36:Mort-Vivant:/,
-      regexJa: /:(\y{Name}):E36:リビングデッド:/,
-      regexCn: /:(\y{Name}):E36:行尸走肉:/,
+      regex: Regexes.ability({ id: 'E36' }),
       condition: function(data) {
         return data.role == 'tank' || data.role == 'healer';
       },
       infoText: function(data, matches) {
+        let name = data.ShortName(matches.source);
         return {
-          en: 'Living: ' + data.ShortName(matches[1]),
-          de: 'Totenerweckung: ' + data.ShortName(matches[1]),
-          fr: 'Mort-vivant: ' + data.ShortName(matches[1]),
-          ja: 'リビングデッド: ' + data.ShortName(matches[1]),
-          cn: '行尸走肉: ' + data.ShortName(matches[1]),
+          en: 'Living: ' + name,
+          de: 'Totenerweckung: ' + name,
+          fr: 'Mort-vivant: ' + name,
+          ja: 'リビングデッド: ' + name,
+          cn: '行尸走肉: ' + name,
         };
       },
     },
     {
       id: 'General Walking',
-      regex: / 1A:\y{ObjectId}:(\y{Name}) gains the effect of Walking Dead/,
-      regexDe: / 1A:\y{ObjectId}:(\y{Name}) gains the effect of Erweckter/,
-      regexFr: / 1A:\y{ObjectId}:(\y{Name}) gains the effect of Marcheur Des Limbes/,
-      regexJa: / 1A:\y{ObjectId}:(\y{Name}) gains the effect of ウォーキングデッド/,
-      regexCn: / 1A:\y{ObjectId}:(\y{Name}) gains the effect of 死而不僵/,
+      regex: Regexes.gainsEffect({ effect: 'Walking Dead' }),
+      regexDe: Regexes.gainsEffect({ effect: 'Erweckter' }),
+      regexFr: Regexes.gainsEffect({ effect: 'Marcheur Des Limbes' }),
+      regexJa: Regexes.gainsEffect({ effect: 'ウォーキングデッド' }),
+      regexCn: Regexes.gainsEffect({ effect: '死而不僵' }),
       condition: function(data) {
         return data.role == 'tank' || data.role == 'healer';
       },
       infoText: function(data, matches) {
+        let name = data.ShortName(matches.source);
         return {
-          en: 'Walking: ' + data.ShortName(matches[1]),
-          de: 'Erweckter: ' + data.ShortName(matches[1]),
-          fr: 'Marcheur Des Limbes: ' + data.ShortName(matches[1]),
-          ja: 'ウォーキングデッド: ' + data.ShortName(matches[1]),
-          cn: '死而不僵: ' + data.ShortName(matches[1]),
+          en: 'Walking: ' + name,
+          de: 'Erweckter: ' + name,
+          fr: 'Marcheur Des Limbes: ' + name,
+          ja: 'ウォーキングデッド: ' + name,
+          cn: '死而不僵: ' + name,
         };
       },
     },
     {
       id: 'General Ready check',
-      regex: /:(?:\y{Name} has initiated|You have commenced) a ready check\./,
-      regexDe: /:(?:\y{Name} hat|Du hast) eine Bereitschaftsanfrage gestellt\./,
-      regexFr: /:Un appel de préparation a été lancé par \y{Name}\./,
-      regexJa: /:(?:\y{Name}が)?レディチェックを開始しました。/,
-      regexCn: /:\y{Name}?发起了准备确认/,
+      regex: Regexes.gameLog({ line: '(?:\y{Name} has initiated|You have commenced) a ready check\.', capture: false }),
+      regexDe: Regexes.gameLog({ line: '(?:\y{Name} hat|Du hast) eine Bereitschaftsanfrage gestellt\.', capture: false }),
+      regexFr: Regexes.gameLog({ line: 'Un appel de préparation a été lancé par \y{Name}\.', capture: false }),
+      regexJa: Regexes.gameLog({ line: '(?:\y{Name}が)?レディチェックを開始しました。', capture: false }),
+      regexCn: Regexes.gameLog({ line: '\y{Name}?发起了准备确认', capture: false }),
       sound: '../../resources/sounds/Overwatch/D.Va_-_Game_on.ogg',
       soundVolume: 0.6,
     },
