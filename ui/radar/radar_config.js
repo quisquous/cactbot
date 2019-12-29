b439413d2bbf60256de83f392d0668c74cec7a1b
ShadyWhite <44015352+ShadyWhite@users.noreply.github.com>
2019-12-30 03:00:27 +0800

    Config UI FR translations

--- ui/radar/radar_config.js
+++ ui/radar/radar_config.js
@@ -7,6 +7,7 @@ UserConfig.registerOptions('radar', {
       name: {
         en: 'Minimum distance to detect mobs (yalms)',
         fr: 'Distance minimale pour détection des mobs (yalms)',
+        cn: '探测目标的最小距离（米）',
       },
       type: 'float',
       default: 0,
@@ -16,6 +17,7 @@ UserConfig.registerOptions('radar', {
       name: {
         en: 'Announce new mobs with text to speech',
         fr: 'Annoncer les nouveaux mobs via TTS',
+        cn: '使用TTS提醒新发现目标',
       },
       type: 'checkbox',
       default: false,
@@ -25,6 +27,7 @@ UserConfig.registerOptions('radar', {
       name: {
         en: 'Announce new mobs with a sound',
         fr: 'Annoncer les nouveaux mobs avec un son',
+        cn: '使用声音提醒新发现目标',
       },
       type: 'checkbox',
       default: true,
@@ -34,6 +37,7 @@ UserConfig.registerOptions('radar', {
       name: {
         en: 'Volume to play pop sound (0-1)',
         fr: 'Volume du son (0-1)',
+        cn: '播放声音音量（0-1）',
       },
       type: 'float',
       default: 0.5,
@@ -43,6 +47,7 @@ UserConfig.registerOptions('radar', {
       name: {
         en: 'Show puller of mob',
         fr: 'Afficher le puller du mob',
+        cn: '显示目标开怪者',
       },
       type: 'checkbox',
       default: true,
@@ -52,6 +57,7 @@ UserConfig.registerOptions('radar', {
       name: {
         en: 'Show position of mob',
         fr: 'Afficher la position du mob',
+        cn: '显示目标位置',
       },
       type: 'checkbox',
       default: true,
