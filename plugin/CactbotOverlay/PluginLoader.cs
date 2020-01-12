aa92adf80046d0c5caad55d9afd88b5318548926
Adrienne Walker <enne@quisquo.us>
2020-01-11 18:41:36 -0800

    plugin: add AssemblyResolver from ngld OverlayPlugin
    Same custom license as rainbowmage overlayplugin.
    
    This was not exposed publicly so is just copied and pasted.
    
    This allows the CactbotEventSource.dll to be loaded from the same
    directory as CactbotOverlay.dll.  Also, reading dlls into memory
    makes it possible to overwrite these dlls on disk when upgrading versions.

--- plugin/CactbotOverlay/PluginLoader.cs
+++ plugin/CactbotOverlay/PluginLoader.cs
@@ -1,12 +1,16 @@
 ﻿using System.Windows.Forms;
 using RainbowMage.OverlayPlugin;
 using Advanced_Combat_Tracker;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System;
 
 namespace Cactbot
 {
     public class PluginLoader : IActPluginV1, IOverlayAddonV2
     {
-        public static string pluginPath = "";
+        static AssemblyResolver asmResolver;
 
         public void DeInitPlugin()
         {
@@ -15,24 +19,32 @@ namespace Cactbot
 
         public void InitPlugin(TabPage pluginScreenSpace, Label pluginStatusText)
         {
+            if (asmResolver == null) {
+                asmResolver = new AssemblyResolver(new List<string>{GetPluginDirectory()});
+            }
+
             pluginStatusText.Text = "Ready.";
 
             // We don't need a tab here.
             ((TabControl)pluginScreenSpace.Parent).TabPages.Remove(pluginScreenSpace);
-
-            foreach (var plugin in ActGlobals.oFormActMain.ActPlugins)
-            {
-                if (plugin.pluginObj == this)
-                {
-                    pluginPath = plugin.pluginFile.FullName;
-                    break;
-                }
-            }
         }
 
         public void Init()
         {
             Registry.RegisterEventSource<CactbotEventSource>();
+        }
+
+        private string GetPluginDirectory()
+        {
+            var plugin = ActGlobals.oFormActMain.ActPlugins.Where(x => x.pluginObj == this).FirstOrDefault();
+            if (plugin != null)
+            {
+                return Path.GetDirectoryName(plugin.pluginFile.FullName);
+            }
+            else
+            {
+                throw new Exception("Could not find ourselves in the plugin list!");
+            }
         }
     }
 }
