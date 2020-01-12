b7a7df6507351c989f5077aafd2412ad55818fe9
Adrienne Walker <enne@quisquo.us>
2020-01-11 18:11:42 -0800

    plugin: add empty CactbotEventSource project

---
+++ plugin/CactbotEventSource/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CactbotEventSource")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CactbotEventSource")]
+[assembly: AssemblyCopyright("Copyright 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("9eb3ba4d-b285-4445-b88b-2fb59b8bb321")]
+
+// Version:
+// - Major Version
+// - Minor Version
+// - Build Number
+// - Revision
+// GitHub has only 3 version components, so Revision should always be 0.
+// CactbotOverlay and CactbotEventSource version should match.
+[assembly: AssemblyVersion("0.14.1.0")]
+[assembly: AssemblyFileVersion("0.14.1.0")]
