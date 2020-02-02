745ba11e42aa3415d273a4d84c4fbc75d69fd5bb
Adrienne Walker <enne@quisquo.us>
2020-02-01 21:31:40 -0800

    test: refactor timeline tests to pass in triggers

--- test/timeline/test_timeline.js
+++ test/timeline/test_timeline.js
@@ -1,25 +1,34 @@
 'use strict';
 
-// This test loads an individual raidboss timeline and makes sure
-// that timeline.js can parse it without errors.
-
 let fs = require('fs');
 let Timeline = require('../../ui/raidboss/timeline.js');
 
 let exitCode = 0;
 
-let inputFilename = String(process.argv.slice(2));
+let timelineFile = String(process.argv[2]);
+let triggersFile = String(process.argv[3]);
+let timeline = fs.readFileSync(timelineFile) + '';
+let triggers = fs.readFileSync(triggersFile) + '';
 
-let testTimelineFile = function(file) {
-  let contents = fs.readFileSync(file) + '';
-  let t = new Timeline(contents);
-  for (let i = 0; i < t.errors.length; ++i) {
-    let e = t.errors[i];
-    console.error(file + ':' + e.lineNumber + ': ' + e.error + ': ' + e.line);
-    exitCode = 1;
-  }
+let errorFunc = (str) => {
+  console.error(str);
+  exitCode = 1;
 };
 
-testTimelineFile(inputFilename);
+let tests = {
+  // This test loads an individual raidboss timeline and makes sure
+  // that timeline.js can parse it without errors.
+  timelineErrorTest: () => {
+    let t = new Timeline(timeline);
+    for (let i = 0; i < t.errors.length; ++i) {
+      let e = t.errors[i];
+      errorFunc(timelineFile + ':' + e.lineNumber + ': ' + e.error + ': ' + e.line);
+    }
+  },
+};
+
+for (let name in tests)
+  tests[name]();
+
 
 process.exit(exitCode);
