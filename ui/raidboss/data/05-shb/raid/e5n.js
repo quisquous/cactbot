e83ef15983d0b85bb95376546ba738ebda403fb6
Jaehyuk-Lee <alex00728@outlook.com>
2020-02-20 01:07:32 +0900

    Raidboss: base work for v5.2 (#1052)

---
+++ ui/raidboss/data/05-shb/raid/e5n.js
@@ -0,0 +1,13 @@
+'use strict';
+
+[{
+  zoneRegex: {
+    en: /^Eden's Verse: Fulmination$/,
+    ko: /^희망의 낙원 에덴: 공명편 \(1\)$/,
+  },
+  timelineFile: 'e5n.txt',
+  triggers: [
+  ],
+  timelineReplace: [
+  ],
+}];
