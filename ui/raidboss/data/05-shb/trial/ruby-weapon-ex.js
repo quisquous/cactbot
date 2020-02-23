332e16e34fe31c616a9a9416a9ccc133302ab266
Panic Stevenson <panic.stevenson@gmail.com>
2020-02-23 02:02:50 -0800

    Updates

--- ui/raidboss/data/05-shb/trial/ruby-weapon-ex.js
+++ ui/raidboss/data/05-shb/trial/ruby-weapon-ex.js
@@ -20,7 +20,7 @@
       condition: function(data) {
         return data.role == 'tank' || data.role == 'healer';
       },
-      response: Responses.tankBuster(),
+      response: Responses.tankBusterSwap(),
     },
     {
       id: 'RubyEx Undermine',
@@ -29,8 +29,8 @@
         data.rubyCounter = data.rubyCounter || 0;
         data.rubyCounter++;
       },
-      alertText: {
-        en: 'Away From Lines',
+      infoText: {
+        en: 'Away from Lines',
       },
     },
     {
@@ -40,6 +40,7 @@
         data.rubyCounter = data.rubyCounter || 0;
         data.rubyCounter++;
       },
+      // Ignore Liquefaction casts during dashes
       condition: function(data) {
         return data.rubyCounter % 2;
       },
@@ -50,29 +51,25 @@
     {
       id: 'RubyEx Ruby Ray',
       regex: Regexes.startsUsing({ source: 'The Ruby Weapon', id: '4B02', capture: false }),
-      alertText: {
-        en: 'Away From Front',
-      },
+      response: Responses.outOfFront()
     },
     {
       id: 'RubyEx Cut And Run',
       regex: Regexes.startsUsing({ source: 'The Ruby Weapon', id: '4B05', capture: false }),
-      infoText: {
-        en: 'Go Sides',
-      },
+      response: Responses.goSides(),
     },
     {
       id: 'RubyEx High-Powered Homing Lasers',
       regex: Regexes.startsUsing({ source: 'The Ruby Weapon', id: '4AD8' }),
       condition: Conditions.targetIsYou(),
-      infoText: {
-        en: 'Stack on YOU',
-      },
+      response: Responses.stackOn(),
     },
     {
-      id: 'RubyEx Optimized Ultima',
+      // Enrage can start casting before Ruby Weapon has finished their rotation
+      // Give a friendly reminder to pop LB3 if you haven't already
+      id: 'RubyEx Optimized Ultima Enrage',
       regex: Regexes.startsUsing({ source: 'The Ruby Weapon', id: '4B2D', capture: false }),
-      alertText: {
+      infoText: {
         en: 'Enrage!',
       },
     },
@@ -80,25 +77,29 @@
       id: 'RubyEx Pall of Rage',
       regex: Regexes.gainsEffect({ effect: 'Pall of Rage' }),
       condition: Conditions.targetIsYou(),
+      preRun: function(data) {
+        data.color = 'blue';
+      },
       infoText: {
-        en: 'Attack Blue',
+        en: 'Attack Blue (East)',
       },
     },
     {
       id: 'RubyEx Pall of Grief',
       regex: Regexes.gainsEffect({ effect: 'Pall of Grief' }),
       condition: Conditions.targetIsYou(),
+      preRun: function(data) {
+        data.color = 'red';
+      },
       infoText: {
-        en: 'Attack Red',
+        en: 'Attack Red (West)',
       },
     },
     {
       id: 'RubyEx Meteor Stream',
       regex: Regexes.headMarker({ id: '00E0' }),
       condition: Conditions.targetIsYou(),
-      infoText: {
-        en: 'Spread on YOU',
-      },
+      response: Responses.spread(),
     },
     {
       id: 'RubyEx Ruby Claw',
@@ -107,16 +108,37 @@
         return data.role == 'healer' || data.role == 'tank';
       },
       suppressSeconds: 1,
-      infoText: {
-        en: 'Tankbusters',
+      response: Responses.tankBuster(),
+    },
+    {
+      id: 'RubyEx Change of Heart',
+      regex: Regexes.ability({ source: 'The Ruby Weapon', id: '4AFC', capture: false }),
+      preRun: function(data) {
+        if (data.color == 'red')
+          data.color = 'blue';
+        else
+          data.color = 'red';
       },
+      infoText: function(data) {
+        if (data.color == 'red') {
+          return {
+            en: 'Attack Red (West)',
+          };
+        }
+        return {
+          en: 'Attack Blue (East)',
+        };
+      },
+    },
+    {
+      id: 'RubyEx Negative Aura',
+      regex: Regexes.startsUsing({ source: 'The Ruby Weapon', id: '4AFE', capture: false }),
+      response: Responses.lookAway(),
     },
     {
       id: 'RubyEx Screech',
       regex: Regexes.startsUsing({ source: 'The Ruby Weapon', id: '4AEE', capture: false }),
-      infoText: {
-        en: 'Knockback',
-      },
+      response: Responses.knockback(),
     },
     {
       id: 'RubyEx Outrage',
