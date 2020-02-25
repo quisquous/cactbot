80e198bb312e538d9f8c26cabc78d5c4e25cb1ca
Panic Stevenson <panic.stevenson@gmail.com>
2020-02-24 23:13:27 -0800

    raidboss: Initial E6S Triggers (#1092)
    Adding some triggers; probably not overly helpful without video
    reference to decipher things like Ifrit locations or Ferostorm
    positioning.

--- ui/raidboss/data/05-shb/raid/e6s.js
+++ ui/raidboss/data/05-shb/raid/e6s.js
@@ -7,6 +7,157 @@
   },
   timelineFile: 'e6s.txt',
   triggers: [
+    {
+      id: 'E6S Superstorm',
+      regex: Regexes.startsUsing({ source: 'Garuda', id: '4BF7', capture: false }),
+      condition: Conditions.caresAboutMagical(),
+      response: Responses.aoe(),
+      run: function(data) {
+        data.phase = 'garuda';
+      },
+    },
+    {
+      id: 'E6S Air Bump',
+      regex: Regexes.headMarker({ id: '00D3' }),
+      suppressSeconds: 1,
+      infoText: function(data, matches) {
+        if (data.me == matches.target) {
+          return {
+            en: 'Enumeration on YOU',
+          };
+        }
+        return {
+          en: 'Enumeration',
+        };
+      },
+    },
+    {
+      id: 'E6S Touchdown',
+      regex: Regexes.startsUsing({ source: 'Ifrit', id: '4C09', capture: false }),
+      run: function(data) {
+        data.phase = 'ifrit';
+      },
+    },
+    {
+      id: 'E6S Inferno Howl',
+      regex: Regexes.startsUsing({ source: ['Ifrit', 'Raktapaksa'], id: '4C14', capture: false }),
+      condition: Conditions.caresAboutMagical(),
+      response: Responses.aoe(),
+    },
+    {
+      // Save ability state since the generic tether used has multiple uses in this fight
+      id: 'E6S Hands of Flame Start',
+      regex: Regexes.startsUsing({ source: ['Ifrit', 'Raktapaksa'], id: '4D00', capture: false }),
+      preRun: function(data) {
+        data.handsOfFlame = true;
+      },
+    },
+    {
+      // Tank swap if you're not the target
+      // Break tether if you're the target during Ifrit+Garuda phase
+      id: 'E6S Hands of Flame Tether',
+      regex: Regexes.tether({ id: '0068' }),
+      condition: function(data) {
+        return data.handsOfFlame = true;
+      },
+      infoText: function(data, matches) {
+        if (data.me == matches.target) {
+          return {
+            en: 'Charge on YOU',
+          };
+        }
+        if (data.role != 'tank' || data.phase == 'both')
+          return;
+        return {
+          en: 'Tank Swap',
+        };
+      },
+    },
+    {
+      id: 'E6S Hands of Flame Cast',
+      regex: Regexes.ability({ source: ['Ifrit', 'Raktapaksa'], id: '4D00', capture: false }),
+      suppressSeconds: 1,
+      preRun: function(data) {
+        data.handsOfFlame = false;
+      },
+    },
+    {
+      id: 'E6S Instant Incineration',
+      regex: Regexes.startsUsing({ source: ['Ifrit', 'Raktapaksa'], id: '4C0E' }),
+      condition: Conditions.caresAboutMagical(),
+      response: Responses.tankBuster(),
+    },
+    {
+      id: 'E6S Meteor Strike',
+      regex: Regexes.startsUsing({ source: ['Ifrit', 'Raktapaksa'], id: '4C0F', capture: false }),
+      response: Responses.awayFromFront(),
+    },
+    {
+      id: 'E6S Hands of Hell',
+      regex: Regexes.headMarker({ id: '0016' }),
+      condition: Conditions.targetIsYou(),
+      alertText: {
+        en: 'Tether Marker on YOU',
+      },
+    },
+    {
+      id: 'E6S Hated of the Vortex',
+      regex: Regexes.startsUsing({ source: 'Garuda', id: '4F9F', capture: false }),
+      run: function(data) {
+        data.phase = 'both';
+      },
+    },
+    {
+      id: 'E6S Hated of the Vortex Effect',
+      regex: Regexes.gainsEffect({ effect: 'Hated of the Vortex' }),
+      condition: Conditions.targetIsYou(),
+      infoText: {
+        en: 'Attack Garuda',
+      },
+    },
+    {
+      id: 'E6S Hated of the Embers Effect',
+      regex: Regexes.gainsEffect({ effect: 'Hated of the Embers' }),
+      condition: Conditions.targetIsYou(),
+      infoText: {
+        en: 'Attack Ifrit',
+      },
+    },
+    {
+      id: 'E6S Raktapaksa Spawn',
+      regex: Regexes.ability({ source: 'Raktapaksa', id: '4D55', capture: false }),
+      run: function(data) {
+        data.phase = 'raktapaksa';
+      },
+    },
+    {
+      id: 'E6S Conflag Strike Knockback',
+      regex: Regexes.startsUsing({ source: 'Raktapaksa', id: '4C10', capture: false }),
+      response: Responses.knockback(),
+    },
+    {
+      id: 'E6S Irons Of Purgatory',
+      regex: Regexes.tether({ id: '006C' }),
+      condition: function(data, matches) {
+        return data.me == matches.target || data.me == matches.source;
+      },
+      alertText: function(data, matches) {
+        if (data.me == matches.source) {
+          return {
+            en: 'Tethered to ' + matches.target,
+          };
+        }
+        return {
+          en: 'Tethered to ' + matches.source,
+        };
+      },
+    },
+    {
+      id: 'E6S Conflag Strike Behind',
+      regex: Regexes.startsUsing({ source: 'Raktapaksa', id: '4C10', capture: false }),
+      delaySeconds: 31,
+      response: Responses.getBehind(),
+    },
   ],
   timelineReplace: [
   ],
