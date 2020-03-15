4296f36914e5ee25d0e02a1820f22b0637960362
呆萌 <admin@diemoe.net>
2020-03-15 17:55:02 +0800

    misc: update CN translation,anti-jamming regex

--- resources/responses.js
+++ resources/responses.js
@@ -232,6 +232,7 @@ let Responses = {
       de: 'Kleiner Tankbuster',
       fr: 'Mini Buster',
       ja: 'ミニバスター',
+      cn: '小死刑',
       ko: '약한 탱버',
     };
     return obj;
@@ -495,7 +496,7 @@ let Responses = {
       de: 'Raus aus Nahkampf',
       fr: 'Eloignez-vous du CaC',
       ja: '近接最大レンジ',
-      cn: '远离近战',
+      cn: '近战最远距离回避',// '远离近战' is an inaccurate translation 
       ko: '근접범위 밖으로',
     };
     return obj;
@@ -530,6 +531,7 @@ let Responses = {
       en: 'go into middle',
       fr: 'Allez au milieu',
       de: 'in die Mitte gehen',
+      cn: '去中间',
     };
     return obj;
   },
@@ -821,6 +823,7 @@ let Responses = {
         de: 'Erdstoß auf DIR',
         fr: 'Marque de terre sur VOUS',
         ja: '自分にアースシェイカー',
+        cn: '大地摇动点名',
         ko: '어스징 대상자',
 
       };
