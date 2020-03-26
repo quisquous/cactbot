7f22362b64a0806e8fb59775e6014e63ce611323
VanSlanzar <61565834+VanSlanzar@users.noreply.github.com>
2020-03-26 15:50:41 +0100

    Update a1s.js

--- ui/raidboss/data/03-hw/raid/a1s.js
+++ ui/raidboss/data/03-hw/raid/a1s.js
@@ -134,6 +134,8 @@
         'Gunnery Pod': 'Feu d\'artillerie',
         'Hydrothermal Missile': 'Missile hydrothermique',
         'Hypercompressed Plasma': 'Plasma hypercomprimé',
+        'Kaltstrahl': 'Kaltstrahl'
+        'Kaltstrahl Enrage': 'Kaltstrahl Enrage'
         'Missile Impact': 'Frappe de missile',
         'Photon Spaser': 'Spaser à photons',
         'Pressure Increase': 'Hausse de pression',
