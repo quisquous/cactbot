1be1347064bc38ec4a8b5f350b1625395bb1185b
Kyle Buller <bullerk@gmail.com>
2020-03-26 21:40:16 -0500

    raidboss: e7s tornado spot callouts based on color (#1283)

--- ui/raidboss/data/05-shb/raid/e7s.js
+++ ui/raidboss/data/05-shb/raid/e7s.js
@@ -6,28 +6,6 @@
     ko: /^희망의 낙원 에덴: 공명편\(영웅\) \(3\)$/,
   },
   timelineFile: 'e7s.txt',
-  timelineTriggers: [
-    {
-      // TODO: it might be nice to call out spots directly once people have a buff.
-      id: 'E7S Tornado Spots',
-      regex: /Words Of Fervor/,
-      beforeSeconds: 10,
-      infoText: {
-        en: 'Dark NE, Light NW, one pair S',
-        cn: '黑右前 白左前 两人后',
-        de: 'Dunkel NO, Licht NW, ein Paar S',
-        fr: 'Noir NE, Blanc NO, une paire S',
-        ko: '어둠 북동, 빛 북서, 한쌍은 남으로',
-      },
-      // Some tts users complained that this was way too long.
-      tts: {
-        en: 'Dark Northeast',
-        cn: '黑右前',
-        de: 'Dunkel Nordosten',
-        fr: 'Noir Nord-Est',
-      },
-    },
-  ],
   triggers: [
     {
       id: 'E7S Empty Wave',
@@ -493,6 +471,30 @@
         de: 'Lass dich in die Ecke zurückstoßen',
         fr: 'Faites-vous pousser dans les coins',
         ko: '구석으로 넉백',
+      },
+    },
+    {
+      id: 'E7S Unjoined Aspect P3',
+      regex: Regexes.ability({ source: 'The Idol Of Darkness', id: '4C7A', capture: false }),
+      regexDe: Regexes.ability({ source: 'Götzenbild Der Dunkelheit', id: '4C7A', capture: false }),
+      regexFr: Regexes.ability({ source: 'Idole Des Ténèbres', id: '4C7A', capture: false }),
+      regexJa: Regexes.ability({ source: 'ダークアイドル', id: '4C7A', capture: false }),
+      // Color buffs go out immediately after the cast
+      delaySeconds: 0.1,
+      infoText: function(data) {
+        if (data.role == 'tank') {
+          return {
+            en: 'Go South',
+          };
+        }
+        if (data.color == 'light') {
+          return {
+            en: 'Go Northwest',
+          };
+        }
+        return {
+          en: 'Go Northeast',
+        };
       },
     },
     {
