c46decbc31255a6034686ef7f18695da825ac6b1
VanSlanzar <61565834+VanSlanzar@users.noreply.github.com>
2020-04-03 03:47:39 +0200

    Raidboss: A2S fr (#1288)

--- ui/raidboss/data/03-hw/raid/a2s.js
+++ ui/raidboss/data/03-hw/raid/a2s.js
@@ -17,10 +17,12 @@
     {
       id: 'A2S Breakblock',
       regex: /(?:Brainhurt|Bodyhurt) Breakblock/,
+      regexFr: /(?:Spirituel|Corporel) Blindage/,
       beforeSeconds: 10,
       suppressSeconds: 1,
       infoText: {
         en: 'Stun Soon',
+        fr: 'Stun bientôt',
       },
     },
   ],
@@ -28,13 +30,16 @@
     {
       id: 'A2S Bomb',
       regex: Regexes.addedCombatant({ name: 'Bomb', capture: false }),
+      regexFr: Regexes.addedCombatant({ name: 'Bombe', capture: false }),
       alertText: {
         en: 'Bomb',
+        fr: 'Bombe',
       },
     },
     {
       id: 'A2S Prey',
       regex: Regexes.ability({ source: 'Magitek Gobwidow G-IX', id: '1413' }),
+      regexFr: Regexes.ability({ source: 'Gobmygale Magitek G-IX', id: '1413' }),
       condition: function(data) {
         return data.role == 'healer' || data.job == 'blu';
       },
@@ -42,21 +47,25 @@
       infoText: function(data, matches) {
         return {
           en: 'Keep ' + data.ShortName(matches.target) + ' topped',
+          fr: 'Maintenez ' + data.ShortName(matches.target) + ' Max PV',
         };
       },
     },
     {
       id: 'A2S Prey You',
       regex: Regexes.ability({ source: 'Magitek Gobwidow G-IX', id: '1413' }),
+      regexFr: Regexes.ability({ source: 'Gobmygale Magitek G-IX', id: '1413' }),
       condition: Conditions.targetIsYou(),
       suppressSeconds: 10,
       alertText: {
         en: 'Boomcannon on YOU',
+        fr: 'Double cannon sur VOUS',
       },
     },
     {
       id: 'A2S Soldier Spawn',
       regex: Regexes.addedCombatant({ name: 'Gordian Soldier', capture: false }),
+      regexFr: Regexes.addedCombatant({ name: 'Soldat Gordien', capture: false }),
       run: function(data) {
         delete data.bangyzoom;
       },
@@ -64,14 +73,55 @@
     {
       id: 'A2S Bangyzoom',
       regex: Regexes.ability({ id: 'FD9', target: 'Gordian Soldier', capture: false }),
+      regexFr: Regexes.ability({ id: 'FD9', target: 'Soldat Gordien', capture: false }),
       condition: function(data) {
         return !data.bangyzoom;
       },
       infoText: {
         en: 'Stun Soldier',
+        fr: 'Stun sur le soldat',
       },
       run: function(data) {
         data.bangyzoom = true;
+      },
+    },
+  ],
+  timelineReplace: [
+    {
+      'locale': 'fr',
+      'replaceSync': {
+        'Boomtype Magitek Gobwalker G-VII': 'Gobblindé magitek G-VII Boumbardier',
+        'Giant Bomb': 'Bombe géante',
+        'Gordian Hardhelm': 'Casque-dur gordien',
+        'Gordian Hardmind': 'Cerveau-dur gordien',
+        'Gordian Soldier': 'Soldat gordien',
+        'Gordian Sniper': 'Sniper gordien',
+        'Jagd Doll': 'Poupée jagd',
+        'King Gobtank G-IV': 'Gobtank G-IV roi',
+        'Magitek Gobwidow G-IX': 'Gobmygale magitek G-IX',
+      },
+      'replaceText': {
+        'mid': 'milieu',
+        'NW': 'NO',
+        'SW': 'SO',
+        'Bangyzoom': 'Ordre d\'attaque',
+        'Blitzstrahl': 'Blitzstrahl',
+        'Bodyhurt Breakblock': 'Blindage corporel',
+        'Brainhurt Breakblock': 'Blindage spirituel',
+        'Boomcannon': 'Double cannon',
+        'Carpet Bomb': 'Tapis de bombes',
+        'Explosion': 'Explosion',
+        'Hardhelm': 'Casque-dur',
+        'Hardmind': 'Cerveau-dur',
+        'Giant Bomb': 'Bombe géante',
+        'Gobwalker': 'Gobblindé',
+        'Gobwidow': 'Gobmygale',
+        'Jagd Doll': 'Poupée jagd',
+        'Kaltstrahl': 'Kaltstrahl',
+        'Massive Explosion Enrage': 'Grosse Explosion Enrage',
+        'Sniper': 'Sniper',
+        'Soldier': 'Soldat',
+        'Wave': 'Vague',
       },
     },
   ],
