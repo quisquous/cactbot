8d08d665f1f33dcbe5bc80a8fa9cf3c0424d68b6
Akurosia <kamothegamer@web.de>
2020-04-07 23:41:46 +0200

    all language regex + german translation for a9s

--- ui/raidboss/data/03-hw/raid/a9s.js
+++ ui/raidboss/data/03-hw/raid/a9s.js
@@ -20,18 +20,22 @@
       infoText: function(data) {
         let nw1_se1 = {
           en: 'Place Generators NW/SE',
+          de: 'Plaziere Generatoren NW/SO',
           cn: '搬运发电器到西北/东南',
         };
         let nw2 = {
           en: 'Place Generators NW',
+          de: 'Plaziere Generatoren NW',
           cn: '搬运发电器到西北',
         };
         let nw1 = {
           en: 'Place Generator NW',
+          de: 'Plaziere Generator NW',
           cn: '搬运发电器到西北',
         };
         let se2 = {
           en: 'Place Generators SE',
+          de: 'Plaziere Generatoren SO',
           cn: '搬运发电器到东南',
         };
 
@@ -55,11 +59,13 @@
         // .. or anywhere not NW
         let se = {
           en: 'Kill Alarum SE',
+          de: 'SO Alarm besiegen',
           cn: '在东南击杀警报',
         };
         // ... or anywhere not NW/SE
         let sw = {
           en: 'Kill Alarum SW',
+          de: 'SW Alarm besiegen',
           cn: '在西南击杀警报',
         };
 
@@ -77,6 +83,7 @@
       beforeSeconds: 7,
       infoText: {
         en: 'Bombs Soon',
+        de: 'Bomben bald',
         cn: '炸弹马上爆炸',
       },
     },
@@ -85,6 +92,11 @@
     {
       id: 'A9S Stockpile Count',
       regex: Regexes.startsUsing({ source: 'Refurbisher 0', id: '1A38', capture: false }),
+      regexDe: Regexes.startsUsing({ source: 'Rekompositor', id: '1A38', capture: false }),
+      regexFr: Regexes.startsUsing({ source: 'Récupérateur', id: '1A38', capture: false }),
+      regexJa: Regexes.startsUsing({ source: 'リファビッシャー', id: '1A38', capture: false }),
+      regexCn: Regexes.startsUsing({ source: '废品翻新装置', id: '1A38', capture: false }),
+      regexKo: Regexes.startsUsing({ source: '재생자', id: '1A38', capture: false }),
       run: function(data) {
         data.stockpileCount = data.stockpileCount || 0;
         data.stockpileCount++;
@@ -93,6 +105,11 @@
     {
       id: 'A9S Scrapline',
       regex: Regexes.startsUsing({ source: 'Refurbisher 0', id: '1A3C', capture: false }),
+      regexDe: Regexes.startsUsing({ source: 'Rekompositor', id: '1A3C', capture: false }),
+      regexFr: Regexes.startsUsing({ source: 'Récupérateur', id: '1A3C', capture: false }),
+      regexJa: Regexes.startsUsing({ source: 'リファビッシャー', id: '1A3C', capture: false }),
+      regexCn: Regexes.startsUsing({ source: '废品翻新装置', id: '1A3C', capture: false }),
+      regexKo: Regexes.startsUsing({ source: '재생자', id: '1A3C', capture: false }),
       alertText: function(data) {
         if (data.mainTank == data.me)
           return;
@@ -110,6 +127,7 @@
           return;
         return {
           en: 'Scrapline on YOU',
+          de: 'Schrottlinie auf DIR',
           cn: '死刑',
         };
         // ...probably, we hope...
@@ -118,8 +136,14 @@
     {
       id: 'A9S Double Scrapline',
       regex: Regexes.startsUsing({ source: 'Refurbisher 0', id: '1A3D', capture: false }),
+      regexDe: Regexes.startsUsing({ source: 'Rekompositor', id: '1A3D', capture: false }),
+      regexFr: Regexes.startsUsing({ source: 'Récupérateur', id: '1A3D', capture: false }),
+      regexJa: Regexes.startsUsing({ source: 'リファビッシャー', id: '1A3D', capture: false }),
+      regexCn: Regexes.startsUsing({ source: '废品翻新装置', id: '1A3D', capture: false }),
+      regexKo: Regexes.startsUsing({ source: '재생자', id: '1A3D', capture: false }),
       alertText: {
         en: 'Stand in Alarum Puddle',
+        de: 'In Alarm Fläche stehen',
         cn: '站进紫色圈圈',
       },
     },
@@ -129,6 +153,7 @@
       condition: Conditions.targetIsYou(),
       infoText: {
         en: 'Rock on YOU',
+        de: 'Stein auf DIR',
         cn: '落石点名',
       },
     },
@@ -139,6 +164,7 @@
       suppressSeconds: 1,
       alertText: {
         en: 'Hide Fully Behind Rock',
+        de: 'Komplett hinter dem Stein verstecken',
         cn: '躲在石头后',
       },
     },
@@ -158,6 +184,11 @@
     {
       id: 'A9S Auto',
       regex: Regexes.ability({ source: 'Refurbisher 0', id: '1AFE' }),
+      regexDe: Regexes.ability({ source: 'Rekompositor', id: '1AFE' }),
+      regexFr: Regexes.ability({ source: 'Récupérateur', id: '1AFE' }),
+      regexJa: Regexes.ability({ source: 'リファビッシャー', id: '1AFE' }),
+      regexCn: Regexes.ability({ source: '废品翻新装置', id: '1AFE' }),
+      regexKo: Regexes.ability({ source: '재생자', id: '1AFE' }),
       run: function(data, matches) {
         data.mainTank = matches.target;
       },
@@ -165,24 +196,29 @@
     {
       id: 'A9S Power Generator Add Tether',
       regex: Regexes.tether({ id: '0011', capture: false }),
+
       suppressSeconds: 30,
       infoText: function(data) {
         // Some of the last phases have multiple options.
         // This is an old fight, so just pick one for people.
         let ne = {
           en: 'Adds to NE Lava',
+          de: 'Adds in NO Lava',
           cn: '拉小怪到东北击杀',
         };
         let se = {
           en: 'Adds to SE Lava',
+          de: 'Adds in SO Lava',
           cn: '拉小怪到东南击杀',
         };
         let sw = {
           en: 'Adds to SW Lava',
+          de: 'Adds in SW Lava',
           cn: '拉小怪到西南击杀',
         };
         let nw = {
           en: 'Adds to NW Lava',
+          de: 'Adds in NW Lava',
           cn: '拉小怪到西北击杀',
         };
 
