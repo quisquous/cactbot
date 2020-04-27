f90b5ca7d8a69a88d890ae911175a4472b34849b
VanSlanzar <61565834+VanSlanzar@users.noreply.github.com>
2020-04-27 13:57:01 +0200

    Correction general.js

--- ui/oopsyraidsy/data/00-misc/general.js
+++ ui/oopsyraidsy/data/00-misc/general.js
@@ -27,10 +27,10 @@
           blame: e.targetName,
           text: {
             en: 'lost food buff',
-            fr: 'Buff nourriture terminée',
             de: 'Nahrungsbuff verloren',
-            ko: '음식 버프 해제',
+            fr: 'Buff nourriture terminée',
             cn: '失去食物BUFF',
+            ko: '음식 버프 해제',
           },
         };
       },
@@ -56,8 +56,8 @@
           text: {
             en: 'bunny',
             de: 'Hase',
-            fr: e.abilityName,
-            ja: e.abilityName,
+            fr: 'lapin',
+            ja: 'ウサギ',
             ko: '토끼',
             cn: '兔子',
           },
