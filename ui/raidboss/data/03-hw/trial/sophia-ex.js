0db4c78b51d07038d2b2b2cbc2934af58af297c6
Jonathan Garber <linkthevaliant@gmail.com>
2020-05-03 12:49:06 -0400

    Added missing translations

--- ui/raidboss/data/03-hw/trial/sophia-ex.js
+++ ui/raidboss/data/03-hw/trial/sophia-ex.js
@@ -12,13 +12,14 @@
     {
       'locale': 'de',
       'replaceSync': {
-        'Aion Teleos': 'Aion Teleos', // FIXME
-        'The Second Demiurge': 'The Second Demiurge', // FIXME
-        'Sophia': 'Sophia', // FIXME
+        'Aion Teleos': 'Aion Teleos',
+        'Barbelo': 'Barbelo',
+        'Sophia': 'Sophia',
+        'The First Demiurge': 'Erst[a] Demiurg',
+        'The Second Demiurge': 'Zweit[a] Demiurg',
+        'The Third Demiurge': 'Dritt[a] Demiurg',
       },
       'replaceText': {
-        'Add Tethers': 'Add Tethers', // FIXME
-        'Adds Spawn': 'Adds Spawn', // FIXME
         'Aero III': 'Windga',
         'Arms of Wisdom': 'Arme der Weisheit',
         'Cintamani': 'Chintamani',
@@ -29,144 +30,153 @@
         'Execute': 'Exekutieren',
         'Gnosis': 'Gnosis',
         'Gnostic Rant': 'Gnostischer Gegenlauf',
+        'Gnostic Spear': 'Gnostischer Speer',
         'Infusion': 'Schneisenschläger',
+        'Kenoma': 'Kenoma',
+        'Light Dew': 'Lichttau',
         'Onrush': 'Heranstürmen',
         'Quasar': 'Quasar',
-        'Scale Tethers': 'Scale Tethers', // FIXME
-        'Scales of Wisdom': 'Scales of Wisdom', // FIXME
-        'Shadows Spawn': 'Shadows Spawn', // FIXME
+        'Ring of Pain': 'Ring des Schmerzes',
+        'The Scales Of Wisdom': 'Waage der Weisheit',
         'Thunder(?! )': 'Blitz',
         'Thunder II(?!I)': 'Blitzra',
         'Thunder III': 'Blitzga',
-        'Tilt': 'Tilt', // FIXME
+        '--Clones Appear--': '--Klone erscheinen--',
       },
     },
     {
       'locale': 'fr',
       'replaceSync': {
-        'Aion Teleos': 'Aion Teleos', // FIXME
-        'The Second Demiurge': 'The Second Demiurge', // FIXME
-        'Sophia': 'Sophia', // FIXME
+        'Aion Teleos': 'Aion Teleos',
+        'Barbelo': 'Barbelo',
+        'Sophia': 'Sophia',
+        'The First Demiurge': 'Premier Démiurge',
+        'The Second Demiurge': 'Second Démiurge',
+        'The Third Demiurge': 'Troisième Démiurge',
       },
       'replaceText': {
-        'Add Tethers': 'Add Tethers', // FIXME
-        'Adds Spawn': 'Adds Spawn', // FIXME
         'Aero III': 'Méga Vent',
         'Arms of Wisdom': 'Bras de la sagesse',
         'Cintamani': 'Chintamani',
         'Cloudy Heavens': 'Ciel nébuleux',
-        'Dischordant Damnation': 'Damnation discordante',
+        'Dischordant Cleansing': 'Purification Discordante',
         'Divine Spark': 'Étincelle divine',
         'Duplicate': 'Duplication',
         'Execute': 'Exécution',
         'Gnosis': 'Gnose',
         'Gnostic Rant': 'Diatribe gnostique',
+        'Gnostic Spear': 'Épieu Gnostique',
         'Infusion': 'Infusion',
+        'Kenoma': 'Kenoma',
+        'Light Dew': 'Rosée De Lumière',
         'Onrush': 'Charge',
         'Quasar': 'Quasar',
-        'Scale Tethers': 'Scale Tethers', // FIXME
-        'Scales of Wisdom': 'Scales of Wisdom', // FIXME
-        'Shadows Spawn': 'Shadows Spawn', // FIXME
-        'Thunder(?! )': 'Foudre',
+        'Ring of Pain': 'Anneau de douleur',
         'Thunder II(?!I)': 'Extra Foudre',
         'Thunder III': 'Méga Foudre',
-        'Tilt': 'Tilt', // FIXME
+        '--Clones Appear--': '--Apparition des clones--',
       },
     },
     {
       'locale': 'ja',
       'replaceSync': {
-        'Aion Teleos': 'Aion Teleos', // FIXME
-        'The Second Demiurge': 'The Second Demiurge', // FIXME
-        'Sophia': 'Sophia', // FIXME
+        'Aion Teleos': 'Aion Teleos',
+        'Barbelo': 'バルベロ',
+        'Sophia': 'ソフィア',
+        'The First Demiurge': '一の従者',
+        'The Second Demiurge': '二の従者',
+        'The Third Demiurge': '三の従者',
       },
       'replaceText': {
-        'Add Tethers': 'Add Tethers', // FIXME
-        'Adds Spawn': 'Adds Spawn', // FIXME
         'Aero III': 'エアロガ',
         'Arms of Wisdom': 'ウィズダムアームズ',
         'Cintamani': 'チンターマニ',
         'Cloudy Heavens': 'クラウディヘヴン',
-        'Dischordant Damnation': '不調和の神罰',
+        'Dischordant Cleansing': '不調和の罰',
         'Divine Spark': '熱いまなざし',
         'Duplicate': 'デュプリケイト',
         'Execute': 'エクセキュート',
         'Gnosis': 'グノーシス',
         'Gnostic Rant': '魔槍乱舞',
+        'Gnostic Spear': '魔槍突き',
         'Infusion': '猛突進',
+        'Kenoma': '堅守',
+        'Light Dew': 'ライトデュー',
         'Onrush': 'オンラッシュ',
         'Quasar': 'クエーサー',
-        'Scale Tethers': 'Scale Tethers', // FIXME
-        'Scales of Wisdom': 'Scales of Wisdom', // FIXME
-        'Shadows Spawn': 'Shadows Spawn', // FIXME
-        'Thunder(?! )': 'サンダー',
+        'Ring of Pain': 'リング・オブ・ペイン',
+        'The Scales Of Wisdom': 'バランス・オブ・ウィズダム',
         'Thunder II(?!I)': 'サンダラ',
         'Thunder III': 'サンダガ',
-        'Tilt': 'Tilt', // FIXME
+        '--Clones Appear--': '--幻影が現れる--',
       },
     },
     {
       'locale': 'cn',
       'replaceSync': {
-        'Aion Teleos': 'Aion Teleos', // FIXME
-        'The Second Demiurge': 'The Second Demiurge', // FIXME
-        'Sophia': 'Sophia', // FIXME
+        'Aion Teleos': '移涌',
+        'Barbelo': '芭碧萝',
+        'Sophia': '索菲娅',
+        'The First Demiurge': '信徒其一',
+        'The Second Demiurge': '信徒其二',
+        'The Third Demiurge': '信徒其三',
       },
       'replaceText': {
-        'Add Tethers': 'Add Tethers', // FIXME
-        'Adds Spawn': 'Adds Spawn', // FIXME
         'Aero III': '暴风',
         'Arms of Wisdom': '睿智之秤',
         'Cintamani': '如意宝珠',
         'Cloudy Heavens': '阴云天堂',
-        'Dischordant Damnation': '不平衡神罚',
+        'Dischordant Cleansing': '不平衡之罚',
         'Divine Spark': '灼热视线',
         'Duplicate': '复制',
         'Execute': '处决',
         'Gnosis': '灵知',
         'Gnostic Rant': '魔枪乱舞',
+        'Gnostic Spear': '魔枪突刺',
         'Infusion': '猛突进',
+        'Kenoma': 'Kenoma',
+        'Light Dew': '光露',
         'Onrush': '突袭',
-        'Quasar': '', // FIXME
-        'Scale Tethers': 'Scale Tethers', // FIXME
-        'Scales of Wisdom': 'Scales of Wisdom', // FIXME
-        'Shadows Spawn': 'Shadows Spawn', // FIXME
-        'Thunder(?! )': '闪雷',
+        'Quasar': '类星体',
+        'Ring of Pain': '痛苦环刺',
+        'The Scales Of Wisdom': '睿智之天平',
         'Thunder II(?!I)': '震雷',
         'Thunder III': '暴雷',
-        'Tilt': 'Tilt', // FIXME
+        '--Clones Appear--': '--Clones Appear--',
       },
     },
     {
       'locale': 'ko',
       'replaceSync': {
-        'Aion Teleos': 'Aion Teleos', // FIXME
-        'The Second Demiurge': 'The Second Demiurge', // FIXME
-        'Sophia': 'Sophia', // FIXME
+        'Aion Teleos': '아이온 소피아',
+        'Barbelo': '바르벨로',
+        'Sophia': '소피아',
+        'The First Demiurge': '제1신도',
+        'The Second Demiurge': '제2신도',
+        'The Third Demiurge': '제3신도',
       },
       'replaceText': {
-        'Add Tethers': 'Add Tethers', // FIXME
-        'Adds Spawn': 'Adds Spawn', // FIXME
         'Aero III': '에어로가',
         'Arms of Wisdom': '지혜의 무기',
         'Cintamani': '친타마니',
         'Cloudy Heavens': '흐린 낙원',
-        'Dischordant Damnation': '부조화의 신벌',
+        'Dischordant Cleansing': '부조화의 벌',
         'Divine Spark': '뜨거운 시선',
         'Duplicate': '복제',
         'Execute': '이행',
         'Gnosis': '영적 지혜',
         'Gnostic Rant': '마창 난무',
+        'Gnostic Spear': '마창 찌르기',
         'Infusion': '맹돌진',
+        'Kenoma': '견제',
+        'Light Dew': '빛의 이슬',
         'Onrush': '돌진',
-        'Quasar': '', // FIXME
-        'Scale Tethers': 'Scale Tethers', // FIXME
-        'Scales of Wisdom': 'Scales of Wisdom', // FIXME
-        'Shadows Spawn': 'Shadows Spawn', // FIXME
-        'Thunder(?! )': '선더',
+        'Quasar': '퀘이사',
+        'Ring Of Pain': '고통의 소용돌이',
+        'The Scales Of Wisdom': '지혜의 저울',
         'Thunder II(?!I)': '선더라',
         'Thunder III': '선더가',
-        'Tilt': 'Tilt', // FIXME
+        '--Clones Appear--': '--Clones Appear--',
       },
     },
   ],
