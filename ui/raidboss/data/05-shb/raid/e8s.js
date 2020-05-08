94f0009534d115752dcd0b633919c73549b6c94b
VanSlanzar <61565834+VanSlanzar@users.noreply.github.com>
2020-05-08 11:04:58 +0200

    Added space for best jap viewing

--- ui/raidboss/data/05-shb/raid/e8s.js
+++ ui/raidboss/data/05-shb/raid/e8s.js
@@ -969,7 +969,7 @@
         'the Path of Light': '光の波動',
         'the House of Light': '光の津波',
         'Twin Silence/Stillness': '静寂の双剣技 / 静寂の双剣技',
-        'Twin Stillness/Silence': '静寂の双剣技 /静寂の双剣技',
+        'Twin Stillness/Silence': '静寂の双剣技 / 静寂の双剣技',
         'Wyrm\'s Lament': '聖竜の咆哮',
       },
       '~effectNames': {
