quisquous/cactbot

commit 57a26ddfb27bdf2bbd772c120f60c9db4a4c6f22

Regex Capturing and Language additions (#1408)
- Add jobToJobEnum to common.js Util
- Add kLanguage property to lang.js CactbotLanguage class
- Add kPetNames variable to language classes
- Add timestamp/name/time capture groups to language files, adjust jobs.js to accomodate
- Rewrite regexes in language files to use gameLog instead
- Add timestamp capture to Regexes
- Add timestamp capture to NetRegexes
- Add some missing data capture to Regexes for IDs
- Add missing data captures to Regexes.statusEffectExplicit, abilityFull

Co-authored-by: Valarnin <valarnin@users.noreply.github.com>

resources/lang/en.js
@@ -84,20 +84,55 @@ class CactbotLanguageEn extends CactbotLanguage {
       Pull: 'Pull',
     });
 
+    this.kPetNames = Object.freeze([
+      // Pulled from https://xivapi.com/Pet?pretty=true&language=en
+      'Emerald Carbuncle',
+      'Topaz Carbuncle',
+      'Ifrit-Egi',
+      'Titan-Egi',
+      'Garuda-Egi',
+      'Eos',
+      'Selene',
+      'Rook Autoturret',
+      'Bishop Autoturret',
+      'Demi-Bahamut',
+      'Demi-Phoenix',
+      'Seraph',
+      'Moonstone Carbuncle',
+      'Esteem',
+      'Automaton Queen',
+      'Bunshin',
+    ]);
+
     this.countdownStartRegex = function() {
-      return Regexes.parse(/Battle commencing in (\y{Float}) seconds!/);
+      return Regexes.gameLog({
+        capture: true,
+        line: 'Battle commencing in (?<time>\\y{Float}) seconds!',
+      });
     };
     this.countdownEngageRegex = function() {
-      return /:Engage!/;
+      return Regexes.gameLog({
+        capture: true,
+        line: 'Engage!',
+      });
     };
     this.countdownCancelRegex = function() {
-      return /Countdown canceled by /;
+      return Regexes.gameLog({
+        capture: true,
+        line: 'Countdown canceled by (?<name>\\y{Name}).*?',
+      });
     };
     this.areaSealRegex = function() {
-      return /:(.*) will be sealed off in /;
+      return Regexes.gameLog({
+        capture: true,
+        line: '(?<name>\\y{Name}) will be sealed off in (?<time>\\y{Float}) seconds!',
+      });
     };
     this.areaUnsealRegex = function() {
-      return /:(.*) is no longer sealed/;
+      return Regexes.gameLog({
+        capture: true,
+        line: '(?<name>\\y{Name}) is no longer sealed.*?',
+      });
     };
   }
 }

resources/common.js
@@ -61,6 +61,9 @@ let Util = {
   jobEnumToJob: function(id) {
     return kJobEnumToName[id];
   },
+  jobToJobEnum: function(job) {
+    return Object.keys(kJobEnumToName).filter((k) => kJobEnumToName[k] === job).pop();
+  },
   jobToRole: function(job) {
     let role;
     if (job.search(/^(WAR|DRK|PLD|GNB|MRD|GLA)$/) >= 0) {

resources/lang.js
@@ -129,6 +129,15 @@ class CactbotLanguage {
       EurekaPyros: /(Eureka Pyros|Unknown Zone \(31B\))/,
       EurekaHydatos: /(Eureka Hydatos|Unknown Zone \(33B\))/,
     });
+
+    this.kLanguages = Object.freeze([
+      'ja',
+      'en',
+      'de',
+      'fr',
+      'cn',
+      'ko',
+    ]);
   }
 
   InitStrings() {

resources/lang/cn.js
@@ -84,20 +84,55 @@ class CactbotLanguageCn extends CactbotLanguage {
       Pull: '开怪',
     });
 
+    this.kPetNames = Object.freeze([
+      // Pulled from http://cafemaker.wakingsands.com/Pet?pretty=true
+      '绿宝石兽',
+      '黄宝石兽',
+      '伊弗利特之灵',
+      '泰坦之灵',
+      '迦楼罗之灵',
+      '朝日小仙女',
+      '夕月小仙女',
+      '车式浮空炮塔',
+      '象式浮空炮塔',
+      '亚灵神巴哈姆特',
+      '亚灵神不死鸟',
+      '炽天使',
+      '月长宝石兽',
+      '英雄的掠影',
+      '后式自走人偶',
+      '分身',
+    ]);
+
     this.countdownStartRegex = function() {
-      return Regexes.parse(/^.{14} 00:.{4}:距离战斗开始还有(\y{Float})秒！/);
+      return Regexes.gameLog({
+        capture: true,
+        line: '距离战斗开始还有(?<time>\y{Float})秒！',
+      });
     };
     this.countdownEngageRegex = function() {
-      return /^.{14} 00:.{4}:战斗开始！/;
+      return Regexes.gameLog({
+        capture: true,
+        line: '战斗开始！',
+      });
     };
     this.countdownCancelRegex = function() {
-      return /^.{14} 00:.{4}:(\y{Name})取消了战斗开始倒计时。/;
+      return Regexes.gameLog({
+        capture: true,
+        line: '(?<name\\y{Name})取消了战斗开始倒计时。',
+      });
     };
     this.areaSealRegex = function() {
-      return /^.{14} 00:.{4}:距(.*)被封锁还有\y{Float}秒/;
+      return Regexes.gameLog({
+        capture: true,
+        line: '距(?<name>\\y{Name})被封锁还有(?<time>\\y{Float})秒.*?',
+      });
     };
     this.areaUnsealRegex = function() {
-      return /^.{14} 00:.{4}:(.*)的封锁解除了/;
+      return Regexes.gameLog({
+        capture: true,
+        line: '(?<name>\\y{Name})的封锁解除了.*?',
+      });
     };
   }
 }

resources/lang/de.js
@@ -83,20 +83,55 @@ class CactbotLanguageDe extends CactbotLanguage {
       Pull: 'Pull',
     });
 
+    this.kPetNames = Object.freeze([
+      // Pulled from https://xivapi.com/Pet?pretty=true&language=de
+      'Smaragd-Karfunkel',
+      'Topas-Karfunkel',
+      'Ifrit-Egi',
+      'Titan-Egi',
+      'Garuda-Egi',
+      'Eos',
+      'Selene',
+      'Selbstschuss-Gyrocopter TURM',
+      'Selbstschuss-Gyrocopter LÄUFER',
+      'Demi-Bahamut',
+      'Demi-Phönix',
+      'Seraph',
+      'Mondstein-Karfunkel',
+      'Schattenschemen',
+      'Automaton DAME',
+      'Gedoppeltes Ich',
+    ]);
+
     this.countdownStartRegex = function() {
-      return Regexes.parse(/Noch (\y{Float}) Sekunden bis Kampfbeginn!/);
+      return Regexes.gameLog({
+        capture: true,
+        line: 'Noch (?<time>\\y{Float}) Sekunden bis Kampfbeginn!',
+      });
     };
     this.countdownEngageRegex = function() {
-      return /:Start!/;
+      return Regexes.gameLog({
+        capture: true,
+        line: 'Start!',
+      });
     };
     this.countdownCancelRegex = function() {
-      return /:(\y{Name}) hat den Countdown abgebrochen/;
+      return Regexes.gameLog({
+        capture: true,
+        line: '(?<name>\\y{Name}) hat den Countdown abgebrochen.*?',
+      });
     };
     this.areaSealRegex = function() {
-      return /:Noch (\y{float}) Sekunden, bis sich (.*) schließt/;
+      return Regexes.gameLog({
+        capture: true,
+        line: 'Noch (?<time>\\y{Float}) Sekunden, bis sich (?<name>\\y{Name}) schließt.*?',
+      });
     };
     this.areaUnsealRegex = function() {
-      return /:(.*) öffnet sich erneut/;
+      return Regexes.gameLog({
+        capture: true,
+        line: '(?<name>\\y{Name}) öffnet sich erneut.*?',
+      });
     };
   }
 }

resources/lang/fr.js
@@ -83,20 +83,55 @@ class CactbotLanguageFr extends CactbotLanguage {
       Pull: 'Pull',
     });
 
+    this.kPetNames = Object.freeze([
+      // Pulled from https://xivapi.com/Pet?pretty=true&language=fr
+      'Carbuncle émeraude',
+      'Carbuncle topaze',
+      'Ifrit-Egi',
+      'Titan-Egi',
+      'Garuda-Egi',
+      'Eos',
+      'Selene',
+      'Auto-tourelle Tour',
+      'Auto-tourelle Fou',
+      'Demi-Bahamut',
+      'Demi-Phénix',
+      'Séraphin',
+      'Carbuncle hécatolite',
+      'Estime',
+      'Automate Reine',
+      'Ombre',
+    ]);
+
     this.countdownStartRegex = function() {
-      return Regexes.parse(/Début du combat dans (\y{Float}) secondes !/);
+      return Regexes.gameLog({
+        capture: true,
+        line: 'Début du combat dans (?<time>\\y{Float}) secondes[ ]?!',
+      });
     };
     this.countdownEngageRegex = function() {
-      return /:À l'attaque !/;
+      return Regexes.gameLog({
+        capture: true,
+        line: 'À l\'attaque[ ]?!',
+      });
     };
     this.countdownCancelRegex = function() {
-      return /Le compte à rebours a été interrompu par /;
+      return Regexes.gameLog({
+        capture: true,
+        line: 'Le compte à rebours a été interrompu par (?<name>\\y{Name})[ ]?\\.',
+      });
     };
     this.areaSealRegex = function() {
-      return /:Fermeture (.*) dans /;
+      return Regexes.gameLog({
+        capture: true,
+        line: 'Fermeture (?<name>\\y{Name}) dans (?<time>\\y{Float}) secondes[ ]?\\.',
+      });
     };
     this.areaUnsealRegex = function() {
-      return /:Ouverture (.*)/;
+      return Regexes.gameLog({
+        capture: true,
+        line: 'Ouverture (?<name>\\y{Name})[ ]?!',
+      });
     };
   }
 }

resources/lang/ja.js
@@ -84,20 +84,55 @@ class CactbotLanguageJa extends CactbotLanguage {
       Pull: 'タゲ取る',
     });
 
+    this.kPetNames = Object.freeze([
+      // Pulled from https://xivapi.com/Pet?pretty=true&language=ja
+      'カーバンクル・エメラルド',
+      'カーバンクル・トパーズ',
+      'イフリート・エギ',
+      'タイタン・エギ',
+      'ガルーダ・エギ',
+      'フェアリー・エオス',
+      'フェアリー・セレネ',
+      'オートタレット・ルーク',
+      'オートタレット・ビショップ',
+      'デミ・バハムート',
+      'デミ・フェニックス',
+      'セラフィム',
+      'カーバンクル・ムーンストーン',
+      '英雄の影身',
+      'オートマトン・クイーン',
+      '分身',
+    ]);
+
     this.countdownStartRegex = function() {
-      return Regexes.parse(/戦闘開始まで(\y{Float})秒！/);
+      return Regexes.gameLog({
+        capture: true,
+        line: '戦闘開始まで(?<time>\\y{Float})秒！',
+      });
     };
     this.countdownEngageRegex = function() {
-      return /:戦闘開始！/;
+      return Regexes.gameLog({
+        capture: true,
+        line: '戦闘開始！',
+      });
     };
     this.countdownCancelRegex = function() {
-      return /:(\y{Name})により、戦闘開始カウントがキャンセルされました。/;
+      return Regexes.gameLog({
+        capture: true,
+        line: '(?<name>\\y{Name})により、戦闘開始カウントがキャンセルされました。',
+      });
     };
     this.areaSealRegex = function() {
-      return /:(.*)の封鎖まであと(\y{float})秒/;
+      return Regexes.gameLog({
+        capture: true,
+        line: '(?<name>\\y{Name})の封鎖まであと(?<time>\\y{Float})秒.*?',
+      });
     };
     this.areaUnsealRegex = function() {
-      return /:(.*)の封鎖が解かれた……/;
+      return Regexes.gameLog({
+        capture: true,
+        line: '(?<name>\\y{Name})の封鎖が解かれた……',
+      });
     };
   }
 }

resources/lang/ko.js
@@ -84,21 +84,55 @@ class CactbotLanguageKo extends CactbotLanguage {
       Pull: '풀링',
     });
 
-    // TODO: Countdowns
+    this.kPetNames = Object.freeze([
+      // Pulled from https://github.com/Ra-Workspace/ffxiv-datamining-ko/blob/master/csv/Pet.csv
+      '카벙클 에메랄드',
+      '카벙클 토파즈',
+      '이프리트 에기',
+      '타이탄 에기',
+      '가루다 에기',
+      '요정 에오스',
+      '요정 셀레네',
+      '자동포탑 룩',
+      '자동포탑 비숍',
+      '데미바하무트',
+      '데미피닉스',
+      '세라핌',
+      '카벙클 문스톤',
+      '영웅의 환영',
+      '자동인형 퀸',
+      '분신',
+    ]);
+
     this.countdownStartRegex = function() {
-      return Regexes.parse(/전투 시작 (\y{Float})초 전!/);
+      return Regexes.gameLog({
+        capture: true,
+        line: '전투 시작 (?<time>\\y{Float})초 전!',
+      });
     };
     this.countdownEngageRegex = function() {
-      return /:전투 시작!/;
+      return Regexes.gameLog({
+        capture: true,
+        line: '전투 시작!',
+      });
     };
     this.countdownCancelRegex = function() {
-      return / 님이 초읽기를 취소했습니다\./;
+      return Regexes.gameLog({
+        capture: true,
+        line: '(?<name>\\y{Name}) 님이 초읽기를 취소했습니다\\.',
+      });
     };
     this.areaSealRegex = function() {
-      return /:(\y{Float})초 후에 (.*)(이|가) 봉쇄됩니다\./;
+      return Regexes.gameLog({
+        capture: true,
+        line: '(?<time>\\y{Float})초 후에 (?<name>\\y{Name})(이|가) 봉쇄됩니다\\.',
+      });
     };
     this.areaUnsealRegex = function() {
-      return /:(.*)의 봉쇄가 해제되었습니다\./;
+      return Regexes.gameLog({
+        capture: true,
+        line: '(?<name>\\y{Name})의 봉쇄가 해제되었습니다\\.',
+      });
     };
   }
 }

commit 7a04cb7dbe9b3ae80e6f147c816c32eeaa978701

raidboss: fix en countdown regex
This was broken by #1408, as there's a player name after the seconds.

resources/lang/en.js
@@ -107,7 +107,7 @@ class CactbotLanguageEn extends CactbotLanguage {
     this.countdownStartRegex = function() {
       return Regexes.gameLog({
         capture: true,
-        line: 'Battle commencing in (?<time>\\y{Float}) seconds!',
+        line: 'Battle commencing in (?<time>\\y{Float}) seconds!.*?',
       });
     };
     this.countdownEngageRegex = function() {