48c5ce98a22c55071af6697f7e19d512a7071c11
cacbot <Gamerbauer1985@gmail.com>
2020-06-21 20:06:48 +0200

    try fr

--- ui/raidboss/data/05-shb/trial/varis-ex.js
+++ ui/raidboss/data/05-shb/trial/varis-ex.js
@@ -402,7 +402,8 @@
         'Ventus Est': 'Ventus Est',
       },
       'replaceText': {
-        '\\?': ' ?',
+        '\\?(?!--)': ' ?',
+        '--clones appear\\?--': 'apparition des clones ?',
         'Aetherochemical Grenado': 'Grenade Magitek',
         'Alea Iacta Est': 'Alea Jacta Est',
         'Altius': 'Altius',
