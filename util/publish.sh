18a05db9c766f563ff1ee525f9bcbc1442459cfa
Panic Stevenson <panic.stevenson@gmail.com>
2020-06-23 20:19:22 -0700

    Release action (#4)
    * Add Release Workflow
    
    * Change to PR for iterations
    
    * Change dotnet version
    
    * Look around
    
    * Point to Sln
    
    * test
    
    * docs: Update Dependency Fetching Documentation (#1507)
    
    Update the language around the dependency fetching documentation, fixing
    the path to the dependency fetching script.
    
    Add a shebang to the script and change the execution permissions to
    allow calling the script in a matter matching the documentation.
    
    * config: fix; parser language is set to English (#1510)
    
    * More
    
    * less goof
    
    * why does not python not work?
    
    * ughhh
    
    * good dir
    
    * can I just run bash?
    
    * heh..
    
    * we uploadin
    
    * can powershell figure it out?
    
    * can pwsh do this?
    
    * not powerful enough
    
    * x to doubt
    
    * no way
    
    * debug
    
    * mayhaps
    
    * updates and stuff
    
    * wow
    
    Co-authored-by: Jaehyuk-Lee <alex00728@outlook.com>

--- util/publish.sh
+++ util/publish.sh
@@ -1,17 +1,15 @@
+#!/usr/bin/env bash
+
 BASE=$(dirname "$0")/../
 BIN=$BASE/bin/x64/Release
 OUT=$BASE/publish/cactbot-release/cactbot
 
-rm -rf $OUT
-mkdir -p $OUT
+rm -rf "$OUT"
+mkdir -p "$OUT"
 
-cp -r $BIN/CactbotOverlay.dll $OUT/
-cp -r $BIN/CactbotEventSource.dll $OUT/
-cp -r $BASE/ui/ $OUT/
-cp -r $BASE/resources/ $OUT/
-cp $BASE/*.md $OUT/
+cp -r "$BIN"/{CactbotOverlay.dll,CactbotEventSource.dll} "$OUT/"
+cp -r "$BASE"/{ui/,resources/,*.md} "$OUT/"
 
 # manually copy readme due to avoid local user/ files
-mkdir -p $OUT/user/
-cp $BASE/user/README.txt $OUT/user/
-cp $BASE/user/*-example.* $OUT/user/
+mkdir -p "$OUT/user/"
+cp "$BASE"/user/{README.txt,*-example.*} "$OUT/user/"
