e2e5bc099ac006e0a076db7a2fd81c928e92d852
Adrienne Walker <enne@quisquo.us>
2020-06-29 19:55:43 -0700

    oopsy: update titan ex to match titan hm

--- ui/oopsyraidsy/data/02-arr/trial/titan_ex.js
+++ ui/oopsyraidsy/data/02-arr/trial/titan_ex.js
@@ -7,32 +7,17 @@
     ko: /^극 타이탄 토벌전$/,
   },
   damageWarn: {
-    'TitanEx Landslide': '5BB',
     'TitanEx Weight Of The Land': '5BE',
     'TitanEx Burst': '5BF',
+  },
+  damageFail: {
+    'TitanEx Landslide': '5BB',
     'TitanEx Gaoler Landslide': '5C3',
   },
-  triggers: [
-    // Things that should only hit one person.
-    {
-      id: 'TitanEx Mountain Buster',
-      damageRegex: '5B8',
-      condition: function(e, data) {
-        return e.type != '15';
-      },
-      mistake: function(e, data) {
-        return { type: 'fail', blame: e.targetName, text: e.abilityName };
-      },
-    },
-    {
-      id: 'TitanEx Rock Buster',
-      damageRegex: '5B7',
-      condition: function(e, data) {
-        return e.type != '15';
-      },
-      mistake: function(e, data) {
-        return { type: 'fail', blame: e.targetName, text: e.abilityName };
-      },
-    },
-  ],
+  shareWarn: {
+    'TitanEx Rock Buster': '5B7',
+  },
+  shareFail: {
+    'TitanEx Mountain Buster': '5B8',
+  },
 }];
