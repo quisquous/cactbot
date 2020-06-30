b6b2ccb10331bad51d6a11d833b5625097add337
Adrienne Walker <enne@quisquo.us>
2020-06-29 19:54:22 -0700

    oopsy: titan hm

---
+++ ui/oopsyraidsy/data/02-arr/trial/titan_hm.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Titan Hard
+[{
+  zoneRegex: {
+    en: /^The Navel \(Hard\)$/,
+  },
+  damageWarn: {
+    'TitanHm Weight Of The Land': '553',
+    'TitanHm Burst': '41C',
+  },
+  damageFail: {
+    'TitanHm Landslide': '554',
+  },
+  shareWarn: {
+    'TitanHm Rock Buster': '550',
+  },
+  shareFail: {
+    'TitanHm Mountain Buster': '283',
+  },
+}];
