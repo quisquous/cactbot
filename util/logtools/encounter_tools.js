d37939c23e8163a08b2311c46d90e5438feb2b4b
Adrienne Walker <enne@quisquo.us>
2020-07-15 14:41:25 -0700

    Add encounter_tools.js

---
+++ util/logtools/encounter_tools.js
@@ -0,0 +1,159 @@
+'use strict';
+
+let ZoneId = require('../../resources/zone_id.js');
+let NetRegexes = require('../../resources/netregexes.js');
+
+let replacements = require('../../ui/raidboss/common_replacement.js');
+let commonReplacement = replacements.commonReplacement;
+let syncKeys = replacements.syncKeys;
+
+let idToZoneName = {};
+for (let zoneName in ZoneId)
+  idToZoneName[ZoneId[zoneName]] = zoneName;
+
+
+class EncounterFinder {
+  constructor() {
+    this.currentZone = null;
+    this.currentFight = null;
+
+    this.regex = {
+      changeZone: NetRegexes.changeZone(),
+      cactbotWipe: NetRegexes.echo({ line: 'cactbot wipe.*?' }),
+      win: NetRegexes.network6d({ command: '40000003' }),
+      wipe: NetRegexes.network6d({ command: '40000010' }),
+      commence: NetRegexes.network6d({ command: '4000000[16]' }),
+      playerAttackingMob: NetRegexes.ability({ sourceId: '1.{7}', targetId: '4.{7}' }),
+      mobAttackingPlayer: NetRegexes.ability({ sourceId: '4.{7}', targetId: '1.{7}' }),
+    };
+
+    // TODO: handle sealing / unsealing properly, use this to start/stop fights / pick names
+    for (const keyName in syncKeys) {
+      const key = syncKeys[keyName];
+      this.regex[keyName] = {
+        en: key,
+      };
+      for (const lang in commonReplacement[key])
+        this.regex[keyName][lang] = commonReplacement[key][lang];
+    }
+  }
+
+  process(line) {
+    let m;
+
+    m = line.match(this.regex.changeZone);
+    if (m) {
+      if (this.currentFight !== null) {
+        this.onEndFight(line, this.currentFight, { ...m.groups, endType: 'Zone' });
+        this.currentFight = null;
+      }
+      if (this.currentZone !== null)
+        this.onEndZone(line, this.currentZone, m.groups);
+
+      this.currentZone = m.groups.name;
+      this.onStartZone(line, this.currentZone, m.groups);
+      return;
+    }
+
+    m = line.match(this.regex.cactbotWipe);
+    if (m) {
+      if (this.currentFight !== null) {
+        this.onEndFight(line, this.currentFight, { ...m.groups, endType: 'CactbotWipe' });
+        this.currentFight = null;
+      }
+      return;
+    }
+
+    m = line.match(this.regex.wipe);
+    if (m) {
+      if (this.currentFight !== null) {
+        this.onEndFight(line, this.currentFight, { ...m.groups, endType: 'Wipe' });
+        this.currentFight = null;
+      }
+      return;
+    }
+
+    m = line.match(this.regex.win);
+    if (m) {
+      if (this.currentFight !== null) {
+        this.onEndFight(line, this.currentFight, { ...m.groups, endType: 'Win' });
+        this.currentFight = null;
+      }
+      return;
+    }
+
+    if (this.currentFight === null) {
+      m = line.match(this.regex.playerAttackingMob);
+      if (!m)
+        m = line.match(this.regex.mobAttackingPlayer);
+      if (m) {
+        this.currentFight = this.currentZone;
+        this.onStartFight(line, this.currentFight, m.groups);
+        return;
+      }
+    }
+  }
+
+  onStartZone(line, name, matches) {}
+  onEndZone(line, name, matches) {}
+  onStartFight(line, name, matches) {}
+  onEndFight(line, name, matches) {}
+}
+
+class EncounterCollector extends EncounterFinder {
+  constructor() {
+    super();
+    this.zones = [];
+    this.fights = [];
+
+    this.lastZone = null;
+    this.lastFight = null;
+  }
+
+  dateFromMatches(matches) {
+    return new Date(Date.parse(matches.timestamp));
+  }
+
+  onStartZone(line, name, matches) {
+    this.lastZone = {
+      name: name,
+      startLine: line,
+      id: matches.id,
+      startTime: this.dateFromMatches(matches),
+    };
+  }
+
+  onEndZone(line, name, matches) {
+    this.lastZone.endLine = line;
+    this.lastZone.endTime = this.dateFromMatches(matches);
+
+    this.zones.push(this.lastZone);
+    this.lastZone = null;
+  }
+
+  onStartFight(line, name, matches) {
+    this.lastFight = {
+      name: name,
+      startLine: line,
+      startTime: this.dateFromMatches(matches),
+    };
+  }
+
+  onEndFight(line, name, matches) {
+    this.lastFight.endLine = line;
+    this.lastFight.endTime = this.dateFromMatches(matches);
+    this.lastFight.endType = matches.endType;
+    // In case we found something better.
+    this.lastFight.name = name;
+
+    this.fights.push(this.lastFight);
+    this.lastFight = null;
+  }
+}
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = {
+    EncounterFinder: EncounterFinder,
+    EncounterCollector: EncounterCollector,
+  };
+}
--- util/logtools/test_log.js
+++ util/logtools/test_log.js
@@ -3,6 +3,8 @@
 let fs = require('fs');
 let Anonymizer = require('./anonymizer.js');
 let Splitter = require('./splitter.js');
+let encounterTools = require('./encounter_tools.js');
+let EncounterCollector = encounterTools.EncounterCollector;
 
 const logFileName = process.argv[2];
 
@@ -23,6 +25,7 @@ class ConsoleNotifier {
 
 let notifier = new ConsoleNotifier();
 let anonymizer = new Anonymizer();
+let collector = new EncounterCollector();
 
 let lineReader = require('readline').createInterface({
   input: require('fs').createReadStream(logFileName),
@@ -35,23 +38,141 @@ let splitter = new Splitter(startLine, stopLine);
 
 let lines = [];
 lineReader.on('line', (line) => {
+  try {
+    collector.process(line);
+  } catch (e) {
+    console.error(e);
+  }
+
   splitter.processWithCallback(line, (line) => {
     const anonLine = anonymizer.process(line, notifier);
     if (typeof anonLine === 'undefined')
       return;
 
     lines.push(anonLine);
-    console.log(anonLine);
+    // console.log(anonLine);
   });
 
   if (splitter.isDone())
     lineReader.close();
 });
 
+const timeFromDate = (date) => {
+  return date.toISOString().slice(11, 19);
+};
+
+const dayFromDate = (date) => {
+  return date.toISOString().slice(0, 10);
+};
+
+const durationFromDates = (start, end) => {
+  const ms = end - start;
+  const totalSeconds = Math.round(ms / 1000);
+
+  let str = '';
+  const totalMinutes = Math.floor(totalSeconds / 60);
+  if (totalMinutes > 0)
+    str += totalMinutes + 'm';
+  str += (totalSeconds % 60) + 's';
+  return str;
+};
+
+// For an array of arrays, return an array where each value is the max length at that index
+// among all of the inner arrays, e.g. find the max length per field of an array of rows.
+const maxLengthPerIndex = (outputRows) => {
+  const outputSizes = outputRows.map((row) => row.map((field) => field.length));
+  return outputSizes.reduce((max, row) => {
+    return max.map((val, idx) => Math.max(val, row[idx]));
+  });
+};
+
+const leftExtendStr = (row, lengths, idx) => {
+  let str = row[idx];
+  while (str.length < lengths[idx])
+    str = ' ' + str;
+  return str;
+};
+
+const rightExtendStr = (row, lengths, idx) => {
+  let str = row[idx];
+  while (str.length < lengths[idx])
+    str += ' ';
+  return str;
+};
+
+const printCollectedZones = (collector) => {
+  let idx = 1;
+  let outputRows = [];
+  for (const zone of collector.zones) {
+    outputRows.push([
+      idx.toString(),
+      dayFromDate(zone.startTime),
+      timeFromDate(zone.startTime),
+      durationFromDates(zone.startTime, zone.endTime),
+      zone.name,
+    ]);
+    idx++;
+  }
+
+  const lengths = maxLengthPerIndex(outputRows);
+
+  const dateIdx = 1;
+  let lastDate = null;
+  for (const row of outputRows) {
+    if (row[dateIdx] !== lastDate) {
+      lastDate = row[dateIdx];
+      console.log(lastDate);
+    }
+
+    console.log('  ' +
+      leftExtendStr(row, lengths, 0) + ') ' +
+      leftExtendStr(row, lengths, 2) + ' ' +
+      leftExtendStr(row, lengths, 3) + ' ' +
+      rightExtendStr(row, lengths, 4));
+  }
+};
+
+const printCollectedFights = (collector) => {
+  let idx = 1;
+  let outputRows = [];
+  for (const fight of collector.fights) {
+    outputRows.push([
+      idx.toString(),
+      dayFromDate(fight.startTime),
+      timeFromDate(fight.startTime),
+      durationFromDates(fight.startTime, fight.endTime),
+      fight.name,
+      fight.endType,
+    ]);
+    idx++;
+  }
+
+  const lengths = maxLengthPerIndex(outputRows);
+
+  const dateIdx = 1;
+  let lastDate = null;
+  for (const row of outputRows) {
+    if (row[dateIdx] !== lastDate) {
+      lastDate = row[dateIdx];
+      console.log(lastDate);
+    }
+
+    console.log('  ' +
+      leftExtendStr(row, lengths, 0) + ') ' +
+      leftExtendStr(row, lengths, 2) + ' ' +
+      leftExtendStr(row, lengths, 3) + ' ' +
+      rightExtendStr(row, lengths, 4) + ' ' +
+      '[' + row[5] + ']');
+  }
+};
+
 lineReader.on('close', () => {
   anonymizer.validateIds();
-  for (let line of lines)
+  for (const line of lines)
     anonymizer.validateLine(line);
+
+  printCollectedZones(collector);
+  printCollectedFights(collector);
 
   process.exit(exitCode);
 });
