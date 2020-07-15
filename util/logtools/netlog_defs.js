09637cfee7c3ddf6ae00ce6949ba7a85f53827eb
Adrienne Walker <enne@quisquo.us>
2020-07-14 23:00:03 -0700

    Add log splitter

--- util/logtools/netlog_defs.js
+++ util/logtools/netlog_defs.js
@@ -195,6 +195,7 @@ const logDefinitions = {
     optionalFields: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
       16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26],
     canAnonymize: true,
+    lastInclude: true,
   },
   '12': {
     type: 'PlayerStats',
@@ -219,6 +220,7 @@ const logDefinitions = {
       18: 'tenacity',
     },
     canAnonymize: true,
+    lastInclude: true,
   },
   '13': {
     type: 'CombatantHP',
--- util/logtools/test_log.js
+++ util/logtools/test_log.js
@@ -2,6 +2,7 @@
 
 let fs = require('fs');
 let Anonymizer = require('./anonymizer.js');
+let Splitter = require('./splitter.js');
 
 const logFileName = process.argv[2];
 
@@ -27,14 +28,24 @@ let lineReader = require('readline').createInterface({
   input: require('fs').createReadStream(logFileName),
 });
 
+// TOOD: add a JavaScript version of encounter_tools.find_fights_in_file <_<
+let startLine = '01|2020-05-11T19:21:36.6480000-07:00|213|Alexander - The Arm of the Son (Savage)|a7a5be3a0d25548f95086ca00b98e610';
+let stopLine = '01|2020-05-11T19:21:54.3240000-07:00|153|Mist|a6798291af6520138519e4d38a028545';
+let splitter = new Splitter(startLine, stopLine);
+
 let lines = [];
 lineReader.on('line', (line) => {
-  const anonLine = anonymizer.process(line, notifier);
-  if (typeof anonLine === 'undefined')
-    return;
+  splitter.processWithCallback(line, (line) => {
+    const anonLine = anonymizer.process(line, notifier);
+    if (typeof anonLine === 'undefined')
+      return;
 
-  lines.push(anonLine);
-  console.log(anonLine);
+    lines.push(anonLine);
+    console.log(anonLine);
+  });
+
+  if (splitter.isDone())
+    lineReader.close();
 });
 
 lineReader.on('close', () => {
