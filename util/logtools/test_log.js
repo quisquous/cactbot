caf872bfe207cfde81abe004167a505f1e71a4ec
Adrienne Walker <enne@quisquo.us>
2020-07-15 21:38:14 -0700

    util: add -lf / -lz support

--- util/logtools/test_log.js
+++ util/logtools/test_log.js
@@ -1,61 +1,57 @@
 'use strict';
 
 let fs = require('fs');
+let path = require('path');
 let Anonymizer = require('./anonymizer.js');
 let Splitter = require('./splitter.js');
 let encounterTools = require('./encounter_tools.js');
 let EncounterCollector = encounterTools.EncounterCollector;
+let argparse = require('argparse');
 
-const logFileName = process.argv[2];
+// TODO: add options for not splitting / not anonymizing.
+let parser = new argparse.ArgumentParser({
+  addHelp: true,
+});
+parser.addArgument(['-f', '--file'], {
+  required: true,
+  help: 'File to analyze',
+});
+parser.addArgument(['-lf', '--search-fights'], {
+  nargs: '?',
+  defaultValue: '-1',
+  type: 'int',
+  help: 'Fight in log to use, e.g. \'1\'. ' +
+    'If no number is specified, returns a list of fights.',
+});
+parser.addArgument(['-lz', '--search-zones'], {
+  nargs: '?',
+  defaultValue: '-1',
+  type: 'int',
+  help: 'Zone in log to use, e.g. \'1\'. ' +
+    'If no number is specified, returns a list of zones.',
+});
 
+const args = parser.parseArgs();
+
+// -1 implies not included
+// null implies included but no value
+if (args['search_fights'] === -1 && args['search_zones'] === -1) {
+  console.error('Error: Must specify one of -lf or -lz\n');
+  parser.printHelp();
+  process.exit(-1);
+}
+if (args['search_fights'] !== -1 && args['search_zones'] !== -1) {
+  console.error('Error: Must specify exactly one of -lf or -lz\n');
+  parser.printHelp();
+  process.exit(-1);
+}
+
+const logFileName = args.file;
 let exitCode = 0;
 const errorFunc = (str) => {
   console.error(str);
   exitCode = 1;
 };
-
-class ConsoleNotifier {
-  warn(reason, splitLine) {
-    if (typeof splitLine === 'undefined')
-      errorFunc(this.fileName + ': ' + reason);
-    else
-      errorFunc(this.fileName + ': ' + reason + ': ' + splitLine.join('|'));
-  }
-}
-
-let notifier = new ConsoleNotifier();
-let anonymizer = new Anonymizer();
-let collector = new EncounterCollector();
-
-let lineReader = require('readline').createInterface({
-  input: require('fs').createReadStream(logFileName),
-});
-
-// TOOD: add a JavaScript version of encounter_tools.find_fights_in_file <_<
-let startLine = '01|2020-05-11T19:21:36.6480000-07:00|213|Alexander - The Arm of the Son (Savage)|a7a5be3a0d25548f95086ca00b98e610';
-let stopLine = '01|2020-05-11T19:21:54.3240000-07:00|153|Mist|a6798291af6520138519e4d38a028545';
-let splitter = new Splitter(startLine, stopLine);
-
-let lines = [];
-lineReader.on('line', (line) => {
-  try {
-    collector.process(line);
-  } catch (e) {
-    console.error(e);
-  }
-
-  splitter.processWithCallback(line, (line) => {
-    const anonLine = anonymizer.process(line, notifier);
-    if (typeof anonLine === 'undefined')
-      return;
-
-    lines.push(anonLine);
-    // console.log(anonLine);
-  });
-
-  if (splitter.isDone())
-    lineReader.close();
-});
 
 const timeFromDate = (date) => {
   return date.toISOString().slice(11, 19);
@@ -166,13 +162,82 @@ const printCollectedFights = (collector) => {
   }
 };
 
-lineReader.on('close', () => {
-  anonymizer.validateIds();
-  for (const line of lines)
-    anonymizer.validateLine(line);
+// No top-level await, so wrap everything an async function.  <_<
+(async function() {
+  const makeCollectorFromPrepass = async (filename) => {
+    let collector = new EncounterCollector();
+    let lineReader = require('readline').createInterface({
+      input: fs.createReadStream(filename),
+    });
+    for await (const line of lineReader) {
+      // TODO: this could be more efficient if it stopped when it found the requested encounter.
+      collector.process(line);
+    }
+    return collector;
+  };
 
-  printCollectedZones(collector);
-  printCollectedFights(collector);
+  const collector = await makeCollectorFromPrepass(logFileName);
 
-  process.exit(exitCode);
-});
+  if (args['search_fights'] === null) {
+    printCollectedFights(collector);
+    process.exit(0);
+  }
+  if (args['search_zones'] === null) {
+    printCollectedZones(collector);
+    process.exit(0);
+  }
+
+  class ConsoleNotifier {
+    warn(reason, splitLine) {
+      if (typeof splitLine === 'undefined')
+        errorFunc(this.fileName + ': ' + reason);
+      else
+        errorFunc(this.fileName + ': ' + reason + ': ' + splitLine.join('|'));
+    }
+  }
+
+  let notifier = new ConsoleNotifier();
+  let anonymizer = new Anonymizer();
+
+  let lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream(logFileName),
+  });
+
+  let startLine = null;
+  let endLine = null;
+  // This utility prints 1-indexed fights and zones, so adjust - 1.
+  if (args['search_fights'] !== -1) {
+    let fight = collector.fights[args['search_fights'] - 1];
+    startLine = fight.startLine;
+    endLine = fight.endLine;
+  } else {
+    let zone = collector.zones[args['search_zones'] - 1];
+    startLine = zone.startLine;
+    endLine = zone.endLine;
+  }
+
+  let splitter = new Splitter(startLine, endLine);
+
+  let lines = [];
+  lineReader.on('line', (line) => {
+    splitter.processWithCallback(line, (line) => {
+      const anonLine = anonymizer.process(line, notifier);
+      if (typeof anonLine === 'undefined')
+        return;
+
+      lines.push(anonLine);
+      console.log(anonLine);
+    });
+
+    if (splitter.isDone())
+      lineReader.close();
+  });
+
+  lineReader.on('close', () => {
+    anonymizer.validateIds();
+    for (const line of lines)
+      anonymizer.validateLine(line);
+
+    process.exit(exitCode);
+  });
+}());
