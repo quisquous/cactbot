42fe566bcb4146fa75c082f3b202119e135d4e16
Adrienne Walker <enne@quisquo.us>
2020-07-16 23:29:26 -0700

    Include combatants prior to log split
    This makes ACT imports work properly.

--- util/logtools/splitter.js
+++ util/logtools/splitter.js
@@ -17,6 +17,9 @@ class Splitter {
     this.globalLines = [];
     // log type => line
     this.lastInclude = {};
+
+    // id -> line
+    this.addedCombatants = {};
   }
 
   // Can return string, array of strings, or undefined (i.e. skip).
@@ -40,6 +43,21 @@ class Splitter {
     else if (type.lastInclude)
       this.lastInclude[typeField] = line;
 
+    // Combatant special case:
+    if (typeField === '01') {
+      // When changing zones, reset all combatants.
+      // They will get re-added again.
+      this.addedCombatants = {};
+    } else if (typeField === '03') {
+      const idIdx = 2;
+      const combatantId = splitLine[idIdx].toUpperCase();
+      this.addedCombatants[combatantId] = line;
+    } else if (typeField === '04') {
+      const idIdx = 2;
+      const combatantId = splitLine[idIdx].toUpperCase();
+      delete this.addedCombatants[combatantId];
+    }
+
     if (!this.haveStarted && line !== this.startLine)
       return;
 
@@ -58,12 +76,22 @@ class Splitter {
 
     for (const typeField in this.lastInclude)
       lines.push(this.lastInclude[typeField]);
-
+    for (const key in this.addedCombatants)
+      lines.push(this.addedCombatants[key]);
     lines.push(line);
+
+    lines = lines.sort((a, b) => {
+      // Sort by earliest time first, then by earliest log id.
+      // This makes the log a little bit fake but maybe it's good enough.
+      const aStr = a.slice(3, 36) + a.slice(0, 3);
+      const bStr = b.slice(3, 36) + b.slice(0, 3);
+      return aStr.localeCompare(bStr);
+    });
 
     // These should be unused from here on out.
     this.globalLines = null;
     this.lastInclude = null;
+    this.addedCombatants = null;
 
     return lines;
   }
