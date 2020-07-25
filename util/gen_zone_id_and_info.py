28f086d703da2b268747694ffa19aac382d3f852
Adrienne Walker <enne@quisquo.us>
2020-07-24 20:45:17 -0700

    util: use rawexd to look up correct map offsets

--- util/gen_zone_id_and_info.py
+++ util/gen_zone_id_and_info.py
@@ -13,6 +13,19 @@ import re
 _ZONE_ID_OUTPUT_FILE = "zone_id.js"
 _ZONE_INFO_OUTPUT_FILE = "zone_info.js"
 
+# Notes: use rawexd here instead of exd to get place ids / territory ids
+# instead of the lookups for PlaceName / TerritoryType that are not unique.
+# The connections here are:
+#   TerritoryType has Map, PlaceName, anonymous ContentFinderCondition (but not all have this)
+#   ContentFinderCondition has Name, TerritoryType (but not all TerritoryType has CFC)
+#   PlaceName has Name
+#
+# Name-finding Algorithm:
+# Look up TerritoryType
+# if it has a CFC: use that
+# else if CFC has a TerritoryType that matches: use that
+# else: use PlaceName
+
 # Turn names from ContentFinderCondition into JavaScript-safe string keys.
 def clean_name(str):
     if not str:
@@ -71,25 +84,31 @@ def make_map(file, inputs, outputs):
 
 
 def make_territory_map(contents):
-    inputs = ["#", 11, "PlaceName", "Name", "WeatherRate"]
-    outputs = ["territory_id", "cfc_id", "place_name", "name", "weather_rate"]
+    inputs = ["#", 11, "PlaceName", "Name", "WeatherRate", "Map"]
+    outputs = ["territory_id", "cfc_id", "place_id", "name", "weather_rate", "map_id"]
+    return make_map(contents, inputs, outputs)
+
+
+def make_place_name_map(contents):
+    inputs = ["#", "Name"]
+    outputs = ["place_id", "place_name"]
     return make_map(contents, inputs, outputs)
 
 
 def make_cfc_map(contents):
-    inputs = ["#", "Name", "TerritoryType"]
-    outputs = ["place_name_id", "name", "territory_type"]
+    inputs = ["#", "TerritoryType", "Name"]
+    outputs = ["cfc_id", "territory_id", "name"]
     return make_map(contents, inputs, outputs)
 
 
 # :eyes:
 def make_map_map(contents):
-    inputs = ["PlaceName", "SizeFactor", "Offset{X}", "Offset{Y}"]
-    outputs = ["place_name", "size_factor", "offset_x", "offset_y"]
+    inputs = ["#", "SizeFactor", "Offset{X}", "Offset{Y}"]
+    outputs = ["map_id", "size_factor", "offset_x", "offset_y"]
     return make_map(contents, inputs, outputs)
 
 
-def generate_name_data(territory_map, cfc_map):
+def generate_name_data(territory_map, cfc_map, place_name_map):
     map = {}
     map["MatchAll"] = None
     territory_to_cfc_map = {}
@@ -98,18 +117,14 @@ def generate_name_data(territory_map, cfc_map):
     collision_names = set()
 
     # Build territory name to cfc id map.  Collisions have value None.
-    name_to_cfc = {}
+    territory_to_cfc = {}
     for cfc_id, cfc in cfc_map.items():
-        territory = cfc["territory_type"]
-
-        # Some are empty.
-        if not territory:
-            continue
+        territory_id = cfc["territory_id"]
 
         # Collision?
-        if territory in name_to_cfc:
-            name_to_cfc[territory] = None
-        name_to_cfc[territory] = cfc_id
+        if territory_id in territory_to_cfc:
+            territory_to_cfc[territory_id] = None
+        territory_to_cfc[territory_id] = cfc_id
 
     # First pass, find everything with a cfc name.
     # These take precedence over any later collisions.
@@ -126,7 +141,8 @@ def generate_name_data(territory_map, cfc_map):
     # Second pass, generate all the names, giving cfc names priority.
     for territory_id, territory in territory_map.items():
         cfc_id = territory["cfc_id"]
-        place_name = territory["place_name"]
+        place_id = territory["place_id"]
+        place_name = place_name_map[place_id]["place_name"]
         territory_name = territory["name"]
 
         cfc_id_for_name = None
@@ -134,8 +150,8 @@ def generate_name_data(territory_map, cfc_map):
         if cfc_id != "0":
             cfc_id_for_name = cfc_id
             name_key = clean_name(cfc_map[cfc_id]["name"])
-        elif territory_name in name_to_cfc and name_to_cfc[territory_name]:
-            cfc_id_for_name = name_to_cfc[territory_name]
+        elif territory_id in territory_to_cfc and territory_to_cfc[territory_id]:
+            cfc_id_for_name = territory_to_cfc[territory_id]
             name_key = clean_name(cfc_map[cfc_id_for_name]["name"])
         else:
             # World zones like Middle La Noscea are not in CFC.
@@ -171,18 +187,18 @@ def generate_name_data(territory_map, cfc_map):
     return map, territory_to_cfc_map
 
 
-def generate_ZONE_INFO(
-    territory_map, cfc_map, map_map, territory_to_cfc_map,
-):
+def generate_zone_info(territory_map, cfc_map, map_map, territory_to_cfc_map, place_name_map):
 
     map = {}
     for territory_id in territory_to_cfc_map:
         output = {}
         map[territory_id] = output
 
-        place_name = territory_map[territory_id]["place_name"]
+        territory = territory_map[territory_id]
+        place_id = territory["place_id"]
+        place_name = place_name_map[place_id]["place_name"]
 
-        output["weatherRate"] = int(territory_map[territory_id]["weather_rate"])
+        output["weatherRate"] = int(territory["weather_rate"])
 
         cfc_id = territory_to_cfc_map[territory_id]
         if cfc_id == None:
@@ -190,11 +206,14 @@ def generate_ZONE_INFO(
         else:
             output["name"] = {"en": cfc_map[cfc_id]["name"]}
 
-        if place_name in map_map:
-            map_info = map_map[place_name]
+        map_id = territory["map_id"]
+        if map_id in map_map:
+            map_info = map_map[map_id]
             output["sizeFactor"] = int(map_info["size_factor"])
             output["offsetX"] = int(map_info["offset_x"])
             output["offsetY"] = int(map_info["offset_y"])
+        else:
+            print("missing map: %s" % territory_id)
 
     return map
 
@@ -204,11 +223,12 @@ if __name__ == "__main__":
     reader = coinach.CoinachReader(verbose=True)
     writer = coinach.CoinachWriter(verbose=True)
 
-    territory_map = make_territory_map(reader.exd("TerritoryType"))
-    cfc_map = make_cfc_map(reader.exd("ContentFinderCondition"))
-    map_map = make_map_map(reader.exd("Map"))
+    territory_map = make_territory_map(reader.rawexd("TerritoryType"))
+    place_name_map = make_place_name_map(reader.rawexd("PlaceName"))
+    cfc_map = make_cfc_map(reader.rawexd("ContentFinderCondition"))
+    map_map = make_map_map(reader.rawexd("Map"))
 
-    name_data, territory_to_cfc_map = generate_name_data(territory_map, cfc_map)
+    name_data, territory_to_cfc_map = generate_name_data(territory_map, cfc_map, place_name_map)
 
     writer.write(
         os.path.join("resources", _ZONE_ID_OUTPUT_FILE),
@@ -219,7 +239,9 @@ if __name__ == "__main__":
 
     # TODO: get one cfc_map / territory_map per language and then we can translate ZoneId.
     # This would allow for auto-translating the raidboss config per-file.
-    territory_info = generate_ZONE_INFO(territory_map, cfc_map, map_map, territory_to_cfc_map)
+    territory_info = generate_zone_info(
+        territory_map, cfc_map, map_map, territory_to_cfc_map, place_name_map
+    )
     writer.write(
         os.path.join("resources", _ZONE_INFO_OUTPUT_FILE),
         os.path.basename(os.path.abspath(__file__)),
