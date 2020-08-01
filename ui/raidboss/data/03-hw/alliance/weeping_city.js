be99f7df1f90fd8602adcaf543e630e1302aeb00
Jonathan Garber <linkthevaliant@gmail.com>
2020-08-01 14:23:04 -0400

    whoops

--- ui/raidboss/data/03-hw/alliance/weeping_city.js
+++ ui/raidboss/data/03-hw/alliance/weeping_city.js
@@ -145,7 +145,7 @@
     },
     {
       id: 'Weeping City Mortal Ray',
-      netRegex: NetRegexes.startsUsing({ id: '17D4', source: 'Summoned Haagenti' }),
+      netRegex: NetRegexes.startsUsing({ id: '17D4', source: 'Summoned Haagenti', capture: false }),
       response: Responses.lookAway(),
     },
     {
