quisquous/cactbot

commit e6e1cbdc833be14436e4daa12a74a44cf3093f7b

radar: use network logs to filter on npc id (#1618)
Fixes #1402.

ui/radar/radar.js
@@ -27,17 +27,17 @@ let Options = {
 // location before a sound is played and it is treated as a new mob.
 // TODO: probably all mobs should be tracked with ids to avoid this.
 // TODO: this would also let us handle mobs with the same name better.
-let kMinDistanceBeforeSound = 100;
+const kMinDistanceBeforeSound = 100;
 
 let gRadar;
 
-let instanceChangedRegex = {
-  'en': / 00:0039:You are now in the instanced area/,
-  'de': / 00:0039:Du bist nun in dem instanziierten Areal/,
-  'fr': / 00:0039:Vous êtes maintenant dans la zone instanciée/,
-  'ja': / 00:0039:インスタンスエリア/,
-  'cn': / 00:0039:当前所在副本区为/,
-  'ko': / 00:0039:인스턴스 지역/,
+const instanceChangedRegex = {
+  en: NetRegexes.gameLog({ code: '0039', line: 'You are now in the instanced area.*?' }),
+  de: NetRegexes.gameLog({ code: '0039', line: 'Du bist nun in dem instanziierten Areal.*?' }),
+  fr: NetRegexes.gameLog({ code: '0039', line: 'Vous êtes maintenant dans la zone instanciée.*?' }),
+  ja: NetRegexes.gameLog({ code: '0039', line: 'インスタンスエリア.*?' }),
+  cn: NetRegexes.gameLog({ code: '0039', line: '当前所在副本区为.*?' }),
+  ko: NetRegexes.gameLog({ code: '0039', line: '인스턴스 지역.*?' }),
 };
 
 class Point2D {
@@ -108,9 +108,11 @@ class Radar {
   }
 
   AddMonster(log, monster, matches) {
+    if (monster.id && matches.npcNameId !== monster.id)
+      return;
     if (monster.regex && !log.match(monster.regex))
       return;
-    if (monster.hp && parseFloat(matches.groups.hp) < monster.hp)
+    if (monster.hp && parseFloat(matches.hp) < monster.hp)
       return;
 
     let options = this.options;
@@ -118,19 +120,19 @@ class Radar {
     if (monster.rank in options.RankOptions)
       options = Object.assign({}, this.options, options.RankOptions[monster.rank]);
     if (options.Type === 'mob') {
-      if (!matches.groups.id.startsWith('4'))
+      if (!matches.id.startsWith('4'))
         return;
-      if (typeof matches.groups.npcId === 'undefined')
+      if (typeof matches.npcId === 'undefined')
         return;
     }
 
-    let mobKey = matches.groups.name.toLowerCase();
+    let mobKey = matches.name.toLowerCase();
     if (mobKey in this.targetMonsters) {
       // Get positions
       let playerPos = new Point2D(this.playerPos.x, this.playerPos.y);
       let oldPos = this.targetMonsters[mobKey].pos;
       let newPos =
-        new Point2D(parseFloat(matches.groups.x), parseFloat(matches.groups.y));
+        new Point2D(parseFloat(matches.x), parseFloat(matches.y));
 
       // Calculate distances
       let oldDistance = playerPos.distance(oldPos);
@@ -139,7 +141,7 @@ class Radar {
       // Update position only if its closer than the current one
       if (newDistance < oldDistance) {
         this.targetMonsters[mobKey].pos = newPos;
-        this.targetMonsters[mobKey].posZ = matches.groups.z;
+        this.targetMonsters[mobKey].posZ = matches.z;
 
         // Update DOM
         this.UpdateMonsterDom(this.targetMonsters[mobKey]);
@@ -150,7 +152,7 @@ class Radar {
       }
     } else {
       // Add DOM
-      let arrowId = 'arrow-' + matches.groups.id;
+      let arrowId = 'arrow-' + matches.id;
       let tr = document.createElement('tr');
       let th = document.createElement('th');
       let img = document.createElement('img');
@@ -167,14 +169,14 @@ class Radar {
       this.table.insertBefore(tr, this.table.childNodes[0]);
 
       let m = {
-        'id': matches.groups.id,
-        'name': matches.groups.name,
+        'id': matches.id,
+        'name': matches.name,
         'rank': monster.rank || '',
-        'hp': parseFloat(matches.groups.hp),
-        'currentHp': parseFloat(matches.groups.hp),
+        'hp': parseFloat(matches.hp),
+        'currentHp': parseFloat(matches.hp),
         'battleTime': 0,
-        'pos': new Point2D(parseFloat(matches.groups.x), parseFloat(matches.groups.y)),
-        'posZ': matches.groups.z,
+        'pos': new Point2D(parseFloat(matches.x), parseFloat(matches.y)),
+        'posZ': matches.z,
         'addTime': Date.now(),
         'dom': tr,
         'puller': null,
@@ -243,35 +245,52 @@ class Radar {
     }
   }
 
-  OnLogEvent(e) {
-    let lang = this.options.ParserLanguage;
-    for (let i = 0; i < e.detail.logs.length; i++) {
-      // added new combatant
-      let matches = e.detail.logs[i].match(Regexes.addedCombatantFull());
+  OnNetLog(e) {
+    const type = e.line[0];
+    const log = e.rawLine;
+
+    // added new combatant
+    if (type === '03') {
+      const matches = log.match(NetRegexes.addedCombatantFull());
       if (matches) {
-        let monster = this.nameToMonster[matches.groups.name.toLowerCase()];
+        const monster = this.nameToMonster[matches.groups.name.toLowerCase()];
         if (monster)
-          this.AddMonster(e.detail.logs[i], monster, matches);
+          this.AddMonster(log, monster, matches.groups);
       }
-      // network ability
-      matches = e.detail.logs[i].match(Regexes.abilityFull());
+      return;
+    }
+
+    // network ability
+    if (type === '21' || type === '22') {
+      const matches = log.match(NetRegexes.abilityFull());
       if (matches) {
-        let monster = this.targetMonsters[matches.groups.target.toLowerCase()];
+        const monster = this.targetMonsters[matches.groups.target.toLowerCase()];
         if (monster) {
           // provoke doesn't work on hunt mobs
-          let isProvoke = e.detail.logs[i].match(Regexes.ability({ id: '1D6D' }));
+          const isProvoke = matches.id === '1D6D';
           if (!isProvoke)
             this.UpdateMonsterPuller(monster, matches.groups.source);
         }
       }
-      // change instance
-      let r = e.detail.logs[i].match(instanceChangedRegex[lang] || instanceChangedRegex['en']);
-      if (r)
-        this.ClearTargetMonsters(10); // don't remove mobs lasting less than 10 seconds
-      // removing combatant
-      matches = e.detail.logs[i].match(Regexes.wasDefeated());
+      return;
+    }
+
+    // change instance
+    if (type === '00') {
+      const lang = this.options.ParserLanguage;
+      if (log.match(instanceChangedRegex[lang] || instanceChangedRegex['en'])) {
+        // don't remove mobs lasting less than 10 seconds
+        this.ClearTargetMonsters(10);
+      }
+      return;
+    }
+
+    // removing combatant
+    if (type === '25') {
+      const matches = log.match(NetRegexes.wasDefeated());
       if (matches)
         this.RemoveMonster(matches.groups.target.toLowerCase());
+      return;
     }
   }
 
@@ -300,15 +319,15 @@ class Radar {
 }
 
 UserConfig.getUserConfigLocation('radar', function() {
-  addOverlayListener('onLogEvent', function(e) {
-    gRadar.OnLogEvent(e);
+  addOverlayListener('LogLine', (e) => {
+    gRadar.OnNetLog(e);
   });
 
-  addOverlayListener('onPlayerChangedEvent', function(e) {
+  addOverlayListener('onPlayerChangedEvent', (e) => {
     gRadar.OnPlayerChange(e);
   });
 
-  addOverlayListener('onZoneChangedEvent', function(e) {
+  addOverlayListener('onZoneChangedEvent', (e) => {
     gRadar.OnZoneChange(e);
   });
 

commit d4a39af4d3746b656b5bd73cf11d62b41d76b180

radar: fix mobs not showing up with default options
This was broken by e6e1cbdc, because addedCombatantFull has different
field names for Regexes vs NetRegexes.

ui/radar/radar.js
@@ -122,7 +122,7 @@ class Radar {
     if (options.Type === 'mob') {
       if (!matches.id.startsWith('4'))
         return;
-      if (typeof matches.npcId === 'undefined')
+      if (!matches.npcNameId || matches.npcNameId === '0')
         return;
     }
 