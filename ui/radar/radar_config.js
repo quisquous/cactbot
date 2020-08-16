090e942a59f4ee26d844abf5bbb47aa72ed0a44c
cacbot <Gamerbauer1985@gmail.com>
2020-08-16 20:34:28 +0200

    add missing translation fr

--- ui/radar/radar_config.js
+++ ui/radar/radar_config.js
@@ -6,6 +6,7 @@ UserConfig.registerOptions('radar', {
       id: 'BRankEnabled',
       name: {
         en: 'B-Rank enabled',
+        fr: 'Rang-B activé',
         ja: 'ランクBモブ',
         cn: '开启B怪探测',
       },
