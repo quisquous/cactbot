35fd62c729eea3ccf037929518e8978f83b2de05
Adrienne Walker <enne@quisquo.us>
2020-08-17 19:29:16 -0700

    raidboss: add some initial puppets' bunker triggers (#1706)

--- ui/raidboss/data/05-shb/alliance/the_puppets_bunker.js
+++ ui/raidboss/data/05-shb/alliance/the_puppets_bunker.js
@@ -1,11 +1,557 @@
 'use strict';
 
-// The Puppets' Bunker
+// TODO: is it worth adding triggers for gaining/losing shield protocol? effect 8F[0-2]
+// TODO: Incongruous Spin timeline trigger?
 
 [{
   zoneId: ZoneId.ThePuppetsBunker,
   timelineFile: 'the_puppets_bunker.txt',
   triggers: [
+    {
+      id: 'Puppet Aegis Anti-Personnel Laser You',
+      netRegex: NetRegexes.headMarker({ id: '00C6' }),
+      condition: Conditions.targetIsYou(),
+      response: Responses.tankBuster('alert'),
+    },
+    {
+      id: 'Puppet Aegis Anti-Personnel Laser Collect',
+      netRegex: NetRegexes.headMarker({ id: '00C6' }),
+      run: function(data, matches) {
+        data.busterTargets = data.busterTargets || [];
+        data.busterTargets.push(matches.target);
+      },
+    },
+    {
+      id: 'Puppet Aegis Anti-Personnel Laser Not You',
+      netRegex: NetRegexes.headMarker({ id: '00C6', capture: false }),
+      delaySeconds: 0.5,
+      suppressSeconds: 5,
+      infoText: function(data) {
+        if (!data.busterTargets)
+          return;
+        if (data.busterTargets.includes(data.me))
+          return;
+
+        if (data.role === 'healer') {
+          return {
+            en: 'Tank Buster',
+            de: 'Tank buster',
+            fr: 'Tank buster',
+            ja: 'タンクバスター',
+            cn: '坦克死刑',
+            ko: '탱버',
+          };
+        }
+        return {
+          en: 'Avoid tank buster',
+        };
+      },
+      run: (data) => delete data.busterTargets,
+    },
+    {
+      id: 'Puppet Aegis Beam Cannons',
+      netRegex: NetRegexes.startsUsing({ source: '813P-Operated Aegis Unit', id: '5073', capture: false }),
+      alertText: {
+        en: 'Go To Narrow Intersection',
+      },
+    },
+    {
+      id: 'Puppet Aegis Aerial Support Surface Laser',
+      netRegex: NetRegexes.headMarker({ id: '0017' }),
+      condition: (data, matches) => data.me === matches.target && data.phase !== 'superior',
+      alarmText: {
+        en: 'Chasing Laser: Get Away',
+      },
+    },
+    {
+      id: 'Puppet Aegis Refraction Cannons 1',
+      netRegex: NetRegexes.startsUsing({ source: '813P-Operated Aegis Unit', id: '5080', capture: false }),
+      alertText: function(data) {
+        return {
+          en: 'Get Under Wings',
+        };
+      },
+    },
+    {
+      id: 'Puppet Aegis Refraction Cannons 2',
+      netRegex: NetRegexes.startsUsing({ source: '813P-Operated Aegis Unit', id: '507F', capture: false }),
+      alertText: function(data) {
+        return {
+          en: 'Get Between Wings',
+        };
+      },
+    },
+    {
+      id: 'Puppet Aegis High-Powered Laser',
+      // This is also head marker 003E, but since there's three stacks, just say "stack".
+      netRegex: NetRegexes.startsUsing({ source: '813P-Operated Aegis Unit', id: '508F', capture: false }),
+      response: Responses.stack(),
+    },
+    {
+      id: 'Puppet Aegis Life\'s Last Song',
+      netRegex: NetRegexes.startsUsing({ source: '813P-Operated Aegis Unit', id: '53B3', capture: false }),
+      // This is more a "if you haven't done this ever or in a while, here's a reminder."
+      // Tell it once, but as this repeats nearly continously forever, only say it once.
+      suppressSeconds: 9999,
+      infoText: {
+        en: 'Dodge into ring gap',
+      },
+    },
+    {
+      id: 'Puppet Light Volt Array',
+      netRegex: NetRegexes.startsUsing({ source: 'Light Artillery Unit', id: '5211' }),
+      condition: (data) => data.CanSilence(),
+      // Multiple of these cast at the same time.
+      suppressSeconds: 5,
+      response: Responses.interrupt('alarm'),
+    },
+    {
+      id: 'Puppet Spread Headmarker',
+      // Used for:
+      // Homing Missile (Light Artillery)
+      // Mechanical Contusion (The Compound)
+      // R012: Laser (Compound 2P)
+      netRegex: NetRegexes.headMarker({ id: '008B' }),
+      condition: Conditions.targetIsYou(),
+      response: Responses.spread('info'),
+    },
+    {
+      id: 'Puppet Light Maneuver Martial Arm Target',
+      netRegex: NetRegexes.startsUsing({ source: 'Light Artillery Unit', id: '5213' }),
+      condition: Conditions.targetIsYou(),
+      response: Responses.tankBuster('alert'),
+    },
+    {
+      id: 'Puppet Light Maneuver Martial Arm Collect',
+      netRegex: NetRegexes.startsUsing({ source: 'Light Artillery Unit', id: '5213' }),
+      run: function(data, matches) {
+        data.busterTargets = data.busterTargets || [];
+        data.busterTargets.push(matches.target);
+      },
+    },
+    {
+      id: 'Puppet Light Maneuver Martial Arm Healer',
+      netRegex: NetRegexes.startsUsing({ source: 'Light Artillery Unit', id: '5213', capture: false }),
+      delaySeconds: 0.5,
+      suppressSeconds: 5,
+      infoText: function(data) {
+        if (!data.busterTargets)
+          return;
+        if (data.busterTargets.includes(data.me))
+          return;
+
+        if (data.role === 'healer') {
+          return {
+            en: 'Tank Buster',
+            de: 'Tank buster',
+            fr: 'Tank buster',
+            ja: 'タンクバスター',
+            cn: '坦克死刑',
+            ko: '탱버',
+          };
+        }
+
+        // Note: this doesn't cleave, so don't say anything about avoiding it.
+      },
+      run: (data) => delete data.busterTargets,
+    },
+    {
+      id: 'Puppet Superior Shield Protocol',
+      netRegex: NetRegexes.startsUsing({ id: '4FA[678]', capture: false }),
+      run: (data) => data.phase = 'superior',
+    },
+    {
+      id: 'Puppet Superior Missile Command',
+      netRegex: NetRegexes.startsUsing({ id: '4FBD', capture: false }),
+      condition: Conditions.caresAboutAOE(),
+      suppressSeconds: 5,
+      response: Responses.aoe(),
+    },
+    {
+      // This is for Maneuver: Incendiary Bombing and Maneuver: Area Bombardment.
+      id: 'Puppet Superior Incendiary Bombing',
+      netRegex: NetRegexes.headMarker({ id: '0017' }),
+      condition: (data, matches) => data.me === matches.target && data.phase === 'superior',
+      alertText: {
+        en: 'Fire Puddle on YOU',
+      },
+    },
+    {
+      id: 'Puppet Superior High-Powered Laser',
+      // Note: no 1B marker for this???
+      netRegex: NetRegexes.startsUsing({ id: '4FB4', capture: false }),
+      suppressSeconds: 5,
+      alertText: {
+        en: 'Line Stack (Point Outside)',
+      },
+    },
+    {
+      // TODO: we could theoretically get combatants here when sharp turn starts casting.
+      // Sharp Turn ability ids differentiate left/right.  We could use effect ids to know group.
+      // But how would you communicate where to stand? "behind and slightly/very right"?
+      id: 'Puppet Superior Sharp Turn',
+      netRegex: NetRegexes.startsUsing({ id: '4FAB', capture: false }),
+      delaySeconds: 3,
+      suppressSeconds: 5,
+      alertText: {
+        en: 'Dodge Sword Charges',
+      },
+    },
+    {
+      id: 'Puppet Superior Precision Guided Missile You',
+      netRegex: NetRegexes.startsUsing({ id: '4FC5' }),
+      condition: Conditions.targetIsYou(),
+      response: Responses.tankBuster('alert'),
+    },
+    {
+      id: 'Puppet Superior Precision Guided Missile Collect',
+      netRegex: NetRegexes.startsUsing({ id: '4FC5' }),
+      run: function(data, matches) {
+        data.busterTargets = data.busterTargets || [];
+        data.busterTargets.push(matches.target);
+      },
+    },
+    {
+      id: 'Puppet Superior Precision Guided Missile Not You',
+      netRegex: NetRegexes.startsUsing({ id: '4FC5', capture: false }),
+      delaySeconds: 0.5,
+      suppressSeconds: 5,
+      infoText: function(data) {
+        if (!data.busterTargets)
+          return;
+        if (data.busterTargets.includes(data.me))
+          return;
+
+        if (data.role === 'healer') {
+          return {
+            en: 'Tank Buster',
+            de: 'Tank buster',
+            fr: 'Tank buster',
+            ja: 'タンクバスター',
+            cn: '坦克死刑',
+            ko: '탱버',
+          };
+        }
+        return {
+          en: 'Avoid tank buster',
+        };
+      },
+      run: (data) => delete data.busterTargets,
+    },
+    {
+      id: 'Puppet Superior Sliding Swipe First',
+      netRegex: NetRegexes.startsUsing({ id: ['4FA[CD]', '550[DEF]', '5510'] }),
+      preRun: function(data, matches) {
+        data.swipe = data.swipe || [];
+        const kRight = {
+          en: 'Right',
+          de: 'Rechts',
+          fr: 'Droite ',
+          ja: '右へ',
+          cn: '右',
+          ko: '오른쪽',
+        };
+        const kLeft = {
+          en: 'Left',
+          de: 'Links',
+          fr: 'Gauche',
+          ja: '左へ',
+          cn: '左',
+          ko: '왼쪽',
+        };
+
+        data.swipe.push({
+          '4FAC': kRight,
+          '4FAD': kLeft,
+          '550D': kRight,
+          '550E': kLeft,
+          '550F': kRight,
+          '5510': kLeft,
+        }[matches.id]);
+      },
+      durationSeconds: 6,
+      alertText: function(data) {
+        if (data.swipe.length !== 1)
+          return;
+
+        // Call and clear the first swipe so we can not call it a second time below.
+        const swipe = data.swipe[0];
+        data.swipe[0] = null;
+        return swipe;
+      },
+    },
+    {
+      id: 'Puppet Superior Sliding Swipe Others',
+      netRegex: NetRegexes.ability({ id: ['4FA[CD]', '550[DEF]', '5510'] }),
+      alertText: function(data, matches) {
+        if (!data.swipe)
+          return;
+
+        // The first swipe callout has been cleared to null.
+        // Deliberately skip it so that when the first swipe goes off, we call the second.
+        let swipe = data.swipe.shift();
+        if (!swipe)
+          swipe = data.swipe.shift();
+        if (!swipe)
+          return;
+        return swipe;
+      },
+    },
+    {
+      id: 'Puppet Heavy Volt Array',
+      netRegex: NetRegexes.startsUsing({ source: '905P-Operated Heavy Artillery Unit', id: '5006', capture: false }),
+      condition: Conditions.caresAboutAOE(),
+      response: Responses.aoe(),
+    },
+    {
+      id: 'Puppet Heavy Active Laser Turret Initial',
+      netRegex: NetRegexes.startsUsing({ source: '905P-Operated Heavy Artillery Unit', id: '4FED', capture: false }),
+      alertText: {
+        en: 'Get Outside Upper Laser',
+      },
+    },
+    {
+      id: 'Puppet Heavy Active Laser Turret Move',
+      netRegex: NetRegexes.startsUsing({ source: '905P-Operated Heavy Artillery Unit', id: '5086', capture: false }),
+      delaySeconds: 5.3,
+      suppressSeconds: 5,
+      response: Responses.move('info'),
+    },
+    {
+      id: 'Puppet Heavy Unconventional Voltage',
+      netRegex: NetRegexes.headMarker({ id: '00AC' }),
+      condition: Conditions.targetIsYou(),
+      alertText: {
+        en: 'Voltage cleave on YOU',
+      },
+    },
+    {
+      id: 'Puppet Heavy Revolving Laser',
+      netRegex: NetRegexes.startsUsing({ source: '905P-Operated Heavy Artillery Unit', id: '5000', capture: false }),
+      response: Responses.getIn(),
+    },
+    {
+      id: 'Puppet Heavy High-Powered Laser',
+      // There's only one starts using, but it targets all the tanks sequentially.
+      netRegex: NetRegexes.startsUsing({ source: '905P-Operated Heavy Artillery Unit', id: '5001' }),
+      response: function(data, matches) {
+        if (data.role === 'tank' || matches.target === data.me) {
+          return {
+            alertText: {
+              en: 'Tank Laser Cleave on YOU',
+            },
+          };
+        }
+        return {
+          infoText: {
+            en: 'Avoid tank laser cleaves',
+          },
+        };
+      },
+    },
+    {
+      id: 'Puppet Heavy Support Pod',
+      netRegex: NetRegexes.startsUsing({ source: '905P-Operated Heavy Artillery Unit', id: '4FE9', capture: false }),
+      // This is approximately when the pods appear.
+      delaySeconds: 6,
+      alertText: function(data) {
+        data.heavyPodCount = data.heavyPodCount || 0;
+        data.heavyPodCount++;
+        if (data.heavyPodCount <= 2) {
+          // The first two are lasers/hammers in either order.
+          // The safe spot in both cases is the same direction.
+          return {
+            en: 'Get Outside Between Pods',
+          };
+        }
+        // There's nothing in the log that indicates what the screens do.
+        // TODO: could check logs for tether target/source and say shift left/right?
+        return {
+          en: 'Get Between Lasers (Watch Tethers)',
+        };
+      },
+    },
+    {
+      id: 'Puppet Heavy Synthesize Compound',
+      netRegex: NetRegexes.startsUsing({ source: '905P-Operated Heavy Artillery Unit', id: '4FEC', capture: false }),
+      // TODO: should this say "towers"? or...something else to indicate variable people needed?
+      alertText: {
+        en: 'Turn Towers Blue',
+      },
+    },
+    {
+      id: 'Puppet Hallway Targeted Laser',
+      netRegex: NetRegexes.headMarker({ id: '00A4' }),
+      condition: Conditions.targetIsYou(),
+      infoText: {
+        en: 'Laser on YOU',
+      },
+    },
+    {
+      id: 'Puppet Compound Mechanical Laceration',
+      netRegex: NetRegexes.startsUsing({ source: 'The Compound', id: '51B8', capture: false }),
+      condition: Conditions.caresAboutMagical(),
+      response: Responses.aoe(),
+      run: (data) => data.phase = 'compound',
+    },
+    {
+      id: 'Puppet Compound Mechanical Dissection',
+      netRegex: NetRegexes.startsUsing({ source: 'The Compound', id: '51B3', capture: false }),
+      response: Responses.goSides(),
+    },
+    {
+      id: 'Puppet Compound Mechanical Decapitation',
+      netRegex: NetRegexes.startsUsing({ source: 'The Compound', id: '51B4', capture: false }),
+      response: Responses.getIn(),
+    },
+    {
+      id: 'Puppet Compound 2P Centrifugal Slice',
+      netRegex: NetRegexes.startsUsing({ source: 'Compound 2P', id: '51B8', capture: false }),
+      condition: Conditions.caresAboutPhysical(),
+      response: Responses.aoe(),
+      // Cover this phase for the checkpoint as well.
+      run: (data) => data.phase = 'compound',
+    },
+    {
+      id: 'Puppet Compound 2P Prime Blade Out',
+      netRegex: NetRegexes.startsUsing({ source: 'Compound 2P', id: ['541F', '5198'], capture: false }),
+      response: Responses.getOut(),
+    },
+    {
+      id: 'Puppet Compound 2P Prime Blade Behind',
+      netRegex: NetRegexes.startsUsing({ source: 'Compound 2P', id: '5420', capture: false }),
+      response: Responses.getBehind(),
+    },
+    {
+      id: 'Puppet Compound 2P Prime Blade In',
+      netRegex: NetRegexes.startsUsing({ source: 'Compound 2P', id: ['5421', '519A'], capture: false }),
+      response: Responses.getIn(),
+    },
+    {
+      id: 'Puppet Compound 2P R012: Laser You',
+      // R012: Laser also puts out 008B headmarkers on non-tanks.
+      netRegex: NetRegexes.headMarker({ id: '00DA' }),
+      condition: Conditions.targetIsYou(),
+      response: Responses.tankBuster('alert'),
+      run: function(data, matches) {
+        data.busterTargets = data.busterTargets || [];
+        data.busterTargets.push(matches.target);
+      },
+    },
+    {
+      id: 'Puppet Compound 2P R012: Laser Not You',
+      netRegex: NetRegexes.headMarker({ id: '00DA', capture: false }),
+      delaySeconds: 0.5,
+      suppressSeconds: 5,
+      alertText: function(data) {
+        if (!data.busterTargets)
+          return;
+        if (data.busterTargets.includes(data.me))
+          return;
+
+        if (data.role === 'healer') {
+          return {
+            en: 'Tank Buster',
+            de: 'Tank buster',
+            fr: 'Tank buster',
+            ja: 'タンクバスター',
+            cn: '坦克死刑',
+            ko: '탱버',
+          };
+        }
+
+        // Note: do not call out "avoid tank" here because there's a lot of markers going out.
+      },
+      run: (data) => delete data.busterTargets,
+    },
+    {
+      id: 'Puppet Compound 2P Three Parts Disdain',
+      netRegex: NetRegexes.headMarker({ id: '003E' }),
+      condition: (data) => data.phase === 'compound',
+      response: Responses.stackOn(),
+    },
+    {
+      id: 'Puppet Compound 2P Three Parts Disdain Knockback',
+      netRegex: NetRegexes.headMarker({ id: '003E', capture: false }),
+      condition: (data) => data.phase === 'compound',
+      // Knockback prevention is 6 seconds long, and there's ~9.6s between marker and final hit.
+      delaySeconds: 3.6,
+      response: Responses.knockback('info'),
+    },
+    {
+      id: 'Puppet Compound 2P Four Parts Resolve',
+      netRegex: NetRegexes.headMarker({ id: ['004F', '0050', '0051', '0052'] }),
+      condition: Conditions.targetIsYou(),
+      alertText: function(data, matches) {
+        return {
+          '004F': {
+            en: 'Jump #1 on YOU',
+          },
+          '0050': {
+            en: 'Cleave #1 on YOU',
+          },
+          '0051': {
+            en: 'Jump #2 on YOU',
+          },
+          '0052': {
+            en: 'Cleave #2 on YOU',
+          },
+        }[matches.id];
+      },
+    },
+    {
+      id: 'Puppet Compound 2P Energy Compression',
+      netRegex: NetRegexes.startsUsing({ source: 'Compound 2P', id: '51A6', capture: false }),
+      delaySeconds: 4,
+      infoText: {
+        en: 'Get Towers',
+        de: 'Türme nehmen',
+        fr: 'Prenez les tours',
+        ja: '塔を踏む',
+        ko: '장판 들어가기',
+        cn: '踩塔',
+      },
+    },
+    {
+      id: 'Puppet Compound Pod R011: Laser',
+      netRegex: NetRegexes.startsUsing({ source: 'Compound Pod', id: '541B', capture: false }),
+      suppressSeconds: 2,
+      // TODO: maybe this could be smarter and we could tell you where to go??
+      infoText: {
+        en: 'Avoid Lasers',
+      },
+    },
+    {
+      id: 'Puppet Puppet 2P Prime Blade Puppet Guaranteed In',
+      netRegex: NetRegexes.startsUsing({ source: 'Puppet 2P', id: '5421', capture: false }),
+      suppressSeconds: 2,
+      // TODO: have only seen this happen for the guaranteed Puppet In at 6250.7 with 4 clones.
+      // TODO: can this happen at other times??
+      alertText: {
+        en: 'Get Under Clone Corner',
+      },
+    },
+    {
+      id: 'Puppet Puppet 2P Prime Blade Puppet In',
+      netRegex: NetRegexes.startsUsing({ source: 'Puppet 2P', id: '519A', capture: false }),
+      suppressSeconds: 2,
+      // TODO: when I've seen this happen at 6379.4, it's been two clones, that start
+      // at corners and then teleport to two cardinals across from each other with fake
+      // teleports on the other cardinals.
+      // TODO: fix this if these clones can go to corners.
+      alertText: {
+        en: 'Get Under Cardinal Clone',
+      },
+    },
+    {
+      id: 'Puppet Puppet 2P Prime Blade Puppet Out Corner',
+      netRegex: NetRegexes.startsUsing({ source: 'Puppet 2P', id: '5198', capture: false }),
+      suppressSeconds: 2,
+      // TODO: have only seen this where the 4 clones stay out at 6379.4.
+      // TODO: have seen a report on a guide that the clones can teleport in??
+      // TODO: fix this if the clones can do something else here
+      response: Responses.goMiddle('alert'),
+    },
   ],
   timelineReplace: [
     {
