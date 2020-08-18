fd0724cac0e8c976bdb23e3be2bc0a409fb0a71b
CHP <una_p@kakao.com>
2020-08-18 10:30:15 +0900

    fix it

--- ui/oopsyraidsy/data/00-misc/buffs.js
+++ ui/oopsyraidsy/data/00-misc/buffs.js
@@ -77,7 +77,7 @@ let missedFunc = (args) => {
             de: thingName + ' verfehlt ' + missed.map((x) => data.ShortName(x)).join(', '),
             fr: thingName + ' manqué(e) sur ' + missed.map((x) => data.ShortName(x)).join(', '),
             cn: thingName + ' 没奶到 ' + missed.map((x) => data.ShortName(x)).join(', '),
-            ko: thingName + '가 ' + missed.map((x) => data.ShortName(x)).join(', ') +'에게 적용되지 않음',
+            ko: thingName + '가 ' + missed.map((x) => data.ShortName(x)).join(', ') + '에게 적용되지 않음',
           },
         };
       }
