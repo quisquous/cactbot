59f1a8a749242442ec49cfdb0da5b18110fc1a8f
Maiko Tan <maiko.tan.coding@gmail.com>
2020-09-15 17:50:00 +0800

    oopsy add ja/cn trans in e7s

--- ui/oopsyraidsy/data/05-shb/raid/e7s.js
+++ ui/oopsyraidsy/data/05-shb/raid/e7s.js
@@ -12,6 +12,8 @@ let wrongBuff = (str) => {
     en: str + ' (wrong buff)',
     de: str + ' (falscher Buff)',
     fr: str + ' (mauvais buff)',
+    ja: str + ' (不適切なバフ)',
+    cn: str + ' (Buff错了)',
     ko: str + ' (버프 틀림)',
   };
 };
@@ -21,6 +23,8 @@ let noBuff = (str) => {
     en: str + ' (no buff)',
     de: str + ' (kein Buff)',
     fr: str + ' (pas de buff)',
+    ja: str + ' (バフ無し)',
+    cn: str + ' (没有Buff)',
     ko: str + ' (버프 없음)',
   };
 };
