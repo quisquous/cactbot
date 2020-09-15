7d5c4b039a7221c6d624533dabe81476d377264f
Maiko Tan <maiko.tan.coding@gmail.com>
2020-09-15 10:53:09 +0800

    oopsy add cn sync in test

--- ui/oopsyraidsy/data/00-misc/test.js
+++ ui/oopsyraidsy/data/00-misc/test.js
@@ -9,6 +9,7 @@
       netRegex: NetRegexes.gameNameLog({ line: 'You bow courteously to the striking dummy.*?' }),
       netRegexFr: NetRegexes.gameNameLog({ line: 'Vous vous inclinez devant le mannequin d\'entraînement.*?' }),
       netRegexJa: NetRegexes.gameNameLog({ line: '.*は木人にお辞儀した.*?' }),
+      netRegexCn: NetRegexes.gameNameLog({ line: '.*恭敬地对木人行礼.*?' }),
       netRegexKo: NetRegexes.gameNameLog({ line: '.*나무인형에게 공손하게 인사합니다.*?' }),
       mistake: function(e, data) {
         return {
@@ -30,6 +31,7 @@
       netRegex: NetRegexes.gameNameLog({ line: 'You bid farewell to the striking dummy.*?' }),
       netRegexFr: NetRegexes.gameNameLog({ line: 'Vous faites vos adieux au mannequin d\'entraînement.*?' }),
       netRegexJa: NetRegexes.gameNameLog({ line: '.*は木人に別れの挨拶をした.*?' }),
+      netRegexCn: NetRegexes.gameNameLog({ line: '.*向木人告别.*?' }),
       netRegexKo: NetRegexes.gameNameLog({ line: '.*나무인형에게 작별 인사를 합니다.*?' }),
       mistake: function(e, data) {
         return {
@@ -90,6 +92,7 @@
       netRegex: NetRegexes.gameNameLog({ line: 'You poke the striking dummy.*?' }),
       netRegexFr: NetRegexes.gameNameLog({ line: 'Vous touchez légèrement le mannequin d\'entraînement du doigt.*?' }),
       netRegexJa: NetRegexes.gameNameLog({ line: '.*は木人をつついた.*?' }),
+      netRegexCn: NetRegexes.gameNameLog({ line: '.*用手指戳向木人.*?' }),
       netRegexKo: NetRegexes.gameNameLog({ line: '.*나무인형을 쿡쿡 찌릅니다.*?' }),
       collectSeconds: 5,
       mistake: function(events, data) {
