88b1d82894f68ef61c3229243b35907cc3feafa7
Jaehyuk-Lee <alex00728@outlook.com>
2020-10-07 11:48:08 +0900

    fix for prefix check

--- test/trigger/test_trigger.js
+++ test/trigger/test_trigger.js
@@ -274,6 +274,8 @@ let testValidIds = function(file, contents) {
   // as the prefix "O4" is not a full word (and have a space after it,
   // as "Prefix " does.  This is a bit rigid, but prevents many typos.
   if (ids.size > 1 && !brokenPrefixes && prefix && prefix.length > 0) {
+    // if prefix includes more than one word, just remove latter letters.
+    prefix.substr(0, prefix.indexOf(" "));
     if (prefix[prefix.length - 1] != ' ')
       errorFunc(`${file}: id prefix '${prefix}' is not a full word, must end in a space`);
   }
