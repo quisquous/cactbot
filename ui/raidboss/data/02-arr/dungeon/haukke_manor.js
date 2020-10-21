0ff1afc472bc42321c44bf8abc3115a87d31c6db
Saulchar <73225889+Saulchar@users.noreply.github.com>
2020-10-21 15:00:58 -0700

    Update haukke_manor.js

--- ui/raidboss/data/02-arr/dungeon/haukke_manor.js
+++ ui/raidboss/data/02-arr/dungeon/haukke_manor.js
@@ -4,52 +4,19 @@
   zoneId: ZoneId.HaukkeManor,
   triggers: [
     {
-      id: 'Haukke Normal Dark Mist DPS/Heals',
-      netRegex: NetRegexes.startsUsing({ id: '2C1', source: ['Manor Maidservant', 'Manor Claviger', 'Lady Amandine'], capture: false }),
-      condition: function(data) {
-        return data.role != 'tank';
-      },
-      suppressSeconds: 2,
-      response: Responses.getOut('alert'),
-    },
-    {
-      id: 'Haukke Normal Dark Mist Tank',
+      id: 'Haukke Normal Dark Mist Stun',
       netRegex: NetRegexes.startsUsing({ id: '2C1', source: ['Manor Maidservant', 'Manor Claviger', 'Lady Amandine'] }),
       condition: function(data) {
-        return data.role == 'tank';
+        return data.canStun();
       },
       suppressSeconds: 2,
       response: Responses.stun('info'),
     },
     {
-      id: 'Haukke Normal Claviger Void Fire II DPS/Heals',
-      netRegex: NetRegexes.startsUsing({ id: '357', source: 'Manor Claviger', capture: false }),
-      condition: function(data) {
-        return data.role != 'tank';
-      },
-      response: Responses.getOut('alert'),
-    },
-    {
-      id: 'Haukke Normal Claviger Void Fire II Tank',
-      netRegex: NetRegexes.startsUsing({ id: '357', source: 'Manor Claviger' }),
-      condition: function(data) {
-        return data.role == 'tank';
-      },
-      response: Responses.interrupt('info'),
-    },
-    {
-      id: 'Haukke Normal Steward Soul Drain DPS/Heals',
-      netRegex: NetRegexes.startsUsing({ id: '35C', source: 'Manor Steward', capture: false }),
-      condition: function(data) {
-        return data.role != 'tank';
-      },
-      response: Responses.outOfMelee('alert'),
-    },
-    {
-      id: 'Haukke Normal Steward Soul Drain Tank',
+      id: 'Haukke Normal Steward Soul Drain Stun',
       netRegex: NetRegexes.startsUsing({ id: '35C', source: 'Manor Steward' }),
       condition: function(data) {
-        return data.role == 'tank';
+        return data.canStun();
       },
       response: Responses.stun('info'),
     },
@@ -57,7 +24,7 @@
       id: 'Haukke Normal Amandine Void Fire III',
       netRegex: NetRegexes.startsUsing({ id: '356', source: 'Lady Amandine' }),
       condition: function(data) {
-        return data.role == 'tank';
+        return data.canInterrupt();
       },
       response: Responses.interrupt('info'),
     },
@@ -65,7 +32,7 @@
       id: 'Haukke Normal Amandine Void Thunder III',
       netRegex: NetRegexes.startsUsing({ id: '358', source: 'Manor Claviger' }),
       condition: function(data) {
-        return data.role == 'tank';
+        return data.canInterrupt();
       },
       response: Responses.interrupt('info'),
     },
@@ -82,12 +49,7 @@
       id: 'Haukke Normal Ladys Candle',
       netRegex: NetRegexes.addedCombatant({ name: 'Lady\'s Candle', capture: false }),
       response: Responses.killAdds('info'),
-    },
-    {
-      id: 'Haukke Normal Ladys Candle Self Destruct',
-      netRegex: NetRegexes.startsUsing({ id: '151', source: 'Lady\'s Candle', capture: false }),
-      response: Responses.getOut('alert'),
-    },
+    }
     {
       id: 'Haukke Normal Ladys Handmaiden',
       netRegex: NetRegexes.addedCombatant({ name: 'Lady\'s Handmaiden', capture: false }),
