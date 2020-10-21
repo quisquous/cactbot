946fbfded1a19ea20ab8f8fffb753029517c0ffa
Saulchar <73225889+Saulchar@users.noreply.github.com>
2020-10-21 16:25:40 -0700

    Update haukke_manor.js

--- ui/raidboss/data/02-arr/dungeon/haukke_manor.js
+++ ui/raidboss/data/02-arr/dungeon/haukke_manor.js
@@ -4,6 +4,7 @@
   zoneId: ZoneId.HaukkeManor,
   triggers: [
     {
+      // Dark Mist Stun with Suppression for duplicate messages
       id: 'Haukke Normal Dark Mist Stun',
       netRegex: NetRegexes.startsUsing({ id: '2C1', source: ['Manor Maidservant', 'Manor Claviger', 'Lady Amandine'] }),
       condition: function(data) {
@@ -13,6 +14,7 @@
       response: Responses.stun('info'),
     },
     {
+      // Soul Drain
       id: 'Haukke Normal Steward Soul Drain Stun',
       netRegex: NetRegexes.startsUsing({ id: '35C', source: 'Manor Steward' }),
       condition: function(data) {
@@ -21,6 +23,7 @@
       response: Responses.stun('info'),
     },
     {
+      // Amandine Specific Dark Mist
       id: 'Haukke Normal Amandine Dark Mist Dodge',
       netRegex: NetRegexes.startsUsing({ id: '2C1', source: 'Lady Amandine' }),
       condition: function(data) {
@@ -29,6 +32,7 @@
       response: Responses.outOfMelee('alert'),
     },
     {
+      // Void Fire 3
       id: 'Haukke Normal Amandine Void Fire III',
       netRegex: NetRegexes.startsUsing({ id: '356', source: 'Lady Amandine' }),
       condition: function(data) {
@@ -37,6 +41,7 @@
       response: Responses.interrupt('info'),
     },
     {
+      // Void Thunder 3
       id: 'Haukke Normal Amandine Void Thunder III',
       netRegex: NetRegexes.startsUsing({ id: '358', source: 'Manor Claviger' }),
       condition: function(data) {
@@ -64,6 +69,7 @@
       // 2 Lady's Handmaiden and 1 Manor Sentry Spawn
       // The sentry outside the bosses room loads when you enter the zone.
       // This causes the trigger to go off early, parsing for the Handmaiden fixes the problem.
+      // Suppression included since 2 Handmaiden's spawn at the same time
       id: 'Haukke Normal Ladys Handmaiden',
       netRegex: NetRegexes.addedCombatantFull({ npcNameId: '424', capture: false }),
       suppressSeconds: 2,
