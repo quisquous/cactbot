e537df6d90f9c7dfe531669786ca8e8fbcc012af
Akurosia <kamothegamer@web.de>
2020-11-02 11:19:27 +0100

    made wake up into a response

--- ui/raidboss/data/05-shb/eureka/bozjan_southern_front.js
+++ ui/raidboss/data/05-shb/eureka/bozjan_southern_front.js
@@ -10,15 +10,7 @@
       netRegexFr: NetRegexes.gameLog({ line: 'Votre personnage est inactif depuis 5 minutes.*?', capture: false }),
       netRegexCn: NetRegexes.gameLog({ line: '已经5分钟没有进行任何操作.*?', capture: false }),
       netRegexKo: NetRegexes.gameLog({ line: '5분 동안 아무 조작을 하지 않았습니다..*?', capture: false }),
-      // TODO: add a response for this.
-      alarmText: {
-        en: 'WAKE UP',
-        de: 'AUFWACHEN',
-        fr: 'RÉVEILLES-TOI',
-        ja: '目を覚めて！',
-        cn: '醒醒！动一动！！',
-        ko: '강제 퇴장 5분 전',
-      },
+      response: Responses.wakeUp(),
     },
   ],
 }];
