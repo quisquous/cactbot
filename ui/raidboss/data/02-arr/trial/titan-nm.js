5aacb16db8510ae4c3737a62bae4963f89436429
Saulchar <73225889+Saulchar@users.noreply.github.com>
2020-11-02 18:28:35 -0800

    raidboss: Titan Normal (#1920)

---
+++ ui/raidboss/data/02-arr/trial/titan-nm.js
@@ -0,0 +1,39 @@
+'use strict';
+
+[{
+  zoneId: ZoneId.TheNavel,
+  timelineFile: 'titan-nm.txt',
+  timelineTriggers: [
+    {
+      // Early Callout for Tank Cleave
+      id: 'TitanNm Rock Buster',
+      regex: /Rock Buster/,
+      beforeSeconds: 5,
+      response: Responses.tankCleave(),
+    },
+  ],
+  triggers: [
+    {
+      id: 'TitanNm Tumult',
+      netRegex: NetRegexes.ability({ id: '282', source: 'Titan', capture: false }),
+      condition: Conditions.caresAboutAOE(),
+      suppressSeconds: 2,
+      response: Responses.aoe(),
+    },
+    {
+      // Gaol callout for both yourself and others
+      id: 'TitanNm Gaols',
+      netRegex: NetRegexes.gainsEffect({ effectId: '124' }),
+      alertText: function(data, matches) {
+        if (matches.target !== data.me) {
+          return {
+            en: 'Break Gaol on ' + data.ShortName(matches.target),
+          };
+        }
+        return {
+          en: 'Gaol on YOU',
+        };
+      },
+    },
+  ],
+}];
