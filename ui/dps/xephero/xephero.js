7917d172e445f1a7cc14656f85df1bd5fe6b8793
Panic Stevenson <panic.stevenson@gmail.com>
2020-11-15 19:42:09 -0800

    Remove Unused Import

--- ui/dps/xephero/xephero.js
+++ ui/dps/xephero/xephero.js
@@ -4,8 +4,6 @@ import DpsPhaseTracker from './dps_phase_tracker.js';
 import { InitDpsModule, Options } from '../dps_common.js';
 import UserConfig from '../../../resources/user_config.js';
 
-import '../../../resources/zone_id.js';
-
 let rows = 10;
 let rdpsMax = 0;
 
