585ac4a53f282a6228573320f83a7d32c4975039
Echoring <68432572+Echoring@users.noreply.github.com>
2020-11-20 22:17:21 +0800

    minor fix

--- ui/jobs/jobs.js
+++ ui/jobs/jobs.js
@@ -1617,10 +1617,10 @@ class Bars {
     timerBox.hideafter = '';
     timerBox.roundupthreshold = false;
     timerBox.valuescale = options.scale ? options.scale : 1;
-    if (options.id)
+    if (options.id) {
       timerBox.id = options.id;
       timerBox.classList.add('timerbox');
-
+    }
     return timerBox;
   }
 
@@ -1632,7 +1632,7 @@ class Bars {
     let timer = document.createElement('timer-bar');
     container.appendChild(timerDiv);
     timerDiv.appendChild(timer);
-    timer.classList.add('timerbar')
+    timer.classList.add('timerbar');
 
     timer.width = window.getComputedStyle(timerDiv).width;
     timer.height = window.getComputedStyle(timerDiv).height;
