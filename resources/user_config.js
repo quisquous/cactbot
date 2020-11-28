quisquous/cactbot

commit d7a1b4205ac5055001a7134d0c2e831cf3d8ba28

Convert trigger files into modules (#2054)
Co-authored-by: Panic Stevenson <panic.stevenson@gmail.com>

ui/raidboss/raidboss_config.js
@@ -1,26 +1,9 @@
-// TODO:
-// The convention of "import X as _X; const X = _X;" is currently
-// being used as a method to workaround for downstream code
-// that is running via eval(). Because importing statements do not
-// create a variable of the same name, the eval()'d code does not know
-// about the import, and thus throws ReferenceErrors.
 import PartyTracker from '../../resources/party.js';
-import _Regexes from '../../resources/regexes.js';
-const Regexes = _Regexes;
+import Regexes from '../../resources/regexes.js';
+import { triggerOutputFunctions } from '../../resources/responses.js';
 import UserConfig from '../../resources/user_config.js';
 import { Util } from '../../resources/common.js';
 import raidbossFileData from './data/manifest.txt';
-
-// Used by downstream eval
-import _Conditions from '../../resources/conditions.js';
-const Conditions = _Conditions;
-import _NetRegexes from '../../resources/netregexes.js';
-const NetRegexes = _NetRegexes;
-import { Responses as _Responses, triggerOutputFunctions } from '../../resources/responses.js';
-const Responses = _Responses;
-import _ZoneId from '../../resources/zone_id.js';
-const ZoneId = _ZoneId;
-
 
 const kOptionKeys = {
   output: 'Output',
@@ -688,12 +671,11 @@ class RaidbossConfigurator {
         trigger: [],
         timeline: [],
       };
-      for (const triggerSet of item.json) {
-        if (triggerSet.triggers)
-          rawTriggers.trigger.push(...triggerSet.triggers);
-        if (triggerSet.timelineTriggers)
-          rawTriggers.timeline.push(...triggerSet.timelineTriggers);
-      }
+      const triggerSet = item.triggerSet;
+      if (triggerSet.triggers)
+        rawTriggers.trigger.push(...triggerSet.triggers);
+      if (triggerSet.timelineTriggers)
+        rawTriggers.timeline.push(...triggerSet.timelineTriggers);
 
       item.triggers = {};
       for (const key in rawTriggers) {

commit a1f3d8bdee72602d4ed21621b5948e2b1b906921

resources: consolidate user file eval into user_config.js
User file triggers have been broken since #2054 (oops).
This consolidates all evals of user files into a single spot
so that any additional files we import for triggers end up here.

TODO: it'd be nice to test that all imports in triggers are
represented here??

resources/user_config.js
@@ -112,12 +112,11 @@ class UserConfig {
         if (jsFile in localFiles) {
           try {
             printUserFile('local user file: ' + basePath + '\\' + jsFile);
-            if (this.userFileCallbacks[overlayName]) {
+            const Options = options;
+            eval(localFiles[jsFile]);
+
+            if (this.userFileCallbacks[overlayName])
               this.userFileCallbacks[overlayName](jsFile, localFiles, options);
-            } else {
-              const Options = options;
-              eval(localFiles[jsFile]);
-            }
           } catch (e) {
             // Be very visible for users.
             console.log('*** ERROR IN USER FILE ***');

ui/raidboss/raidboss_config.js
@@ -740,9 +740,6 @@ class RaidbossConfigurator {
 
 // Raidboss needs to do some extra processing of user files.
 const userFileHandler = (name, files, options) => {
-  const Options = options;
-  eval(files[name]);
-
   if (!options.Triggers)
     return;
 