628a4f65344e613684a4c8c9388813b06ec9d598
Adrienne Walker <enne@quisquo.us>
2020-12-07 11:01:55 -0800

    raidboss: add Eden's Promise files

---
+++ ui/raidboss/data/05-shb/raid/e12s.js
@@ -0,0 +1,8 @@
+import ZoneId from '../../../../../resources/zone_id.js';
+
+export default {
+  zoneId: ZoneId.EdensPromiseEternitySavage,
+  timelineFile: 'e12s.txt',
+  triggers: [
+  ],
+};
---
+++ ui/raidboss/data/05-shb/raid/e9s.js
@@ -0,0 +1,8 @@
+import ZoneId from '../../../../../resources/zone_id.js';
+
+export default {
+  zoneId: ZoneId.EdensPromiseUmbraSavage,
+  timelineFile: 'e9s.txt',
+  triggers: [
+  ],
+};
