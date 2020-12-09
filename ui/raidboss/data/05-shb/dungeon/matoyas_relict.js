c51804e54e2e26a8ff2ce6c0e9b86bab6a459b0a
Maiko Tan <maiko.tan.coding@gmail.com>
2020-12-09 13:58:57 +0800

    fix all error

--- ui/raidboss/data/05-shb/dungeon/matoyas_relict.js
+++ ui/raidboss/data/05-shb/dungeon/matoyas_relict.js
@@ -32,10 +32,13 @@ export default {
       netRegexDe: NetRegexes.ability({ id: '5482', source: 'Matschmann', capture: false }),
       netRegexFr: NetRegexes.ability({ id: '5482', source: 'tadboue', capture: false }),
       netRegexJa: NetRegexes.ability({ id: '5482', source: 'マッドマン', capture: false }),
-      alertText: {
-        en: 'Pull orb to an empty hole',
-        ja: '泥団子を四隅の穴に誘導',
-        cn: '诱导泥球到无敌人的风圈',
+      alertText: (data, _, output) => output.pullOrb(),
+      outputStrings: {
+        pullOrb: {
+          en: 'Pull orb to an empty hole',
+          ja: '泥団子を四隅の穴に誘導',
+          cn: '诱导泥球到无敌人的风圈',
+        },
       },
     },
     {
@@ -53,10 +56,13 @@ export default {
       netRegexDe: NetRegexes.startsUsing({ id: '598F', source: 'Nixchen', capture: false }),
       netRegexFr: NetRegexes.startsUsing({ id: '598F', source: 'nixe', capture: false }),
       netRegexJa: NetRegexes.startsUsing({ id: '598F', source: 'ノッケン', capture: false }),
-      alertText: {
-        en: 'Away from Tank and his Tether',
-        ja: 'タンクや線に離れ',
-        cn: '远离坦克及其连线',
+      alertText: (data, _, output) => output.awayFrom(),
+      outputStrings: {
+        awayFrom: {
+          en: 'Away from Tank and his Tether',
+          ja: 'タンクや線に離れ',
+          cn: '远离坦克及其连线',
+        },
       },
     },
     {
@@ -65,10 +71,13 @@ export default {
       netRegexDe: NetRegexes.startsUsing({ id: '5991', source: 'Nixchen', capture: false }),
       netRegexFr: NetRegexes.startsUsing({ id: '5991', source: 'nixe', capture: false }),
       netRegexJa: NetRegexes.startsUsing({ id: '5991', source: 'ノッケン', capture: false }),
-      alertText: {
-        en: 'Move to ...',
-        ja: '光ってない横列に移動',
-        cn: '站在墙壁未发光的一列',
+      alertText: (data, _, output) => output.moveTo(),
+      outputStrings: {
+        moveTo: {
+          en: 'Move to ...',
+          ja: '光ってない横列に移動',
+          cn: '站在墙壁未发光的一列',
+        },
       },
     },
     {
@@ -78,10 +87,13 @@ export default {
       netRegexFr: NetRegexes.ability({ id: '5991', source: 'nixe', capture: false }),
       netRegexJa: NetRegexes.ability({ id: '5991', source: 'ノッケン', capture: false }),
       delaySeconds: 3,
-      infoText: {
-        en: 'Step in Puddle near the Cloud',
-        ja: '雲に近い水を踏む',
-        cn: '站在靠近云朵的水流里等待浮空',
+      infoText: (data, _, output) => output.stepIn(),
+      outputStrings: {
+        stepIn: {
+          en: 'Step in Puddle near the Cloud',
+          ja: '雲に近い水を踏む',
+          cn: '站在靠近云朵的水流里等待浮空',
+        },
       },
     },
     {
@@ -100,10 +112,13 @@ export default {
       netRegexDe: NetRegexes.startsUsing({ id: '5919', source: 'Muttersau', capture: false }),
       netRegexFr: NetRegexes.startsUsing({ id: '5919', source: 'mère porxie', capture: false }),
       netRegexJa: NetRegexes.startsUsing({ id: '5919', source: 'マザーポークシー', capture: false }),
-      alarmText: {
-        en: 'Stand in front of Boss (anti-knockback not usable)',
-        ja: 'ボスの正面に (堅実魔無効)',
-        cn: '站在Boss正面 (防击退无效)',
+      alarmText: (data, _, output) => output.standFront(),
+      outputStrings: {
+        standFront: {
+          en: 'Stand in front of Boss (anti-knockback not usable)',
+          ja: 'ボスの正面に (堅実魔無効)',
+          cn: '站在Boss正面 (防击退无效)',
+        },
       },
     },
     {
@@ -112,10 +127,13 @@ export default {
       netRegexDe: NetRegexes.startsUsing({ id: '5916', source: 'Muttersau', capture: false }),
       netRegexFr: NetRegexes.startsUsing({ id: '5916', source: 'mère porxie', capture: false }),
       netRegexJa: NetRegexes.startsUsing({ id: '5916', source: 'マザーポークシー', capture: false }),
-      alarmText: {
-        en: 'Away From AoE',
-        ja: '反対側へ',
-        cn: '对面躲避坠落',
+      alarmText: (data, _, output) => output.awayFromAoe(),
+      outputStrings: {
+        awayFromAoe: {
+          en: 'Away From AoE',
+          ja: '反対側へ',
+          cn: '对面躲避坠落',
+        },
       },
     },
     {
@@ -125,27 +143,33 @@ export default {
       netRegexFr: NetRegexes.ability({ id: '5916', source: 'mère porxie', capture: false }),
       netRegexJa: NetRegexes.ability({ id: '5916', source: 'マザーポークシー', capture: false }),
       delaySeconds: 5,
-      alarmText: {
-        en: 'Away From Boss',
-        ja: 'ボスに離れ',
-        cn: '远离Boss所在的行',
+      alarmText: (data, _, output) => output.awayFrom(),
+      outputStrings: {
+        awayFrom: {
+          en: 'Away From Boss',
+          ja: 'ボスに離れ',
+          cn: '远离Boss所在的行',
+        },
       },
     },
     {
       id: 'Matoyas Porxie Sucked In',
-      netRegex: NetRegexes.gainsEffect({ effectId: '5916' }),
-      alarmText: {
-        en: 'RUN AWAY',
-        ja: 'ボスに離れ',
-        cn: '远离即死区',
+      netRegex: NetRegexes.gainsEffect({ effectId: '5916', capture: false }),
+      alarmText: (data, _, output) => output.runAway(),
+      outputStrings: {
+        runAway: {
+          en: 'RUN AWAY',
+          ja: 'ボスに離れ',
+          cn: '远离即死区',
+        },
       },
     },
     {
       id: 'Matoyas Porxie Minced Meat',
-      netRegex: NetRegexes.startsUsing({ id: '5911', source: 'Mother Porxie', capture: false }),
-      netRegexDe: NetRegexes.startsUsing({ id: '5911', source: 'Muttersau', capture: false }),
-      netRegexFr: NetRegexes.startsUsing({ id: '5911', source: 'mère porxie', capture: false }),
-      netRegexJa: NetRegexes.startsUsing({ id: '5911', source: 'マザーポークシー', capture: false }),
+      netRegex: NetRegexes.startsUsing({ id: '5911', source: 'Mother Porxie' }),
+      netRegexDe: NetRegexes.startsUsing({ id: '5911', source: 'Muttersau' }),
+      netRegexFr: NetRegexes.startsUsing({ id: '5911', source: 'mère porxie' }),
+      netRegexJa: NetRegexes.startsUsing({ id: '5911', source: 'マザーポークシー' }),
       condition: function(data, matches) {
         return data.me === matches.target || data.role === 'healer';
       },
@@ -158,10 +182,13 @@ export default {
       netRegexFr: NetRegexes.ability({ id: '4E34', source: 'élémentaire des cavernes venteuses', capture: false }),
       netRegexJa: NetRegexes.ability({ id: '4E34', source: 'ウィンドケイブ・スプライト', capture: false }),
       delaySeconds: 2,
-      alarmText: {
-        en: 'Remember where Boss is and move there quickly',
-        ja: 'ボスの居場所に移動',
-        cn: '升空时记住Boss的位置然后迅速靠近',
+      alarmText: (data, _, output) => output.move(),
+      outputStrings: {
+        move: {
+          en: 'Remember where Boss is and move there quickly',
+          ja: 'ボスの居場所に移動',
+          cn: '升空时记住Boss的位置然后迅速靠近',
+        },
       },
     },
     {
