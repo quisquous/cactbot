f9bd723370c979798c834d02c5e2753938868644
Akurosia <kamothegamer@web.de>
2020-12-09 20:37:26 +0100

    add e9n translation

--- ui/raidboss/data/05-shb/raid/e9n.js
+++ ui/raidboss/data/05-shb/raid/e9n.js
@@ -10,6 +10,11 @@ export default {
     {
       id: 'E9N Ground-Razing Particle Beam',
       netRegex: NetRegexes.startsUsing({ id: '55ED', source: 'Cloud Of Darkness', capture: false }),
+      netRegexDe: NetRegexes.startsUsing({ id: '55ED', source: 'Wolke Der Dunkelheit', capture: false }),
+      netRegexFr: NetRegexes.startsUsing({ id: '55ED', source: 'Nuage De Ténèbres', capture: false }),
+      netRegexJa: NetRegexes.startsUsing({ id: '55ED', source: '暗闇の雲', capture: false }),
+      netRegexCn: NetRegexes.startsUsing({ id: '55ED', source: '暗黑之云', capture: false }),
+      netRegexKo: NetRegexes.startsUsing({ id: '55ED', source: '어둠의 구름', capture: false }),
       condition: Conditions.caresAboutAOE(),
       durationSeconds: 5,
       response: Responses.aoe(),
@@ -17,29 +22,54 @@ export default {
     {
       id: 'E9N The Art Of Darkness Right',
       netRegex: NetRegexes.startsUsing({ id: '5223', source: 'Cloud Of Darkness', capture: false }),
+      netRegexDe: NetRegexes.startsUsing({ id: '5223', source: 'Wolke Der Dunkelheit', capture: false }),
+      netRegexFr: NetRegexes.startsUsing({ id: '5223', source: 'Nuage De Ténèbres', capture: false }),
+      netRegexJa: NetRegexes.startsUsing({ id: '5223', source: '暗闇の雲', capture: false }),
+      netRegexCn: NetRegexes.startsUsing({ id: '5223', source: '暗黑之云', capture: false }),
+      netRegexKo: NetRegexes.startsUsing({ id: '5223', source: '어둠의 구름', capture: false }),
       durationSeconds: 4,
       response: Responses.goLeft(),
     },
     {
       id: 'E9N The Art Of Darkness Left',
       netRegex: NetRegexes.startsUsing({ id: '5224', source: 'Cloud Of Darkness', capture: false }),
+      netRegexDe: NetRegexes.startsUsing({ id: '5224', source: 'Wolke Der Dunkelheit', capture: false }),
+      netRegexFr: NetRegexes.startsUsing({ id: '5224', source: 'Nuage De Ténèbres', capture: false }),
+      netRegexJa: NetRegexes.startsUsing({ id: '5224', source: '暗闇の雲', capture: false }),
+      netRegexCn: NetRegexes.startsUsing({ id: '5224', source: '暗黑之云', capture: false }),
+      netRegexKo: NetRegexes.startsUsing({ id: '5224', source: '어둠의 구름', capture: false }),
       durationSeconds: 4,
       response: Responses.goRight(),
     },
     {
       id: 'E9N Wide-Angle Particle Beam',
       netRegex: NetRegexes.startsUsing({ id: '5AFF', source: 'Cloud Of Darkness', capture: false }),
+      netRegexDe: NetRegexes.startsUsing({ id: '5AFF', source: 'Wolke Der Dunkelheit', capture: false }),
+      netRegexFr: NetRegexes.startsUsing({ id: '5AFF', source: 'Nuage De Ténèbres', capture: false }),
+      netRegexJa: NetRegexes.startsUsing({ id: '5AFF', source: '暗闇の雲', capture: false }),
+      netRegexCn: NetRegexes.startsUsing({ id: '5AFF', source: '暗黑之云', capture: false }),
+      netRegexKo: NetRegexes.startsUsing({ id: '5AFF', source: '어둠의 구름', capture: false }),
       response: Responses.awayFromFront('info'),
     },
     {
       id: 'E9N Zero-Form Particle Beam',
       netRegex: NetRegexes.startsUsing({ id: '55EB', source: 'Cloud Of Darkness' }),
+      netRegexDe: NetRegexes.startsUsing({ id: '55EB', source: 'Wolke Der Dunkelheit' }),
+      netRegexFr: NetRegexes.startsUsing({ id: '55EB', source: 'Nuage De Ténèbres' }),
+      netRegexJa: NetRegexes.startsUsing({ id: '55EB', source: '暗闇の雲' }),
+      netRegexCn: NetRegexes.startsUsing({ id: '55EB', source: '暗黑之云' }),
+      netRegexKo: NetRegexes.startsUsing({ id: '55EB', source: '어둠의 구름' }),
       condition: Conditions.caresAboutMagical(),
       response: Responses.tankBuster(),
     },
     {
       id: 'E9N Empty Plane',
       netRegex: NetRegexes.startsUsing({ id: '4FC6', source: 'Cloud Of Darkness', capture: false }),
+      netRegexDe: NetRegexes.startsUsing({ id: '4FC6', source: 'Wolke Der Dunkelheit', capture: false }),
+      netRegexFr: NetRegexes.startsUsing({ id: '4FC6', source: 'Nuage De Ténèbres', capture: false }),
+      netRegexJa: NetRegexes.startsUsing({ id: '4FC6', source: '暗闇の雲', capture: false }),
+      netRegexCn: NetRegexes.startsUsing({ id: '4FC6', source: '暗黑之云', capture: false }),
+      netRegexKo: NetRegexes.startsUsing({ id: '4FC6', source: '어둠의 구름', capture: false }),
       condition: Conditions.caresAboutAOE(),
       durationSeconds: 5,
       response: Responses.aoe(),
@@ -47,6 +77,11 @@ export default {
     {
       id: 'E9N Obscure Woods',
       netRegex: NetRegexes.startsUsing({ id: '4FA2', source: 'Cloud Of Darkness', capture: false }),
+      netRegexDe: NetRegexes.startsUsing({ id: '4FA2', source: 'Wolke Der Dunkelheit', capture: false }),
+      netRegexFr: NetRegexes.startsUsing({ id: '4FA2', source: 'Nuage De Ténèbres', capture: false }),
+      netRegexJa: NetRegexes.startsUsing({ id: '4FA2', source: '暗闇の雲', capture: false }),
+      netRegexCn: NetRegexes.startsUsing({ id: '4FA2', source: '暗黑之云', capture: false }),
+      netRegexKo: NetRegexes.startsUsing({ id: '4FA2', source: '어둠의 구름', capture: false }),
       condition: Conditions.caresAboutAOE(),
       durationSeconds: 5,
       response: Responses.aoe(),
@@ -54,6 +89,11 @@ export default {
     {
       id: 'E9N Waste Away',
       netRegex: NetRegexes.startsUsing({ id: '55DE', source: 'Cloud Of Darkness', capture: false }),
+      netRegexDe: NetRegexes.startsUsing({ id: '55DE', source: 'Wolke Der Dunkelheit', capture: false }),
+      netRegexFr: NetRegexes.startsUsing({ id: '55DE', source: 'Nuage De Ténèbres', capture: false }),
+      netRegexJa: NetRegexes.startsUsing({ id: '55DE', source: '暗闇の雲', capture: false }),
+      netRegexCn: NetRegexes.startsUsing({ id: '55DE', source: '暗黑之云', capture: false }),
+      netRegexKo: NetRegexes.startsUsing({ id: '55DE', source: '어둠의 구름', capture: false }),
       response: Responses.knockback('info'),
     },
     {
@@ -64,6 +104,7 @@ export default {
       outputStrings: {
         text: {
           en: 'Wait Near Bramble',
+          de: 'Warte in der Nähe des Dornenstrauch',
         },
       },
     },
@@ -77,29 +118,46 @@ export default {
     {
       id: 'E9N Wide-Angle Phaser',
       netRegex: NetRegexes.startsUsing({ id: ['55DF', '55E[01]'], source: 'Cloud Of Darkness', capture: false }),
+      netRegexDe: NetRegexes.startsUsing({ id: ['55DF', '55E[01]'], source: 'Wolke Der Dunkelheit', capture: false }),
+      netRegexFr: NetRegexes.startsUsing({ id: ['55DF', '55E[01]'], source: 'Nuage De Ténèbres', capture: false }),
+      netRegexJa: NetRegexes.startsUsing({ id: ['55DF', '55E[01]'], source: '暗闇の雲', capture: false }),
+      netRegexCn: NetRegexes.startsUsing({ id: ['55DF', '55E[01]'], source: '暗黑之云', capture: false }),
+      netRegexKo: NetRegexes.startsUsing({ id: ['55DF', '55E[01]'], source: '어둠의 구름', capture: false }),
       durationSeconds: 5,
       suppressSeconds: 1,
       alertText: (data, _, output) => output.text(),
       outputStrings: {
         text: {
           en: 'Go Sides At Tethered Wall',
+          de: 'Geh seitlich der verundenen Wände',
         },
       },
     },
     {
       id: 'E9N Rejuvenating Balm',
       netRegex: NetRegexes.startsUsing({ id: '55E2', source: 'Cloud Of Darkness', capture: false }),
+      netRegexDe: NetRegexes.startsUsing({ id: '55E2', source: 'Wolke Der Dunkelheit', capture: false }),
+      netRegexFr: NetRegexes.startsUsing({ id: '55E2', source: 'Nuage De Ténèbres', capture: false }),
+      netRegexJa: NetRegexes.startsUsing({ id: '55E2', source: '暗闇の雲', capture: false }),
+      netRegexCn: NetRegexes.startsUsing({ id: '55E2', source: '暗黑之云', capture: false }),
+      netRegexKo: NetRegexes.startsUsing({ id: '55E2', source: '어둠의 구름', capture: false }),
       durationSeconds: 5,
       infoText: (data, _, output) => output.text(),
       outputStrings: {
         text: {
           en: 'Away From Tethered Walls',
+          de: 'Weg von den verbundenen Wänden',
         },
       },
     },
     {
       id: 'E9N Deluge Of Darkness',
       netRegex: NetRegexes.startsUsing({ id: '5155', source: 'Cloud Of Darkness', capture: false }),
+      netRegexDe: NetRegexes.startsUsing({ id: '5155', source: 'Wolke Der Dunkelheit', capture: false }),
+      netRegexFr: NetRegexes.startsUsing({ id: '5155', source: 'Nuage De Ténèbres', capture: false }),
+      netRegexJa: NetRegexes.startsUsing({ id: '5155', source: '暗闇の雲', capture: false }),
+      netRegexCn: NetRegexes.startsUsing({ id: '5155', source: '暗黑之云', capture: false }),
+      netRegexKo: NetRegexes.startsUsing({ id: '5155', source: '어둠의 구름', capture: false }),
       condition: Conditions.caresAboutAOE(),
       durationSeconds: 5,
       response: Responses.aoe(),
@@ -107,6 +165,11 @@ export default {
     {
       id: 'E9N Particle Concentration',
       netRegex: NetRegexes.startsUsing({ id: '55E8', source: 'Cloud Of Darkness', capture: false }),
+      netRegexDe: NetRegexes.startsUsing({ id: '55E8', source: 'Wolke Der Dunkelheit', capture: false }),
+      netRegexFr: NetRegexes.startsUsing({ id: '55E8', source: 'Nuage De Ténèbres', capture: false }),
+      netRegexJa: NetRegexes.startsUsing({ id: '55E8', source: '暗闇の雲', capture: false }),
+      netRegexCn: NetRegexes.startsUsing({ id: '55E8', source: '暗黑之云', capture: false }),
+      netRegexKo: NetRegexes.startsUsing({ id: '55E8', source: '어둠의 구름', capture: false }),
       delaySeconds: 4,
       durationSeconds: 4,
       infoText: (data, _, output) => output.text(),
@@ -124,6 +187,12 @@ export default {
     {
       id: 'E9N Hypercharged Condensation',
       netRegex: NetRegexes.startsUsing({ id: '532E', source: 'Cloud Of Darkness', capture: false }),
+      netRegexDe: NetRegexes.startsUsing({ id: '532E', source: 'Wolke Der Dunkelheit', capture: false }),
+      netRegexFr: NetRegexes.startsUsing({ id: '532E', source: 'Nuage De Ténèbres', capture: false }),
+      netRegexJa: NetRegexes.startsUsing({ id: '532E', source: '暗闇の雲', capture: false }),
+      netRegexCn: NetRegexes.startsUsing({ id: '532E', source: '暗黑之云', capture: false }),
+      netRegexKo: NetRegexes.startsUsing({ id: '532E', source: '어둠의 구름', capture: false }),
+
       delaySeconds: 2,
       durationSeconds: 5,
       response: Responses.killAdds(),
@@ -131,8 +200,44 @@ export default {
     {
       id: 'E9N Anti-Air Particle Beam',
       netRegex: NetRegexes.startsUsing({ id: '55DC', source: 'Cloud Of Darkness', capture: false }),
+      netRegexDe: NetRegexes.startsUsing({ id: '55DC', source: 'Wolke Der Dunkelheit', capture: false }),
+      netRegexFr: NetRegexes.startsUsing({ id: '55DC', source: 'Nuage De Ténèbres', capture: false }),
+      netRegexJa: NetRegexes.startsUsing({ id: '55DC', source: '暗闇の雲', capture: false }),
+      netRegexCn: NetRegexes.startsUsing({ id: '55DC', source: '暗黑之云', capture: false }),
+      netRegexKo: NetRegexes.startsUsing({ id: '55DC', source: '어둠의 구름', capture: false }),
       response: Responses.getOut('info'),
     },
   ],
-  timelineReplace: [],
+  timelineReplace: [
+    {
+      'locale': 'de',
+      'replaceSync': {
+        'Cloud Of Darkness': 'Wolke der Dunkelheit',
+      },
+    },
+    {
+      'locale': 'fr',
+      'replaceSync': {
+        'Cloud Of Darkness': 'Nuage de Ténèbres',
+      },
+    },
+    {
+      'locale': 'ja',
+      'replaceSync': {
+        'Cloud Of Darkness': '暗闇の雲',
+      },
+    },
+    {
+      'locale': 'cn',
+      'replaceSync': {
+        'Cloud Of Darkness': '暗黑之云',
+      },
+    },
+    {
+      'locale': 'ko',
+      'replaceSync': {
+        'Cloud Of Darkness': '어둠의 구름',
+      },
+    },
+  ],
 };
