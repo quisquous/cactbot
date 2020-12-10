4e5d81a90d88bb26c04e2b76e2210b11ca9fddee
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2020-12-11 02:16:03 +0800

    Update matoyas_relict.js

--- ui/raidboss/data/05-shb/dungeon/matoyas_relict.js
+++ ui/raidboss/data/05-shb/dungeon/matoyas_relict.js
@@ -140,7 +140,7 @@ export default {
       alertText: (data, _, output) => output.awayFromAoe(),
       outputStrings: {
         awayFromAoe: {
-          en: 'Away From AoE',
+          en: 'Go to Opposite Side',
           ja: '反対側へ',
           cn: '对面躲避坠落',
         },
