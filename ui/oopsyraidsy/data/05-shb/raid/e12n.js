6c35fdf6a743d2d8ca8b2ebf24dd56551c1540d9
Adrienne Walker <enne@quisquo.us>
2020-12-21 20:19:41 -0800

    oopsy: add e12n warnings

--- ui/oopsyraidsy/data/05-shb/raid/e12n.js
+++ ui/oopsyraidsy/data/05-shb/raid/e12n.js
@@ -3,7 +3,18 @@ import ZoneId from '../../../../../resources/zone_id.js';
 export default {
   zoneId: ZoneId.EdensPromiseEternity,
   damageWarn: {
+    'E12N Judgment Jolt': '585F', // Ramuh get out cast
+    'E12N Temporary Current': '585C', // Levi get under cast
+    'E12N Conflag Strike': '585D', // Ifrit get sides cast
+    'E12N Ferostorm': '585E', // Garuda get intercardinals cast
+    'E12N Rapturous Reach Left': '5877', // Haircut with left side safe
+    'E12N Rapturous Reach Right': '5878', // Haircut with right side safe
+    'E12N Bomb Explosion': '586D', // Small bomb explosion
+    'E12N Titanic Bomb Explosion': '586F', // Large bomb explosion
   },
-  damageFail: {
+  shareWarn: {
+    'E12N Earthshaker': '5885', // Earthshaker on first platform
+    'E12N Promise Frigid Stone 1': '5867', // Shiva spread with sliding
+    'E12N Promise Frigid Stone 2': '5869', // Shiva spread with Rapturous Reach
   },
 };
