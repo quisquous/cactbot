a29a880206d6bf9db14c8b2b078e24af7da6f97c
Maiko Tan <maiko.tan.coding@gmail.com>
2020-12-23 14:58:19 +0800

    concurrent?

--- test/run_tests.py
+++ test/run_tests.py
@@ -1,5 +1,6 @@
 """Aggregate test runner for Cactbot Python tests."""
 
+import concurrent.futures
 import sys
 import test_manifests
 import test_timelines
@@ -7,8 +8,15 @@ import test_triggers
 import unittests
 
 if __name__ == "__main__":
-    EXIT_STATUS = test_manifests.main()
-    EXIT_STATUS |= test_timelines.main()
-    EXIT_STATUS |= test_triggers.main()
-    EXIT_STATUS |= unittests.main()
+    EXIT_STATUS = 0
+    future_list = []
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        future_list.append(executor.submit(test_manifests.main))
+        future_list.append(executor.submit(test_manifests.main))
+        future_list.append(executor.submit(test_timelines.main))
+        future_list.append(executor.submit(test_triggers.main))
+        future_list.append(executor.submit(unittests.main))
+
+    for f in future_list:
+        EXIT_STATUS != f.result()
     sys.exit(EXIT_STATUS)
