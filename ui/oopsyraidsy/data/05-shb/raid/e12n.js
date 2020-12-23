9954df9434a6c7e4a21791ea1322cbdc764e4e4c
Adrienne Walker <enne@quisquo.us>
2020-12-22 23:12:59 -0800

    e12n fixups

--- ui/oopsyraidsy/data/05-shb/raid/e12n.js
+++ ui/oopsyraidsy/data/05-shb/raid/e12n.js
@@ -3,12 +3,16 @@ import ZoneId from '../../../../../resources/zone_id.js';
 export default {
   zoneId: ZoneId.EdensPromiseEternity,
   damageWarn: {
-    'E12N Judgment Jolt': '585F', // Ramuh get out cast
-    'E12N Temporary Current': '585C', // Levi get under cast
-    'E12N Conflag Strike': '585D', // Ifrit get sides cast
-    'E12N Ferostorm': '585E', // Garuda get intercardinals cast
-    'E12N Rapturous Reach Left': '5877', // Haircut with left side safe
-    'E12N Rapturous Reach Right': '5878', // Haircut with right side safe
+    'E12N Judgment Jolt Single': '585F', // Ramuh get out cast
+    'E12N Judgment Jolt': '4E30', // Ramuh get out cast
+    'E12N Temporary Current Single': '585C', // Levi get under cast
+    'E12N Temporary Current': '4E2D', // Levi get under cast
+    'E12N Conflag Strike Single': '585D', // Ifrit get sides cast
+    'E12N Conflag Strike': '4E2E', // Ifrit get sides cast
+    'E12N Ferostorm Single': '585E', // Garuda get intercardinals cast
+    'E12N Ferostorm': '4E2F', // Garuda get intercardinals cast
+    'E12N Rapturous Reach 1': '5878', // Haircut
+    'E12N Rapturous Reach 2': '5877', // Haircut
     'E12N Bomb Explosion': '586D', // Small bomb explosion
     'E12N Titanic Bomb Explosion': '586F', // Large bomb explosion
   },
