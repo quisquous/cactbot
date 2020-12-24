04a6e4d7705ad2a444c6d8636b2a0f7eede2f487
Adrienne Walker <enne@quisquo.us>
2020-12-23 20:07:18 -0800

    webpack: share raidboss/oopsy data across entrypoints

--- webpack/webpack.config.cjs
+++ webpack/webpack.config.cjs
@@ -4,19 +4,36 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    config: './ui/config/config.js',
-    coverage: './util/coverage/coverage.js',
-    rdmty: './ui/dps/rdmty/dps.js',
-    xephero: './ui/dps/xephero/xephero.js',
-    eureka: './ui/eureka/eureka.js',
-    fisher: './ui/fisher/fisher.js',
-    jobs: './ui/jobs/jobs.js',
-    oopsyraidsy: './ui/oopsyraidsy/oopsyraidsy.js',
-    pullcounter: './ui/pullcounter/pullcounter.js',
-    radar: './ui/radar/radar.js',
-    raidboss: './ui/raidboss/raidboss.js',
-    raidemulator: './ui/raidboss/raidemulator.js',
-    test: './ui/test/test.js',
+    'config': {
+      import: './ui/config/config.js',
+      dependOn: 'raidboss_data',
+    },
+    'coverage': {
+      import: './util/coverage/coverage.js',
+      dependOn: ['raidboss_data', 'oopsy_data'],
+    },
+    'rdmty': './ui/dps/rdmty/dps.js',
+    'xephero': './ui/dps/xephero/xephero.js',
+    'eureka': './ui/eureka/eureka.js',
+    'fisher': './ui/fisher/fisher.js',
+    'jobs': './ui/jobs/jobs.js',
+    'oopsyraidsy': {
+      import: './ui/oopsyraidsy/oopsyraidsy.js',
+      dependOn: 'oopsy_data',
+    },
+    'oopsy_data': './ui/oopsyraidsy/data/manifest.txt',
+    'pullcounter': './ui/pullcounter/pullcounter.js',
+    'radar': './ui/radar/radar.js',
+    'raidboss': {
+      import: './ui/raidboss/raidboss.js',
+      dependOn: 'raidboss_data',
+    },
+    'raidboss_data': './ui/raidboss/data/manifest.txt',
+    'raidemulator': {
+      import: './ui/raidboss/raidemulator.js',
+      dependOn: 'raidboss_data',
+    },
+    'test': './ui/test/test.js',
   },
   output: {
     filename: '[name].bundle.js',
