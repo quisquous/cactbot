84097fb55ffdbf7ddabde0a32762f835af0cb05f
cacbot <Gamerbauer1985@gmail.com>
2021-01-02 01:15:55 +0100

    minor correction fr

--- ui/raidboss/data/05-shb/raid/e10s.js
+++ ui/raidboss/data/05-shb/raid/e10s.js
@@ -686,7 +686,7 @@ export default {
         secondPitchBog: {
           en: 'Final Puddle Positions',
           de: 'Flächen interkardinal ablegen',
-          fr: 'Zones au sol en intercardinal',
+          fr: 'Zones au sol en intercardinaux',
           ja: '最後のスワンプ',
           cn: '最后一次点名放到外圈',
           ko: '각자 장판 위치로',
