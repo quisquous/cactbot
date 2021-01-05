489e7a25059f57c8ced42c42f0d34c18e5575eac
Legends0 <nac10fsu@gmail.com>
2021-01-04 19:33:04 -0500

    Add advanced relativity yellow hourglass callouts
    To be tested.

--- ui/raidboss/data/05-shb/raid/e12s.js
+++ ui/raidboss/data/05-shb/raid/e12s.js
@@ -858,6 +858,81 @@ export default {
         },
       },
     },
+    {
+      id: 'E12S Adv Relativity Hourglass Collect',
+      // Collect Sorrow Hourglass locations
+      netRegex: NetRegexes.addedCombatantFull({ npcNameId: '9823' }),
+      run: (data, matches) => {
+        const id = parseInt(matches.id, 16);
+        const y = parseFloat(matches.y) + 75;
+        const x = parseFloat(matches.x);
+
+        // Positions are NE, N, NW, SW, S, SE + numerical slop on a radius=20 circle.
+        // Positions are also moved downward 75
+        // N = (0, -86), S = (0, -64)
+        // NE = (10, -80), SE = (10, -69), SW = (-10, -69), NW = (-10, -80)
+        // Map N = 0, NE = 1, ..., NW = 7
+        const dir = Math.round(4 - 4 * Math.atan2(x, y) / Math.PI) % 8;
+
+        data.sorrows = data.sorrows || {};
+        data.sorrows[id] = [dir, false];
+      },
+    },
+    {
+      id: 'E12S Adv Relativity Hourglass Collect Yellow Tethers',
+      // '0086' is the Yellow tether that buffs "Quicken"
+      // '0085' is the Red tether that buffs "Slow"
+      netRegex: NetRegexes.tether({ id: '0086' }),
+      run: (data, matches) => {
+        const id = parseInt(matches.sourceId, 16);
+        data.sorrows[id][1] = true;
+      },
+    },
+    {
+      id: 'E12S Adv Relativity Speed',
+      // Orient where Oracle Quickens Sorrow Hourglass
+      netRegex: NetRegexes.startsUsing({ id: '58DD' }),
+      // Tethers happen on same interval, add some delay
+      delaySeconds: 0.5,
+      durationSeconds: 8,
+      infoText: (data, matches, output) => {
+        const dirs = {
+          0: output.north(),
+          1: output.northeast(),
+          2: output.east(),
+          3: output.southeast(),
+          4: output.south(),
+          5: output.southwest(),
+          6: output.west(),
+          7: output.northwest(),
+        };
+
+        const sorrows = [];
+
+        // Push yellow tethered hourglasses to array
+        for (const [key, value] of Object.entries(data.sorrows)){
+          if (value[1]) sorrows.push(value[0]);
+        }
+
+        return output.hourglass({
+          dir1: dirs[sorrows[0]],
+          dir2: dirs[sorrows[1]],
+        });
+      },
+      outputStrings: {
+        north: Outputs.north,
+        northeast: Outputs.northeast,
+        east: Outputs.east,
+        southeast: Outputs.southeast,
+        south: Outputs.south,
+        southwest: Outputs.southwest,
+        west: Outputs.west,
+        northwest: Outputs.northwest,
+        hourglass: {
+          en: 'Yellow: ${dir1} / ${dir2}',
+        },
+      },
+    },
   ],
   timelineReplace: [
     {
