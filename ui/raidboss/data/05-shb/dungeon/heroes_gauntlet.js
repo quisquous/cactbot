23f075c1fc08f167350b80f014d4507586776427
Akurosia <kamothegamer@web.de>
2021-01-17 10:14:48 +0100

    add missing regex translations in heroes_gauntlet

--- ui/raidboss/data/05-shb/dungeon/heroes_gauntlet.js
+++ ui/raidboss/data/05-shb/dungeon/heroes_gauntlet.js
@@ -55,6 +55,11 @@ export default {
     {
       id: 'Heroes Gauntlet Spectral White Mage Absolute Protect',
       netRegex: NetRegexes.startsUsing({ id: '524D', source: 'Spectral White Mage' }),
+      netRegexDe: NetRegexes.startsUsing({ id: '524D', source: 'Phantom-Weißmagier' }),
+      netRegexFr: NetRegexes.startsUsing({ id: '524D', source: 'Mage Blanc Spectral' }),
+      netRegexJa: NetRegexes.startsUsing({ id: '524D', source: '幻光の白魔道士' }),
+      netRegexCn: NetRegexes.startsUsing({ id: '524D', source: '幻光白魔法师' }),
+      netRegexKo: NetRegexes.startsUsing({ id: '524D', source: '환상빛의 백마도사' }),
       condition: (data) => data.CanSilence(),
       response: Responses.interrupt('alert'),
     },
