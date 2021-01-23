81cfc99582c737071d5b2e2580bf13be98a3cd05
Adrienne Walker <enne@quisquo.us>
2021-01-23 10:39:07 -0800

    oopsy: fix e10s error with missing netregexes

--- ui/oopsyraidsy/data/05-shb/raid/e10s.js
+++ ui/oopsyraidsy/data/05-shb/raid/e10s.js
@@ -1,3 +1,4 @@
+import NetRegexes from '../../../../../resources/netregexes.js';
 import ZoneId from '../../../../../resources/zone_id.js';
 
 // TODO: hitting shadow of the hero with abilities can cause you to take damage, list those?
