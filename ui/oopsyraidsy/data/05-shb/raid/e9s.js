1a2d4a51d9019128bb4dc46c22e00f2bd1b8d467
Adrienne Walker <enne@quisquo.us>
2021-01-27 23:00:23 -0800

    oopsy: fix missing import in e9s

--- ui/oopsyraidsy/data/05-shb/raid/e9s.js
+++ ui/oopsyraidsy/data/05-shb/raid/e9s.js
@@ -1,3 +1,4 @@
+import NetRegexes from '../../../../../resources/netregexes.js';
 import ZoneId from '../../../../../resources/zone_id.js';
 
 // TODO: 561D Evil Seed hits everyone, hard to know if there's a double tap
