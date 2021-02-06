c741f239bb3f4a25974823b89e709026b2db975b
Adrienne Walker <enne@quisquo.us>
2021-02-06 14:05:53 -0800

    oopsy: add titan unreal

---
+++ ui/oopsyraidsy/data/05-shb/trial/titan-un.js
@@ -0,0 +1,20 @@
+import ZoneId from '../../../../../resources/zone_id.js';
+
+// Titan Unreal
+export default {
+  zoneId: ZoneId.TheNavelUnreal,
+  damageWarn: {
+    'TitanUn Weight Of The Land': '58FE',
+    'TitanUn Burst': '5ADF',
+  },
+  damageFail: {
+    'TitanUn Landslide': '5ADC',
+    'TitanUn Gaoler Landslide': '5902',
+  },
+  shareWarn: {
+    'TitanUn Rock Buster': '58F6',
+  },
+  shareFail: {
+    'TitanUn Mountain Buster': '58F7',
+  },
+};
