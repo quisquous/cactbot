eeef0d18252a2867ab38339a414a1eb4d2313323
Trim21 <i@trim21.me>
2021-02-14 10:11:39 +0800

    update types

--- types/data.d.ts
+++ types/data.d.ts
@@ -1,27 +1,33 @@
 import PartyTracker from '../resources/party.js';
 import { Lang } from './global';
 
+export interface Option {
+  ParserLanguage: Lang;
+  LangShortLocale: Lang;
+  DisplayLanguage: Lang;
+  [key: string]: unknown;
+  // todo: complete this type
+}
+
 export interface Data {
   job: string;
   me: string;
-  role: string,
-  party: PartyTracker,
-  lang: string,
-  currentHP: number,
-  options: {
-    ParserLanguage: Lang,
-    LangShortLocale: Lang,
-    DisplayLanguage: Lang,
-    [key: string]: unknown,
-    // todo: complete this type
-  },
-  ShortName(x: string): string,
-  StopCombat(): void,
+  role: string;
+  party: PartyTracker;
+  lang: string;
+  currentHP: number;
+  options: Option;
+  ShortName(x: string): string;
+  StopCombat(): void;
+  /**
+   * @deprecated Use parseFloat instead
+   */
   ParseLocaleFloat(string: string): number;
-  CanStun(): boolean,
-  CanSilence(): boolean,
-  CanSleep(): boolean,
-  CanCleanse(): boolean,
-  CanFeint(): boolean,
-  CanAddle(): boolean,
+  IsPlayerId?(id: string): boolean;
+  CanStun(): boolean;
+  CanSilence(): boolean;
+  CanSleep(): boolean;
+  CanCleanse(): boolean;
+  CanFeint(): boolean;
+  CanAddle(): boolean;
 }
