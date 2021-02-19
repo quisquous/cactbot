5e6dc984f5c2045153f7c0a198f14e2e31c4cda0
Adrienne Walker <enne@quisquo.us>
2021-02-18 18:27:20 -0800

    test: test triggers via mocha (#2487)

--- test/run_tests.py
+++ test/run_tests.py
@@ -2,9 +2,7 @@
 
 import sys
 import test_manifests
-import test_triggers
 
 if __name__ == "__main__":
     EXIT_STATUS = test_manifests.main()
-    EXIT_STATUS |= test_triggers.main()
     sys.exit(EXIT_STATUS)
