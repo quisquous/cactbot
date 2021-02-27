ee763f21994bafa84a856fbbf6bee7a26aedfc6c
Adrienne Walker <enne@quisquo.us>
2021-02-27 15:40:27 -0800

    stygimoloch lord

--- ui/oopsyraidsy/data/05-shb/eureka/delubrum_reginae_savage.js
+++ ui/oopsyraidsy/data/05-shb/eureka/delubrum_reginae_savage.js
@@ -7,8 +7,11 @@ export default {
     // 5AB9 5ABA 5ABB 5ABC these are also Mercy Fourfold??
     'DelubrumSav Seeker Baleful Swathe': '5AD1', // Ground aoe to either side of boss
     'DelubrumSav Seeker Baleful Blade': '5B2A', // Hide behind pillars attack
-
     'DelubrumSav Seeker Scorching Shackle': '5ACB', // Chains
+
+    'DelubrumSav Lord Whack': '57D0', // cleave
+    'DelubrumSav Lord Devastating Bolt 1': '57C5', // lightning rings
+    'DelubrumSav Lord Devastating Bolt 2': '57C6', // lightning rings
   },
   damageFail: {
 
