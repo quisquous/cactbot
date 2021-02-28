9ca894bb75e6c0be140aa48ff75939bda42bd6a5
Adrienne Walker <enne@quisquo.us>
2021-02-27 23:33:00 -0800

    initial triggers

--- ui/raidboss/data/05-shb/eureka/delubrum_reginae_savage.js
+++ ui/raidboss/data/05-shb/eureka/delubrum_reginae_savage.js
@@ -6,6 +6,8 @@ import ZoneId from '../../../../../resources/zone_id.js';
 
 // TODO: warnings for mines after bosses?
 
+// TODO: headmarkers of course have a random offset here eyeroll
+
 // TODO: promote something like this to Conditions?
 const tankBusterOnParty = (data, matches) => {
   if (data.target === data.me)
@@ -19,6 +21,217 @@ export default {
   zoneId: ZoneId.DelubrumReginaeSavage,
   timelineFile: 'delubrum_reginae_savage.txt',
   triggers: [
+    {
+      id: 'DelubrumSav Seeker Baleful Swath',
+      // This is an early warning on the Verdant Path cast.
+      netRegex: NetRegexes.startsUsing({ source: 'Trinity Seeker', id: '5A98', capture: false }),
+      response: Responses.goFrontBack('info'),
+    },
+    {
+      id: 'DelubrumSav Seeker Act Of Mercy',
+      // This is an early warning on the Verdant Path cast.
+      netRegex: NetRegexes.startsUsing({ source: 'Trinity Seeker', id: '5A97', capture: false }),
+      alertText: (data, _, outputs) => outputs.text(),
+      outputStrings: {
+        text: {
+          en: 'Intercardinals',
+        },
+      },
+    },
+    {
+      id: 'DelubrumSav Seeker Baleful Onslaught Buster',
+      netRegex: NetRegexes.startsUsing({ source: 'Trinity Seeker', id: '5AD5', capture: false }),
+      response: (data, _, output) => {
+        // cactbot-builtin-response
+        output.responseOutputStrings = {
+          avoidTankCleave: Outputs.avoidTankCleave,
+          sharedTankBuster: {
+            en: 'Shared Tank Buster',
+          },
+        };
 
+        if (data.role === 'tank' || data.role === 'healer')
+          return { alertText: output.sharedTankBuster() };
+        return { infoText: output.avoidTankCleave() };
+      },
+    },
+    {
+      id: 'DelubrumSav Seeker Baleful Onslaught Stillness',
+      netRegex: NetRegexes.startsUsing({ source: 'Trinity Seeker', id: '5AD6', capture: false }),
+      alertText: (data, _, output) => output.text(),
+      outputStrings: {
+        text: {
+          en: 'Stillness Tank Cleave',
+        },
+      },
+    },
+    {
+      id: 'DelubrumSav Seeker Baleful Blade Out',
+      netRegex: NetRegexes.startsUsing({ source: 'Trinity Seeker', id: '5ABE', capture: false }),
+      alertText: (data, _, output) => output.text(),
+      outputStrings: {
+        text: {
+          en: 'Get Out Behind Barricade',
+          de: 'Geh raus, hinter die Barrikaden',
+          ko: '밖으로, 바리케이트 뒤로',
+        },
+      },
+    },
+    {
+      id: 'DelubrumSav Seeker Baleful Blade Knockback',
+      netRegex: NetRegexes.startsUsing({ source: 'Trinity Seeker', id: '5ABF', capture: false }),
+      alertText: (data, _, output) => output.text(),
+      outputStrings: {
+        text: {
+          en: 'In Front Of Barricade',
+        },
+      },
+    },
+    {
+      id: 'DelubrumSav Dahu Shockwave',
+      netRegex: NetRegexes.startsUsing({ source: 'Dahu', id: ['5770', '576F'] }),
+      // There's a 3s slow windup on the first, then a 1s opposite cast.
+      suppressSeconds: 10,
+      alertText: (data, matches, output) => {
+        if (matches.id === '5770')
+          return output.leftThenRight();
+        return output.rightThenLeft();
+      },
+      outputStrings: {
+        leftThenRight: {
+          en: 'Left, Then Right',
+          de: 'Links, dann Rechts',
+          fr: 'Gauche, puis droite',
+          ja: '左 => 右',
+          cn: '左 => 右',
+          ko: '왼쪽 => 오른쪽',
+        },
+        rightThenLeft: {
+          en: 'Right, Then Left',
+          de: 'Rechts, dann Links',
+          fr: 'Droite, puis gauche',
+          ja: '右 => 左',
+          cn: '右 => 左',
+          ko: '오른쪽 => 왼쪽',
+        },
+      },
+    },
+    {
+      id: 'DelubrumSav Guard Optimal Offensive Sword',
+      netRegex: NetRegexes.startsUsing({ source: 'Queen\'s Knight', id: '5819', capture: false }),
+      alertText: (data, _, output) => output.text(),
+      outputStrings: {
+        text: {
+          en: 'Take Outside Bombs',
+        },
+      },
+    },
+    {
+      id: 'DelubrumSav Guard Optimal Offensive Shield',
+      netRegex: NetRegexes.startsUsing({ source: 'Queen\'s Knight', id: '581A', capture: false }),
+      alertText: (data, _, output) => output.text(),
+      outputStrings: {
+        text: {
+          en: 'Knockback Away From Sphere',
+        },
+      },
+    },
+    {
+      id: 'DelubrumSav Guard Optimal Play Sword',
+      netRegex: NetRegexes.startsUsing({ source: 'Queen\'s Knight', id: '5816', capture: false }),
+      alertText: (data, _, output) => output.text(),
+      outputStrings: {
+        text: {
+          en: 'Out, Avoid Cleaves',
+        },
+      },
+    },
+    {
+      id: 'DelubrumSav Guard Optimal Play Shield',
+      netRegex: NetRegexes.startsUsing({ source: 'Queen\'s Knight', id: '5817', capture: false }),
+      alertText: (data, _, output) => output.text(),
+      outputStrings: {
+        text: {
+          en: 'In, Avoid Cleaves',
+        },
+      },
+    },
+    {
+      id: 'DelubrumSav Guard Yellow Tether',
+      netRegex: NetRegexes.tether({ source: 'Queen\'s Warrior', target: 'Queen\'s Knight', id: '0088', capture: false }),
+      // Yellow tether between Knight and Warrior gives them a Physical Vulnerability Down debuff.
+      infoText: (data, _, output) => output.text(),
+      outputStrings: {
+        text: {
+          en: 'Spellforge on physical dps',
+        },
+      },
+    },
+    {
+      id: 'DelubrumSav Guard Purple Tether',
+      netRegex: NetRegexes.tether({ source: 'Queen\'s Warrior', target: 'Queen\'s Knight', id: '0089', capture: false }),
+      // Yellow tether between Knight and Warrior gives them a Physical Vulnerability Down debuff.
+      infoText: (data, _, output) => output.text(),
+      outputStrings: {
+        text: {
+          en: 'Steelstring on magical dps',
+        },
+      },
+    },
+    {
+      id: 'DelubrumSav Avowed Fury Of Bozja',
+      // Allegiant Arsenal 5987 = staff (out), followed up with Fury of Bozja 594C
+      netRegex: NetRegexes.startsUsing({ source: 'Trinity Avowed', id: '5987', capture: false }),
+      response: Responses.getOut('alert'),
+    },
+    {
+      id: 'DelubrumSav Avowed Flashvane',
+      // Allegiant Arsenal 5986 = bow (get behind), followed up by Flashvane 594B
+      netRegex: NetRegexes.startsUsing({ source: 'Trinity Avowed', id: '5986', capture: false }),
+      response: Responses.getBehind('alert'),
+    },
+    {
+      id: 'DelubrumSav Avowed Infernal Slash',
+      // Allegiant Arsenal 5985 = sword (get front), followed up by Infernal Slash 594A
+      netRegex: NetRegexes.startsUsing({ source: 'Trinity Avowed', id: '5985', capture: false }),
+      alertText: (data, _, output) => output.text(),
+      outputStrings: {
+        text: {
+          en: 'Get In Front',
+          de: 'Geh vor den Boss',
+          ko: '정면에 서기',
+        },
+      },
+    },
+    {
+      id: 'DelubrumSav Lord Foe Splitter',
+      netRegex: NetRegexes.startsUsing({ source: 'Stygimoloch Lord', id: '57D7' }),
+      response: (data, matches, output) => {
+        // cactbot-builtin-response
+        output.responseOutputStrings = {
+          cleaveOnYou: Outputs.tankCleaveOnYou,
+          cleaveNoTarget: Outputs.tankCleave,
+          avoidCleave: Outputs.avoidTankCleave,
+          cleaveOn: {
+            en: 'Tank Cleave on ${player}',
+          },
+        };
+        if (matches.target === data.me)
+          return { alarmText: output.cleaveOnYou() };
+        if (tankBusterOnParty(data, matches))
+          return { alertText: output.cleaveOn({ player: data.ShortName(matches.target) }) };
+        return { infoText: output.avoidCleave() };
+      },
+    },
+    {
+      id: 'DelubrumSav Lord Vicious Swipe',
+      netRegex: NetRegexes.startsUsing({ source: 'Stygimoloch Lord', id: '5552', capture: false }),
+      response: Responses.knockback(),
+    },
+    {
+      id: 'DelubrumSav Lord 1111-Tonze Swing',
+      netRegex: NetRegexes.startsUsing({ source: '1111-Tonze Swing', id: '57D8', capture: false }),
+      response: Responses.getOut(),
+    },
   ],
 };
