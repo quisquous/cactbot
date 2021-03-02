a1a4f1c3c8bf83493e8809e7eaa9a2a0f7d4d359
Maiko Tan <maiko.tan.coding@gmail.com>
2021-03-02 19:48:29 +0800

    almost fully refactoring...

--- eslint/cactbot-output-strings.js
+++ eslint/cactbot-output-strings.js
@@ -1,44 +1,6 @@
 const t = require('@babel/types');
 const textProps = ['alarmText', 'alertText', 'infoText', 'tts'];
 
-/** @typedef {import("eslint").Rule.RuleContext} Context */
-/**
- * get all keys name from object literal expression
- * @param {(t.Property|t.SpreadElement)[]} props
- * @param {Context} context
- * @return {string[]}
- */
-const getAllKeys = (props, context) => {
-  const propKeys = [];
-
-  if (typeof props === 'undefined') return propKeys;
-
-  const vars = context.getScope().variables;
-
-  props.forEach((prop) => {
-    if (t.isProperty(prop)) {
-      if (t.isIdentifier(prop.key))
-        propKeys.push(prop.key.name);
-      else if (t.isLiteral(prop.key))
-        propKeys.push(prop.key.value);
-    } else if (t.isSpreadElement(prop)) {
-      if (t.isIdentifier(prop.argument)) {
-        const variable = vars.find((v) => v.name === prop.argument.name);
-        if (!variable) return;
-        const props = variable.defs.map((def) => {
-          if (t.isVariableDeclarator(def.node) && t.isObjectExpression(def.node.init))
-            return def.node.init.properties;
-        })
-          .filter((p) => typeof p !== 'undefined')
-          .forEach((p) => {
-            getAllKeys(p, context)
-              .forEach((name) => propKeys.push(name));
-          });
-      }
-    }
-  });
-  return propKeys;
-};
 
 /**
  * @type {import("eslint").Rule.RuleModule}
@@ -60,66 +22,81 @@ const ruleModule = {
     },
   },
   create: function(context) {
-    const sourceCode = context.getSourceCode();
-    return {
-      ObjectExpression(node) {
-        const propNames = getAllKeys(node.properties, context);
-        if (!propNames.includes('id') || !propNames.some((name) => textProps.includes(name))) return;
+    const globalVars = new Map();
+    const stack = {
+      outputParam: null,
+      outputProperties: [],
+      inTriggerFunc: false,
+    };
+    /**
+     * get all keys name from object literal expression
+     * @param {(t.Property|t.SpreadElement)[]} props
+     * @return {string[]}
+     */
+    const getAllKeys = (props) => {
+      const propKeys = [];
 
-        if (!node.properties.find((prop) => prop.key && prop.key.name === 'outputStrings')) {
-          context.report({
-            node: node,
-            messageId: 'noOutputStrings',
-          });
+      if (!props) return propKeys;
+
+      props.forEach((prop) => {
+        if (t.isProperty(prop)) {
+          if (t.isIdentifier(prop.key))
+            propKeys.push(prop.key.name);
+          else if (t.isLiteral(prop.key))
+            propKeys.push(prop.key.value);
+        } else if (t.isSpreadElement(prop)) {
+          if (t.isIdentifier(prop.argument)) {
+            (globalVars.get(prop.argument.name) || [])
+              .forEach((name) => propKeys.push(name));
+          }
+        }
+      });
+      return propKeys;
+    };
+
+
+    return {
+      'Program > VariableDeclaration > VariableDeclarator > ObjectExpression'(node) {
+        globalVars.set(node.parent.id.name, getAllKeys(node.properties));
+      },
+      [`Property[key.name=/${textProps.join('|')}/] > :function`](node) {
+        const props = getAllKeys(node.parent.parent.properties);
+        if (props.find((prop) => prop === 'outputStrings')) {
+          stack.inTriggerFunc = true;
+          stack.outputParam = node.params[2] && node.params[2].name;
+          const outputValue = node.parent.parent.properties.find((prop) => prop.key && prop.key.name === 'outputStrings').value;
+          stack.outputProperties =
+            t.isIdentifier(outputValue)
+              ? (globalVars.get(outputValue.name) || [])
+              : getAllKeys(outputValue.properties);
           return;
         }
-        const outputStringProps = (() => {
-          const propNames = [];
-          const outputProps = node.properties.find((prop) => prop.key && prop.key.name === 'outputStrings');
-          if (outputProps && outputProps.value && outputProps.value.properties) {
-            getAllKeys(outputProps.value.properties, context)
-              .forEach((n) => propNames.push(n));
-          } else if (outputProps && t.isIdentifier(outputProps.value)) {
-            const obj = context
-              .getScope()
-              .variables
-              .find((variable) => variable.name === outputProps.value.name);
-            if (obj) {
-              obj.defs.forEach((def) => {
-                const props = def.node.init && def.node.init.properties;
-                getAllKeys(props, context).forEach((n) => propNames.push(n));
-              });
-            }
-          }
-          return propNames;
-        })();
-        if (!outputStringProps) return;
-        const textFunctions = node
-          .properties
-          .filter((prop) => textProps.includes(prop.key.name))
-          .map((prop) => prop.value);
-        textFunctions.forEach((func) => {
-          if (!t.isFunctionExpression(func) && !t.isArrowFunctionExpression(func)) return;
-          const outputParam = func.params[2] && func.params[2].name;
-          if (!outputParam) return;
-
-          const source = sourceCode.getText(func);
-          const m = source.match(new RegExp(`(?<=\\b${outputParam}\\.)(\\w+)(?=\\()`, 'g'));
-          if (!m) return;
-
-          m.forEach((outputProperty) => {
-            if (!outputStringProps.includes(outputProperty)) {
-              context.report({
-                node: func,
-                messageId: 'notFoundProperty',
-                data: {
-                  prop: outputProperty,
-                  outputParam: outputParam,
-                },
-              });
-            }
-          });
+        context.report({
+          node: node.parent.key,
+          messageId: 'noOutputStrings',
         });
+      },
+      [`Property[key.name=/${textProps.join('|')}/] > :function:exit`]() {
+        if (stack.inTriggerFunc) {
+          stack.inTriggerFunc = false;
+          stack.outputParam = null;
+          stack.outputProperties = [];
+        }
+      },
+      [`Property[key.name=/${textProps.join('|')}/] > :function[params.length=3] CallExpression > MemberExpression`](node) {
+        if (node.object.name === stack.outputParam &&
+          node.computed === false &&
+          t.isIdentifier(node.property) &&
+          !stack.outputProperties.includes(node.property.name)) {
+          context.report({
+            node: node,
+            messageId: 'notFoundProperty',
+            data: {
+              prop: node.property.name,
+              outputParam: stack.outputParam,
+            },
+          });
+        }
       },
     };
   },
