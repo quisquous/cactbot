d6647bcad9bbc2e31da805bc5edff6add3305e38
Trim21 <i@trim21.me>
2021-03-03 15:39:54 +0800

    Fix/import ts in ts (#2526)

--- webpack/webpack.config.cjs
+++ webpack/webpack.config.cjs
@@ -1,32 +1,6 @@
 'use strict';
 
-const fs = require('fs');
 const path = require('path');
-const { resourceUsage } = require('process');
-const { NormalModuleReplacementPlugin } = require('webpack');
-
-// See https://github.com/quisquous/cactbot/pull/2466#issuecomment-782918099
-// for more details about how we got here.  Typescript imports should use
-// `import x from './foo.js', however foo.js might not exist and might
-// really be foo.ts on disk.  WebPack will handle './foo' and even './foo.ts'
-// but ts-node can only handle './foo.js'.  This resolving plugin intercepts
-// any imports for js files and if the js file doesn't exist on disk,
-// tries to use a ts file of the same name.
-const replacementPlugin = new NormalModuleReplacementPlugin(
-    /\.js$/,
-    (resource) => {
-      const importPath = resource.request;
-      const basePath = resource.context;
-      const filename = path.resolve(basePath, importPath);
-
-      // If this is a .js file that exists, return it directly.
-      if (fs.existsSync(filename))
-        return;
-
-      // Otherwise, attempt to find a .ts file in the same location.
-      resource.request = resource.request.replace(/\.js$/, '.ts');
-    },
-);
 
 module.exports = function(env, argv) {
   return {
@@ -68,8 +42,18 @@ module.exports = function(env, argv) {
       path: path.resolve(__dirname, '../dist'),
     },
     devServer: { writeToDisk: true },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
     module: {
       rules: [
+        {
+          // this will allow importing without extension in js files.
+          test: /\.m?js$/,
+          resolve: {
+            fullySpecified: false,
+          },
+        },
         {
           test: /\.ts$/,
           loader: 'ts-loader',
@@ -99,8 +83,5 @@ module.exports = function(env, argv) {
         },
       ],
     },
-    plugins: [
-      replacementPlugin,
-    ],
   };
 };
