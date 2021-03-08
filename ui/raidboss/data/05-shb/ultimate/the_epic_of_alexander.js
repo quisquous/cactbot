09b5cba73be82f977ff2dbb4df2cf2d3a7da047f
Trim21 <i@trim21.me>
2021-03-09 04:09:01 +0800

    remove changes

--- ui/raidboss/data/05-shb/ultimate/the_epic_of_alexander.js
+++ ui/raidboss/data/05-shb/ultimate/the_epic_of_alexander.js
@@ -747,7 +747,7 @@ export default {
     {
       // Applies to both limit cuts.
       id: 'TEA Limit Cut Numbers',
-      netRegex: NetRegexes.headMarker({}),
+      netRegex: NetRegexes.headMarker({ }),
       condition: function(data, matches) {
         // Here and elsewhere, it's probably best to check for whether the user is the target first,
         // as that should short-circuit more often.
@@ -811,7 +811,7 @@ export default {
     {
       // Applies to both limit cuts.
       id: 'TEA Limit Cut Knockback',
-      netRegex: NetRegexes.headMarker({}),
+      netRegex: NetRegexes.headMarker({ }),
       condition: function(data, matches) {
         return data.me === matches.target && (/00(?:4F|5[0-6])/).test(getHeadmarkerId(data, matches));
       },
@@ -951,7 +951,7 @@ export default {
     },
     {
       id: 'TEA Ice Marker',
-      netRegex: NetRegexes.headMarker({}),
+      netRegex: NetRegexes.headMarker({ }),
       condition: function(data, matches) {
         return data.me === matches.target && getHeadmarkerId(data, matches) === '0043';
       },
@@ -993,7 +993,7 @@ export default {
     },
     {
       id: 'TEA Enumeration YOU',
-      netRegex: NetRegexes.headMarker({}),
+      netRegex: NetRegexes.headMarker({ }),
       condition: function(data, matches) {
         return data.me === matches.target && getHeadmarkerId(data, matches) === '0041';
       },
@@ -1011,7 +1011,7 @@ export default {
     },
     {
       id: 'TEA Enumeration Everyone',
-      netRegex: NetRegexes.headMarker({}),
+      netRegex: NetRegexes.headMarker({ }),
       condition: function(data, matches) {
         return getHeadmarkerId(data, matches) === '0041';
       },
@@ -1501,7 +1501,7 @@ export default {
     },
     {
       id: 'TEA Judgment Crystal',
-      netRegex: NetRegexes.headMarker({}),
+      netRegex: NetRegexes.headMarker({ }),
       condition: function(data, matches) {
         return data.me === matches.target && getHeadmarkerId(data, matches) === '0060';
       },
@@ -1680,7 +1680,7 @@ export default {
     },
     {
       id: 'TEA Cactbot Wormhole Strat',
-      netRegex: NetRegexes.headMarker({}),
+      netRegex: NetRegexes.headMarker({ }),
       condition: function(data, matches) {
         if (!data.options.cactbotWormholeStrat)
           return false;
@@ -1812,7 +1812,7 @@ export default {
     },
     {
       id: 'TEA Incinerating Heat',
-      netRegex: NetRegexes.headMarker({}),
+      netRegex: NetRegexes.headMarker({ }),
       condition: function(data, matches) {
         return getHeadmarkerId(data, matches) === '005D';
       },
@@ -1915,7 +1915,7 @@ export default {
     },
     {
       id: 'TEA Perfect Optical Sight Stack',
-      netRegex: NetRegexes.headMarker({}),
+      netRegex: NetRegexes.headMarker({ }),
       condition: function(data, matches) {
         return getHeadmarkerId(data, matches) === '003E';
       },
