e2cadf73adbe64afe56717a89f32740ddbf29139
Trim21 <i@trim21.me>
2021-03-22 00:24:51 +0800

    revert change

--- webpack/webpack.config.cjs
+++ webpack/webpack.config.cjs
@@ -18,7 +18,7 @@ module.exports = function(env, argv) {
       raidboss: './ui/raidboss/raidboss.js',
       raidemulator: './ui/raidboss/raidemulator.js',
       raidemulatorWorker: './ui/raidboss/emulator/data/NetworkLogConverterWorker.js',
-      test: './ui/test/test.ts',
+      test: './ui/test/test.js',
     },
     optimization: {
       splitChunks: {
