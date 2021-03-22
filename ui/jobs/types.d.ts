71c3be136f53076123e85c899337054f0dde9ab5
Maiko Tan <maiko.tan.coding@gmail.com>
2021-03-22 15:02:13 +0000

    refactor: add type definitions

---
+++ ui/jobs/types.d.ts
@@ -0,0 +1,47 @@
+import { Option as BaseOption} from '../../types/data';
+import { Job } from '../../types/job';
+
+export interface IDefaultOptions {
+  JustBuffTracker: boolean;
+  LowerOpacityOutOfCombat: boolean;
+  OpacityOutOfCombat: number;
+  PlayCountdownSound: boolean;
+  HideWellFedAboveSeconds: number;
+  ShowMPTickerOutOfCombat: boolean;
+  MidHealthThresholdPercent: number;
+  LowHealthThresholdPercent: number;
+  BigBuffShowCooldownSeconds: number;
+  BigBuffIconWidth: number;
+  BigBuffIconHeight: number;
+  BigBuffBarHeight: number;
+  BigBuffTextHeight: number;
+  BigBuffBorderSize: number;
+  GpAlarmPoint: number;
+  GpAlarmSoundVolume: number;
+}
+
+export interface IInitOptions {
+  ShowHPNumber: Job[];
+  ShowMPNumber: Job[];
+  ShowMPTicker: Job[];
+
+  MaxLevel: number;
+
+  PerBuffOptions: {
+    [s: string]: {
+      hide?: boolean;
+    };
+  };
+
+  FarThresholdOffence: number;
+  PldMediumMPThreshold: number;
+  PldLowMPThreshold: number;
+  DrkMediumMPThreshold: number;
+  DrkLowMPThreshold: number;
+  /**  One more fire IV and then despair. */
+  BlmMediumMPThreshold: number;
+  /** Should cast despair. */
+  BlmLowMPThreshold: number;
+}
+
+export type IOptions = BaseOption & IDefaultOptions & IInitOptions;
