e52280d25976e286a45fbbf133ba9c3d4385857a
Trim21 <i@trim21.me>
2021-03-23 22:55:07 +0800

    refactor

--- resources/widget_list.ts
+++ resources/widget_list.ts
@@ -28,14 +28,11 @@ class WidgetList extends HTMLElement {
   }
 
   // All visual dimensions are scaled by this.
-  set scale(s: string | null) {
-    if (s === null)
-      this.removeAttribute('scale');
-    else
-      this.setAttribute('scale', s);
+  set scale(s: string) {
+    this.setAttribute('scale', s);
   }
-  get scale(): string | null {
-    return this.getAttribute('scale');
+  get scale(): string {
+    return this.getAttribute('scale') ?? '';
   }
 
   // The direction that the list should grow. It can specify two
