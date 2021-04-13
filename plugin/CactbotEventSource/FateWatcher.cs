aaed8b6864e6cee72332306198772f8c7d970a48
Katt <12807478+KattTails@users.noreply.github.com>
2021-04-13 16:47:11 +0100

    plugin: Update Global CE Opcode for 5.5 (#2718)

--- plugin/CactbotEventSource/FateWatcher.cs
+++ plugin/CactbotEventSource/FateWatcher.cs
@@ -51,6 +51,7 @@ namespace Cactbot {
     // v5.41            0x31B
     // v5.45            0x3e1
     // v5.45h           0x1f5
+    // v5.5             0x2e7
     //
     // CN
     // v5.35            0x144
@@ -72,7 +73,7 @@ namespace Cactbot {
 
     private static readonly CEDirectorOPCodes cedirector_intl = new CEDirectorOPCodes(
       0x30,
-      0x1f5
+      0x2e7
     );
 
     private struct ActorControl143{
