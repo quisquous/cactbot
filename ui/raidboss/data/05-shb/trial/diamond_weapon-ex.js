cbdeb84ae01ff00ce6ecc111d258deae5a9b3e0c
unknown <valarnin@gmail.com>
2021-04-16 09:45:21 -0400

    raidboss: Diamond Weapon Extreme timeline

---
+++ ui/raidboss/data/05-shb/trial/diamond_weapon-ex.js
@@ -0,0 +1,10 @@
+import ZoneId from '../../../../../resources/zone_id';
+import NetRegexes from '../../../../../resources/netregexes';
+import Conditions from '../../../../../resources/conditions';
+import { Responses } from '../../../../../resources/responses';
+
+export default {
+  zoneId: ZoneId.TheCloudDeckExtreme,
+  timelineFile: 'diamond_weapon-ex.txt',
+  triggers: [],
+};
