461620ee48df624f08fc8c1c5487ad0ca33637ad
Adrienne Walker <enne@quisquo.us>
2021-04-17 13:52:19 -0700

    oopsy: add diamond normal

---
+++ ui/oopsyraidsy/data/05-shb/trial/diamond_weapon.js
@@ -0,0 +1,20 @@
+import ZoneId from '../../../../../resources/zone_id';
+
+// Diamond Weapon Normal
+export default {
+  zoneId: ZoneId.TheCloudDeck,
+  damageWarn: {
+    'Diamond Weapon Auri Arts': '5FE3', // Auri Arts dashes
+    'Diamond Weapon Diamond Shrapnel Initial': '5FE1', // initial circle of Diamond Shrapnel
+    'Diamond Weapon Diamond Shrapnel Chasing': '5FE2', // followup circles from Diamond Shrapnel
+    'Diamond Weapon Aetherial Bullet': '5FD5', // bit lasers
+  },
+  damageFail: {
+    'Diamond Weapon Claw Swipe Left': '5FD9', // Adamant Purge platform cleave
+    'Diamond Weapon Claw Swipe Right': '5FDA', // Adamant Purge platform cleave
+    'Diamond Weapon Airship\' Bane': '5FE8', // destroying one of the platforms after Auri Cyclone
+  },
+  shareWarn: {
+    'Diamond Weapon Homing Laser': '5FDB', // spread markers
+  },
+};
