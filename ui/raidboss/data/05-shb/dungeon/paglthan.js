f0129f03dfa602ad7956d69caa2c6475b750ecc2
Jonathan Garber <linkthevaliant@gmail.com>
2021-04-17 17:38:31 -0400

    Raidboss: Paglth'an updates (#2749)

--- ui/raidboss/data/05-shb/dungeon/paglthan.js
+++ ui/raidboss/data/05-shb/dungeon/paglthan.js
@@ -53,14 +53,7 @@ export default {
       id: 'Paglthan Lightning Rod Lose',
       netRegex: NetRegexes.losesEffect({ effectId: 'A0E' }),
       condition: Conditions.targetIsYou(),
-      alertText: (data, _, output) => output.text(),
-      outputStrings: {
-        text: {
-          en: 'Away from lightning circles',
-          de: 'Weg von den Blitz-Kreisen',
-          fr: 'Éloignez-vous des cercles de foudre',
-        },
-      },
+      response: Responses.goMiddle(),
     },
     {
       id: 'Paglthan Ballistic',
@@ -96,13 +89,24 @@ export default {
       response: Responses.stackMarkerOn(),
     },
     {
-      id: 'Paglthan Mega Flare',
+      id: 'Paglthan Mega Flare Spread',
       netRegex: NetRegexes.headMarker({ id: '0017' }),
       condition: Conditions.targetIsYou(),
       response: Responses.spread(),
     },
     {
-      id: 'Paglthan Kan Rhai',
+      id: 'Paglthan Mega Flare Move',
+      netRegex: NetRegexes.ability({ id: '5B4D', source: 'Lunar Bahamut' }),
+      condition: Conditions.targetIsYou(),
+      alertText: (data, _, output) => output.text(),
+      outputStrings: {
+        text: {
+          en: 'Away from circles',
+        },
+      },
+    },
+    {
+      id: 'Paglthan Kan Rhai Marker',
       netRegex: NetRegexes.headMarker({ id: '0104' }),
       condition: Conditions.targetIsYou(),
       alertText: (data, _, output) => output.text(),
@@ -111,6 +115,16 @@ export default {
           en: 'Kan Rhai on YOU',
           de: 'Kan Rhai auf DIR',
           fr: 'Kan Rhai sur VOUS',
+        },
+      },
+    },
+    {
+      id: 'Paglthan Kan Rhai Move',
+      netRegex: NetRegexes.ability({ id: '5B4F', source: 'Lunar Bahamut', capture: false }),
+      alertText: (data, _, output) => output.text(),
+      outputStrings: {
+        text: {
+          en: 'Away from crosses',
         },
       },
     },
