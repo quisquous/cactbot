ba8b382f7131151acb2b4ccb8a207d294bcb9ef5
Panic Stevenson <panic.stevenson@gmail.com>
2021-04-22 19:52:19 -0700

    raidboss: Convert Autoplay Helper to TypeScript

--- ui/raidboss/autoplay_helper.ts
+++ ui/raidboss/autoplay_helper.ts
@@ -1,10 +1,10 @@
 export default class AutoplayHelper {
-  static Check() {
+  static Check(): boolean {
     const context = new AudioContext();
     return context.state === 'suspended';
   }
 
-  static Prompt() {
+  static Prompt(): void {
     const button = document.createElement('button');
     button.innerText = 'Click to enable audio';
     button.classList.add('autoplay-helper-button');
@@ -14,7 +14,7 @@ export default class AutoplayHelper {
     document.body.appendChild(button);
   }
 
-  static CheckAndPrompt() {
+  static CheckAndPrompt(): void {
     if (AutoplayHelper.Check())
       AutoplayHelper.Prompt();
   }
