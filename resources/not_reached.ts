9d395f2ddfdae35d1da6125ef53f4301bef95416
Adrienne Walker <enne@quisquo.us>
2021-05-06 18:58:16 -0700

    add more comments

--- resources/not_reached.ts
+++ resources/not_reached.ts
@@ -4,6 +4,8 @@
 // The intention here is that the programmer does not expect a particular
 // bit of code to happen, and so has not written careful error handling.
 // If it does occur, at least there will be an error and we can figure out why.
+// This is preferable to casting or disabling TypeScript altogether in order to
+// avoid syntax errors.
 
 // One common example is a regex, where if the regex matches then all of the
 // (non-optional) regex groups will also be valid, but TypeScript doesn't know.
