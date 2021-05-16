6fa704a66848d548c80d40352f83b31090009d33
Adrienne Walker <enne@quisquo.us>
2021-05-15 17:23:02 -0700

    oopsy: add o12s part 2 (#2865)

--- ui/oopsyraidsy/data/04-sb/raid/o12s.js
+++ ui/oopsyraidsy/data/04-sb/raid/o12s.js
@@ -1,7 +1,8 @@
 import NetRegexes from '../../../../../resources/netregexes';
 import ZoneId from '../../../../../resources/zone_id';
 
-// TODO: phase 2
+// TODO: could add Patch warnings for double/unbroken tethers
+// TODO: Hello World could have any warnings (sorry)
 
 export default {
   zoneId: ZoneId.AlphascapeV40Savage,
@@ -13,16 +14,28 @@ export default {
     'O12S1 Superliminal Steel 1': '3330', // Omega-F "get front/back" blades phase
     'O12S1 Superliminal Steel 2': '3331', // Omega-F "get front/back" blades phase
     'O12S1 Optimized Blizzard III': '3332', // Omega-F giant cross
+    'O12S2 Diffuse Wave Cannon': '3369', // back/sides lasers
+    'O12S2 Right Arm Unit Hyper Pulse 1': '335A', // Rotating Archive Peripheral lasers
+    'O12S2 Right Arm Unit Hyper Pulse 2': '335B', // Rotating Archive Peripheral lasers
+    'O12S2 Right Arm Unit Colossal Blow': '335F', // Exploding Archive All hands
+    'O12S2 Left Arm Unit Colossal Blow': '3360', // Exploding Archive All hands
   },
   damageFail: {
     'O12S1 Optical Laser': '3347', // middle laser from eye
     'O12S1 Advanced Optical Laser': '334A', // giant circle centered on eye
+    'O12S2 Rear Power Unit Rear Lasers 1': '3361', // Archive All initial laser
+    'O12S2 Rear Power Unit Rear Lasers 2': '3362', // Archive All rotating laser
   },
   shareWarn: {
     'O12S1 Optimized Fire III': '3337', // fire spread
+    'O12S2 Hyper Pulse Tether': '335C', // Index And Archive Peripheral tethers
+    'O12S2 Wave Cannon': '336B', // Index And Archive Peripheral baited lasers
+    'O12S2 Optimized Fire III': '3379', // Archive All spread
   },
   shareFail: {
     'O12S1 Optimized Sagittarius Arrow': '334D', // Omega-M bard limit break
+    'O12S2 Oversampled Wave Cannon': '3366', // Monitor tank busters
+    'O12S2 Savage Wave Cannon': '336D', // Tank buster with the vuln first
   },
   triggers: [
     {
@@ -46,7 +59,7 @@ export default {
     {
       id: 'O12S1 Magic Vulnerability Up Gain',
       netRegex: NetRegexes.gainsEffect({ effectId: '472' }),
-      run: (data, matches) => {
+      run: (e, data, matches) => {
         data.vuln = data.vuln || {};
         data.vuln[matches.target] = true;
       },
@@ -54,7 +67,7 @@ export default {
     {
       id: 'O12S1 Magic Vulnerability Up Lose',
       netRegex: NetRegexes.losesEffect({ effectId: '472' }),
-      run: (data, matches) => {
+      run: (e, data, matches) => {
         data.vuln = data.vuln || {};
         data.vuln[matches.target] = false;
       },
