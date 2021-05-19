8c79b923f3c7b1b072e08149f14f8dd8bc64eee5
Adrienne Walker <enne@quisquo.us>
2021-05-18 23:15:48 -0700

    raidboss: remove browser tts (#2870)
    Remove the extremely unofficial undocumented API tts path which appears to no longer be working for many users along with the raidboss config option. The "browser TTS" path now only supports the built-in browser window.speechSynthesis path for remote raidboss and that option is enabled implicitly when needed.
    
    If users want to change their cactbot tts voices when using an overlay, they should change their ACT tts voice by going to ACT -> Options -> Sound Settings -> Text to Speech Control Panel Applet and change the Windows tts voice there.
    
    Fixes #2860 {/airquotes}

--- ui/raidboss/popup-text.js
+++ ui/raidboss/popup-text.js
@@ -238,9 +238,9 @@ export class PopupText {
     this.parserLang = this.options.ParserLanguage || 'en';
     this.displayLang = this.options.AlertsLanguage || this.options.DisplayLanguage || this.options.ParserLanguage || 'en';
 
-    if (this.options.IsRemoteRaidboss || this.options.BrowserTTS) {
+    if (this.options.IsRemoteRaidboss) {
       this.ttsEngine = new BrowserTTSEngine(this.displayLang);
-      this.ttsSay = function(text) {
+      this.ttsSay = (text) => {
         this.ttsEngine.play(text);
       };
     } else {
