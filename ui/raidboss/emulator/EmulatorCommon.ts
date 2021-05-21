0337ec045de19221bc53fbef6e2eaa5910aefecb
valarnin <valarnin@gmail.com>
2021-05-20 20:25:10 -0400

    raidemulator: Convert EmulatorCommon, LineEvent, LogRepository to typescript

---
+++ ui/raidboss/emulator/EmulatorCommon.ts
@@ -0,0 +1,122 @@
+import { LocaleNetRegex } from '../../../resources/translations';
+import NetRegexes from '../../../resources/netregexes';
+
+// Disable ban-types for cloneData as it needs to work on raw objects for performance reasons.
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type DataType = { [key: string]: any } | null;
+
+export default class EmulatorCommon {
+  static cloneData(data: DataType, exclude = ['options', 'party']): DataType {
+    const ret: DataType = {};
+
+    // Use extra logic for top-level extend for property exclusion
+    // This cut the execution time of this code from 41,000ms to 50ms when parsing a 12 minute pull
+    for (const i in data) {
+      if (exclude.includes(i))
+        continue;
+
+      if (typeof data[i] === 'object')
+        ret[i] = EmulatorCommon._cloneData(data[i]);
+      else
+        // Assignment of any to any. See DataType definition above for reasoning.
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        ret[i] = data[i];
+    }
+    return ret;
+  }
+
+  static _cloneData(data: DataType): DataType {
+    if (typeof data === 'object') {
+      if (Array.isArray(data)) {
+        const ret = [];
+        for (let i = 0; i < data.length; ++i)
+          ret[i] = EmulatorCommon._cloneData(data[i]);
+
+        return ret;
+      }
+
+      if (data === null)
+        return null;
+
+      if (data instanceof RegExp)
+        return new RegExp(data);
+
+      const ret: DataType = {};
+      for (const i in data)
+        ret[i] = EmulatorCommon._cloneData(data[i]);
+
+      return ret;
+    }
+    return data;
+  }
+
+  static timeToString(time: number, includeMillis = true): string {
+    const negative = time < 0 ? '-' : '';
+    time = Math.abs(time);
+    const millisNum = time % 1000;
+    const secsNum = ((time % (60 * 1000)) - millisNum) / 1000;
+    // Milliseconds
+    const millis = `00${millisNum}`.substr(-3);
+    const secs = `0${secsNum}`.substr(-2);
+    const mins = `0${((((time % (60 * 60 * 1000)) - millisNum) / 1000) - secsNum) / 60}`.substr(-2);
+    return negative + mins + ':' + secs + (includeMillis ? '.' + millis : '');
+  }
+
+  static timeToDateString(time: number): string {
+    return this.dateObjectToDateString(new Date(time));
+  }
+
+  static dateObjectToDateString(date: Date): string {
+    const year = date.getFullYear();
+    const month = EmulatorCommon.zeroPad((date.getMonth() + 1).toString());
+    const day = EmulatorCommon.zeroPad(date.getDate().toString());
+    return `${year}-${month}-${day}`;
+  }
+
+  static timeToTimeString(time: number, includeMillis = false): string {
+    return this.dateObjectToTimeString(new Date(time), includeMillis);
+  }
+
+  static dateObjectToTimeString(date: Date, includeMillis = false): string {
+    const hour = EmulatorCommon.zeroPad(date.getHours().toString());
+    const minute = EmulatorCommon.zeroPad(date.getMinutes().toString());
+    const second = EmulatorCommon.zeroPad(date.getSeconds().toString());
+    let ret = `${hour}:${minute}:${second}`;
+    if (includeMillis)
+      ret = ret + `.${date.getMilliseconds()}`;
+
+    return ret;
+  }
+
+  static msToDuration(ms: number): string {
+    const tmp = EmulatorCommon.timeToString(ms, false);
+    return tmp.replace(':', 'm') + 's';
+  }
+
+  static dateTimeToString(time: number, includeMillis = false): string {
+    const date = new Date(time);
+    return `${this.dateObjectToDateString(date)} ${this.dateObjectToTimeString(date, includeMillis)}`;
+  }
+
+  static zeroPad(str: string, len = 2): string {
+    return ('' + str).padStart(len, '0');
+  }
+
+  static properCase(str: string): string {
+    return str.replace(/([^\W_]+[^\s-]*) */g, (txt) => {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+  }
+
+  static spacePadLeft(str: string, len: number): string {
+    return str.padStart(len, ' ');
+  }
+
+  static sealRegexes = LocaleNetRegex.areaSeal;
+  static engageRegexes = LocaleNetRegex.countdownEngage;
+  static countdownRegexes = LocaleNetRegex.countdownStart;
+  static unsealRegexes = LocaleNetRegex.areaUnseal;
+  static wipeRegex = NetRegexes.network6d({ command: '40000010' });
+  static winRegex = NetRegexes.network6d({ command: '40000003' });
+  static cactbotWipeRegex = NetRegexes.echo({ line: 'cactbot wipe.*?' });
+}
--- ui/raidboss/emulator/data/network_log_converter/LineEvent.ts
+++ ui/raidboss/emulator/data/network_log_converter/LineEvent.ts
@@ -1,40 +1,46 @@
 import EmulatorCommon from '../../EmulatorCommon';
+import LogRepository from './LogRepository';
 
 /**
  * Generic class to track an FFXIV log line
  */
 export default class LineEvent {
-  constructor(repo, line, parts) {
-    this.offset = 0; // To be calculated later
+  public offset = 0;
+  public decEvent: number;
+  public hexEvent: string;
+  public timestamp: number;
+  public networkLine: string;
+  public convertedLine = '';
+  public invalid = false;
+  public index = 0;
 
-    this.parts = parts;
-
-    this.decEvent = parseInt(this.parts[0]);
+  constructor(repo: LogRepository, line: string, public parts: string[]) {
+    this.decEvent = parseInt(this.parts[0] || '');
     this.hexEvent = EmulatorCommon.zeroPad(this.decEvent.toString(16).toUpperCase());
 
-    this.timestamp = +new Date(this.parts[1]);
+    this.timestamp = +new Date(this.parts[1] || '');
 
     this.networkLine = line;
     repo.updateTimestamp(this.timestamp);
   }
 
-  convert() {
+  convert(_: LogRepository): void {
     this.convertedLine = this.prefix() + (this.parts.join(':')).replace('|', ':');
   }
 
-  prefix() {
+  prefix(): string {
     return '[' + EmulatorCommon.timeToTimeString(this.timestamp, true) + '] ' + this.hexEvent + ':';
   }
 
-  static isDamageHallowed(damage) {
-    return parseInt(damage, 16) & parseInt('1000', 16);
+  static isDamageHallowed(damage: string): boolean {
+    return (parseInt(damage, 16) & parseInt('1000', 16)) > 0;
   }
 
-  static isDamageBig(damage) {
-    return parseInt(damage, 16) & parseInt('4000', 16);
+  static isDamageBig(damage: string): boolean {
+    return (parseInt(damage, 16) & parseInt('4000', 16)) > 0;
   }
 
-  static calculateDamage(damage) {
+  static calculateDamage(damage: string): number {
     if (LineEvent.isDamageHallowed(damage))
       return 0;
 
@@ -44,14 +50,13 @@ export default class LineEvent {
       damage.substr(2, 2),
       damage.substr(4, 2),
       damage.substr(6, 2),
-    ];
+    ] as const;
 
     if (!LineEvent.isDamageBig(damage))
       return parseInt(parts.slice(0, 2).reverse().join(''), 16);
 
     return parseInt(
-        parts[3] +
-      parts[0] +
+        (parts[3] + parts[0]) +
       (parseInt(parts[1], 16) - parseInt(parts[3], 16)
       ).toString(16), 16);
   }
