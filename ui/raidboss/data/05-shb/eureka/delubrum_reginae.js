80726335fa475a35d9f34c7cfa91805823cf627a
Jaehyuk-Lee <alex00728@outlook.com>
2021-05-21 17:10:04 +0900

    fix Korean word combination

--- ui/raidboss/data/05-shb/eureka/delubrum_reginae.js
+++ ui/raidboss/data/05-shb/eureka/delubrum_reginae.js
@@ -155,7 +155,7 @@ export default {
           fr: 'À droite',
           ja: '右',
           cn: '右',
-          ko: '오른쪽',
+          ko: '오른',
         },
         south: {
           en: 'Back',
@@ -171,7 +171,7 @@ export default {
           fr: 'À gauche',
           ja: '左',
           cn: '左',
-          ko: '왼쪽',
+          ko: '왼',
         },
         double: {
           en: '${dir1} > ${dir2}',
