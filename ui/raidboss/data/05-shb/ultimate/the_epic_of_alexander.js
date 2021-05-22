d30a69575b86011532c78bb84f669ab6163f277a
Panic Stevenson <panic.stevenson@gmail.com>
2021-05-22 13:38:17 -0700

    raidboss: Fix Non-Outputs Cleave Outputs

--- ui/raidboss/data/05-shb/ultimate/the_epic_of_alexander.js
+++ ui/raidboss/data/05-shb/ultimate/the_epic_of_alexander.js
@@ -309,14 +309,7 @@ export default {
         return output.tankCleave();
       },
       outputStrings: {
-        tankCleave: {
-          en: 'Tank Cleave',
-          de: 'Tank Cleave',
-          fr: 'Tank Cleave',
-          ja: 'タンククリーブ',
-          cn: '坦克顺劈',
-          ko: '광역 탱버',
-        },
+        tankCleave: Outputs.tankCleave,
         tankBusters: Outputs.tankBusters,
         tankBusterOn: Outputs.tankBusterOnPlayer,
         tankBuster: Outputs.tankBuster,
