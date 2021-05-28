6c734f9a88600b0498050f164251adc8316e43ae
valarnin <valarnin@gmail.com>
2021-05-28 19:07:22 -0400

    raidemulator: Convert EventBus to typescript

--- ui/raidboss/emulator/EventBus.ts
+++ ui/raidboss/emulator/EventBus.ts
@@ -1,51 +1,65 @@
+type CallbackFunction = () => void;
+type EventMapEntry = {
+  event: string;
+  // Disable ban-types due to scope being any object type
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  scope: object;
+  callback: CallbackFunction;
+};
+type EventMap = {[event: string]: EventMapEntry[]};
+
 /**
  * This is a base class that classes can extend to inherit event bus capabilities.
  * This allows other classes to listen for events with the `on` function.
  * The inheriting class can fire those events with the `dispatch` function.
  */
 export default class EventBus {
-  constructor() {
-    this.listeners = {};
-  }
+  private listeners: EventMap = {};
   /**
    * Subscribe to an event
    *
-   * @param {string} event The event(s) to subscribe to, space separated
-   * @param {function} callback The callback to invoke
-   * @param {object} scope Optional. The scope to apply the function against
-   *
-   * @return {any} The callbacks registered to the event(s)
+   * @param event The event(s) to subscribe to, space separated
+   * @param callback The callback to invoke
+   * @param scope Optional. The scope to apply the function against
+   * @returns The callbacks registered to the event(s)
    */
-  on(event, callback = undefined, scope = undefined) {
+  on(event: string,
+      callback: CallbackFunction | undefined = undefined,
+      // Disable ban-types due to scope being any object type
+      // eslint-disable-next-line @typescript-eslint/ban-types
+      scope: object | undefined = undefined,
+  ): EventMapEntry[] | undefined {
     const events = event.split(' ');
-    const ret = {};
-    scope = scope !== undefined ? scope : (typeof window === 'undefined' ? {} : window);
+    const ret: EventMapEntry[] = [];
+    scope = scope ?? (typeof window === 'undefined' ? {} : window);
     for (const event of events) {
-      this.listeners[event] = this.listeners[event] || [];
+      const events: EventMapEntry[] = this.listeners[event] ??= [];
       if (callback !== undefined)
-        this.listeners[event].push({ scope: scope, callback: callback });
-      else
-        ret[event] = this.listeners[event];
+        events.push({ event: event, scope: scope, callback: callback });
+      ret.push(...(this.listeners[event] ?? []));
     }
-    if (events.length === 1 && ret[event])
-      return ret[event];
-    else if (callback !== undefined)
-      return ret;
+    return ret;
   }
+
   /**
    * Dispatch an event to any subscribers
-   * @param {string} event The event to dispatch
-   * @param {...*} eventArguments The event arguments to pass to listeners
+   *
+   * @param event The event to dispatch
+   * @param eventArguments The event arguments to pass to listeners
+   * @returns A promise that can be await'd or ignored
    */
-  async dispatch(event, ...eventArguments) {
+  // Disable no-expicit-any and explicit-module-boundary-types for eventArguments since it's
+  // deliberately any-type/generic. Also disable max-len for the eslint syntax line because it's
+  // too long.
+  // eslint-disable-next-line max-len
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
+  async dispatch(event: string, ...eventArguments: any): Promise<undefined> {
     if (this.listeners[event] === undefined)
       return;
 
-    for (let i = 0; i < this.listeners[event].length; ++i) {
-      const l = this.listeners[event][i];
+    for (const l of this.listeners[event] ?? []) {
       const res = l.callback.apply(l.scope, eventArguments);
-      if (Promise.resolve(res) === res)
-        await res;
+      await Promise.resolve(res);
     }
   }
 }
