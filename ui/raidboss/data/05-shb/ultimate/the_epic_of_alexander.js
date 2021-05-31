8735bc126e4b1b8805d3f9d1039ca08adedb9b8e
Panic Stevenson <panic.stevenson@gmail.com>
2021-05-30 22:37:08 -0700

    raidboss: Move Short Text Conditionals to Conditions (#2964)

--- ui/raidboss/data/05-shb/ultimate/the_epic_of_alexander.js
+++ ui/raidboss/data/05-shb/ultimate/the_epic_of_alexander.js
@@ -2578,11 +2578,9 @@ export default {
       netRegexFr: NetRegexes.ability({ source: 'Alexander parfait', id: '4B14', capture: false }),
       netRegexJa: NetRegexes.ability({ source: 'パーフェクト・アレキサンダー', id: '4B14', capture: false }),
       netRegexKo: NetRegexes.ability({ source: '완전체 알렉산더', id: '4B14', capture: false }),
+      condition: (data) => data.radiantOutputStringKey,
       delaySeconds: 16,
-      alertText: (data, _matches, output) => {
-        if (data.radiantOutputStringKey)
-          return output[data.radiantOutputStringKey]();
-      },
+      alertText: (data, _matches, output) => output[data.radiantOutputStringKey](),
       outputStrings: radiantOutputStrings,
     },
     {
