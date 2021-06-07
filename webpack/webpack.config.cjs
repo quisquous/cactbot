c2ce88b5ada04afbf660024e9dfeef5f7f455db7
Panic Stevenson <panic.stevenson@gmail.com>
2021-06-06 22:14:37 -0700

    Also remove import

--- webpack/webpack.config.cjs
+++ webpack/webpack.config.cjs
@@ -2,7 +2,6 @@
 
 const path = require('path');
 const webpack = require('webpack');
-const TerserWebpackPlugin = require('terser-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
