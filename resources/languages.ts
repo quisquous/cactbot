43edddbd46dbb98f592f312bee685fe45bbdd863
valarnin <valarnin@gmail.com>
2021-06-07 23:19:47 -0400

    Update per review

--- resources/languages.ts
+++ resources/languages.ts
@@ -1,7 +1,12 @@
-export const Languages = ['en', 'de', 'fr', 'ja', 'cn', 'ko'] as const;
+export const languages = ['en', 'de', 'fr', 'ja', 'cn', 'ko'] as const;
 
-export type Lang = typeof Languages[number];
+export type Lang = typeof languages[number];
 
 export type NonEnLang = Exclude<Lang, 'en'>;
 
-export const isLang = (lang?: string): lang is Lang => Languages.includes(lang as Lang);
+export const isLang = (lang?: string): lang is Lang => {
+  const langStrs: readonly string[] = languages;
+  if (!lang)
+    return false;
+  return langStrs.includes(lang);
+};
