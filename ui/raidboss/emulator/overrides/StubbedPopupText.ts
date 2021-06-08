2cdb9f6df6862d8a9dc0aedf19fbe388f48b62f7
valarnin <valarnin@gmail.com>
2021-06-08 17:01:10 -0400

    raidemulator: Convert StubbedPopupText to typescript

---
+++ ui/raidboss/emulator/overrides/StubbedPopupText.ts
@@ -0,0 +1,13 @@
+import { RaidbossOptions } from 'ui/raidboss/raidboss_options';
+import { TimelineLoader } from 'ui/raidboss/timeline';
+import { PopupText } from '../../popup-text';
+import { RaidbossFileData } from './data/raidboss_manifest.txt';
+
+export default class StubbedPopupText extends PopupText {
+  constructor(options: RaidbossOptions, timelineLoader: TimelineLoader, raidbossFileData: RaidbossFileData) {
+    super(options, timelineLoader, raidbossFileData);
+  }
+
+  // Stubbed, we don't want overlay hooks
+  HookOverlays() { }
+}
