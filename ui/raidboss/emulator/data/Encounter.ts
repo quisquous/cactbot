quisquous/cactbot

commit 544b64f58627bf5f833564e1000b88b66faee576

raidemulator: Convert Encounter to typescript  (#3029)

ui/raidboss/emulator/data/Encounter.ts
@@ -1,15 +1,19 @@
 import CombatantTracker from './CombatantTracker';
-import LogEventHandler from './LogEventHandler';
 import PetNamesByLang from '../../../../resources/pet_names';
 import EmulatorCommon from '../EmulatorCommon';
 import LogRepository from './network_log_converter/LogRepository';
 import NetworkLogConverter from './NetworkLogConverter';
+import { Lang, isLang } from '../../../../resources/languages';
+import LineEvent, { isLineEventSource, isLineEventTarget } from './network_log_converter/LineEvent';
+import { UnreachableCode } from '../../../../resources/not_reached';
 
-const isPetName = (name, language = undefined) => {
+const isPetName = (name: string, language?: Lang) => {
   if (language)
     return PetNamesByLang[language].includes(name);
 
   for (const lang in PetNamesByLang) {
+    if (!isLang(lang))
+      throw new UnreachableCode();
     if (PetNamesByLang[lang].includes(name))
       return true;
   }
@@ -17,40 +21,59 @@ const isPetName = (name, language = undefined) => {
   return false;
 };
 
+const isValidTimestamp = (timestamp: number) => {
+  return timestamp > 0 && timestamp < Number.MAX_SAFE_INTEGER;
+};
+
 export default class Encounter {
-  constructor(encounterDay, encounterZoneId, encounterZoneName, logLines) {
+  private static readonly encounterVersion = 1;
+  public id?: number;
+  version: number;
+  initialOffset = Number.MAX_SAFE_INTEGER;
+  endStatus = 'Unknown';
+  startStatus = 'Unknown';
+  private engageAt = Number.MAX_SAFE_INTEGER;
+  private firstPlayerAbility = Number.MAX_SAFE_INTEGER;
+  private firstEnemyAbility = Number.MAX_SAFE_INTEGER;
+  firstLineIndex = 0;
+  combatantTracker?: CombatantTracker;
+  startTimestamp = 0;
+  endTimestamp = 0;
+  duration = 0;
+  playbackOffset = 0;
+  language: Lang = 'en';
+
+  constructor(
+    public encounterDay: string,
+    public encounterZoneId: string,
+    public encounterZoneName: string,
+    public logLines: LineEvent[]) {
     this.version = Encounter.encounterVersion;
-    this.id = null;
-    this.encounterZoneId = encounterZoneId;
-    this.encounterZoneName = encounterZoneName;
-    this.encounterDay = encounterDay;
-    this.logLines = logLines;
   }
 
-  initialize() {
-    this.initialOffset = Number.MAX_SAFE_INTEGER;
-    this.endStatus = 'Unknown';
-    this.startStatus = new Set();
-    this.engageAt = Number.MAX_SAFE_INTEGER;
-    this.firstPlayerAbility = Number.MAX_SAFE_INTEGER;
-    this.firstEnemyAbility = Number.MAX_SAFE_INTEGER;
+  initialize(): void {
+    const startStatuses = new Set<string>();
 
-    this.firstLineIndex = 0;
+    this.logLines.forEach((line, i) => {
+      if (!line)
+        throw new UnreachableCode();
 
-    for (let i = 0; i < this.logLines.length; ++i) {
-      const line = this.logLines[i];
       let res = EmulatorCommon.matchStart(line.networkLine);
       if (res) {
         this.firstLineIndex = i;
-        this.startStatus.add(res.groups.StartType);
-        const startIn = parseInt(res.groups.StartIn);
-        if (startIn >= 0)
-          this.engageAt = Math.min(line.timestamp + startIn, this.engageAt);
+        if (res.groups?.StartType)
+          startStatuses.add(res.groups.StartType);
+        if (res.groups?.StartIn) {
+          const startIn = parseInt(res.groups.StartIn);
+          if (startIn >= 0)
+            this.engageAt = Math.min(line.timestamp + startIn, this.engageAt);
+        }
       } else {
         res = EmulatorCommon.matchEnd(line.networkLine);
         if (res) {
-          this.endStatus = res.groups.EndType;
-        } else if (line.id && line.targetId) {
+          if (res.groups?.EndType)
+            this.endStatus = res.groups.EndType;
+        } else if (isLineEventSource(line) && isLineEventTarget(line)) {
           if (line.id.startsWith('1') ||
             (line.id.startsWith('4') && isPetName(line.name, this.language))) {
             // Player or pet ability
@@ -67,20 +90,10 @@ export default class Encounter {
           }
         }
       }
-      if (res && res.groups && res.groups.language)
-        this.language = res.groups.language || this.language;
-    }
-
-    this.language = this.language || 'en';
-
-    if (this.firstPlayerAbility === Number.MAX_SAFE_INTEGER)
-      this.firstPlayerAbility = null;
-
-    if (this.firstEnemyAbility === Number.MAX_SAFE_INTEGER)
-      this.firstEnemyAbility = null;
-
-    if (this.engageAt === Number.MAX_SAFE_INTEGER)
-      this.engageAt = null;
+      const matchedLang = res?.groups?.language;
+      if (isLang(matchedLang))
+        this.language = matchedLang;
+    });
 
     this.combatantTracker = new CombatantTracker(this.logLines, this.language);
     this.startTimestamp = this.combatantTracker.firstTimestamp;
@@ -88,26 +101,29 @@ export default class Encounter {
     this.duration = this.endTimestamp - this.startTimestamp;
 
     if (this.initialOffset === Number.MAX_SAFE_INTEGER) {
-      if (this.engageAt !== null)
+      if (this.engageAt < Number.MAX_SAFE_INTEGER)
         this.initialOffset = this.engageAt - this.startTimestamp;
-      else if (this.firstPlayerAbility !== null)
+      else if (this.firstPlayerAbility < Number.MAX_SAFE_INTEGER)
         this.initialOffset = this.firstPlayerAbility - this.startTimestamp;
-      else if (this.firstEnemyAbility !== null)
+      else if (this.firstEnemyAbility < Number.MAX_SAFE_INTEGER)
         this.initialOffset = this.firstEnemyAbility - this.startTimestamp;
       else
         this.initialOffset = 0;
     }
 
-    this.playbackOffset = this.logLines[this.firstLineIndex].offset;
+    const firstLine = this.logLines[this.firstLineIndex];
 
-    this.startStatus = [...this.startStatus].sort().join(', ');
+    if (firstLine && firstLine.offset)
+      this.playbackOffset = firstLine.offset;
+
+    this.startStatus = [...startStatuses].sort().join(', ');
   }
 
-  shouldPersistFight() {
-    return this.firstPlayerAbility > 0 && this.firstEnemyAbility > 0;
+  shouldPersistFight(): boolean {
+    return isValidTimestamp(this.firstPlayerAbility) && isValidTimestamp(this.firstEnemyAbility);
   }
 
-  upgrade(version) {
+  upgrade(version: number): boolean {
     if (Encounter.encounterVersion <= version)
       return false;
 
@@ -123,5 +139,3 @@ export default class Encounter {
     return true;
   }
 }
-
-Encounter.encounterVersion = 1;

ui/raidboss/emulator/overrides/RaidEmulatorTimeline.js
@@ -21,9 +21,9 @@ export default class RaidEmulatorTimeline extends Timeline {
       return;
 
     // This is a bit complicated due to jumps in timelines. If we've already got a timebase,
-    // fightNow needs to be calculated based off of that instead of engageAt
+    // fightNow needs to be calculated based off of that instead of initialOffset
     // timebase = 0 when not set
-    const baseTimestamp = this.timebase || this.emulator.currentEncounter.encounter.engageAt;
+    const baseTimestamp = this.timebase || this.emulator.currentEncounter.encounter.initialOffset;
     const fightNow = (currentLogTime - baseTimestamp) / 1000;
 
     this.SyncTo(fightNow, currentLogTime);

ui/raidboss/emulator/ui/ProgressBar.js
@@ -47,7 +47,7 @@ export default class ProgressBar {
     emulator.on('tick', (currentLogTime) => {
       const currentOffset = currentLogTime - emulator.currentEncounter.encounter.startTimestamp;
       const progPercent = (currentOffset / emulator.currentEncounter.encounter.duration) * 100;
-      const progValue = currentLogTime - emulator.currentEncounter.encounter.engageAt;
+      const progValue = currentLogTime - emulator.currentEncounter.encounter.initialOffset;
       this.$progressBarCurrent.textContent = EmulatorCommon.timeToString(progValue, false);
       this.$progressBar.setAttribute('ariaValueNow', progValue);
       this.$progressBar.style.width = progPercent + '%';

ui/raidboss/raidemulator.js
@@ -214,7 +214,7 @@ import './raidemulator.css';
                   Start: ${new Date(enc.startTimestamp)}
                   End: ${new Date(enc.endTimestamp)}
                   Duration: ${EmulatorCommon.msToDuration(enc.endTimestamp - enc.startTimestamp)}
-                  Pull Duration: ${EmulatorCommon.msToDuration(enc.endTimestamp - enc.engageAt)}
+                  Pull Duration: ${EmulatorCommon.msToDuration(enc.endTimestamp - enc.initialOffset)}
                   Started By: ${enc.startStatus}
                   End Status: ${enc.endStatus}
                   Line Count: ${enc.logLines.length}

commit a9505354e63e49b3133d32a0439eeca13091bd1b

raidemulator: Fix issue caused by #3029 with initialOffset

ui/raidboss/emulator/data/Encounter.ts
@@ -119,6 +119,10 @@ export default class Encounter {
     this.startStatus = [...startStatuses].sort().join(', ');
   }
 
+  public get initialTimestamp() : number {
+    return this.startTimestamp + this.initialOffset;
+  }
+
   shouldPersistFight(): boolean {
     return isValidTimestamp(this.firstPlayerAbility) && isValidTimestamp(this.firstEnemyAbility);
   }

ui/raidboss/emulator/overrides/RaidEmulatorTimeline.js
@@ -23,7 +23,8 @@ export default class RaidEmulatorTimeline extends Timeline {
     // This is a bit complicated due to jumps in timelines. If we've already got a timebase,
     // fightNow needs to be calculated based off of that instead of initialOffset
     // timebase = 0 when not set
-    const baseTimestamp = this.timebase || this.emulator.currentEncounter.encounter.initialOffset;
+    const baseTimestamp = this.timebase ||
+      this.emulator.currentEncounter.encounter.initialTimestamp;
     const fightNow = (currentLogTime - baseTimestamp) / 1000;
 
     this.SyncTo(fightNow, currentLogTime);

ui/raidboss/emulator/ui/ProgressBar.js
@@ -47,7 +47,7 @@ export default class ProgressBar {
     emulator.on('tick', (currentLogTime) => {
       const currentOffset = currentLogTime - emulator.currentEncounter.encounter.startTimestamp;
       const progPercent = (currentOffset / emulator.currentEncounter.encounter.duration) * 100;
-      const progValue = currentLogTime - emulator.currentEncounter.encounter.initialOffset;
+      const progValue = currentLogTime - emulator.currentEncounter.encounter.initialTimestamp;
       this.$progressBarCurrent.textContent = EmulatorCommon.timeToString(progValue, false);
       this.$progressBar.setAttribute('ariaValueNow', progValue);
       this.$progressBar.style.width = progPercent + '%';

ui/raidboss/raidemulator.js
@@ -214,7 +214,7 @@ import './raidemulator.css';
                   Start: ${new Date(enc.startTimestamp)}
                   End: ${new Date(enc.endTimestamp)}
                   Duration: ${EmulatorCommon.msToDuration(enc.endTimestamp - enc.startTimestamp)}
-                  Pull Duration: ${EmulatorCommon.msToDuration(enc.endTimestamp - enc.initialOffset)}
+                  Pull Duration: ${EmulatorCommon.msToDuration(enc.endTimestamp - enc.initialTimestamp)}
                   Started By: ${enc.startStatus}
                   End Status: ${enc.endStatus}
                   Line Count: ${enc.logLines.length}