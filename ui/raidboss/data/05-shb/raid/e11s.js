1393e7d472cd3608f7f2522e83701a77fbf61eac
Jaehyuk-Lee <alex00728@outlook.com>
2021-06-10 15:21:12 +0900

    Update ui/raidboss/data/05-shb/raid/e11s.js
    Co-authored-by: Trim21 <github@trim21.me>

--- ui/raidboss/data/05-shb/raid/e11s.js
+++ ui/raidboss/data/05-shb/raid/e11s.js
@@ -661,7 +661,7 @@ export default {
           de: 'Mehrfache Vergeltung: Blitz',
           fr: 'Multi-taillade magique: Foudre',
           ja: '魔装連続剣：いなずま',
-          cn: '魔装连续剑: 雷',
+          cn: '雷连续剑',
           ko: '연속검: 번개',
         },
       },
