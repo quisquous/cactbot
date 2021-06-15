98e9ce191620657993f5f1e2db176d7e60301350
Maiko Tan <maiko.tan.coding@gmail.com>
2021-06-15 03:06:22 +0000

    change per comment

--- webpack/webpack.ghpages.cjs
+++ webpack/webpack.ghpages.cjs
@@ -2,20 +2,8 @@
 
 const { merge } = require('webpack-merge');
 
-const generateConfig = require('./webpack.config.cjs');
-const constants = require('./constants.cjs');
+const ProdConfig = require('./webpack.prod.cjs');
 
-// Add timerbar_test.html
-constants.cactbotModules['timerbarTest'] = 'ui/test/timerbar_test';
-constants.cactbotHtmlChunksMap['ui/test/timerbar_test.html'] = {
-  chunks: [
-    constants.cactbotModules.timerbarTest,
-  ],
-};
-
-const baseConfig = generateConfig(constants);
-
-module.exports = merge(baseConfig, {
-  mode: 'production',
+module.exports = merge(ProdConfig, {
   devtool: 'inline-source-map',
 });
