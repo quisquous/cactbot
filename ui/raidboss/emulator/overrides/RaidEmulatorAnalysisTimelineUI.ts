87daaa96539f0437e018327c6b29d416eb5c3f08
valarnin <valarnin@gmail.com>
2021-06-15 12:11:16 -0400

    raidemulator: Convert RaidEmulatorAnalysisTimelineUI to typescript

--- ui/raidboss/emulator/overrides/RaidEmulatorAnalysisTimelineUI.ts
+++ ui/raidboss/emulator/overrides/RaidEmulatorAnalysisTimelineUI.ts
@@ -1,16 +1,22 @@
-import RaidEmulatorTimelineUI from './RaidEmulatorTimelineUI';
+import { RaidbossOptions } from '../../raidboss_options';
+import RaidEmulatorTimelineUI, { EmulatorTimerBar } from './RaidEmulatorTimelineUI';
+import { Event } from '../../timeline';
 
 export default class RaidEmulatorAnalysisTimelineUI extends RaidEmulatorTimelineUI {
-  constructor(options) {
+  constructor(options: RaidbossOptions) {
     super(options);
+    // Use orphaned child div to prevent DOM updates
     this.$barContainer = document.createElement('div');
-    this.$progressTemplate = document.querySelector('template.progress').content.firstElementChild;
   }
 
-  // Stub out these methods for performance
-  updateBar(bar, currentLogTime) {}
-  OnAddTimer(fightNow, e, channeling) {}
+  updateBar(_bar: EmulatorTimerBar, _currentLogTime: number): void {
+    // Stubbed out for performance
+  }
+  protected OnAddTimer(_fightNow: number, _e: Event, _channeling: boolean): void {
+    // Stubbed out for performance
+  }
 
-  // Override
-  OnRemoveTimer(e, expired) {}
+  OnRemoveTimer(_e: Event, _expired: boolean): void {
+    // Stubbed out for performance
+  }
 }
