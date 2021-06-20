e28363462a57335279d4ac57bce713f7b4f03b1a
Maiko Tan <maiko.tan.coding@gmail.com>
2021-06-20 21:57:52 +0000

    fix: missing error

--- util/gen_effect_id.ts
+++ util/gen_effect_id.ts
@@ -105,7 +105,7 @@ const makeEffectMap = (table: Table<'#', 'Name'>) => {
   // Make sure everything specified in known_mapping was found in the above loop.
   for (const rawName of Object.keys(knownMapping)) {
     const name = cleanName(rawName);
-    if (name && !(name in foundNames))
+    if (name && !(foundNames.has(name)))
       printError('missing', name, knownMapping, rawName);
   }
 
