031d1f9d5ae3698b2fc7b0226b772572a9c15e39
Adrienne Walker <enne@quisquo.us>
2021-07-07 09:01:26 -0700

    Revert "i18n: Update drowned_city_of_skalla.js (#3273)" (#3274)
    This reverts commit a0584817708b90254d018d250ce9749ee80bd4ad.

--- 04-sb/dungeon/drowned_city_of_skalla.js
+++ 04-sb/dungeon/drowned_city_of_skalla.js
@@ -24,7 +24,7 @@ Options.Triggers.push({
         text: {
           en: 'tail cleave',
           de: 'schweifattacke',
-          fr: 'évitez la queue',
+          fr: 'attaquez la queue',
           ja: 'しっぽ！',
           cn: '尾巴攻击',
           ko: '꼬리쓸기',
