ce37fc8dd055f9062a7c40e8dc6873d0ea56ba78
Maiko Tan <maiko.tan.coding@gmail.com>
2021-07-12 16:18:23 +0800

    style: prefer optional operator

--- ui/jobs/jobs.js
+++ ui/jobs/jobs.js
@@ -568,8 +568,7 @@ export class Bars {
   }
 
   _updateJobComponent(job) {
-    if (this.jobComponent)
-      this.jobComponent.reset();
+    this.jobComponent?.reset();
 
     const JobComponent = getComponent(job);
     if (JobComponent) {
