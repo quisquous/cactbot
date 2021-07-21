d82660116658c1303a53449fa5082299508abae6
Akurosia Kamo <kamothegamer@web.de>
2021-07-21 03:16:24 +0200

    plugin: update CE opcode for 5.58 (#3335)

--- plugin/CactbotEventSource/FateWatcher.cs
+++ plugin/CactbotEventSource/FateWatcher.cs
@@ -55,7 +55,8 @@ namespace Cactbot {
     // v5.5h            0x160
     // v5.55            0x1ac
     // v5.55h           0x248
-    // v5.57           0x3ce
+    // v5.57            0x3ce
+    // v5.58            0x10D
     //
     // CN
     // v5.35            0x144
@@ -81,7 +82,7 @@ namespace Cactbot {
 
     private static readonly CEDirectorOPCodes cedirector_intl = new CEDirectorOPCodes(
       0x30,
-      0x3ce
+      0x10D
     );
 
     private struct ActorControl143{
