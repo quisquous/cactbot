805403d56f6cf4a8ea09341cf8fc094b56956ed1
Adrienne Walker <enne@quisquo.us>
2021-07-25 11:45:20 -0700

    ci: support multiple scopes in PR titles (#3351)

--- .github/scripts/lint-pr-title.cjs
+++ .github/scripts/lint-pr-title.cjs
@@ -64,13 +64,18 @@ Currently your title is: \`${title}\`, but it should:
 \`scope\` can be any of the following:
 ${[...validScope].sort().map((s) => `- \`${s}\``).join('\n')}
 
+Multiple scopes can be combined with slashes if needed, e.g. \`raidboss/oopsy\`.
+
 Valid Title Examples:
 - \`i18n: translating a bunch of new stuff\`
 - \`raidboss: fix bug in New Ultimate Fight\`
 - \`plugin: update Bozja CE code for patch 6.34\`
 - \`lint: change @typescript/irritating-lint warning\`
+- \`raidboss/oopsy: support shiny new dungeon\`
 
 Drafts can also use \`[wip]\` as a prefix to show that they are a "work in progress", e.g. \`[wip] plugin: refactoring everything\`.
+
+Breaking changes can use \`!\` as a prefix to mark a breaking change, e.g. \`!plugin: change all the apis\`.
 
 </details>
 
@@ -93,7 +98,7 @@ const checkTitle = async (octokit, owner, repo, pullNumber) => {
   });
   const { title, user } = pullRequest;
   const userName = user?.login;
-  const m = /^((?<prefix>[\w!\[\]]*)\s*)?\b(?<scope>\w+):\s?.+$/.exec(title);
+  const m = /^(?<prefix>(?:[\w\[\]]*\s+)!?)?(?<scope>[\w\/]+):\s?.+$/.exec(title);
   const lengthValid = title.length <= maxLength;
   let formatValid = false;
 
@@ -103,18 +108,19 @@ const checkTitle = async (octokit, owner, repo, pullNumber) => {
     'issue_number': pullNumber,
   });
 
-  const myComment = comments.find(({ user }) => user?.login === botName);
-
   if (m && m.groups) {
     const groups = m.groups;
     console.log(`Matches: scope: ${groups.scope}, prefix: ${groups.prefix}`);
 
-    const scopeValid = validScope.includes(groups.scope);
-    const prefixValid = validPrefix.includes(groups.prefix) || !groups.prefix;
+    const scopes = groups.scope.split('/');
+    const scopeValid = scopes.every((scope) => validScope.includes(scope));
+    const prefixValid = !groups.prefix || validPrefix.includes(groups.prefix.trim());
     formatValid = scopeValid && prefixValid;
   } else {
     console.error('PR title did not match.');
   }
+
+  const myComment = comments.find(({ user }) => user?.login === botName);
 
   if (formatValid && lengthValid) {
     if (myComment) {
