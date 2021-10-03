c1d190bf96e72bfa0153fb1cd22998d15c39a6d6
Adrienne Walker <enne@quisquo.us>
2021-10-03 14:10:35 -0700

    raidboss/oopsy: add rathalos normal

---
+++ ui/oopsyraidsy/data/04-sb/trial/rathalos.ts
@@ -0,0 +1,51 @@
+import ZoneId from '../../../../../resources/zone_id';
+import { OopsyData } from '../../../../../types/data';
+import { OopsyTriggerSet } from '../../../../../types/oopsy';
+
+// Note: Ignoring Burns (629), which you get temporarily from being in stack marker / fire puddle.
+
+// Just for reference, this fight has "double" abilities, where the first hit
+// does no damage.  Sometimes the set of people hit by the first and not by
+// the second are not the same, but usually they are.  Some of these have
+// starts using lines (but targeted on a player or unknown).
+// * Mangle (286A), paired with (2CB9)
+// * Mangle (287A), paired with (2CC2)
+// * Flaming Recoil (2870), paired with (2CBE)
+// * Tail Swing (286C), paired with (2CBB)
+// * Fire Breath (286E), paired with (2CBD)
+// * Rush (286D), paired with (2CBC)
+// * Rush (2878), paired with (2CC0)
+// * Sweeping Flames (2879), paired with (2CC1)
+// * Fireball (2876), paired with (2CBA)
+
+export type Data = OopsyData;
+
+// Rathalos Normal
+const triggerSet: OopsyTriggerSet<Data> = {
+  zoneId: ZoneId.TheGreatHunt,
+  damageWarn: {
+    'Rathalos Mangle 1': '2CB9', // first phase frontal conal
+    'Rathalos Mangle 2': '2CC2', // second phase conal swipe
+    'Rathalos Rush 1': '2CBC', // first phase charge attack
+    'Rathalos Rush 2': '2CC0', // second phase charge attack, gives poison
+    'Rathalos Tail Smash': '286B', // first phase rear conal, paired with Mangle (2CB9)
+    'Rathalos Tail Swing': '2CBB', // right side attack?
+    'Rathalos Flaming Recoil': '2CBE', // frontal conal + knockback
+    'Rathalos Steppe Sheep Lullaby': '287B', // centered circle
+    'Rathalos Steppe Yamaa Head Butt': '287C', // line aoe
+    'Rathalos Steppe Coeurl Wide Blaster': '287D', // very large conal
+    'Rathalos Garula Rush': '287F', // line charge into the arena
+    'Rathalos Sweeping Flames': '2CC1', // second phase frontal conal
+  },
+  shareWarn: {
+    'Rathalos Fire Breath': '2CBD', // first phase red circle spread headmarker
+  },
+  shareFail: {
+    'Rathalos Garula Heave': '287E', // untelegraphed conal cleave
+  },
+  soloWarn: {
+    'Rathalos Fireball': '2CBA', // second phase stack that leaves a fire puddle
+  },
+};
+
+export default triggerSet;
---
+++ ui/raidboss/data/04-sb/trial/rathalos.ts
@@ -0,0 +1,111 @@
+import Conditions from '../../../../../resources/conditions';
+import NetRegexes from '../../../../../resources/netregexes';
+import { Responses } from '../../../../../resources/responses';
+import ZoneId from '../../../../../resources/zone_id';
+import { RaidbossData } from '../../../../../types/data';
+import { TriggerSet } from '../../../../../types/trigger';
+
+// Note: To avoid too many alert-level triggers here, all of the "out of front"
+// ones are info, under the assumption that you should never be in front.
+
+export type Data = RaidbossData;
+
+// Rathalos Extreme
+const triggerSet: TriggerSet<Data> = {
+  zoneId: ZoneId.TheGreatHunt,
+  triggers: [
+    {
+      // Frontal conal + tail swipe on left side.
+      id: 'Rathalos Mangle Phase 1',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ source: 'Rathalos', id: '286A', capture: false }),
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Right Flank (or out)',
+        },
+      },
+    },
+    {
+      id: 'Rathalos Mangle Phase 2',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ source: 'Rathalos', id: '287A', capture: false }),
+      response: Responses.awayFromFront('info'),
+    },
+    {
+      // Tail swipe on the right side (and then 180 flip, repeat).
+      id: 'Rathalos Tail Swing',
+      type: 'Ability',
+      // No starts using for this, but this ability is 1.5s warning.
+      netRegex: NetRegexes.ability({ source: 'Rathalos', id: '286C', capture: false }),
+      // This hits multiple people.
+      suppressSeconds: 1,
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          // TODO: we could be fancier and say "Run through" or something for #2.
+          en: 'Left Flank (or out)',
+        },
+      },
+    },
+    {
+      id: 'Rathalos Flaming Recoil',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ source: 'Rathalos', id: '2870', capture: false }),
+      response: Responses.awayFromFront('info'),
+    },
+    {
+      id: 'Rathalos Rush',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ source: 'Rathalos', id: ['286D', '2878'], capture: false }),
+      response: Responses.awayFromFront('info'),
+    },
+    {
+      id: 'Rathalos Garula Add',
+      type: 'AddedCombatant',
+      netRegex: NetRegexes.addedCombatantFull({ npcNameId: '6173', capture: false }),
+      // Garula stuns and then puts down the telegraph from the east.
+      // We could be like "go somewhere other than east", but "go west" is clearer.
+      response: Responses.goWest(),
+    },
+    {
+      id: 'Rathalos Garula Targetable',
+      type: 'AddedCombatant',
+      netRegex: NetRegexes.addedCombatantFull({ npcNameId: '6173', capture: false }),
+      delaySeconds: 15,
+      // This is obnoxious to have as an alarm, but it will cause a wipe if nobody does this.
+      alarmText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Use Foothold for QTE',
+        },
+      },
+    },
+    {
+      id: 'Rathalos Fire Breath',
+      type: 'HeadMarker',
+      // Corresponds with 28CE/2CBD ability.
+      netRegex: NetRegexes.headMarker({ id: '0081' }),
+      condition: Conditions.targetIsYou(),
+      response: Responses.spread(),
+    },
+    {
+      id: 'Rathalos Fireball',
+      type: 'HeadMarker',
+      // Corresponds with 2876/2CBA ability.
+      netRegex: NetRegexes.headMarker({ id: '005D' }),
+      response: Responses.stackMarkerOn(),
+    },
+    {
+      id: 'Rathalos Sweeping Flames',
+      type: 'Ability',
+      // No starts using for this, but this ability is 1.5s warning.
+      netRegex: NetRegexes.ability({ source: 'Rathalos', id: '2879', capture: false }),
+      // This hits multiple people.
+      suppressSeconds: 1,
+      response: Responses.awayFromFront('info'),
+    },
+  ],
+};
+
+export default triggerSet;
