21802815f5639344991a888e941e15d163cad4cd
Adrienne Walker <enne@quisquo.us>
2021-10-17 10:40:55 -0700

    eureka: fix bug in flag/refresh config settings
    This was causing flags to disappear instantly if you had ever
    attempted to set a different time.
    
    Fixes #3524.

--- ui/eureka/eureka_config.ts
+++ ui/eureka/eureka_config.ts
@@ -29,9 +29,14 @@ UserConfig.registerOptions('eureka', {
       type: 'float',
       default: 90,
       setterFunc: (options, value) => {
-        if (typeof value !== 'number')
+        let seconds: number;
+        if (typeof value === 'string')
+          seconds = parseFloat(value);
+        else if (typeof value === 'number')
+          seconds = value;
+        else
           return;
-        options['FlagTimeoutMs'] = value * 1000;
+        options['FlagTimeoutMs'] = seconds * 1000;
       },
     },
     {
@@ -187,9 +192,14 @@ UserConfig.registerOptions('eureka', {
       type: 'float',
       default: 1,
       setterFunc: (options, value) => {
-        if (typeof value !== 'number')
+        let seconds: number;
+        if (typeof value === 'string')
+          seconds = parseFloat(value);
+        else if (typeof value === 'number')
+          seconds = value;
+        else
           return;
-        options['RefreshRateMs'] = value * 1000;
+        options['RefreshRateMs'] = seconds * 1000;
       },
     },
   ],
