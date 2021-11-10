effe97e9400d25baa98bb449dc0ec2b61e1cd749
Jacob Keller <jacob.keller@gmail.com>
2021-11-09 22:01:01 -0800

    raidboss: add alerts for Chimera in Cutter's Cry
    Add basic alerts for the "Dragon's Voice" and "Ram's Voice" attacks of
    the Chimera in Cutter's Cry.
    
    These are straightforward alerts to warn the player to get in or get out
    depending on if it is Dragon's Voice (A lightning donut AOE) or Ram's
    Voice (An ice explosion in melee area).
    
    Signed-off-by: Jacob Keller <jacob.keller@gmail.com>

---
+++ ui/raidboss/data/02-arr/dungeon/cutters_cry.ts
@@ -0,0 +1,29 @@
+import NetRegexes from '../../../../../resources/netregexes';
+import { Responses } from '../../../../../resources/responses';
+import ZoneId from '../../../../../resources/zone_id';
+import { RaidbossData } from '../../../../../types/data';
+import { TriggerSet } from '../../../../../types/trigger';
+
+export type Data = RaidbossData;
+
+const triggerSet: TriggerSet<Data> = {
+  zoneId: ZoneId.CuttersCry,
+  triggers: [
+    {
+      id: 'Chimera Ram Voice',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: '450', source: 'Chimera', capture: false }),
+      suppressSeconds: 0.1,
+      response: Responses.outOfMelee(),
+    },
+    {
+      id: 'Chimera Dragon Voice',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: '5A2', source: 'Chimera', capture: false }),
+      suppressSeconds: 0.1,
+      response: Responses.getIn(),
+    },
+  ],
+};
+
+export default triggerSet;
