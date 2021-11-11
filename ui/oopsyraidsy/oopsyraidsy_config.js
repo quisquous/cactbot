d6c4b16e737a7b28fe976f4ee73c4007283e5261
VanSlanzar <61565834+VanSlanzar@users.noreply.github.com>
2021-11-11 12:59:33 +0100

    adds missing translations fr

--- ui/oopsyraidsy/oopsyraidsy_config.js
+++ ui/oopsyraidsy/oopsyraidsy_config.js
@@ -269,6 +269,7 @@ UserConfig.registerOptions('oopsyraidsy', {
       name: {
         en: 'Seconds to show death report on death (0=none)',
         de: 'Sekunden um den Todesreport beim Tot anzuzeigen (0=niemals)',
+        fr: 'Secondes pour montrer le rapport de mort sur la mort (0 = aucun)',
         ja: '倒れた時にデスレポートを表示 (0=非表示)',
         cn: '死亡时显示死亡报告的秒数 (0=不显示)',
         ko: '죽었을 때 사망 보고서를 보여주는 시간(초) (0=비활성화)',
@@ -291,6 +292,7 @@ UserConfig.registerOptions('oopsyraidsy', {
       name: {
         en: 'How to show the death report',
         de: 'Wie zeige ich den Todesreport an',
+        fr: 'Comment montrer le rapport de mort',
       },
       type: 'select',
       options: {
@@ -303,6 +305,11 @@ UserConfig.registerOptions('oopsyraidsy', {
           'Left Side': 'links',
           'Right Side': 'rechts',
           '❌ Disabled': 'deaktiviert',
+        },
+        fr: {
+          'Côté gauche': 'gauche',
+          'Côté droit': 'droite',
+          '❌ Disabled': 'désactivé',
         },
       },
       default: 'left',
