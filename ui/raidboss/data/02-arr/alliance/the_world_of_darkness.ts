quisquous/cactbot

commit 09d04fdb3d47851ea6fab831dca21b85a638d411

raidboss: add basic triggers for Angra Mainyu (#3614)
* raidboss: add basic triggers for Angra Mainyu

Angra Mainyu is the first boss of The World of Darkness alliance raid.
This boss has a few instant-death mechanics, including some basic
mechanics that are very easy to add triggers for.

Add a trigger for the "Mortal Gaze" mechanic. When this is cast, players
should look away. If they fail to look away, they acquire the Doom
debuff and will die in 10 seconds unless they reach a cleansing circle.
Add an alert for the look away, and an alarm for when doom is acquired.

Add a trigger for the "Double Vision" attack. This attack is cast
periodically, and damages players based on their debuff. The whole arena
lights up half red and half white. If the player is hit by the red area
they gain Ireful, and if they are hit by the white area they gain
Sullen. Repeated hits of the same color do increasing damage until it is
fatal. Keep track of the current debuff and warn the player of which
color to stand in based on this.

In the future, it would be nice to add some alerts for Level 100 Flare,
Level 150 Doom, and possibly the roulette mechanic. These are more
complex and have been skipped for now.

Signed-off-by: Jacob Keller <jacob.keller@gmail.com>

* raidboss: remove trigger when nothing to do for double vision

Signed-off-by: Jacob Keller <jacob.keller@gmail.com>

* raidboss: reword triggers for Double Vision

Signed-off-by: Jacob Keller <jacob.keller@gmail.com>

* raidboss: use mainTank to decide Double Vision

If the player doesn't currently have one of the two debuffs, use the
status of whether they're the main tank or not for telling them if they
should stand in front or behind of Angra Mainyu.

Signed-off-by: Jacob Keller <jacob.keller@gmail.com>

* raidboss: remove unnecesasry mainTank from Angra Mainyu

Signed-off-by: Jacob Keller <jacob.keller@gmail.com>

ui/raidboss/data/02-arr/alliance/the_world_of_darkness.ts
@@ -1,0 +1,99 @@
+import Conditions from '../../../../../resources/conditions';
+import NetRegexes from '../../../../../resources/netregexes';
+import { Responses } from '../../../../../resources/responses';
+import ZoneId from '../../../../../resources/zone_id';
+import { RaidbossData } from '../../../../../types/data';
+import { TriggerSet } from '../../../../../types/trigger';
+
+export interface Data extends RaidbossData {
+  sullenDebuff?: boolean;
+  irefulDebuff?: boolean;
+}
+
+// TODO:
+//  Angra Mainyu
+//    Add Level 100 Flare
+//    Add Level 150 Doom
+//    Add Roulette?
+//    Add info text for add spawns?
+//  Five-Headed Dragon
+//  Howling Atomos
+//  Cerberus
+//  Cloud of Darkness
+
+const triggerSet: TriggerSet<Data> = {
+  zoneId: ZoneId.TheWorldOfDarkness,
+  triggers: [
+    {
+      id: 'Angra Mainyu Gain Sullen',
+      type: 'GainsEffect',
+      netRegex: NetRegexes.gainsEffect({ effectId: '27c' }),
+      condition: Conditions.targetIsYou(),
+      run: (data) => data.sullenDebuff = true,
+    },
+    {
+      id: 'Angra Mainyu Lose Sullen',
+      type: 'LosesEffect',
+      netRegex: NetRegexes.losesEffect({ effectId: '27c' }),
+      condition: Conditions.targetIsYou(),
+      run: (data) => data.sullenDebuff = false,
+    },
+    {
+      id: 'Angra Mainyu Gain Ireful',
+      type: 'GainsEffect',
+      netRegex: NetRegexes.gainsEffect({ effectId: '27d' }),
+      condition: Conditions.targetIsYou(),
+      run: (data) => data.irefulDebuff = true,
+    },
+    {
+      id: 'Angra Mainyu Lose Ireful',
+      type: 'LosesEffect',
+      netRegex: NetRegexes.losesEffect({ effectId: '27d' }),
+      condition: Conditions.targetIsYou(),
+      run: (data) => data.irefulDebuff = false,
+    },
+    {
+      id: 'Angra Mainyu Double Vision',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: 'CC8', source: 'Angra Mainyu', capture: false }),
+      alertText: (data, _matches, output) => {
+        if (data.sullenDebuff) {
+          // Stand behind boss in the red half to switch to Ireful
+          return output.red!();
+        } else if (data.irefulDebuff) {
+          // Stand in front of boss in the white half to switch to Sullen
+          return output.white!();
+        }
+      },
+      outputStrings: {
+        red: {
+          en: 'Get Behind (Red)',
+        },
+        white: {
+          en: 'Get in Front (White)',
+        },
+      },
+    },
+    {
+      id: 'Angra Mainyu Mortal Gaze',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: ['CD1', 'DAB'], source: 'Angra Mainyu', capture: false }),
+      suppressSeconds: 0.1,
+      response: Responses.lookAway('alert'),
+    },
+    {
+      id: 'Angra Mainyu Gain Doom',
+      type: 'LosesEffect',
+      netRegex: NetRegexes.losesEffect({ effectId: 'd2' }),
+      condition: Conditions.targetIsYou(),
+      alarmText: (_data, _matches, output) => output.cleanse!(),
+      outputStrings: {
+        cleanse: {
+          en: 'Run to Cleanse Circle',
+        },
+      },
+    },
+  ],
+};
+
+export default triggerSet;

commit c7d2255e3c6a063f0b6adee3d5121a108aad8a0a

raidboss: trigger on gain doom, not losing doom
The trigger for Angra Mainyu's doom effect accidentally was set to
trigger when the player *lost* the doom effect. This should have been
a gainsEffect trigger. This way, the alert happens and warns the player
to properly cleanse the debuff. Fix this.

Fixes: 09d04fdb3d47 ("raidboss: add basic triggers for Angra Mainyu (#3614)")
Signed-off-by: Jacob Keller <jacob.keller@gmail.com>

ui/raidboss/data/02-arr/alliance/the_world_of_darkness.ts
@@ -87,8 +87,8 @@ const triggerSet: TriggerSet<Data> = {
     },
     {
       id: 'Angra Mainyu Gain Doom',
-      type: 'LosesEffect',
-      netRegex: NetRegexes.losesEffect({ effectId: 'd2' }),
+      type: 'GainsEffect',
+      netRegex: NetRegexes.gainsEffect({ effectId: 'd2' }),
       condition: Conditions.targetIsYou(),
       alarmText: (_data, _matches, output) => output.cleanse!(),
       outputStrings: {