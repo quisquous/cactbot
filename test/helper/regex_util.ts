0e5d423348eec0ccc17b0bc46605cf3ad1bc46f8
Adrienne Walker <enne@quisquo.us>
2021-11-28 11:34:34 -0800

    test: convert regex_util to ts

--- test/helper/regex_util.ts
+++ test/helper/regex_util.ts
@@ -1,69 +1,74 @@
 import chai from 'chai';
 
+import { UnreachableCode } from '../../resources/not_reached';
+
 const { assert } = chai;
+
 // Quite bogus.
 const bogusLine = 'using act is cheating';
 
+export type RegexUtilParams = { [key: string]: string | boolean };
+
 // An automated way to test standard regex functions that take a dictionary of fields.
-export default (func, lines) => {
+export default (func: (params?: RegexUtilParams) => RegExp, lines: readonly string[]): void => {
   // regex should not match the bogus line.
-  assert.isNull(bogusLine.match(func({})));
+  assert.isNull(func({}).exec(bogusLine));
 
-  for (let i = 0; i < lines.length; ++i) {
-    const line = lines[i];
-
+  for (const line of lines) {
     // Undefined params (default capture).
-    const undefinedParamsMatch = line.match(func());
-    assert.isNotNull(undefinedParamsMatch, '' + func() + ' did not match ' + line);
+    const undefinedParamsMatch = func().exec(line);
+    assert.isNotNull(undefinedParamsMatch, `${func().toString()} did not match ${line}`);
     assert.notPropertyVal(undefinedParamsMatch, 'groups', undefined, func().source);
 
     // Empty params (default capture).
-    const emptyParamsMatch = line.match(func({}));
-    assert.isNotNull(emptyParamsMatch, '' + func({}) + ' did not match ' + line);
+    const emptyParamsMatch = func({}).exec(line);
+    assert.isNotNull(emptyParamsMatch, `${func({}).toString()} did not match ${line}`);
     assert.notPropertyVal(emptyParamsMatch, 'groups', undefined);
 
     // No capture match.
-    const noCaptureMatch = line.match(func({ capture: false }));
+    const noCaptureMatch = func({ capture: false }).exec(line);
     assert.isNotNull(noCaptureMatch);
     assert.propertyVal(noCaptureMatch, 'groups', undefined);
 
     // Capture match.
-    const captureMatch = line.match(func({ capture: true }));
+    const captureMatch = func({ capture: true }).exec(line);
     assert.isNotNull(captureMatch);
     assert.notPropertyVal(captureMatch, 'groups', undefined);
-    assert.isObject(captureMatch.groups);
+    const captureGroups = captureMatch?.groups;
+    assert.isObject(captureGroups);
+
+    if (typeof captureGroups !== 'object')
+      throw new UnreachableCode();
 
     // Capture always needs at least one thing.
-    const keys = Object.keys(captureMatch.groups);
+    const keys = Object.keys(captureGroups);
     assert.isAbove(keys.length, 0);
 
-    const explicitFields = {};
-    explicitFields.capture = true;
-    for (let j = 0; j < keys.length; ++j) {
-      const key = keys[j];
-
+    const explicitFields: RegexUtilParams = { capture: true };
+    for (const key of keys) {
       // Because matched values may have special regex
       // characters in it, escape these when specifying.
-      const value = captureMatch.groups[key];
+      const value = captureGroups[key];
       let escaped = value;
-      if (typeof value !== 'undefined')
+      if (escaped !== undefined) {
         escaped = escaped.replace(/[.*+?^${}()]/g, '\\$&');
-      explicitFields[key] = escaped;
+        explicitFields[key] = escaped;
+      }
     }
 
     // Specifying all the fields explicitly and capturing should
     // both match, and return the same thing.
     // This verifies that input parameters to the regex fields and
     // named matching groups are equivalent.
-    const explicitCaptureMatch = line.match(func(explicitFields));
+    const explicitCaptureMatch = func(explicitFields).exec(line);
     assert.isNotNull(explicitCaptureMatch);
     assert.notPropertyVal(explicitCaptureMatch, 'groups', undefined);
-    assert.isObject(explicitCaptureMatch.groups);
-    assert.deepEqual(explicitCaptureMatch.groups, captureMatch.groups);
+    assert.isObject(explicitCaptureMatch?.groups);
+    assert.deepEqual(explicitCaptureMatch?.groups, captureMatch?.groups);
 
     // Not capturing with explicit fields should also work.
     explicitFields.capture = false;
-    const explicitNoCaptureMatch = line.match(func(explicitFields));
+    const explicitNoCaptureMatch = func(explicitFields).exec(line);
     assert.isNotNull(explicitNoCaptureMatch);
     assert.propertyVal(explicitNoCaptureMatch, 'groups', undefined);
   }
