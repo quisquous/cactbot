30d9581fb9250d6599f475a4df4614f126bab5c6
Adrienne Walker <enne@quisquo.us>
2021-11-28 11:36:16 -0800

    test: convert test_raidboss_data to ts

--- test/helper/test_data_runner.ts
+++ test/helper/test_data_runner.ts
@@ -1,3 +1,5 @@
+import { TestMochaGlobal } from '../test_raidboss_data';
+
 import testManifestFiles from './test_manifest';
 import testTimelineFiles from './test_timeline';
 import testTriggerFiles from './test_trigger';
@@ -10,11 +12,7 @@ import testTriggerFiles from './test_trigger';
 // and runs them, this file will not be run.  Instead, test_raidboss_data.js
 // will call these test functions below itself.
 
-const annotatedGlobal: typeof global & {
-  triggerFiles?: string[];
-  manifestFiles?: string[];
-  timelineFiles?: string[];
-} = global;
+const annotatedGlobal: TestMochaGlobal = global;
 
 testTriggerFiles(annotatedGlobal.triggerFiles ?? []);
 testManifestFiles(annotatedGlobal.manifestFiles ?? []);
