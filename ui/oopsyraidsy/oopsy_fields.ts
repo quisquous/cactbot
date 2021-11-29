0f5b2e51c710c60e4961619b52eacd1a5a956ad1
Adrienne Walker <enne@quisquo.us>
2021-11-29 08:37:17 -0800

    util: convert gen_coverage_report to ts (#3699)

--- ui/oopsyraidsy/oopsy_fields.ts
+++ ui/oopsyraidsy/oopsy_fields.ts
@@ -1,5 +1,7 @@
+import { LooseOopsyTriggerSet } from '../../types/oopsy';
+
 // This is the set of fields that will create mistakes.
-export const oopsyTriggerSetOutputFields = [
+export const oopsyTriggerSetOutputFields: (keyof LooseOopsyTriggerSet)[] = [
   'damageWarn',
   'damageFail',
   'gainsEffectWarn',
@@ -13,7 +15,7 @@ export const oopsyTriggerSetOutputFields = [
 
 // TODO: use this for testing validity of oopsy trigger sets in the event
 // that we eventually get oopsy unit tests.  /o\
-export const oopsyTriggerSetFields = [
+export const oopsyTriggerSetFields: (keyof LooseOopsyTriggerSet)[] = [
   'zoneId',
   'zoneRegex',
   ...oopsyTriggerSetOutputFields,
