2b3727e84217458b3bd905100cce6e96082780c9
valarnin <valarnin@gmail.com>
2021-12-03 18:12:28 -0500

    Remove conditions on tankbsuters, change to ZoneId

--- ui/raidboss/data/06-ew/dungeon/the_tower_of_zot.ts
+++ ui/raidboss/data/06-ew/dungeon/the_tower_of_zot.ts
@@ -1,26 +1,24 @@
-import Conditions from '../../../../../resources/conditions';
 import NetRegexes from '../../../../../resources/netregexes';
 import { Responses } from '../../../../../resources/responses';
 import { RaidbossData } from '../../../../../types/data';
+import ZoneId from '../../../../../resources/zone_id';
 import { TriggerSet } from '../../../../../types/trigger';
 
 export type Data = RaidbossData;
 
 const triggerSet: TriggerSet<Data> = {
-  zoneId: 0x3B8, // @TODO: change to zoneid entry when available
+  zoneId: ZoneId.TheTowerOfZot,
   triggers: [
     {
       id: 'ToZ Cinduruva Isitva Siddhi',
       type: 'StartsUsing',
       netRegex: NetRegexes.startsUsing({ id: '62A9', source: 'Cinduruva', capture: true }),
-      condition: Conditions.caresAboutPhysical(),
       response: Responses.tankBuster(),
     },
     {
       id: 'ToZ Sanduruva Isitva Siddhi',
       type: 'StartsUsing',
       netRegex: NetRegexes.startsUsing({ id: '62C0', source: 'Sanduruva', capture: true }),
-      condition: Conditions.caresAboutPhysical(),
       response: Responses.tankBuster(),
     },
     // Trash mob tankbuster
@@ -28,7 +26,6 @@ const triggerSet: TriggerSet<Data> = {
       id: 'ToZ Wheel',
       type: 'StartsUsing',
       netRegex: NetRegexes.startsUsing({ id: '5E52', source: 'Zot Roader', capture: true }),
-      condition: Conditions.caresAboutPhysical(),
       response: Responses.tankBuster(),
     },
   ],
