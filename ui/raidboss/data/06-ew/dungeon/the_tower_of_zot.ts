f779541669c9a081ffb31caacb7972fd99b8eab6
Jonathan Garber <linkthevaliant@gmail.com>
2021-12-04 03:02:01 -0500

    Initial timeline

---
+++ ui/raidboss/data/06-ew/dungeon/the_tower_of_zot.ts
@@ -0,0 +1,12 @@
+import ZoneId from '../../../../../resources/zone_id';
+import { RaidbossData } from '../../../../../types/data';
+import { TriggerSet } from '../../../../../types/trigger';
+
+export type Data = RaidbossData;
+
+const triggerSet: TriggerSet<Data> = {
+  zoneId: ZoneId.TheTowerOfZot,
+  timelineFile: 'the_tower_of_zot.txt',
+};
+
+export default triggerSet;
