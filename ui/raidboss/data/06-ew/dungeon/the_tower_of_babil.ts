dc88a0460f40513da69c5aa473a0354f28d6be6c
Jonathan Garber <linkthevaliant@gmail.com>
2021-12-05 00:44:15 -0500

    Initial timeline

---
+++ ui/raidboss/data/06-ew/dungeon/the_tower_of_babil.ts
@@ -0,0 +1,12 @@
+import ZoneId from '../../../../../resources/zone_id';
+import { RaidbossData } from '../../../../../types/data';
+import { TriggerSet } from '../../../../../types/trigger';
+
+export type Data = RaidbossData;
+
+const triggerSet: TriggerSet<Data> = {
+  zoneId: ZoneId.TheTowerOfBabil,
+  timelineFile: 'the_tower_of_babil.txt',
+};
+
+export default triggerSet;
