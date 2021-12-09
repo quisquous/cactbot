quisquous/cactbot

commit 6240bfaf33734ffb96d7c8d7ade373440745aa44

jobs: full refactoring for job module (#3682)
A full refactoring for the jobs module, hope it makes easier in 6.0 jobs module renewing.

### Constructure

Class `Bars` was moved into `bars.ts`, and those code related to player is moved to a new class `Player`, code related to log parsing or events data from C# plugin is moved to a new class `JobsEventEmitter`.

Now the structure is like:

|file|class|feature|
|:-:|:-:|:-:|
|jobs.ts|none|entry of jobs module, initialize several classes|
|bars.ts|Bars|helds HTML DOM nodes, expose some API that can add or modify HTML nodes|
|event_emitter.ts|JobsEventEmitter|register overlay plugin listener and provides event subscription, do some pre-process on event data. Many of other classes subscribe event from it.|
|player.ts|Player|mentaining player data, this class is also an event emitter that expose some API to ease event subscribing. When player's status changed, the corresponding event emittes.|
|components/index.ts|ComponentManager|the entry class of jobs module, register events and invoke methods from `Bars` and other job components.|
|component/base.ts|BaseComponent|the father class of all jobs components, held instances of `Player` and `Bars` and so on, register events and provide methods for overriding.|


### Event Handling

I imported `eventemitter3` which acts just like the `event` module of NodeJS does. So that I can split out several blocks that do different things, would improve the readability a bit I think.

### Job Components

Now all "components" are classes that inherit `BaseComponent` in `ui/jobs/component/base.ts`, `BaseComponent` would register the most popular events and expose several `onXxx` methods that can be overridden by derived classes.
Job Components can also access `this.player`, `this.bars` or else for their exposed API or events.

### User Customisation (Incomplete)

I didn't find a proper way to provide user customisation.

ui/jobs/components/index.ts
@@ -1,66 +1,352 @@
-import { Bars } from '../jobs';
+import EffectId from '../../../resources/effect_id';
+import PartyTracker from '../../../resources/party';
+import Util from '../../../resources/util';
+import { Job } from '../../../types/job';
+import { Bars } from '../bars';
+import { BuffTracker } from '../buff_tracker';
+import { JobsEventEmitter } from '../event_emitter';
+import { JobsOptions } from '../jobs_options';
+import { Player } from '../player';
+import { doesJobNeedMPBar, isPvPZone, RegexesHolder } from '../utils';
 
-import { reset as resetAst, setup as setupAst } from './ast';
-import { reset as resetBlm, setup as setupBlm } from './blm';
-import { reset as resetBlu, setup as setupBlu } from './blu';
-import { reset as resetBrd, setup as setupBrd } from './brd';
-import { reset as resetDnc, setup as setupDnc } from './dnc';
-import { reset as resetDrg, setup as setupDrg } from './drg';
-import { reset as resetDrk, setup as setupDrk } from './drk';
-import { reset as resetGnb, setup as setupGnb } from './gnb';
-import { reset as resetMch, setup as setupMch } from './mch';
-import { reset as resetMnk, setup as setupMnk } from './mnk';
-import { reset as resetNin, setup as setupNin } from './nin';
-import { reset as resetPld, setup as setupPld } from './pld';
-import { reset as resetRdm, setup as setupRdm } from './rdm';
-import { reset as resetSam, setup as setupSam } from './sam';
-import { reset as resetSch, setup as setupSch } from './sch';
-import { reset as resetSmn, setup as setupSmn } from './smn';
-import { reset as resetWar, setup as setupWar } from './war';
-import { reset as resetWhm, setup as setupWhm } from './whm';
+import { ASTComponent } from './ast';
+import { BaseComponent, ComponentInterface, ShouldShow } from './base';
+import { BLMComponent } from './blm';
+import { BLUComponent } from './blu';
+import { BRDComponent } from './brd';
+import { DNCComponent } from './dnc';
+import { DRGComponent } from './drg';
+import { DRKComponent } from './drk';
+import { GNBComponent } from './gnb';
+import { MCHComponent } from './mch';
+import { MNKComponent } from './mnk';
+import { NINComponent } from './nin';
+import { PLDComponent } from './pld';
+import { RDMComponent } from './rdm';
+import { SAMComponent } from './sam';
+import { SCHComponent } from './sch';
+import { SMNComponent } from './smn';
+import { WARComponent } from './war';
+import { WHMComponent } from './whm';
 
-export const getSetup = (job: string): undefined | ((bars: Bars) => void) => {
-  return {
-    'PLD': setupPld,
-    'WAR': setupWar,
-    'DRK': setupDrk,
-    'GNB': setupGnb,
-    'WHM': setupWhm,
-    'SCH': setupSch,
-    'AST': setupAst,
-    'MNK': setupMnk,
-    'DRG': setupDrg,
-    'NIN': setupNin,
-    'SAM': setupSam,
-    'BRD': setupBrd,
-    'MCH': setupMch,
-    'DNC': setupDnc,
-    'BLM': setupBlm,
-    'SMN': setupSmn,
-    'RDM': setupRdm,
-    'BLU': setupBlu,
-  }[job.toUpperCase()];
+const ComponentMap: Record<Job, typeof BaseComponent> = {
+  // tank
+  GLA: PLDComponent,
+  PLD: PLDComponent,
+  MRD: WARComponent,
+  WAR: WARComponent,
+  DRK: DRKComponent,
+  GNB: GNBComponent,
+  // healer
+  CNJ: WHMComponent,
+  WHM: WHMComponent,
+  SCH: SCHComponent,
+  AST: ASTComponent,
+  // melee dps
+  PGL: MNKComponent,
+  MNK: MNKComponent,
+  LNC: BaseComponent,
+  DRG: DRGComponent,
+  ROG: NINComponent,
+  NIN: NINComponent,
+  SAM: SAMComponent,
+  // ranged dps
+  ARC: BRDComponent,
+  BRD: BRDComponent,
+  MCH: MCHComponent,
+  DNC: DNCComponent,
+  // magic dps
+  ACN: SMNComponent,
+  SMN: SMNComponent,
+  THM: BLMComponent,
+  BLM: BLMComponent,
+  RDM: RDMComponent,
+  BLU: BLUComponent,
+  // crafter & gatherer
+  CRP: BaseComponent,
+  BSM: BaseComponent,
+  ARM: BaseComponent,
+  GSM: BaseComponent,
+  LTW: BaseComponent,
+  WVR: BaseComponent,
+  ALC: BaseComponent,
+  CUL: BaseComponent,
+  MIN: BaseComponent,
+  BTN: BaseComponent,
+  FSH: BaseComponent,
+  NONE: BaseComponent,
 };
 
-export const getReset = (job: string): undefined | ((bars: Bars) => void) => {
-  return {
-    'PLD': resetPld,
-    'WAR': resetWar,
-    'DRK': resetDrk,
-    'GNB': resetGnb,
-    'WHM': resetWhm,
-    'SCH': resetSch,
-    'AST': resetAst,
-    'MNK': resetMnk,
-    'DRG': resetDrg,
-    'NIN': resetNin,
-    'SAM': resetSam,
-    'BRD': resetBrd,
-    'MCH': resetMch,
-    'DNC': resetDnc,
-    'BLM': resetBlm,
-    'SMN': resetSmn,
-    'RDM': resetRdm,
-    'BLU': resetBlu,
-  }[job.toUpperCase()];
-};
+export class ComponentManager {
+  bars: Bars;
+  buffTracker?: BuffTracker;
+  ee: JobsEventEmitter;
+  options: JobsOptions;
+  partyTracker: PartyTracker;
+  player: Player;
+  regexes?: RegexesHolder;
+  component?: BaseComponent;
+
+  // misc variables
+  shouldShow: ShouldShow;
+  contentType?: number;
+  // food buffs
+  foodBuffExpiresTimeMs: number;
+  foodBuffTimer: number;
+  // gp potions
+  gpAlarmReady: boolean;
+  gpPotion: boolean;
+  // true if player is too far away from their target
+  far?: boolean;
+
+  constructor(o: ComponentInterface) {
+    this.bars = o.bars;
+    this.ee = o.emitter;
+    this.options = o.options;
+    this.partyTracker = o.partyTracker;
+    this.player = o.player;
+
+    this.shouldShow = {};
+    this.contentType = undefined;
+
+    this.foodBuffExpiresTimeMs = 0;
+    this.foodBuffTimer = 0;
+    this.gpAlarmReady = false;
+    this.gpPotion = false;
+
+    this.far = undefined;
+
+    this.setupListeners();
+  }
+
+  getJobComponents(job: Job): BaseComponent {
+    const o = {
+      bars: this.bars,
+      emitter: this.ee,
+      options: this.options,
+      partyTracker: this.partyTracker,
+      player: this.player,
+    };
+    const Component = ComponentMap[job];
+    if (!Component)
+      return new BaseComponent(o);
+
+    return new Component(o);
+  }
+
+  setupListeners(): void {
+    this.ee.registerOverlayListeners();
+
+    // bind party changed event
+    this.ee.on('party', (party) => this.partyTracker.onPartyChanged({ party }));
+
+    this.player.on('level', (level, prevLevel) => {
+      if (level - prevLevel) {
+        this.bars._updateFoodBuff({
+          inCombat: this.component?.inCombat ?? false,
+          foodBuffExpiresTimeMs: this.foodBuffExpiresTimeMs,
+          foodBuffTimer: this.foodBuffTimer,
+          contentType: this.contentType,
+        });
+      }
+    });
+
+    // change color when target is far away
+    this.ee.on('battle/target', (target) => {
+      if (target && Util.isCasterDpsJob(this.player.job)) {
+        this.far = this.options.FarThresholdOffence >= 0 &&
+          target.effectiveDistance > this.options.FarThresholdOffence;
+        this.bars.updateMpBarColor({ mp: this.player.mp, far: this.far });
+      }
+    });
+
+    this.player.on('mp', (data) => {
+      let umbralStacks = 0;
+      if (this.component instanceof BLMComponent)
+        umbralStacks = this.component.umbralStacks;
+
+      // update mp ticker
+      this.bars._updateMPTicker({
+        ...data,
+        inCombat: this.component?.inCombat ?? false,
+        umbralStacks: umbralStacks,
+      });
+
+      // update mp bar color
+      this.bars.updateMpBarColor({ mp: data.mp, far: this.far });
+    });
+    this.player.on('gp', ({ gp }) => {
+      this.gpAlarmReady = this.bars._shouldPlayGpAlarm({
+        gp: gp,
+        gpAlarmReady: this.gpAlarmReady,
+        gpPotion: this.gpPotion,
+      });
+    });
+
+    this.player.on('job', (job) => {
+      if (!Util.isGatheringJob(this.player.job))
+        this.gpAlarmReady = false;
+
+      this.bars._setupJobContainers(job, {
+        buffList: this.shouldShow.buffList ?? true,
+        pullBar: this.shouldShow.pullBar ?? true,
+        hpBar: this.shouldShow.hpBar ?? (!Util.isCraftingJob(job) && !Util.isGatheringJob(job)),
+        mpBar: this.shouldShow.mpBar ??
+          (!Util.isCraftingJob(job) && !Util.isGatheringJob(job) && doesJobNeedMPBar(job)),
+        cpBar: this.shouldShow.cpBar ?? Util.isCraftingJob(job),
+        gpBar: this.shouldShow.gpBar ?? Util.isGatheringJob(job),
+        mpTicker: this.shouldShow.mpTicker ?? this.options.ShowMPTicker.includes(job),
+      });
+
+      // hide container html element if the player is a crafter
+      this.bars.setJobsContainerVisibility(!Util.isCraftingJob(job));
+
+      // initialize components
+      this.component = this.getJobComponents(job);
+
+      // add food buff trigger
+      this.player.onYouGainEffect((id, matches) => {
+        if (id === EffectId.WellFed) {
+          const seconds = parseFloat(matches.duration ?? '0');
+          const now = Date.now(); // This is in ms.
+          this.foodBuffExpiresTimeMs = now + (seconds * 1000);
+          this.bars._updateFoodBuff({
+            inCombat: this.component?.inCombat ?? false,
+            foodBuffExpiresTimeMs: this.foodBuffExpiresTimeMs,
+            foodBuffTimer: this.foodBuffTimer,
+            contentType: this.contentType,
+          });
+        }
+      });
+      // As you cannot change jobs in combat, we can assume that
+      // it is always false here.
+      this.bars._updateProcBoxNotifyState(false);
+
+      // TODO: this is always created by _updateJob, so maybe this.o needs be optional?
+      if (this.bars.o.leftBuffsList && this.bars.o.rightBuffsList) {
+        // Set up the buff tracker after the job bars are created.
+        this.buffTracker = new BuffTracker(
+          this.options,
+          this.player.name,
+          this.bars.o.leftBuffsList,
+          this.bars.o.rightBuffsList,
+          this.partyTracker,
+        );
+      }
+    });
+
+    // update RegexesHolder when the player name changes
+    this.player.on('player', ({ name }) => {
+      this.regexes = new RegexesHolder(this.options.ParserLanguage, name);
+    });
+
+    this.ee.on('battle/in-combat', ({ game }) => {
+      this.bars._updateProcBoxNotifyState(game);
+      if (this.component && this.component.inCombat !== game) {
+        this.bars._updateFoodBuff({
+          inCombat: this.component.inCombat,
+          foodBuffExpiresTimeMs: this.foodBuffExpiresTimeMs,
+          foodBuffTimer: this.foodBuffTimer,
+          contentType: this.contentType,
+        });
+      }
+
+      // make bars transparent when out of combat if requested
+      this.bars._updateOpacity(!game && this.options.LowerOpacityOutOfCombat);
+    });
+
+    this.ee.on('battle/wipe', () => {
+      this._onPartyWipe();
+    });
+
+    this.player.on('action/you', (id, matches) => {
+      if (this.regexes?.cordialRegex.test(id)) {
+        this.gpPotion = true;
+        window.setTimeout(() => {
+          this.gpPotion = false;
+        }, 2000);
+      }
+      this.buffTracker?.onUseAbility(id, matches);
+    });
+
+    this.player.on('action/other', (id, matches) => this.buffTracker?.onUseAbility(id, matches));
+
+    this.player.on(
+      'effect/gain/you',
+      (id, matches) => this.buffTracker?.onYouGainEffect(id, matches),
+    );
+
+    this.player.on('effect/gain', (id, matches) => {
+      // mob id starts with '4'
+      if (matches.targetId?.startsWith('4'))
+        this.buffTracker?.onMobGainsEffect(id, matches);
+    });
+
+    this.player.on(
+      'effect/lose/you',
+      (id, matches) => this.buffTracker?.onYouLoseEffect(id, matches),
+    );
+
+    this.player.on('effect/lose', (id, matches) => {
+      // mob id starts with '4'
+      if (matches.targetId?.startsWith('4'))
+        this.buffTracker?.onMobLosesEffect(id, matches);
+    });
+
+    this.ee.on('zone/change', (id, _name, info) => {
+      this.contentType = info?.contentType;
+
+      this.bars._updateFoodBuff({
+        inCombat: this.component?.inCombat ?? false,
+        foodBuffExpiresTimeMs: this.foodBuffExpiresTimeMs,
+        foodBuffTimer: this.foodBuffTimer,
+        contentType: this.contentType,
+      });
+
+      this.buffTracker?.clear();
+
+      // Hide UI except HP and MP bar if change to pvp area.
+      this.bars._updateUIVisibility(isPvPZone(id));
+    });
+
+    this.ee.on('log/game', (_log, _line, rawLine) => {
+      const m = this.regexes?.countdownStartRegex.exec(rawLine);
+      if (m && m.groups?.time) {
+        const seconds = parseFloat(m.groups.time);
+        this.bars._setPullCountdown(seconds);
+      }
+      if (this.regexes?.countdownCancelRegex.test(rawLine))
+        this.bars._setPullCountdown(0);
+      if (Util.isCraftingJob(this.player.job))
+        this._onCraftingLog(rawLine);
+    });
+  }
+
+  private _onPartyWipe(): void {
+    this.buffTracker?.clear();
+    // Reset job-specific ui
+    this.component?.reset();
+  }
+
+  private _onCraftingLog(message: string): void {
+    if (!this.regexes)
+      return;
+
+    // Hide CP Bar when not crafting
+    const anyRegexMatched = (line: string, array: RegExp[]) =>
+      array.some((regex) => regex.test(line));
+
+    // if the current player is crafting, show the bars;
+    // otherwise, hide them
+    if (anyRegexMatched(message, this.regexes.craftingStartRegexes))
+      this.bars.setJobsContainerVisibility(true);
+    if (
+      anyRegexMatched(message, this.regexes.craftingStopRegexes) ||
+      this.regexes.craftingFinishRegexes.some((regex) => {
+        const m = regex.exec(message)?.groups;
+        return m && (!m.player || m.player === this.player.name);
+      })
+    )
+      this.bars.setJobsContainerVisibility(false);
+  }
+}

resources/widget_list.ts
@@ -5,7 +5,7 @@ type Sorter = () => number;
 
 type LeftRight = 'left' | 'right';
 type UpDown = 'up' | 'down';
-type Toward = `${LeftRight} ${UpDown}` | `${UpDown} ${LeftRight}`;
+export type Toward = `${LeftRight} ${UpDown}` | `${UpDown} ${LeftRight}`;
 
 const getRandomInt = (max: number) => Math.floor(Math.random() * Math.floor(max));
 

ui/jobs/components/ast.ts
@@ -1,8 +1,10 @@
+import TimerBox from '../../../resources/timerbox';
 import { JobDetail } from '../../../types/event';
+import { ResourceBox } from '../bars';
 import { kAbility } from '../constants';
-import { Bars } from '../jobs';
 
-let resetFunc: (bars: Bars) => void;
+import { BaseComponent, ComponentInterface } from './base';
+
 const cardsMap = {
   'Balance': { 'bonus': 'melee', 'seal': 'Solar' },
   'Bole': { 'bonus': 'range', 'seal': 'Solar' },
@@ -12,38 +14,48 @@ const cardsMap = {
   'Spire': { 'bonus': 'range', 'seal': 'Celestial' },
 } as const;
 
-export const setup = (bars: Bars): void => {
-  const combustBox = bars.addProcBox({
-    id: 'ast-procs-combust',
-    fgColor: 'ast-color-combust',
-    notifyWhenExpired: true,
-  });
+export class ASTComponent extends BaseComponent {
+  combustBox: TimerBox;
+  drawBox: TimerBox;
+  lucidBox: TimerBox;
+  cardBox: ResourceBox;
+  sealBox: ResourceBox;
 
-  const drawBox = bars.addProcBox({
-    id: 'ast-procs-draw',
-    fgColor: 'ast-color-draw',
-  });
+  constructor(o: ComponentInterface) {
+    super(o);
 
-  const lucidBox = bars.addProcBox({
-    id: 'ast-procs-luciddreaming',
-    fgColor: 'ast-color-lucid',
-  });
+    this.combustBox = this.bars.addProcBox({
+      id: 'ast-procs-combust',
+      fgColor: 'ast-color-combust',
+      notifyWhenExpired: true,
+    });
 
-  const cardBox = bars.addResourceBox({
-    classList: ['ast-color-card'],
-  });
+    this.drawBox = this.bars.addProcBox({
+      id: 'ast-procs-draw',
+      fgColor: 'ast-color-draw',
+    });
 
-  const sealBox = bars.addResourceBox({
-    classList: ['ast-color-seal'],
-  });
+    this.lucidBox = this.bars.addProcBox({
+      id: 'ast-procs-luciddreaming',
+      fgColor: 'ast-color-lucid',
+    });
 
-  bars.onJobDetailUpdate('AST', (jobDetail: JobDetail['AST']) => {
+    this.cardBox = this.bars.addResourceBox({
+      classList: ['ast-color-card'],
+    });
+
+    this.sealBox = this.bars.addResourceBox({
+      classList: ['ast-color-seal'],
+    });
+  }
+
+  override onJobDetailUpdate(jobDetail: JobDetail['AST']): void {
     const card = jobDetail.heldCard;
     const seals = jobDetail.arcanums;
 
     // Show on which kind of jobs your card plays better by color
     // Blue on melee, purple on ranged, and grey when no card
-    const cardParent = cardBox.parentNode;
+    const cardParent = this.cardBox.parentNode;
     cardParent.classList.remove('melee', 'range');
     if (card in cardsMap)
       cardParent.classList.add(cardsMap[card].bonus);
@@ -52,53 +64,51 @@ export const setup = (bars: Bars): void => {
     // O means it's OK to play bars card
     // X means don't play bars card directly if time permits
     if (!cardsMap[card])
-      cardBox.innerText = '';
+      this.cardBox.innerText = '';
     else if (seals.includes(cardsMap[card].seal))
-      cardBox.innerText = 'X';
+      this.cardBox.innerText = 'X';
     else
-      cardBox.innerText = 'O';
+      this.cardBox.innerText = 'O';
 
     // Show how many kind of seals you already have
     // Turn green when you have all 3 kinds of seal
     const sealCount = new Set(seals).size;
-    sealBox.innerText = sealCount.toString();
+    this.sealBox.innerText = sealCount.toString();
     if (sealCount === 3)
-      sealBox.parentNode.classList.add('ready');
+      this.sealBox.parentNode.classList.add('ready');
     else
-      sealBox.parentNode.classList.remove('ready');
-  });
+      this.sealBox.parentNode.classList.remove('ready');
+  }
 
-  bars.onUseAbility([kAbility.Combust2, kAbility.Combust3], () => {
-    combustBox.duration = 30;
-  });
-  bars.onUseAbility(kAbility.Combust, () => {
-    combustBox.duration = 18;
-  });
+  override onUseAbility(id: string): void {
+    switch (id) {
+      case kAbility.Combust2:
+      case kAbility.Combust3:
+        this.combustBox.duration = 30;
+        break;
+      case kAbility.Combust:
+        this.combustBox.duration = 18;
+        break;
+      case kAbility.Draw:
+        this.drawBox.duration = 30;
+        break;
+      case kAbility.LucidDreaming:
+        this.lucidBox.duration = 60;
+        break;
+    }
+  }
+  override onStatChange({ gcdSpell }: { gcdSpell: number }): void {
+    this.combustBox.valuescale = gcdSpell;
+    this.combustBox.threshold = gcdSpell + 1;
+    this.drawBox.valuescale = gcdSpell;
+    this.drawBox.threshold = gcdSpell + 1;
+    this.lucidBox.valuescale = gcdSpell;
+    this.lucidBox.threshold = gcdSpell + 1;
+  }
 
-  bars.onUseAbility(kAbility.Draw, () => {
-    drawBox.duration = 30;
-  });
-  bars.onUseAbility(kAbility.LucidDreaming, () => {
-    lucidBox.duration = 60;
-  });
-
-  bars.onStatChange('AST', () => {
-    combustBox.valuescale = bars.gcdSpell;
-    combustBox.threshold = bars.gcdSpell + 1;
-    drawBox.valuescale = bars.gcdSpell;
-    drawBox.threshold = bars.gcdSpell + 1;
-    lucidBox.valuescale = bars.gcdSpell;
-    lucidBox.threshold = bars.gcdSpell + 1;
-  });
-
-  resetFunc = (_bars: Bars): void => {
-    combustBox.duration = 0;
-    drawBox.duration = 0;
-    lucidBox.duration = 0;
-  };
-};
-
-export const reset = (bars: Bars): void => {
-  if (resetFunc)
-    resetFunc(bars);
-};
+  override reset(): void {
+    this.combustBox.duration = 0;
+    this.drawBox.duration = 0;
+    this.lucidBox.duration = 0;
+  }
+}

ui/jobs/components/base.ts
@@ -1,0 +1,100 @@
+import PartyTracker from '../../../resources/party';
+import { JobDetail } from '../../../types/event';
+import { Bars } from '../bars';
+import ComboTracker from '../combo_tracker';
+import { JobsEventEmitter, PartialFieldMatches } from '../event_emitter';
+import { JobsOptions } from '../jobs_options';
+import { Player } from '../player';
+
+export type ShouldShow = {
+  buffList?: boolean;
+  pullBar?: boolean;
+  hpBar?: boolean;
+  mpBar?: boolean;
+  cpBar?: boolean;
+  gpBar?: boolean;
+  mpTicker?: boolean;
+};
+
+export interface ComponentInterface {
+  bars: Bars;
+  emitter: JobsEventEmitter;
+  options: JobsOptions;
+  partyTracker: PartyTracker;
+  player: Player;
+}
+
+export class BaseComponent implements ComponentInterface {
+  bars: Bars;
+  emitter: JobsEventEmitter;
+  options: JobsOptions;
+  partyTracker: PartyTracker;
+  player: Player;
+
+  inCombat: boolean;
+
+  constructor(o: ComponentInterface) {
+    this.bars = o.bars;
+    this.emitter = o.emitter;
+    this.options = o.options;
+    this.partyTracker = o.partyTracker;
+    this.player = o.player;
+
+    this.inCombat = false;
+
+    this._bindListeners();
+  }
+
+  private _bindListeners(): void {
+    this.emitter.on('battle/in-combat', ({ game }) => {
+      this.inCombat = game;
+    });
+    this.player.onYouGainEffect(this.onYouGainEffect.bind(this));
+    this.player.onYouLoseEffect(this.onYouLoseEffect.bind(this));
+    this.player.onMobGainsEffectFromYou(this.onMobGainsEffectFromYou.bind(this));
+    this.player.onMobLosesEffectFromYou(this.onMobLosesEffectFromYou.bind(this));
+    this.player.onUseAbility(this.onUseAbility.bind(this));
+    this.player.onStatChange(this.player.job, this.onStatChange.bind(this));
+    if (this.player.job !== 'NONE') {
+      const job = this.player.job as keyof JobDetail;
+      this.player.onJobDetailUpdate(job, this.onJobDetailUpdate.bind(this));
+    }
+    this.player.onCombo(this.onCombo.bind(this));
+  }
+
+  onCombo(_id: string | undefined, _combo: ComboTracker): void {
+    /** noop */
+  }
+
+  onYouGainEffect(_id: string, _effect: PartialFieldMatches<'GainsEffect'>): void {
+    /** noop */
+  }
+
+  onYouLoseEffect(_id: string, _effect: PartialFieldMatches<'LosesEffect'>): void {
+    /** noop */
+  }
+
+  onMobGainsEffectFromYou(_id: string, _effect: PartialFieldMatches<'GainsEffect'>): void {
+    /** noop */
+  }
+
+  onMobLosesEffectFromYou(_id: string, _effect: PartialFieldMatches<'LosesEffect'>): void {
+    /** noop */
+  }
+
+  onUseAbility(_id: string, _ability: PartialFieldMatches<'Ability'>): void {
+    /** noop */
+  }
+
+  onStatChange(_gcd: { gcdSkill: number; gcdSpell: number }): void {
+    /** noop */
+  }
+
+  onJobDetailUpdate(_jobDetail: JobDetail[keyof JobDetail]): void {
+    /** noop */
+  }
+
+  reset(): void {
+    /** noop */
+  }
+}

commit f89254cc0d127faf94c1f5302fbd31f2cec8fa80

jobs: fix gp alarm from constantly beeping
There are several fixes here:
* gp alarm should only beep once after it has gone below the threshold
* if the threshold is zero, don't beep at all
* if we go above the threshold (with a potion), don't beep
  (this may have been an issue before?)

This is a fix for a bug introduced in #3682.

ui/jobs/bars.ts
@@ -677,17 +677,10 @@ export class Bars {
     this.o.gpBar.maxvalue = data.maxGp.toString();
   }
 
-  _shouldPlayGpAlarm(data: { gp: number; gpAlarmReady: boolean; gpPotion: boolean }): boolean {
-    // GP Alarm
-    if (data.gp < this.options.GpAlarmPoint) {
-      return true;
-    } else if (data.gpAlarmReady && !data.gpPotion && data.gp >= this.options.GpAlarmPoint) {
-      const audio = new Audio('../../resources/sounds/freesound/power_up.webm');
-      audio.volume = this.options.GpAlarmSoundVolume;
-      void audio.play();
-      return false;
-    }
-    return true;
+  _playGpAlarm(): void {
+    const audio = new Audio('../../resources/sounds/freesound/power_up.webm');
+    audio.volume = this.options.GpAlarmSoundVolume;
+    void audio.play();
   }
 
   _updateOpacity(transparent: boolean): void {

ui/jobs/components/index.ts
@@ -187,16 +187,22 @@ export class ComponentManager {
       this.bars.updateMpBarColor({ mp: data.mp, far: this.far });
     });
     this.player.on('gp', ({ gp }) => {
-      this.gpAlarmReady = this.bars._shouldPlayGpAlarm({
-        gp: gp,
-        gpAlarmReady: this.gpAlarmReady,
-        gpPotion: this.gpPotion,
-      });
+      if (!Util.isGatheringJob(this.player.job))
+        return;
+      if (gp < this.options.GpAlarmPoint) {
+        this.gpAlarmReady = true;
+      } else if (this.gpAlarmReady && !this.gpPotion && gp >= this.options.GpAlarmPoint) {
+        this.gpAlarmReady = false;
+        this.bars._playGpAlarm();
+      } else {
+        // We're above the gp point and it has either played or been suppressed by a potion.
+        // Wait until we dip below the alarm point before beeping again.
+        this.gpAlarmReady = false;
+      }
     });
 
     this.player.on('job', (job) => {
-      if (!Util.isGatheringJob(this.player.job))
-        this.gpAlarmReady = false;
+      this.gpAlarmReady = false;
 
       this.bars._setupJobContainers(job, {
         buffList: this.shouldShow.buffList ?? true,