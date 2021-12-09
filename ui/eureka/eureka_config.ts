88485871285ecdbc091b00745960b2ac512dec66
VanSlanzar <61565834+VanSlanzar@users.noreply.github.com>
2021-12-09 20:42:52 +0100

    added and corrections translations Fr

--- ui/eureka/eureka_config.ts
+++ ui/eureka/eureka_config.ts
@@ -44,8 +44,7 @@ UserConfig.registerOptions('eureka', {
       name: {
         en: 'Prefer complete names for Skirmishes/Critical Engagements',
         de: 'Bevorzuge komplette Namen für Scharmützel/Kritische Gefechte',
-        fr:
-          'Préférer les noms complet pour les escarmouches/Affrontements Cruciaux dans Bozja/Zadnor',
+        fr: 'Préférer les noms complet pour les escarmouches/affrontements cruciaux dans Bozja/Zadnor',
         ja: 'スカーミッシュ/CEにフールネームを表示する',
         cn: '显示冲突战/紧急遭遇战全名',
         ko: '돌발 임무/긴급 교전 줄임말 쓰지 않기 (한국어 버전은 차이 없음)',
@@ -58,7 +57,7 @@ UserConfig.registerOptions('eureka', {
       name: {
         en: 'Add information about Field Notes',
         de: 'Füge Informationen über Frontberichte hinzu',
-        fr: 'Ajouter les informations relatives aux Rapports du Front Bozjien dans Bozja/Zadnor',
+        fr: 'Ajouter les informations relatives aux rapports du Front Bozjien dans Bozja/Zadnor',
         ja: 'フィールドノートに戦果記録情報を追加する',
         cn: '显示可能掉落的战果记录',
         ko: '전과기록 정보 보여주기',
@@ -197,6 +196,7 @@ UserConfig.registerOptions('eureka', {
       id: 'showBAPortalTimer',
       name: {
         en: 'Show Timer for BA red/blue Portals',
+        fr: 'Afficher le timer pour les portails rouges/bleus de BA',
       },
       type: 'checkbox',
       default: false,
