64fb1fffb3bed49d9a591f432ac493de3bb74303
Adrienne Walker <enne@quisquo.us>
2021-12-10 16:23:45 -0800

    add comment

--- ui/jobs/jobs.ts
+++ ui/jobs/jobs.ts
@@ -17,6 +17,11 @@ UserConfig.getUserConfigLocation('jobs', defaultOptions, () => {
   const player = new Player(emitter);
   const partyTracker = new PartyTracker();
   const bars = new Bars(options, { emitter, player });
+
+  // Because Chinese/Korean regions are still on older version of FF14,
+  // set this value to whether or not we should treat this as 5.x or 6.x.
+  // This affects things like entire jobs (smn) or combo durations.
   const is5x = ['cn', 'ko'].includes(options.ParserLanguage);
+
   new ComponentManager({ bars, emitter, options, partyTracker, player, is5x });
 });
