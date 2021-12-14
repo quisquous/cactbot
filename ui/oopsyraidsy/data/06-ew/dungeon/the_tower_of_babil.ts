5814423cc06b0d777d6f9ec8fdc1ee3ba1a48db7
Adrienne Walker <enne@quisquo.us>
2021-12-14 09:35:15 -0800

    oopsy: small fixes for Babil/Zot (#3840)

--- ui/oopsyraidsy/data/06-ew/dungeon/the_tower_of_babil.ts
+++ ui/oopsyraidsy/data/06-ew/dungeon/the_tower_of_babil.ts
@@ -14,21 +14,26 @@ const triggerSet: OopsyTriggerSet<Data> = {
     'Babil Satellite Incendiary Circle': '6DE3', // Large donut AoE, before first boss
     'Babil Reaper Magitek Cannon': '6DE4', // AOE circle, before first boss
 
+    'Babil Barnabas Ground and Pound': '62EA', // Telegraphed line knockback, boss 1
+    'Babil Barnabas Dynamic Scrapline': '62F0', // Inner circle during Dynamic Scrapline, boss 1
+    'Babil Barnabas Dynamic Pound': '62EE', // Line during Dynamic Pound, boss 1
     'Babil Barnabas Electromagnetic Release 1': '62F1', // Center AoE circle, boss 1
     'Babil Barnabas Electromagnetic Release 2': '62EF', // Center magnetic line, boss 1
     'Babil Barnabas Rolling Scrapline': '62EB', // Center AoE circle, no magnets, boss 1
     'Babil Thunderball Shock': '62F2', // Cardinal/intercardinal AoE circles, boss 1
 
+    'Babil Sky Armor Aethershot': '6DE7', // Circle AoE, after boss 1
     'Babil Rearguard Cermet Pile': '6DE8', // Line AoE, before boss 2
     'Babil Colossus Grand Sword': '6DE9', // Frontal cone, before boss 2
-    'Babil Avenger Shoulder Cannon': '6DEA', // AoE circle, before boss 2
+    'Babil Avenger Shoulder Cannon': '6DEA', // Targeted AoE circle, before boss 2
+    'Babil Death Machine Magitek Ray': '6DEB', // Long line AoE, before boss 2
 
     'Babil Magitek Chakram Mighty Blow': '62F4', // Arena AoE, if not mini, boss 2
     'Babil Lugae Surface Missile': '62F7', // Targeted AoE circles from Magitek Missile, boss 2
     'Babil Magitek Explosive Explosion': '62F9', // Bomberman line AoE, boss 2
     'Babil Lugae Magitek Ray': '62FC', // Frontal line AoE, boss 2
 
-    'Babil Zot Sky Armor Garlean Fire': '5E4A', // Circle AoE, after boss 2
+    'Babil Gunship Garlean Fire': '6DEE', // Circle AoE, after boss 2
     'Babil Armored Weapon Diffractive Laser': '5E53', // Circle AoE, after boss 2
     'Babil Magitek Crane Crane Game': '6C35', // Large environmental circle AoE, after boss 2
     'Babil Hexadrone 2-Tonze Magitek Missile': '6DEC', // Circle AoE, after boss 2
