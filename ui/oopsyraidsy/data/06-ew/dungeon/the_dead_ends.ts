c6d8e22d3fb5eb35d8205ea14b49b971e9701297
Adrienne Walker <enne@quisquo.us>
2021-12-21 12:10:21 -0800

    updates from testing

--- ui/oopsyraidsy/data/06-ew/dungeon/the_dead_ends.ts
+++ ui/oopsyraidsy/data/06-ew/dungeon/the_dead_ends.ts
@@ -3,12 +3,8 @@ import ZoneId from '../../../../../resources/zone_id';
 import { OopsyData } from '../../../../../types/data';
 import { OopsyTriggerSet } from '../../../../../types/oopsy';
 
-// TODO: Grebuloff:  Necrosis??
-// TODO: Grebuloff:  getting hit by dorito stack
-// TODO: Grebuloff:  being in outside edge
-// TODO: Peacekeeper:  being in outside edge
-// TODO: Peacekeeper: standing in Electromagnetic Repelleant
-// TODO: Ra-la: does Prance hurt?
+// Note: Ignoring Toxicosis (BB8) standing in oil puddles in section 1 as it is just flavor.
+// Note: Ignoring Necrosis (B95) as it's covered by Wave of Nausea and Necrotic Fluid.
 
 export interface Data extends OopsyData {
   hasDoom?: { [name: string]: boolean };
@@ -19,10 +15,8 @@ const triggerSet: OopsyTriggerSet<Data> = {
   damageWarn: {
     'DeadEnds Warped Flesh Amorphic Flail': '6E91', // centered circle (pudding)
     'DeadEnds Warped Flesh Terminal Bloom': '6FFF', // targeted circle (pudding)
-    'DeadEnds Warped Flesh Plague Fang': '6E92', // targeted circle (tall)
-    'DeadEnds Grebuloff Necrotic Fluid': '653F', // exaflare orbs
+    'DeadEnds Warped Flesh Plague Fang': '6E92', // targeted circle (tall
     'DeadEnds Grebuloff Cough Up': '653E', // 3x targeted circles
-    'DeadEnds Grebuloff Wave of Nausea': '6EBB', // donut
     'DeadEnds Ambient No Future': '6CC9', // ambient red reticule (before)
     'DeadEnds Airborne Freedom Fighter Air Blast': '6E72', // line aoe
     'DeadEnds Landed Freedom Fighter Photon Burst': '6E71', // targeted circle
@@ -32,16 +26,22 @@ const triggerSet: OopsyTriggerSet<Data> = {
     'DeadEnds Peacekeeper No Future': '6547', // red reticule (during boss)
     'DeadEnds Xenofauna Relevation': '6C47', // targeted black/yellow circle
     'DeadEnds Xenoflora Creeping Hush': '6C45', // frontal blue cleave
-    'DeadEnds Ra-la Prance 1': '6551', // Ra-la initial jump
-    'DeadEnds Ra-la Prance 2': '6552', // Ra-la jumps
     'DeadEnds Ra-la Lamellar Light': '6553', // expanding circles from Prance
     'DeadEnds Ra-la Lifesbreath': '6554', // line aoe
     'DeadEnds Ra-la Loving Embrace 1': '6557', // left/right cleave
     'DeadEnds Ra-la Loving Embrace 2': '6558', // left/right cleave
   },
   damageFail: {
+    // All give debuffs that require healer attention, so is a worse mistake.
+    'DeadEnds Grebuloff Necrotic Fluid': '653F', // exaflare orbs
+    'DeadEnds Grebuloff Wave of Nausea': '6EBB', // donut
     'DeadEnds Ra-la Golden Wings Lamellar Light 1': '6556', // Doom-giving Lifesbreath butterfly lines
     'DeadEnds Ra-la Golden Wings Lamellar Light 2': '655F', // Doom-giving Lifesbreath butterfly lines
+  },
+  gainsEffectWarn: {
+    'DeadEnds Grebuloff Hysteria': '128', // failing dorito stack
+    'DeadEnds Peacekeeper Burns': '892', // standing or getting knocked into outside edge
+    'DeadEnds Peacekeeper Electrocution': '76B', // standing under Peacekeeper after Electromagnetic Repellant
   },
   shareWarn: {
     'DeadEnds Grebuloff Befoulment': '6544', // spread
@@ -50,7 +50,7 @@ const triggerSet: OopsyTriggerSet<Data> = {
     'DeadEnds Ra-la Still Embrace': '655C', // spread
   },
   shareFail: {
-    'DeadEnds Peacekeeper Elimination': '654F', // tankbuster cleave
+    'DeadEnds Peacekeeper Elimination': '654F', // tankbuster laser cleave
   },
   triggers: [
     {
