b7c7be15d83082c295e7adf8b47f79431302602e
amki <amki@amki.eu>
2021-12-22 15:59:43 +0100

    Fix Spoken Cataract for real, one bug remains with swallowed callouts somehow

--- ui/raidboss/data/06-ew/raid/p2n.ts
+++ ui/raidboss/data/06-ew/raid/p2n.ts
@@ -1,12 +1,14 @@
 // import Conditions from '../../../../../resources/conditions';
 import NetRegexes from '../../../../../resources/netregexes';
+import { callOverlayHandler } from '../../../../../resources/overlay_plugin_api';
 import { Responses } from '../../../../../resources/responses';
 import ZoneId from '../../../../../resources/zone_id';
 import { RaidbossData } from '../../../../../types/data';
+import { PluginCombatantState } from '../../../../../types/event';
 import { TriggerSet } from '../../../../../types/trigger';
 
 export interface Data extends RaidbossData {
-  bodyHeading?: string;
+  bodyActor?: PluginCombatantState;
 }
 
 const triggerSet: TriggerSet<Data> = {
@@ -30,46 +32,69 @@ const triggerSet: TriggerSet<Data> = {
       id: 'P2N Spoken Cataract',
       type: 'StartsUsing',
       netRegex: NetRegexes.startsUsing({ id: ['67FD', '67F8', '67F7', '67F9'], source: 'Hippokampos', capture: true }),
-      alertText: (data, matches, _output) => {
-        const deg = parseFloat(matches.heading) * 180 / Math.PI;
-        if (matches.id === '67FD') {
-          if (deg < -150 || deg > 150)
-            data.bodyHeading = 'north';
-          if (deg < -60 && deg > -120)
-            data.bodyHeading = 'west';
-          if (deg > -30 && deg < 30)
-            data.bodyHeading = 'south';
-          if (deg > 60 && deg < 120)
-            data.bodyHeading = 'east';
+      delaySeconds: 1,
+      promise: async (data) => {
+        const callData = await callOverlayHandler({
+          call: 'getCombatants',
+        });
+        if (!callData || !callData.combatants || !callData.combatants.length) {
+          console.error('SpokenCataract: failed to get combatants: ${JSON.stringify(callData)}');
+          return;
         }
+        // This is the real hippo, according to hp.
+        const hippos = callData.combatants.filter((c) => c.BNpcID === 13721);
+        if (hippos.length !== 1) {
+          console.error('SpokenCataract: There are more than one Hippo?!?: ${JSON.stringify(hippos)}');
+          return;
+        }
+        data.bodyActor = hippos[0];
+      },
+      alertText: (data, matches, _output) => {
+        // return 'body' + data.bodyActor?.Heading;
+        if (!data.bodyActor?.Heading) {
+          console.error('SpokenCataract: No boss actor found. Did the promise fail?');
+          return;
+        }
+        const heading = data.bodyActor?.Heading;
+        let bodyHeading = null;
+
+        if (heading < -3.0 || heading > 3.0)
+          bodyHeading = 'north';
+        if (heading < -1.56 && heading > -1.58)
+          bodyHeading = 'west';
+        if (heading > -0.1 && heading < 0.1)
+          bodyHeading = 'south';
+        if (heading > 1.56 && heading < 1.58)
+          bodyHeading = 'east';
+
         if (matches.id === '67F8') {
-          if (data.bodyHeading === 'north')
+          if (bodyHeading === 'north')
             return 'body north, head south';
-          if (data.bodyHeading === 'west')
+          if (bodyHeading === 'west')
             return 'body west, head east';
-          if (data.bodyHeading === 'south')
+          if (bodyHeading === 'south')
             return 'body south, head north';
-          if (data.bodyHeading === 'east')
+          if (bodyHeading === 'east')
             return 'body east, head west';
         }
         if (matches.id === '67F7') {
-          if (data.bodyHeading === 'north')
+          if (bodyHeading === 'north')
             return 'body north, head east';
-          if (data.bodyHeading === 'west')
+          if (bodyHeading === 'west')
             return 'body west, head north';
-          if (data.bodyHeading === 'south')
+          if (bodyHeading === 'south')
             return 'body south, head west';
-          if (data.bodyHeading === 'east')
+          if (bodyHeading === 'east')
             return 'body east, head south';
         }
         if (matches.id === '67F9') {
-          if (data.bodyHeading === 'north')
+          if (bodyHeading === 'north')
             return 'body north, head west';
-          if (data.bodyHeading === 'west')
+          if (bodyHeading === 'west')
             return 'body west, head south';
-          if (data.bodyHeading === 'south')
+          if (bodyHeading === 'south')
             return 'body south, head east';
-          if (data.bodyHeading === 'east')
+          if (bodyHeading === 'east')
             return 'body east, head north';
         }
       },
