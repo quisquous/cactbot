aadf95d4f2c63effb2ef2eb5ac70450735e38ec5
Adrienne Walker <enne@quisquo.us>
2021-12-24 13:42:28 -0800

    raidboss: add A rank EW hunt triggers

---
+++ ui/raidboss/data/06-ew/hunts/thavnair.ts
@@ -0,0 +1,104 @@
+import Conditions from '../../../../../resources/conditions';
+import NetRegexes from '../../../../../resources/netregexes';
+import { Responses } from '../../../../../resources/responses';
+import ZoneId from '../../../../../resources/zone_id';
+import { RaidbossData } from '../../../../../types/data';
+import { TriggerSet } from '../../../../../types/trigger';
+
+// TODO: Sugriva Butcher (?) tankbuster (cleave?)
+// TOOD: Yilan Brackish Rain (?) frontal untelegraphed(?) cleave
+
+export type Data = RaidbossData;
+
+const triggerSet: TriggerSet<Data> = {
+  zoneId: ZoneId.Thavnair,
+  triggers: [
+    {
+      id: 'Hunt Sugriva Spark',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: '6A55', source: 'Sugriva', capture: false }),
+      // Is this "in" or "under"?
+      response: Responses.getUnder('alert'),
+    },
+    {
+      id: 'Hunt Sugriva Scythe Tail',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: '6A56', source: 'Sugriva', capture: false }),
+      response: Responses.getOut(),
+    },
+    {
+      id: 'Hunt Sugriva Twister',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: '6A53', source: 'Sugriva', capture: false }),
+      infoText: (_data, _matches, output) => output.knockbackStack!(),
+      outputStrings: {
+        knockbackStack: {
+          en: 'Knockback Stack',
+        },
+      },
+    },
+    {
+      id: 'Hunt Sugriva Crosswind',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: '6A5B', source: 'Sugriva', capture: false }),
+      response: Responses.aoe(),
+    },
+    {
+      id: 'Hunt Yilan Forward March',
+      type: 'GainsEffect',
+      netRegex: NetRegexes.gainsEffect({ effectId: '7A6', source: 'Sugriva' }),
+      condition: Conditions.targetIsYou(),
+      // t=0.0 gain effect (this line)
+      // t=6.3 Mini Light starts casting
+      // t=9.0 lose effect (forced march)
+      // t=12.3 Mini Light ability
+      // Full duration is 9s, but have seen this apply late for ~7 to some people.
+      delaySeconds: (_data, matches) => parseFloat(matches.duration) - 4,
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Forward March Away',
+        },
+      },
+    },
+    {
+      id: 'Hunt Yilan About Face',
+      type: 'GainsEffect',
+      netRegex: NetRegexes.gainsEffect({ effectId: '7A7', source: 'Sugriva' }),
+      condition: Conditions.targetIsYou(),
+      delaySeconds: (_data, matches) => parseFloat(matches.duration) - 4,
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Backwards March Away',
+        },
+      },
+    },
+    {
+      id: 'Hunt Yilan Left Face',
+      type: 'GainsEffect',
+      netRegex: NetRegexes.gainsEffect({ effectId: '7A8', source: 'Sugriva' }),
+      condition: Conditions.targetIsYou(),
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Left March Away',
+        },
+      },
+    },
+    {
+      id: 'Hunt Yilan Right Face',
+      type: 'GainsEffect',
+      netRegex: NetRegexes.gainsEffect({ effectId: '7A9', source: 'Sugriva' }),
+      condition: Conditions.targetIsYou(),
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Right March Away',
+        },
+      },
+    },
+  ],
+};
+
+export default triggerSet;
