b94fdc90a522ea50d44ef2930a73a8603b6fbf33
valarnin <valarnin@gmail.com>
2022-01-04 18:30:53 -0500

    raidboss: Add basic P1S timeline and triggers

---
+++ ui/raidboss/data/06-ew/raid/p1s.ts
@@ -0,0 +1,204 @@
+import Conditions from '../../../../../resources/conditions';
+import NetRegexes from '../../../../../resources/netregexes';
+import { Responses } from '../../../../../resources/responses';
+//import ZoneId from '../../../../../resources/zone_id';
+import { RaidbossData } from '../../../../../types/data';
+import { TriggerSet } from '../../../../../types/trigger';
+
+export type Data = RaidbossData;
+
+const triggerSet: TriggerSet<Data> = {
+  zoneId: 0x3EB,
+  timelineFile: 'p1s.txt',
+  triggers: [
+    {
+      id: 'P1S Warder\'s Wrath',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: '662A', source: 'Erichthonios', capture: false }),
+      netRegexDe: NetRegexes.startsUsing({ id: '662A', source: 'Erichthonios', capture: false }),
+      netRegexFr: NetRegexes.startsUsing({ id: '662A', source: 'Érichthonios', capture: false }),
+      netRegexJa: NetRegexes.startsUsing({ id: '662A', source: 'エリクトニオス', capture: false }),
+      response: Responses.aoe(),
+    },
+    {
+      id: 'P1S Shining Cells',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: '6616', source: 'Erichthonios', capture: false }),
+      netRegexDe: NetRegexes.startsUsing({ id: '6616', source: 'Erichthonios', capture: false }),
+      netRegexFr: NetRegexes.startsUsing({ id: '6616', source: 'Érichthonios', capture: false }),
+      netRegexJa: NetRegexes.startsUsing({ id: '6616', source: 'エリクトニオス', capture: false }),
+      response: Responses.aoe(),
+    },
+    {
+      id: 'P1S Slam Shut',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: '6617', source: 'Erichthonios', capture: false }),
+      netRegexDe: NetRegexes.startsUsing({ id: '6617', source: 'Erichthonios', capture: false }),
+      netRegexFr: NetRegexes.startsUsing({ id: '6617', source: 'Érichthonios', capture: false }),
+      netRegexJa: NetRegexes.startsUsing({ id: '6617', source: 'エリクトニオス', capture: false }),
+      response: Responses.aoe(),
+    },
+    {
+      id: 'P1S Gaoler\'s Flail Right => Left',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: '65F6', source: 'Erichthonios', capture: false }),
+      netRegexDe: NetRegexes.startsUsing({ id: '65F6', source: 'Erichthonios', capture: false }),
+      netRegexFr: NetRegexes.startsUsing({ id: '65F6', source: 'Érichthonios', capture: false }),
+      netRegexJa: NetRegexes.startsUsing({ id: '65F6', source: 'エリクトニオス', capture: false }),
+      alertText: (_data, _matches, output) => output.directions!(),
+      outputStrings: {
+        directions: {
+          en: 'Right => Left',
+        },
+      },
+    },
+    {
+      id: 'P1S Gaoler\'s Flail Left => Right',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: '65F7', source: 'Erichthonios', capture: false }),
+      netRegexDe: NetRegexes.startsUsing({ id: '65F7', source: 'Erichthonios', capture: false }),
+      netRegexFr: NetRegexes.startsUsing({ id: '65F7', source: 'Érichthonios', capture: false }),
+      netRegexJa: NetRegexes.startsUsing({ id: '65F7', source: 'エリクトニオス', capture: false }),
+      alertText: (_data, _matches, output) => output.directions!(),
+      outputStrings: {
+        directions: {
+          en: 'Left => Right',
+        },
+      },
+    },
+    {
+      id: 'P1S Gaoler\'s Flail Out => In',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: '65F8', source: 'Erichthonios', capture: false }),
+      netRegexDe: NetRegexes.startsUsing({ id: '65F8', source: 'Erichthonios', capture: false }),
+      netRegexFr: NetRegexes.startsUsing({ id: '65F8', source: 'Érichthonios', capture: false }),
+      netRegexJa: NetRegexes.startsUsing({ id: '65F8', source: 'エリクトニオス', capture: false }),
+      alertText: (_data, _matches, output) => output.directions!(),
+      outputStrings: {
+        directions: {
+          en: 'Out => In',
+        },
+      },
+    },
+    {
+      id: 'P1S Gaoler\'s Flail In => Out',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: '65F9', source: 'Erichthonios', capture: false }),
+      netRegexDe: NetRegexes.startsUsing({ id: '65F9', source: 'Erichthonios', capture: false }),
+      netRegexFr: NetRegexes.startsUsing({ id: '65F9', source: 'Érichthonios', capture: false }),
+      netRegexJa: NetRegexes.startsUsing({ id: '65F9', source: 'エリクトニオス', capture: false }),
+      alertText: (_data, _matches, output) => output.directions!(),
+      outputStrings: {
+        directions: {
+          en: 'In => Out',
+        },
+      },
+    },
+    {
+      id: 'P1S Heavy Hand',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: '6629', source: 'Erichthonios' }),
+      netRegexDe: NetRegexes.startsUsing({ id: '6629', source: 'Erichthonios' }),
+      netRegexFr: NetRegexes.startsUsing({ id: '6629', source: 'Érichthonios' }),
+      netRegexJa: NetRegexes.startsUsing({ id: '6629', source: 'エリクトニオス' }),
+      condition: Conditions.caresAboutPhysical(),
+      response: Responses.tankBuster(),
+    },
+    {
+      id: 'P1S Pitiless Flail of Grace',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: '660E', source: 'Erichthonios' }),
+      netRegexDe: NetRegexes.startsUsing({ id: '660E', source: 'Erichthonios' }),
+      netRegexFr: NetRegexes.startsUsing({ id: '660E', source: 'Érichthonios' }),
+      netRegexJa: NetRegexes.startsUsing({ id: '660E', source: 'エリクトニオス' }),
+      alertText: (_data, _matches, output) => output.directions!(),
+      outputStrings: {
+        directions: {
+          en: 'Tankbuster+Knockback => Stack',
+        },
+      },
+    },
+    {
+      id: 'P1S Pitiless Flail of Purgation',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: '660E', source: 'Erichthonios' }),
+      netRegexDe: NetRegexes.startsUsing({ id: '660E', source: 'Erichthonios' }),
+      netRegexFr: NetRegexes.startsUsing({ id: '660E', source: 'Érichthonios' }),
+      netRegexJa: NetRegexes.startsUsing({ id: '660E', source: 'エリクトニオス' }),
+      alertText: (_data, _matches, output) => output.directions!(),
+      outputStrings: {
+        directions: {
+          en: 'Tankbuster+Knockback => Flare',
+        },
+      },
+    },
+    // Copy/paste from normal, seems to be the same
+    {
+      id: 'P1S Hot/Cold Spell',
+      type: 'GainsEffect',
+      netRegex: NetRegexes.gainsEffect({ effectId: ['AB3', 'AB4'], capture: true }),
+      condition: Conditions.targetIsYou(),
+      alertText: (_data, _matches, _output) => {
+        return _matches.effectId === 'AB3' ? _output.red!() : _output.blue!();
+      },
+      outputStrings: {
+        red: {
+          en: 'Get hit by red',
+          de: 'Von Rot treffen lassen',
+          fr: 'Faites-vous toucher par le rouge',
+          cn: '去吃火',
+          ko: '빨간색 맞기',
+        },
+        blue: {
+          en: 'Get hit by blue',
+          de: 'Von Blau treffen lassen',
+          fr: 'Faites-vous toucher par le bleu',
+          cn: '去吃冰',
+          ko: '파란색 맞기',
+        },
+      },
+    },
+    // Copy/paste from normal, seems to be the same
+    {
+      id: 'P1S Powerful Light/Fire',
+      type: 'GainsEffect',
+      netRegex: NetRegexes.gainsEffect({ effectId: '893', capture: true }),
+      alertText: (_data, matches, _output) => {
+        if (matches.count === '14C')
+          return _output.light!();
+        return _output.fire!();
+      },
+      outputStrings: {
+        fire: {
+          en: 'Stand on fire',
+          de: 'Auf der Feuerfläche stehen',
+          fr: 'Placez-vous sur le feu',
+          cn: '站在火',
+          ko: '빨간색 바닥 위에 서기',
+        },
+        light: {
+          en: 'Stand on light',
+          de: 'Auf der Lichtfläche stehen',
+          fr: 'Placez-vous sur la lumière',
+          cn: '站在光',
+          ko: '흰색 바닥 위에 서기',
+        },
+      },
+    },
+  ],
+  timelineTriggers: [
+    {
+      id: 'P1S Tile Positions',
+      regex: /(?:First|Second|Third) Element/,
+      beforeSeconds: 3,
+      infoText: (_data, _matches, output) => output.positions!(),
+      outputStrings: {
+        positions: {
+          en: 'Tile Positions',
+        },
+      },
+    },
+  ],
+};
+
+export default triggerSet;
