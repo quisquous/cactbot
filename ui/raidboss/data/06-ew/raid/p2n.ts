d65954137c4b3b477fbec35372cb6eae2045b206
VanSlanzar <61565834+VanSlanzar@users.noreply.github.com>
2022-01-04 19:19:58 +0100

    added missing translations Fr

--- ui/raidboss/data/06-ew/raid/p2n.ts
+++ ui/raidboss/data/06-ew/raid/p2n.ts
@@ -195,6 +195,7 @@ const triggerSet: TriggerSet<Data> = {
       outputStrings: {
         lineStackOn: {
           en: 'Line stack on ${player}',
+          fr: 'Packez-vous en ligne sur ${player}',
         },
       },
     },
@@ -253,12 +254,15 @@ const triggerSet: TriggerSet<Data> = {
     },
     {
       'locale': 'fr',
-      'missingTranslations': true,
       'replaceSync': {
         'Hippokampos': 'hippokampos',
       },
       'replaceText': {
-        'Dissociation': 'Dissociation',
+        '\\(knockback\\)': '(poussée)',
+        'Coherence Flare': 'Cohérence Brasier',
+        'Coherence Line': 'Cohérence en ligne',
+        'Dissociation(?! Dive)': 'Dissociation',
+        'Dissociation Dive': 'Dissociation et plongeon',
         'Doubled Impact': 'Double impact',
         'Murky Depths': 'Tréfonds troubles',
         'Predatory Sight': 'Marque de la proie',
