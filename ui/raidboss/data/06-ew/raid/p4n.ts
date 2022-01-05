e6fee80094d059f4d32a56c1d40b6f5bf7150344
mooondark <40036185+mooondark@users.noreply.github.com>
2022-01-05 09:46:10 +0100

    raidboss: wrong FR meaning

--- ui/raidboss/data/06-ew/raid/p4n.ts
+++ ui/raidboss/data/06-ew/raid/p4n.ts
@@ -115,7 +115,7 @@ const triggerSet: TriggerSet<Data> = {
         text: {
           en: 'Get Other Role Tower',
           de: 'Geh in einen Turm einer anderen Rolle',
-          fr: 'Prenez la tour de votre rôle',
+          fr: 'Prenez la tour d\'un autre rôle',
           cn: '踩其他职能的塔',
           ko: '내 직업군이 아닌쪽 장판 밟기',
         },
