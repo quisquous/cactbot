89d945383d82d31e26a3483a24bba002f08c18f5
Legends0 <nac10fsu@gmail.com>
2022-01-09 17:49:30 -0500

    remove bloodrake/belone coils for easier merge
    They need some more work, will add another PR after.

--- ui/raidboss/data/06-ew/raid/p4s.ts
+++ ui/raidboss/data/06-ew/raid/p4s.ts
@@ -7,6 +7,10 @@ import { RaidbossData } from '../../../../../types/data';
 import { NetMatches } from '../../../../../types/net_matches';
 import { TriggerSet } from '../../../../../types/trigger';
 
+// Part one
+// TODO: Fixup Director's Belone callouts
+// TODO: Fixup Well Pinax in combo with Directional Shift (knockback) timings
+
 // Part Two
 // TODO: Wreath of Thorns 1 callout safe spot order (N/S or E/W)
 // TODO: Wreath of Thorns 2 headmarkers and tethers
@@ -21,36 +25,10 @@ import { TriggerSet } from '../../../../../types/trigger';
 export interface Data extends RaidbossData {
   actingRole?: string;
   decOffset?: number;
-  tetherRole?: string[];
-  debuffRole?: string[];
-  hasRoleCall?: boolean;
-  beloneCoilsTwo?: boolean;
   bloodrakeCounter?: number;
   fleetingImpulseCounter?: number;
   meFleetingImpulse?: number;
 }
-
-const roleOutputStrings = {
-  tankHealer: {
-    en: 'Tank/Healer',
-  },
-  dps: {
-    en: 'DPS',
-  },
-  roleTethers: {
-    en: '${role} Tethers',
-  },
-  roleDebuffs: {
-    en: '${role} Role Calls',
-  },
-  roleEverything: {
-    en: '${role} Everything',
-  },
-  roleTowers: {
-    en: '${role} Towers',
-  },
-  unknown: Outputs.unknown,
-};
 
 // Due to changes introduced in patch 5.2, overhead markers now have a random offset
 // added to their ID. This offset currently appears to be set per instance, so
@@ -160,181 +138,6 @@ const triggerSet: TriggerSet<Data> = {
       response: Responses.aoe(),
     },
     {
-      id: 'P4S Bloodrake Store',
-      type: 'Ability',
-      netRegex: NetRegexes.ability({ id: '69D8', source: 'Hesperos' }),
-      netRegexDe: NetRegexes.ability({ id: '69D8', source: 'Hesperos' }),
-      netRegexFr: NetRegexes.ability({ id: '69D8', source: 'Hespéros' }),
-      netRegexJa: NetRegexes.ability({ id: '69D8', source: 'ヘスペロス' }),
-      condition: (data) => (data.bloodrakeCounter ?? 0) < 3,
-      suppressSeconds: 1,
-      infoText: (data, matches, output) => {
-        const roles: { [role: string]: string } = {
-          'dps': output.dps!(),
-          'tank/healer': output.tankHealer!(),
-        };
-
-        const role = data.party.isDPS(matches.target) ? 'dps' : 'tank/healer';
-
-        // Second bloodrake = Tethers later
-        if ((data.bloodrakeCounter ?? 0) === 2) {
-          if (role === 'dps') {
-            (data.tetherRole ??= []).push(role);
-          } else {
-            (data.tetherRole ??= []).push('healer');
-            data.tetherRole.push('tank');
-          }
-
-          // May end up needing both tether and debuff
-          const debuffRole = data.debuffRole ??= [];
-          if (data.tetherRole[0] === debuffRole[0])
-            return output.roleEverything!({ role: roles[role] });
-          return output.roleTethers!({ role: roles[role] });
-        }
-
-        // First bloodrake = Debuffs later
-        if (role === 'dps') {
-          (data.debuffRole ??= []).push(role);
-        } else {
-          (data.debuffRole ??= []).push('healer');
-          data.debuffRole.push('tank');
-        }
-        return output.roleTethers!({ role: roles[role] });
-      },
-      outputStrings: roleOutputStrings,
-    },
-    {
-      id: 'P4S Belone Coils',
-      // 69DE is No Tank/Healer Belone Coils
-      // 69DF is No DPS Belone Coils
-      type: 'StartsUsing',
-      netRegex: NetRegexes.startsUsing({ id: ['69DE', '69DF'], source: 'Hesperos' }),
-      netRegexDe: NetRegexes.startsUsing({ id: ['69DE', '69DF'], source: 'Hesperos' }),
-      netRegexFr: NetRegexes.startsUsing({ id: ['69DE', '69DF'], source: 'Hespéros' }),
-      netRegexJa: NetRegexes.startsUsing({ id: ['69DE', '69DF'], source: 'ヘスペロス' }),
-      preRun: (data) => {
-        if (!data.beloneCoilsTwo) {
-          delete data.debuffRole;
-          delete data.tetherRole;
-          data.hasRoleCall = false;
-        }
-      },
-      response: (data, matches, output) => {
-        // cactbot-builtin-response
-        output.responseOutputStrings = roleOutputStrings;
-
-        const roles: { [role: string]: string } = {
-          'dps': output.dps!(),
-          'tank/healer': output.tankHealer!(),
-        };
-
-        const role = matches.id === '69DE' ? 'dps' : 'tank/healer';
-
-        // Second Coils = Debuffs later
-        if (data.beloneCoilsTwo) {
-          if (role === 'dps') {
-            (data.debuffRole ??= []).push(role);
-          } else {
-            (data.debuffRole ??= []).push('healer');
-            data.debuffRole.push('tank');
-          }
-
-          // For second coils, if you are not in the debuff list here you are tower
-          if (!data.debuffRole.includes(data.role))
-            return { ['alertText']: output.roleTowers!({ role: roles[role] }) };
-
-          // If you have tethers and debuff, you need everything
-          const tetherRole = data.tetherRole ??= [];
-          if (data.debuffRole[0] === tetherRole[0])
-            return { ['infoText']: output.roleEverything!({ role: roles[role] }) };
-          return { ['infoText']: output.roleDebuffs!({ role: roles[role] }) };
-        }
-
-        // First Coils = Tethers later
-        if (role === 'dps') {
-          (data.tetherRole ??= []).push(role);
-        } else {
-          (data.tetherRole ??= []).push('healer');
-          data.tetherRole.push('tank');
-        }
-
-        // For first coils, there are tower and tethers
-        if (data.tetherRole.includes(data.role))
-          return { ['alertText']: output.roleTethers!({ role: roles[role] }) };
-        return { ['alertText']: output.roleTowers!({ role: roles[role] }) };
-      },
-      run: (data) => data.beloneCoilsTwo = true,
-    },
-    {
-      id: 'P4S Role Call',
-      type: 'GainsEffect',
-      netRegex: NetRegexes.gainsEffect({ effectId: ['AF2', 'AF3'], capture: true }),
-      condition: Conditions.targetIsYou(),
-      alertText: (data, matches, output) => {
-        const debuffRole = (data.debuffRole ??= []).includes(data.role);
-        if (debuffRole && matches.effectId === 'AF2')
-          return output.passRoleCall!();
-        // AF3 is obtained after passing Role Call (AF2)
-        data.hasRoleCall = false;
-      },
-      outputStrings: {
-        passRoleCall: {
-          en: 'Pass Role Call',
-        },
-      },
-    },
-    {
-      id: 'P4S Director\'s Belone',
-      type: 'Ability',
-      netRegex: NetRegexes.ability({ id: '69E6', source: 'Hesperos', capture: false }),
-      netRegexDe: NetRegexes.ability({ id: '69E6', source: 'Hesperos', capture: false }),
-      netRegexFr: NetRegexes.ability({ id: '69E6', source: 'Hespéros', capture: false }),
-      netRegexJa: NetRegexes.ability({ id: '69E6', source: 'ヘスペロス', capture: false }),
-      // Delay callout until debuffs are out
-      delaySeconds: 1.4,
-      alertText: (data, _matches, output) => {
-        const debuffRole = (data.debuffRole ??= []).includes(data.role);
-        if (!data.hasRoleCall && !debuffRole)
-          return output.text!();
-      },
-      run: (data) => {
-        delete data.debuffRole;
-      },
-      outputStrings: {
-        text: {
-          en: 'Get Role Call',
-        },
-      },
-    },
-    {
-      id: 'P4S Inversive Chlamys',
-      // Possible a player still has not yet passed debuff
-      type: 'StartsUsing',
-      netRegex: NetRegexes.startsUsing({ id: '69ED', source: 'Hesperos', capture: false }),
-      netRegexDe: NetRegexes.startsUsing({ id: '69ED', source: 'Hesperos', capture: false }),
-      netRegexFr: NetRegexes.startsUsing({ id: '69ED', source: 'Hespéros', capture: false }),
-      netRegexJa: NetRegexes.startsUsing({ id: '69ED', source: 'ヘスペロス', capture: false }),
-      alertText: (data, _matches, output) => {
-        const roles: { [role: string]: string } = {
-          'dps': output.dps!(),
-          'tank/healer': output.tankHealer!(),
-          '???': output.unknown!(),
-        };
-
-        const dps = (data.tetherRole ??= []).includes('dps');
-        if (dps)
-          return output.roleTethers!({ role: roles['dps'] });
-        if (data.tetherRole.length)
-          return output.roleTethers!({ role: roles['tank/healer'] });
-        return output.roleTethers!({ role: roles['???'] });
-      },
-      run: (data) => {
-        delete data.tetherRole;
-        data.hasRoleCall = false;
-      },
-      outputStrings: roleOutputStrings,
-    },
-    {
       id: 'P4S Elegant Evisceration',
       // This one does an aoe around the tank
       type: 'StartsUsing',
@@ -371,16 +174,12 @@ const triggerSet: TriggerSet<Data> = {
       netRegexDe: NetRegexes.startsUsing({ id: '69D6', source: 'Hesperos', capture: false }),
       netRegexFr: NetRegexes.startsUsing({ id: '69D6', source: 'Hespéros', capture: false }),
       netRegexJa: NetRegexes.startsUsing({ id: '69D6', source: 'ヘスペロス', capture: false }),
-      // 5.5 seconds to time with Directional Shift knockbacks
+      // Set to 5.5 to time with Direction Shift knockbacks
       delaySeconds: 5.5,
       alertText: (_data, _matches, output) => output.text!(),
       outputStrings: {
         text: {
-          en: 'Middle Knockback',
-          de: 'Rückstoß von der Mitte',
-          fr: 'Poussée au milieu',
-          cn: '中间击退',
-          ko: '중앙에서 넉백',
+          en: 'Well',
         },
       },
     },
@@ -475,16 +274,10 @@ const triggerSet: TriggerSet<Data> = {
       netRegexDe: NetRegexes.startsUsing({ id: '69FD', source: 'Hesperos', capture: false }),
       netRegexFr: NetRegexes.startsUsing({ id: '69FD', source: 'Hespéros', capture: false }),
       netRegexJa: NetRegexes.startsUsing({ id: '69FD', source: 'ヘスペロス', capture: false }),
-      // 2.5 seconds to time with Well Pinax
-      delaySeconds: 2.5,
       alertText: (_data, _matches, output) => output.text!(),
       outputStrings: {
         text: {
-          en: 'North Knockback',
-          de: 'Rückstoß vom Norden',
-          fr: 'Poussée au nord',
-          cn: '上/北 击退',
-          ko: '북쪽 넉백',
+          en: 'North Cape',
         },
       },
     },
@@ -495,16 +288,10 @@ const triggerSet: TriggerSet<Data> = {
       netRegexDe: NetRegexes.startsUsing({ id: '69FF', source: 'Hesperos', capture: false }),
       netRegexFr: NetRegexes.startsUsing({ id: '69FF', source: 'Hespéros', capture: false }),
       netRegexJa: NetRegexes.startsUsing({ id: '69FF', source: 'ヘスペロス', capture: false }),
-      // 2.5 seconds to time with Well Pinax
-      delaySeconds: 2.5,
       alertText: (_data, _matches, output) => output.text!(),
       outputStrings: {
         text: {
-          en: 'East Knockback',
-          de: 'Rückstoß vom Osten',
-          fr: 'Poussée à l\'est',
-          cn: '右/东 击退',
-          ko: '동쪽 넉백',
+          en: 'East Cape',
         },
       },
     },
@@ -515,16 +302,10 @@ const triggerSet: TriggerSet<Data> = {
       netRegexDe: NetRegexes.startsUsing({ id: '69FE', source: 'Hesperos', capture: false }),
       netRegexFr: NetRegexes.startsUsing({ id: '69FE', source: 'Hespéros', capture: false }),
       netRegexJa: NetRegexes.startsUsing({ id: '69FE', source: 'ヘスペロス', capture: false }),
-      // 2.5 seconds to time with Well Pinax
-      delaySeconds: 2.5,
       alertText: (_data, _matches, output) => output.text!(),
       outputStrings: {
         text: {
-          en: 'South Knockback',
-          de: 'Rückstoß vom Süden',
-          fr: 'Poussée au sud',
-          cn: '下/南 击退',
-          ko: '남쪽 넉백',
+          en: 'South Cape',
         },
       },
     },
@@ -535,16 +316,10 @@ const triggerSet: TriggerSet<Data> = {
       netRegexDe: NetRegexes.startsUsing({ id: '6A00', source: 'Hesperos', capture: false }),
       netRegexFr: NetRegexes.startsUsing({ id: '6A00', source: 'Hespéros', capture: false }),
       netRegexJa: NetRegexes.startsUsing({ id: '6A00', source: 'ヘスペロス', capture: false }),
-      // 2.5 seconds to time with Well Pinax
-      delaySeconds: 2.5,
       alertText: (_data, _matches, output) => output.text!(),
       outputStrings: {
         text: {
-          en: 'West Knockback',
-          de: 'Rückstoß vom Westen',
-          fr: 'Poussée à l\'ouest',
-          cn: '左/西 击退',
-          ko: '서쪽 넉백',
+          en: 'West Cape',
         },
       },
     },
