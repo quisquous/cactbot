4ccef617077a973e41d52b56d82e645469c9c900
Akurosia <kamothegamer@web.de>
2022-02-03 10:26:13 +0100

    i18n: small translation update

--- ui/raidboss/data/06-ew/hunts/thavnair.ts
+++ ui/raidboss/data/06-ew/hunts/thavnair.ts
@@ -52,9 +52,9 @@ const triggerSet: TriggerSet<Data> = {
       // This is followed up with Rip (6A58) which is also a tank cleave.
       // We could call out 2x tank cleave, but maybe that's overkill.
       netRegex: NetRegexes.startsUsing({ id: '6A59', source: 'Sugriva' }),
-      netRegexDe: NetRegexes.startsUsing({ id: '6A57', source: 'Sugriva' }),
-      netRegexFr: NetRegexes.startsUsing({ id: '6A57', source: 'Sugriva' }),
-      netRegexJa: NetRegexes.startsUsing({ id: '6A57', source: 'スグリーヴァ' }),
+      netRegexDe: NetRegexes.startsUsing({ id: '6A59', source: 'Sugriva' }),
+      netRegexFr: NetRegexes.startsUsing({ id: '6A59', source: 'Sugriva' }),
+      netRegexJa: NetRegexes.startsUsing({ id: '6A59', source: 'スグリーヴァ' }),
       response: Responses.tankCleave(),
     },
     {
@@ -80,6 +80,9 @@ const triggerSet: TriggerSet<Data> = {
       id: 'Hunt Yilan Forward March',
       type: 'GainsEffect',
       netRegex: NetRegexes.gainsEffect({ effectId: '7A6', source: 'Yilan' }),
+      netRegexDe: NetRegexes.gainsEffect({ effectId: '7A6', source: 'Yilan' }),
+      netRegexFr: NetRegexes.gainsEffect({ effectId: '7A6', source: 'Yilan' }),
+      netRegexJa: NetRegexes.gainsEffect({ effectId: '7A6', source: 'ユラン' }),
       condition: Conditions.targetIsYou(),
       // t=0.0 gain effect (this line)
       // t=6.3 Mini Light starts casting
@@ -100,6 +103,9 @@ const triggerSet: TriggerSet<Data> = {
       id: 'Hunt Yilan About Face',
       type: 'GainsEffect',
       netRegex: NetRegexes.gainsEffect({ effectId: '7A7', source: 'Yilan' }),
+      netRegexDe: NetRegexes.gainsEffect({ effectId: '7A7', source: 'Yilan' }),
+      netRegexFr: NetRegexes.gainsEffect({ effectId: '7A7', source: 'Yilan' }),
+      netRegexJa: NetRegexes.gainsEffect({ effectId: '7A7', source: 'ユラン' }),
       condition: Conditions.targetIsYou(),
       delaySeconds: (_data, matches) => parseFloat(matches.duration) - 4,
       alertText: (_data, _matches, output) => output.text!(),
@@ -115,6 +121,9 @@ const triggerSet: TriggerSet<Data> = {
       id: 'Hunt Yilan Left Face',
       type: 'GainsEffect',
       netRegex: NetRegexes.gainsEffect({ effectId: '7A8', source: 'Yilan' }),
+      netRegexDe: NetRegexes.gainsEffect({ effectId: '7A8', source: 'Yilan' }),
+      netRegexFr: NetRegexes.gainsEffect({ effectId: '7A8', source: 'Yilan' }),
+      netRegexJa: NetRegexes.gainsEffect({ effectId: '7A8', source: 'ユラン' }),
       condition: Conditions.targetIsYou(),
       alertText: (_data, _matches, output) => output.text!(),
       outputStrings: {
@@ -129,6 +138,9 @@ const triggerSet: TriggerSet<Data> = {
       id: 'Hunt Yilan Right Face',
       type: 'GainsEffect',
       netRegex: NetRegexes.gainsEffect({ effectId: '7A9', source: 'Yilan' }),
+      netRegexDe: NetRegexes.gainsEffect({ effectId: '7A9', source: 'Yilan' }),
+      netRegexFr: NetRegexes.gainsEffect({ effectId: '7A9', source: 'Yilan' }),
+      netRegexJa: NetRegexes.gainsEffect({ effectId: '7A9', source: 'ユラン' }),
       condition: Conditions.targetIsYou(),
       alertText: (_data, _matches, output) => output.text!(),
       outputStrings: {
@@ -144,6 +156,9 @@ const triggerSet: TriggerSet<Data> = {
       type: 'StartsUsing',
       // Untelegraphed conal attack.
       netRegex: NetRegexes.startsUsing({ id: '6A62', source: 'Yilan', capture: false }),
+      netRegexDe: NetRegexes.startsUsing({ id: '6A62', source: 'Yilan', capture: false }),
+      netRegexFr: NetRegexes.startsUsing({ id: '6A62', source: 'Yilan', capture: false }),
+      netRegexJa: NetRegexes.startsUsing({ id: '6A62', source: 'ユラン', capture: false }),
       response: Responses.getBehind(),
     },
   ],
--- ui/raidboss/data/06-ew/raid/p2s.ts
+++ ui/raidboss/data/06-ew/raid/p2s.ts
@@ -150,10 +150,12 @@ const triggerSet: TriggerSet<Data> = {
         output.responseOutputStrings = {
           marks: {
             en: 'Marks: ${player1}, ${player2}',
+            de: 'Marker: ${player1}, ${player2}',
             fr: 'Marques sur : ${player1}, ${player2}',
           },
           avariceOnYou: {
             en: 'Avarice on YOU',
+            de: 'Marker auf DIR',
             fr: 'Marque sur VOUS',
           },
           unknown: Outputs.unknown,
