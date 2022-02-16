d7b0e4603dbb3d1ce96cabb3effe588cbabeecca
VanSlanzar <61565834+VanSlanzar@users.noreply.github.com>
2022-02-16 19:21:08 +0100

    better translation fr

--- ui/raidboss/data/06-ew/raid/p3s.ts
+++ ui/raidboss/data/06-ew/raid/p3s.ts
@@ -71,7 +71,7 @@ const triggerSet: TriggerSet<Data> = {
         text: {
           en: 'Fire Positions',
           de: 'Feuer-Positionen',
-          fr: 'Positions feu',
+          fr: 'Positions pour les flammes',
           ja: '黒い炎の位置に散開',
           cn: '暗炎站位',
           ko: '불꽃 산개 위치로',
