61b90d3c97b7aab5d12c0036ebb7b4089c4a417a
Jacob Keller <jacob.keller@gmail.com>
2022-03-09 00:03:56 -0800

    raidboss: add Sharp Gust knockback callout
    During the 3rd phase of Limitless Blue Extreme, Bismarck will sometimes
    use an untelegraphed knockback called "Sharp Gust". The cast only
    happens when the weather is windy, but it has no obvious ground
    telegraph or cast bar.
    
    Signed-off-by: Jacob Keller <jacob.keller@gmail.com>

---
+++ ui/raidboss/data/03-hw/trial/bismarck-ex.ts
@@ -0,0 +1,21 @@
+import NetRegexes from '../../../../../resources/netregexes';
+import { Responses } from '../../../../../resources/responses';
+import ZoneId from '../../../../../resources/zone_id';
+import { RaidbossData } from '../../../../../types/data';
+import { TriggerSet } from '../../../../../types/trigger';
+
+export type Data = RaidbossData;
+
+const triggerSet: TriggerSet<Data> = {
+  zoneId: ZoneId.TheLimitlessBlueExtreme,
+  triggers: [
+    {
+      id: 'Bismarck Sharp Gust',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: 'FAF', source: 'Bismarck', capture: false }),
+      response: Responses.knockback(),
+    },
+  ],
+};
+
+export default triggerSet;
