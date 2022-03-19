d63e13a9475b1da8af78c49429b880355750b894
Adrienne Walker <enne@quisquo.us>
2022-03-18 21:35:23 -0700

    fixups

--- ui/oopsyraidsy/data/06-ew/raid/p4s.ts
+++ ui/oopsyraidsy/data/06-ew/raid/p4s.ts
@@ -16,11 +16,11 @@ const triggerSet: OopsyTriggerSet<Data> = {
     'P4S Lava Pinax': '69D5', // standing on fire square
     'P4S Well Pinax': '69D6', // standing on water square
     'P4S Levinstrike Pinax': '69D7', // standing on lightning square
-    'P4S Acid Pinax Elemental Belone': '69F5', // unsafe square during Elemental Belone
-    'P4S Lava Pinax Elemental Belone': '69F6', // unsafe square during Elemental Belone
-    'P4S Well Pinax Elemental Belone': '69F7', // unsafe square during Elemental Belone
-    'P4S Levinstrike Pinax Elemental Belone': '69F8', // unsafe square during Elemental Belone
-    'P4S Levinstrike Mekhane': '69D3', // lightning pinax ability
+    // The safe squares during Elemental Belone are 69F5 through 69F8.
+    'P4S Elemental Belone Acid Pinax': '69F9', // acid square during Elemental Belone
+    'P4S Elemental Belone Lava Pinax': '69FA', // fire square during Elemental Belone
+    'P4S Elemental Belone Well Pinax': '69FB', // water square during Elemental Belone
+    'P4S Elemental Belone Levinstrike Pinax': '69FC', // lightning square during Elemental Belone
     'P4S Shifting Strike': '6A06', // jump to cardinal + cleave
   },
   shareWarn: {
