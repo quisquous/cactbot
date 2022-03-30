2c344d60a413172c0bfc50c142868ea380dc7ff4
Echoring <68432572+Echoring@users.noreply.github.com>
2022-03-31 00:40:10 +0800

    jobs: WAR: more timer box (#4219)

--- ui/jobs/components/war.ts
+++ ui/jobs/components/war.ts
@@ -10,20 +10,28 @@ import { PartialFieldMatches } from '../event_emitter';
 import { BaseComponent, ComponentInterface } from './base';
 
 export class WARComponent extends BaseComponent {
-  textBox: ResourceBox;
-  eyeBox: TimerBox;
+  BeastBox: ResourceBox;
+  tempestBox: TimerBox;
+  upheavalBox: TimerBox;
+  innerReleaseBox: TimerBox;
   comboTimer: TimerBar;
   bonus: number;
 
   constructor(o: ComponentInterface) {
     super(o);
-    this.textBox = this.bars.addResourceBox({
+    this.BeastBox = this.bars.addResourceBox({
       classList: ['war-color-beast'],
     });
 
-    this.eyeBox = this.bars.addProcBox({
-      fgColor: 'war-color-eye',
+    this.tempestBox = this.bars.addProcBox({
+      fgColor: 'war-color-tempest',
       notifyWhenExpired: true,
+    });
+    this.upheavalBox = this.bars.addProcBox({
+      fgColor: 'war-color-upheaval',
+    });
+    this.innerReleaseBox = this.bars.addProcBox({
+      fgColor: 'war-color-innerrelease',
     });
 
     this.comboTimer = this.bars.addTimerBar({
@@ -38,14 +46,14 @@ export class WARComponent extends BaseComponent {
 
   override onJobDetailUpdate(jobDetail: JobDetail['WAR']): void {
     const beast = jobDetail.beast.toString();
-    if (this.textBox.innerText === beast)
+    if (this.BeastBox.innerText === beast)
       return;
-    this.textBox.innerText = beast;
-    const p = this.textBox.parentNode;
+    this.BeastBox.innerText = beast;
+    const p = this.BeastBox.parentNode;
     if (jobDetail.beast < 50) {
       p.classList.add('low');
       p.classList.remove('mid');
-    } else if (jobDetail.beast < 100) {
+    } else if (jobDetail.beast < 90) {
       p.classList.remove('low');
       p.classList.add('mid');
     } else {
@@ -54,10 +62,26 @@ export class WARComponent extends BaseComponent {
     }
   }
 
+  override onUseAbility(skill: string): void {
+    switch (skill) {
+      case kAbility.Upheaval:
+      case kAbility.Orogeny:
+        this.upheavalBox.duration = 30;
+        break;
+      case kAbility.InnerRelease:
+      case kAbility.Berserk:
+        if (this.is5x)
+          this.innerReleaseBox.duration = 90;
+        else
+          this.innerReleaseBox.duration = 60;
+        break;
+    }
+  }
+
   override onCombo(skill: string, combo: ComboTracker): void {
     this.comboTimer.duration = 0;
     // Storm's Eye initiation will freeze Surging Tempest buff for about 1.6s before countdown start
-    if (skill === kAbility.StormsEye && !this.eyeBox.duration)
+    if (skill === kAbility.StormsEye && !this.tempestBox.duration)
       this.bonus = 1.6;
     if (combo.isFinalSkill)
       return;
@@ -69,22 +93,28 @@ export class WARComponent extends BaseComponent {
     if (id !== EffectId.SurgingTempest && id !== EffectId.StormsEye)
       return;
     const duration = parseFloat(matches.duration ?? '0');
-    this.eyeBox.duration = duration + this.bonus - 0.5; // buff logline delay
+    this.tempestBox.duration = duration + this.bonus - 0.5; // buff logline delay
     this.bonus = 0;
   }
   override onYouLoseEffect(id: string): void {
     // TODO: delete StormsEye after every region launch 6.0
     if (id === EffectId.SurgingTempest || id === EffectId.StormsEye)
-      this.eyeBox.duration = 0;
+      this.tempestBox.duration = 0;
   }
 
   override onStatChange({ gcdSkill }: { gcdSkill: number }): void {
-    this.eyeBox.valuescale = gcdSkill;
-    this.eyeBox.threshold = gcdSkill * 3 + 1;
+    this.tempestBox.valuescale = gcdSkill;
+    this.tempestBox.threshold = gcdSkill * 3 + 1;
+    this.upheavalBox.valuescale = gcdSkill;
+    this.upheavalBox.threshold = gcdSkill;
+    this.innerReleaseBox.valuescale = gcdSkill;
+    this.innerReleaseBox.threshold = gcdSkill * 3;
   }
 
   override reset(): void {
-    this.eyeBox.duration = 0;
+    this.tempestBox.duration = 0;
+    this.upheavalBox.duration = 0;
+    this.innerReleaseBox.duration = 0;
     this.comboTimer.duration = 0;
   }
 }
