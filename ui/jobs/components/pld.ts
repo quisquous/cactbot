98e7fc812c80312f55c2b6fc9f71b33389ac1397
Echoring <68432572+Echoring@users.noreply.github.com>
2022-03-31 00:39:09 +0800

    jobs: PLD: More function and MP Threshold adjust (#4218)

--- ui/jobs/components/pld.ts
+++ ui/jobs/components/pld.ts
@@ -4,6 +4,7 @@ import { JobDetail } from '../../../types/event';
 import { ResourceBox } from '../bars';
 import { kAbility } from '../constants';
 import { PartialFieldMatches } from '../event_emitter';
+import { computeBackgroundColorFrom } from '../utils';
 
 import { BaseComponent, ComponentInterface } from './base';
 
@@ -11,6 +12,11 @@ export class PLDComponent extends BaseComponent {
   oathBox: ResourceBox;
   atonementBox: ResourceBox;
   goreBox: TimerBox;
+  expiacionBox: TimerBox;
+  fightOrFlightBox: TimerBox;
+  tid1 = 0;
+  stacksContainer: HTMLDivElement;
+  requiescat: HTMLElement[] = [];
 
   constructor(o: ComponentInterface) {
     super(o);
@@ -25,8 +31,28 @@ export class PLDComponent extends BaseComponent {
       fgColor: 'pld-color-gore',
       notifyWhenExpired: true,
     });
+    this.fightOrFlightBox = this.bars.addProcBox({
+      fgColor: 'pld-color-fightorflight',
+    });
+    this.expiacionBox = this.bars.addProcBox({
+      fgColor: 'pld-color-expiacion',
+    });
 
     this.setAtonement(this.atonementBox, 0);
+
+    this.stacksContainer = document.createElement('div');
+    this.stacksContainer.id = 'pld-stacks';
+    this.stacksContainer.classList.add('stacks', 'hide');
+    this.bars.addJobBarContainer().appendChild(this.stacksContainer);
+    const requiescatContainer = document.createElement('div');
+    requiescatContainer.id = 'pld-stacks-requiescat';
+    this.stacksContainer.appendChild(requiescatContainer);
+
+    for (let i = 0; i < 5; ++i) {
+      const d = document.createElement('div');
+      requiescatContainer.appendChild(d);
+      this.requiescat.push(d);
+    }
 
     this.reset();
   }
@@ -57,6 +83,10 @@ export class PLDComponent extends BaseComponent {
     else
       p.classList.add('any');
   }
+  setRequiescat(stacks: number): void {
+    for (let i = 0; i < 5; ++i)
+      this.requiescat[i]?.classList.toggle('active', stacks > i);
+  }
 
   override onCombo(skill: string): void {
     if (skill === kAbility.GoringBlade)
@@ -64,8 +94,25 @@ export class PLDComponent extends BaseComponent {
   }
 
   override onUseAbility(skill: string): void {
-    if (skill === kAbility.BladeOfValor)
-      this.goreBox.duration = 21;
+    switch (skill) {
+      case kAbility.BladeOfValor:
+        this.goreBox.duration = 21;
+        break;
+      case kAbility.Expiacion:
+      case kAbility.SpiritsWithin:
+        this.expiacionBox.duration = 30;
+        break;
+      case kAbility.FightOrFlight:
+        this.fightOrFlightBox.duration = 25;
+        this.fightOrFlightBox.threshold = 1000;
+        this.fightOrFlightBox.fg = computeBackgroundColorFrom(this.fightOrFlightBox, 'pld-color-fightorflight.active');
+        this.tid1 = window.setTimeout(() => {
+          this.fightOrFlightBox.duration = 35;
+          this.fightOrFlightBox.threshold = this.player.gcdSkill * 2 + 1;
+          this.fightOrFlightBox.fg = computeBackgroundColorFrom(this.fightOrFlightBox, 'pld-color-fightorflight');
+        }, 25000);
+        break;
+    }
   }
 
   // As atonement counts down, the player gets successive "gains effects"
@@ -74,21 +121,40 @@ export class PLDComponent extends BaseComponent {
   override onYouGainEffect(id: string, matches: PartialFieldMatches<'GainsEffect'>): void {
     if (id === EffectId.SwordOath)
       this.setAtonement(this.atonementBox, parseInt(matches.count ?? '0'));
+    if (id === EffectId.Requiescat && !this.is5x) {
+      this.stacksContainer.classList.remove('hide');
+      this.setRequiescat(parseInt(matches.count ?? '0'));
+    }
   }
 
   override onYouLoseEffect(id: string): void {
     if (id === EffectId.SwordOath)
       this.setAtonement(this.atonementBox, 0);
+    if (id === EffectId.Requiescat && !this.is5x) {
+      this.setRequiescat(0);
+      this.stacksContainer.classList.add('hide');
+    }
   }
 
   override onStatChange({ gcdSkill } : { gcdSkill: number }): void {
     this.goreBox.valuescale = gcdSkill;
     this.goreBox.threshold = gcdSkill * 3 + 0.3;
+    this.expiacionBox.valuescale = gcdSkill;
+    this.expiacionBox.threshold = gcdSkill;
+    this.fightOrFlightBox.valuescale = gcdSkill;
+    this.fightOrFlightBox.threshold = gcdSkill * 2 + 1;
   }
 
   override reset(): void {
     this.goreBox.duration = 0;
+    this.expiacionBox.duration = 0;
+    this.fightOrFlightBox.duration = 0;
+    this.fightOrFlightBox.threshold = this.player.gcdSkill * 2 + 1;
+    this.fightOrFlightBox.fg = computeBackgroundColorFrom(this.fightOrFlightBox, 'pld-color-fightorflight');
+    window.clearTimeout(this.tid1);
     this.setAtonement(this.atonementBox, 0);
+    this.setRequiescat(0);
+    this.stacksContainer.classList.add('hide');
   }
 }
 
