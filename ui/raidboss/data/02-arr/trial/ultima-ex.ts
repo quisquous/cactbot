0148d4bfa7f7fecaad4b6b234d28c9ede44ff062
Jonathan Garber <linkthevaliant@gmail.com>
2022-04-04 02:50:57 -0400

    Initial triggers and first two timeline phases

---
+++ ui/raidboss/data/02-arr/trial/ultima-ex.ts
@@ -0,0 +1,41 @@
+import NetRegexes from '../../../../../resources/netregexes';
+import { Responses } from '../../../../../resources/responses';
+import ZoneId from '../../../../../resources/zone_id';
+import { RaidbossData } from '../../../../../types/data';
+import { TriggerSet } from '../../../../../types/trigger';
+
+export type Data = RaidbossData;
+
+const triggerSet: TriggerSet<Data> = {
+  zoneId: ZoneId.TheMinstrelsBalladUltimasBane,
+  timelineFile: 'ultima-ex.txt',
+  timelineTriggers: [
+    {
+      // Early Callout for Tank Cleave
+      id: 'Ultima EX Homing Lasers',
+      regex: /Homing Lasers/,
+      beforeSeconds: 5,
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Off-tank cleave',
+        },
+      },
+    },
+  ],
+  triggers: [
+    {
+      id: 'Ultima EX Tank Purge',
+      netRegex: NetRegexes.startsUsing({ id: '5EA', source: 'The Ultima Weapon', capture: false }),
+      response: Responses.bigAoe(),
+    },
+    {
+      // At 5 stacks of Aetheroplasm, the target begins taking massive damage.
+      id: 'Ultima EX Viscous Aetheroplasm',
+      netRegex: NetRegexes.gainsEffect({ effectId: '171', count: '4' }),
+      response: Responses.tankBusterSwap(),
+    },
+  ],
+};
+
+export default triggerSet;
