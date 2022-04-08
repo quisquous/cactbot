32732db2a4ff93540beb9fd54362b78faf2031b2
Jonathan Garber <linkthevaliant@gmail.com>
2022-04-07 23:44:05 -0400

    Trigger updates; add Aetheroplasm + Boom triggers

--- ui/raidboss/data/02-arr/trial/ultima-ex.ts
+++ ui/raidboss/data/02-arr/trial/ultima-ex.ts
@@ -4,17 +4,26 @@ import ZoneId from '../../../../../resources/zone_id';
 import { RaidbossData } from '../../../../../types/data';
 import { TriggerSet } from '../../../../../types/trigger';
 
-export type Data = RaidbossData;
+export interface Data extends RaidbossData {
+  plasmTargets?: Array<string>;
+  boomCounter: number;
+}
 
 const triggerSet: TriggerSet<Data> = {
   zoneId: ZoneId.TheMinstrelsBalladUltimasBane,
   timelineFile: 'ultima-ex.txt',
+  initData: () => {
+    return {
+      plasmTargets: [],
+      boomCounter: 0,
+    };
+  },
   timelineTriggers: [
     {
       // Early Callout for Tank Cleave
       id: 'Ultima EX Homing Lasers',
       regex: /Homing Lasers/,
-      beforeSeconds: 5,
+      beforeSeconds: 4,
       alertText: (_data, _matches, output) => output.text!(),
       outputStrings: {
         text: {
@@ -22,18 +31,94 @@ const triggerSet: TriggerSet<Data> = {
         },
       },
     },
+    {
+      id: 'Ultima EX Vulcan Burst',
+      regex: /Vulcan Burst/,
+      beforeSeconds: 4,
+      infoText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Melee knockback',
+        },
+      },
+    },
   ],
   triggers: [
     {
       id: 'Ultima EX Tank Purge',
+      type: 'StartsUsing',
       netRegex: NetRegexes.startsUsing({ id: '5EA', source: 'The Ultima Weapon', capture: false }),
       response: Responses.bigAoe(),
     },
     {
-      // At 5 stacks of Aetheroplasm, the target begins taking massive damage.
+      // At 5 stacks of Viscous Aetheroplasm, the target begins taking massive damage.
       id: 'Ultima EX Viscous Aetheroplasm',
-      netRegex: NetRegexes.gainsEffect({ effectId: '171', count: '04' }),
-      response: Responses.tankBusterSwap(),
+      type: 'GainsEffect',
+      netRegex: NetRegexes.gainsEffect({ effectId: '171', count: '04', capture: false }),
+      condition: (data) => data.role === 'tank',
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Tank Swap Now--4 Stacks',
+        },
+      },
+    },
+    {
+      id: 'Ultima EX Homing Aetheroplasm Collect',
+      type: 'Ability',
+      netRegex: NetRegexes.ability({ id: '672' }),
+      run: (data, matches) => {
+        data.plasmTargets = data.plasmTargets ??= [];
+        data.plasmTargets.push(matches.target);
+      },
+    },
+    {
+      // This ability is ARR 2.0's version of headmarkers. No associated 27 lines are present.
+      // These lines are sent by entities with no name and no 03/04 lines.
+      id: 'Ultima EX Homing Aetheroplasm Call',
+      type: 'Ability',
+      netRegex: NetRegexes.ability({ id: '672', capture: false }),
+      delaySeconds: 0.5,
+      suppressSeconds: 5,
+      infoText: (data, _matches, output) => {
+        if (data.plasmTargets?.includes(data.me))
+          return output.target!();
+        return output.avoid!();
+      },
+      outputStrings: {
+        target: {
+          en: 'Homing Aetheroplasm on YOU',
+        },
+        avoid: {
+          en: 'Avoid Homing Aetheroplasm',
+        },
+      },
+    },
+    {
+      id: 'Ultima EX Homing Aetheroplasm Cleanup',
+      type: 'Ability',
+      netRegex: NetRegexes.ability({ id: '672', capture: false }),
+      suppressSeconds: 5,
+      run: (data) => delete data.plasmTargets,
+    },
+    {
+      // We use a StartsUsing line here because we can't use timeline triggers for this,
+      // and we want to warn players as early as possible.
+      id: 'Ultima EX Aetheric Boom',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: '5E7', source: 'The Ultima Weapon', capture: false }),
+      alarmText: (data, _matches, output) => output[data.boomCounter]!(),
+      outputStrings: {
+        0: {
+          en: 'Boom Orbs: 1x at cardinals',
+        },
+        1: {
+          en: 'Boom Orbs: 1x N/S, 2x E/W',
+        },
+        2: {
+          en: 'Boom Orbs: 2x at intercardinals',
+        },
+      },
     },
   ],
 };
