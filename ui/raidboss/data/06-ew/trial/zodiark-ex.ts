9c100706542eba4c140a17991f0c0b4e48090052
Akurosia Kamo <kamothegamer@web.de>
2022-04-12 09:07:44 +0200

    i18n: added zodiark-ex translations

--- ui/raidboss/data/06-ew/trial/zodiark-ex.ts
+++ ui/raidboss/data/06-ew/trial/zodiark-ex.ts
@@ -57,12 +57,20 @@ const triggerSet: TriggerSet<Data> = {
       id: 'ZodiarkEx Kokytos',
       type: 'StartsUsing',
       netRegex: NetRegexes.startsUsing({ id: '6C60', source: 'Zodiark', capture: false }),
+      netRegexDe: NetRegexes.startsUsing({ id: '6C60', source: 'Zodiark', capture: false }),
+      netRegexFr: NetRegexes.startsUsing({ id: '6C60', source: 'Zordiarche', capture: false }),
+      netRegexJa: NetRegexes.startsUsing({ id: '6C60', source: 'ゾディアーク', capture: false }),
+      netRegexCn: NetRegexes.startsUsing({ id: '6C60', source: '佐迪亚克', capture: false }),
       response: Responses.bigAoe(),
     },
     {
       id: 'ZodiarkEx Paradeigma',
       type: 'Ability',
       netRegex: NetRegexes.ability({ id: '67BF', source: 'Zodiark', capture: false }),
+      netRegexDe: NetRegexes.ability({ id: '67BF', source: 'Zodiark', capture: false }),
+      netRegexFr: NetRegexes.ability({ id: '67BF', source: 'Zordiarche', capture: false }),
+      netRegexJa: NetRegexes.ability({ id: '67BF', source: 'ゾディアーク', capture: false }),
+      netRegexCn: NetRegexes.ability({ id: '67BF', source: '佐迪亚克', capture: false }),
       alertText: (data, _matches, output) => {
         ++data.paradeigmaCounter;
         if (data.paradeigmaCounter === 1)
@@ -71,6 +79,7 @@ const triggerSet: TriggerSet<Data> = {
       outputStrings: {
         underQuetz: {
           en: 'Under NW Quetzalcoatl',
+          de: 'Unter NW Quetzalcoatl',
         },
       },
     },
@@ -78,10 +87,15 @@ const triggerSet: TriggerSet<Data> = {
       id: 'ZodiarkEx Styx',
       type: 'StartsUsing',
       netRegex: NetRegexes.startsUsing({ id: '67F3', source: 'Zodiark', capture: false }),
+      netRegexDe: NetRegexes.startsUsing({ id: '67F3', source: 'Zodiark', capture: false }),
+      netRegexFr: NetRegexes.startsUsing({ id: '67F3', source: 'Zordiarche', capture: false }),
+      netRegexJa: NetRegexes.startsUsing({ id: '67F3', source: 'ゾディアーク', capture: false }),
+      netRegexCn: NetRegexes.startsUsing({ id: '67F3', source: '佐迪亚克', capture: false }),
       alertText: (_data, _matches, output) => output.text!(),
       outputStrings: {
         text: {
           en: 'Stack x6',
+          de: 'Sammeln x6',
         },
       },
     },
@@ -101,6 +115,10 @@ const triggerSet: TriggerSet<Data> = {
       id: 'ZodiarkEx Blue Cone Tether',
       type: 'Tether',
       netRegex: NetRegexes.tether({ id: '00A4', source: 'Zodiark' }),
+      netRegexDe: NetRegexes.tether({ id: '00A4', source: 'Zodiark' }),
+      netRegexFr: NetRegexes.tether({ id: '00A4', source: 'Zordiarche' }),
+      netRegexJa: NetRegexes.tether({ id: '00A4', source: 'ゾディアーク' }),
+      netRegexCn: NetRegexes.tether({ id: '00A4', source: '佐迪亚克' }),
       promise: async (data, matches) => {
         const portalActors = await fetchCombatantsById([matches.targetId]);
         for (const actor of portalActors) {
@@ -128,15 +146,19 @@ const triggerSet: TriggerSet<Data> = {
       outputStrings: {
         northCone: {
           en: 'North Cone',
+          de: 'Nördliche Kegel-AoE',
         },
         eastCone: {
           en: 'East Cone',
+          de: 'Östliche Kegel-AoE',
         },
         westCone: {
           en: 'West Cone',
+          de: 'Westliche Kegel-AoE',
         },
         southCone: {
           en: 'South Cone',
+          de: 'Südliche Kegel-AoE',
         },
       },
     },
@@ -144,6 +166,10 @@ const triggerSet: TriggerSet<Data> = {
       id: 'ZodiarkEx Red Box Tether',
       type: 'Tether',
       netRegex: NetRegexes.tether({ id: '00AB', source: 'Zodiark' }),
+      netRegexDe: NetRegexes.tether({ id: '00AB', source: 'Zodiark' }),
+      netRegexFr: NetRegexes.tether({ id: '00AB', source: 'Zordiarche' }),
+      netRegexJa: NetRegexes.tether({ id: '00AB', source: 'ゾディアーク' }),
+      netRegexCn: NetRegexes.tether({ id: '00AB', source: '佐迪亚克' }),
       promise: async (data, matches) => {
         const portalActors = await fetchCombatantsById([matches.targetId]);
         for (const actor of portalActors) {
@@ -179,11 +205,16 @@ const triggerSet: TriggerSet<Data> = {
       id: 'ZodiarkEx Roiling Darkness Spawn',
       type: 'AddedCombatant',
       netRegex: NetRegexes.addedCombatant({ name: 'Roiling Darkness', capture: false }),
+      netRegexDe: NetRegexes.addedCombatant({ name: 'Strom Der Dunkelheit', capture: false }),
+      netRegexFr: NetRegexes.addedCombatant({ name: 'Orbe Des Ténèbres', capture: false }),
+      netRegexJa: NetRegexes.addedCombatant({ name: '闇の奔流', capture: false }),
+      netRegexCn: NetRegexes.addedCombatant({ name: '黑暗奔流', capture: false }),
       suppressSeconds: 1,
       alertText: (_data, _matches, output) => output.text!(),
       outputStrings: {
         text: {
           en: Outputs.killAdds.en + '(back first)',
+          de: Outputs.killAdds.de + '(hinten zuerst)',
         },
       },
     },
@@ -191,6 +222,10 @@ const triggerSet: TriggerSet<Data> = {
       id: 'ZodiarkEx Arcane Sigil Start',
       type: 'StartsUsing',
       netRegex: NetRegexes.startsUsing({ id: [sigil.greenBeam, sigil.redBox, sigil.blueCone], source: 'Arcane Sigil' }),
+      netRegexDe: NetRegexes.startsUsing({ id: [sigil.greenBeam, sigil.redBox, sigil.blueCone], source: 'Geheimzeichen' }),
+      netRegexFr: NetRegexes.startsUsing({ id: [sigil.greenBeam, sigil.redBox, sigil.blueCone], source: 'Emblème Secret' }),
+      netRegexJa: NetRegexes.startsUsing({ id: [sigil.greenBeam, sigil.redBox, sigil.blueCone], source: '秘紋' }),
+      netRegexCn: NetRegexes.startsUsing({ id: [sigil.greenBeam, sigil.redBox, sigil.blueCone], source: '秘纹' }),
       run: (data, matches, _output) => {
         if (parseFloat(matches.y) < 100)
           data.activeFrontSigils.push({ x: parseFloat(matches.x), y: parseFloat(matches.y), typeId: matches.id, npcId: matches.sourceId });
@@ -200,6 +235,10 @@ const triggerSet: TriggerSet<Data> = {
       id: 'ZodiarkEx Arcane Sigil',
       type: 'StartsUsing',
       netRegex: NetRegexes.startsUsing({ id: [sigil.greenBeam, sigil.redBox, sigil.blueCone], source: 'Arcane Sigil', capture: false }),
+      netRegexDe: NetRegexes.startsUsing({ id: [sigil.greenBeam, sigil.redBox, sigil.blueCone], source: 'Geheimzeichen', capture: false }),
+      netRegexFr: NetRegexes.startsUsing({ id: [sigil.greenBeam, sigil.redBox, sigil.blueCone], source: 'Emblème Secret', capture: false }),
+      netRegexJa: NetRegexes.startsUsing({ id: [sigil.greenBeam, sigil.redBox, sigil.blueCone], source: '秘紋', capture: false }),
+      netRegexCn: NetRegexes.startsUsing({ id: [sigil.greenBeam, sigil.redBox, sigil.blueCone], source: '秘纹', capture: false }),
       delaySeconds: 0.2,
       suppressSeconds: 0.5,
       alertText: (data, _matches, output) => {
@@ -232,20 +271,25 @@ const triggerSet: TriggerSet<Data> = {
         north: Outputs.north,
         frontsides: {
           en: 'front sides',
+          de: 'Vorne Seiten',
         },
         backmiddle: {
           en: 'back middle',
+          de: 'Hinten Mitte',
         },
         frontmiddle: {
           en: 'front middle',
+          de: 'Vorne Mitte',
         },
         sides: {
           // Specify "for laser" to disambiguate with the astral eclipse going on at the same time.
           // Similarly, there's a algodon knockback call too.
           en: 'sides (for laser)',
+          de: 'Seiten (für die Laser)',
         },
         middle: {
           en: 'middle (for laser)',
+          de: 'Mitte (für die Laser)',
         },
       },
     },
@@ -254,6 +298,10 @@ const triggerSet: TriggerSet<Data> = {
       id: 'ZodiarkEx Algedon',
       type: 'StartsUsing',
       netRegex: NetRegexes.startsUsing({ id: ['67EC', '67ED'], source: 'Zodiark' }),
+      netRegexDe: NetRegexes.startsUsing({ id: ['67EC', '67ED'], source: 'Zodiark' }),
+      netRegexFr: NetRegexes.startsUsing({ id: ['67EC', '67ED'], source: 'Zordiarche' }),
+      netRegexJa: NetRegexes.startsUsing({ id: ['67EC', '67ED'], source: 'ゾディアーク' }),
+      netRegexCn: NetRegexes.startsUsing({ id: ['67EC', '67ED'], source: '佐迪亚克' }),
       alertText: (_data, matches, output) => {
         if (matches.id === '67EC') {
           // NE/SW
@@ -271,6 +319,7 @@ const triggerSet: TriggerSet<Data> = {
         southwest: Outputs.dirSW,
         combo: {
           en: 'Go ${first} / ${second} (knockback)',
+          de: 'Geh ${first} / ${second} (Rückstoß)',
         },
       },
     },
@@ -278,6 +327,10 @@ const triggerSet: TriggerSet<Data> = {
       id: 'ZodiarkEx Adikia',
       type: 'StartsUsing',
       netRegex: NetRegexes.startsUsing({ id: '63A9', source: 'Zodiark', capture: false }),
+      netRegexDe: NetRegexes.startsUsing({ id: '63A9', source: 'Zodiark', capture: false }),
+      netRegexFr: NetRegexes.startsUsing({ id: '63A9', source: 'Zordiarche', capture: false }),
+      netRegexJa: NetRegexes.startsUsing({ id: '63A9', source: 'ゾディアーク', capture: false }),
+      netRegexCn: NetRegexes.startsUsing({ id: '63A9', source: '佐迪亚克', capture: false }),
       alertText: (data, _matches, output) => {
         return data.seenAdikia ? output.adikia2!() : output.adikia1!();
       },
@@ -285,9 +338,11 @@ const triggerSet: TriggerSet<Data> = {
       outputStrings: {
         adikia1: {
           en: 'Double fists (look for pythons)',
+          de: 'Doppel-Fäuste (halt Ausschau nach den Pythons)',
         },
         adikia2: {
           en: 'Double fists',
+          de: 'Doppel-Fäuste',
         },
       },
     },
@@ -295,10 +350,15 @@ const triggerSet: TriggerSet<Data> = {
       id: 'ZodiarkEx Phobos',
       type: 'StartsUsing',
       netRegex: NetRegexes.startsUsing({ id: '67F0', source: 'Zodiark', capture: false }),
+      netRegexDe: NetRegexes.startsUsing({ id: '67F0', source: 'Zodiark', capture: false }),
+      netRegexFr: NetRegexes.startsUsing({ id: '67F0', source: 'Zordiarche', capture: false }),
+      netRegexJa: NetRegexes.startsUsing({ id: '67F0', source: 'ゾディアーク', capture: false }),
+      netRegexCn: NetRegexes.startsUsing({ id: '67F0', source: '佐迪亚克', capture: false }),
       infoText: (_data, _matches, output) => output.text!(),
       outputStrings: {
         text: {
           en: 'Heavy DoT',
+          de: 'Starker DoT',
         },
       },
     },
