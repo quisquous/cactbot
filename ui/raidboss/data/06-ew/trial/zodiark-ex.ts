24c0fa977453d3348a86adc2abbb5d50daa42a3a
Adrienne Walker <enne@quisquo.us>
2022-04-11 23:38:53 -0700

    raidboss: zodiark ex (#4247)
    This is #3933, rebased, and with a few minor fixes from running it on top.
    
    Closes #3933.
    
    Co-authored-by: amki <amki@amki.eu>

--- ui/raidboss/data/06-ew/trial/zodiark-ex.ts
+++ ui/raidboss/data/06-ew/trial/zodiark-ex.ts
@@ -1,14 +1,47 @@
 import NetRegexes from '../../../../../resources/netregexes';
+import Outputs from '../../../../../resources/outputs';
+import { callOverlayHandler } from '../../../../../resources/overlay_plugin_api';
 import { Responses } from '../../../../../resources/responses';
 import ZoneId from '../../../../../resources/zone_id';
 import { RaidbossData } from '../../../../../types/data';
 import { TriggerSet } from '../../../../../types/trigger';
 
-export type Data = RaidbossData;
+// TODO: The second middle/sides laser after Astral Eclipse should be
+// called only after the first goes off.
+
+export interface Data extends RaidbossData {
+  activeSigils: { x: number; y: number; typeId: string; npcId: string }[];
+  activeFrontSigils: { x: number; y: number; typeId: string; npcId: string }[];
+  paradeigmaCounter: number;
+  seenAdikia: boolean;
+}
+
+const sigil = {
+  greenBeam: '67E4',
+  redBox: '67E5',
+  blueCone: '67E6',
+} as const;
+
+const fetchCombatantsById = async (id: string[]) => {
+  const decIds = [];
+  for (const i of id)
+    decIds.push(parseInt(i, 16));
+  const callData = await callOverlayHandler({
+    call: 'getCombatants',
+    ids: decIds,
+  });
+  return callData.combatants;
+};
 
 const triggerSet: TriggerSet<Data> = {
   zoneId: ZoneId.TheMinstrelsBalladZodiarksFall,
   timelineFile: 'zodiark-ex.txt',
+  initData: () => ({
+    activeSigils: [],
+    activeFrontSigils: [],
+    paradeigmaCounter: 0,
+    seenAdikia: false,
+  }),
   triggers: [
     {
       id: 'ZodiarkEx Ania',
@@ -18,7 +51,256 @@ const triggerSet: TriggerSet<Data> = {
       netRegexFr: NetRegexes.startsUsing({ id: '67EF', source: 'Zordiarche' }),
       netRegexJa: NetRegexes.startsUsing({ id: '67EF', source: 'ゾディアーク' }),
       netRegexCn: NetRegexes.startsUsing({ id: '67EF', source: '佐迪亚克' }),
-      response: Responses.tankBuster(),
+      response: Responses.tankBusterSwap(),
+    },
+    {
+      id: 'ZodiarkEx Kokytos',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: '6C60', source: 'Zodiark', capture: false }),
+      response: Responses.bigAoe(),
+    },
+    {
+      id: 'ZodiarkEx Paradeigma',
+      type: 'Ability',
+      netRegex: NetRegexes.ability({ id: '67BF', source: 'Zodiark', capture: false }),
+      alertText: (data, _matches, output) => {
+        ++data.paradeigmaCounter;
+        if (data.paradeigmaCounter === 1)
+          return output.underQuetz!();
+      },
+      outputStrings: {
+        underQuetz: {
+          en: 'Under NW Quetzalcoatl',
+        },
+      },
+    },
+    {
+      id: 'ZodiarkEx Styx',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: '67F3', source: 'Zodiark', capture: false }),
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Stack x6',
+        },
+      },
+    },
+    {
+      id: 'ZodiarkEx Arcane Sigil End',
+      type: 'Ability',
+      netRegex: NetRegexes.ability({ id: [sigil.greenBeam, sigil.redBox, sigil.blueCone], source: 'Arcane Sigil' }),
+      run: (data, matches, _output) => {
+        for (let i = 0; i < data.activeSigils.length; ++i) {
+          const sig = data.activeSigils[i];
+          if (sig?.npcId === matches.sourceId)
+            data.activeSigils.splice(i, 1);
+        }
+      },
+    },
+    {
+      id: 'ZodiarkEx Blue Cone Tether',
+      type: 'Tether',
+      netRegex: NetRegexes.tether({ id: '00A4', source: 'Zodiark' }),
+      promise: async (data, matches) => {
+        const portalActors = await fetchCombatantsById([matches.targetId]);
+        for (const actor of portalActors) {
+          if (actor.ID)
+            data.activeSigils.push({ x: actor.PosX, y: actor.PosY, typeId: sigil.blueCone, npcId: actor.ID.toString(16).toUpperCase() });
+        }
+      },
+      alertText: (data, matches, output) => {
+        const target = data.activeSigils[data.activeSigils.length - 1];
+        if (!target) {
+          console.error(`Blue Tether: Missing target, ${JSON.stringify(matches)}`);
+          return;
+        }
+
+        if (target.x < 100)
+          return output.westCone!();
+
+        if (target.x > 100)
+          return output.eastCone!();
+
+        if (target.y < 100)
+          return output.northCone!();
+        return output.southCone!();
+      },
+      outputStrings: {
+        northCone: {
+          en: 'North Cone',
+        },
+        eastCone: {
+          en: 'East Cone',
+        },
+        westCone: {
+          en: 'West Cone',
+        },
+        southCone: {
+          en: 'South Cone',
+        },
+      },
+    },
+    {
+      id: 'ZodiarkEx Red Box Tether',
+      type: 'Tether',
+      netRegex: NetRegexes.tether({ id: '00AB', source: 'Zodiark' }),
+      promise: async (data, matches) => {
+        const portalActors = await fetchCombatantsById([matches.targetId]);
+        for (const actor of portalActors) {
+          if (actor.ID)
+            data.activeSigils.push({ x: actor.PosX, y: actor.PosY, typeId: sigil.redBox, npcId: actor.ID.toString(16).toUpperCase() });
+        }
+      },
+      alertText: (data, matches, output) => {
+        const target = data.activeSigils[data.activeSigils.length - 1];
+        if (!target) {
+          console.error(`Red Tether: Missing target, ${JSON.stringify(matches)}`);
+          return;
+        }
+
+        if (target.x < 100)
+          return output.east!();
+
+        if (target.x > 100)
+          return output.west!();
+
+        if (target.y < 100)
+          return output.south!();
+        return output.north!();
+      },
+      outputStrings: {
+        north: Outputs.north,
+        west: Outputs.west,
+        south: Outputs.south,
+        east: Outputs.east,
+      },
+    },
+    {
+      id: 'ZodiarkEx Roiling Darkness Spawn',
+      type: 'AddedCombatant',
+      netRegex: NetRegexes.addedCombatant({ name: 'Roiling Darkness', capture: false }),
+      suppressSeconds: 1,
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: Outputs.killAdds.en + '(back first)',
+        },
+      },
+    },
+    {
+      id: 'ZodiarkEx Arcane Sigil Start',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: [sigil.greenBeam, sigil.redBox, sigil.blueCone], source: 'Arcane Sigil' }),
+      run: (data, matches, _output) => {
+        if (parseFloat(matches.y) < 100)
+          data.activeFrontSigils.push({ x: parseFloat(matches.x), y: parseFloat(matches.y), typeId: matches.id, npcId: matches.sourceId });
+      },
+    },
+    {
+      id: 'ZodiarkEx Arcane Sigil',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: [sigil.greenBeam, sigil.redBox, sigil.blueCone], source: 'Arcane Sigil', capture: false }),
+      delaySeconds: 0.2,
+      suppressSeconds: 0.5,
+      alertText: (data, _matches, output) => {
+        const activeFrontSigils = data.activeFrontSigils;
+        data.activeFrontSigils = [];
+        if (activeFrontSigils.length === 1 && activeFrontSigils[0]?.typeId === sigil.greenBeam)
+          return output.sides!();
+        if (activeFrontSigils.length === 1 && activeFrontSigils[0]?.typeId === sigil.redBox)
+          return output.south!();
+        if (activeFrontSigils.length === 1 && activeFrontSigils[0]?.typeId === sigil.blueCone)
+          return output.north!();
+        if (activeFrontSigils.length === 2 && activeFrontSigils[0]?.typeId === sigil.greenBeam && activeFrontSigils[1]?.typeId === sigil.greenBeam)
+          return output.middle!();
+        if (activeFrontSigils.length === 3) {
+          for (const sig of activeFrontSigils) {
+            // Find the middle sigil
+            if (sig.x > 90 && sig.x < 110) {
+              if (sig.typeId === sigil.greenBeam)
+                return output.frontsides!();
+              if (sig.typeId === sigil.redBox)
+                return output.backmiddle!();
+              if (sig.typeId === sigil.blueCone)
+                return output.frontmiddle!();
+            }
+          }
+        }
+      },
+      outputStrings: {
+        south: Outputs.south,
+        north: Outputs.north,
+        frontsides: {
+          en: 'front sides',
+        },
+        backmiddle: {
+          en: 'back middle',
+        },
+        frontmiddle: {
+          en: 'front middle',
+        },
+        sides: {
+          // Specify "for laser" to disambiguate with the astral eclipse going on at the same time.
+          // Similarly, there's a algodon knockback call too.
+          en: 'sides (for laser)',
+        },
+        middle: {
+          en: 'middle (for laser)',
+        },
+      },
+    },
+    {
+      // 67EC is leaning left, 67ED is leaning right
+      id: 'ZodiarkEx Algedon',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: ['67EC', '67ED'], source: 'Zodiark' }),
+      alertText: (_data, matches, output) => {
+        if (matches.id === '67EC') {
+          // NE/SW
+          return output.combo!({ first: output.northeast!(), second: output.southwest!() });
+        }
+        if (matches.id === '67ED') {
+          // NW/SE
+          return output.combo!({ first: output.northwest!(), second: output.southeast!() });
+        }
+      },
+      outputStrings: {
+        northeast: Outputs.dirNE,
+        northwest: Outputs.dirNW,
+        southeast: Outputs.dirSE,
+        southwest: Outputs.dirSW,
+        combo: {
+          en: 'Go ${first} / ${second} (knockback)',
+        },
+      },
+    },
+    {
+      id: 'ZodiarkEx Adikia',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: '63A9', source: 'Zodiark', capture: false }),
+      alertText: (data, _matches, output) => {
+        return data.seenAdikia ? output.adikia2!() : output.adikia1!();
+      },
+      run: (data) => data.seenAdikia = true,
+      outputStrings: {
+        adikia1: {
+          en: 'Double fists (look for pythons)',
+        },
+        adikia2: {
+          en: 'Double fists',
+        },
+      },
+    },
+    {
+      id: 'ZodiarkEx Phobos',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: '67F0', source: 'Zodiark', capture: false }),
+      infoText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Heavy DoT',
+        },
+      },
     },
   ],
   timelineReplace: [
