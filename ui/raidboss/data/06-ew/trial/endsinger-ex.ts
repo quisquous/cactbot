b89a70ff5910bd89e52e0e8145acc0bb32ac8d94
valarnin <valarnin@gmail.com>
2022-04-13 11:49:17 -0400

    raidboss: Add Endsinger Ex timeline (#4259)

---
+++ ui/raidboss/data/06-ew/trial/endsinger-ex.ts
@@ -0,0 +1,25 @@
+import ZoneId from '../../../../../resources/zone_id';
+import { RaidbossData } from '../../../../../types/data';
+import { PluginCombatantState } from '../../../../../types/event';
+import { TriggerSet } from '../../../../../types/trigger';
+
+export interface Data extends RaidbossData {
+  storedStars: { [name: string]: PluginCombatantState };
+  phase: 1 | 2;
+  storedBoss?: PluginCombatantState;
+}
+
+const triggerSet: TriggerSet<Data> = {
+  zoneId: ZoneId.TheMinstrelsBalladEndsingersAria,
+  timelineFile: 'endsinger-ex.txt',
+  initData: () => {
+    return {
+      storedStars: {},
+      phase: 1,
+    };
+  },
+  triggers: [],
+  timelineReplace: [],
+};
+
+export default triggerSet;
