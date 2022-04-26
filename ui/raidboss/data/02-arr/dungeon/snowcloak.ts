e81f82bc7d8b1f55d464ad4c7d7fa129eaf3c4d0
Aho Senpai <52920533+Aho-Senpai@users.noreply.github.com>
2022-04-27 10:21:37 +1100

    capture false fixes

--- ui/raidboss/data/02-arr/dungeon/snowcloak.ts
+++ ui/raidboss/data/02-arr/dungeon/snowcloak.ts
@@ -13,10 +13,7 @@ const triggerSet: TriggerSet<Data> = {
     {
       id: 'Snowcloak Lunar Cry',
       type: 'StartsUsing',
-      netRegex: NetRegexes.startsUsing({ id: 'C1F', source: 'Fenrir'}),
-      netRegexDe: NetRegexes.startsUsing({ id: 'C1F'}),
-      netRegexFr: NetRegexes.startsUsing({ id: 'C1F'}),
-      netRegexJa: NetRegexes.startsUsing({ id: 'C1F'}),
+      netRegex: NetRegexes.startsUsing({ id: 'C1F', source: 'Fenrir' , capture: false }),
       alertText: (_data, _matches, output) => output.text!(),
       outputStrings: {
         text: {
