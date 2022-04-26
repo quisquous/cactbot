aa60bd80fa5dca2e8efd7b23533ca0594919f620
Adrienne Walker <enne@quisquo.us>
2022-04-26 08:59:21 -0700

    raidboss: add blank files for ultimate

---
+++ ui/raidboss/data/06-ew/ultimate/dragonsongs_reprise_ultimate.ts
@@ -0,0 +1,35 @@
+import ZoneId from '../../../../../resources/zone_id';
+import { RaidbossData } from '../../../../../types/data';
+import { NetMatches } from '../../../../../types/net_matches';
+import { TriggerSet } from '../../../../../types/trigger';
+
+export interface Data extends RaidbossData {
+  decOffset?: number;
+}
+
+// TODO
+const firstDecimalMarker = 0;
+
+// Due to changes introduced in patch 5.2, overhead markers now have a random offset
+// added to their ID. This offset currently appears to be set per instance, so
+// we can determine what it is from the first overhead marker we see.
+const getHeadmarkerId = (data: Data, matches: NetMatches['HeadMarker']) => {
+  // If we naively just check !data.decOffset and leave it, it breaks if the first marker is 00DA.
+  // (This makes the offset 0, and !0 is true.)
+  if (typeof data.decOffset === 'undefined')
+    data.decOffset = parseInt(matches.id, 16) - firstDecimalMarker;
+  // The leading zeroes are stripped when converting back to string, so we re-add them here.
+  // Fortunately, we don't have to worry about whether or not this is robust,
+  // since we know all the IDs that will be present in the encounter.
+  return (parseInt(matches.id, 16) - data.decOffset).toString(16).toUpperCase().padStart(4, '0');
+};
+// TODO remove this when used.
+console.assert(getHeadmarkerId);
+
+const triggerSet: TriggerSet<Data> = {
+  zoneId: ZoneId.DragonsongsRepriseUltimate,
+  timelineFile: 'dragonsongs_reprise_ultimate.txt',
+  triggers: [],
+};
+
+export default triggerSet;
