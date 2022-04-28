8bd7ae14c6d5e0566894e420e682d3e39003fbe2
Akurosia Kamo <kamothegamer@web.de>
2022-04-28 10:09:33 +0200

    i18n: severall new translations

--- ui/raidboss/data/02-arr/dungeon/the_sunken_temple_of_quarn.ts
+++ ui/raidboss/data/02-arr/dungeon/the_sunken_temple_of_quarn.ts
@@ -18,6 +18,7 @@ const triggerSet: TriggerSet<Data> = {
       outputStrings: {
         text: {
           en: 'Step on Glowing Plate',
+          de: 'Auf der leuchtende Platte stehen',
           fr: 'Marchez sur la plaque qui brille',
         },
       },
