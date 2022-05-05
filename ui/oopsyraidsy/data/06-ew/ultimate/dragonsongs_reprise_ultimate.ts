3214f1c07d18d087d61bd9c62d2befb6a42d5dfc
Adrienne Walker <enne@quisquo.us>
2022-05-05 09:54:09 -0700

    oopsy: more DSR Thordan/Nidhogg mistakes (#4391)

--- ui/oopsyraidsy/data/06-ew/ultimate/dragonsongs_reprise_ultimate.ts
+++ ui/oopsyraidsy/data/06-ew/ultimate/dragonsongs_reprise_ultimate.ts
@@ -40,15 +40,27 @@ const triggerSet: OopsyTriggerSet<Data> = {
     'DSR Ser Eternal Conviction': '63E0', // messing up towers
     'DSR Holy Comet Holy Impact': '63EA', // meteor explosion from being too close
     'DSR King Thordan Ascalon\'s Mercy Concealed': '63C9', // protean 2nd hit
+    'DSR Nidhogg Darkdragon Dive Miss': '671B', // tower failure
+  },
+  gainsEffectFail: {
+    'DSR Burns': 'B81', // fire puddles during Sanctity of the Ward
+    'DSR Frostbite': 'B82', // ice puddles during Sanctity of the Ward
   },
   shareWarn: {
     'DSR Ser Adelphel Execution': '62D5', // dive on main tank after 62CE Shining Blade
     'DSR Ser Charibert Heavensflame': '62DF', // spread with 62E0 Holy Chain
     'DSR Ser Charibert Brightwing': '6319', // cleaves during Pure of Heart
+    'DSR King Thordan Attack': '63BB', // King Thordan cleave autos
     'DSR King Thordan Lightning Storm': '63CD', // spread during Strength of the Ward
+    'DSR Nidhogg Attack': '6730', // Nidhogg cleave autos
+    'DSR Nidhogg Dark High Jump': '670E', // sharing a circle marker tower drop
+    'DSR Nidhogg Dark Spineshatter Dive': '670F', // sharing an up/forwards arrow marker tower drop
+    'DSR Nidhogg Dark Elusive Jump': '6710', // sharing a down/back arrow marker tower drop
+    'DSR Nidhogg Darkdragon Dive Share': '6711', // sharing a single tower
   },
   shareFail: {
     'DSR King Thordan Ascalon\'s Might': '63C5', // tank cleaves
+    'DSR Holy Shield Bash': '62D1', // Ser Adelphel/Janlenoux tank tether stun charges
     'DSR Nidhogg Soul Tether': '671C', // tank tether cleaves
   },
   soloWarn: {
