56e4489816d0947a8af3bbc665d6456f5af4aa5f
Akurosia Kamo <kamothegamer@web.de>
2022-05-07 21:35:31 +0200

    some de and cn syn updates

--- ui/raidboss/data/04-sb/trial/shinryu-ex.ts
+++ ui/raidboss/data/04-sb/trial/shinryu-ex.ts
@@ -854,6 +854,7 @@ const triggerSet: TriggerSet<Data> = {
         'Shinryu': '神龙',
         'Tail': '龙尾',
         'The Worm\'s Heart': '神龙的核心',
+        'Hakkinryu': '白金龙',
       },
       'replaceText': {
         'Aerial Blast': '大气爆发',
