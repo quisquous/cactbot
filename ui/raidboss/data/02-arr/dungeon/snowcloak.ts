244c9a50e526ba631e6bf2c0cf6e081a603544a1
Aho Senpai <52920533+Aho-Senpai@users.noreply.github.com>
2022-05-11 04:24:03 +1100

    raidboss: ARR Dungeons Triggers Normal (#4349)

---
+++ ui/raidboss/data/02-arr/dungeon/snowcloak.ts
@@ -0,0 +1,26 @@
+import NetRegexes from '../../../../../resources/netregexes';
+import ZoneId from '../../../../../resources/zone_id';
+import { RaidbossData } from '../../../../../types/data';
+import { TriggerSet } from '../../../../../types/trigger';
+
+export type Data = RaidbossData;
+
+const triggerSet: TriggerSet<Data> = {
+  zoneId: ZoneId.Snowcloak,
+  triggers: [
+    {
+      id: 'Snowcloak Lunar Cry',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: 'C1F', source: 'Fenrir', capture: false }),
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Hide behind Ice',
+          fr: 'Cachez vous derrière un pilier de glace',
+        },
+      },
+    },
+  ],
+};
+
+export default triggerSet;
