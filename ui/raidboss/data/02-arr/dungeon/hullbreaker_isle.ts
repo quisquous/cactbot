b52b4d5c9d4be732b9434f1e39650f0f66cfef2c
Akurosia Kamo <kamothegamer@web.de>
2022-05-10 21:29:49 +0200

    i18n: arr translations

--- ui/raidboss/data/02-arr/dungeon/hullbreaker_isle.ts
+++ ui/raidboss/data/02-arr/dungeon/hullbreaker_isle.ts
@@ -13,18 +13,61 @@ const triggerSet: TriggerSet<Data> = {
       id: 'Hullbreaker Isle Stool Pelt',
       type: 'StartsUsing',
       netRegex: NetRegexes.startsUsing({ id: '89E', source: 'Sasquatch', capture: false }),
+      netRegexDe: NetRegexes.startsUsing({ id: '89E', source: 'Sasquatch', capture: false }),
+      netRegexFr: NetRegexes.startsUsing({ id: '89E', source: 'Sasquatch', capture: false }),
+      netRegexJa: NetRegexes.startsUsing({ id: '89E', source: 'サスカッチ', capture: false }),
+      netRegexCn: NetRegexes.startsUsing({ id: '89E', source: '大脚巨猿', capture: false }),
+      netRegexKo: NetRegexes.startsUsing({ id: '89E', source: '사스콰치', capture: false }),
       response: Responses.aoe(),
     },
     {
       id: 'Hullbreaker Isle Chest Thump',
       type: 'Ability',
       netRegex: NetRegexes.ability({ id: '89F', source: 'Sasquatch', capture: false }),
+      netRegexDe: NetRegexes.ability({ id: '89F', source: 'Sasquatch', capture: false }),
+      netRegexFr: NetRegexes.ability({ id: '89F', source: 'Sasquatch', capture: false }),
+      netRegexJa: NetRegexes.ability({ id: '89F', source: 'サスカッチ', capture: false }),
+      netRegexCn: NetRegexes.ability({ id: '89F', source: '大脚巨猿', capture: false }),
+      netRegexKo: NetRegexes.ability({ id: '89F', source: '사스콰치', capture: false }),
       alertText: (_data, _matches, output) => output.text!(),
       outputStrings: {
         text: {
           en: 'Shake Banana tree',
+          de: 'Bananenbaum schütteln',
           fr: 'Secouez le bananier',
         },
+      },
+    },
+  ],
+  timelineReplace: [
+    {
+      'locale': 'de',
+      'replaceSync': {
+        'Sasquatch': 'Sasquatch',
+      },
+    },
+    {
+      'locale': 'fr',
+      'replaceSync': {
+        'Sasquatch': 'Sasquatch',
+      },
+    },
+    {
+      'locale': 'ja',
+      'replaceSync': {
+        'Sasquatch': 'サスカッチ',
+      },
+    },
+    {
+      'locale': 'cn',
+      'replaceSync': {
+        'Sasquatch': '大脚巨猿',
+      },
+    },
+    {
+      'locale': 'ko',
+      'replaceSync': {
+        'Sasquatch': '사스콰치',
       },
     },
   ],
