quisquous/cactbot

commit e2409f8cf85b258a62d8cecbaee0fe75af822aa6

lint/raidboss: Clean up conditionals in raidboss (#4449)

ui/raidboss/popup-text.ts
@@ -337,7 +337,7 @@ class TriggerOutputProxy {
     params: TriggerParams,
     name: string,
     id: string): string | undefined {
-    if (!template)
+    if (template === undefined)
       return;
 
     let value: unknown;
@@ -353,7 +353,7 @@ class TriggerOutputProxy {
     }
 
     return value.replace(/\${\s*([^}\s]+)\s*}/g, (_fullMatch: string, key: string) => {
-      if (params && key in params) {
+      if (key in params) {
         const str = params[key];
         switch (typeof str) {
           case 'string':
@@ -581,7 +581,7 @@ export class PopupText {
   }
 
   ReloadTimelines(): void {
-    if (!this.triggerSets || !this.me || !this.zoneName || !this.timelineLoader.IsReady())
+    if (!this.me || !this.zoneName || !this.timelineLoader.IsReady())
       return;
 
     // Drop the triggers and timelines from the previous zone, so we can add new ones.
@@ -633,7 +633,7 @@ export class PopupText {
         continue;
       }
 
-      if (set.zoneId) {
+      if (set.zoneId !== undefined) {
         if (set.zoneId !== ZoneId.MatchAll && set.zoneId !== this.zoneId && !(typeof set.zoneId === 'object' && set.zoneId.includes(this.zoneId)))
           continue;
       } else if (set.zoneRegex) {
@@ -758,7 +758,7 @@ export class PopupText {
         }
       }
 
-      if (set.timeline)
+      if (set.timeline !== undefined)
         addTimeline(set.timeline);
       if (set.timelineReplace)
         replacements.push(...set.timelineReplace);
@@ -1091,51 +1091,50 @@ export class PopupText {
 
   _onTriggerInternalCondition(triggerHelper: TriggerHelper): boolean {
     const condition = triggerHelper.triggerOptions.Condition ?? triggerHelper.trigger.condition;
-    if (condition) {
-      if (condition === true)
-        return true;
-      if (!condition(this.data, triggerHelper.matches, triggerHelper.output))
-        return false;
-    }
-    return true;
+    // If the condition is missing or hardcoded as `true`
+    if (condition === undefined || condition === true)
+      return true;
+    // If the condition is hardcoded as `false`
+    else if (condition === false)
+      return false;
+
+    const conditionFuncReturn = condition(this.data, triggerHelper.matches, triggerHelper.output);
+    if (conditionFuncReturn === true)
+      return true;
+    // Treat all other return values as false (undefined | false)
+    return false;
   }
 
   // Set defaults for triggerHelper object (anything that won't change based on
   // other trigger functions running)
   _onTriggerInternalHelperDefaults(triggerHelper: TriggerHelper): void {
-    if (triggerHelper.triggerAutoConfig) {
-      const textAlertsEnabled = triggerHelper.triggerAutoConfig.TextAlertsEnabled;
-      if (textAlertsEnabled !== undefined)
-        triggerHelper.textAlertsEnabled = textAlertsEnabled;
-      const soundAlertsEnabled = triggerHelper.triggerAutoConfig.SoundAlertsEnabled;
-      if (soundAlertsEnabled !== undefined)
-        triggerHelper.soundAlertsEnabled = soundAlertsEnabled;
-      const spokenAlertsEnabled = triggerHelper.triggerAutoConfig.SpokenAlertsEnabled;
-      if (spokenAlertsEnabled !== undefined)
-        triggerHelper.spokenAlertsEnabled = spokenAlertsEnabled;
-    }
+    // Load settings from triggerAutoConfig if they're set
+    triggerHelper.textAlertsEnabled =
+      triggerHelper.triggerAutoConfig.TextAlertsEnabled ?? triggerHelper.textAlertsEnabled;
+    triggerHelper.soundAlertsEnabled =
+      triggerHelper.triggerAutoConfig.SoundAlertsEnabled ?? triggerHelper.soundAlertsEnabled;
+    triggerHelper.spokenAlertsEnabled =
+      triggerHelper.triggerAutoConfig.SpokenAlertsEnabled ?? triggerHelper.spokenAlertsEnabled;
 
-    if (triggerHelper.triggerOptions) {
-      const textAlertsEnabled = triggerHelper.triggerOptions.TextAlert;
-      if (textAlertsEnabled !== undefined)
-        triggerHelper.textAlertsEnabled = textAlertsEnabled;
-      const soundAlertsEnabled = triggerHelper.triggerOptions.SoundAlert;
-      if (soundAlertsEnabled !== undefined)
-        triggerHelper.soundAlertsEnabled = soundAlertsEnabled;
-      const spokenAlertsEnabled = triggerHelper.triggerOptions.SpeechAlert;
-      if (spokenAlertsEnabled !== undefined)
-        triggerHelper.spokenAlertsEnabled = spokenAlertsEnabled;
-      const groupSpokenAlertsEnabled = triggerHelper.triggerOptions.GroupSpeechAlert;
-      if (groupSpokenAlertsEnabled !== undefined)
-        triggerHelper.groupSpokenAlertsEnabled = groupSpokenAlertsEnabled;
-    }
+    // Load settings from triggerOptions if they're set
+    triggerHelper.textAlertsEnabled =
+      triggerHelper.triggerOptions.TextAlert ?? triggerHelper.textAlertsEnabled;
+    triggerHelper.soundAlertsEnabled =
+      triggerHelper.triggerOptions.SoundAlert ?? triggerHelper.soundAlertsEnabled;
+    triggerHelper.spokenAlertsEnabled =
+      triggerHelper.triggerOptions.SpeechAlert ?? triggerHelper.spokenAlertsEnabled;
+    triggerHelper.groupSpokenAlertsEnabled =
+      triggerHelper.triggerOptions.GroupSpeechAlert ?? triggerHelper.groupSpokenAlertsEnabled;
 
+    // If the user has suppressed all output for the trigger, reflect that here
     if (triggerHelper.userSuppressedOutput) {
       triggerHelper.textAlertsEnabled = false;
       triggerHelper.soundAlertsEnabled = false;
       triggerHelper.spokenAlertsEnabled = false;
       triggerHelper.groupSpokenAlertsEnabled = false;
     }
+
+    // If the user has disabled audio output, reflect that here
     if (!this.options.AudioAllowed) {
       triggerHelper.soundAlertsEnabled = false;
       triggerHelper.spokenAlertsEnabled = false;
@@ -1152,7 +1151,7 @@ export class PopupText {
 
   _onTriggerInternalDelaySeconds(triggerHelper: TriggerHelper): Promise<void> | undefined {
     const delay = 'delaySeconds' in triggerHelper.trigger ? triggerHelper.valueOrFunction(triggerHelper.trigger.delaySeconds) : 0;
-    if (!delay || delay <= 0 || typeof delay !== 'number')
+    if (delay === undefined || delay === null || delay <= 0 || typeof delay !== 'number')
       return;
 
     const triggerID = this.currentTriggerID++;
@@ -1161,7 +1160,7 @@ export class PopupText {
       window.setTimeout(() => {
         if (this.timers[triggerID])
           res();
-        else if (rej)
+        else
           rej();
         delete this.timers[triggerID];
       }, delay * 1000);
@@ -1280,20 +1279,21 @@ export class PopupText {
   _onTriggerInternalTTS(triggerHelper: TriggerHelper): void {
     if (!triggerHelper.groupSpokenAlertsEnabled || typeof triggerHelper.ttsText === 'undefined') {
       let result = undefined;
-      if (triggerHelper.triggerOptions.TTSText) {
+      if (triggerHelper.triggerOptions.TTSText !== undefined) {
         result = triggerHelper.valueOrFunction(triggerHelper.triggerOptions.TTSText);
       } else if (triggerHelper.trigger.tts !== undefined) {
         // Allow null/false/NaN/0/'' in this branch.
         result = triggerHelper.valueOrFunction(triggerHelper.trigger.tts);
       } else if (triggerHelper.response) {
         const resp: ResponseField<RaidbossData, Matches> = triggerHelper.response;
-        if (resp.tts)
+        if (resp.tts !== undefined)
           result = triggerHelper.valueOrFunction(resp.tts);
       }
 
       // Allow falsey values to disable tts entirely
       // Undefined will fall back to defaultTTSText
       if (result !== undefined) {
+        // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
         if (result)
           triggerHelper.ttsText = result?.toString();
       } else {
@@ -1303,7 +1303,7 @@ export class PopupText {
   }
 
   _onTriggerInternalPlayAudio(triggerHelper: TriggerHelper): void {
-    if (triggerHelper.trigger.sound &&
+    if (triggerHelper.trigger.sound !== undefined &&
         triggerHelper.soundUrl &&
         soundStrs.includes(triggerHelper.soundUrl)) {
       const namedSound = triggerHelper.soundUrl + 'Sound';
@@ -1406,14 +1406,14 @@ export class PopupText {
 
     let textObj: RaidbossTriggerOutput =
       triggerHelper.triggerOptions[upperTextKey];
-    if (!textObj && triggerHelper.trigger[lowerTextKey])
+    if (textObj === undefined && triggerHelper.trigger[lowerTextKey] !== undefined)
       textObj = triggerHelper.trigger[lowerTextKey];
-    if (!textObj && triggerHelper.response)
+    if (textObj === undefined && triggerHelper.response !== undefined)
       textObj = triggerHelper.response[lowerTextKey];
-    if (!textObj)
+    if (textObj === undefined || textObj === null)
       return;
     let text = triggerHelper.valueOrFunction(textObj);
-    if (!text)
+    if (text === undefined || text === null)
       return;
     if (typeof text === 'number')
       text = text.toString();
@@ -1462,7 +1462,7 @@ export class PopupText {
 
   getDataObject(): RaidbossData {
     let preserveHP = 0;
-    if (this.data && this.data.currentHP)
+    if (this.data.currentHP)
       preserveHP = this.data.currentHP;
 
     // TODO: make a breaking change at some point and

ui/raidboss/raidboss_config.ts
@@ -589,7 +589,7 @@ class RaidbossConfigurator {
       // Timeline editing is tied to a single, specific zoneId per file for now.
       // We could add more indirection (via fileKey?) and look up zoneId -> fileKey[]
       // and fileKey -> timeline edits if needed.
-      if (info.triggerSet.timeline && typeof info.zoneId === 'number')
+      if (info.triggerSet.timeline !== undefined && typeof info.zoneId === 'number')
         this.buildTimelineUIContainer(info.zoneId, info.triggerSet, triggerContainer, userOptions);
 
       const triggerOptions = document.createElement('div');
@@ -651,7 +651,7 @@ class RaidbossConfigurator {
             continue;
           const trigOutput = trig.configOutput?.[detailKey];
           const trigFunc = trig[detailKey];
-          if (!trigFunc)
+          if (trigFunc === undefined || trigFunc === null)
             continue;
 
           const detailCls = [opt.cls];
@@ -1195,7 +1195,7 @@ class RaidbossConfigurator {
     const evalTrigger = (trig: LooseTrigger, key: OutputKey, data: RaidbossData) => {
       try {
         const result = this.valueOrFunction(trig[key], data, kFakeMatches, fakeOutputProxy);
-        if (!result)
+        if (result === null || result === undefined)
           return false;
 
         // Super hack:
@@ -1277,8 +1277,6 @@ class RaidbossConfigurator {
       if (!langSpecificRegex)
         return;
       const baseRegex = Regexes.parse(langSpecificRegex);
-      if (!baseRegex)
-        return;
       return Regexes.parse(baseRegex);
     };
 
@@ -1440,17 +1438,15 @@ const flattenTimeline = (
 const userFileHandler: UserFileCallback = (
   name: string,
   files: { [filename: string]: string },
-  baseOptions: BaseOptions,
+  baseOptions: (BaseOptions & Partial<RaidbossOptions>),
   basePath: string,
 ) => {
   // TODO: Rewrite user_config to be templated on option type so that this function knows
   // what type of options it is using.
-  const options = baseOptions as RaidbossOptions;
-
-  if (!options.Triggers)
+  if (!baseOptions.Triggers)
     return;
 
-  for (const baseTriggerSet of options.Triggers) {
+  for (const baseTriggerSet of baseOptions.Triggers) {
     const set: ConfigLooseTriggerSet = baseTriggerSet;
 
     // Annotate triggers with where they came from.  Note, options is passed in repeatedly
@@ -1475,7 +1471,7 @@ const processPerTriggerAutoConfig = (options: RaidbossOptions, savedConfig: Save
   if (typeof savedConfig !== 'object' || Array.isArray(savedConfig))
     return;
   const triggers = savedConfig['triggers'];
-  if (!triggers || typeof triggers !== 'object' || Array.isArray(triggers))
+  if (triggers === undefined || typeof triggers !== 'object' || Array.isArray(triggers))
     return;
 
   const outputObjs: { [key: string]: TriggerAutoConfig } = {};
@@ -1519,7 +1515,7 @@ const processPerTriggerAutoConfig = (options: RaidbossOptions, savedConfig: Save
     )
       autoConfig[kOptionKeys.outputStrings] = outputStrings;
 
-    if (output || duration || outputStrings)
+    if (output || duration || outputStrings !== undefined)
       perTriggerAutoConfig[id] = autoConfig;
   }
 };
@@ -1535,7 +1531,7 @@ const processPerZoneTimelineConfig = (options: RaidbossOptions, savedConfig: Sav
   if (typeof savedConfig !== 'object' || Array.isArray(savedConfig))
     return;
   const timeline = savedConfig['timeline'];
-  if (!timeline || typeof timeline !== 'object' || Array.isArray(timeline))
+  if (typeof timeline !== 'object' || Array.isArray(timeline))
     return;
 
   for (const [zoneKey, zoneEntry] of Object.entries(timeline)) {
@@ -1544,28 +1540,28 @@ const processPerZoneTimelineConfig = (options: RaidbossOptions, savedConfig: Sav
       continue;
     const timelineConfig = perZoneTimelineConfig[zoneId] ??= {};
 
-    if (!zoneEntry || typeof zoneEntry !== 'object' || Array.isArray(zoneEntry))
+    if (typeof zoneEntry !== 'object' || Array.isArray(zoneEntry))
       continue;
 
     const enableEntry = zoneEntry['enable'];
     const replaceEntry = zoneEntry['globalReplace'];
     const addEntry = zoneEntry['add'];
 
-    if (enableEntry && typeof enableEntry === 'object' && !Array.isArray(enableEntry)) {
+    if (typeof enableEntry === 'object' && !Array.isArray(enableEntry)) {
       for (const [key, value] of Object.entries(enableEntry)) {
         if (typeof value === 'boolean' && !value)
           (timelineConfig.Ignore ??= []).push(key);
       }
     }
 
-    if (replaceEntry && typeof replaceEntry === 'object' && !Array.isArray(replaceEntry)) {
+    if (typeof replaceEntry === 'object' && !Array.isArray(replaceEntry)) {
       for (const [key, value] of Object.entries(replaceEntry)) {
         if (typeof value === 'string')
           (timelineConfig.Rename ??= {})[key] = value;
       }
     }
 
-    if (addEntry && typeof addEntry === 'object' && Array.isArray(addEntry)) {
+    if (addEntry !== undefined && typeof addEntry === 'object' && Array.isArray(addEntry)) {
       for (const row of addEntry) {
         if (typeof row !== 'object' || Array.isArray(row))
           continue;

ui/raidboss/timeline.ts
@@ -92,7 +92,6 @@ const computeBackgroundColorFrom = (element: HTMLElement, classList: string): st
 };
 
 export class Timeline {
-  private options: RaidbossOptions;
   private replacements: TimelineReplacement[];
 
   private activeText: string;
@@ -122,10 +121,9 @@ export class Timeline {
     replacements: TimelineReplacement[],
     triggers: LooseTimelineTrigger[],
     styles: TimelineStyle[],
-    options: RaidbossOptions,
+    private options: RaidbossOptions,
     private zoneId: number,
   ) {
-    this.options = options || {};
     this.replacements = replacements;
 
     const lang = this.options.TimelineLanguage || this.options.ParserLanguage || 'en';
@@ -526,8 +524,6 @@ export class TimelineUI {
 
     for (let i = 0; i < this.options.MaxNumberOfTimerBars; ++i) {
       const helperBar = document.createElement('div');
-      if (!helperBar)
-        continue;
       helperBar.classList.add('text');
       helperBar.classList.add('resize-helper-bar');
       helperBar.classList.add('timeline-bar-color');

ui/raidboss/timeline_parser.ts
@@ -90,7 +90,7 @@ export class TimelineParser {
     zoneId?: number,
   ) {
     this.options = options ?? defaultOptions;
-    this.perTriggerAutoConfig = this.options['PerTriggerAutoConfig'] || {};
+    this.perTriggerAutoConfig = this.options.PerTriggerAutoConfig;
     this.replacements = replacements;
 
     // A set of names which will not be notified about.
@@ -401,9 +401,6 @@ export class TimelineParser {
   }
 
   private GetReplacedSync(sync: string): string {
-    if (!this.replacements)
-      return sync;
-
     const replaceLang = this.options.ParserLanguage ?? 'en';
     return translateRegex(sync, replaceLang, this.replacements);
   }

commit 94492df4cc31076e3606e0ec45190a2195d608dd

raidboss: fix edited-but-empty strings from being skipped
This was broken in #4449 because I didn't realize that the empty
string check was important behavior, whoops.

If a user edits something in the config ui and then clears it,
then it will leave an empty string in the config json and
output strings overrides.  This needs to be skipped (and could
be cleaned up elsewhere, but this is a small bugfix for now).

ui/raidboss/popup-text.ts
@@ -336,8 +336,13 @@ class TriggerOutputProxy {
     template: { [lang: string]: unknown } | string | undefined,
     params: TriggerParams,
     name: string,
-    id: string): string | undefined {
-    if (template === undefined)
+    id: string,
+  ): string | undefined {
+    // If an output strings entry is edited in the config UI and then blanked,
+    // the entry will still exist in the config file as an empty string.
+    // These should be ignored as not being an override.
+    // TODO: maybe blanked/default entries should be deleted from the config?
+    if (template === undefined || template === '')
       return;
 
     let value: unknown;