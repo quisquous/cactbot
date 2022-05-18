c0006b7f1778a459d531449e38fb8cdf9f6d53fb
Legends0 <nac10fsu@gmail.com>
2022-05-18 01:02:30 -0400

    add another related todo

--- ui/raidboss/data/06-ew/ultimate/dragonsongs_reprise_ultimate.ts
+++ ui/raidboss/data/06-ew/ultimate/dragonsongs_reprise_ultimate.ts
@@ -13,7 +13,8 @@ import { LocaleText, TriggerSet } from '../../../../../types/trigger';
 // TODO: Ser Adelphel left/right movement after initial charge
 // TODO: Meteor "run" call?
 // TODO: Wyrmsbreath 2 cardinal positions for Cauterize and adjust delay
-// TODO: Call safe Intercard instead of left/right for Hallowed Wings and Plumes (requires capturing Cauterize)
+// TODO: Intercard instead of left/right for Hallowed Wings with Cauterize
+// TODO: Trigger for Hallowed Wings with Hot Tail/Hot Wings
 
 type Phase = 'doorboss' | 'thordan' | 'nidhogg' | 'haurchefant' | 'thordan2' | 'nidhogg2' | 'dragon-king';
 
