92505331741735e366b2f8d20bd9a5b4635b940e
Adrienne Walker <enne@quisquo.us>
2022-06-07 23:13:33 -0700

    util: TypeScriptify fake_name_generator (#4570)

--- util/logtools/fake_name_generator.ts
+++ util/logtools/fake_name_generator.ts
@@ -1,3 +1,5 @@
+import { UnreachableCode } from '../../resources/not_reached';
+
 const fakePlayerNames = [
   'Tini Poutini',
   'Potato Chippy',
@@ -7,41 +9,44 @@ const fakePlayerNames = [
   'French Fry',
 ];
 
-function randomFromArray(array) {
-  return array[Math.floor(Math.random() * array.length)];
-}
+const randomFromArray = <T>(array: T[]): T => {
+  const value = array[Math.floor(Math.random() * array.length)];
+  if (value === undefined)
+    throw new UnreachableCode();
+  return value;
+};
 
 export default class FakeNameGenerator {
-  constructor() {
-    this.fakePlayerNames = [...fakePlayerNames];
-    // playerId -> name
-    this.idMap = {};
-    this.nameMap = {};
+  private fakePlayerNames = [...fakePlayerNames];
+  private maxIterations = 1000;
+  // playerId (decimal) -> name
+  private idMap: { [name: number]: string } = {};
+  // name -> playerId (decimal)
+  private nameMap: { [name: string]: number } = {};
 
-    this.maxIterations = 1000;
-  }
-
-  makeName(playerId) {
+  public makeName(playerId: string | number): string {
     // Support decimal ids and hex strings.
     if (typeof playerId === 'string')
       playerId = parseInt(playerId, 16);
 
-    if (playerId in this.idMap)
-      return this.idMap[playerId];
+    const lookup = this.idMap[playerId];
+    if (lookup !== undefined)
+      return lookup;
 
     let name = this.makeNameInternal(playerId);
     // Just in case this ends up being not unique, make it unique.
     if (name in this.nameMap)
-      name = makeUniqueMiqoteName(playerId);
+      name = this.makeUniqueMiqoteName(playerId);
 
     this.idMap[playerId] = name;
     this.nameMap[name] = playerId;
     return name;
   }
 
-  makeNameInternal(playerId) {
-    if (this.fakePlayerNames.length > 0)
-      return this.fakePlayerNames.shift();
+  private makeNameInternal(playerId: number): string {
+    const preBuilt = this.fakePlayerNames.shift();
+    if (preBuilt !== undefined)
+      return preBuilt;
 
     for (let i = 0; i < this.maxIterations; ++i) {
       const name = this.makeRandomLalafellName();
@@ -52,7 +57,7 @@ export default class FakeNameGenerator {
     return this.makeUniqueMiqoteName(playerId);
   }
 
-  makeLalaSyl1() {
+  private makeLalaSyl1(): string {
     // Skip v, x?
     const consonants = [
       'b',
@@ -79,15 +84,15 @@ export default class FakeNameGenerator {
     return randomFromArray(consonants) + randomFromArray(vowels);
   }
 
-  makeLalaSyl2() {
+  private makeLalaSyl2(): string {
     return this.makeLalaSyl1() + this.makeLalaSyl1();
   }
 
-  makeLalaSyl1Or2() {
+  private makeLalaSyl1Or2(): string {
     return randomFromArray([this.makeLalaSyl1(), this.makeLalaSyl2()]);
   }
 
-  makeRandomLalafellName() {
+  private makeRandomLalafellName(): string {
     // https://forum.square-enix.com/ffxiv/threads/61151
     // Plainsfolk:
     // * AB CB (1-2 syllables)
@@ -118,24 +123,23 @@ export default class FakeNameGenerator {
         () => {
           const a = this.makeLalaSyl1();
           const b = this.makeLalaSyl1();
-          const c = this.makeLalaSyl1();
           return a + a + b + ' ' + a + b;
         },
       ])();
 
-    return makeName().split(' ').map((str) => {
-      return str[0].toUpperCase() + str.slice(1);
+    return makeName().split(' ').map((str: string) => {
+      return `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
     }).join(' ');
   }
 
-  makeUniqueMiqoteName(id) {
+  private makeUniqueMiqoteName(id: number): string {
     // Turn id into a lowercase string of letters.
     const randomStr = id.toString(26).split('').map((c) => {
       // shift [0-9] to [a-j]
       if (c.match(/\d/) !== null)
-        return String.fromCharCode(parseInt(c) + 'a'.charCodeAt());
+        return String.fromCharCode(parseInt(c) + 'a'.charCodeAt(0));
       // shift [a-p] to [k-z]
-      return String.fromCharCode(c.charCodeAt() + 10);
+      return String.fromCharCode(c.charCodeAt(0) + 10);
     }).join('');
     return 'X\'' + randomStr + ' Tia';
   }
