5c8b62ce8da9fe4080288b3bd8cfcd707b3327f8
Bingsu <ks2515@naver.com>
2022-06-22 09:46:13 +0900

    i18n: fix ko Asphodelos

--- ui/oopsyraidsy/data/06-ew/raid/p2s.ts
+++ ui/oopsyraidsy/data/06-ew/raid/p2s.ts
@@ -60,7 +60,7 @@ const triggerSet: OopsyTriggerSet<Data> = {
             fr: 'Poussé(e) dans le mur',
             ja: '壁へノックバック',
             cn: '击退至墙',
-            ko: '넉백',
+            ko: '벽으로 넉백',
           },
         };
       },
