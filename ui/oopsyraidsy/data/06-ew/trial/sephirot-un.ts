c8895422698dd25c9a7b0910e329045381bd6856
Adrienne Walker <enne@quisquo.us>
2022-08-23 11:09:36 -0700

    oopsy: Add 6.2 Skeleton Files

---
+++ ui/oopsyraidsy/data/06-ew/trial/sephirot-un.ts
@@ -0,0 +1,11 @@
+import ZoneId from '../../../../../resources/zone_id';
+import { OopsyData } from '../../../../../types/data';
+import { OopsyTriggerSet } from '../../../../../types/oopsy';
+
+export type Data = OopsyData;
+
+const triggerSet: OopsyTriggerSet<Data> = {
+  zoneId: ZoneId.ContainmentBayS1T7Unreal,
+};
+
+export default triggerSet;
