8838483224894fa4d3fc707545cc2c225a6cb24a
Adrienne Walker <enne@quisquo.us>
2022-08-23 11:30:39 -0700

    oopsy: Add 6.2 Skeleton Files (#4687)

---
+++ ui/oopsyraidsy/data/06-ew/raid/p5n.ts
@@ -0,0 +1,11 @@
+import ZoneId from '../../../../../resources/zone_id';
+import { OopsyData } from '../../../../../types/data';
+import { OopsyTriggerSet } from '../../../../../types/oopsy';
+
+export type Data = OopsyData;
+
+const triggerSet: OopsyTriggerSet<Data> = {
+  zoneId: ZoneId.AbyssosTheFifthCircle,
+};
+
+export default triggerSet;
