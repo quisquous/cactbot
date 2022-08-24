d3388566531ea1da9b4b4ac796395fb39d2a18a1
Legends0 <nac10fsu@gmail.com>
2022-08-23 22:53:29 -0400

    add draft of triggers for barbaricciaex

--- ui/raidboss/data/06-ew/trial/barbariccia-ex.ts
+++ ui/raidboss/data/06-ew/trial/barbariccia-ex.ts
@@ -1,13 +1,54 @@
+import Conditions from '../../../../../resources/conditions';
+import NetRegexes from '../../../../../resources/netregexes';
 import ZoneId from '../../../../../resources/zone_id';
 import { RaidbossData } from '../../../../../types/data';
 import { TriggerSet } from '../../../../../types/trigger';
+import { Responses } from '../../../../../resources/responses';
 
 export type Data = RaidbossData;
 
 const triggerSet: TriggerSet<Data> = {
   zoneId: ZoneId.StormsCrownExtreme,
   timelineFile: 'barbariccia-ex.txt',
-  triggers: [],
+  timelineTriggers: [
+    {
+      id: 'BarbaricciaEx Knuckle Drum',
+      regex: /Knuckle Drum/,
+      beforeSeconds: 5,
+      response: Responses.aoe(),
+    },
+  ],
+  triggers: [
+    {
+      id: 'BarbaricciaEx Void Aero IV',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: '7570', source: 'Barbariccia', capture: false }),
+      response: Responses.aoe(),
+    },
+    {
+      id: 'BarbaricciaEx Void Aero III',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: '7571', source: 'Barbariccia' }),
+      condition: Conditions.caresAboutPhysical(),
+      response: Responses.tankBuster(),
+    },
+    {
+      id: 'BarbaricciaEx Secret Breeze',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: '7580', source: 'Barbariccia', capture: false }),
+      infoText: (_data, _matches, output) => output.protean!(),
+      outputStrings: {
+        protean: {
+          en: 'Protean',
+          de: 'Himmelsrichtungen',
+          fr: 'Positions',
+          ja: '8方向散開',
+          cn: '分散站位',
+          ko: '정해진 위치로 산개',
+        },
+      },
+    },
+  ],
 };
 
 export default triggerSet;
