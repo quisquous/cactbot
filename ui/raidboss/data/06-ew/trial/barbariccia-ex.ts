85517cd6222be0c72b06a3e2f5d921f6f4c36bd7
Legends0 <nac10fsu@gmail.com>
2022-08-24 00:25:02 -0400

    reorder responses import

--- ui/raidboss/data/06-ew/trial/barbariccia-ex.ts
+++ ui/raidboss/data/06-ew/trial/barbariccia-ex.ts
@@ -1,9 +1,9 @@
 import Conditions from '../../../../../resources/conditions';
 import NetRegexes from '../../../../../resources/netregexes';
+import { Responses } from '../../../../../resources/responses';
 import ZoneId from '../../../../../resources/zone_id';
 import { RaidbossData } from '../../../../../types/data';
 import { TriggerSet } from '../../../../../types/trigger';
-import { Responses } from '../../../../../resources/responses';
 
 export type Data = RaidbossData;
 
