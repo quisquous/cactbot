337e2d281f23bbc04362f2b0b9fd61d96dd85b61
Panic Stevenson <panic.stevenson@gmail.com>
2022-08-30 16:17:27 -0700

    raidboss: P5S Topaz Rays Callouts

--- ui/raidboss/data/06-ew/raid/p5s.ts
+++ ui/raidboss/data/06-ew/raid/p5s.ts
@@ -5,13 +5,39 @@ import ZoneId from '../../../../../resources/zone_id';
 import { RaidbossData } from '../../../../../types/data';
 import { TriggerSet } from '../../../../../types/trigger';
 
+const directions = {
+  'NE': true,
+  'SE': true,
+  'SW': true,
+  'NW': true,
+};
+
 export interface Data extends RaidbossData {
   target?: string;
+  topazRays: { [time: number]: (keyof typeof directions | undefined)[] };
 }
+
+const convertAbilityIdToTopazRayIndex = (id: string): number => {
+  // 7703 is the Topaz Ray cast with the lowest cast time
+  return parseInt(id, 16) - parseInt('7703', 16);
+};
+
+const convertCoordinatesToDirection = (x: number, y: number): keyof typeof directions | undefined => {
+  if (x > 100)
+    return y < 100 ? 'NE' : 'SE';
+  if (x < 100)
+    return y < 100 ? 'NW' : 'SW';
+  return undefined;
+};
 
 const triggerSet: TriggerSet<Data> = {
   zoneId: ZoneId.AbyssosTheFifthCircleSavage,
   timelineFile: 'p5s.txt',
+  initData: () => {
+    return {
+      topazRays: {},
+    };
+  },
   triggers: [
     {
       // The tank busters are not cast on a target,
@@ -114,6 +140,72 @@ const triggerSet: TriggerSet<Data> = {
       netRegex: NetRegexes.startsUsing({ id: '770E', source: 'Proto-Carbuncle', capture: false }),
       durationSeconds: 5,
       response: Responses.getBackThenFront(),
+    },
+    {
+      id: 'P5S Topaz Ray Collect',
+      type: 'StartsUsing',
+      // 7703: 3.7s, 7704: 6.2s, 7705: 8.7s, 7706: 11.2s
+      netRegex: NetRegexes.startsUsing({ id: '770[3456]', source: 'Proto-Carbuncle' }),
+      run: (data, matches) => {
+        const index = convertAbilityIdToTopazRayIndex(matches.id);
+        if (data.topazRays[index] === undefined)
+          data.topazRays[index] = [];
+        const direction = convertCoordinatesToDirection(parseFloat(matches.x), parseFloat(matches.y));
+        data.topazRays[index]?.push(direction);
+      },
+    },
+    {
+      id: 'P5S Topaz Ray',
+      type: 'StartsUsing',
+      // 7703: 3.7s, 7704: 6.2s, 7705: 8.7s, 7706: 11.2s
+      netRegex: NetRegexes.startsUsing({ id: '770[3456]', source: 'Proto-Carbuncle' }),
+      delaySeconds: (_data, matches) => parseFloat(matches.castTime) - 2.5,
+      infoText: (data, matches, output) => {
+        // 770[34] cast 2 times, 770[56] cast 3 times
+        const expectedArrayLength = ['7705', '7706'].includes(matches.id) ? 3 : 2;
+        const index = convertAbilityIdToTopazRayIndex(matches.id);
+        if (data.topazRays[index]?.length !== expectedArrayLength)
+          return;
+        const safeDirs = new Set<keyof typeof directions | undefined>(['NE', 'SE', 'SW', 'NW']);
+        for (const dir of data.topazRays[index] ?? [])
+          safeDirs.delete(dir);
+        const dirs = [...safeDirs];
+        if (dirs.length !== (4 - expectedArrayLength))
+          return;
+        if (!dirs[0])
+          return;
+
+        const dirStr = `dir${dirs[0]}`;
+        if (dirs.length === 1 || !dirs[1])
+          return output[dirStr]!();
+
+        const dirMap = {
+          'NE': output.dirNE!(),
+          'SE': output.dirSE!(),
+          'SW': output.dirSW!(),
+          'NW': output.dirNW!(),
+        };
+
+        return output.two!({ dir1: dirMap[dirs[0]], dir2: dirMap[dirs[1]] });
+      },
+      run: (data, matches) => delete data.topazRays[convertAbilityIdToTopazRayIndex(matches.id)],
+      outputStrings: {
+        dirNE: Outputs.dirNE,
+        dirSE: Outputs.dirSE,
+        dirSW: Outputs.dirSW,
+        dirNW: Outputs.dirNW,
+        two: {
+          en: '${dir1} or ${dir2}',
+        },
+      },
+    },
+    {
+      id: 'P5S Topaz Ray Cleanup',
+      type: 'Ability',
+      // Topaz Cluster
+      netRegex: NetRegexes.ability({ id: '7702', source: 'Proto-Carbuncle', capture: false }),
+      delaySeconds: 10,
+      run: (data) => data.topazRays = {},
     },
   ],
 };
