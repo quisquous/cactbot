82214fe228bcfef1ae422735a3187ef99bf1032e
Legends0 <nac10fsu@gmail.com>
2022-09-01 02:46:53 -0400

    raidboss: p6s Exchange of Agonies Markers

--- ui/raidboss/data/06-ew/raid/p6s.ts
+++ ui/raidboss/data/06-ew/raid/p6s.ts
@@ -1,15 +1,47 @@
 import NetRegexes from '../../../../../resources/netregexes';
+import Outputs from '../../../../../resources/outputs';
 import { Responses } from '../../../../../resources/responses';
 import ZoneId from '../../../../../resources/zone_id';
 import { RaidbossData } from '../../../../../types/data';
+import { NetMatches } from '../../../../../types/net_matches';
 import { TriggerSet } from '../../../../../types/trigger';
 
-export type Data = RaidbossData;
+export interface Data extends RaidbossData {
+  decOffset?: number;
+  pathogenicCellsNumber?: number;
+  pathogenicCellsDelay?: number;
+}
+
+// Due to changes introduced in patch 5.2, overhead markers now have a random offset
+// added to their ID. This offset currently appears to be set per instance, so
+// we can determine what it is from the first overhead marker we see.
+// The first 1B marker in the encounter is an Exocleaver (013E).
+const firstHeadmarker = parseInt('013E', 16);
+const getHeadmarkerId = (data: Data, matches: NetMatches['HeadMarker']) => {
+  // If we naively just check !data.decOffset and leave it, it breaks if the first marker is 013E.
+  // (This makes the offset 0, and !0 is true.)
+  if (typeof data.decOffset === 'undefined')
+    data.decOffset = parseInt(matches.id, 16) - firstHeadmarker;
+  // The leading zeroes are stripped when converting back to string, so we re-add them here.
+  // Fortunately, we don't have to worry about whether or not this is robust,
+  // since we know all the IDs that will be present in the encounter.
+  return (parseInt(matches.id, 16) - data.decOffset).toString(16).toUpperCase().padStart(4, '0');
+};
 
 const triggerSet: TriggerSet<Data> = {
   zoneId: ZoneId.AbyssosTheSixthCircleSavage,
   timelineFile: 'p6s.txt',
   triggers: [
+    {
+      id: 'P6S Headmarker Tracker',
+      type: 'HeadMarker',
+      netRegex: NetRegexes.headMarker({}),
+      condition: (data) => data.decOffset === undefined,
+      // Unconditionally set the first headmarker here so that future triggers are conditional.
+      run: (data, matches) => {
+        getHeadmarkerId(data, matches);
+      },
+    },
     {
       id: 'P6S Hemitheos\'s Dark IV',
       type: 'StartsUsing',
@@ -47,6 +79,51 @@ const triggerSet: TriggerSet<Data> = {
       type: 'StartsUsing',
       netRegex: NetRegexes.startsUsing({ id: '7881', source: 'Hegemone', capture: false }),
       response: Responses.goSides(),
+    },
+    {
+      id: 'P6S Exchange of Agonies Markers',
+      type: 'HeadMarker',
+      netRegex: NetRegexes.headMarker({}),
+      condition: (data, matches) => {
+        return data.me === matches.target && !(/(00(?:4F|5[0-6]))|013E|0148|0065/).test(getHeadmarkerId(data, matches));
+      },
+      infoText: (data, matches, output) => {
+        const correctedMatch = getHeadmarkerId(data, matches);
+        switch (correctedMatch) {
+          case '0163':
+          case '0167':
+            return output.stackOnYou!();
+          case '0164':
+          case '0165':
+          case '016A':
+            return output.goCorner!();
+          case '0168':
+          case '016E':
+            return output.donut!();
+          default:
+            return output.unknown!();
+        }
+      },
+      outputStrings: {
+        stackOnYou: Outputs.stackOnYou,
+        donut: {
+          en: 'Stack Donut',
+          de: 'Sammeln Donut',
+          fr: 'Packez-vous, donut',
+          ja: '頭割り',
+          cn: '集合放月环',
+          ko: '도넛 장판, 쉐어',
+        },
+        goCorner: {
+          en: 'Go To Corner',
+          de: 'Geh in die Ecken',
+          fr: 'Allez dans un coin',
+          ja: 'コーナーへ',
+          cn: '去角落',
+          ko: '구석으로',
+        },
+        unknown: Outputs.unknown,
+      },
     },
     {
       id: 'P6S Dark Dome Bait',
