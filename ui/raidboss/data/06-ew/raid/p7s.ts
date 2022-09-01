4ed4ffb7453af4c16d2ffededec4c2ca42306075
Adrienne Walker <enne@quisquo.us>
2022-09-01 11:04:08 -0700

    raidboss/oopsy: p7s initial timeline, tankbuster triggers (#4763)

--- ui/raidboss/data/06-ew/raid/p7s.ts
+++ ui/raidboss/data/06-ew/raid/p7s.ts
@@ -1,3 +1,5 @@
+import NetRegexes from '../../../../../resources/netregexes';
+import { Responses } from '../../../../../resources/responses';
 import ZoneId from '../../../../../resources/zone_id';
 import { RaidbossData } from '../../../../../types/data';
 import { TriggerSet } from '../../../../../types/trigger';
@@ -7,7 +9,31 @@ export type Data = RaidbossData;
 const triggerSet: TriggerSet<Data> = {
   zoneId: ZoneId.AbyssosTheSeventhCircleSavage,
   timelineFile: 'p7s.txt',
-  triggers: [],
+  triggers: [
+    {
+      id: 'P7S Condensed Aero II',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: '7836', source: 'Agdistis' }),
+      response: Responses.sharedTankBuster(),
+    },
+    {
+      id: 'P7S Dispersed Aero II',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: '7835', source: 'Agdistis', capture: false }),
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: 'Split Tankbusters',
+      },
+    },
+  ],
+  timelineReplace: [
+    {
+      'locale': 'en',
+      'replaceText': {
+        'Condensed Aero II/Dispersed Aero II': 'Condensed/Dispersed Aero II',
+      },
+    },
+  ],
 };
 
 export default triggerSet;
