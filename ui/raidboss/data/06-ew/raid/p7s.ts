e2480f45623add4abd9a2f25e1fb62a9e9fa90ff
Kissor Jeyabalan <git@kij.no>
2022-09-02 07:21:51 +0200

    raidboss: p7s inviolate bonds, inviolate purgation (#4766)
    Quick early implementation for inviolate bonds and inviolate purgation. Seems to be working as intended - might need to test a bit more. The duration is set to match the last debuffs expiring - not sure if too long.

--- ui/raidboss/data/06-ew/raid/p7s.ts
+++ ui/raidboss/data/06-ew/raid/p7s.ts
@@ -1,14 +1,36 @@
+import Conditions from '../../../../../resources/conditions';
 import NetRegexes from '../../../../../resources/netregexes';
+import { UnreachableCode } from '../../../../../resources/not_reached';
+import Outputs from '../../../../../resources/outputs';
 import { Responses } from '../../../../../resources/responses';
 import ZoneId from '../../../../../resources/zone_id';
 import { RaidbossData } from '../../../../../types/data';
 import { TriggerSet } from '../../../../../types/trigger';
 
-export type Data = RaidbossData;
+export interface Data extends RaidbossData {
+  purgationDebuffs: { [role: string]: { [name: string]: number } };
+  purgationDebuffCount: number;
+}
+
+// effect ids for inviolate purgation
+const effectIdToOutputStringKey: { [effectId: string]: string } = {
+  'CEE': 'spread',
+  'D3F': 'spread',
+  'D40': 'spread',
+  'D41': 'spread',
+  'CEF': 'stack',
+  'D42': 'stack',
+  'D43': 'stack',
+  'D44': 'stack',
+};
 
 const triggerSet: TriggerSet<Data> = {
   zoneId: ZoneId.AbyssosTheSeventhCircleSavage,
   timelineFile: 'p7s.txt',
+  initData: () => ({
+    purgationDebuffs: { 'dps': {}, 'support': {} },
+    purgationDebuffCount: 0,
+  }),
   triggers: [
     {
       id: 'P7S Condensed Aero II',
@@ -23,6 +45,89 @@ const triggerSet: TriggerSet<Data> = {
       alertText: (_data, _matches, output) => output.text!(),
       outputStrings: {
         text: 'Split Tankbusters',
+      },
+    },
+    {
+      id: 'P7S Spark of Life',
+      type: 'StartsUsing',
+      netRegex: NetRegexes.startsUsing({ id: '7839', source: 'Agdistis', capture: false }),
+      infoText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: 'aoe + bleed',
+      },
+    },
+    {
+      id: 'P7S Inviolate Bonds',
+      type: 'GainsEffect',
+      // CEC/D45 = Inviolate Winds
+      // CED/D56 = Holy Bonds
+      netRegex: NetRegexes.gainsEffect({ effectId: ['CEC', 'D45'] }),
+      condition: Conditions.targetIsYou(),
+      durationSeconds: 20,
+      response: (_data, matches, output) => {
+        // cactbot-builtin-response
+        output.responseOutputStrings = {
+          stackThenSpread: Outputs.stackThenSpread,
+          spreadThenStack: Outputs.spreadThenStack,
+        };
+
+        const longTimer = parseFloat(matches.duration) > 9;
+        if (longTimer)
+          return { infoText: output.stackThenSpread!() };
+        return { infoText: output.spreadThenStack!() };
+      },
+    },
+    {
+      id: 'P7S Inviolate Purgation',
+      type: 'GainsEffect',
+      // CEE = Purgatory Winds I
+      // D3F = Purgatory Winds II
+      // D40 = Purgatory Winds III
+      // D41 = Purgatory Winds IV
+      // CEF = Holy Purgation I
+      // D42 = Holy Purgation II
+      // D43 = Holy Purgation III
+      // D44 = Holy Purgation IV
+      netRegex: NetRegexes.gainsEffect({ effectId: ['CE[EF]', 'D3F', 'D4[01234]'] }),
+      preRun: (data, matches) => {
+        data.purgationDebuffCount += 1;
+
+        const role = data.party.isDPS(matches.target) ? 'dps' : 'support';
+        const debuff = data.purgationDebuffs[role];
+        if (!debuff)
+          return;
+        debuff[matches.effectId.toUpperCase()] = parseFloat(matches.duration);
+      },
+      durationSeconds: 55,
+      infoText: (data, _matches, output) => {
+        if (data.purgationDebuffCount !== 20)
+          return;
+
+        // Sort effects ascending by duration
+        const role = data.role === 'dps' ? 'dps' : 'support';
+        const unsortedDebuffs = Object.keys(data.purgationDebuffs[role] ?? {});
+        const sortedDebuffs = unsortedDebuffs.sort((a, b) => (data.purgationDebuffs[role]?.[a] ?? 0) - (data.purgationDebuffs[role]?.[b] ?? 0));
+
+        // get stack or spread from effectId
+        const effects = sortedDebuffs.map((effectId) => effectIdToOutputStringKey[effectId]);
+
+        const [effect1, effect2, effect3, effect4] = effects;
+        if (!effect1 || !effect2 || !effect3 || !effect4)
+          throw new UnreachableCode();
+
+        return output.comboText!({
+          effect1: output[effect1]!(),
+          effect2: output[effect2]!(),
+          effect3: output[effect3]!(),
+          effect4: output[effect4]!(),
+        });
+      },
+      outputStrings: {
+        comboText: {
+          en: '${effect1} => ${effect2} => ${effect3} => ${effect4}',
+        },
+        spread: Outputs.spread,
+        stack: Outputs.stackMarker,
       },
     },
   ],
