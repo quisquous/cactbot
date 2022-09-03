2c993bac72e1735fa0d79ff7787e3dc061dc1c4a
Legends0 <nac10fsu@gmail.com>
2022-09-02 21:39:36 -0400

    remove duplicated line

--- ui/raidboss/data/06-ew/raid/p7s.ts
+++ ui/raidboss/data/06-ew/raid/p7s.ts
@@ -13,7 +13,6 @@ export interface Data extends RaidbossData {
   purgationDebuffs: { [role: string]: { [name: string]: number } };
   purgationDebuffCount: number;
   tetherCollect: string[];
-  tetherCollect: string[];
   tetherCollectPhase?: string;
 }
 
