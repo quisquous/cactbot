421d836b6d773cd437ef23cf72ffab4a5dc9779d
Legends0 <nac10fsu@gmail.com>
2022-09-03 22:18:52 -0400

    fix missing key

--- ui/raidboss/data/06-ew/raid/p7s.ts
+++ ui/raidboss/data/06-ew/raid/p7s.ts
@@ -185,7 +185,7 @@ const triggerSet: TriggerSet<Data> = {
           famineMinotaurTethers: {
             en: 'Cross Minotaur Tethers ${location}',
           },
-          warBaitMinotaur: {
+          warMinotaurTethers: {
             en: 'Minotaur Tether ${location}',
           },
           noTether: {
