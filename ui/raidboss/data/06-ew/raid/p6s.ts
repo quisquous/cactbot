51f03c06e4f428e89280daf8744a680601a851a5
Adrienne Walker <enne@quisquo.us>
2022-09-15 21:49:08 -0700

    raidboss: shorten P6S Pathogenic Cells Counter duration (#4838)

--- ui/raidboss/data/06-ew/raid/p6s.ts
+++ ui/raidboss/data/06-ew/raid/p6s.ts
@@ -172,6 +172,7 @@ const triggerSet: TriggerSet<Data> = {
       type: 'Ability',
       netRegex: NetRegexes.ability({ id: '7865', source: 'Hegemone', capture: false }),
       preRun: (data, _matches) => data.pathogenicCellsCounter++,
+      durationSeconds: 1.5,
       suppressSeconds: 1,
       sound: '',
       infoText: (data, _matches, output) => output.text!({ num: data.pathogenicCellsCounter }),
