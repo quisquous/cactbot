fe8f64719f865471dc19c1d7fed763fd01c2b147
Legends0 <nac10fsu@gmail.com>
2022-09-16 00:16:01 -0400

    account for turns being greater than pi

--- ui/raidboss/data/06-ew/raid/p8s.ts
+++ ui/raidboss/data/06-ew/raid/p8s.ts
@@ -900,7 +900,8 @@ const triggerSet: TriggerSet<Data> = {
 
         // Boss faces 3.14159274 when North
         // Flip callout if crush (7A05)
-        if (hephaistos.Heading >= 3.14)
+        const epsilon = 0.1;
+        if (Math.abs(hephaistos.Heading - 3.14) < epsilon)
           data.crushImpactSafeZone = (matches.id === '7A05' ? 'south' : 'north');
         else // Boss will be facing South
           data.crushImpactSafeZone = (matches.id === '7A05' ? 'north' : 'south');
