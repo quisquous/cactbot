47b0da4c94b62aabf12a587fcec85fe060d589c3
Trim21 <trim21.me@gmail.com>
2022-09-26 00:32:18 +0800

    build: stabilize emulator service worker's output filename (#4875)
    close https://github.com/quisquous/cactbot/issues/3247
    
    currently the final file of emulator's service worker script is named by
    chunk name `[id].bundle.js`, and its id is curretnly 378, so webpack
    will generate a service worker file with meanless filename
    `378.bundle.js` in `dist/` .
    
    
    set `webpackEntryOptions: { filename:
    "ui/raidboss/raidemulator.worker.js" }` so it will be generated to
    `dist/ui/raidboss/raidemulator.worker.js`

--- ui/raidboss/raidemulator.ts
+++ ui/raidboss/raidemulator.ts
@@ -170,6 +170,7 @@ const raidEmulatorOnLoad = async () => {
   const emulatedWebSocket = new RaidEmulatorOverlayApiHook(emulator);
   emulatedWebSocket.connected = websocketConnected;
   const logConverterWorker = new Worker(
+    /* webpackEntryOptions: { filename: "ui/raidboss/raidemulator.worker.js" } */
     new URL('./emulator/data/NetworkLogConverter.worker.ts', import.meta.url),
   );
 
