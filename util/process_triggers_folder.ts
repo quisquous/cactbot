d590340ef9386bec7aeb575be0546e8374a3670f
Trim21 <trim21.me@gmail.com>
2022-09-28 02:58:03 +0800

    build: transpile triggers with ts-node api

--- util/process_triggers_folder.ts
+++ util/process_triggers_folder.ts
@@ -1,9 +1,11 @@
-import { execSync } from 'child_process';
 import fs from 'fs';
 import path from 'path';
 import process from 'process';
+import * as url from 'url';
 
 import eslint from 'eslint';
+// import ts from 'typescript';
+import * as tsNode from 'ts-node';
 
 import UserConfig from '../resources/user_config';
 import { LooseTriggerSet } from '../types/trigger';
@@ -11,15 +13,10 @@ import defaultRaidbossOptions from '../ui/raidboss/raidboss_options';
 
 import { walkDirAsync } from './file_utils';
 
-const root = '../dist/triggers/ui/raidboss/data/';
-
-const outputFileToOrigFile = (distFile: string): string => {
-  return distFile.replace(root, 'ui/raidboss/data/');
-};
-
-const tscCmd = `${['..', 'node_modules', '.bin', 'tsc'].join(path.sep)} --build ${
-  ['..', 'tsconfig.triggers.json'].join(path.sep)
-}`;
+const __filename = url.fileURLToPath(new URL('.', import.meta.url));
+const __dirname = path.basename(__filename);
+const root = path.join(__dirname, '../ui/raidboss/data/');
+const distRoot = path.join(__dirname, '../dist/triggers/ui/raidboss/data/');
 
 // Probably we could do this more cleanly with babel, but we'll just regex for simplicitly.
 const removeImports = (lines: string[]) => {
@@ -83,7 +80,11 @@ const lint = async (filename: string, lines: string[]) => {
   delete config?.rules?.['dprint/dprint'];
 
   // Run without dprint first, because dprint is slow on unformatted files.
-  const linter = new eslint.ESLint({ fix: true, useEslintrc: false, baseConfig: config });
+  const linter = new eslint.ESLint({
+    fix: true,
+    useEslintrc: false,
+    baseConfig: config,
+  });
   const contents = lines.join('\n');
   const eslintResults = await linter.lintText(contents, { filePath: filename });
   const eslintLintResult = eslintResults[0];
@@ -109,16 +110,27 @@ const lint = async (filename: string, lines: string[]) => {
   return lintResult;
 };
 
-const processFile = async (filename: string) => {
-  console.error(`Processing file: ${filename}`);
-  const originalContents = fs.readFileSync(filename).toString();
-  const originalFilename = outputFileToOrigFile(filename);
-  let lines = originalContents.split(/[\r\n]+/);
+const tsc = tsNode.create({
+  transpileOnly: true,
+  compilerOptions: {
+    target: 'ES2020',
+  },
+});
+
+const processFile = async (originalFilename: string) => {
+  console.error(`Processing file: ${path.relative(path.join(__dirname, '..'), originalFilename)}`);
+  const transpiledContents = tsc.compile(
+    fs.readFileSync(originalFilename).toString(),
+    originalFilename,
+  );
+  const distFilePath = path.join(distRoot, path.relative(root, originalFilename));
+
+  let lines = transpiledContents.split(/[\r\n]+/);
 
   lines = removeImports(lines);
   lines = changeExportToPush(lines);
   lines = removeExportOnDeclarations(lines);
-  const lintResult = await lint(originalFilename, lines);
+  const lintResult = await lint(originalFilename.replace('.ts', '.js'), lines);
   if (!lintResult) {
     console.error('${filename}: No result from linting?');
     process.exit(2);
@@ -141,16 +153,16 @@ const processFile = async (filename: string) => {
   const numWarnings = messages.filter((x) => x.severity === 1).length;
 
   if (numErrors > 0) {
-    console.error(`${filename}: Lint ran with errors: ${JSON.stringify(messages)}`);
+    console.error(`${originalFilename}: Lint ran with errors: ${JSON.stringify(messages)}`);
     process.exit(3);
   } else if (numWarnings > 0) {
     // Print warnings, but don't stop.
-    console.error(`${filename}: Lint ran with warnings: ${JSON.stringify(messages)}`);
+    console.error(`${originalFilename}: Lint ran with warnings: ${JSON.stringify(messages)}`);
   }
 
   const contents = lintResult.output;
   if (contents === undefined) {
-    console.error(`${filename}: Lint returned no contents`);
+    console.error(`${originalFilename}: Lint returned no contents`);
     process.exit(4);
   }
 
@@ -159,51 +171,40 @@ const processFile = async (filename: string) => {
     const options = { ...defaultRaidbossOptions };
     UserConfig.evalUserFile(contents, options);
   } catch (e) {
-    console.error(`${filename}: Failed eval.`);
+    console.error(`${distFilePath}: Failed eval.`);
     console.log(e);
     process.exit(5);
   }
 
   // Overwrite the file.
-  fs.writeFileSync(filename, contents);
+  fs.mkdirSync(path.dirname(distFilePath), { recursive: true });
+  fs.writeFileSync(distFilePath.replace('.ts', '.js'), contents);
 
   // Copy timeline file if present
-  const importPath = path.join(
-    '..',
-    originalFilename.replace(path.extname(originalFilename), '.ts'),
+  const importPath = ('../ui/raidboss/data/' + path.relative(root, originalFilename)).replace(
+    '\\',
+    '/',
   );
   // Dynamic imports don't have a type, so add type assertion.
   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
   const triggerSet = (await import(importPath)).default as LooseTriggerSet;
   const timelineFilename = triggerSet?.timelineFile;
   if (timelineFilename !== undefined) {
-    const timelineFile = path.join(path.dirname(importPath), timelineFilename);
+    const timelineFile = path.join(path.dirname(originalFilename), timelineFilename);
     if (fs.existsSync(timelineFile)) {
-      const destination = path.join(path.dirname(filename), timelineFilename);
+      const destination = path.join(path.dirname(distFilePath), timelineFilename);
       fs.copyFileSync(timelineFile, destination);
     }
   }
 };
 
-const processAllFiles = async (root: string, tscCmd: string) => {
-  process.chdir(path.dirname(process.argv[1] ?? ''));
-
-  // Clean up previous directory so tsc doesn't ignore them.
-  fs.rmSync(root, { recursive: true, force: true });
-
-  // Generate javascript from typescript.
-  // TODO: replace this with programatic use of TypeScript API.
-  try {
-    execSync(tscCmd);
-  } catch (e) {
-    console.error(`Failed to run ${tscCmd}`);
-    console.error(e);
-    process.exit(6);
-  }
-
+const processAllFiles = async (root: string) => {
   // Process files.
-  await walkDirAsync(root, async (filename) => processFile(filename));
+  await walkDirAsync(root, async (filename) => {
+    if (filename.endsWith('.js') || filename.endsWith('.ts'))
+      await processFile(filename);
+  });
   process.exit(0);
 };
 
-void processAllFiles(root, tscCmd);
+void processAllFiles(root);
