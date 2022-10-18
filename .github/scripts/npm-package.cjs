0441f3ac44556f11a6472d4f0bf8ddda774186e8
Trim21 <trim21.me@gmail.com>
2022-10-18 17:56:14 +0800

    move logic to script

---
+++ .github/scripts/npm-package.cjs
@@ -0,0 +1,44 @@
+'use strict';
+const fs = require('fs');
+const path = require('path');
+
+const fsExtra = require('fs-extra');
+const { exec } = require('@actions/exec');
+
+const pkg = require('../../package.json');
+
+const projectRoot = path.join(__dirname, '..', '..');
+console.log(projectRoot);
+
+async function main() {
+  // force pwd to at .../cactbot/
+  process.chdir(projectRoot);
+
+  fs.rmSync('npm-package', { recursive: true, force: true });
+  fs.rmSync('dist', { recursive: true, force: true });
+  fs.mkdirSync('npm-package');
+
+  await exec('npm run build', undefined, { env: { CI: '1' } });
+  fs.renameSync('dist', 'npm-package/dist');
+
+  await exec('npx tsc --declaration');
+  fs.renameSync('dist/ui', 'npm-package/ui');
+  fsExtra.copySync('types', 'npm-package/types', {});
+  fs.renameSync('dist/resources', 'npm-package/resources');
+
+  fs.writeFileSync(
+    'npm-package/package.json',
+    JSON.stringify({
+      name: pkg.name,
+      type: 'module',
+      version: pkg.version,
+      license: pkg.license,
+      repository: pkg.repository,
+      files: pkg.files,
+    }),
+  );
+}
+
+main().catch((e) => {
+  throw e;
+});
