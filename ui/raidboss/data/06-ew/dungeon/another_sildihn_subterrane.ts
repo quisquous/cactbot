f7892bee8302c6bec3e5be89af47d6333b5de756
xiashtra <91220277+xiashtra@users.noreply.github.com>
2022-10-27 16:05:05 -0400

    raidboss: fix ASS Silkie triggers
    Attempt to fix ASS Silkie triggers for first and third Soaping Spree and third Slippery Soap.
    
    Addresses #4992
    cc: @Legends0

--- ui/raidboss/data/06-ew/dungeon/another_sildihn_subterrane.ts
+++ ui/raidboss/data/06-ew/dungeon/another_sildihn_subterrane.ts
@@ -126,13 +126,22 @@ const triggerSet: TriggerSet<Data> = {
       response: Responses.goRight(),
     },
     {
-      id: 'ASS Suds Collect',
-      // 7757 Bracing Suds (Wind / Donut)
-      // 7758 Chilling Suds (Ice / Cardinal)
-      // 7759 Fizzling Suds (Lightning / Intercardinal)
-      type: 'StartsUsing',
-      netRegex: { id: ['7757', '7758', '7759'], source: 'Silkie' },
-      run: (data, matches) => data.suds = matches.id,
+      id: 'ASS Suds Gain',
+      // CE1 Bracing Suds (Wind / Donut)
+      // CE2 Chilling Suds (Ice / Cardinal)
+      // CE3 Fizzling Suds (Lightning / Intercardinal)
+      type: 'GainsEffect',
+      netRegex: { effectId: ['CE[1-3]'], target: 'Silkie' },
+      run: (data, matches) => data.suds = matches.effectId,
+    },
+    {
+      id: 'ASS Suds Lose',
+      // CE1 Bracing Suds (Wind / Donut)
+      // CE2 Chilling Suds (Ice / Cardinal)
+      // CE3 Fizzling Suds (Lightning / Intercardinal)
+      type: 'LosesEffect',
+      netRegex: { effectId: ['CE[1-3]'], target: 'Silkie' },
+      run: (data, matches) => data.suds = '',
     },
     {
       id: 'ASS Slippery Soap',
@@ -141,7 +150,7 @@ const triggerSet: TriggerSet<Data> = {
       netRegex: { id: '79FB', source: 'Silkie' },
       preRun: (data) => data.soapCounter++,
       alertText: (data, matches, output) => {
-        if (data.suds === '7757') {
+        if (data.suds === 'CE1') {
           // Does not happen on first or third Slippery Soap
           if (matches.target === data.me)
             return output.getBehindPartyKnockback!();
@@ -150,8 +159,6 @@ const triggerSet: TriggerSet<Data> = {
         if (matches.target === data.me) {
           if (data.soapCounter === 1)
             return output.getBehindPuff!();
-          if (data.soapCounter === 3)
-            return output.getBehindPuffs!();
           return output.getBehindParty!();
         }
         return output.getInFrontOfPlayer!({ player: data.ShortName(matches.target) });
@@ -160,10 +167,6 @@ const triggerSet: TriggerSet<Data> = {
         getBehindPuff: {
           en: 'Behind puff and party',
           de: 'Hinter Puschel und Gruppe',
-        },
-        getBehindPuffs: {
-          en: 'Behind puffs and party (East/West)',
-          de: 'Hinter Puschel und Gruppe (Osten/Westen)',
         },
         getBehindParty: {
           en: 'Behind party',
@@ -187,7 +190,7 @@ const triggerSet: TriggerSet<Data> = {
       id: 'ASS Slippery Soap with Chilling Suds',
       type: 'StartsUsing',
       netRegex: { id: '775E', source: 'Silkie' },
-      condition: (data) => data.suds === '7758',
+      condition: (data) => data.suds === 'CE2',
       delaySeconds: (_data, matches) => parseFloat(matches.castTime) - 1,
       response: Responses.moveAround(),
     },
@@ -197,11 +200,11 @@ const triggerSet: TriggerSet<Data> = {
       netRegex: { id: '775E', source: 'Silkie', capture: false },
       infoText: (data, _matches, output) => {
         switch (data.suds) {
-          case '7757':
+          case 'CE1':
             return output.getUnder!();
-          case '7758':
+          case 'CE2':
             return output.intercards!();
-          case '7759':
+          case 'CE3':
             return output.spreadCardinals!();
         }
       },
@@ -250,16 +253,43 @@ const triggerSet: TriggerSet<Data> = {
       },
     },
     {
+      id: 'ASS Slippery Soap 3',
+      // trigger off of second Carpet Beater since 79FB is not used for this instance only
+      type: 'Ability',
+      netRegex: { id: '774F', source: 'Silkie' },
+      condition: (data) => data.soapCounter === 2,
+      run: (data) => data.beaterCounter++,
+      alertText: (data, matches, output) => {
+        if (matches.target === data.me)
+          return output.getBehindPuffs!();
+        return output.getInFrontOfPlayer!({ player: data.ShortName(matches.target) });
+      },
+      outputStrings: {
+        getBehindPuffs: {
+          en: 'Behind puffs and party (East/West)',
+          de: 'Hinter Puschel und Gruppe (Osten/Westen)',
+        },
+        getInFrontOfPlayer: {
+          en: 'In front of ${player}',
+          de: 'Sei vor ${player}',
+        },
+      },
+    },
+    {
       id: 'ASS Soaping Spree',
       // Boss does not cast Fizzling Duster with Soaping Spree
       type: 'StartsUsing',
       netRegex: { id: '7767', source: 'Silkie', capture: false },
       infoText: (data, _matches, output) => {
         switch (data.suds) {
-          case '7757':
+          case 'CE1':
             return output.getUnder!();
-          case '7758':
+          case 'CE2':
             return output.intercards!();
+          default:
+            if (data.soapCounter === 1)
+              return output.underPuff!();
+          return output.avoidPuffs!();
         }
       },
       outputStrings: {
@@ -271,6 +301,12 @@ const triggerSet: TriggerSet<Data> = {
           ja: '斜めへ',
           cn: '四角',
           ko: '대각선 쪽으로',
+        },
+        underPuff: {
+          en: 'Under green puff',
+        },
+        avoidPuffs: {
+          en: 'Avoid puff aoes',
         },
       },
     },
