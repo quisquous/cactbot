f61f17fab95af22fce7919ce85b922b32b34713a
Adrienne Walker <enne@quisquo.us>
2022-10-30 10:38:11 -0700

    build: Bump version to 0.27.21

--- plugin/CactbotEventSource/Properties/AssemblyInfo.cs
+++ plugin/CactbotEventSource/Properties/AssemblyInfo.cs
@@ -21,5 +21,5 @@ using System.Runtime.InteropServices;
 // - Revision
 // GitHub has only 3 version components, so Revision should always be 0.
 // CactbotOverlay and CactbotEventSource version should match.
-[assembly: AssemblyVersion("0.27.20.0")]
-[assembly: AssemblyFileVersion("0.27.20.0")]
+[assembly: AssemblyVersion("0.27.21.0")]
+[assembly: AssemblyFileVersion("0.27.21.0")]
