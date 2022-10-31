74819468d1aa4ced96df8ad1d4c904303ff1ce6d
xiashtra <91220277+xiashtra@users.noreply.github.com>
2022-10-31 12:10:07 -0400

    raidboss: ASS/S add Hells' Nebula trigger (#5031)
    Hells' Nebula is an ability done by trash mobs between the first and
    second boss in ASS and ASS(S) that reduces all players' HP to 1. There
    is no follow-up Doom debuff or other such mechanic that requires
    immediately topping up players' health, other than the next source of
    raidwide damage which is approximately 10s later. This trigger provides
    a heads-up callout for the HP reduction.
    
    There was a question if a trigger for this ability was useful when
    merging the initial ASS triggers, so this pull request is made in
    isolation so it can be rejected if undesired.

--- ui/raidboss/data/06-ew/dungeon/another_sildihn_subterrane.ts
+++ ui/raidboss/data/06-ew/dungeon/another_sildihn_subterrane.ts
@@ -357,6 +357,18 @@ const triggerSet: TriggerSet<Data> = {
       response: Responses.getOut(),
     },
     {
+      id: 'ASS Hells\' Nebula',
+      type: 'StartsUsing',
+      netRegex: { id: '796C', source: 'Aqueduct Armor', capture: false },
+      condition: (data) => data.role === 'healer',
+      infoText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'HP to 1',
+        },
+      },
+    },
+    {
       id: 'ASS Infernal Weight',
       type: 'StartsUsing',
       netRegex: { id: '796B', source: 'Aqueduct Armor', capture: false },
