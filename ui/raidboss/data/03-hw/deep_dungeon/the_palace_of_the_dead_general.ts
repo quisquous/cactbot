c5f850ae2bcb0214ce3288ec878259567f67f1c3
xiashtra <91220277+xiashtra@users.noreply.github.com>
2022-11-12 20:50:53 -0500

    Update the_palace_of_the_dead_general.ts

--- ui/raidboss/data/03-hw/deep_dungeon/the_palace_of_the_dead_general.ts
+++ ui/raidboss/data/03-hw/deep_dungeon/the_palace_of_the_dead_general.ts
@@ -1,5 +1,3 @@
-// import Conditions from '../../../../../resources/conditions';
-// import Outputs from '../../../../../resources/outputs';
 import { Responses } from '../../../../../resources/responses';
 import ZoneId from '../../../../../resources/zone_id';
 import { RaidbossData } from '../../../../../types/data';
@@ -8,6 +6,29 @@ import { TriggerSet } from '../../../../../types/trigger';
 // Triggers applicable to all Palace of the Dead floors.
 
 export type Data = RaidbossData;
+
+// pomanders: https://xivapi.com/deepdungeonItem?pretty=true
+const pomanders = [
+  'Safety',
+  'Sight',
+  'Strength',
+  'Steel',
+  'Affluence',
+  'Flight',
+  'Alteration',
+  'Purity',
+  'Fortune',
+  'Witching',
+  'Serenity',
+  'Rage',
+  'Lust',
+  'Intuition',
+  'Raising',
+  'Resolution',
+  'Frailty',
+  'Concealment',
+  'Petrification',
+];
 
 const triggerSet: TriggerSet<Data> = {
   zoneId: [
@@ -34,26 +55,84 @@ const triggerSet: TriggerSet<Data> = {
   ],
 
   triggers: [
-    // ---------------- Floor Effects ----------------
-    // ---------------- Pomanders ----------------
     // ---------------- Mimics ----------------
     {
-      id: 'PotD General Mimic Spawned',
+      id: 'PotD General Mimic Spawn',
       // 2566 = Mimic
       type: 'AddedCombatant',
-      netRegex: { npcNameId: '2566' },
-      infoText: (_data, matches, output) => output.text!({ name: matches.name }),
+      netRegex: { npcNameId: '2566', capture: false },
+      infoText: (_data, _matches, output) => output.text!(),
       outputStrings: {
         text: {
-          en: '${name} spawned!',
+          en: 'Mimic spawned!',
         },
       },
     },
     {
       id: 'PotD General Mimic Infatuation',
+      // gives Accursed Pox (43F) if not interrupted
       type: 'StartsUsing',
       netRegex: { id: '18FD', source: 'Mimic' },
       response: Responses.interrupt(),
+    },
+    // ---------------- Pomanders ----------------
+    {
+      id: 'PotD General Pomander Duplicate',
+      // duplicate item message: https://xivapi.com/LogMessage/7222?pretty=true
+      // en: You return the pomander of ${pomander} to the coffer. You cannot carry any more of that item.
+      type: 'SystemLogMessage',
+      netRegex: { id: '1C36' },
+      infoText: (_data, matches, output) => output.duplicate!({ pomander: pomanders[ parseInt(matches.param1, 16) ] }),
+      outputStrings: {
+        duplicate: {
+          en: '${pomander} duplicate',
+        },
+      },
+    },
+    // ---------------- Floor Notifications ----------------
+    {
+      id: 'PotD General Cairn of Return',
+      // resurrects dead players
+      // Cairn of Return activation message: https://xivapi.com/LogMessage/7242?pretty=true
+      // en: The Cairn of Return begins to glow!
+      type: 'SystemLogMessage',
+      netRegex: { id: '1C4A', capture: false },
+      infoText: (_data, matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Cairn of Return activated',
+        },
+      },
+    },
+    {
+      id: 'PotD General Cairn of Passage',
+      // portal to transfer between floors
+      // Cairn of Passage activation message: https://xivapi.com/LogMessage/7245?pretty=true
+      // en: The Cairn of Passage is activated!
+      type: 'SystemLogMessage',
+      netRegex: { id: '1C4D', capture: false },
+      infoText: (_data, matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Cairn of Passage activated',
+        },
+      },
+    },
+    {
+      id: 'PotD General Accursed Hoard',
+      // Accursed Hoard on this floor message: https://xivapi.com/LogMessage/7272?pretty=true
+      // it seems that this SystemLogMessage isn't sent if the Accursed Hoard message is sent
+      //   immediately after transference (transfer between floors)
+      // trigger off of the GameLog message instead
+      // en: You sense the Accursed Hoard calling you...
+      type: 'GameLog',
+      netRegex: { line: 'You sense the Accursed Hoard calling you\.\.\.', capture: false },
+      infoText: (_data, matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Accursed Hoard on this floor',
+        },
+      },
     },
   ],
 };
