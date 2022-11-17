735daacd98bb9c9dbb15167d67d5b812b07de1a1
xiashtra <91220277+xiashtra@users.noreply.github.com>
2022-11-17 00:46:34 -0500

    potd general triggers

---
+++ ui/raidboss/data/03-hw/deepdungeon/the_palace_of_the_dead_general.ts
@@ -0,0 +1,115 @@
+import { Responses } from '../../../../../resources/responses';
+import ZoneId from '../../../../../resources/zone_id';
+import { RaidbossData } from '../../../../../types/data';
+import { TriggerSet } from '../../../../../types/trigger';
+
+// Triggers applicable to all Palace of the Dead floors.
+
+export type Data = RaidbossData;
+
+// pomanders: https://xivapi.com/deepdungeonItem?pretty=true
+const pomanders = [
+  'Safety',
+  'Sight',
+  'Strength',
+  'Steel',
+  'Affluence',
+  'Flight',
+  'Alteration',
+  'Purity',
+  'Fortune',
+  'Witching',
+  'Serenity',
+  'Rage',
+  'Lust',
+  'Intuition',
+  'Raising',
+  'Resolution',
+  'Frailty',
+  'Concealment',
+  'Petrification',
+];
+
+const triggerSet: TriggerSet<Data> = {
+  zoneId: [
+    ZoneId.ThePalaceOfTheDeadFloors1_10,
+    ZoneId.ThePalaceOfTheDeadFloors11_20,
+    ZoneId.ThePalaceOfTheDeadFloors21_30,
+    ZoneId.ThePalaceOfTheDeadFloors31_40,
+    ZoneId.ThePalaceOfTheDeadFloors41_50,
+    ZoneId.ThePalaceOfTheDeadFloors51_60,
+    ZoneId.ThePalaceOfTheDeadFloors61_70,
+    ZoneId.ThePalaceOfTheDeadFloors71_80,
+    ZoneId.ThePalaceOfTheDeadFloors81_90,
+    ZoneId.ThePalaceOfTheDeadFloors91_100,
+    ZoneId.ThePalaceOfTheDeadFloors101_110,
+    ZoneId.ThePalaceOfTheDeadFloors111_120,
+    ZoneId.ThePalaceOfTheDeadFloors121_130,
+    ZoneId.ThePalaceOfTheDeadFloors131_140,
+    ZoneId.ThePalaceOfTheDeadFloors141_150,
+    ZoneId.ThePalaceOfTheDeadFloors151_160,
+    ZoneId.ThePalaceOfTheDeadFloors161_170,
+    ZoneId.ThePalaceOfTheDeadFloors171_180,
+    ZoneId.ThePalaceOfTheDeadFloors181_190,
+    ZoneId.ThePalaceOfTheDeadFloors191_200,
+  ],
+  zoneLabel: {
+    en: 'The Palace of the Dead (All Floors)',
+  },
+
+  triggers: [
+    // ---------------- Mimics ----------------
+    {
+      id: 'PotD General Mimic Spawn',
+      // 2566 = Mimic
+      // TODO: some Mimics may spawn after transference between floors and get called early before being found
+      type: 'AddedCombatant',
+      netRegex: { npcNameId: '2566', capture: false },
+      infoText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Mimic spawned!',
+        },
+      },
+    },
+    {
+      id: 'PotD General Mimic Infatuation',
+      // gives Accursed Pox (43F) if not interrupted
+      type: 'StartsUsing',
+      netRegex: { id: '18FD', source: 'Mimic' },
+      response: Responses.interrupt(),
+    },
+    // ---------------- Pomanders ----------------
+    {
+      id: 'PotD General Pomander Duplicate',
+      // duplicate item message: https://xivapi.com/LogMessage/7222?pretty=true
+      // en: You return the pomander of ${pomander} to the coffer. You cannot carry any more of that item.
+      type: 'SystemLogMessage',
+      netRegex: { id: '1C36' },
+      infoText: (_data, matches, output) =>
+        output.duplicate!({ pomander: pomanders[parseInt(matches.param1, 16) - 1] }),
+      outputStrings: {
+        duplicate: {
+          en: '${pomander} duplicate',
+        },
+      },
+    },
+    // ---------------- Floor Notifications ----------------
+    {
+      id: 'PotD General Cairn of Passage',
+      // portal to transfer between floors
+      // Cairn of Passage activation message: https://xivapi.com/LogMessage/7245?pretty=true
+      // en: The Cairn of Passage is activated!
+      type: 'SystemLogMessage',
+      netRegex: { id: '1C4D', capture: false },
+      infoText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Cairn of Passage activated',
+        },
+      },
+    },
+  ],
+};
+
+export default triggerSet;
