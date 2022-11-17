de11fe63aa1d04f6650cbd3e3616f6eaa4c0d56e
xiashtra <91220277+xiashtra@users.noreply.github.com>
2022-11-17 04:34:54 -0500

    floors 141-150

---
+++ ui/raidboss/data/03-hw/deepdungeon/the_palace_of_the_dead_floors_141-150.ts
@@ -0,0 +1,66 @@
+import { Responses } from '../../../../../resources/responses';
+import ZoneId from '../../../../../resources/zone_id';
+import { RaidbossData } from '../../../../../types/data';
+import { TriggerSet } from '../../../../../types/trigger';
+
+// Palace of the Dead Floors 141-150
+
+export type Data = RaidbossData;
+
+const triggerSet: TriggerSet<Data> = {
+  zoneId: ZoneId.ThePalaceOfTheDeadFloors141_150,
+
+  triggers: [
+    // ---------------- Floor 141-149 Mobs ----------------
+    {
+      id: 'PotD 141-150 Deep Palace Bhoot Paralyze III',
+      // gives Paralyze
+      type: 'StartsUsing',
+      netRegex: { id: '18F2', source: 'Deep Palace Bhoot' },
+      response: Responses.interrupt(),
+    },
+    {
+      id: 'PotD 141-150 Deep Palace Persona Paralyze III',
+      // same ability name, different mob
+      // gives Paralyze
+      type: 'StartsUsing',
+      netRegex: { id: '18F4', source: 'Deep Palace Persona' },
+      response: Responses.interrupt(),
+    },
+    {
+      id: 'PotD 141-150 Deep Palace Wraith Scream',
+      // gives Terror
+      type: 'StartsUsing',
+      netRegex: { id: '190A', source: 'Deep Palace Wraith' },
+      response: Responses.interrupt(),
+    },
+    {
+      id: 'PotD 141-150 Deep Palace Succubus Void Fire IV',
+      type: 'StartsUsing',
+      netRegex: { id: '1B81', source: 'Deep Palace Succubus' },
+      response: Responses.interrupt(),
+    },
+    {
+      id: 'PotD 141-150 Deep Palace Manticore Ripper Claw',
+      // untelegraphed front cone AoE
+      type: 'StartsUsing',
+      netRegex: { id: '18FA', source: 'Deep Palace Manticore', capture: false },
+      response: Responses.awayFromFront(),
+    },
+    {
+      id: 'PotD 141-150 Onyx Dragon Evil Eye',
+      type: 'StartsUsing',
+      netRegex: { id: '1B83', source: 'Onyx Dragon', capture: false },
+      response: Responses.lookAway(),
+    },
+    // ---------------- Floor 150 Boss: Tisiphone ----------------
+    {
+      id: 'PotD 141-150 Tisiphone Blood Rain',
+      type: 'StartsUsing',
+      netRegex: { id: '1BF1', source: 'Tisiphone', capture: false },
+      response: Responses.bigAoe(),
+    },
+  ],
+};
+
+export default triggerSet;
