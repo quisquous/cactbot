ec95544703fed32093c1b80e9930cc72f4e1f257
ShadyWhite <44015352+ShadyWhite@users.noreply.github.com>
2022-11-24 03:14:11 +0800

    i18n: add missing cn translations (#5120)

--- ui/raidboss/data/03-hw/deepdungeon/the_palace_of_the_dead_general.ts
+++ ui/raidboss/data/03-hw/deepdungeon/the_palace_of_the_dead_general.ts
@@ -33,6 +33,7 @@ const triggerSet: TriggerSet<Data> = {
   ],
   zoneLabel: {
     en: 'The Palace of the Dead (All Floors)',
+    cn: '死者宫殿 (全楼层)',
     ko: '망자의 궁전',
   },
 
@@ -48,6 +49,7 @@ const triggerSet: TriggerSet<Data> = {
       outputStrings: {
         text: {
           en: 'Mimic spawned!',
+          cn: '已生成 拟态怪!',
           ko: '미믹 등장!',
         },
       },
@@ -111,6 +113,7 @@ const triggerSet: TriggerSet<Data> = {
       outputStrings: {
         duplicate: {
           en: '${pomander} duplicate',
+          cn: '${pomander} 重复',
           ko: '${pomander} 중복',
         },
         // pomanders: https://xivapi.com/deepdungeonItem?pretty=true
@@ -280,6 +283,7 @@ const triggerSet: TriggerSet<Data> = {
       outputStrings: {
         text: {
           en: 'Cairn of Passage activated',
+          cn: '转移石冢已启动',
           ko: '전송 석탑 활성화',
         },
       },
