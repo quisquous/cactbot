ad118ba07f8a6bbea7b3dfbb38380de349eb818c
ShadyWhite <44015352+ShadyWhite@users.noreply.github.com>
2023-01-02 07:14:05 +0800

    i18n: add missing cn translations (#5170)

--- ui/raidboss/data/04-sb/map/the_shifting_altars_of_uznair.ts
+++ ui/raidboss/data/04-sb/map/the_shifting_altars_of_uznair.ts
@@ -11,7 +11,7 @@ const uznairOutputStrings = {
   spawn: {
     en: '${name} spawned!',
     de: '${name} erscheint!',
-    cn: '正在生成 ${name}!',
+    cn: '已生成 ${name}!',
     ko: '${name} 등장!',
   },
   adds: {
@@ -67,6 +67,7 @@ const triggerSet: TriggerSet<Data> = {
       outputStrings: {
         text: {
           en: 'Altar Assembly spawned, kill in order!',
+          cn: '已生成 神殿蔓德拉战队, 依次击杀!',
           ko: '만드라즈 등장, 순서대로 잡기!',
         },
       },
@@ -121,6 +122,7 @@ const triggerSet: TriggerSet<Data> = {
       outputStrings: {
         text: {
           en: 'Bait three puddles',
+          cn: '诱导三个圈圈',
           ko: '장판 3개 유도',
         },
       },
