842eadaf274818d7ec46cae9727f4231b47ef2cf
xiashtra <91220277+xiashtra@users.noreply.github.com>
2023-01-17 00:15:51 -0500

    Update the_shifting_gymnasion_agonon.ts

--- ui/raidboss/data/06-ew/map/the_shifting_gymnasion_agonon.ts
+++ ui/raidboss/data/06-ew/map/the_shifting_gymnasion_agonon.ts
@@ -1,13 +1,25 @@
+import Conditions from '../../../../../resources/conditions';
+import Outputs from '../../../../../resources/outputs';
 import { Responses } from '../../../../../resources/responses';
 import ZoneId from '../../../../../resources/zone_id';
 import { RaidbossData } from '../../../../../types/data';
 import { TriggerSet } from '../../../../../types/trigger';
 
-// TODO: lesser summons
-// TODO: greater summons
-// TODO: elder summons
-// TODO: Hippomenes (final summon)
-// TODO: Phaethon (alternate final summon)
+// TODO: ::lesser summons::
+// TODO: Gymnasiou Leon: Roar - large PBAoE
+// TODO: Gymnasiou Leon: Pounce - tankbuster
+// TODO: Gymnasiou Megakantha: all abilities
+// TODO: Gymnasiou Pithekos: all abilities
+// TODO: ::greater summons::
+// TODO: Gymnasiou Acheloios: Volcanic Howl - ???
+// TODO: Gymnasiou Styphnolobion: all abilities
+// TODO: ::elder summons::
+// TODO: ::final summons::
+// TODO: Narkissos: Rock Hard - ???
+// TODO: Narkissos: Putrid Breath - ???
+// TODO: Narkissos: all abilities
+// TODO: Hippomenes: all abilities
+// TODO: Phaethon: all abilities
 
 const agononOutputStrings = {
   spawn: {
@@ -32,21 +44,12 @@ const triggerSet: TriggerSet<Data> = {
   triggers: [
     // ---------------- random treasure mobs ----------------
     {
-      id: 'Shifting Gymnasion Agonon Gymnasiou Lampas Spawn',
+      id: 'Shifting Gymnasion Agonon Gymnasiou Lampas/Lyssa Spawn',
       // 12034 = Gymnasiou Lampas
-      type: 'AddedCombatant',
-      netRegex: { npcNameId: '12034' },
-      suppressSeconds: 1,
-      infoText: (_data, matches, output) => output.spawn!({ name: matches.name }),
-      outputStrings: {
-        spawn: agononOutputStrings.spawn,
-      },
-    },
-    {
-      id: 'Shifting Gymnasion Agonon Gymnasiou Lyssa Spawn',
       // 12035 = Gymnasiou Lyssa
+      // these two sometimes spawn at the same time, so only trigger on one to keep from being too noisy
       type: 'AddedCombatant',
-      netRegex: { npcNameId: '12035' },
+      netRegex: { npcNameId: '1203[45]' },
       suppressSeconds: 1,
       infoText: (_data, matches, output) => output.spawn!({ name: matches.name }),
       outputStrings: {
@@ -71,48 +74,289 @@ const triggerSet: TriggerSet<Data> = {
       },
     },
     // ---------------- lesser summons ----------------
-    // Gymnasiou Leon
-    // Gymnasiou Satyros
-    // Gymnasiou Triton
-    // Gymnasiou Tigris
+    {
+      id: 'Shifting Gymnasion Agonon Gymnasiou Leon Inferno Blast',
+      type: 'StartsUsing',
+      netRegex: { id: '7DCC', source: 'Gymnasiou Leon', capture: false },
+      response: Responses.awayFromFront(),
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Gymnasiou Leon Flare Star',
+      // 802F - places proximity marker
+      // 8030 - proximity aoe cast
+      type: 'StartsUsing',
+      netRegex: { id: '8030', source: 'Gymnasiou Leon', capture: false },
+      response: Responses.aoe(),
+    },
+    // TODO: Gymnasiou Leon: Roar - large PBAoE
+    // TODO: Gymnasiou Leon: Pounce - tankbuster
+    {
+      id: 'Shifting Gymnasion Agonon Gymnasiou Satyros Storm Wing',
+      type: 'StartsUsing',
+      netRegex: { id: '7DD[BD]', source: 'Gymnasiou Satyros', capture: false },
+      suppressSeconds: 1,
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Avoid green nails',
+          de: 'Weiche den grünen Nägeln aus',
+          fr: 'Évitez les griffes',
+          ja: '緑の杭に避け',
+          cn: '躲避风刃',
+          ko: '초록 발톱 피하기',
+        },
+      },
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Gymnasiou Satyros Dread Dive',
+      type: 'StartsUsing',
+      netRegex: { id: '7DDA', source: 'Gymnasiou Satyros' },
+      response: Responses.tankBuster(),
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Gymnasiou Satyros Wingblow',
+      type: 'StartsUsing',
+      netRegex: { id: '7DE[01]', source: 'Gymnasiou Satyros', capture: false },
+      suppressSeconds: 1,
+      response: Responses.getOut(),
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Gymnasiou Triton Pelagic Cleaver',
+      type: 'StartsUsing',
+      netRegex: { id: '7DE6', source: 'Gymnasiou Triton', capture: false },
+      response: Responses.awayFromFront(),
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Gymnasiou Triton Aquatic Lance',
+      type: 'StartsUsing',
+      netRegex: { id: '7DE7', source: 'Gymnasiou Triton', capture: false },
+      response: Responses.getOut(),
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Gymnasiou Triton Protolithic Puncture',
+      type: 'StartsUsing',
+      netRegex: { id: '7DE4', source: 'Gymnasiou Triton' },
+      response: Responses.tankBuster(),
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Gymnasiou Tigris Absolute Zero',
+      type: 'StartsUsing',
+      netRegex: { id: '7DD0', source: 'Gymnasiou Tigris', capture: false },
+      response: Responses.awayFromFront(),
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Gymnasiou Tigris Eyeshine',
+      type: 'StartsUsing',
+      netRegex: { id: '7DCF', source: 'Gymnasiou Tigris', capture: false },
+      response: Responses.lookAway('alert'),
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Gymnasiou Tigris Frumious Jaws',
+      type: 'StartsUsing',
+      netRegex: { id: '7DCE', source: 'Gymnasiou Tigris' },
+      response: Responses.tankBuster(),
+    },
     // Gymnasiou Megakantha: Odious Atmosphere - persistent front 180 channeled cleave
     // Gymnasiou Megakantha: Vine Whip - tankbuster
     // Gymnasiou Megakantha: Sludge Bomb - aoe under random player?
     // Gymnasiou Megakantha: Odious Air - front cone
+    // Gymnasiou Pithekos: Thundercall - summon adds (Ball of Levin: cast Thunder IV)
+    // Gymnasiou Pithekos:: Ball of Levin: Thunder IV - large PBAoE
+    // Gymnasiou Pithekos: Spark - donut aoe
+    // Gymnasiou Pithekos: Sweeping Gouge - tankbuster
+    // Gymnasiou Pithekos: Lightning Bolt - aoe under random player?
+    // Gymnasiou Pithekos: ??? - headmarker, determines where Ball of Levin will spawn? (bait to edge so Spark and Thunder IV don't overlap?)
     // ---------------- greater summons ----------------
-    // Gymnasiou Styphnolobion: Earth Quaker - pboe followed by donut aoe; later does earthshakers simultaneously
+    {
+      id: 'Shifting Gymnasion Agonon Gymnasiou Acheloios Tail Swing',
+      type: 'StartsUsing',
+      netRegex: { id: '7E17', source: 'Gymnasiou Acheloios', capture: false },
+      response: Responses.getOut(),
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Gymnasiou Acheloios Double Hammer',
+      // TODO: same pattern every time?
+      type: 'StartsUsing',
+      netRegex: { id: '7E19', source: 'Gymnasiou Acheloios', capture: false },
+      alertText: (_data, _matches, output) => {
+        return output.text!({ dir1: output.left!(), dir2: output.right!() });
+      },
+      outputStrings: {
+        left: Outputs.left,
+        right: Outputs.right,
+        text: {
+          en: '${dir1} => ${dir2}',
+          de: '${dir1} => ${dir2}',
+          fr: '${dir1} => ${dir2}',
+          ja: '${dir1} => ${dir2}',
+          cn: '${dir1} => ${dir2}',
+          ko: '${dir1} => ${dir2}',
+        },
+      },
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Gymnasiou Acheloios Quadruple Hammer',
+      // rotates counterclockwise after each cleave
+      // TODO: same rotation/pattern every time?
+      type: 'StartsUsing',
+      netRegex: { id: '7E18', source: 'Gymnasiou Acheloios', capture: false },
+      alertText: (_data, _matches, output) => {
+        return output.text!({
+          dir1: output.left!(),
+          dir2: output.right!(),
+          dir3: output.left!(),
+          dir4: output.right!(),
+        });
+      },
+      outputStrings: {
+        left: Outputs.left,
+        right: Outputs.right,
+        text: {
+          en: '${dir1} => ${dir2} => ${dir3} => ${dir4}',
+          de: '${dir1} => ${dir2} => ${dir3} => ${dir4}',
+          fr: '${dir1} => ${dir2} => ${dir3} => ${dir4}',
+          ja: '${dir1} => ${dir2} => ${dir3} => ${dir4}',
+          cn: '${dir1} => ${dir2} => ${dir3} => ${dir4}',
+          ko: '${dir1} => ${dir2} => ${dir3} => ${dir4}',
+        },
+      },
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Gymnasiou Acheloios Deadly Hold',
+      type: 'StartsUsing',
+      netRegex: { id: '7E13', source: 'Gymnasiou Acheloios' },
+      response: Responses.tankBuster(),
+    },
+    // TODO: Gymnasiou Acheloios: Volcanic Howl - ???
+    {
+      id: 'Shifting Gymnasion Agonon Gymnasiou Meganereis Wave of Turmoil',
+      type: 'StartsUsing',
+      netRegex: { id: '7E0[12]', source: 'Gymnasiou Meganereis', capture: false },
+      suppressSeconds: 1,
+      response: Responses.knockback(),
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Gymnasiou Meganereis Falling Water',
+      type: 'StartsUsing',
+      netRegex: { id: '7E04', source: 'Gymnasiou Meganereis' },
+      condition: Conditions.targetIsYou(),
+      response: Responses.spread(),
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Gymnasiou Meganereis Ceras',
+      // inflicts Poison (829) for 15s
+      type: 'StartsUsing',
+      netRegex: { id: '7DFF', source: 'Gymnasiou Meganereis' },
+      response: Responses.tankBuster(),
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Gymnasiou Meganereis Hydrocannon',
+      type: 'StartsUsing',
+      netRegex: { id: '7E00', source: 'Gymnasiou Meganereis', capture: false },
+      response: Responses.awayFromFront(),
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Gymnasiou Sphinx Explosion',
+      // donut aoe centered on adds
+      type: 'StartsUsing',
+      netRegex: { id: '7E11', source: 'Verdant Plume', capture: false },
+      suppressSeconds: 1,
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Get Under Verdant Plume',
+        },
+      },
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Gymnasiou Sphinx Fervid Pulse',
+      type: 'StartsUsing',
+      netRegex: { id: '7E10', source: 'Gymnasiou Sphinx', capture: false },
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Intercards',
+          de: 'Interkardinal',
+          fr: 'Intercardinal',
+          ja: '斜め',
+          ko: '대각선 쪽으로',
+        },
+      },
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Gymnasiou Sphinx Frigid Pulse',
+      type: 'StartsUsing',
+      netRegex: { id: '7E0E', source: 'Gymnasiou Sphinx', capture: false },
+      response: Responses.getUnder(),
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Gymnasiou Sphinx Feather Rain',
+      // can overlap with Frigid Pulse, possibly too noisy?
+      type: 'StartsUsing',
+      netRegex: { id: '7E0F', source: 'Gymnasiou Sphinx' },
+      condition: Conditions.targetIsYou(),
+      response: Responses.spread(),
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Gymnasiou Sphinx Scratch',
+      type: 'StartsUsing',
+      netRegex: { id: '7E09', source: 'Gymnasiou Sphinx' },
+      response: Responses.tankBuster(),
+    },
+    // Gymnasiou Styphnolobion: Earth Quaker - PBAoE followed by donut aoe; later does earthshakers simultaneously
     // Gymnasiou Styphnolobion: Rake - tankbuster
     // Gymnasiou Styphnolobion: Stone III - aoe under random players?
     // Gymnasiou Styphnolobion: Earth Shaker - earth shakers on random players
-    // Gymnasiou Styphnolobion: Tiiimbeeer - raidwide
-    // Gymnasiou Acheloios: Double Hammer - left/right or right/left cleaves
-    // Gymnasiou Acheloios: Tail Swing - pbaoe
-    // Gymnasiou Acheloios: Quadruple Hammer - 4x rotating left/right or right/left cleaves
-    // Gymnasiou Acheloios: Left Hammer - 180 left cleave, used during Double/Quadruple Hammer
-    // Gymnasiou Acheloios: Right Hammer - 180 right cleave, used during Double/Quadruple Hammer
-    // Gymnasiou Acheloios: Deadly Hold - tankbuster
-    // Gymnasiou Meganereis: Wave of Turmoil - knockback with 4x aoes on outside edge of arena
-    // Gymnasiou Meganereis: Waterspout - aoe under random players?
-    // Gymnasiou Meganereis: Falling Water - spread aoe on random players (simultaneous with Wave of Turmoil)
-    // Gymnasiou Meganereis: Ceras - tankbuster w/ bleed
-    // Gymnasiou Sphinx: Feather Wind - summons adds (Verdant Plume: cast Explosion - donut aoe)
-    // Gymnasiou Sphinx: Fervid Pulse - large cross aoe
-    // Gymnasiou Sphinx: Frigid Pulse - large donut aoe
-    // Gymnasiou Sphinx: Feather Rain - spread aoe on all? players, simultaneous with Frigid Pulse
-    // Gymnasiou Sphinx: Scratch - tankbuster
+    // Gymnasiou Styphnolobion: Tiiimbeeer (yes, it has 3 i's and 3 e's) - raidwide
     // ---------------- elder summons ----------------
-    // Lyssa Chrysine: Icicall - summons ice pillars
-    // Lyssa Chrysine: Skull Dasher - tankbuster
-    // Lyssa Chrysine: Howl - summons adds?
-    // Lyssa Chrysine: Circle of Ice - donut aoe followed by pbaoe
-    // Lyssa Chrysine: Heavy Smash - stack
-    // Lyssa Chrysine: Frigid Stone - aoe under random players?
-    // Lyssa Chrysine: Frigid Needle - pbaoe followed by donut aoe
-    // Lampas Chrysine: Aetherial Light - front/back cones
-    // Lampas Chrysine: Lightburst - tankbuster
-    // Lampas Chrysine: Shine - aoe under random players?
-    // Lampas Chrysine: Summon - summons adds (Gymnasiou Lampas)
-    // Lampas Chrysine: Aetherial Light (alternate) - 3x rotating front/back cones
+    {
+      id: 'Shifting Gymnasion Agonon Lyssa Chrysine Heavy Smash',
+      type: 'StartsUsing',
+      netRegex: { id: '7E3A', source: 'Lyssa Chrysine' },
+      response: Responses.stackMarkerOn(),
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Lyssa Chrysine Frigid Needle',
+      type: 'StartsUsing',
+      netRegex: { id: '7E36', source: 'Lyssa Chrysine', capture: false },
+      response: Responses.getOutThenIn(),
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Lyssa Chrysine Circle of Ice',
+      type: 'StartsUsing',
+      netRegex: { id: '7E38', source: 'Lyssa Chrysine', capture: false },
+      response: Responses.getInThenOut(),
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Lyssa Chrysine Howl',
+      type: 'StartsUsing',
+      netRegex: { id: '7E28', source: 'Lyssa Chrysine', capture: false },
+      infoText: (_data, _matches, output) => output.adds!(),
+      outputStrings: {
+        adds: agononOutputStrings.adds,
+      },
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Lyssa Chrysine Skull Dasher',
+      type: 'StartsUsing',
+      netRegex: { id: '7E32', source: 'Lyssa Chrysine' },
+      response: Responses.tankBuster(),
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Lampas Chrysine Lightburst',
+      type: 'StartsUsing',
+      netRegex: { id: '7E22', source: 'Lampas Chrysine' },
+      response: Responses.tankBuster(),
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Lampas Chrysine Summon',
+      type: 'StartsUsing',
+      netRegex: { id: '7E20', source: 'Lampas Chrysine', capture: false },
+      infoText: (_data, _matches, output) => output.adds!(),
+      outputStrings: {
+        adds: agononOutputStrings.adds,
+      },
+    },
     {
       id: 'Shifting Gymnasion Agonon Gymnasiou Mandragoras Ram',
       type: 'StartsUsing',
@@ -128,14 +372,91 @@ const triggerSet: TriggerSet<Data> = {
         adds: agononOutputStrings.adds,
       },
     },
-    // ---------------- final summon: Hippomenes ----------------
+    // ---------------- final summon: Narkissos ----------------
+    {
+      id: 'Shifting Gymnasion Agonon Narkissos Fetching Fulgence',
+      type: 'StartsUsing',
+      netRegex: { id: '7E4C', source: 'Narkissos', capture: false },
+      response: Responses.lookAway('alert'),
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Narkissos Lash',
+      type: 'StartsUsing',
+      netRegex: { id: '7E4A', source: 'Narkissos' },
+      response: Responses.tankBuster(),
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Narkissos Extensible Tendrils',
+      type: 'StartsUsing',
+      netRegex: { id: '7E53', source: 'Narkissos', capture: false },
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Cardinals',
+          de: 'Kardinal',
+          fr: 'Cardinaux',
+          ja: '十字回避',
+          ko: '십자방향으로',
+        },
+      },
+    },
+    {
+      id: 'Shifting Gymnasion Agonon Narkissos Brainstorm',
+      type: 'GainsEffect',
+      // 7A6 = Forward March
+      // 7A7 = About Face
+      // 7A8 = Left Face
+      // 7A9 = Right Face
+      netRegex: { effectId: '7A[6-9]', source: 'Narkissos' },
+      condition: Conditions.targetIsYou(),
+      infoText: (_data, matches, output) => {
+        const effectId = matches.effectId.toUpperCase();
+        if (effectId === '7A6')
+          return output.forward!();
+        if (effectId === '7A7')
+          return output.backward!();
+        if (effectId === '7A8')
+          return output.left!();
+        if (effectId === '7A9')
+          return output.right!();
+      },
+      outputStrings: {
+        forward: {
+          en: 'March Forward into Safe Spot',
+          de: 'marschiere Vorwärts in die sichere Stelle',
+          cn: '向前强制移动到安全区',
+          ko: '강제 이동 앞',
+        },
+        backward: {
+          en: 'March Backward into Safe Spot',
+          de: 'marschiere Rückwärts in die sichere Stelle',
+          cn: '向后强制移动到安全区',
+          ko: '강제 이동 뒤',
+        },
+        left: {
+          en: 'March Left into Safe Spot',
+          de: 'marschiere Links in die sichere Stelle',
+          cn: '向左强制移动到安全区',
+          ko: '강제 이동 왼쪽',
+        },
+        right: {
+          en: 'March Right into Safe Spot',
+          de: 'marschiere Rechts in die sichere Stelle',
+          cn: '向右强制移动到安全区',
+          ko: '강제 이동 오른쪽',
+        },
+      },
+    },
+    // Narkissos: Rock Hard - ???
+    // Narkissos: Putrid Breath - ???
+    // ---------------- alternate final summon: Hippomenes ----------------
     // Hippomenes: Charge Blaster - applies 270 cone buffs to boss
     // Hippomenes: Dibrid Blaster - fires two 270 cones back-to-back, direction indicated by boss buffs from Charge Blaster
     // Hippomenes: Gouge - tankbuster
     // Hippomenes: Rumbling Thunder - aoe under random players?
     // Hippomenes: Electric Whisker - cardinal cones plus spread aoes on players?
     // Hippomenes: Tetrabrid Blaster - fires four 270 cones back-to-back, direction indicated by boss buffs from Charge Blaster
-    // Hippomenes: Electric Burst - raidwide, summons adds (Ball of Levin: cast Shock - large pbaoe)
+    // Hippomenes: Electric Burst - raidwide, summons adds (Ball of Levin: cast Shock - large PBAoE)
     // ---------------- alternate final summon: Phaethon ----------------
     // Phaethon: Illusive Fire - summons adds (Phantasmal Phaethon: cast Gallop - line aoe across arena)
     // Phaethon: Heat Blast - raidwide
