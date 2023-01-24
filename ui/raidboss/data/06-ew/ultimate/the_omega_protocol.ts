4aafd6b7ec9caf7cccae96e0c31eb1169b60e23a
Adrienne Walker <enne@quisquo.us>
2023-01-24 12:26:32 -0800

    raidboss/oopsy: add skeleton TOP files

---
+++ ui/raidboss/data/06-ew/ultimate/the_omega_protocol.ts
@@ -0,0 +1,85 @@
+import { UnreachableCode } from '../../../../../resources/not_reached';
+import ZoneId from '../../../../../resources/zone_id';
+import { RaidbossData } from '../../../../../types/data';
+import { PluginCombatantState } from '../../../../../types/event';
+import { NetMatches } from '../../../../../types/net_matches';
+import { TriggerSet } from '../../../../../types/trigger';
+
+export const playstationMarkers = ['circle', 'cross', 'triangle', 'square'] as const;
+export type PlaystationMarker = typeof playstationMarkers[number];
+
+export interface Data extends RaidbossData {
+  combatantData: PluginCombatantState[];
+  decOffset?: number;
+}
+
+// Due to changes introduced in patch 5.2, overhead markers now have a random offset
+// added to their ID. This offset currently appears to be set per instance, so
+// we can determine what it is from the first overhead marker we see.
+export const headmarkers = {
+  // vfx/lockon/eff/r1fz_firechain_01x.avfx through 04x
+  'firechainCircle': '0119',
+  'firechainTriangle': '011A',
+  'firechainSquare': '011B',
+  'firechainX': '011C',
+} as const;
+
+export const playstationHeadmarkerIds: readonly string[] = [
+  headmarkers.firechainCircle,
+  headmarkers.firechainTriangle,
+  headmarkers.firechainSquare,
+  headmarkers.firechainX,
+] as const;
+
+export const playstationMarkerMap: { [id: string]: PlaystationMarker } = {
+  [headmarkers.firechainCircle]: 'circle',
+  [headmarkers.firechainTriangle]: 'triangle',
+  [headmarkers.firechainSquare]: 'square',
+  [headmarkers.firechainX]: 'cross',
+} as const;
+
+export const firstMarker = 'TODO';
+
+export const getHeadmarkerId = (
+  data: Data,
+  matches: NetMatches['HeadMarker'],
+  firstDecimalMarker?: number,
+) => {
+  // If we naively just check !data.decOffset and leave it, it breaks if the first marker is 00DA.
+  // (This makes the offset 0, and !0 is true.)
+  if (data.decOffset === undefined) {
+    // This must be set the first time this function is called in DSR Headmarker Tracker.
+    if (firstDecimalMarker === undefined)
+      throw new UnreachableCode();
+    data.decOffset = parseInt(matches.id, 16) - firstDecimalMarker;
+  }
+  // The leading zeroes are stripped when converting back to string, so we re-add them here.
+  // Fortunately, we don't have to worry about whether or not this is robust,
+  // since we know all the IDs that will be present in the encounter.
+  return (parseInt(matches.id, 16) - data.decOffset).toString(16).toUpperCase().padStart(4, '0');
+};
+
+const triggerSet: TriggerSet<Data> = {
+  zoneId: ZoneId.TheOmegaProtocolUltimate,
+  timelineFile: 'the_omega_protocol.txt',
+  initData: () => {
+    return {
+      combatantData: [],
+    };
+  },
+  triggers: [
+    {
+      id: 'TOP Headmarker Tracker',
+      type: 'HeadMarker',
+      netRegex: {},
+      condition: (data) => data.decOffset === undefined,
+      // Unconditionally set the first headmarker here so that future triggers are conditional.
+      run: (data, matches) => {
+        const firstHeadmarker: number = parseInt(firstMarker, 16);
+        getHeadmarkerId(data, matches, firstHeadmarker);
+      },
+    },
+  ],
+};
+
+export default triggerSet;
