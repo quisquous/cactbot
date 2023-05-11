b4dc41a01c06350cd0acfedab29fccafec3864a5
Legends0 <nac10fsu@gmail.com>
2023-05-10 22:29:18 -0400

    adjust delay
    1 in 20 pulls seems to fail for me at 1.1s

--- ui/raidboss/data/06-ew/ultimate/the_omega_protocol.ts
+++ ui/raidboss/data/06-ew/ultimate/the_omega_protocol.ts
@@ -2021,7 +2021,7 @@ const triggerSet: TriggerSet<Data> = {
       id: 'TOP Unlimited Wave Cannon Dodges',
       type: 'StartsUsing',
       netRegex: { id: '7BAC', source: 'Alpha Omega', capture: false },
-      delaySeconds: 1.1,
+      delaySeconds: 1.2,
       durationSeconds: 10.6, // Time until 3rd puddle
       promise: async (data) => {
         if (data.waveCannonFlares.length < 2) {
