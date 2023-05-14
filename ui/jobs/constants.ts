c4877e1a5a1e3ba6ebad1e6f247992f6a7a30ff4
Echoring <68432572+Echoring@users.noreply.github.com>
2023-05-14 13:11:58 +0800

    Revert "Merge branch 'main' into jobs-version"
    This reverts commit ee5da08491859189e4221bd3f0ba4ace88138267, reversing
    changes made to c49ca0f6b2118ce0aae16dd22a7141e5f7624d75.

--- ui/jobs/constants.ts
+++ ui/jobs/constants.ts
@@ -69,7 +69,6 @@ export const kAbility = {
   BladeofFaith: '6494',
   BladeofTruth: '6495',
   BladeOfValor: '6496',
-  Atonement: '404C',
   // WAR
   HeavySwing: '1F',
   Maim: '25',
@@ -469,7 +468,6 @@ export const kComboBreakers = [
   kAbility.TotalEclipse,
   kAbility.Prominence,
   kAbility.ShieldBash,
-  kAbility.Atonement,
   // WAR
   kAbility.HeavySwing,
   kAbility.Maim,
@@ -541,6 +539,8 @@ export const kComboBreakers = [
 
 export const kComboBreakers620 = [
   ...kComboBreakers,
+  kAbility.GoringBlade,
+  kAbility.Confiteor,
 ];
 
 export const kComboBreakers610 = [
