quisquous/cactbot

commit e9ab234cee3582a185339d58e7b4d09bca92d616

jobs: use ffxivVersion instead of ffxivRegion (#5390)
Based on #5389 
This allows us to change the behavior by just edit the version in
jobs.ts, instead of changing all the conditions in each jobs module.

ui/jobs/combo_tracker.ts
@@ -3,11 +3,11 @@ import { EventEmitter } from 'eventemitter3';
 import {
   kComboActions,
   kComboBreakers,
-  kComboBreakersCn,
-  kComboBreakersKo,
+  kComboBreakers610,
+  kComboBreakers620,
   kComboDelay,
 } from './constants';
-import { FfxivRegion } from './jobs';
+import { FfxivVersion } from './jobs';
 import { Player } from './player';
 
 type StartMap = {
@@ -113,12 +113,12 @@ export class ComboTracker extends EventEmitter<{ combo: ComboCallback }> {
     this.StateTransition(id);
   }
 
-  static setup(ffxivRegion: FfxivRegion, player: Player): ComboTracker {
+  static setup(ffxivVersion: FfxivVersion, player: Player): ComboTracker {
     let breakers;
-    if (ffxivRegion === 'ko')
-      breakers = kComboBreakersKo;
-    else if (ffxivRegion === 'cn')
-      breakers = kComboBreakersCn;
+    if (ffxivVersion < 630)
+      breakers = kComboBreakers620;
+    else if (ffxivVersion < 620)
+      breakers = kComboBreakers610;
     else
       breakers = kComboBreakers;
 

ui/jobs/buff_tracker.ts
@@ -32,7 +32,7 @@ import WidgetList from '../../resources/widget_list';
 import { NetMatches } from '../../types/net_matches';
 
 import { kAbility } from './constants';
-import { FfxivRegion } from './jobs';
+import { FfxivVersion } from './jobs';
 import { JobsOptions } from './jobs_options';
 import { makeAuraTimerIcon } from './utils';
 
@@ -278,7 +278,7 @@ export class BuffTracker {
     private leftBuffDiv: WidgetList,
     private rightBuffDiv: WidgetList,
     private partyTracker: PartyTracker,
-    private ffxivRegion: FfxivRegion,
+    private ffxivVersion: FfxivVersion,
   ) {
     this.options = options;
     this.playerName = playerName;
@@ -609,19 +609,19 @@ export class BuffTracker {
       },
     };
 
-    // Abilities that are different in Cn region.
-    const vCn: { [s: string]: BuffInfo } = {};
+    // Abilities that are different in 6.1 version.
+    const v610: { [s: string]: BuffInfo } = {};
 
-    // Abilities that are different in Ko region.
-    const vKo: { [s: string]: BuffInfo } = {};
+    // Abilities that are different in 6.0 version.
+    const v600: { [s: string]: BuffInfo } = {};
 
-    if (this.ffxivRegion === 'ko') {
-      for (const [key, entry] of Object.entries(vKo))
+    if (this.ffxivVersion < 620) {
+      for (const [key, entry] of Object.entries(v610))
         this.buffInfo[key] = entry;
     }
 
-    if (this.ffxivRegion === 'cn') {
-      for (const [key, entry] of Object.entries(vCn))
+    if (this.ffxivVersion < 610) {
+      for (const [key, entry] of Object.entries(v600))
         this.buffInfo[key] = entry;
     }
 

ui/jobs/components/base.ts
@@ -4,7 +4,7 @@ import { Bars } from '../bars';
 import { ComboTracker } from '../combo_tracker';
 import { kComboDelay } from '../constants';
 import { JobsEventEmitter, PartialFieldMatches } from '../event_emitter';
-import { FfxivRegion } from '../jobs';
+import { FfxivVersion } from '../jobs';
 import { JobsOptions } from '../jobs_options';
 import { Player } from '../player';
 
@@ -24,7 +24,7 @@ export interface ComponentInterface {
   options: JobsOptions;
   partyTracker: PartyTracker;
   player: Player;
-  ffxivRegion: FfxivRegion;
+  ffxivVersion: FfxivVersion;
 }
 
 export class BaseComponent implements ComponentInterface {
@@ -33,7 +33,7 @@ export class BaseComponent implements ComponentInterface {
   options: JobsOptions;
   partyTracker: PartyTracker;
   player: Player;
-  ffxivRegion: FfxivRegion;
+  ffxivVersion: FfxivVersion;
 
   inCombat: boolean;
   comboDuration: number;
@@ -44,7 +44,7 @@ export class BaseComponent implements ComponentInterface {
     this.options = o.options;
     this.partyTracker = o.partyTracker;
     this.player = o.player;
-    this.ffxivRegion = o.ffxivRegion;
+    this.ffxivVersion = o.ffxivVersion;
     this.comboDuration = kComboDelay;
 
     this.inCombat = false;

ui/jobs/components/index.ts
@@ -6,7 +6,7 @@ import { Bars } from '../bars';
 import { BuffTracker } from '../buff_tracker';
 import { kWellFedContentTypes } from '../constants';
 import { JobsEventEmitter } from '../event_emitter';
-import { FfxivRegion } from '../jobs';
+import { FfxivVersion } from '../jobs';
 import { JobsOptions } from '../jobs_options';
 import { Player } from '../player';
 import { doesJobNeedMPBar, isPvPZone, RegexesHolder } from '../utils';
@@ -89,7 +89,7 @@ export class ComponentManager {
   ee: JobsEventEmitter;
   options: JobsOptions;
   partyTracker: PartyTracker;
-  ffxivRegion: FfxivRegion;
+  ffxivVersion: FfxivVersion;
   player: Player;
   regexes?: RegexesHolder;
   component?: BaseComponent;
@@ -113,7 +113,7 @@ export class ComponentManager {
     this.options = o.options;
     this.partyTracker = o.partyTracker;
     this.player = o.player;
-    this.ffxivRegion = o.ffxivRegion;
+    this.ffxivVersion = o.ffxivVersion;
 
     this.shouldShow = {};
     this.contentType = undefined;
@@ -129,7 +129,7 @@ export class ComponentManager {
   }
 
   getJobComponents(job: Job): BaseComponent {
-    // if (this.o.ffxivRegion === 'cn/ko') {
+    // if (this.o.ffxivVersion < ???) {
     //   if (job === 'XXX')
     //     return new XXXOldComponent(this.o);
     // }
@@ -238,7 +238,7 @@ export class ComponentManager {
           this.bars.o.leftBuffsList,
           this.bars.o.rightBuffsList,
           this.partyTracker,
-          this.ffxivRegion,
+          this.ffxivVersion,
         );
       }
     });

ui/jobs/components/mch.ts
@@ -92,7 +92,7 @@ export class MCHComponent extends BaseComponent {
     // These two seconds are shown by half adjust, not like others' ceil.
     if (jobDetail.overheatActive === true) {
       this.heatGauge.parentNode.classList.add('overheat');
-      if (this.ffxivRegion !== 'ko')
+      if (this.ffxivVersion >= 630)
         this.heatGauge.innerText = this.overheatstack.toString();
       else
         this.heatGauge.innerText = Math.round(jobDetail.overheatMilliseconds / 1000).toString();
@@ -125,7 +125,7 @@ export class MCHComponent extends BaseComponent {
   }
 
   override onYouGainEffect(id: string, matches: PartialFieldMatches<'GainsEffect'>): void {
-    if (id === EffectId.Overheated && this.ffxivRegion !== 'ko')
+    if (id === EffectId.Overheated && this.ffxivVersion >= 630)
       this.overheatstack = parseInt(matches.count ?? '0');
   }
   override onMobGainsEffectFromYou(id: string, matches: PartialFieldMatches<'GainsEffect'>): void {

ui/jobs/components/pld.ts
@@ -55,7 +55,7 @@ export class PLDComponent extends BaseComponent {
     const requiescatContainer = document.createElement('div');
     requiescatContainer.id = 'pld-stacks-requiescat';
     this.stacksContainer.appendChild(requiescatContainer);
-    if (this.ffxivRegion !== 'ko') {
+    if (this.ffxivVersion >= 630) {
       for (let i = 0; i < 4; ++i) {
         const d = document.createElement('div');
         requiescatContainer.appendChild(d);
@@ -104,7 +104,7 @@ export class PLDComponent extends BaseComponent {
   }
 
   override onCombo(skill: string, combo: ComboTracker): void {
-    if (skill === kAbility.GoringBlade && this.ffxivRegion !== 'ko')
+    if (skill === kAbility.GoringBlade && this.ffxivVersion >= 630)
       return;
     this.comboTimer.duration = 0;
     if (skill === kAbility.GoringBlade)
@@ -118,11 +118,11 @@ export class PLDComponent extends BaseComponent {
   override onUseAbility(skill: string): void {
     switch (skill) {
       case kAbility.BladeOfValor:
-        if (this.ffxivRegion === 'ko')
+        if (this.ffxivVersion < 630)
           this.goreBox.duration = 21;
         break;
       case kAbility.GoringBlade:
-        if (this.ffxivRegion !== 'ko')
+        if (this.ffxivVersion >= 630)
           this.goreBox.duration = this.bars.player.getActionCooldown(60000, 'skill');
         break;
       case kAbility.Expiacion:
@@ -133,7 +133,7 @@ export class PLDComponent extends BaseComponent {
         this.tid1 = showDuration({
           tid: this.tid1,
           timerbox: this.fightOrFlightBox,
-          duration: this.ffxivRegion !== 'ko' ? 20 : 25,
+          duration: this.ffxivVersion >= 630 ? 20 : 25,
           cooldown: 60,
           threshold: this.player.gcdSkill * 2 + 1,
           activecolor: 'pld-color-fightorflight.active',

ui/jobs/components/sge.ts
@@ -84,7 +84,7 @@ export class SGEComponent extends BaseComponent {
       case kAbility.Phlegma2:
       case kAbility.Phlegma3:
         if (matches.targetIndex === '0') { // Avoid multiple call in AOE
-          if (this.ffxivRegion !== 'ko')
+          if (this.ffxivVersion >= 630)
             this.phlegma.duration = 40 + this.phlegma.value;
           else
             this.phlegma.duration = 45 + this.phlegma.value;

ui/jobs/components/whm.ts
@@ -92,7 +92,7 @@ export class WHMComponent extends BaseComponent {
         this.diaBox.duration = 30;
         break;
       case kAbility.Assize:
-        if (this.ffxivRegion !== 'ko')
+        if (this.ffxivVersion >= 630)
           this.assizeBox.duration = 40;
         else
           this.assizeBox.duration = 45;

commit 95b1419fe2f9c79a95daf6627514e05cdef9378c

jobs: fix logic error with combo breakers
This was introduced in #5390.

Thanks, @xiashtra.

ui/jobs/combo_tracker.ts
@@ -115,10 +115,10 @@ export class ComboTracker extends EventEmitter<{ combo: ComboCallback }> {
 
   static setup(ffxivVersion: FfxivVersion, player: Player): ComboTracker {
     let breakers;
-    if (ffxivVersion < 630)
-      breakers = kComboBreakers620;
-    else if (ffxivVersion < 620)
+    if (ffxivVersion < 620)
       breakers = kComboBreakers610;
+    else if (ffxivVersion < 630)
+      breakers = kComboBreakers620;
     else
       breakers = kComboBreakers;
 