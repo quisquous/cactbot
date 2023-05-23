cf8c365ae36ae2ec989cc8ed290cfe134f0bffa3
Adrienne Walker <enne@quisquo.us>
2023-05-23 08:55:24 -0700

    oopsy: add blank files for 6.4 (#5409)

---
+++ ui/oopsyraidsy/data/06-ew/raid/p11n.ts
@@ -0,0 +1,12 @@
+import ZoneId from '../../../../../resources/zone_id';
+import { OopsyData } from '../../../../../types/data';
+import { OopsyTriggerSet } from '../../../../../types/oopsy';
+
+export type Data = OopsyData;
+
+const triggerSet: OopsyTriggerSet<Data> = {
+  zoneId: ZoneId.AnabaseiosTheEleventhCircle,
+  damageWarn: {},
+};
+
+export default triggerSet;
