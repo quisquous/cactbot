8f290365336ae3d9e72e3925209564b23397fab8
valarnin <valarnin@gmail.com>
2023-05-25 21:47:39 -0400

    Fix 2nd Terrastorm trigger Add Gale Sphere triggers Add Arctic Assault trigger Add Flames of Eventide trigger

--- ui/raidboss/data/06-ew/trial/golbez-ex.ts
+++ ui/raidboss/data/06-ew/trial/golbez-ex.ts
@@ -8,10 +8,51 @@ import { RaidbossData } from '../../../../../types/data';
 import { PluginCombatantState } from '../../../../../types/event';
 import { TriggerSet } from '../../../../../types/trigger';
 
+const arcticAssaultQuadrants = {
+  '00': 'ne',
+  '01': 'nw',
+  '02': 'ne',
+  '03': 'se',
+  '04': 'se',
+  '05': 'sw',
+  '06': 'nw',
+  '07': 'sw',
+} as const;
+
+type ArcticAssaultSlots = keyof typeof arcticAssaultQuadrants;
+
 export interface Data extends RaidbossData {
+  galeSphereCasts: {
+    x: number;
+    y: number;
+    castTime: number;
+  }[];
   terrastormCount: number;
   terrastormCombatantDirs: number[];
+  arcticAssaultMapEffects: ArcticAssaultSlots[];
 }
+
+// MapEffect info:
+/*
+Slots:
+
+00 = Middle North ice wall, facing east
+01 = Middle North ice wall, facing west
+02 = Center East ice wall, facing north
+03 = Center East ice wall, facing south
+04 = Middle South ice wall, facing east
+05 = Middle South ice wall, facing west
+06 = Center West ice wall, facing north
+07 = Center West ice wall, facing south
+
+08/09/0A/0B/0C/0D = Seen in log, but unknown
+
+Flags:
+For ice wall, `00020001` enables, `00080004` disables
+*/
+
+type GaleDirections = 'n' | 'e' | 's' | 'w';
+type GaleSafeSpots = GaleDirections | 'middle' | 'unknown';
 
 // Calculate combatant position in an all 8 cards/intercards
 const matchedPositionTo8Dir = (combatant: PluginCombatantState) => {
@@ -34,8 +75,18 @@ const triggerSet: TriggerSet<Data> = {
     return {
       terrastormCount: 0,
       terrastormCombatantDirs: [],
+      galeSphereCasts: [],
+      arcticAssaultMapEffects: [],
     };
   },
+  timelineTriggers: [
+    {
+      id: 'GolbezEx Flames of Eventide',
+      regex: /Flames of Eventide 1/,
+      beforeSeconds: 5,
+      response: Responses.tankBusterSwap(),
+    },
+  ],
   triggers: [
     {
       id: 'GolbezEx Terrastorm',
@@ -63,8 +114,7 @@ const triggerSet: TriggerSet<Data> = {
         data.terrastormCombatantDirs.push(matchedPositionTo8Dir(meteor));
       },
       alertText: (data, _matches, output) => {
-        const wanted = data.terrastormCount === 0 ? 2 : 3;
-        if (data.terrastormCombatantDirs.length < wanted)
+        if (data.terrastormCombatantDirs.length < 2)
           return;
 
         const meteors = data.terrastormCombatantDirs;
@@ -217,6 +267,224 @@ const triggerSet: TriggerSet<Data> = {
       alertText: (_data, _matches, output) => output.healerGroups!(),
       outputStrings: {
         healerGroups: Outputs.healerGroups,
+      },
+    },
+    {
+      id: 'GolbezEx Gale Sphere Collector',
+      type: 'StartsUsing',
+      netRegex: { id: '845[89AB]', source: 'Gale Sphere', capture: true },
+      run: (data, matches) => {
+        data.galeSphereCasts.push({
+          x: parseFloat(matches.x),
+          y: parseFloat(matches.y),
+          castTime: parseFloat(matches.castTime),
+        });
+        if (data.galeSphereCasts.length === 16) {
+          data.galeSphereCasts.sort((left, right) => {
+            return left.castTime - right.castTime;
+          });
+        }
+      },
+    },
+    {
+      id: 'GolbezEx Gale Sphere Directions',
+      type: 'StartsUsing',
+      netRegex: { id: '845[89AB]', source: 'Gale Sphere', capture: false },
+      condition: (data) => data.galeSphereCasts.length === 16,
+      infoText: (data, _matches, output) => {
+        const order: (GaleDirections)[] = [];
+
+        data.galeSphereCasts.forEach((sphere) => {
+          let dir: GaleDirections;
+          // x = 115.0 for north, 85.0 for south, etc
+          if (sphere.x > 113)
+            dir = 'e';
+          else if (sphere.y > 113)
+            dir = 's';
+          else if (sphere.x < 87)
+            dir = 'w';
+          // (sphere.y < 87)
+          else
+            dir = 'n';
+
+          if (!order.includes(dir))
+            order.push(dir);
+        });
+
+        const [dir1, dir2, dir3, dir4] = order;
+
+        return output.clones!({
+          dir1: dir1 ?? 'unknown',
+          dir2: dir2 ?? 'unknown',
+          dir3: dir3 ?? 'unknown',
+          dir4: dir4 ?? 'unknown',
+        });
+      },
+      outputStrings: {
+        n: Outputs.dirN,
+        e: Outputs.dirE,
+        s: Outputs.dirS,
+        w: Outputs.dirW,
+        unknown: Outputs.unknown,
+        clones: {
+          en: 'Clones: ${dir1}->${dir2}->${dir3}->${dir4}',
+        },
+      },
+    },
+    {
+      id: 'GolbezEx Gale Safe Spots',
+      type: 'StartsUsing',
+      netRegex: { id: '845[89AB]', source: 'Gale Sphere', capture: false },
+      condition: (data) => data.galeSphereCasts.length === 16,
+      infoText: (data, _matches, output) => {
+        const order: (GaleDirections)[] = [];
+        const safeSpots: { [dir in GaleDirections]: GaleSafeSpots } = {
+          n: 'unknown',
+          e: 'unknown',
+          s: 'unknown',
+          w: 'unknown',
+        };
+
+        data.galeSphereCasts.forEach((sphere) => {
+          let dir: GaleDirections;
+          if (sphere.x > 113)
+            dir = 'e';
+          else if (sphere.y > 113)
+            dir = 's';
+          else if (sphere.x < 87)
+            dir = 'w';
+          else
+            dir = 'n';
+
+          if (!order.includes(dir))
+            order.push(dir);
+        });
+
+        const sphereDirections: { [dir in GaleDirections]: Data['galeSphereCasts'] } = {
+          n: data.galeSphereCasts.filter((sphere) => sphere.y < 87),
+          e: data.galeSphereCasts.filter((sphere) => sphere.x > 113),
+          s: data.galeSphereCasts.filter((sphere) => sphere.y > 113),
+          w: data.galeSphereCasts.filter((sphere) => sphere.x < 87),
+        };
+
+        const possibleDirs = ['n', 'e', 's', 'w'] as const;
+
+        for (const dir of possibleDirs) {
+          const spheres = sphereDirections[dir];
+          const key: 'x' | 'y' = ['n', 's'].includes(dir) ? 'x' : 'y';
+
+          // For these, there are 6 possible cast locations, of which 4 will be present
+          // We only need to check three of the six to determine the safe spot
+          // All of these coordinates are 0.50 higher. To avoid floating point issues
+          // we're just using the floor'd coordinates.
+          const possibleSpots: { [coord: number]: GaleSafeSpots[] } = {
+            112: ['n', 'w'],
+            102: ['middle'],
+            87: ['s', 'e'],
+          };
+
+          for (const sphere of spheres) {
+            delete possibleSpots[Math.floor(sphere[key])];
+          }
+
+          const remainingSpots = Object.values(possibleSpots);
+          const spot = remainingSpots[0];
+          if (remainingSpots.length > 1 || !spot)
+            continue;
+
+          let finalSpot: GaleSafeSpots | undefined = 'unknown';
+
+          if (spot[0] === 'middle')
+            finalSpot = 'middle';
+          else
+            finalSpot = key === 'y' ? spot[0] : spot[1];
+
+          safeSpots[dir] = finalSpot ?? 'unknown';
+        }
+
+        const dirOutputs: string[] = [];
+
+        for (const dir of order) {
+          dirOutputs.push(output[safeSpots[dir]]!());
+        }
+
+        return dirOutputs.join('->');
+      },
+      outputStrings: {
+        n: Outputs.dirN,
+        e: Outputs.dirE,
+        s: Outputs.dirS,
+        w: Outputs.dirW,
+        middle: Outputs.middle,
+        unknown: Outputs.unknown,
+      },
+    },
+    {
+      id: 'GolbezEx Gale Sphere Cleanup',
+      type: 'StartsUsing',
+      netRegex: { id: '845[89AB]', source: 'Gale Sphere', capture: false },
+      condition: (data) => data.galeSphereCasts.length === 16,
+      delaySeconds: 15,
+      run: (data) => {
+        data.galeSphereCasts = [];
+      },
+    },
+    {
+      id: 'GolbezEx Arctic Assault',
+      type: 'MapEffect',
+      netRegex: { location: '0[0-7]', flags: '00020001', capture: true },
+      alertText: (data, matches, output) => {
+        data.arcticAssaultMapEffects.push(matches.location as ArcticAssaultSlots);
+
+        if (data.arcticAssaultMapEffects.length < 2)
+          return;
+
+        const possibleSpots = {
+          'nw': true,
+          'ne': true,
+          'sw': true,
+          'se': true,
+        };
+
+        for (const slot of data.arcticAssaultMapEffects) {
+          delete possibleSpots[arcticAssaultQuadrants[slot]];
+        }
+
+        data.arcticAssaultMapEffects = [];
+
+        const dirOutputs: string[] = [];
+
+        const remainingSpots = Object.keys(possibleSpots);
+
+        if (remainingSpots.length !== 2)
+          return output.unknown!();
+
+        const firstChar = remainingSpots[0]?.[0] ?? '';
+        const lastChar = remainingSpots[0]?.[1] ?? '';
+
+        // Handle the cardinal direction safe case for 2nd arctic assault
+        if (remainingSpots.every((spot) => spot.startsWith(firstChar)))
+          return output[firstChar]!();
+
+        if (remainingSpots.every((spot) => spot.endsWith(lastChar)))
+          return output[lastChar]!();
+
+        for (const dir of Object.keys(possibleSpots)) {
+          dirOutputs.push(output[dir]!());
+        }
+
+        return dirOutputs.join('/');
+      },
+      outputStrings: {
+        nw: Outputs.dirNW,
+        ne: Outputs.dirNE,
+        sw: Outputs.dirSW,
+        se: Outputs.dirSE,
+        n: Outputs.dirN,
+        e: Outputs.dirE,
+        s: Outputs.dirS,
+        w: Outputs.dirW,
+        unknown: Outputs.unknown,
       },
     },
   ],
