38927e53868bfe5c1aaff2265785c750004ddead
Adrienne Walker <enne@quisquo.us>
2023-05-30 22:31:57 -0700

    build: Bump version to 0.29.3

--- plugin/CactbotOverlay/Properties/AssemblyInfo.cs
+++ plugin/CactbotOverlay/Properties/AssemblyInfo.cs
@@ -20,5 +20,5 @@ using System.Runtime.InteropServices;
 // - Build Number
 // - Revision
 // GitHub has only 3 version components, so Revision should always be 0.
-[assembly: AssemblyVersion("0.29.2.0")]
-[assembly: AssemblyFileVersion("0.29.2.0")]
+[assembly: AssemblyVersion("0.29.3.0")]
+[assembly: AssemblyFileVersion("0.29.3.0")]
