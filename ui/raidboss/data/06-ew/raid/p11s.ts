fb9b38ddbaab2a4e13bfb4b96853fa826b097582
xiashtra <91220277+xiashtra@users.noreply.github.com>
2023-06-01 01:33:37 -0400

    raidboss: p10s & p11s - aoe bleeds

--- ui/raidboss/data/06-ew/raid/p11s.ts
+++ ui/raidboss/data/06-ew/raid/p11s.ts
@@ -1,3 +1,4 @@
+import { Responses } from '../../../../../resources/responses';
 import ZoneId from '../../../../../resources/zone_id';
 import { RaidbossData } from '../../../../../types/data';
 import { TriggerSet } from '../../../../../types/trigger';
@@ -8,7 +9,14 @@ const triggerSet: TriggerSet<Data> = {
   id: 'AnabaseiosTheEleventhCircleSavage',
   zoneId: ZoneId.AnabaseiosTheEleventhCircleSavage,
   timelineFile: 'p11s.txt',
-  triggers: [],
+  triggers: [
+    {
+      id: 'P11S Eunomia',
+      type: 'StartsUsing',
+      netRegex: { id: '822B', source: 'Themis', capture: false },
+      response: Responses.bleedAoe(),
+    },
+  ],
 };
 
 export default triggerSet;
