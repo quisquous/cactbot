db37f0886149a448d2774b31d5bdd53e9c840809
Adrienne Walker <enne@quisquo.us>
2023-06-07 22:09:53 -0700

    raidboss: add p10s pandaemoniac ray trigger (#5523)

--- ui/raidboss/data/06-ew/raid/p10s.ts
+++ ui/raidboss/data/06-ew/raid/p10s.ts
@@ -487,6 +487,20 @@ const triggerSet: TriggerSet<Data> = {
         },
       },
     },
+    {
+      id: 'P10S Pandaemoniac Ray',
+      type: 'StartsUsing',
+      netRegex: { id: ['8289', '828B'], source: bossNameUnicode },
+      infoText: (_data, matches, output) => {
+        // Half-room cleave west (8289) or east (828B)
+        const safeOutput = matches.id === '8289' ? 'east' : 'west';
+        return output[safeOutput]!();
+      },
+      outputStrings: {
+        east: Outputs.getRightAndEast,
+        west: Outputs.getLeftAndWest,
+      },
+    },
   ],
   timelineReplace: [
     {
