826fcb5034612cbec9392c952b0cbbe5ae75dc10
柚木 鉉 <GloomyGhost@GloomyGhost.com>
2023-06-14 00:22:55 +0800

    i18n: add chinese translate

--- ui/raidboss/data/06-ew/trial/golbez.ts
+++ ui/raidboss/data/06-ew/trial/golbez.ts
@@ -28,6 +28,7 @@ const triggerSet: TriggerSet<Data> = {
         text: {
           en: 'Avoid meteor',
           de: 'Vermeide Meteor',
+          cn: '远离陨石',
           ko: '메테오 피하기',
         },
       },
@@ -65,6 +66,7 @@ const triggerSet: TriggerSet<Data> = {
         text: {
           en: 'Get behind, then out',
           de: 'Geh hinter, dann raus',
+          cn: '去背后 => 远离',
           ko: '보스 뒤로 => 밖으로',
         },
       },
@@ -109,6 +111,7 @@ const triggerSet: TriggerSet<Data> = {
         clones: {
           en: 'Clones: ${dir1}->${dir2}->${dir3}->${dir4}',
           de: 'Klone: ${dir1}->${dir2}->${dir3}->${dir4}',
+          cn: '分身：${dir1}->${dir2}->${dir3}->${dir4}',
           ko: '분신: ${dir1}->${dir2}->${dir3}->${dir4}',
         },
         unknown: Outputs.unknown,
@@ -133,6 +136,7 @@ const triggerSet: TriggerSet<Data> = {
         text: {
           en: 'Out first => stack w/ ${player}',
           de: 'Zuerst Raus => Sammeln mit ${player}',
+          cn: '远离 => 与 ${player} 分摊',
           ko: '밖으로 => ${player} 쉐어',
         },
       },
