565ac892195e1a4b79bd1f3135b6ebb30c106c85
柚木 鉉 <GloomyGhost@GloomyGhost.com>
2023-06-18 10:52:02 +0800

    i18n: add Chinese for Jade Passage

--- ui/raidboss/data/06-ew/raid/p10s.ts
+++ ui/raidboss/data/06-ew/raid/p10s.ts
@@ -586,9 +586,11 @@ const triggerSet: TriggerSet<Data> = {
       outputStrings: {
         lines: {
           en: 'On Lines (Avoid Lasers)',
+          cn: '站在线上（躲避激光）',
         },
         boxes: {
           en: 'Inside Boxes (Avoid Lasers)',
+          cn: '站在盒子里（躲避激光）',
         },
       },
     },
