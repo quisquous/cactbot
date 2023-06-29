f67933bd003456e15956273eee2e07a4e5d0da7f
ksh <kshman@msn.com>
2023-06-30 04:08:54 +0900

    raidboss: more P12S second phase (#5620)
    I made this PR for someone who can make more better triggers.
    If this PR is not enough for purpose, please discard this.
    Thank you.
    
    Added:
    * Palladian Grasp
    * Gaiaochos
    * The Classical Concepts
    * Caloric Theory

--- ui/raidboss/data/06-ew/raid/p12s.ts
+++ ui/raidboss/data/06-ew/raid/p12s.ts
@@ -27,6 +27,15 @@ import { TriggerSet } from '../../../../../types/trigger';
 // TODO: add triggerset ui for playstation order + classical location
 // TODO: detect(?!) hex strat for caloric2 and tell people who to go to??
 
+type Phase =
+  | 'superchain1'
+  | 'palladion'
+  | 'superchain2a'
+  | 'superchain2b'
+  | 'gaiaochos'
+  | 'classical'
+  | 'caloric';
+
 const centerX = 100;
 const centerY = 100;
 
@@ -179,6 +188,9 @@ const pangenesisEffectIds: readonly string[] = Object.values(pangenesisEffects);
 
 type PangenesisRole = 'shortLight' | 'shortDark' | 'longLight' | 'longDark' | 'one' | 'not';
 
+type PlaystationMarker = 'circle' | 'cross' | 'triangle' | 'square';
+type CaloricMarker = 'fire' | 'wind';
+
 const getHeadmarkerId = (data: Data, matches: NetMatches['HeadMarker']) => {
   if (data.decOffset === undefined) {
     if (data.expectedFirstHeadmarker === undefined) {
@@ -195,7 +207,7 @@ export interface Data extends RaidbossData {
   decOffset?: number;
   expectedFirstHeadmarker?: string;
   isDoorBoss: boolean;
-  phase?: 'superchain1' | 'palladion' | 'superchain2a' | 'superchain2b';
+  phase?: Phase;
   combatantData: PluginCombatantState[];
   paradeigmaCounter: number;
   glaukopisFirstHit?: string;
@@ -226,6 +238,17 @@ export interface Data extends RaidbossData {
   pangenesisTowerCount: number;
   lastPangenesisTowerColor?: 'light' | 'dark';
   pangenesisCurrentColor?: 'light' | 'dark';
+  gaiaochosCounter: number;
+  palladionGrapsTarget?: string;
+  classicalCounter: number;
+  classicalMarker: { [name: string]: PlaystationMarker };
+  classicalAlphaBeta: { [name: string]: 'alpha' | 'beta' };
+  caloricCounter: number;
+  caloric1First: string[];
+  caloric1Buff: { [name: string]: CaloricMarker };
+  caloric1Mine?: CaloricMarker;
+  caloric2Fire?: string;
+  caloric2Count: number;
 }
 
 const triggerSet: TriggerSet<Data> = {
@@ -288,6 +311,14 @@ const triggerSet: TriggerSet<Data> = {
       whiteFlameCounter: 0,
       pangenesisRole: {},
       pangenesisTowerCount: 0,
+      gaiaochosCounter: 0,
+      classicalCounter: 0,
+      classicalMarker: {},
+      classicalAlphaBeta: {},
+      caloricCounter: 0,
+      caloric1First: [],
+      caloric1Buff: {},
+      caloric2Count: 0,
     };
   },
   triggers: [
@@ -299,7 +330,7 @@ const triggerSet: TriggerSet<Data> = {
         data.whiteFlameCounter = 0;
         data.superchainCollect = [];
 
-        const phaseMap: { [id: string]: Data['phase'] } = {
+        const phaseMap: { [id: string]: Phase } = {
           '82DA': 'superchain1',
           '82F5': 'palladion',
           '86FA': 'superchain2a',
@@ -316,6 +347,27 @@ const triggerSet: TriggerSet<Data> = {
       run: (data) => {
         data.isDoorBoss = false;
         data.expectedFirstHeadmarker = headmarkers.palladianGrasp;
+      },
+    },
+    {
+      id: 'P12S Phase Tracker 3',
+      type: 'StartsUsing',
+      netRegex: { id: ['8326', '8331', '8338'], source: 'Pallas Athena' },
+      run: (data, matches) => {
+        switch (matches.id) {
+          case '8326':
+            data.phase = 'gaiaochos';
+            data.gaiaochosCounter++;
+            break;
+          case '8331':
+            data.phase = 'classical';
+            data.classicalCounter++;
+            break;
+          case '8338':
+            data.phase = 'caloric';
+            data.caloricCounter++;
+            break;
+        }
       },
     },
     {
@@ -2258,6 +2310,474 @@ const triggerSet: TriggerSet<Data> = {
           return { alertText: output.lightTowerSwitch!() };
         return { alertText: output.darkTowerSwitch!() };
       },
+    },
+    {
+      id: 'P12S Second Phase Tether',
+      type: 'Tether',
+      netRegex: { id: '0001' },
+      suppressSeconds: 2,
+      alertText: (data, matches, output) => {
+        if (data.phase === 'gaiaochos' && data.gaiaochosCounter === 2) {
+          // Gaiaochos tether block
+          if (data.party.isDPS(matches.target)) {
+            if (data.role !== 'dps')
+              return output.blockPartner!();
+          } else if (data.role === 'dps')
+            return output.blockPartner!();
+        } /* else if (data.phase === 'classical') {
+          // The Classical Concepts
+          // reminder or alert escape position?
+        } */
+      },
+      outputStrings: {
+        blockPartner: {
+          en: 'Block tether for partner', // FIX-ME
+          ja: '相棒の前でビームを受ける',
+        },
+      },
+    },
+    {
+      id: 'P12S Ultima',
+      type: 'StartsUsing',
+      netRegex: { id: ['8682', '86F6'], source: 'Pallas Athena', capture: false },
+      response: Responses.bigAoe('alert'),
+    },
+    {
+      id: 'P12S Palladian Grasp Target',
+      type: 'HeadMarker',
+      netRegex: {},
+      run: (data, matches) => {
+        const id = getHeadmarkerId(data, matches);
+        if (id === headmarkers.palladianGrasp)
+          data.palladionGrapsTarget = matches.target;
+      },
+    },
+    {
+      id: 'P12S Palladian Grasp',
+      type: 'StartsUsing',
+      netRegex: { id: '831A', source: 'Pallas Athena', capture: false },
+      alertText: (data, _match, output) => {
+        if (data.palladionGrapsTarget === data.me)
+          return output.target!();
+        return output.avoid!();
+      },
+      outputStrings: {
+        target: Outputs.tankBusterCleaves,
+        avoid: Outputs.avoidTankCleaves,
+      },
+    },
+    {
+      id: 'P12S Gaiaochos',
+      type: 'StartsUsing',
+      netRegex: { id: '8326', source: 'Pallas Athena', capture: false },
+      response: Responses.bigAoe('alert'),
+    },
+    {
+      id: 'P12S Gaiaochos tether',
+      type: 'Tether',
+      netRegex: { id: '0009' },
+      infoText: (data, matches, output) => {
+        if (matches.source !== data.me && matches.target !== data.me)
+          return;
+        const partner = matches.source === data.me ? matches.target : matches.source;
+        return output.text!({ partner: data.ShortName(partner) });
+      },
+      outputStrings: {
+        text: {
+          en: 'Break tether! (w/ ${partner})',
+          ja: '線切る (${partner})',
+        },
+      },
+    },
+    {
+      id: 'P12S The Classical Concepts PS marker',
+      type: 'HeadMarker',
+      netRegex: {},
+      run: (data, matches) => {
+        const id = getHeadmarkerId(data, matches);
+        const psMarkerMap: { [id: string]: PlaystationMarker } = {
+          '016F': 'circle',
+          '0170': 'triangle',
+          '0171': 'square',
+          '0172': 'cross',
+        };
+        const marker = psMarkerMap[id];
+        if (marker !== undefined)
+          data.classicalMarker[matches.target] = marker;
+      },
+    },
+    {
+      id: 'P12S The Classical Concepts Alpha Beta',
+      type: 'GainsEffect',
+      netRegex: { effectId: ['DE8', 'DE9'] },
+      run: (data, matches) => {
+        if (matches.effectId === 'DE8')
+          data.classicalAlphaBeta[matches.target] = 'alpha';
+        else
+          data.classicalAlphaBeta[matches.target] = 'beta';
+      },
+    },
+    {
+      id: 'P12S The Classical Concepts Inversion',
+      type: 'StartsUsing',
+      netRegex: { id: '8331', source: 'Pallas Athena', capture: false },
+      condition: (data) => data.classicalCounter === 2,
+      delaySeconds: 12,
+      alertText: (_data, _matches, output) => output.inversion!(),
+      outputStrings: {
+        inversion: {
+          en: 'Go to inversion position', // FIX-ME
+          ja: '反転位置へ移動',
+        },
+      },
+    },
+    {
+      id: 'P12S The Classical Concepts',
+      type: 'Ability',
+      netRegex: { id: '8331', source: 'Pallas Athena', capture: false },
+      delaySeconds: 2,
+      durationSeconds: (data) => data.classicalCounter === 1 ? 9 : 16,
+      suppressSeconds: 1,
+      infoText: (data, _matches, output) => {
+        const marker = data.classicalMarker[data.me];
+        const tether = data.classicalAlphaBeta[data.me];
+        if (marker === undefined || tether === undefined)
+          return;
+        return output.text!({ marker: output[marker]!(), tether: output[tether]!() });
+      },
+      outputStrings: {
+        text: {
+          en: '${marker} + ${tether}',
+          ja: '${marker} + ${tether}',
+        },
+        circle: {
+          en: 'Circle',
+          de: 'Kreis',
+          fr: 'Cercle',
+          ja: 'まる',
+          cn: '圆圈',
+          ko: '동그라미',
+        },
+        triangle: {
+          en: 'Triangle',
+          de: 'Dreieck',
+          fr: 'Triangle',
+          ja: 'さんかく',
+          cn: '三角',
+          ko: '삼각',
+        },
+        square: {
+          en: 'Square',
+          de: 'Viereck',
+          fr: 'Carré',
+          ja: 'しかく',
+          cn: '方块',
+          ko: '사각',
+        },
+        cross: {
+          en: 'X',
+          de: 'X',
+          fr: 'Croix',
+          ja: 'バツ',
+          cn: 'X',
+          ko: 'X',
+        },
+        alpha: {
+          en: 'Alpha',
+          de: 'Alpha',
+          fr: 'Alpha',
+          ja: 'アルファ',
+          cn: '阿尔法',
+          ko: '알파',
+        },
+        beta: {
+          en: 'Beta',
+          de: 'Beta',
+          fr: 'Beta',
+          ja: 'ベータ',
+          cn: '贝塔',
+          ko: '베타',
+        },
+      },
+    },
+    {
+      id: 'P12S The Classical Concepts Move',
+      type: 'Ability',
+      netRegex: { id: '8323', source: 'Pallas Athena', capture: false },
+      delaySeconds: 2.5,
+      alarmText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'MOVE!',
+          ja: '回避！',
+        },
+      },
+    },
+    {
+      id: 'P12S Caloric Theory 1 First Fire',
+      type: 'HeadMarker',
+      netRegex: {},
+      run: (data, matches) => {
+        const id = getHeadmarkerId(data, matches);
+        if (id === '012F')
+          data.caloric1First.push(matches.target);
+      },
+    },
+    {
+      id: 'P12S Caloric Theory 1 Alert First Fire',
+      type: 'StartsUsing',
+      netRegex: { id: '8338', source: 'Pallas Athena', capture: false },
+      condition: (data) => data.caloricCounter === 1,
+      preRun: (data) => {
+        data.caloric1Buff = {};
+        data.caloric1Mine = undefined;
+      },
+      delaySeconds: 1,
+      alertText: (data, _matches, output) => {
+        if (data.caloric1First.length !== 2)
+          return;
+        const index = data.caloric1First.indexOf(data.me);
+        if (index < 0)
+          return;
+        const partner = index === 0 ? 1 : 0;
+        return output.text!({ partner: data.ShortName(data.caloric1First[partner]) });
+      },
+      outputStrings: {
+        text: {
+          en: 'First fire on YOU (w/ ${partner})', // FIX-ME
+          ja: '自分に初炎 (${partner})',
+        },
+      },
+    },
+    {
+      id: 'P12S Caloric Theory 1 Wind', // Atmosfaction
+      type: 'GainsEffect',
+      netRegex: { effectId: 'E07' },
+      run: (data, matches) => data.caloric1Buff[matches.target] = 'wind',
+    },
+    {
+      id: 'P12S Caloric Theory 1 Fire', // Pyrefaction
+      type: 'GainsEffect',
+      netRegex: { effectId: 'E06' },
+      alertText: (data, matches, output) => {
+        data.caloric1Buff[matches.target] = 'fire';
+        const duration = parseFloat(matches.duration);
+        if (duration < 12 && matches.target === data.me)
+          return output.text!();
+      },
+      outputStrings: {
+        text: {
+          en: 'Fire again on YOU!', // FIX-ME
+          ja: '再び炎！無職とあたまわり',
+        },
+      },
+    },
+    {
+      id: 'P12S Caloric Theory 1 Fire Result',
+      type: 'GainsEffect',
+      netRegex: { effectId: ['E06'] },
+      condition: (_data, matches) => parseFloat(matches.duration) > 11,
+      delaySeconds: 12.8,
+      suppressSeconds: 2,
+      alertText: (data, _matches, output) => {
+        if (data.caloric1Mine === 'fire' && data.caloric1Buff[data.me] === undefined)
+          return output.none!();
+        if (data.caloric1Mine === 'wind')
+          return output.wind!();
+      },
+      outputStrings: {
+        none: {
+          en: 'No Marker!', // FIX-ME
+          ja: '無職！炎とあたまわり',
+        },
+        wind: {
+          en: 'Wind on YOU!', // FIX-ME
+          ja: '風！ 散会',
+        },
+      },
+    },
+    {
+      id: 'P12S Caloric Theory 1',
+      type: 'Ability',
+      netRegex: { id: '8338', source: 'Pallas Athena', capture: false },
+      condition: (data) => data.caloricCounter === 1,
+      delaySeconds: 2,
+      durationSeconds: 8,
+      suppressSeconds: 1,
+      infoText: (data, _matches, output) => {
+        const myBuff = data.caloric1Buff[data.me];
+        data.caloric1Mine = myBuff;
+        if (myBuff === undefined)
+          return;
+
+        if (myBuff === 'fire') {
+          const myTeam: string[] = [];
+          for (const [name, stat] of Object.entries(data.caloric1Buff)) {
+            if (stat === myBuff && name !== data.me)
+              myTeam.push(data.ShortName(name));
+          }
+          return output.fire!({ team: myTeam.sort().join(', ') });
+        }
+
+        if (data.caloric1First.includes(data.me))
+          return output.wind1st!();
+
+        const myTeam: string[] = [];
+        for (const [name, stat] of Object.entries(data.caloric1Buff)) {
+          if (stat === myBuff && name !== data.me && !data.caloric1First.includes(name))
+            myTeam.push(data.ShortName(name));
+        }
+        return output.wind!({ team: myTeam.sort().join(', ') });
+      },
+      run: (data) => {
+        data.caloric1First = [];
+        data.caloric1Buff = {};
+      },
+      outputStrings: {
+        fire: {
+          en: 'Fire on YOU (${team})', // FIX-ME
+          ja: '自分に炎 (${team})',
+        },
+        wind: {
+          en: 'Wind on YOU (${team})', // FIX-ME
+          ja: '自分に風 (${team})',
+        },
+        wind1st: {
+          en: 'First wind on YOU', // FIX-ME
+          ja: '自分に初風',
+        },
+      },
+    },
+    {
+      id: 'P12S Caloric Theory 2 Fire',
+      type: 'HeadMarker',
+      netRegex: {},
+      alertText: (data, matches, output) => {
+        const id = getHeadmarkerId(data, matches);
+        if (id !== headmarkers.caloric2InitialFire)
+          return;
+        data.caloric2Fire = matches.target;
+        if (data.me === matches.target)
+          return output.fireOnMe!();
+        return output.fireOn!({ player: data.ShortName(matches.target) });
+      },
+      run: (data) => data.caloric2Count = 0,
+      outputStrings: {
+        fireOnMe: {
+          en: 'First fire on YOU!', // FIX-ME
+          ja: '自分に初炎!',
+        },
+        fireOn: {
+          en: 'First fire: ${player}', //  FIX ME
+          ja: '初炎: ${player}',
+        },
+      },
+    },
+    {
+      id: 'P12S Caloric Theory 2 Wind',
+      type: 'HeadMarker',
+      netRegex: {},
+      condition: (data, matches) => data.me === matches.target,
+      infoText: (data, matches, output) => {
+        const id = getHeadmarkerId(data, matches);
+        if (id !== headmarkers.caloric2Wind)
+          return;
+        return output.text!();
+      },
+      outputStrings: {
+        text: {
+          en: 'Wind on YOU', // FIX-ME
+          ja: '自分に風、散会',
+        },
+      },
+    },
+    {
+      id: 'P12S Caloric Theory 2 Fire Puddle',
+      type: 'GainsEffect',
+      netRegex: { effectId: ['E08'] },
+      response: (data, matches, output) => {
+        // cactbot-builtin-response
+        output.responseOutputStrings = {
+          puddle: {
+            en: '#${num} Puddle on YOU', // FIX-ME
+            ja: '自分に${num}番目の炎',
+          },
+          puddle1st: {
+            en: 'First Puddle on YOU! Pass to next!', // FIX-ME
+            ja: '自分に1番目の炎！次に移る！',
+          },
+        };
+
+        if (data.caloric2Fire !== matches.target)
+          data.caloric2Count++;
+        if (matches.target === data.me && data.caloric2Fire !== data.me) {
+          if (data.caloric2Count === 1)
+            return { alertText: output.puddle1st!() };
+          return { infoText: output.puddle!({ num: data.caloric2Count }) };
+        }
+      },
+      run: (data, matches) => data.caloric2Fire = matches.target,
+    },
+    {
+      id: 'P12S Caloric Theory 2 Move',
+      type: 'Ability',
+      netRegex: { id: '833C', source: 'Pallas Athena', capture: false },
+      condition: (data) => data.caloricCounter === 2,
+      alertText: (data, _matches, output) => {
+        if (data.me === data.caloric2Fire) {
+          if (data.caloric2Count === 7) {
+            // Depends on strategy. Drop puddle on empty area
+            return output.pass7th!();
+          }
+          if (data.caloric2Count === 8) {
+            // No one has fire debuff but last puddle appears
+            return output.final!();
+          }
+          return output.pass!();
+        }
+      },
+      run: (data) => {
+        if (data.caloric2Count === 7)
+          data.caloric2Count++;
+      },
+      outputStrings: {
+        pass: {
+          en: 'Pass to next!', // FIX-ME
+          ja: '次に移る！',
+        },
+        pass7th: {
+          en: 'Drop puddle!', // FIX-ME
+          ja: '空き空間に捨てる！',
+        },
+        final: {
+          en: 'Drop the last puddle!', // FIX-ME
+          ja: 'ラスト！空き空間に捨てる！',
+        },
+      },
+    },
+    {
+      id: 'P12S Ekpyrosis',
+      type: 'StartsUsing',
+      netRegex: { id: '831E', source: 'Pallas Athena', capture: false },
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Exaflare + Big AoE!', // FIX-ME
+          de: 'Exaflare + Große AoE!', // FIX-ME
+          fr: 'ExaBrasier + Grosse AoE!', // FIX-ME
+          ja: 'エクサフレア + 全体攻撃',
+          cn: '地火 + 大AoE伤害！', // FIX-ME
+          ko: '엑사플레어 + 전체 공격!', // FIX-ME
+        },
+      },
+    },
+    {
+      id: 'P12S Ekpyrosis Move',
+      type: 'Ability',
+      netRegex: { id: '831F', source: 'Pallas Athena', capture: false },
+      delaySeconds: 0.5,
+      suppressSeconds: 2,
+      response: Responses.spread('alarm'),
     },
   ],
   timelineReplace: [
