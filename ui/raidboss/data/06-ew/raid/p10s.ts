quisquous/cactbot

commit 6b5c47441a72e2c29e18a76096630dd4a343b0c1

raidboss: fix P10S bond and other triggers for non-English (#5662)
The translation of the boss name did not work properly and so no trigger
with a `source` property was firing.

Fixes #5643.

ui/raidboss/data/06-ew/raid/p10s.ts
@@ -634,8 +634,8 @@ const triggerSet: TriggerSet<Data> = {
       'missingTranslations': true,
       'replaceSync': {
         'Arcane Sphere': 'arkan(?:e|er|es|en) Körper',
-        'Pand\\\\u00e6moniac Pillar': 'pand\\u00e6monisch(?:e|er|es|en) Turm',
-        'Pand\\\\u00e6monium': 'Pand\\u00e6monium',
+        'Pand\u00e6moniac Pillar': 'pand\u00e6monisch(?:e|er|es|en) Turm',
+        'Pand\u00e6monium': 'Pand\u00e6monium',
       },
       'replaceText': {
         '\\(cast\\)': '(Wirken)',
@@ -671,8 +671,8 @@ const triggerSet: TriggerSet<Data> = {
       'missingTranslations': true,
       'replaceSync': {
         'Arcane Sphere': 'sphère arcanique',
-        'Pand\\\\u00e6moniac Pillar': 'pilier pand\\u00e6moniaque',
-        'Pand\\\\u00e6monium': 'Pand\\u00e6monium',
+        'Pand\u00e6moniac Pillar': 'pilier pand\u00e6moniaque',
+        'Pand\u00e6monium': 'Pand\u00e6monium',
       },
       'replaceText': {
         'Bury': 'Impact',
@@ -704,8 +704,8 @@ const triggerSet: TriggerSet<Data> = {
       'missingTranslations': true,
       'replaceSync': {
         'Arcane Sphere': '立体魔法陣',
-        'Pand\\\\u00e6moniac Pillar': '万魔殿の塔',
-        'Pand\\\\u00e6monium': 'パンデモニウム',
+        'Pand\u00e6moniac Pillar': '万魔殿の塔',
+        'Pand\u00e6monium': 'パンデモニウム',
       },
       'replaceText': {
         'Bury': '衝撃',

commit d392d4d15c6ab49087afdda6d74553e977053ea4

raidboss: take two on P10S translations
Sorry, this is more complicated than it should be.
Maybe we can avoid unnecessary escaping in the future.

Caused by #5662 (sorry).
Fixes #5664.

ui/raidboss/data/06-ew/raid/p10s.ts
@@ -631,11 +631,10 @@ const triggerSet: TriggerSet<Data> = {
     },
     {
       'locale': 'de',
-      'missingTranslations': true,
       'replaceSync': {
         'Arcane Sphere': 'arkan(?:e|er|es|en) Körper',
-        'Pand\u00e6moniac Pillar': 'pand\u00e6monisch(?:e|er|es|en) Turm',
-        'Pand\u00e6monium': 'Pand\u00e6monium',
+        'Pand(?:\\\\u00e6|\u00e6)moniac Pillar': 'pand\u00e6monisch(?:e|er|es|en) Turm',
+        'Pand(?:\\\\u00e6|\u00e6)monium': 'Pand\u00e6monium',
       },
       'replaceText': {
         '\\(cast\\)': '(Wirken)',
@@ -668,11 +667,10 @@ const triggerSet: TriggerSet<Data> = {
     },
     {
       'locale': 'fr',
-      'missingTranslations': true,
       'replaceSync': {
         'Arcane Sphere': 'sphère arcanique',
-        'Pand\u00e6moniac Pillar': 'pilier pand\u00e6moniaque',
-        'Pand\u00e6monium': 'Pand\u00e6monium',
+        'Pand(?:\\\\u00e6|\u00e6)moniac Pillar': 'pilier pand\u00e6moniaque',
+        'Pand(?:\\\\u00e6|\u00e6)monium': 'Pand\u00e6monium',
       },
       'replaceText': {
         'Bury': 'Impact',
@@ -701,11 +699,10 @@ const triggerSet: TriggerSet<Data> = {
     },
     {
       'locale': 'ja',
-      'missingTranslations': true,
       'replaceSync': {
         'Arcane Sphere': '立体魔法陣',
-        'Pand\u00e6moniac Pillar': '万魔殿の塔',
-        'Pand\u00e6monium': 'パンデモニウム',
+        'Pand(?:\\\\u00e6|\u00e6)moniac Pillar': '万魔殿の塔',
+        'Pand(?:\\\\u00e6|\u00e6)monium': 'パンデモニウム',
       },
       'replaceText': {
         'Bury': '衝撃',