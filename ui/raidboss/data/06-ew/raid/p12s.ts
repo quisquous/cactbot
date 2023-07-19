6aac711c41fe2006a06bb5ae8ecd5eb319044168
Akurosia Kamo <kamothegamer@web.de>
2023-07-19 03:11:07 +0200

    i18n: fix p12s callout (#5709)

--- ui/raidboss/data/06-ew/raid/p12s.ts
+++ ui/raidboss/data/06-ew/raid/p12s.ts
@@ -1756,7 +1756,7 @@ const triggerSet: TriggerSet<Data> = {
         },
         tanksInPartyOut: {
           en: 'Tanks In (Party Out)',
-          de: 'Gruppe Rein (Tanks Raus)',
+          de: 'Tanks Rein (Gruppe Raus)',
           fr: 'Tanks à l\'intérieur (Équipe à l\'extérieur',
           ja: 'ボスに足元へ (パーティーは離れる)',
           cn: 'T进 (小队出)',
