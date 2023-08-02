9719a2ab1f7cbbc244870b97b4f00fe5460760a7
Souma-Sumire <553469159@qq.com>
2023-08-02 08:39:41 +0800

    Update trigger.d.ts
    that should be deleted.

--- types/trigger.d.ts
+++ types/trigger.d.ts
@@ -125,7 +125,6 @@ export type BaseTrigger<
 
 type BaseNetTrigger<Data extends RaidbossData, Type extends TriggerTypes> = {
   id: string;
-  label?: Partial<FullLocaleText>;
   type: Type;
   netRegex: NetParams[Type];
   disabled?: boolean;
