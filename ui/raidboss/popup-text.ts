431f663f3c33edfea72877c88a6b1aca59b5157e
Souma <33572696+Souma-Sumire@users.noreply.github.com>
2023-09-10 18:42:22 +0800

    Update ui/raidboss/popup-text.ts
    Co-authored-by: Adrienne Walker <enne@quisquo.us>

--- ui/raidboss/popup-text.ts
+++ ui/raidboss/popup-text.ts
@@ -921,7 +921,7 @@ export class PopupText {
 
     trigger.output = TriggerOutputProxy.makeOutput(
       trigger,
-      this.options.AlertsLanguage ?? this.options.DisplayLanguage,
+      this.displayLang,
       this.options.PerTriggerAutoConfig,
     );
   }
