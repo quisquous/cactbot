87f87228e3f1e5e5445069d338261c24f658eca7
valarnin <valarnin@gmail.com>
2023-10-05 10:28:14 -0400

    Drop Miasmic Blast trigger

--- ui/raidboss/data/06-ew/trial/zeromus-ex.ts
+++ ui/raidboss/data/06-ew/trial/zeromus-ex.ts
@@ -1,5 +1,4 @@
 import Outputs from '../../../../../resources/outputs';
-import { callOverlayHandler } from '../../../../../resources/overlay_plugin_api';
 import { Responses } from '../../../../../resources/responses';
 import ZoneId from '../../../../../resources/zone_id';
 import { RaidbossData } from '../../../../../types/data';
@@ -7,7 +6,6 @@ import { NetMatches } from '../../../../../types/net_matches';
 import { TriggerSet } from '../../../../../types/trigger';
 
 export interface Data extends RaidbossData {
-  miasmicBlastPositions?: ({ x: number; y: number })[];
   decOffset?: number;
 }
 
@@ -67,109 +65,6 @@ const triggerSet: TriggerSet<Data> = {
         },
         nw: Outputs.northwest,
         se: Outputs.southeast,
-      },
-    },
-    {
-      id: 'ZeromusEx Miasmic Blast',
-      type: 'StartsUsing',
-      netRegex: { id: '8B49', capture: true },
-      delaySeconds: 0.5,
-      promise: async (data, matches) => {
-        // TODO: This trigger does not work 100% of the time in raidemulator and needs more testing in-zone
-        data.miasmicBlastPositions ??= [];
-
-        const combatants = (await callOverlayHandler({
-          call: 'getCombatants',
-          ids: [parseInt(matches.sourceId, 16)],
-        })).combatants;
-
-        if (combatants === undefined || combatants.length !== 1) {
-          console.error(
-            `Miasmic Blast: unexpected response to getCombatants, got ${
-              JSON.stringify(combatants)
-            }`,
-          );
-          return;
-        }
-
-        const combatant = combatants[0];
-
-        if (combatant === undefined)
-          return;
-
-        data.miasmicBlastPositions.push({
-          x: combatant.PosX,
-          y: combatant.PosY,
-        });
-      },
-      infoText: (data, _matches, output) => {
-        if (data.miasmicBlastPositions === undefined || data.miasmicBlastPositions.length < 3)
-          return;
-        const possibleSafeSpots = [
-          'WNW',
-          'NW',
-          'NNW',
-          'NNE',
-          'NE',
-          'ENE',
-        ] as const;
-        type safeSpotType = typeof possibleSafeSpots[number];
-        // There's probably a better way to handle this rather than an exhaustive check
-        let safeSpots: safeSpotType[] = [
-          'WNW',
-          'NW',
-          'NNW',
-          'NNE',
-          'NE',
-          'ENE',
-        ];
-
-        // From the center, Xs always spawn 0y, 7y, or 14y on either axis away from middle
-        // So treat safe spots as half way between those possible points, on the edge
-        const safeSpotMap: {
-          [key in safeSpotType]: { x: number; y: number };
-        } = {
-          'WNW': { x: 80, y: 94 },
-          'NW': { x: 80, y: 80 },
-          'NNW': { x: 94, y: 80 },
-          'NNE': { x: 106, y: 80 },
-          'NE': { x: 120, y: 80 },
-          'ENE': { x: 120, y: 94 },
-        };
-
-        for (const pos of data.miasmicBlastPositions) {
-          const removeSpots: safeSpotType[] = [];
-          for (const spot of safeSpots) {
-            // If this blast is at a 45º angle to the safe spot, remove the safe spot
-            const angle =
-              ((Math.atan2(pos.y - safeSpotMap[spot].y, pos.x - safeSpotMap[spot].x) * 180 /
-                Math.PI) + 180) % 90;
-            if (Math.abs(angle - 45) < Number.EPSILON) {
-              removeSpots.push(spot);
-            }
-          }
-          safeSpots = safeSpots.filter((spot) => !removeSpots.includes(spot));
-        }
-
-        delete data.miasmicBlastPositions;
-
-        if (safeSpots.length !== 1) {
-          console.error(`Miasmic Blast: Could not find safe spot`, safeSpots);
-          return output.unknown!();
-        }
-
-        const safeSpot = safeSpots[0] ?? 'unknown';
-
-        return output[safeSpot]!();
-      },
-      outputStrings: {
-        WNW: Outputs.dirWNW,
-        NW: Outputs.dirNW,
-        NNW: Outputs.dirNNW,
-        NNE: Outputs.dirNNE,
-        NE: Outputs.dirNE,
-        ENE: Outputs.dirENE,
-        unknown: Outputs.unknown,
       },
     },
     {
