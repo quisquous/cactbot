7494da55064d330c94a8f442504b997c70aad151
Adrienne Walker <enne@quisquo.us>
2023-10-09 13:22:15 -0700

    raidboss: update wording for ex7 miasmic (#5847)
    Most players seem to dodge north for this (even though it makes the
    spread tough). Also, the current version orders the outputs by their
    ordering in the array, which means that sometimes the "north" direction
    is first and sometimes second.
    
    To add clarity, only consider safe spots on the north side of the room,
    and then add explicit wording for where to go.
    
    This is maybe controversial as "east wall" could be ambiguous between
    ENE and ESE but it felt like to me that if most people dodge north, we
    can just say the "north" safe spot with details in parentheses. I found
    this to be more clear, at least.
    
    This output strings change would also let people change to using marker
    names in the output strings more easily.

--- ui/raidboss/data/06-ew/trial/zeromus-ex.ts
+++ ui/raidboss/data/06-ew/trial/zeromus-ex.ts
@@ -228,7 +228,16 @@ const triggerSet: TriggerSet<Data> = {
           intercard: [-2, 0, 2, 3, 8, 13],
         };
 
-        let possibleSafeSpots = Directions.output16Dir;
+        // Filter to north half.
+        const validSafeSpots = [
+          'dirNNE',
+          'dirNE',
+          'dirENE',
+          'dirWNW',
+          'dirNW',
+          'dirNNW',
+        ] as const;
+        let possibleSafeSpots = [...validSafeSpots];
 
         for (const blast of data.miasmicBlasts) {
           // special case for center - don't need to find relative dirs, just remove all intercards
@@ -252,25 +261,38 @@ const triggerSet: TriggerSet<Data> = {
           }
         }
 
-        if (possibleSafeSpots.length !== 2)
+        if (possibleSafeSpots.length !== 1)
           return output.avoidUnknown!();
 
-        const [safeDir1, safeDir2] = possibleSafeSpots;
-        if (safeDir1 === undefined || safeDir2 === undefined)
+        const [safeDir] = possibleSafeSpots;
+        if (safeDir === undefined)
           return output.avoidUnknown!();
 
-        return output.combo!({ dir1: output[safeDir1]!(), dir2: output[safeDir2]!() });
+        return output[safeDir]!();
       },
       outputStrings: {
-        combo: {
-          en: '${dir1} / ${dir2}',
-          de: '${dir1} / ${dir2}',
-        },
         avoidUnknown: {
           en: 'Avoid Line Cleaves',
           de: 'Weiche den Linien Cleaves aus',
         },
-        ...Directions.outputStrings16Dir,
+        dirNNE: {
+          en: 'North Wall (NNE/WSW)',
+        },
+        dirNNW: {
+          en: 'North Wall (NNW/ESE)',
+        },
+        dirNE: {
+          en: 'Corners (NE/SW)',
+        },
+        dirNW: {
+          en: 'Corners (NW/SE)',
+        },
+        dirENE: {
+          en: 'East Wall (ENE/SSW)',
+        },
+        dirWNW: {
+          en: 'West Wall (WNW/SSE)',
+        },
       },
     },
     {
