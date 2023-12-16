b5af82f0ce6ddabc4814b63cbfdae57c3682e4ad
mooondark <40036185+mooondark@users.noreply.github.com>
2023-12-16 18:14:03 +0100

    i18n: FR translation update (#6046)

--- ui/raidboss/data/04-sb/deepdungeon/heaven-on-high_floors_21-30.ts
+++ ui/raidboss/data/04-sb/deepdungeon/heaven-on-high_floors_21-30.ts
@@ -38,6 +38,7 @@ const triggerSet: TriggerSet<Data> = {
         text: {
           en: 'Stand under a cloud',
           de: 'Steh unter einer Wolke',
+          fr: 'Restez sous un nuage',
           cn: '站在一朵云下',
           ko: '구름 아래에 서기',
         },
